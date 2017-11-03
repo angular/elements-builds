3b1023e785e5a1a77aa14d86f47d33f50896bb83
Victor Berchet <victor@suumit.com>
2017-11-03 19:11:08 +0000

    26f8299 fix(compiler): don't overwrite missingTranslation's value in JIT (#19952)

--- esm5/elements.js
+++ esm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-c7fd172
+ * @license Angular v5.1.0-beta.0-26f8299
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function registerAsCustomElements(customElementComponents, platformRefOrBootstra
 /**
  * \@experimental
  */
-var VERSION = new Version('5.1.0-beta.0-c7fd172');
+var VERSION = new Version('5.1.0-beta.0-26f8299');
 
 /**
  * @fileoverview added by tsickle
