77225776e176ff17789ab8899a6078563a250d16
Victor Berchet <victor@suumit.com>
2017-11-03 19:52:40 +0000

    a99eb16 test(aio): make e2e tests less flakey

--- esm5/elements.js
+++ esm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function registerAsCustomElements(customElementComponents, platformRefOrBootstra
 /**
  * \@experimental
  */
-var VERSION = new Version('5.1.0-beta.0-faa6212');
+var VERSION = new Version('5.1.0-beta.0-a99eb16');
 
 /**
  * @fileoverview added by tsickle
