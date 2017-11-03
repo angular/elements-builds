20c7e89c1df2c7c757ce0f295b50fb221e86c873
Victor Berchet <victor@suumit.com>
2017-11-03 19:48:59 +0000

    faa6212 feat(core): add source to `StaticInjectorError` message (#19482)

--- esm2015/elements.js
+++ esm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-567cc26
+ * @license Angular v5.1.0-beta.0-faa6212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -856,7 +856,7 @@ function registerAsCustomElements(customElementComponents, platformRefOrBootstra
 /**
  * \@experimental
  */
-const VERSION = new Version('5.1.0-beta.0-567cc26');
+const VERSION = new Version('5.1.0-beta.0-faa6212');
 
 /**
  * @fileoverview added by tsickle
