90add10d427c686b49a31ccce8277169bffc8559
Igor Minar <igor@angular.io>
2018-03-20 22:49:49 +0000

    3cc5c2e build: update to rxjs@6.0.0-beta.0 (#22887)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e6c731f
+ * @license Angular v6.0.0-beta.7-3cc5c2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-e6c731f
+ * @license Angular v6.0.0-beta.7-3cc5c2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-e6c731f');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-3cc5c2e');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
