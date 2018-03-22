28625770c347736ee785e26eefcad19cdb3ac9b0
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:24:44 +0000

    f88fba0 fix(animations): avoid animation insertions during router back/refresh (#21977)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-32105c8');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-f88fba0');
 //# sourceMappingURL=version.js.map
