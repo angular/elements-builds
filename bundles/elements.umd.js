d13dbfce5ca3104e09b7ba7da56dc49307de7472
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:16:49 +0000

    3a30f5d build(aio): remove rxjs-compat dependency from examples (#22872)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-5ab9d4d
+ * @license Angular v6.0.0-rc.0-3a30f5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-5ab9d4d
+ * @license Angular v6.0.0-rc.0-3a30f5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-5ab9d4d');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-3a30f5d');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
