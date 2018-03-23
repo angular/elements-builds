846f07b08bdf15dbe14266efaa3fe6c87b0289e5
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:47:53 +0000

    49396ca refactor(ivy): move directives into separate array (#22918)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-8ca26a9
+ * @license Angular v6.0.0-rc.0-49396ca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-8ca26a9
+ * @license Angular v6.0.0-rc.0-49396ca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-8ca26a9');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-49396ca');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
