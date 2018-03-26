b489dafbb31cf0bc3ec3e790974f8324bbacf87a
Matias Niemelä <matias@yearofmoo.com>
2018-03-26 23:00:56 +0000

    7a406a3 refactor: add a commit-msg git hook to check commit messages (#22969)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-bf6a416
+ * @license Angular v6.0.0-rc.0-7a406a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -709,7 +709,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.0-bf6a416');
+var VERSION = new Version('6.0.0-rc.0-7a406a3');
 
 /**
  * @fileoverview added by tsickle
