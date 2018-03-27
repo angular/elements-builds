4fa5a3c375e75ddf1438d40a398256a173e6f912
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 04:46:16 +0000

    2aabbc5 fix(bazel): ng_package packages attr not forwarded to npm_package (#22967)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-27e14b2
+ * @license Angular v6.0.0-rc.0-2aabbc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-27e14b2
+ * @license Angular v6.0.0-rc.0-2aabbc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-27e14b2');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-2aabbc5');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
