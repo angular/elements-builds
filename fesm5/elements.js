d4cf7d4b44df33cc9feedce1a85e244dcb4b8dc8
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 21:32:40 +0000

    d28ce50 test(ivy): make sure `goog.getMsg()` is defined before being used (#22998)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-ab348ee
+ * @license Angular v6.0.0-rc.0-d28ce50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -709,7 +709,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.0-ab348ee');
+var VERSION = new Version('6.0.0-rc.0-d28ce50');
 
 /**
  * @fileoverview added by tsickle
