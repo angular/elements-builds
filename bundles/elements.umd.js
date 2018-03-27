4db9933cccba8586b7d196198bcc92b8d2d2e049
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 04:47:44 +0000

    910a16a refactor(ivy): remove directive references from template (#22986)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-2aabbc5
+ * @license Angular v6.0.0-rc.0-910a16a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-2aabbc5
+ * @license Angular v6.0.0-rc.0-910a16a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-2aabbc5');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-910a16a');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
