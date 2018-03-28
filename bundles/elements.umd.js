b1c7630a1ec5f1b401d9d2dd3bad2c4776f3bf44
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:23:22 +0000

    d9dc46e fix(service-worker): ignore invalid `only-if-cached` requests (#22883)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-e2e80ec
+ * @license Angular v6.0.0-rc.0-d9dc46e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-e2e80ec
+ * @license Angular v6.0.0-rc.0-d9dc46e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-e2e80ec');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-d9dc46e');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
