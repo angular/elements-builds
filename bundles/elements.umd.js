517d6887e0bb4e83baa5372649f4db34ac6fc066
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:07:33 +0000

    6cb1adf ci(aio): add monitoring for angular.io (#22483)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-0c4e371
+ * @license Angular v6.0.0-rc.1-6cb1adf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-0c4e371
+ * @license Angular v6.0.0-rc.1-6cb1adf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-0c4e371');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-6cb1adf');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
