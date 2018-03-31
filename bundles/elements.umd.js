24031640d37f5f7c66e43284a2ea2e98b4f4c0aa
Igor Minar <igor@angular.io>
2018-03-31 00:52:17 +0000

    43d6202 build(aio): update to angular@6.0.0-rc.1 (#23101)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-d6bf71a
+ * @license Angular v6.0.0-rc.1-43d6202
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-d6bf71a
+ * @license Angular v6.0.0-rc.1-43d6202
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -741,7 +741,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-d6bf71a');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-43d6202');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
