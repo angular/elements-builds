2e43eaa842b14286e1b2a49bad8f270841007610
Igor Minar <igor@angular.io>
2018-04-05 19:23:54 +0000

    f3d2623 docs: add changelog for 6.0.0-rc.2

---
+++ esm2015/public_api.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export { NgElement, createCustomElement } from './src/create-custom-element';
+export { VERSION } from './src/version';
+// This file only reexports content of the `src` folder. Keep it that way.
+//# sourceMappingURL=public_api.js.map
