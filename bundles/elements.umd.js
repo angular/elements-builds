7ea03b5d58b35aaff650d4f13c91a1d2323906e2
Igor Minar <igor@angular.io>
2018-04-05 18:00:27 +0000

    3b607e6 refactor(aio): remove unused images (#23018)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ce43e96
+ * @license Angular v6.0.0-rc.1-3b607e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-ce43e96
+ * @license Angular v6.0.0-rc.1-3b607e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -749,7 +749,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-ce43e96');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-3b607e6');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
