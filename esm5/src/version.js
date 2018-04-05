e6068513c0baa4200f1df28348caf20c8a4210e0
Igor Minar <igor@angular.io>
2018-04-05 22:08:58 +0000

    5cd36c7 build: improve the publish-next script (#23206)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.2-f3d2623');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.2-5cd36c7');
 //# sourceMappingURL=version.js.map
