64ecab55227f46725a9a855e661a6eecbcedf280
Igor Minar <igor@angular.io>
2018-04-05 18:06:47 +0000

    fc813f6 fix(ivy): fix issue with refreshing embedded views (#23164)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-3b607e6
+ * @license Angular v6.0.0-rc.1-fc813f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-3b607e6
+ * @license Angular v6.0.0-rc.1-fc813f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -749,7 +749,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-3b607e6');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-fc813f6');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
