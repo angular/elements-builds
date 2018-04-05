503dc4b1f807f55d2c20cbc993017c66e84c9a0c
Igor Minar <igor@angular.io>
2018-04-05 01:09:25 +0000

    7c03961 test: update hello_world__closure to google-closure-compiler@20180319.0.0 (#23149)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-6699fb5');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-7c03961');
 //# sourceMappingURL=version.js.map
