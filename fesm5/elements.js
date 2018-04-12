41879a9337d432aeb1df317707e44778da17ffe3
Igor Minar <igor@angular.io>
2018-04-12 07:45:06 +0000

    1fc72e5 build(core): update zone.js peer dep to ~0.8.26

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-bbfa1d3
+ * @license Angular v6.0.0-rc.3-1fc72e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.3-bbfa1d3');
+var VERSION = new Version('6.0.0-rc.3-1fc72e5');
 
 /**
  * @fileoverview added by tsickle
