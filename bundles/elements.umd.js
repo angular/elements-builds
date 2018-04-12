58e621741640a1cc2705aff3b56dda5937064a2e
Igor Minar <igor@angular.io>
2018-04-12 07:32:41 +0000

    58faa0c build: reorder PACKAGE_GROUP entries to make @angular/core the first one (#23303)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-af46d09
+ * @license Angular v6.0.0-rc.3-58faa0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-af46d09
+ * @license Angular v6.0.0-rc.3-58faa0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -753,7 +753,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-af46d09');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-58faa0c');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
