20e835ff81cc9b7726d6d4e5e1e6a19185d05cc0
Igor Minar <igor@angular.io>
2018-04-13 23:52:44 +0000

    d1177c7 refactor: update CLDR data for closure locale & use a const for undefined (#23372) PR Close #23372

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-12f90ef');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-d1177c7');
 //# sourceMappingURL=version.js.map
