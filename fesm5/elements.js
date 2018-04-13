22c83c1bfe8050fd5aaac6a92f7a85dfe74c0a68
Igor Minar <igor@angular.io>
2018-04-13 06:38:53 +0000

    993eeab docs: fix typo in injected variable name (#23315)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-b8f4e43
+ * @license Angular v6.0.0-rc.4-993eeab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.4-b8f4e43');
+var VERSION = new Version('6.0.0-rc.4-993eeab');
 
 /**
  * @fileoverview added by tsickle
