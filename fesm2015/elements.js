bfa6d01bcb3515573c7a738f75a60f37eaa471cf
Igor Minar <igor@angular.io>
2018-04-13 21:04:43 +0000

    e7ef027 fix(ivy): local directives and pipes should be applied to TemplateRef (#23312)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-d5e7f60
+ * @license Angular v6.0.0-rc.4-e7ef027
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -594,7 +594,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.4-d5e7f60');
+const VERSION = new Version('6.0.0-rc.4-e7ef027');
 
 /**
  * @fileoverview added by tsickle
