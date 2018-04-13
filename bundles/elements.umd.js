98afb558c249135324d5b364bbbd7dbc55b58fd4
Igor Minar <igor@angular.io>
2018-04-13 06:43:42 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-993eeab
+ * @license Angular v6.0.0-rc.4-75febe7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-993eeab
+ * @license Angular v6.0.0-rc.4-75febe7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -753,7 +753,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-993eeab');
+var VERSION = new _angular_core.Version('6.0.0-rc.4-75febe7');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-993eeab');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-75febe7');
 //# sourceMappingURL=version.js.map
