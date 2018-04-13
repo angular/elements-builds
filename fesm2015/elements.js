1a354737aba1dfad211a7fb6fb927d86e84aab6f
Igor Minar <igor@angular.io>
2018-04-13 20:58:20 +0000

    d5e7f60 refactor(ivy): misc (#23351)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-6e73300
+ * @license Angular v6.0.0-rc.4-d5e7f60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -594,7 +594,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.4-6e73300');
+const VERSION = new Version('6.0.0-rc.4-d5e7f60');
 
 /**
  * @fileoverview added by tsickle
