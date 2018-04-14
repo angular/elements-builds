9cdd1d11c7bd6b0f3bd972c81b5a3199d7f47e28
Igor Minar <igor@angular.io>
2018-04-14 10:53:52 +0000

    8f690c9 docs: add changelog for 6.0.0-rc.5

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-674c3de
+ * @license Angular v6.0.0-rc.5-8f690c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-674c3de
+ * @license Angular v6.0.0-rc.5-8f690c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -753,7 +753,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-674c3de');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-8f690c9');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
