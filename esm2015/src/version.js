44fd51abf077589feec1e0aec5c05dea01803f41
Victor Berchet <victor@suumit.com>
2018-04-19 23:56:46 +0000

    f2563ca ci(compiler): replace chuckjaz with alxhub as compiler owner (#23456)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-9757347');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-f2563ca');
 //# sourceMappingURL=version.js.map
