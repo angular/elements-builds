e62e3a993b00c3d0fd5ccae8133793eb528a7bba
Victor Berchet <victor@suumit.com>
2018-04-22 19:20:32 +0000

    ba47997 style(compiler): fix lint issues (#23480)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-6761a64
+ * @license Angular v6.0.0-rc.5-ba47997
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.5-6761a64');
+var VERSION = new Version('6.0.0-rc.5-ba47997');
 
 /**
  * @fileoverview added by tsickle
