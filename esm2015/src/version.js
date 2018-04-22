1fdf300507adce528679d74e01b58c840d1659e1
Victor Berchet <victor@suumit.com>
2018-04-22 19:56:46 +0000

    02424ff Revert "style(compiler): fix lint issues (#23480)"

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-b9431e8');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-02424ff');
 //# sourceMappingURL=version.js.map
