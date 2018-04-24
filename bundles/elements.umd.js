807a6c0b76e3781569ed1057b3f80d620b7698bf
Victor Berchet <victor@suumit.com>
2018-04-24 21:20:52 +0000

    e1c4930 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+76.sha-b1d03fe
+ * @license Angular v6.0.0-rc.5+78.sha-e1c4930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+76.sha-b1d03fe');
+var VERSION = new core.Version('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @license
