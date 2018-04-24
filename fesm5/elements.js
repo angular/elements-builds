d471aa245c49e44d102ae4479abce27ecdd2ab99
Victor Berchet <victor@suumit.com>
2018-04-24 21:24:43 +0000

    0bc8443 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-db17231
+ * @license Angular v6.0.0-rc.5-0bc8443
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.5-db17231');
+var VERSION = new Version('6.0.0-rc.5-0bc8443');
 
 /**
  * @fileoverview added by tsickle
