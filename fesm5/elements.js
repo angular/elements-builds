f1a293c951725628bf0de69e653ea2adbe9168e9
Victor Berchet <victor@suumit.com>
2018-04-24 18:37:07 +0000

    540626a build(common): mark locales files as side-effect-full (#23509)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-391bfce
+ * @license Angular v6.0.0-rc.5-540626a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.5-391bfce');
+var VERSION = new Version('6.0.0-rc.5-540626a');
 
 /**
  * @fileoverview added by tsickle
