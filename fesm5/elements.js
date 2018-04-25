01c87e1f6942446a1631582abd94ecf9052394b5
Igor Minar <igor@angular.io>
2018-04-25 20:54:35 +0000

    db77d8d feat(ivy): support injection flags at runtime (#23518)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+83.sha-f567e18
+ * @license Angular v6.0.0-rc.5+85.sha-db77d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -542,7 +542,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+83.sha-f567e18');
+var VERSION = new Version('6.0.0-rc.5+85.sha-db77d8d');
 
 /**
  * @license
