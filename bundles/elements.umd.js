7bf67be6d6d16e258f82abbc38a80f87015b0431
Igor Minar <igor@angular.io>
2018-04-25 20:50:43 +0000

    f567e18 docs: update glossary architectural terms (#23045)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+84.sha-ab5bc42
+ * @license Angular v6.0.0-rc.5+83.sha-f567e18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+84.sha-ab5bc42');
+var VERSION = new core.Version('6.0.0-rc.5+83.sha-f567e18');
 
 /**
  * @license
