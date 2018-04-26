f6502df85d235aee0224d3781de90598dc15fcf9
Igor Minar <igor@angular.io>
2018-04-26 02:22:31 +0000

    b1f040f fix(compiler-cli): don't rely on incompatible TS method (#23550)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+87.sha-eb031c6
+ * @license Angular v6.0.0-rc.5+88.sha-b1f040f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+87.sha-eb031c6');
+var VERSION = new core.Version('6.0.0-rc.5+88.sha-b1f040f');
 
 /**
  * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+87.sha-eb031c6
+ * @license Angular v6.0.0-rc.5+88.sha-b1f040f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -542,7 +542,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+87.sha-eb031c6');
+var VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
 
 /**
  * @license
