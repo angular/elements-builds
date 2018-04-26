654776df77335198db6469935ecb4e2826a15ff3
Igor Minar <igor@angular.io>
2018-04-26 02:24:47 +0000

    1a44a0b feat(ivy): support lifecycle hooks of ViewContainerRef (#23396)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+88.sha-b1f040f
+ * @license Angular v6.0.0-rc.5+89.sha-1a44a0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
+const VERSION = new Version('6.0.0-rc.5+89.sha-1a44a0b');
 
 /**
  * @fileoverview added by tsickle
