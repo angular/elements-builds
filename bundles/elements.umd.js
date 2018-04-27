28e56016c1ff676026de05b9289eb6bbe2a18b2e
Igor Minar <igor@angular.io>
2018-04-27 14:47:59 +0000

    5021bdd ci: add Brandon Roberts as an aio approver (#23417)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-64bf6ed
+ * @license Angular v6.0.0-rc.5-5021bdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-64bf6ed
+ * @license Angular v6.0.0-rc.5-5021bdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -753,7 +753,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-64bf6ed');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-5021bdd');
 
 exports.NgElement = NgElement;
 exports.createCustomElement = createCustomElement;
