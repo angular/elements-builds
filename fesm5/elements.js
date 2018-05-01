07dea73ea515fdbd23e03a0703916d2945d7f055
Igor Minar <igor@angular.io>
2018-05-01 18:04:11 +0000

    f11daa2 docs(aio): update Egghead.io URL (#23598)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-31a435e
+ * @license Angular v6.0.0-rc.6-f11daa2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.6-31a435e');
+var VERSION = new Version('6.0.0-rc.6-f11daa2');
 
 /**
  * @fileoverview added by tsickle
