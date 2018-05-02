c1fd1fb6d7a51062e76c4d28c74b0a57f7f13d8b
Igor Minar <igor@angular.io>
2018-05-02 23:16:21 +0000

    91d4da0 docs: add missing link to bootstrapping section (#23214)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-22eb8e2');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-91d4da0');
 //# sourceMappingURL=version.js.map
