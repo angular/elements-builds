3c80395a9e908908a0f4b3a14d1ba300989a2869
Igor Minar <igor@angular.io>
2018-05-02 23:27:17 +0000

    983e5f2 fix(aio): correctly route embedded live-example URLs from SW (#23637)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-5fc4299');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-983e5f2');
 //# sourceMappingURL=version.js.map
