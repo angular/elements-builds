3a180dfcc7d3503171fba4521dafb8729f171e61
Igor Minar <igor@angular.io>
2018-05-03 20:07:51 +0000

    d0ccf5f release: cut the v6.0.0 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-ecde152
+ * @license Angular v6.0.0-d0ccf5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.6-ecde152');
+var VERSION = new Version('6.0.0-d0ccf5f');
 
 /**
  * @fileoverview added by tsickle
