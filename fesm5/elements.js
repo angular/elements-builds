7b6dd2356d4ceed8fe00e9c7b0347e4101f52e46
Igor Minar <igor@angular.io>
2018-05-03 18:19:04 +0000

    ecde152 build: update to rxjs@6.0.0 (#23679)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-983e5f2
+ * @license Angular v6.0.0-rc.6-ecde152
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-rc.6-983e5f2');
+var VERSION = new Version('6.0.0-rc.6-ecde152');
 
 /**
  * @fileoverview added by tsickle
