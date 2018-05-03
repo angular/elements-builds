1078dd03af43f516fbebd68aa330320b79602839
Igor Minar <igor@angular.io>
2018-05-03 21:49:25 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-5bd8c68
+ * @license Angular v6.0.0-9027818
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-5bd8c68');
+var VERSION = new Version('6.0.0-9027818');
 
 /**
  * @fileoverview added by tsickle
