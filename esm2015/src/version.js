9093122d20bc74390ee41ed8d2ad9ab0af517ca1
Igor Minar <igor@angular.io>
2018-05-04 23:20:51 +0000

    373a47d test: fix firebase deployment script test

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@experimental
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-83f12f3');
+export const /** @type {?} */ VERSION = new Version('6.0.0-373a47d');
 //# sourceMappingURL=version.js.map
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-83f12f3
+ * @license Angular v6.0.0-373a47d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -721,7 +721,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-var VERSION = new Version('6.0.0-83f12f3');
+var VERSION = new Version('6.0.0-373a47d');
 
 /**
  * @fileoverview added by tsickle
