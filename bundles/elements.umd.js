5fee969bf0f6121873267049e4543aa23eed0092
Igor Minar <igor@angular.io>
2018-05-08 21:19:36 +0000

    fe7f48c docs(aio): Upgrade example dependencies to Angular V6 (#23660)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+28.sha-5581e97
+ * @license Angular v6.0.0+30.sha-fe7f48c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0+28.sha-5581e97');
+var VERSION = new core.Version('6.0.0+30.sha-fe7f48c');
 
 /**
  * @license
