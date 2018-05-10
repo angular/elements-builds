14ae4ccc1c805662a66ff6fd7d9a4269aaeb2477
Igor Minar <igor@angular.io>
2018-05-10 15:20:50 +0000

    8733843 fix(router): correct the segment parsing so it won't break on ampersand (#23684)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+40.sha-f109791
+ * @license Angular v6.0.0+41.sha-8733843
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0+40.sha-f109791');
+const VERSION = new Version('6.0.0+41.sha-8733843');
 
 /**
  * @fileoverview added by tsickle
