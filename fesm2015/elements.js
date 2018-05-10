6e1eaa1a1aaf43138bda07f32a25fbadd86fffda
Igor Minar <igor@angular.io>
2018-05-10 19:27:43 +0000

    484233f docs(aio): add Alain Chautard in GDE list (#23783)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+42.sha-3d8799b
+ * @license Angular v6.0.0+43.sha-484233f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0+42.sha-3d8799b');
+const VERSION = new Version('6.0.0+43.sha-484233f');
 
 /**
  * @fileoverview added by tsickle
