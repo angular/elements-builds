8234c7f1f9eecf003d1e1480996e295e31eaa541
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 21:57:39 +0000

    cfde36d fix(compiler): generate constant array for i18n attributes (#23837)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+173.sha-d889f57
+ * @license Angular v6.0.0-rc.5+174.sha-cfde36d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+173.sha-d889f57');
+var VERSION = new core.Version('6.0.0-rc.5+174.sha-cfde36d');
 
 /**
  * @license
