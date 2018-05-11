e36dca261c8ad912b86e81ada8d5e9c7a1a4a7c5
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 21:05:45 +0000

    816bc8a feat(ivy): support injectable sanitization service (#23809)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+171.sha-d2a8687
+ * @license Angular v6.0.0-rc.5+172.sha-816bc8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+171.sha-d2a8687');
+const VERSION = new Version('6.0.0-rc.5+172.sha-816bc8a');
 
 /**
  * @fileoverview added by tsickle
