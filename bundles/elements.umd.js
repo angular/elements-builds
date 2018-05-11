e0150f3543cdebc0cb7aab52a3fd3b94452c2ba0
Igor Minar <igor@angular.io>
2018-05-11 06:23:01 +0000

    4ecae64 build: replace the old publish script with a new bazel-based one

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+166.sha-5e307d5
+ * @license Angular v6.0.0-rc.5+167.sha-4ecae64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+166.sha-5e307d5');
+var VERSION = new core.Version('6.0.0-rc.5+167.sha-4ecae64');
 
 /**
  * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+166.sha-5e307d5
+ * @license Angular v6.0.0-rc.5+167.sha-4ecae64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+166.sha-5e307d5');
+const VERSION = new Version('6.0.0-rc.5+167.sha-4ecae64');
 
 /**
  * @fileoverview added by tsickle
