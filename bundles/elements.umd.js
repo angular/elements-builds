a2f4261f4f66915248b7f9ce15fd4a648caf8607
Igor Minar <igor@angular.io>
2018-05-11 06:22:05 +0000

    fd880a8 build: replace the old publish script with a new bazel-based one

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+49.sha-1336a94
+ * @license Angular v6.0.0+51.sha-fd880a8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0+49.sha-1336a94');
+var VERSION = new core.Version('6.0.0+51.sha-fd880a8');
 
 /**
  * @license
