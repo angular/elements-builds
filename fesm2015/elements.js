eb98b5a433ace2a68906546bf28cb1bcdb8f914a
Igor Minar <igor@angular.io>
2018-05-11 00:07:55 +0000

    089fe83 build: update to latest TypeScript rules (#23828)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+164.sha-b1cda36
+ * @license Angular v6.0.0-rc.5+165.sha-089fe83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+164.sha-b1cda36');
+const VERSION = new Version('6.0.0-rc.5+165.sha-089fe83');
 
 /**
  * @fileoverview added by tsickle
