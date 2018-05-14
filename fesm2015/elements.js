f7c190e6f0984a6942ba72b08aba79a0aeaef88d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 22:56:59 +0000

    d4b6c41 fix(benchpress): Fix promise chain in chrome_driver_extension. (#23458)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+179.sha-66b2d78
+ * @license Angular v6.0.0-rc.5+180.sha-d4b6c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+179.sha-66b2d78');
+const VERSION = new Version('6.0.0-rc.5+180.sha-d4b6c41');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+179.sha-66b2d78
+ * @license Angular v6.0.0-rc.5+180.sha-d4b6c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+179.sha-66b2d78');
+var VERSION = new Version('6.0.0-rc.5+180.sha-d4b6c41');
 
 /**
  * @license
