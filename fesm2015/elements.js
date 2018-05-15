7dea6916ba790e90747d51dcbbf7e4a4d292e4c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 18:59:41 +0000

    83631b2 perf(ivy): avoid creating bound function in pipeBind3 (#23882)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+180.sha-d4b6c41
+ * @license Angular v6.0.0-rc.5+181.sha-83631b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+180.sha-d4b6c41');
+const VERSION = new Version('6.0.0-rc.5+181.sha-83631b2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+180.sha-d4b6c41
+ * @license Angular v6.0.0-rc.5+181.sha-83631b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+180.sha-d4b6c41');
+var VERSION = new Version('6.0.0-rc.5+181.sha-83631b2');
 
 /**
  * @license
