0ad105cf6cf53b0d447f86118a3e5349c9a8d778
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 22:59:05 +0000

    95fca24 feat(aio): add brand and concept icons, img style class more flexible (#23589)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2
+ * @license Angular v6.0.2+1.sha-95fca24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.2');
+const VERSION = new Version('6.0.2+1.sha-95fca24');
 
 /**
  * @fileoverview added by tsickle
