494634772b104f9e8c556fbf4965d4e45b1d47fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 20:09:11 +0000

    a8f6542 release: cut the v6.0.2 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.1+12.sha-c6b618d
+ * @license Angular v6.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.1+12.sha-c6b618d');
+const VERSION = new Version('6.0.2');
 
 /**
  * @fileoverview added by tsickle
