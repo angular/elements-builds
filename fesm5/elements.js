06fa04f69291055f928d6dac577b75256b8003d1
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 21:59:02 +0000

    68bd45b docs(bazel): add a link to the Bazel doc (#22940)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+2.sha-0f64077
+ * @license Angular v6.0.2+3.sha-68bd45b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.2+2.sha-0f64077');
+var VERSION = new Version('6.0.2+3.sha-68bd45b');
 
 /**
  * @license
