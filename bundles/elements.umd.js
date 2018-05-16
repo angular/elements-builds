558eac5cf3b1f632cdd5fa782d99a5f0ae82990e
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:38 +0000

    091b11a docs(aio): update HTTP error test example (#22844)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+189.sha-4042a84
+ * @license Angular v6.0.0-rc.5+190.sha-091b11a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+189.sha-4042a84');
+var VERSION = new core.Version('6.0.0-rc.5+190.sha-091b11a');
 
 /**
  * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+189.sha-4042a84
+ * @license Angular v6.0.0-rc.5+190.sha-091b11a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+189.sha-4042a84');
+const VERSION = new Version('6.0.0-rc.5+190.sha-091b11a');
 
 /**
  * @fileoverview added by tsickle
