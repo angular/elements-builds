8a0b8cb70812d845d949294c8a01a3d6c676f247
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:09:18 +0000

    8ee25e6 ci: ensure github-robot listens on circleci builds (#23863) PR Close #23863

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+193.sha-4359727
+ * @license Angular v6.0.0-rc.5+194.sha-8ee25e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+193.sha-4359727');
+var VERSION = new Version('6.0.0-rc.5+194.sha-8ee25e6');
 
 /**
  * @license
