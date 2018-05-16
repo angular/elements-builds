025753d08ba412ed690b089926f21b10a43254e6
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:03:14 +0000

    f3361ab test: don't run unit tests on Firefox (#23942)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+5.sha-41c2030
+ * @license Angular v6.0.2+7.sha-f3361ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.2+5.sha-41c2030');
+const VERSION = new Version('6.0.2+7.sha-f3361ab');
 
 /**
  * @fileoverview added by tsickle
