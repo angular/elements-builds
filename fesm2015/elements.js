7bf7b8c0b7fbee371b745bb83acb6dead516ca88
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:02:33 +0000

    5510341 docs(aio): add Angular Conf Australia to events (#22929)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+191.sha-547efb5
+ * @license Angular v6.0.0-rc.5+192.sha-5510341
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+191.sha-547efb5');
+const VERSION = new Version('6.0.0-rc.5+192.sha-5510341');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+191.sha-547efb5
+ * @license Angular v6.0.0-rc.5+192.sha-5510341
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+191.sha-547efb5');
+var VERSION = new Version('6.0.0-rc.5+192.sha-5510341');
 
 /**
  * @license
