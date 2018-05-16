1c9a7f039b4d0d19d252c1a0a5afa5e5e32f4168
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:28 +0000

    daee41a docs(aio): update HTTP error test example (#22844)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+3.sha-68bd45b
+ * @license Angular v6.0.2+4.sha-daee41a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.2+3.sha-68bd45b');
+const VERSION = new Version('6.0.2+4.sha-daee41a');
 
 /**
  * @fileoverview added by tsickle
