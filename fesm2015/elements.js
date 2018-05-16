6f1d09bcf92954dabd6095a585d6c34c513eb02c
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:13:46 +0000

    43ee10f docs(aio): changed 'onVoted' output property to 'voted' to be in line with the styleguide (#23832)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+8.sha-5d318ff
+ * @license Angular v6.0.2+10.sha-43ee10f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.2+8.sha-5d318ff');
+const VERSION = new Version('6.0.2+10.sha-43ee10f');
 
 /**
  * @fileoverview added by tsickle
