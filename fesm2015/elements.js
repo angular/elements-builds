79250f4be70acb9c2df84e2620b876806db8d038
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:54 +0000

    547efb5 docs(aio): fix path to observables guide (#23858) PR Close #23858

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+190.sha-091b11a
+ * @license Angular v6.0.0-rc.5+191.sha-547efb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+190.sha-091b11a');
+const VERSION = new Version('6.0.0-rc.5+191.sha-547efb5');
 
 /**
  * @fileoverview added by tsickle
