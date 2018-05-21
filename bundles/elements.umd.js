afeb472275cd1456ebfaad8fad711b741651475c
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:45:43 +0000

    95344d6 fix(aio): avoid unnecessary re-calculations in live-examples (#23960)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.2+17.sha-eec231d');
+var VERSION = new core.Version('6.0.2+16.sha-95344d6');
 
 /**
  * @license
