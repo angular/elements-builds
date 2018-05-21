bd3ac835ae9e35eb8b69fc1e5a16aa266b9f8a4a
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:49:00 +0000

    4226070 docs(aio): Add missing dependencies and files to testing zip file download (#23948)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+16.sha-95344d6
+ * @license Angular v6.0.2+18.sha-4226070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.2+16.sha-95344d6');
+const VERSION = new Version('6.0.2+18.sha-4226070');
 
 /**
  * @fileoverview added by tsickle
