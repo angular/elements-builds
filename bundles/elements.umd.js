8ff9f8e5b10eb9416367ffa1bfceb0aa8e0434f9
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:43:10 +0000

    de267e9 docs(aio): add ant design of angular in resources (#23953)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+203.sha-f8c6947
+ * @license Angular v6.0.0-rc.5+204.sha-de267e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+203.sha-f8c6947');
+var VERSION = new core.Version('6.0.0-rc.5+204.sha-de267e9');
 
 /**
  * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+203.sha-f8c6947
+ * @license Angular v6.0.0-rc.5+204.sha-de267e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+203.sha-f8c6947');
+var VERSION = new Version('6.0.0-rc.5+204.sha-de267e9');
 
 /**
  * @license
