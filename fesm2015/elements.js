70b2c39b6488519d6f8c681a1e1d3d69766c5d02
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:16:54 +0000

    3d6e82e docs(aio): remove outdated rangle link (#24108) PR Close #24108

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3
+ * @license Angular v6.0.3+1.sha-3d6e82e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.3');
+const VERSION = new Version('6.0.3+1.sha-3d6e82e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3
+ * @license Angular v6.0.3+1.sha-3d6e82e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3');
+var VERSION = new Version('6.0.3+1.sha-3d6e82e');
 
 /**
  * @license
