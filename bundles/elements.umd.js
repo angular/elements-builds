a2c04c9104880839c86adb52d6e380838b05cfbe
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:17:12 +0000

    186118e docs(aio): remove outdated rangle link (#24108) PR Close #24108

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+221.sha-01b5acd
+ * @license Angular v6.0.0-rc.5+220.sha-186118e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+221.sha-01b5acd');
+var VERSION = new core.Version('6.0.0-rc.5+220.sha-186118e');
 
 /**
  * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+221.sha-01b5acd
+ * @license Angular v6.0.0-rc.5+220.sha-186118e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+221.sha-01b5acd');
+const VERSION = new Version('6.0.0-rc.5+220.sha-186118e');
 
 /**
  * @fileoverview added by tsickle
