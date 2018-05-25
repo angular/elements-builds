339a03608e83cb80eeb6096be794a789f66cf7b2
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:26:37 +0000

    729c797 fix(ivy): `pipeBindV` takes an array of values (#24039)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+223.sha-3e39fef
+ * @license Angular v6.0.0-rc.5+228.sha-729c797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+223.sha-3e39fef');
+var VERSION = new Version('6.0.0-rc.5+228.sha-729c797');
 
 /**
  * @license
