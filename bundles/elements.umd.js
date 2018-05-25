183e5bb527a927086a64efeea05aa8a8269656e5
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:23:22 +0000

    a638f50 docs(aio): add material community components (#24042)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+3.sha-a29c756
+ * @license Angular v6.0.3+4.sha-a638f50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.3+3.sha-a29c756');
+var VERSION = new core.Version('6.0.3+4.sha-a638f50');
 
 /**
  * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+3.sha-a29c756
+ * @license Angular v6.0.3+4.sha-a638f50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+3.sha-a29c756');
+var VERSION = new Version('6.0.3+4.sha-a638f50');
 
 /**
  * @license
