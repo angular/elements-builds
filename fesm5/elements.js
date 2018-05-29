7c9c58afd44bd8e20bb7ab508996925af1369130
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:16:59 +0000

    d74078f docs(http): correct spelling error (#23675)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+233.sha-ddd6124
+ * @license Angular v6.0.0-rc.5+234.sha-d74078f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+233.sha-ddd6124');
+var VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
 
 /**
  * @license
