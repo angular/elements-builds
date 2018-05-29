f68fcd5e79765c145cfe84a33e9af0695355200d
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:14:25 +0000

    84b43da build: pick up rules_typescript karma fix (#24184)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+9.sha-a583d12
+ * @license Angular v6.0.3+7.sha-84b43da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.3+9.sha-a583d12');
+var VERSION = new core.Version('6.0.3+7.sha-84b43da');
 
 /**
  * @license
