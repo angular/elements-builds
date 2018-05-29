a68124aeec77a50ada0351f093d4ff94e6744ab9
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:44:10 +0000

    3fd3c2a test(animations): fix Node.js detection in animation tests (#24139)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+243.sha-1eafd04
+ * @license Angular v6.0.0-rc.5+244.sha-3fd3c2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+243.sha-1eafd04');
+var VERSION = new Version('6.0.0-rc.5+244.sha-3fd3c2a');
 
 /**
  * @license
