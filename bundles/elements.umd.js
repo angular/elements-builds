95ed0854104c4d7c87a2ed9a56f3c7a0979cba77
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:15:30 +0000

    ddd6124 feat(aio): display code-tabs in a Material Design "card" (#24027)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+231.sha-e43d3fa
+ * @license Angular v6.0.0-rc.5+233.sha-ddd6124
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+231.sha-e43d3fa');
+var VERSION = new core.Version('6.0.0-rc.5+233.sha-ddd6124');
 
 /**
  * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+231.sha-e43d3fa
+ * @license Angular v6.0.0-rc.5+233.sha-ddd6124
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+231.sha-e43d3fa');
+const VERSION = new Version('6.0.0-rc.5+233.sha-ddd6124');
 
 /**
  * @fileoverview added by tsickle
