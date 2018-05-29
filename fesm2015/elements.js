f5160b57ab5a51a9d3e167dfa98d104a662f403a
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:28:11 +0000

    1eafd04 build(ivy): support alternate compilation modes to enable Ivy testing (#24056)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+241.sha-c2e1311
+ * @license Angular v6.0.0-rc.5+243.sha-1eafd04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+241.sha-c2e1311');
+const VERSION = new Version('6.0.0-rc.5+243.sha-1eafd04');
 
 /**
  * @fileoverview added by tsickle
