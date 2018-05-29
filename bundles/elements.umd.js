4330e85da18a608799989db26769dd097ca9d24d
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:13:24 +0000

    31795b6 style(compiler): fix typo and formatting (#23729)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+245.sha-41cd8f3
+ * @license Angular v6.0.0-rc.5+246.sha-31795b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+245.sha-41cd8f3');
+var VERSION = new core.Version('6.0.0-rc.5+246.sha-31795b6');
 
 /**
  * @license
