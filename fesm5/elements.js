26fa487872bd76dcea213f96768b1cd1d4ddd611
Victor Berchet <victor@suumit.com>
2018-05-30 17:33:24 +0000

    f6f44ed docs: update ivy perf notes (#24035)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+247.sha-e53179e
+ * @license Angular v6.0.0-rc.5+250.sha-f6f44ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
+var VERSION = new Version('6.0.0-rc.5+250.sha-f6f44ed');
 
 /**
  * @license
