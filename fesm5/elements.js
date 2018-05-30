2988a6f090de3423d754e585d00c4e25b9a95fb6
Victor Berchet <victor@suumit.com>
2018-05-30 22:01:19 +0000

    e9f2203 fix(platform-server): avoid clash between server and client style encapsulation attributes (#24158)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+29.sha-f04aef4
+ * @license Angular v6.0.3+33.sha-e9f2203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+29.sha-f04aef4');
+var VERSION = new Version('6.0.3+33.sha-e9f2203');
 
 /**
  * @license
