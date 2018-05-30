067a7ec2db16d686bbcae0bc097be30922d3f67c
Victor Berchet <victor@suumit.com>
2018-05-30 17:46:56 +0000

    29dfc8f docs(bazel): improve error message for ng_package with no metadata (#22964)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+22.sha-b5533e0
+ * @license Angular v6.0.3+23.sha-29dfc8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.3+22.sha-b5533e0');
+const VERSION = new Version('6.0.3+23.sha-29dfc8f');
 
 /**
  * @fileoverview added by tsickle
