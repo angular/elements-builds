e4d89cf2e57ffdf7d495dc4b8f9e8bd4b331eba0
Victor Berchet <victor@suumit.com>
2018-05-30 19:24:34 +0000

    4d25705 docs(aio): fix typo for @NgModuledecorator to @NgModule decorator (#24201)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+23.sha-29dfc8f
+ * @license Angular v6.0.3+26.sha-4d25705
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+23.sha-29dfc8f');
+var VERSION = new Version('6.0.3+26.sha-4d25705');
 
 /**
  * @license
