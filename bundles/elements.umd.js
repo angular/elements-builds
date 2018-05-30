b57b8277860a92193140d5b60f1bd80d98e9210f
Victor Berchet <victor@suumit.com>
2018-05-30 19:27:26 +0000

    d249852 build: update rules_webtesting (#24198)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+27.sha-707dd59
+ * @license Angular v6.0.3+28.sha-d249852
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.3+27.sha-707dd59');
+var VERSION = new core.Version('6.0.3+28.sha-d249852');
 
 /**
  * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+27.sha-707dd59
+ * @license Angular v6.0.3+28.sha-d249852
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+27.sha-707dd59');
+var VERSION = new Version('6.0.3+28.sha-d249852');
 
 /**
  * @license
