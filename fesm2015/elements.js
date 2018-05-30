c13038bec16da5133dfd48c1c9cb30dafd71b66f
Matias Niemelä <matias@yearofmoo.com>
2018-05-30 06:04:49 +0000

    e53179e refactor(ivy): move parent from LNode to TNode (#24189)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+246.sha-31795b6
+ * @license Angular v6.0.0-rc.5+247.sha-e53179e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+246.sha-31795b6');
+const VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+246.sha-31795b6
+ * @license Angular v6.0.0-rc.5+247.sha-e53179e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+246.sha-31795b6');
+var VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
 
 /**
  * @license
