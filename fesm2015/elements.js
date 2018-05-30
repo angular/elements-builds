57d0bc2fe1889f638a7b434168da4a3f5d932906
Victor Berchet <victor@suumit.com>
2018-05-30 17:52:51 +0000

    d6595eb feat(platform-server): use EventManagerPlugin on the server (#24132)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
+ * @license Angular v6.0.0-rc.5+255.sha-d6595eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
+const VERSION = new Version('6.0.0-rc.5+255.sha-d6595eb');
 
 /**
  * @fileoverview added by tsickle
