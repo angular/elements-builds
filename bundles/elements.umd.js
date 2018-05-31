12a296acb8148db5ad8cf84a7457ad0834e3d04b
Victor Berchet <victor@suumit.com>
2018-05-31 00:26:55 +0000

    5d6074e docs: fix typo (#24210)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+275.sha-b492b9e
+ * @license Angular v6.0.0-rc.5+282.sha-5d6074e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.0.0-rc.5+275.sha-b492b9e');
+var VERSION = new core.Version('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @license
