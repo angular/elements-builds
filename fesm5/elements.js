32451532810a0a83f67231d1b06b7a0ff40135f0
Victor Berchet <victor@suumit.com>
2018-05-31 17:44:15 +0000

    43e3073 build: update to rules_nodejs 0.9.1 and rules_typescript 0.15.0 (#24212)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+47.sha-c17098d
+ * @license Angular v6.0.3+46.sha-43e3073
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+47.sha-c17098d');
+var VERSION = new Version('6.0.3+46.sha-43e3073');
 
 /**
  * @license
