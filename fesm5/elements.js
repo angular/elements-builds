940a9a908d2e89485cd36d42d786883f2ad1b05b
Victor Berchet <victor@suumit.com>
2018-05-31 00:00:58 +0000

    b492b9e fix(animations): Fix browser detection logic (#24188)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+274.sha-b99ef2b
+ * @license Angular v6.0.0-rc.5+275.sha-b492b9e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -558,7 +558,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+274.sha-b99ef2b');
+var VERSION = new Version('6.0.0-rc.5+275.sha-b492b9e');
 
 /**
  * @license
