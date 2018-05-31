6a05794ab47c8cfbbb6c21735813faebeda8d4f0
Victor Berchet <victor@suumit.com>
2018-05-31 17:34:37 +0000

    6a663a4 fix(platform-server): don't reflect innerHTML property to attibute (#24213)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+286.sha-ec57133
+ * @license Angular v6.0.0-rc.5+287.sha-6a663a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+286.sha-ec57133');
+const VERSION = new Version('6.0.0-rc.5+287.sha-6a663a4');
 
 /**
  * @fileoverview added by tsickle
