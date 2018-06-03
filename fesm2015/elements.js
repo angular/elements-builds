0729be3ae08ade373e32ecc22e7a19ee659bc6a3
Victor Berchet <victor@suumit.com>
2018-06-03 02:55:47 +0000

    cb65724 refactor(ivy): combine lifecycleStage with LViewFlags (#24263)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+299.sha-0561b66
+ * @license Angular v6.0.0-rc.5+302.sha-cb65724
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+299.sha-0561b66');
+const VERSION = new Version('6.0.0-rc.5+302.sha-cb65724');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+299.sha-0561b66
+ * @license Angular v6.0.0-rc.5+302.sha-cb65724
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+299.sha-0561b66');
+var VERSION = new Version('6.0.0-rc.5+302.sha-cb65724');
 
 /**
  * @license
