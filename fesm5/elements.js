7cda06bcf41f34e5b1b30e4ac5ec4c90f1a8cd2f
Victor Berchet <victor@suumit.com>
2018-06-04 17:58:53 +0000

    efe49c1 docs(aio): clean up frequent ng-modules (#24025)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+54.sha-a3d9878
+ * @license Angular v6.0.3+55.sha-efe49c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+54.sha-a3d9878');
+var VERSION = new Version('6.0.3+55.sha-efe49c1');
 
 /**
  * @license
