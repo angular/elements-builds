834b7e9fbef49d84f4674cedd65ac528f7311003
Victor Berchet <victor@suumit.com>
2018-06-05 22:38:02 +0000

    20c463e feat(router): add navigation execution context info to activation hooks (#24204)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+317.sha-57eacf4
+ * @license Angular v6.0.0-rc.5+318.sha-20c463e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+317.sha-57eacf4');
+var VERSION = new Version('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @license
