18e61347012c10eb71a05e2645d8fe4f99393e90
Victor Berchet <victor@suumit.com>
2018-06-05 22:36:19 +0000

    57eacf4 refactor(ivy): move LView.template and component templates to TView (#24300)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+316.sha-d814eaa
+ * @license Angular v6.0.0-rc.5+317.sha-57eacf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+316.sha-d814eaa');
+var VERSION = new Version('6.0.0-rc.5+317.sha-57eacf4');
 
 /**
  * @license
