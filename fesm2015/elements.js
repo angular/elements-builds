be326bc0e5e6d4f95aa15c3b20c9ae439c1a671c
Victor Berchet <victor@suumit.com>
2018-06-06 19:00:27 +0000

    acf270d feat(ivy): added new namespace and element instructions to JIT environment (#23899)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+324.sha-c494d3c
+ * @license Angular v6.0.0-rc.5+335.sha-acf270d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+324.sha-c494d3c');
+const VERSION = new Version('6.0.0-rc.5+335.sha-acf270d');
 
 /**
  * @fileoverview added by tsickle
