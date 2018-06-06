95cb44535c8f01554a4a34b38e67adb6d38d2e11
Victor Berchet <victor@suumit.com>
2018-06-06 00:56:55 +0000

    a9222c0 docs(aio): Add null type to form validation example (#23949)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+62.sha-ea3127e
+ * @license Angular v6.0.3+63.sha-a9222c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+62.sha-ea3127e');
+var VERSION = new Version('6.0.3+63.sha-a9222c0');
 
 /**
  * @license
