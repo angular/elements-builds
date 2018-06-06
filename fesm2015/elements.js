406be80ba7c3ea35334744e5626d0368ca44e4f0
Victor Berchet <victor@suumit.com>
2018-06-06 01:52:34 +0000

    8db928d fix(animations): retain trigger-state for nodes that are moved around (#24238)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+322.sha-86b13cc
+ * @license Angular v6.0.0-rc.5+321.sha-8db928d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
+const VERSION = new Version('6.0.0-rc.5+321.sha-8db928d');
 
 /**
  * @fileoverview added by tsickle
