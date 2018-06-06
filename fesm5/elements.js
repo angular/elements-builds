3aebf821e63ca46792d11fbfc42120ae44ec7722
Victor Berchet <victor@suumit.com>
2018-06-06 01:52:24 +0000

    86b13cc refactor(ivy): move static parts of LView.cleanup to TView (#24301)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+320.sha-9367e91
+ * @license Angular v6.0.0-rc.5+322.sha-86b13cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.0-rc.5+320.sha-9367e91');
+var VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
 
 /**
  * @license
