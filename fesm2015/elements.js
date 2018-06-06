a7207c421f6067434e8b971cabc33be74c01d891
Victor Berchet <victor@suumit.com>
2018-06-06 05:35:24 +0000

    c494d3c Revert "feat(ivy): add support of ApplicationRef.bootstrapModuleFactory (#23811)"

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+323.sha-22b58a7
+ * @license Angular v6.0.0-rc.5+324.sha-c494d3c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.0-rc.5+323.sha-22b58a7');
+const VERSION = new Version('6.0.0-rc.5+324.sha-c494d3c');
 
 /**
  * @fileoverview added by tsickle
