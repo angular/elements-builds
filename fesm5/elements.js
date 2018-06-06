3d69dfb5f2e3e9f3359a8de0c2bb86958eeb8cc2
Victor Berchet <victor@suumit.com>
2018-06-06 17:49:54 +0000

    0ae8ea2 build(aio): ensure the correct decorator properties are merged (#24289)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+65.sha-19deca1
+ * @license Angular v6.0.3+66.sha-0ae8ea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+65.sha-19deca1');
+var VERSION = new Version('6.0.3+66.sha-0ae8ea2');
 
 /**
  * @license
