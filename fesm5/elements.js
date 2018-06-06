bc88311f8652ef0673abfebf0c6b485bdd685377
Victor Berchet <victor@suumit.com>
2018-06-06 19:27:21 +0000

    41698bf release: cut the v6.0.4 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+67.sha-23c50e2
+ * @license Angular v6.0.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.3+67.sha-23c50e2');
+var VERSION = new Version('6.0.4');
 
 /**
  * @license
