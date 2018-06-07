adde493965d6ca6d2e1823749317308b0e16791c
Miško Hevery <misko@hevery.com>
2018-06-07 22:20:30 +0000

    a6e797b build(bazel): fix ng_package rollup root dir for fesm2015 output (#24298)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+15.sha-ca79e11
+ * @license Angular v6.1.0-beta.0+16.sha-a6e797b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.0+15.sha-ca79e11');
+const VERSION = new Version('6.1.0-beta.0+16.sha-a6e797b');
 
 /**
  * @fileoverview added by tsickle
