b494f55d5884b7864021f3fdc21c8cfd9f5b7ae1
Miško Hevery <misko@hevery.com>
2018-06-07 23:10:33 +0000

    ce1543f docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+17.sha-ce1543f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.0+16.sha-a6e797b');
+var VERSION = new Version('6.1.0-beta.0+17.sha-ce1543f');
 
 /**
  * @license
