ee6fe6985760617ac1bf38c0831c8a99909e7778
Miško Hevery <misko@hevery.com>
2018-06-08 23:03:27 +0000

    8c1ac28 feat(ivy): now supports SVG and MathML elements (#24377)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+24.sha-5ef7a07
+ * @license Angular v6.1.0-beta.0+25.sha-8c1ac28
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -471,7 +471,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-beta.0+24.sha-5ef7a07');
+const VERSION = new Version('6.1.0-beta.0+25.sha-8c1ac28');
 
 /**
  * @license
