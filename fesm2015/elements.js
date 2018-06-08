9869f12a3d54353cb2e8a74d1925ad6daaa7d51a
Miško Hevery <misko@hevery.com>
2018-06-08 21:29:23 +0000

    9e61ad8 build(docs-infra): ensure stability is computed before the API list (#24356)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+5.sha-e876535
+ * @license Angular v6.0.4+4.sha-9e61ad8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.4+5.sha-e876535');
+const VERSION = new Version('6.0.4+4.sha-9e61ad8');
 
 /**
  * @fileoverview added by tsickle
