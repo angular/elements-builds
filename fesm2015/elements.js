723e3e25ee924e7f1d70b5210d732ec9aba495cf
Miško Hevery <misko@hevery.com>
2018-06-09 00:06:53 +0000

    49c5234 feat(router): implement scrolling restoration service (#20030)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+23.sha-1135563
+ * @license Angular v6.1.0-beta.0+27.sha-49c5234
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.0+23.sha-1135563');
+const VERSION = new Version('6.1.0-beta.0+27.sha-49c5234');
 
 /**
  * @fileoverview added by tsickle
