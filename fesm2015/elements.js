d87330154e469e5b3f57c12e143267c4acd0ac4d
Miško Hevery <misko@hevery.com>
2018-06-11 18:29:46 +0000

    ea2987c fix(service-worker): fix `SwPush.unsubscribe()` (#24162)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+7.sha-0139173
+ * @license Angular v6.0.4+13.sha-ea2987c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.4+7.sha-0139173');
+const VERSION = new Version('6.0.4+13.sha-ea2987c');
 
 /**
  * @fileoverview added by tsickle
