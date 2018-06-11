107026fb394e39259d23ddfd525fe1f5111020cb
Miško Hevery <misko@hevery.com>
2018-06-11 16:50:29 +0000

    5e8bf2f build(docs-infra): ensure `dist/` directory is cleaned before running `tsc --watch` (#24372)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+const VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @fileoverview added by tsickle
