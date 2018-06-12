c0dffc007034341db76ad4f881d98ed75696c47a
Miško Hevery <misko@hevery.com>
2018-06-12 19:04:31 +0000

    ea3669e fix(bazel): Allow ng_module to depend on targets w no deps (#24446)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+13.sha-ea2987c
+ * @license Angular v6.0.4+14.sha-ea3669e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.4+13.sha-ea2987c');
+const VERSION = new Version('6.0.4+14.sha-ea3669e');
 
 /**
  * @fileoverview added by tsickle
