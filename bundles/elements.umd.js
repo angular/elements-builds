ad44a1b7274e50b0f05c0b790e4aaa5056247d8f
Miško Hevery <misko@hevery.com>
2018-06-13 18:02:58 +0000

    70ef061 fix(ivy): remove debugger statement (#24480)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
+ * @license Angular v6.1.0-beta.0+43.sha-70ef061
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -559,7 +559,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new core.Version('6.1.0-beta.0+42.sha-c2b5ebf');
+var VERSION = new core.Version('6.1.0-beta.0+43.sha-70ef061');
 
 /**
  * @license
