deaf45854f2956e599df9649287c583a54395923
Miško Hevery <misko@hevery.com>
2018-06-13 21:07:31 +0000

    9a9a7de docs: add message property to compose-message component (#24310)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+15.sha-451d996
+ * @license Angular v6.0.4+22.sha-9a9a7de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.4+15.sha-451d996');
+const VERSION = new Version('6.0.4+22.sha-9a9a7de');
 
 /**
  * @fileoverview added by tsickle
