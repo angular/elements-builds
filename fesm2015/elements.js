562f97ed133efccea1655f07c8e570df07243978
Miško Hevery <misko@hevery.com>
2018-06-14 00:26:03 +0000

    39af314 build(aio): add github links to API doc members (#24000)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+1.sha-03f93b3
+ * @license Angular v6.1.0-beta.1+22.sha-39af314
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.1+1.sha-03f93b3');
+const VERSION = new Version('6.1.0-beta.1+22.sha-39af314');
 
 /**
  * @fileoverview added by tsickle
