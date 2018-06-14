636998f0089aeb27303f91fefb984a9550974661
Miško Hevery <misko@hevery.com>
2018-06-14 21:45:07 +0000

    7c8159b test(bazel): fix flakey bazel integration e2e test (#24522)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.1+24.sha-5aa12c7');
+const VERSION = new Version('6.1.0-beta.1+25.sha-7c8159b');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.1+24.sha-5aa12c7');
+var VERSION = new Version('6.1.0-beta.1+25.sha-7c8159b');
 
 /**
  * @license
