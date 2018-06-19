4511f736c16f6b8516199f8be12b64829e54eb4c
Miško Hevery <misko@hevery.com>
2018-06-19 18:27:25 +0000

    5b76f04 docs: More edits (#24255)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+34.sha-794584e
+ * @license Angular v6.1.0-beta.1+41.sha-5b76f04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.1+34.sha-794584e');
+const VERSION = new Version('6.1.0-beta.1+41.sha-5b76f04');
 
 /**
  * @fileoverview added by tsickle
