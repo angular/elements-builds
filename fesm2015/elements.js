ed5baa147f0c6874019f389254c2ecdf8a3d8e70
Miško Hevery <misko@hevery.com>
2018-06-20 18:31:05 +0000

    776bc38 test(animations): properly reference body node for SSR environments (#23300)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+28.sha-74f07f4
+ * @license Angular v6.0.5+36.sha-776bc38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.5+28.sha-74f07f4');
+const VERSION = new Version('6.0.5+36.sha-776bc38');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+28.sha-74f07f4
+ * @license Angular v6.0.5+36.sha-776bc38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.5+28.sha-74f07f4');
+var VERSION = new Version('6.0.5+36.sha-776bc38');
 
 /**
  * @license
