6b96ca9b24fab1d4373bb35d0e00c60e4e276fbf
Miško Hevery <misko@hevery.com>
2018-06-21 00:10:05 +0000

    9782736 release: cut the v6.0.6 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+42.sha-e8354ed
+ * @license Angular v6.1.0-beta.1+43.sha-9782736
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.1+42.sha-e8354ed');
+const VERSION = new Version('6.1.0-beta.1+43.sha-9782736');
 
 /**
  * @fileoverview added by tsickle
