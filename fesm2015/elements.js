455646e8a0c9182112dd53f6b986a3aabf614180
Miško Hevery <misko@hevery.com>
2018-06-21 20:46:22 +0000

    84272e2 feat(ivy): runtime i18n (#24037)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+6.sha-cb31381
+ * @license Angular v6.1.0-beta.2+7.sha-84272e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -471,7 +471,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-beta.2+6.sha-cb31381');
+const VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
 
 /**
  * @license
