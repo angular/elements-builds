b3bc6ca9ae925cef8e20fecc5b54185bddcdfd9b
Miško Hevery <misko@hevery.com>
2018-06-21 22:07:47 +0000

    7b2b1af fix(ivy): support inputs/outputs in decorator metadata in JIT (#24565)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+5.sha-3e1a3b2
+ * @license Angular v6.1.0-beta.2+10.sha-7b2b1af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.2+5.sha-3e1a3b2');
+const VERSION = new Version('6.1.0-beta.2+10.sha-7b2b1af');
 
 /**
  * @fileoverview added by tsickle
