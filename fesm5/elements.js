66f603f09b51d962006bd5f4da204ba5d002655d
Miško Hevery <misko@hevery.com>
2018-06-21 00:28:38 +0000

    1ce5a49 docs(aio): add elana olson to contributor.json file (#24579)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.6+1.sha-1f532aa');
+var VERSION = new Version('6.0.6+2.sha-1ce5a49');
 
 /**
  * @license
