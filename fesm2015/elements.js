73bae4fa97a53fb49037e12066b5c5124df3db4c
George Kalpakas <kalpakas.g@gmail.com>
2018-06-25 20:18:08 +0000

    a5799e6 build(ivy): fix bazel `srcs` for ngcc

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+45.sha-db21dbf
+ * @license Angular v6.1.0-beta.1+46.sha-a5799e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.1+45.sha-db21dbf');
+const VERSION = new Version('6.1.0-beta.1+46.sha-a5799e6');
 
 /**
  * @fileoverview added by tsickle
