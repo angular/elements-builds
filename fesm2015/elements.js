631a4f39580eaf229fef26b946fd9e3ad7b4c4e8
Miško Hevery <misko@hevery.com>
2018-06-25 16:02:52 +0000

    39c7769 docs(core): typo in static injector tests (#24548)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+16.sha-8c51ce6
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.2+16.sha-8c51ce6');
+const VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
 
 /**
  * @fileoverview added by tsickle
