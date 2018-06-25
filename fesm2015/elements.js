858633b4b924f6b528ba9ea83f0f3a3c4f2270a7
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:00 +0000

    fba3f10 docs: add guide-angular.wishtack.io to education resources (#24585)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+17.sha-39c7769
+ * @license Angular v6.1.0-beta.2+19.sha-fba3f10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
+const VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @fileoverview added by tsickle
