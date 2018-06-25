4d0576c9c11535c8a5efc2bb4cccbf1d434fa055
Miško Hevery <misko@hevery.com>
2018-06-25 16:14:06 +0000

    f229449 refactor(ivy): insert embedded views immediately (#24629)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+21.sha-6e20e0a
+ * @license Angular v6.1.0-beta.2+22.sha-f229449
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.2+21.sha-6e20e0a');
+var VERSION = new Version('6.1.0-beta.2+22.sha-f229449');
 
 /**
  * @license
