8e5dfe61936ea2e4b8a1c7964d91cec307693b81
Miško Hevery <misko@hevery.com>
2018-06-25 15:56:28 +0000

    8919577 docs: update Angular CLI option for sourcemaps (#24437)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+4.sha-84eff42
+ * @license Angular v6.0.6+5.sha-8919577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.6+4.sha-84eff42');
+var VERSION = new Version('6.0.6+5.sha-8919577');
 
 /**
  * @license
