e2a4963eba4248a68a9f90f667951efde57449a3
Miško Hevery <misko@hevery.com>
2018-06-25 17:41:16 +0000

    2a68ba4 docs: fix misdirected group links (#24569)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+25.sha-941d2cd
+ * @license Angular v6.1.0-beta.2+27.sha-2a68ba4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.2+25.sha-941d2cd');
+var VERSION = new Version('6.1.0-beta.2+27.sha-2a68ba4');
 
 /**
  * @license
