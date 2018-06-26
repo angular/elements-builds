8eb8f737cd0f09fabb2ef54bf88fa679613f6966
Jason Aden <jasonaden@google.com>
2018-06-26 18:31:04 +0000

    426f971 build(docs-infra): support hiding constructors of injectable classes (#24529)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+16.sha-ae01c70
+ * @license Angular v6.0.6+17.sha-426f971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.6+16.sha-ae01c70');
+var VERSION = new Version('6.0.6+17.sha-426f971');
 
 /**
  * @license
