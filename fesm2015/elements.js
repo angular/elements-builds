5807c974ca66e6eb6b400286280ef1120fbc224f
Jason Aden <jasonaden@google.com>
2018-06-27 23:10:22 +0000

    39c8bae fix(common): use correct ICU plural for locale mk (#24659) PR Close #24659

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+54.sha-39c8bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -471,7 +471,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+const VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @license
