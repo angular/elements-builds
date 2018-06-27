87d96b20097e38531d7b34f2317b7238a5b4c26d
Jason Aden <jasonaden@google.com>
2018-06-27 22:25:44 +0000

    97897ab refactor(upgrade): fix examples for `strictPropertyInitialization` and remove internal comments (#18487)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+24.sha-94433f3
+ * @license Angular v6.0.6+34.sha-97897ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.6+24.sha-94433f3');
+const VERSION = new Version('6.0.6+34.sha-97897ab');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+24.sha-94433f3
+ * @license Angular v6.0.6+34.sha-97897ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.6+24.sha-94433f3');
+var VERSION = new Version('6.0.6+34.sha-97897ab');
 
 /**
  * @license
