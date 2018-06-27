e5ba32b3e531332445adb9fb54f2239e0472075e
Jason Aden <jasonaden@google.com>
2018-06-27 22:14:44 +0000

    8dec381 docs: fix unit tests in toh-pt6 (#24491)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+42.sha-32da3e1
+ * @license Angular v6.1.0-beta.2+43.sha-8dec381
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -471,7 +471,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-beta.2+42.sha-32da3e1');
+const VERSION = new Version('6.1.0-beta.2+43.sha-8dec381');
 
 /**
  * @license
