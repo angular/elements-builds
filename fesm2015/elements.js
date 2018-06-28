71482479bb0b95ccebec34cde63379566abe07c6
Miško Hevery <misko@hevery.com>
2018-06-28 23:52:21 +0000

    13d60ea fix(platform-browser): add missing deps for HammerGesturesPlugin (#24682)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+54.sha-39c8bae
+ * @license Angular v6.1.0-beta.3+16.sha-13d60ea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
+const VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
 
 /**
  * @fileoverview added by tsickle
