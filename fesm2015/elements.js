e9e2b4194b9cfd21a5a4063862142ce67b606189
Miško Hevery <misko@hevery.com>
2018-06-28 22:40:13 +0000

    ac5b69f fix(docs-infra): use clean package.json template when generating zips (#24691)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+1.sha-ad68332
+ * @license Angular v6.0.7+3.sha-ac5b69f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.7+1.sha-ad68332');
+const VERSION = new Version('6.0.7+3.sha-ac5b69f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+1.sha-ad68332
+ * @license Angular v6.0.7+3.sha-ac5b69f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.7+1.sha-ad68332');
+var VERSION = new Version('6.0.7+3.sha-ac5b69f');
 
 /**
  * @license
