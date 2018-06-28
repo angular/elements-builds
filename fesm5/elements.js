edfdc563278e53f2ac558b132e263497d5cb46dc
Jason Aden <jasonaden@google.com>
2018-06-28 02:02:14 +0000

    c57b491 release: cut the v6.1.0-beta.3 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+55.sha-1dc7d0d
+ * @license Angular v6.1.0-beta.2+56.sha-c57b491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.2+55.sha-1dc7d0d');
+var VERSION = new Version('6.1.0-beta.2+56.sha-c57b491');
 
 /**
  * @license
