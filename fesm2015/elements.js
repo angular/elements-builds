a90ca43df37dc16dc7921b7bbcf7e444ddf5f4df
Misko Hevery <misko@hevery.com>
2018-06-29 14:35:05 +0000

    d243baf refactor(ivy): remove pChild from LNode (#24705)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.3+19.sha-ff84c5c');
+const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @fileoverview added by tsickle
