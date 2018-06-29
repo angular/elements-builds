7f7dfe456913b6f2455f5940387711d15c34f772
Misko Hevery <misko@hevery.com>
2018-06-29 14:22:50 +0000

    9803cb0 feat(ivy): Add InheritanceDefinitionFeature to support directive inheritance (#24570)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+17.sha-9803cb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
+const VERSION = new Version('6.1.0-beta.3+17.sha-9803cb0');
 
 /**
  * @fileoverview added by tsickle
