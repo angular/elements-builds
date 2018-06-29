08db43c31725c33e573b60eeb6b64f3e7bd3eefe
Misko Hevery <misko@hevery.com>
2018-06-29 14:29:56 +0000

    0f1de35 docs(core): rephrase doc for Injector.get (#24670)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+4.sha-d89f57f
+ * @license Angular v6.0.7+5.sha-0f1de35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.7+4.sha-d89f57f');
+var VERSION = new Version('6.0.7+5.sha-0f1de35');
 
 /**
  * @license
