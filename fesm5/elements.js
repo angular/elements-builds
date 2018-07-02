46e2a2c7dd26237af4368df6c67ea7ef0566a269
Miško Hevery <misko@hevery.com>
2018-07-02 22:10:36 +0000

    1ae3f87 docs: update HTTP error test example again (#24701)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+22.sha-4e7a44c
+ * @license Angular v6.1.0-beta.3+23.sha-1ae3f87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.3+22.sha-4e7a44c');
+var VERSION = new Version('6.1.0-beta.3+23.sha-1ae3f87');
 
 /**
  * @license
