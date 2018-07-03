e4df7e9231e7099018cdbb9c504927ae7f7115a8
Miško Hevery <misko@hevery.com>
2018-07-03 23:04:57 +0000

    7f3242a docs: fix documention for attributes directive (#24367)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+31.sha-7f3242a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -638,7 +638,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+const VERSION = new Version('6.1.0-beta.3+31.sha-7f3242a');
 
 /**
  * @fileoverview added by tsickle
