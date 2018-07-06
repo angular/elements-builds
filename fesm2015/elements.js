978ab18af6d51d9e2f5455a731e5a37b973b75b3
Miško Hevery <misko@hevery.com>
2018-07-06 18:15:41 +0000

    b51ce62 docs(aio): added a link to Angular Zero online course (Traditional Chinese) (#24228)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+17.sha-69c8226
+ * @license Angular v6.0.7+19.sha-b51ce62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -607,7 +607,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const VERSION = new Version('6.0.7+17.sha-69c8226');
+const VERSION = new Version('6.0.7+19.sha-b51ce62');
 
 /**
  * @fileoverview added by tsickle
