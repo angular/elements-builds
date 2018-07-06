d62446e6c90dfddb5afc095e1f095c5736c342fd
Miško Hevery <misko@hevery.com>
2018-07-06 21:38:35 +0000

    52d43a9 fix(service-worker): avoid network requests when looking up hashed resources in cache (#24127)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+58.sha-250527c
+ * @license Angular v6.1.0-beta.3+60.sha-52d43a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-beta.3+58.sha-250527c');
+    var VERSION = new core.Version('6.1.0-beta.3+60.sha-52d43a9');
 
     /**
      * @license
