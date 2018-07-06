4c5730c1c67847340178f00ca1a6db76a277bd3f
Miško Hevery <misko@hevery.com>
2018-07-06 18:19:27 +0000

    2d1102f docs: add workspace and related cli terms (#24633)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+20.sha-bc2bf18
+ * @license Angular v6.0.7+22.sha-2d1102f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.7+20.sha-bc2bf18');
+var VERSION = new Version('6.0.7+22.sha-2d1102f');
 
 /**
  * @license
