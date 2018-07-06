eed694e2b2d9cef6098c0664ac13a667d7dbb79f
Miško Hevery <misko@hevery.com>
2018-07-06 18:10:34 +0000

    ada486a docs: fix typos in 'Httpclient' docs (#19127)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+14.sha-563e8e3
+ * @license Angular v6.0.7+15.sha-ada486a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -489,7 +489,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.7+14.sha-563e8e3');
+var VERSION = new Version('6.0.7+15.sha-ada486a');
 
 /**
  * @license
