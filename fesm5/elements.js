01e678683530c7028e5c8cb82f53da5d59fe99b1
Miško Hevery <misko@hevery.com>
2018-07-06 18:25:45 +0000

    fddd2af test: integration test for TS 2.9.x (#24749)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+44.sha-d5a9396
+ * @license Angular v6.1.0-beta.3+45.sha-fddd2af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.3+44.sha-d5a9396');
+var VERSION = new Version('6.1.0-beta.3+45.sha-fddd2af');
 
 /**
  * @license
