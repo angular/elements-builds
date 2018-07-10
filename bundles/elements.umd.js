ad9d0755d857cab9c06da947234f358a5179d0b6
Miško Hevery <misko@hevery.com>
2018-07-10 19:27:09 +0000

    1089261 fix(core): mark NgModule as not the root if APP_ROOT is set to false (#24814)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+66.sha-ddb792d
+ * @license Angular v6.1.0-beta.3+67.sha-1089261
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-beta.3+66.sha-ddb792d');
+    var VERSION = new core.Version('6.1.0-beta.3+67.sha-1089261');
 
     /**
      * @license
