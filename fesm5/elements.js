dee770c2b51090f21bfc3059351b76edb374d1ef
Miško Hevery <misko@hevery.com>
2018-07-10 19:52:12 +0000

    3a19f70 refactor(ivy): replace pNextOrParent with TNode props (#24752)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+71.sha-0d5f2d3
+ * @license Angular v6.1.0-beta.3+75.sha-3a19f70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0-beta.3+71.sha-0d5f2d3');
+var VERSION = new Version('6.1.0-beta.3+75.sha-3a19f70');
 
 /**
  * @license
