c0c0e17959483f3031794a32f8216d39ece534fe
Miško Hevery <misko@hevery.com>
2018-07-11 22:06:38 +0000

    0b4d85e fix(common): format fractional seconds (#24844)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+88.sha-b9e095a
+ * @license Angular v6.1.0-beta.3+89.sha-0b4d85e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-beta.3+88.sha-b9e095a');
+    var VERSION = new core.Version('6.1.0-beta.3+89.sha-0b4d85e');
 
     /**
      * @license
