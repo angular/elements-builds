bc4e2bf355770b6bb44ad91119effc070db080e3
GitHub <noreply@github.com>
2018-07-11 00:43:41 +0000

    7d27ecc fix(platform-browser): workaround wrong import path generated by ngc for DOCUMENT (#24830)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+76.sha-03616bc
+ * @license Angular v6.1.0-beta.3+77.sha-7d27ecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+76.sha-03616bc');
+const VERSION = new Version('6.1.0-beta.3+77.sha-7d27ecc');
 
 /**
  * @fileoverview added by tsickle
