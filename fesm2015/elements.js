71b11180ad7fc1bfd690dfd14fd4a1a7f716f74d
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 22:20:45 +0000

    80a74b4 docs(forms): update form builder API reference (#24693)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+94.sha-328971f
+ * @license Angular v6.1.0-beta.3+108.sha-80a74b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+94.sha-328971f');
+const VERSION = new Version('6.1.0-beta.3+108.sha-80a74b4');
 
 /**
  * @fileoverview added by tsickle
