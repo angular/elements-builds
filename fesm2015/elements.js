a8be855eb1cda91fe57f6e12cada23b2a071ee69
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:56:20 +0000

    a663565 build: fix windows scripts (#23121)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+109.sha-85d9c20
+ * @license Angular v6.1.0-beta.3+110.sha-a663565
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+109.sha-85d9c20');
+const VERSION = new Version('6.1.0-beta.3+110.sha-a663565');
 
 /**
  * @fileoverview added by tsickle
