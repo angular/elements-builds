9b57cc7aea78abe5cc2f8b733e2ec2566b265b46
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:49:11 +0000

    9a6f27c fix(ivy): support zero-argument @NgModule() invocations (#24738)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+108.sha-80a74b4
+ * @license Angular v6.1.0-beta.3+107.sha-9a6f27c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+108.sha-80a74b4');
+const VERSION = new Version('6.1.0-beta.3+107.sha-9a6f27c');
 
 /**
  * @fileoverview added by tsickle
