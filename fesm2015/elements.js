2b106869423edf32825956ad50e51cdf93881274
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 19:17:09 +0000

    4d8b8ad build(bazel): Undo temporary dependency on unleased TS bazel rules (#24826)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+94.sha-328971f
+ * @license Angular v6.1.0-beta.3+93.sha-4d8b8ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+94.sha-328971f');
+const VERSION = new Version('6.1.0-beta.3+93.sha-4d8b8ad');
 
 /**
  * @fileoverview added by tsickle
