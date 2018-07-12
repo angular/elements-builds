f5514b5621ee17803afd9e9fcc07013fdba53322
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:52:24 +0000

    e295206 docs: update component styles doc regarding relative URL (#24471)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+6.sha-6bf8aef
+ * @license Angular v6.0.8+8.sha-e295206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -556,7 +556,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const /** @type {?} */ VERSION = new Version('6.0.8+6.sha-6bf8aef');
+const /** @type {?} */ VERSION = new Version('6.0.8+8.sha-e295206');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+6.sha-6bf8aef
+ * @license Angular v6.0.8+8.sha-e295206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -482,7 +482,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.8+6.sha-6bf8aef');
+var VERSION = new Version('6.0.8+8.sha-e295206');
 
 /**
  * @license
