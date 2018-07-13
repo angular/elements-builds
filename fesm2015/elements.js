1375195b1cc136cc0f87f327d2ca828913c78d8d
Matias Niemelä <matias@yearofmoo.com>
2018-07-13 19:30:38 +0000

    15df5f7 build: update to Bazel 0.15 (#24841)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+7.sha-0e7501b
+ * @license Angular v6.0.8+9.sha-15df5f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -556,7 +556,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const /** @type {?} */ VERSION = new Version('6.0.8+7.sha-0e7501b');
+const /** @type {?} */ VERSION = new Version('6.0.8+9.sha-15df5f7');
 
 /**
  * @fileoverview added by tsickle
