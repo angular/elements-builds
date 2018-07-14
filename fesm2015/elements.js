015e99fda8365e96c531f82cc23a432213a3a6ce
Matias Niemelä <matias@yearofmoo.com>
2018-07-14 00:20:17 +0000

    74b250b feat(docs-infra): enable filtering by package on API list page (#24631)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+113.sha-97277bc
+ * @license Angular v6.1.0-beta.3+115.sha-74b250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+113.sha-97277bc');
+const VERSION = new Version('6.1.0-beta.3+115.sha-74b250b');
 
 /**
  * @fileoverview added by tsickle
