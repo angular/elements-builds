67941480023d918950ebaa980b83b7f4cf199c9c
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:09:06 +0000

    ae04929 feat(ivy): implement initial ngcc package transformer

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-31b4043
+ * @license Angular v6.1.0-beta.3+129.sha-ae04929
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
+const VERSION = new Version('6.1.0-beta.3+129.sha-ae04929');
 
 /**
  * @fileoverview added by tsickle
