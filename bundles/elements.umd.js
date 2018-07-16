4954bfc2b170db7c2786c65658ba4c5f49acc7b9
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:36:21 +0000

    9be8abd build: disable IE web worker tests (#24908)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+115.sha-74b250b
+ * @license Angular v6.1.0-beta.3+116.sha-9be8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-beta.3+115.sha-74b250b');
+    var VERSION = new core.Version('6.1.0-beta.3+116.sha-9be8abd');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+115.sha-74b250b
+ * @license Angular v6.1.0-beta.3+116.sha-9be8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+115.sha-74b250b');
+const VERSION = new Version('6.1.0-beta.3+116.sha-9be8abd');
 
 /**
  * @fileoverview added by tsickle
