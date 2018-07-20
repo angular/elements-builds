95cf0a81299fa15ab48f5d79d95a2d5abc69d7af
Victor Berchet <victor@suumit.com>
2018-07-20 19:24:36 +0000

    2a5a05b fix(docs-infra): fix table header layout in API pages (#24919)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+25.sha-4a29100
+ * @license Angular v6.0.9+26.sha-2a5a05b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -556,7 +556,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+25.sha-4a29100');
+const /** @type {?} */ VERSION = new Version('6.0.9+26.sha-2a5a05b');
 
 /**
  * @fileoverview added by tsickle
