192febcd51ff26996727758375bc1c49b3f72e1d
Victor Berchet <victor@suumit.com>
2018-07-20 19:57:20 +0000

    bb58138 docs: fix bad link (#24825)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+16.sha-b8f740b
+ * @license Angular v6.1.0-rc.3+17.sha-bb58138
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-rc.3+16.sha-b8f740b');
+    var VERSION = new core.Version('6.1.0-rc.3+17.sha-bb58138');
 
     /**
      * @license
