ee27ce58c22479e8e5bde745027fb3b5e0b2438c
Victor Berchet <victor@suumit.com>
2018-07-20 19:26:47 +0000

    2d38fa1 test(platform-webworker): avoid flakes due to existing `PlatformRef` (#24916)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+11.sha-c438b5e
+ * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
+const VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
 
 /**
  * @fileoverview added by tsickle
