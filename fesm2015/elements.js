9c6b77b9276b813abd4e2599838e65dce4c5d04d
Victor Berchet <victor@suumit.com>
2018-07-20 19:59:40 +0000

    8620373 fix(service-worker): don't include sourceMappingURL in ngsw-worker (#24877)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+17.sha-bb58138
+ * @license Angular v6.1.0-rc.3+41.sha-8620373
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+17.sha-bb58138');
+const VERSION = new Version('6.1.0-rc.3+41.sha-8620373');
 
 /**
  * @fileoverview added by tsickle
