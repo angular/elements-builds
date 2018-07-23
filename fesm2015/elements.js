6bce72cf91bb8716aa80c5963a5b0aba832f27eb
Victor Berchet <victor@suumit.com>
2018-07-23 17:23:34 +0000

    ac52e1a build(docs-infra): ensure all API headings are sentence cased (#24949)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+35.sha-7f3a5ec
+ * @license Angular v6.0.9+36.sha-ac52e1a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -556,7 +556,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+35.sha-7f3a5ec');
+const /** @type {?} */ VERSION = new Version('6.0.9+36.sha-ac52e1a');
 
 /**
  * @fileoverview added by tsickle
