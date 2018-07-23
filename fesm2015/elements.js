bc80cf16c6a9a0c2df89ec236d3e1e74af629d1d
Victor Berchet <victor@suumit.com>
2018-07-23 17:27:05 +0000

    bd576bb docs: fix multicasting example in observable guide (#24911)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+47.sha-7729bb2
+ * @license Angular v6.1.0-rc.3+49.sha-bd576bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-rc.3+47.sha-7729bb2');
+const VERSION = new Version('6.1.0-rc.3+49.sha-bd576bb');
 
 /**
  * @license
