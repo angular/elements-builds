861d34fa7096acd1431a5843cb71745f205bb9f0
Victor Berchet <victor@suumit.com>
2018-07-23 19:13:56 +0000

    060867d build: Fix windows tests (#24927)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+38.sha-4d840cb
+ * @license Angular v6.0.9+39.sha-060867d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -482,7 +482,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.9+38.sha-4d840cb');
+var VERSION = new Version('6.0.9+39.sha-060867d');
 
 /**
  * @license
