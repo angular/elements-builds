0f0ea5fe937627c0659872e0be7abe237c5e1c49
Victor Berchet <victor@suumit.com>
2018-07-23 19:07:14 +0000

    4d840cb docs: technical review incorporated (#24744)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+34.sha-ac7658e
+ * @license Angular v6.0.9+38.sha-4d840cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -482,7 +482,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.9+34.sha-ac7658e');
+var VERSION = new Version('6.0.9+38.sha-4d840cb');
 
 /**
  * @license
