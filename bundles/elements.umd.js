9d8cc04722f8e8e1f18cded57f9578ebbf2a55fb
Victor Berchet <victor@suumit.com>
2018-07-24 22:39:46 +0000

    7960d18 docs: technical review incorporated (#24744)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+54.sha-be3cca4
+ * @license Angular v6.1.0-rc.3+43.sha-7960d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-rc.3+54.sha-be3cca4');
+    var VERSION = new core.Version('6.1.0-rc.3+43.sha-7960d18');
 
     /**
      * @license
