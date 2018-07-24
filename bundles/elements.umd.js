875ea2a2715ba8de6f5e4153dd7038e7c3d631c6
Victor Berchet <victor@suumit.com>
2018-07-24 23:31:16 +0000

    c7d147e Revert "build: update to newer circleCI bazel remote cache proxy (#25054)" (#25076)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+32.sha-6a74ef0
+ * @license Angular v6.0.9+44.sha-c7d147e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -532,7 +532,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.0.9+32.sha-6a74ef0');
+    var VERSION = new core.Version('6.0.9+44.sha-c7d147e');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+32.sha-6a74ef0
+ * @license Angular v6.0.9+44.sha-c7d147e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -556,7 +556,7 @@ function createCustomElement(component, config) {
 /**
  * \@experimental
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+32.sha-6a74ef0');
+const /** @type {?} */ VERSION = new Version('6.0.9+44.sha-c7d147e');
 
 /**
  * @fileoverview added by tsickle
