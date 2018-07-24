57797a30f26c3bdf33e066d663180549b522ec04
Victor Berchet <victor@suumit.com>
2018-07-24 18:41:03 +0000

    dc0340c docs: add Truly-UI to resources (#24615) PR Close #24615

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+41.sha-5f680d4
+ * @license Angular v6.0.9+33.sha-dc0340c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -482,7 +482,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.0.9+41.sha-5f680d4');
+var VERSION = new Version('6.0.9+33.sha-dc0340c');
 
 /**
  * @license
