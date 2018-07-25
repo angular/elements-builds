cf40b3ec722ede3ffc0041fab920cf4a236fcea5
Victor Berchet <victor@suumit.com>
2018-07-25 21:10:49 +0000

    e1c6fd5 Revert "feat(core): add support for using async/await with Jasmine" (#25096)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-rc.3+64.sha-1ceddb6');
+    var VERSION = new core.Version('6.1.0-rc.3+75.sha-e1c6fd5');
 
     /**
      * @license
