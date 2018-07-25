4db9cb6476acef16bfa1b5e4baa7d78f21978f80
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-25 07:15:31 +0000

    70668f7 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-0650f32
+ * @license Angular v6.1.0-rc.3+85.sha-70668f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0-rc.3+71.sha-0650f32');
+const VERSION = new Version('6.1.0-rc.3+85.sha-70668f7');
 
 /**
  * @license
