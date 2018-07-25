85b3e4736368e14f995072caedb915ddabf5e8c3
Victor Berchet <victor@suumit.com>
2018-07-25 18:19:27 +0000

    1ceddb6 fix(ivy): support re-order embedded templates (#24805)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+60.sha-2cb0f68
+ * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+60.sha-2cb0f68');
+const VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
 
 /**
  * @fileoverview added by tsickle
