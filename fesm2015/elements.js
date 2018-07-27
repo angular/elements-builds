61428094eac4e2c2494f0358402214887977f361
Igor Minar <igor@angular.io>
2018-07-27 19:30:01 +0000

    3426784 test: fix typings for `DoneFn` (#25163)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0+17.sha-e8d4211');
+const VERSION = new Version('6.1.0+18.sha-3426784');
 
 /**
  * @license
