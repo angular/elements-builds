d3edf51f941f4962f78eac18c6e623a0f1b262bc
Igor Minar <igor@angular.io>
2018-07-27 01:06:27 +0000

    3169edd fix(ivy): don't crash in listLazyRoutes() (#25080)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0+8.sha-3169edd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -534,7 +534,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0-rc.3+75.sha-e1c6fd5');
+    var VERSION = new core.Version('6.1.0+8.sha-3169edd');
 
     /**
      * @license
