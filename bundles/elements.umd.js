b7ba5019992468f1b61954c49bdb9dc71f08ef8e
Igor Minar <igor@angular.io>
2018-07-27 17:55:12 +0000

    a3cf61b docs: refactor feature modules example (#25069)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-a1b185b
+ * @license Angular v6.1.0+15.sha-a3cf61b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+14.sha-a1b185b');
+    var VERSION = new core.Version('6.1.0+15.sha-a3cf61b');
 
     /**
      * @license
