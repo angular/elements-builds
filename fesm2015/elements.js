59e93ed6b2777ed9dfe208b1ee7480968d144da0
Igor Minar <igor@angular.io>
2018-07-28 01:05:29 +0000

    f82b6b2 build(bazel): fix typos in comments (#25172)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+40.sha-a87d44c
+ * @license Angular v6.1.0+41.sha-f82b6b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0+40.sha-a87d44c');
+const VERSION = new Version('6.1.0+41.sha-f82b6b2');
 
 /**
  * @license
