8c01f90955b18e2a44db27a8f29f52ea77aca485
Igor Minar <igor@angular.io>
2018-07-30 23:34:55 +0000

    2ef777b fix(ivy): convert context code into a tree-shakable instruction (#24943)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+44.sha-9a6d26e
+ * @license Angular v6.1.0+47.sha-2ef777b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0+44.sha-9a6d26e');
+const VERSION = new Version('6.1.0+47.sha-2ef777b');
 
 /**
  * @fileoverview added by tsickle
