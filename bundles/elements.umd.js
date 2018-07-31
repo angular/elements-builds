2d8574f630a5d57f78892d77191a1c92ee754942
Igor Minar <igor@angular.io>
2018-07-31 20:02:04 +0000

    3f6fc00 docs(forms): fix incorrect variables naming in the comments (#25150)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+55.sha-e3834b7
+ * @license Angular v6.1.0+59.sha-3f6fc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+55.sha-e3834b7');
+    var VERSION = new core.Version('6.1.0+59.sha-3f6fc00');
 
     /**
      * @license
