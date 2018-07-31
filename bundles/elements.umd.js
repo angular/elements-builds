907c50193347021412c944b7f54ecbfe1c04c2c5
Igor Minar <igor@angular.io>
2018-07-31 20:57:16 +0000

    fa59748 build: make `postinstall` script compatible with Windows (#25232)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+21.sha-fa38828
+ * @license Angular v6.1.0+20.sha-fa59748
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+21.sha-fa38828');
+    var VERSION = new core.Version('6.1.0+20.sha-fa59748');
 
     /**
      * @license
