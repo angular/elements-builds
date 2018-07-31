1ae09941cfbb395776c0828b2fc5d49790b1dd2b
Igor Minar <igor@angular.io>
2018-07-31 22:46:31 +0000

    d4bf2da refactor(core): remove withBody from public testing API (#25171)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+20.sha-fa59748
+ * @license Angular v6.1.0+22.sha-d4bf2da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+20.sha-fa59748');
+    var VERSION = new core.Version('6.1.0+22.sha-d4bf2da');
 
     /**
      * @license
