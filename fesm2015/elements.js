eec05113e1bebe0210722c2da419671fa7f1f2bb
Igor Minar <igor@angular.io>
2018-07-31 05:30:09 +0000

    3f20a2f docs: fix link to "Override component providers" (#24967)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+54.sha-3664829
+ * @license Angular v6.1.0+56.sha-3f20a2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0+54.sha-3664829');
+const VERSION = new Version('6.1.0+56.sha-3f20a2f');
 
 /**
  * @license
