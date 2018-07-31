0ecb46fe981913b57fc3fe9a035aa813b3c24f21
Igor Minar <igor@angular.io>
2018-07-31 00:32:51 +0000

    f94f964 ci: correctly encode quoted params passed as params to curl

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+48.sha-efc6684
+ * @license Angular v6.1.0+50.sha-f94f964
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+48.sha-efc6684');
+    var VERSION = new core.Version('6.1.0+50.sha-f94f964');
 
     /**
      * @license
