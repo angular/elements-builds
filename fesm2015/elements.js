99363045ff53e0e4b13ebedfe7a7055f61e71830
Igor Minar <igor@angular.io>
2018-08-01 00:49:27 +0000

    c8a4fb1 fix(ivy): walk declaration views in listener (#25228)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+64.sha-64516da
+ * @license Angular v6.1.0+65.sha-c8a4fb1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0+64.sha-64516da');
+const VERSION = new Version('6.1.0+65.sha-c8a4fb1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+64.sha-64516da
+ * @license Angular v6.1.0+65.sha-c8a4fb1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0+64.sha-64516da');
+var VERSION = new Version('6.1.0+65.sha-c8a4fb1');
 
 /**
  * @license
