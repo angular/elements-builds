5069fc71646fe141a670e8c40d29fbfe4e11c42f
Igor Minar <igor@angular.io>
2018-08-01 20:10:33 +0000

    7075c41 docs(changelog): remove reverted feature entry (#25206)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+66.sha-bafe1a0
+ * @license Angular v6.1.0+69.sha-7075c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+66.sha-bafe1a0');
+    var VERSION = new core.Version('6.1.0+69.sha-7075c41');
 
     /**
      * @license
