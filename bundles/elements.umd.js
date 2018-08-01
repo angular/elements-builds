cd236cff429bf1f9815aa3425def2c0d9bb0fd7c
Igor Minar <igor@angular.io>
2018-08-01 20:28:31 +0000

    166ddaa docs(router): clarify scroll position wording (#25077)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+79.sha-af07ffc
+ * @license Angular v6.1.0+73.sha-166ddaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+79.sha-af07ffc');
+    var VERSION = new core.Version('6.1.0+73.sha-166ddaa');
 
     /**
      * @license
