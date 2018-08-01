6b451bad18e9f474d6e8eed82d638c7024654424
Igor Minar <igor@angular.io>
2018-08-01 20:48:25 +0000

    5342aea docs(aio): update Kendo UI description in resource.json (#24845)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+80.sha-1dd2eaa
+ * @license Angular v6.1.0+81.sha-5342aea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+80.sha-1dd2eaa');
+    var VERSION = new core.Version('6.1.0+81.sha-5342aea');
 
     /**
      * @license
