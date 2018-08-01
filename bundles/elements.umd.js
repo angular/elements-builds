bf5f2ecacd85dd3398370631e191ac62d7055f58
Igor Minar <igor@angular.io>
2018-08-01 21:45:51 +0000

    3a60138 test(upgrade): run tests against AngularJS v1.7.x as well (#25231)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+40.sha-7a1fdde
+ * @license Angular v6.1.0+41.sha-3a60138
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+40.sha-7a1fdde');
+    var VERSION = new core.Version('6.1.0+41.sha-3a60138');
 
     /**
      * @license
