9757728e98c6c2cd2bd503c674fc7d26134abb0e
Igor Minar <igor@angular.io>
2018-08-01 20:27:16 +0000

    166ddaa docs(router): clarify scroll position wording (#25077)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+79.sha-af07ffc
+ * @license Angular v6.1.0+73.sha-166ddaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0+79.sha-af07ffc');
+const VERSION = new Version('6.1.0+73.sha-166ddaa');
 
 /**
  * @fileoverview added by tsickle
