53e21be3c0ce33f4d246e1fc749020416b9065f3
Igor Minar <igor@angular.io>
2018-08-01 20:28:21 +0000

    af07ffc docs(core): remove experimental tag (#24032)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+77.sha-7a4fb44
+ * @license Angular v6.1.0+79.sha-af07ffc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.0+77.sha-7a4fb44');
+var VERSION = new Version('6.1.0+79.sha-af07ffc');
 
 /**
  * @license
