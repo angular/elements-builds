6eaabee1416d7abb78ce689951ac461f09c391be
Igor Minar <igor@angular.io>
2018-08-02 00:16:04 +0000

    d5d034a docs: update reactiveconf 2018 in events (#24739)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+43.sha-5ca35b3
+ * @license Angular v6.1.0+44.sha-d5d034a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+43.sha-5ca35b3');
+    var VERSION = new core.Version('6.1.0+44.sha-d5d034a');
 
     /**
      * @license
