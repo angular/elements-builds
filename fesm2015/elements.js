68d09730208efa330656c9fe2b696b7698c747cf
Igor Minar <igor@angular.io>
2018-08-02 16:36:13 +0000

    7b89711 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+90.sha-a224df4
+ * @license Angular v6.1.0+93.sha-7b89711
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0+90.sha-a224df4');
+const VERSION = new Version('6.1.0+93.sha-7b89711');
 
 /**
  * @fileoverview added by tsickle
