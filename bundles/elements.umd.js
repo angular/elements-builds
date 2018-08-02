b6b813be0e9127c8a907551be7201adb5f22a141
Igor Minar <igor@angular.io>
2018-08-02 00:27:03 +0000

    06a1974 docs: Update the link to the Jasmine docs (#25175)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+86.sha-3d7f555
+ * @license Angular v6.1.0+87.sha-06a1974
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+86.sha-3d7f555');
+    var VERSION = new core.Version('6.1.0+87.sha-06a1974');
 
     /**
      * @license
