627989e459153d70af44f46c0536472f2e07c592
Igor Minar <igor@angular.io>
2018-08-02 17:47:10 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+98.sha-74bce18
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+98.sha-74bce18');
+    var VERSION = new core.Version('6.1.0+100.sha-183757d');
 
     /**
      * @license
