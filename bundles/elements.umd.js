e1a113e767b038f3a9e2003cd43b9ac1352dfb21
Igor Minar <igor@angular.io>
2018-08-02 16:12:25 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.0+45.sha-1ef2eae');
+    var VERSION = new core.Version('6.1.0+48.sha-9ddf269');
 
     /**
      * @license
