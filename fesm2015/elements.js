eb2aa949c88d138c8b964cb1ab9f952ae8d5acac
Igor Minar <igor@angular.io>
2018-08-02 00:06:17 +0000

    5ca35b3 docs: Update the link to the Jasmine docs (#25175)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+42.sha-0a6a3f3
+ * @license Angular v6.1.0+43.sha-5ca35b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0+42.sha-0a6a3f3');
+const VERSION = new Version('6.1.0+43.sha-5ca35b3');
 
 /**
  * @license
