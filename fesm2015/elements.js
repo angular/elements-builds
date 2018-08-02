9301511ec7af33ad0718c11d84250ea40d0c7a7b
Igor Minar <igor@angular.io>
2018-08-02 22:41:36 +0000

    4f741e7 docs: release notes for the v6.1.1 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0
+ * @license Angular v7.0.0-beta.0+1.sha-4f741e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.0');
+const VERSION = new Version('7.0.0-beta.0+1.sha-4f741e7');
 
 /**
  * @license
