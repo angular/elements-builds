7997662ccdff7f7015bd93e3507bcd8458a221bd
Kara Erickson <karakara@google.com>
2018-08-03 23:01:04 +0000

    4e26478 refactor(ivy): do not mention LViewData in public documentation (#25292)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+9.sha-0822dc7
+ * @license Angular v7.0.0-beta.0+15.sha-4e26478
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.0+9.sha-0822dc7');
+var VERSION = new Version('7.0.0-beta.0+15.sha-4e26478');
 
 /**
  * @license
