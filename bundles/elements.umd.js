6fd16c40a4e2f734d19e9fcfecd843012a63cf53
Kara Erickson <karakara@google.com>
2018-08-03 21:52:21 +0000

    3f43dbb style(common): fix short param names (#23667)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1
+ * @license Angular v6.1.1+2.sha-3f43dbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.1');
+    var VERSION = new core.Version('6.1.1+2.sha-3f43dbb');
 
     /**
      * @license
