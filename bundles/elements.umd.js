73e368e8b0e9613ee6b9dad92bd0d40dcb341c13
Kara Erickson <karakara@google.com>
2018-08-04 21:50:49 +0000

    2e16039 build: skip ivy builds when not publishing (#25299)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+4.sha-0c9c2ac
+ * @license Angular v6.1.1+5.sha-2e16039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.1+4.sha-0c9c2ac');
+    var VERSION = new core.Version('6.1.1+5.sha-2e16039');
 
     /**
      * @license
