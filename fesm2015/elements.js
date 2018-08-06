6e244560f106ffffa976ed0a51240872c246ba51
Kara Erickson <karakara@google.com>
2018-08-06 22:28:51 +0000

    66547d8 docs(core): clarify supported ViewChild selectors (#22784)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+14.sha-0c028a0
+ * @license Angular v6.1.1+17.sha-66547d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.1+14.sha-0c028a0');
+const VERSION = new Version('6.1.1+17.sha-66547d8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+14.sha-0c028a0
+ * @license Angular v6.1.1+17.sha-66547d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.1+14.sha-0c028a0');
+var VERSION = new Version('6.1.1+17.sha-66547d8');
 
 /**
  * @license
