867a85284de8a50a0d11943e67162285784293ea
Kara Erickson <karakara@google.com>
2018-08-06 22:24:10 +0000

    88f01f5 docs: fix typo (#25331) PR Close #25331

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+10.sha-c66794c
+ * @license Angular v6.1.1+11.sha-88f01f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.1+10.sha-c66794c');
+    var VERSION = new core.Version('6.1.1+11.sha-88f01f5');
 
     /**
      * @license
