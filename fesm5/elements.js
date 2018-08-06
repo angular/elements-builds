457b761f9b469355a5f4c34faccc443ca3f335d3
Kara Erickson <karakara@google.com>
2018-08-06 23:16:04 +0000

    3355502 fix(ivy): support ng-container at the root of a view with delayed insertion (#25329)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+38.sha-16c03c0
+ * @license Angular v7.0.0-beta.0+37.sha-3355502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.0+38.sha-16c03c0');
+var VERSION = new Version('7.0.0-beta.0+37.sha-3355502');
 
 /**
  * @license
