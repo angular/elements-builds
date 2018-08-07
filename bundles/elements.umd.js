984917b9af312b1f6d50e963e12df7b6760111c2
Kara Erickson <karakara@google.com>
2018-08-07 18:59:30 +0000

    054fbbe fix: add mappings for ngfactory & ngsummary files to their module names in aot summary resolver (#25335)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+24.sha-94a2ac7
+ * @license Angular v6.1.1+26.sha-054fbbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.1+24.sha-94a2ac7');
+    var VERSION = new core.Version('6.1.1+26.sha-054fbbe');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+24.sha-94a2ac7
+ * @license Angular v6.1.1+26.sha-054fbbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.1+24.sha-94a2ac7');
+const VERSION = new Version('6.1.1+26.sha-054fbbe');
 
 /**
  * @license
