bac34cbb3a6eb052e5739771dba795a14a1f1a1f
Kara Erickson <karakara@google.com>
2018-08-07 21:45:55 +0000

    16c03c0 fix(core): In Testability.whenStable update callback, pass more complete (#25010) data about tasks.

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+100.sha-183757d
+ * @license Angular v7.0.0-beta.0+38.sha-16c03c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.0+100.sha-183757d');
+const VERSION = new Version('7.0.0-beta.0+38.sha-16c03c0');
 
 /**
  * @license
