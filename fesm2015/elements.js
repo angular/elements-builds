11a563bf23517a89bcd840a451c29158b7517d4b
Kara Erickson <karakara@google.com>
2018-08-07 18:50:00 +0000

    155d938 docs: refining code of tutorial 7 routing (#22151)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+23.sha-b75a985
+ * @license Angular v6.1.1+25.sha-155d938
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.1+23.sha-b75a985');
+const VERSION = new Version('6.1.1+25.sha-155d938');
 
 /**
  * @license
