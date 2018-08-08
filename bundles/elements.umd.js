e1bd02f4a71abe01c36098f7ad05d96b8fa15de8
Kara Erickson <karakara@google.com>
2018-08-08 21:06:37 +0000

    3beb711 release: cut the v6.1.2 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+29.sha-4b1a825
+ * @license Angular v6.1.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.1+29.sha-4b1a825');
+    var VERSION = new core.Version('6.1.2');
 
     /**
      * @license
