3eac675c4c59a5fcac89d07bf68ad4dbb9a18e96
Kara Erickson <karakara@google.com>
2018-08-08 22:20:09 +0000

    3449f1e docs: copy edit architecture guide (#25328)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+7.sha-2951e72
+ * @license Angular v6.1.2+6.sha-3449f1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+7.sha-2951e72');
+    var VERSION = new core.Version('6.1.2+6.sha-3449f1e');
 
     /**
      * @license
