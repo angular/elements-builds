63f552bce4f10f44a3c316a7b0ef7884e8a5ce54
Kara Erickson <karakara@google.com>
2018-08-08 22:03:11 +0000

    6480d1b docs: make css multiline in styleguide for consistency (#25300)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+1.sha-f39551c
+ * @license Angular v6.1.2+5.sha-6480d1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+1.sha-f39551c');
+    var VERSION = new core.Version('6.1.2+5.sha-6480d1b');
 
     /**
      * @license
