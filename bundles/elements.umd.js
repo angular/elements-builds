a0c9b87a4d83b568c62172226191e1a18b80f79c
Kara Erickson <karakara@google.com>
2018-08-08 21:46:31 +0000

    f39551c docs: Clarify breaking change in minor release (#25393)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+4.sha-e76211a
+ * @license Angular v6.1.2+1.sha-f39551c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+4.sha-e76211a');
+    var VERSION = new core.Version('6.1.2+1.sha-f39551c');
 
     /**
      * @license
