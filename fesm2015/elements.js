ef1b0efc5ee23f53bc7d35ffc9c992a4cf0cc035
Kara Erickson <karakara@google.com>
2018-08-08 21:46:25 +0000

    e76211a docs: add ngrx book to the docs (#23389)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+3.sha-a16de8f
+ * @license Angular v6.1.2+4.sha-e76211a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.2+3.sha-a16de8f');
+const VERSION = new Version('6.1.2+4.sha-e76211a');
 
 /**
  * @fileoverview added by tsickle
