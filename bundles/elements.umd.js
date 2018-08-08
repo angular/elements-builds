4e138b1644f17e6a81441f67fb1b59e8e3d98b78
Kara <kara@users.noreply.github.com>
2018-08-08 22:11:56 +0000

    2951e72 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25365)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+5.sha-6480d1b
+ * @license Angular v6.1.2+7.sha-2951e72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+5.sha-6480d1b');
+    var VERSION = new core.Version('6.1.2+7.sha-2951e72');
 
     /**
      * @license
