ad5f37d57e49739d81b8999365cd7cbda71c4742
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-08-09 19:06:23 +0000

    99b2e7e feat(ivy): find all packages to be compiled by ngcc

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+66.sha-26adee9
+ * @license Angular v7.0.0-beta.1+40.sha-99b2e7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.0+66.sha-26adee9');
+const VERSION = new Version('7.0.0-beta.1+40.sha-99b2e7e');
 
 /**
  * @fileoverview added by tsickle
