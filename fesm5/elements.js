67dadef5a33a0aa3f1aac9473a6e68f24e61fc3d
Ben Lesh <ben@benlesh.com>
2018-08-09 15:37:55 +0000

    9c92a6f test(ivy): add test for <ng-container> and shallow queries (#25379)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+9.sha-8bbce3f
+ * @license Angular v7.0.0-beta.1+12.sha-9c92a6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.1+9.sha-8bbce3f');
+var VERSION = new Version('7.0.0-beta.1+12.sha-9c92a6f');
 
 /**
  * @license
