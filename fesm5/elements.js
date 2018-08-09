2f04e5933aaff568496addcecbfd0e820959178f
Ben Lesh <ben@benlesh.com>
2018-08-09 18:15:25 +0000

    7058072 feat(ivy): enable .ngfactory.js generation in g3 only (#25392)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+12.sha-9c92a6f
+ * @license Angular v7.0.0-beta.1+18.sha-7058072
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.1+12.sha-9c92a6f');
+var VERSION = new Version('7.0.0-beta.1+18.sha-7058072');
 
 /**
  * @license
