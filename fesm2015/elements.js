dee85df36b69c36822843f10acb729c9055b363b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-08-10 13:44:45 +0000

    82c8052 fix(ivy): consider exported modules from other compilation scopes

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+40.sha-99b2e7e
+ * @license Angular v7.0.0-beta.1+43.sha-82c8052
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.1+40.sha-99b2e7e');
+const VERSION = new Version('7.0.0-beta.1+43.sha-82c8052');
 
 /**
  * @fileoverview added by tsickle
