34768fb76f1b19e4d3bdbd8e6110b41c3b61f902
Ben Lesh <ben@benlesh.com>
2018-08-10 17:25:28 +0000

    c8c1aa7 docs(aio): add async validation chapter (#25189)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+19.sha-2d75992
+ * @license Angular v7.0.0-beta.1+23.sha-c8c1aa7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.1+19.sha-2d75992');
+const VERSION = new Version('7.0.0-beta.1+23.sha-c8c1aa7');
 
 /**
  * @license
