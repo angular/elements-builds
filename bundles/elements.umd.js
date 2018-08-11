27f86391b0fb99769e01a5ffff2986c74eee8cde
Ben Lesh <ben@benlesh.com>
2018-08-11 01:22:12 +0000

    4a4d6fb test(ivy): compiler compliance tests should support ? (#25435)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+20.sha-209cc7e
+ * @license Angular v7.0.0-beta.1+25.sha-4a4d6fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.1+20.sha-209cc7e');
+    var VERSION = new core.Version('7.0.0-beta.1+25.sha-4a4d6fb');
 
     /**
      * @license
