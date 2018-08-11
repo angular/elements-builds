d91af4bc07d12b8ea004b5f8b0cda9f13eaf1280
Ben Lesh <ben@benlesh.com>
2018-08-11 01:20:57 +0000

    4a4d6fb test(ivy): compiler compliance tests should support ? (#25435)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+20.sha-209cc7e
+ * @license Angular v7.0.0-beta.1+25.sha-4a4d6fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.1+20.sha-209cc7e');
+var VERSION = new Version('7.0.0-beta.1+25.sha-4a4d6fb');
 
 /**
  * @license
