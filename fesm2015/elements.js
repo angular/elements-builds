5dd4cbaf1f8677311b23736a6cd5c3408e6b4648
Ben Lesh <ben@benlesh.com>
2018-08-14 21:29:54 +0000

    cdee9ad build(docs-infra): remove stability labels from API docs (#25453)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+15.sha-9d6e869
+ * @license Angular v6.1.2+19.sha-cdee9ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.2+15.sha-9d6e869');
+const VERSION = new Version('6.1.2+19.sha-cdee9ad');
 
 /**
  * @fileoverview added by tsickle
