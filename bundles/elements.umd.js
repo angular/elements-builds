d5f7069b6eb4eddfd9ca2971b8561314f8bc86ae
Ben Lesh <ben@benlesh.com>
2018-08-14 20:11:21 +0000

    9117fa1 test(ivy): activate local references canonical spec (#25462)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+30.sha-c869b14
+ * @license Angular v7.0.0-beta.1+38.sha-9117fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.1+30.sha-c869b14');
+    var VERSION = new core.Version('7.0.0-beta.1+38.sha-9117fa1');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+30.sha-c869b14
+ * @license Angular v7.0.0-beta.1+38.sha-9117fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.1+30.sha-c869b14');
+const VERSION = new Version('7.0.0-beta.1+38.sha-9117fa1');
 
 /**
  * @license
