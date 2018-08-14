a2c79d5173a444c73ecde21a73f72162755484bd
Ben Lesh <ben@benlesh.com>
2018-08-14 20:14:05 +0000

    ecb5dc0 docs: add Accelebrate to resources (#23204)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+38.sha-9117fa1
+ * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.1+38.sha-9117fa1');
+const VERSION = new Version('7.0.0-beta.1+34.sha-ecb5dc0');
 
 /**
  * @fileoverview added by tsickle
