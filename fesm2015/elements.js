e6370c1e4c7a1d95d44a1ef5b82a31fd60ac9c53
Ben Lesh <ben@benlesh.com>
2018-08-14 05:01:49 +0000

    1bb3014 fix(ivy): add typeof guard around ngDevMode for instances where we cannot set it in Node (#25475)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+27.sha-fb2c524
+ * @license Angular v7.0.0-beta.1+28.sha-1bb3014
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.1+27.sha-fb2c524');
+const VERSION = new Version('7.0.0-beta.1+28.sha-1bb3014');
 
 /**
  * @license
