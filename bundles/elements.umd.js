3a3e768c6b9940b01178c40f67d125dec856d9c5
Ben Lesh <ben@benlesh.com>
2018-08-14 06:00:08 +0000

    c869b14 build: drop unused re-export of json_marshal. (#25449)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+29.sha-e0314b5
+ * @license Angular v7.0.0-beta.1+30.sha-c869b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.1+29.sha-e0314b5');
+    var VERSION = new core.Version('7.0.0-beta.1+30.sha-c869b14');
 
     /**
      * @license
