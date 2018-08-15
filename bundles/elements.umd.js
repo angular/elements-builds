2690e5243dea466482a36382f9be9efa7d79ba48
Ben Lesh <ben@benlesh.com>
2018-08-15 00:50:12 +0000

    ed2a47f build(bazel): update to rules_typescript 0.16.0 & update to tagged rules_webtesting 0.2.1 (#25486)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+19.sha-cdee9ad
+ * @license Angular v6.1.2+20.sha-ed2a47f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+19.sha-cdee9ad');
+    var VERSION = new core.Version('6.1.2+20.sha-ed2a47f');
 
     /**
      * @license
