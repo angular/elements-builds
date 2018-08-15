63c8cd308a2a2f97986b65d139ac2b34c24dafae
Ben Lesh <ben@benlesh.com>
2018-08-15 23:08:56 +0000

    ea83445 release: cut the v6.1.3 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+22.sha-9c1311c
+ * @license Angular v6.1.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.2+22.sha-9c1311c');
+    var VERSION = new core.Version('6.1.3');
 
     /**
      * @license
