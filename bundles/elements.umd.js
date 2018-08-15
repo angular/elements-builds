e5579fd2bcdfc0c83f9f3d78c05c53f928fc428e
Ben Lesh <ben@benlesh.com>
2018-08-15 01:02:04 +0000

    be2cf4d docs(core): Correct spelling error in directives docs (#25377)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
+ * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.1+47.sha-a0a29fd');
+    var VERSION = new core.Version('7.0.0-beta.1+50.sha-be2cf4d');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
+ * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.1+47.sha-a0a29fd');
+const VERSION = new Version('7.0.0-beta.1+50.sha-be2cf4d');
 
 /**
  * @fileoverview added by tsickle
