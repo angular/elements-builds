e29f7be7f9f05bf5df289d9481ac7273fa74960b
Ben Lesh <ben@benlesh.com>
2018-08-16 00:04:26 +0000

    4ce70b9 release: cut the vv7.0.0-beta.2 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+52.sha-1f11039
+ * @license Angular v7.0.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.1+52.sha-1f11039');
+const VERSION = new Version('7.0.0-beta.2');
 
 /**
  * @fileoverview added by tsickle
