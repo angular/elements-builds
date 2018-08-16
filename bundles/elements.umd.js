4054c4270e4394cc9abbfbf1148c4e6ccba72c42
Jason Aden <jasonaden@google.com>
2018-08-16 22:11:35 +0000

    6482f6f refactor(ivy): separate container into 2 instructions (#25509)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+14.sha-abcc430
+ * @license Angular v7.0.0-beta.2+15.sha-6482f6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.2+14.sha-abcc430');
+    var VERSION = new core.Version('7.0.0-beta.2+15.sha-6482f6f');
 
     /**
      * @license
