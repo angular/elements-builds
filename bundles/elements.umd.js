6d957f4cf71e7aac522f80b337ab969c61ee1786
Jason Aden <jasonaden@google.com>
2018-08-16 22:11:56 +0000

    36fb4f4 docs: reactive forms guide copy edits (#25417)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3
+ * @license Angular v6.1.3+1.sha-36fb4f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.3');
+    var VERSION = new core.Version('6.1.3+1.sha-36fb4f4');
 
     /**
      * @license
