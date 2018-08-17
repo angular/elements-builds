2ffb0d800a93bad7dd17131648441b6fbf8d9cd2
Jason Aden <jasonaden@google.com>
2018-08-17 22:41:13 +0000

    d2be3d5 docs: copy edit glossary (#25468)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+24.sha-21d22ce
+ * @license Angular v7.0.0-beta.2+27.sha-d2be3d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.2+24.sha-21d22ce');
+const VERSION = new Version('7.0.0-beta.2+27.sha-d2be3d5');
 
 /**
  * @fileoverview added by tsickle
