9e8e491eb0131745a350c8d7bf964c3bfcaaaf00
Jason Aden <jasonaden@google.com>
2018-08-20 19:12:26 +0000

    77d9975 build(aio): update dgeni-packages to 0.26.3 to fix reference types issue (#25528)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+15.sha-5939c42
+ * @license Angular v6.1.3+18.sha-77d9975
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.3+15.sha-5939c42');
+const VERSION = new Version('6.1.3+18.sha-77d9975');
 
 /**
  * @license
