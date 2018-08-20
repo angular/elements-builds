7871ac29b2b6e4accf5f8d1db2ec58d21fa88c87
Jason Aden <jasonaden@google.com>
2018-08-20 19:27:14 +0000

    292b435 docs: fix typo in reactive forms guide (#25543)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+18.sha-77d9975
+ * @license Angular v6.1.3+16.sha-292b435
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.3+18.sha-77d9975');
+const VERSION = new Version('6.1.3+16.sha-292b435');
 
 /**
  * @fileoverview added by tsickle
