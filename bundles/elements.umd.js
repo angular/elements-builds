71c96ff411bb43ff201c3003ba8cd68c1d10b89f
Jason Aden <jasonaden@google.com>
2018-08-21 00:53:57 +0000

    46b0ce9 refactor(bazel): allow and ignore extra args for _ts_expected_outs (#25558)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+19.sha-78750a7
+ * @license Angular v6.1.3+20.sha-46b0ce9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.3+19.sha-78750a7');
+    var VERSION = new core.Version('6.1.3+20.sha-46b0ce9');
 
     /**
      * @license
