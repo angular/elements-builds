26232aa0e08ef7b0bbc2658c724b32d1e4baaec8
Matias Niemelä <matias@yearofmoo.com>
2018-08-22 21:16:04 +0000

    ace4e4f build: remove `NGBUILDS_IO_KEY` now that it is not used any more (#25601)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+22.sha-1fa9790
+ * @license Angular v6.1.3+23.sha-ace4e4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.3+22.sha-1fa9790');
+    var VERSION = new core.Version('6.1.3+23.sha-ace4e4f');
 
     /**
      * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+22.sha-1fa9790
+ * @license Angular v6.1.3+23.sha-ace4e4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.3+22.sha-1fa9790');
+var VERSION = new Version('6.1.3+23.sha-ace4e4f');
 
 /**
  * @license
