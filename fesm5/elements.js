3817a79a00293b55ca5b4cec0b976dac63beaa6c
Jason Aden <jasonaden@google.com>
2018-08-22 23:54:48 +0000

    52605aa docs: release notes for the v6.1.4 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
+ * @license Angular v7.0.0-beta.2+42.sha-52605aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.2+39.sha-f33dbf4');
+var VERSION = new Version('7.0.0-beta.2+42.sha-52605aa');
 
 /**
  * @license
