bee4456f63b6a2f5f56cee6ba7e524acf5fee69f
Jason Aden <jasonaden@google.com>
2018-08-23 00:26:37 +0000

    94332af release: cut the v7.0.0-beta.3 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+42.sha-52605aa
+ * @license Angular v7.0.0-beta.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.2+42.sha-52605aa');
+    var VERSION = new core.Version('7.0.0-beta.3');
 
     /**
      * @license
