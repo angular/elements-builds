ec0a03ff764046482c8258c5dc5f9f5bec6ff50e
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:32:54 +0000

    82f26fe build(bazel): remove workaround no longer needed for module names for ngfactory & ngsummary files (#25604)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4
+ * @license Angular v6.1.4+1.sha-82f26fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.4');
+    var VERSION = new core.Version('6.1.4+1.sha-82f26fe');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4
+ * @license Angular v6.1.4+1.sha-82f26fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.4');
+const VERSION = new Version('6.1.4+1.sha-82f26fe');
 
 /**
  * @license
