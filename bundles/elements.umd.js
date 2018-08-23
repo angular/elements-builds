9ec512eb0f3f2091e3f9374c3f98f4a4d4410c5f
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 03:06:43 +0000

    cc0d0a9 fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+1.sha-82f26fe
+ * @license Angular v6.1.4+2.sha-cc0d0a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.4+1.sha-82f26fe');
+    var VERSION = new core.Version('6.1.4+2.sha-cc0d0a9');
 
     /**
      * @license
