6ac3b27142ecd35beb5509c761433e59c0ffe646
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 20:43:16 +0000

    51d5b43 fix(aio): show aio-themed 404 page for unknown resources (#23188)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+2.sha-cc0d0a9
+ * @license Angular v6.1.4+3.sha-51d5b43
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.4+2.sha-cc0d0a9');
+const VERSION = new Version('6.1.4+3.sha-51d5b43');
 
 /**
  * @license
