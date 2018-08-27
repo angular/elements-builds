754cdcd773f3bff943e967dc0acf40a5444af2e5
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:24:17 +0000

    b1902db build(docs-infra): render all overloads if they are abstract (#25670)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+58.sha-6a0f78f
+ * @license Angular v7.0.0-beta.3+61.sha-b1902db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.3+58.sha-6a0f78f');
+    var VERSION = new core.Version('7.0.0-beta.3+61.sha-b1902db');
 
     /**
      * @license
