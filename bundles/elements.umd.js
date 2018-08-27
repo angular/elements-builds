903e8de3b6b094e93558675c21b8d355c2a70dd8
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:27:20 +0000

    a081d20 build(docs-infra): remove "annotations" section from API pages (#25677)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+59.sha-6ed7993
+ * @license Angular v7.0.0-beta.3+60.sha-a081d20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.3+59.sha-6ed7993');
+    var VERSION = new core.Version('7.0.0-beta.3+60.sha-a081d20');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+59.sha-6ed7993
+ * @license Angular v7.0.0-beta.3+60.sha-a081d20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.3+59.sha-6ed7993');
+const VERSION = new Version('7.0.0-beta.3+60.sha-a081d20');
 
 /**
  * @license
