d867ae41062ded6f646f65b3a6f2b555b35f39b4
Matias Niemelä <matias@yearofmoo.com>
2018-08-28 02:02:12 +0000

    0024d68 feat(ivy): add support for resolving view data from a DOM node (#25627)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+63.sha-c230173
+ * @license Angular v7.0.0-beta.3+67.sha-0024d68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.3+63.sha-c230173');
+const VERSION = new Version('7.0.0-beta.3+67.sha-0024d68');
 
 /**
  * @fileoverview added by tsickle
