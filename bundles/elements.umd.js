09d0e464974b1030e769dd5d0ab1bb1df9269d25
Miško Hevery <misko@hevery.com>
2018-08-31 18:56:50 +0000

    88f7ddb build: fix bad merge onto patch branch (#25729)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+2.sha-ed6b68b
+ * @license Angular v6.1.6+8.sha-88f7ddb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.6+2.sha-ed6b68b');
+    var VERSION = new core.Version('6.1.6+8.sha-88f7ddb');
 
     /**
      * @license
