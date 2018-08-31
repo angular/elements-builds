f09b50aad3001ac8f7d413f341e954a8c05ae3db
Misko Hevery <misko@hevery.com>
2018-08-31 21:34:16 +0000

    abd29f5 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+31.sha-31349fd
+ * @license Angular v7.0.0-beta.4+35.sha-abd29f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.4+31.sha-31349fd');
+const VERSION = new Version('7.0.0-beta.4+35.sha-abd29f5');
 
 /**
  * @license
