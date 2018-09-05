6e8679765040d3f445eab460dc42cf9d3225868b
Misko Hevery <misko@hevery.com>
2018-09-05 20:02:54 +0000

    8f8c390 test: remove deprecated Buffer usage in sourcemap test (#25805)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.6+16.sha-27f0817');
+const VERSION = new Version('6.1.6+20.sha-8f8c390');
 
 /**
  * @license
