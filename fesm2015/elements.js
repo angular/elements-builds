f0422d716908cf99c947bcd165b5d7fa213f9cb2
Misko Hevery <misko@hevery.com>
2018-09-05 19:19:35 +0000

    46de203 refactor(docs-infra): simplify custom-element polyfill setup (#25806)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+10.sha-ad7be50
+ * @license Angular v6.1.6+14.sha-46de203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.6+10.sha-ad7be50');
+const VERSION = new Version('6.1.6+14.sha-46de203');
 
 /**
  * @fileoverview added by tsickle
