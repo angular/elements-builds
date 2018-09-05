7c95b87d96ee0e546c0b19758fcd5cf6dd707430
Misko Hevery <misko@hevery.com>
2018-09-05 18:34:35 +0000

    c1ae3c1 build(docs-infra): ensure root node_modules exists (#25811)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+36.sha-e84da19
+ * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.4+36.sha-e84da19');
+const VERSION = new Version('7.0.0-beta.4+42.sha-c1ae3c1');
 
 /**
  * @fileoverview added by tsickle
