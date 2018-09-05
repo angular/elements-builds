526903c5d37e95dd7c8f15fb19e5b24cea11e6d5
Misko Hevery <misko@hevery.com>
2018-09-05 20:50:49 +0000

    2c1f550 build: update ngcontainer to node 10.9.0 (#25812)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+24.sha-e72f741
+ * @license Angular v6.1.6+25.sha-2c1f550
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.6+24.sha-e72f741');
+const VERSION = new Version('6.1.6+25.sha-2c1f550');
 
 /**
  * @fileoverview added by tsickle
