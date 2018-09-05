54f4be124c4a2ff10ed28b2d12343fa1c97da844
Misko Hevery <misko@hevery.com>
2018-09-05 19:24:09 +0000

    92298e5 build(docs-infra): do not render internals in package API pages (#25723)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+19.sha-23a96dc
+ * @license Angular v6.1.6+17.sha-92298e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.6+19.sha-23a96dc');
+var VERSION = new Version('6.1.6+17.sha-92298e5');
 
 /**
  * @license
