8bcb1e49d821d6a5924761ee4215c68b5923ab98
Misko Hevery <misko@hevery.com>
2018-09-05 19:24:24 +0000

    234661b feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+46.sha-a417b2b
+ * @license Angular v7.0.0-beta.4+48.sha-234661b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.4+46.sha-a417b2b');
+    var VERSION = new core.Version('7.0.0-beta.4+48.sha-234661b');
 
     /**
      * @license
