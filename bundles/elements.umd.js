9eadd28749a3aff7b6e49e9db81d981d5022edf8
Misko Hevery <misko@hevery.com>
2018-09-05 20:34:33 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.6+22.sha-82e0676');
+    var VERSION = new core.Version('6.1.6+24.sha-e72f741');
 
     /**
      * @license
