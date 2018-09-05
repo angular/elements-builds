c4b4c839b0feb1e61a13c7f0629faff6b39a8841
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:05 +0000

    46de203 refactor(docs-infra): simplify custom-element polyfill setup (#25806)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+10.sha-ad7be50
+ * @license Angular v6.1.6+14.sha-46de203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.6+10.sha-ad7be50');
+    var VERSION = new core.Version('6.1.6+14.sha-46de203');
 
     /**
      * @license
