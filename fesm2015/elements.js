ec8feb64377d757c6cb373558cff55a9d84be5b7
Misko Hevery <misko@hevery.com>
2018-09-06 06:06:54 +0000

    268e977 release: cut the v7.0.0-beta.5 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+59.sha-dcad054
+ * @license Angular v7.0.0-beta.5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.4+59.sha-dcad054');
+const VERSION = new Version('7.0.0-beta.5');
 
 /**
  * @license
