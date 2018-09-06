7c2af47c2bc27d5782cf5618587ef869b2183d07
Igor Minar <igor@angular.io>
2018-09-06 23:21:42 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.6+28.sha-d1063c6');
+    var VERSION = new core.Version('6.1.7+8.sha-b9a5ce1');
 
     /**
      * @license
