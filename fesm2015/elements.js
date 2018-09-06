883ff5f8c74899275a9f0e745b0f2a36c8a1d054
Miško Hevery <misko@hevery.com>
2018-09-06 02:30:07 +0000

    d1063c6 build(docs-infra): restore correct dependency versions in `package.json` (#25827)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+25.sha-2c1f550
+ * @license Angular v6.1.6+28.sha-d1063c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.6+25.sha-2c1f550');
+const VERSION = new Version('6.1.6+28.sha-d1063c6');
 
 /**
  * @fileoverview added by tsickle
