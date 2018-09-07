17265b5b5873f7d8626e3163b57a23107342fa38
Igor Minar <igor@angular.io>
2018-09-07 23:45:18 +0000

    83a1334 refactor(ivy): migrate previousOrParentNode to use TNodes (#25829)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+8.sha-2a21ca0
+ * @license Angular v7.0.0-beta.5+9.sha-83a1334
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.5+8.sha-2a21ca0');
+    var VERSION = new core.Version('7.0.0-beta.5+9.sha-83a1334');
 
     /**
      * @license
