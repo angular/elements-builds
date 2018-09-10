fd13a0187625da92528ea62882e430ef0aa249d9
Igor Minar <igor@angular.io>
2018-09-10 18:48:12 +0000

    9f3da65 docs(aio): add ng-sq-ui to resources (#25874)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+3.sha-e244b51
+ * @license Angular v6.1.7+10.sha-9f3da65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.7+3.sha-e244b51');
+const VERSION = new Version('6.1.7+10.sha-9f3da65');
 
 /**
  * @fileoverview added by tsickle
