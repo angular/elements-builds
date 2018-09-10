9d656f9b53f641b3f9ed732eb2a41adfce14e75f
Igor Minar <igor@angular.io>
2018-09-10 19:13:19 +0000

    ed266da docs(aio): add ng-sq-ui to resources (#25874)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+11.sha-91d7993
+ * @license Angular v7.0.0-beta.5+13.sha-ed266da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.5+11.sha-91d7993');
+    var VERSION = new core.Version('7.0.0-beta.5+13.sha-ed266da');
 
     /**
      * @license
