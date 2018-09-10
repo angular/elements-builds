7bf6ac70123fa9171a6b3108bc9bbddeedda669a
Igor Minar <igor@angular.io>
2018-09-10 17:41:19 +0000

    91d7993 refactor(ivy): traverse tNode tree directly (#25872)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+10.sha-96eb79b
+ * @license Angular v7.0.0-beta.5+11.sha-91d7993
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.5+10.sha-96eb79b');
+var VERSION = new Version('7.0.0-beta.5+11.sha-91d7993');
 
 /**
  * @license
