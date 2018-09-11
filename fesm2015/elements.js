638ac0fb28fa075d151a8292f69f633180d4cee9
Igor Minar <igor@angular.io>
2018-09-11 14:52:56 +0000

    d5bd86a fix(ivy): don't accidently read the inherited definition (#25736)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+20.sha-a9099e8
+ * @license Angular v7.0.0-beta.5+21.sha-d5bd86a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-beta.5+20.sha-a9099e8');
+const VERSION = new Version('7.0.0-beta.5+21.sha-d5bd86a');
 
 /**
  * @license
