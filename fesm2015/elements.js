cf7d3e7bfa5df95fd925d8c47ef5e151af285519
Igor Minar <igor@angular.io>
2018-09-12 00:42:53 +0000

    8d7fbb6 docs(router): change description of parameter usage for navigation functions (#21840)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+11.sha-55a1ce7
+ * @license Angular v6.1.7+13.sha-8d7fbb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.7+11.sha-55a1ce7');
+const VERSION = new Version('6.1.7+13.sha-8d7fbb6');
 
 /**
  * @license
