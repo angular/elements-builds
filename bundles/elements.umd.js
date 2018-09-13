7e06ea4704dfa8184598d24557740df83e5cb517
Ben Lesh <ben@benlesh.com>
2018-09-13 23:58:11 +0000

    076374b docs: delete old comments from example (#25931)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.7+12.sha-a31cfc5');
+    var VERSION = new core.Version('6.1.7+15.sha-076374b');
 
     /**
      * @license
