cf5b9c90cf316292a176626d5e357ddd30d6d3b0
Ben Lesh <ben@benlesh.com>
2018-09-15 01:20:42 +0000

    d9bd860 docs: fix typo in bootstrapping guide (#25939)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+15.sha-076374b
+ * @license Angular v6.1.7+16.sha-d9bd860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.7+15.sha-076374b');
+const VERSION = new Version('6.1.7+16.sha-d9bd860');
 
 /**
  * @license
