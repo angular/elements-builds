5cb602ba5be0b21c1fb7cbf192460307c4d8ebdb
Ben Lesh <ben@benlesh.com>
2018-09-17 20:41:08 +0000

    5653874 fix(ivy): events should not mark views dirty by default (#25969)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+34.sha-8f81dba
+ * @license Angular v7.0.0-beta.5+38.sha-5653874
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.5+34.sha-8f81dba');
+var VERSION = new Version('7.0.0-beta.5+38.sha-5653874');
 
 /**
  * @license
