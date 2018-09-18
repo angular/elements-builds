96f199aaf0b781ff98aa9483a9c696b66d523684
Ben Lesh <ben@benlesh.com>
2018-09-18 21:13:01 +0000

    56283ed build: update .nvmrc file to correct node version (#25992)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+50.sha-96ee898
+ * @license Angular v7.0.0-beta.5+46.sha-56283ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.5+50.sha-96ee898');
+    var VERSION = new core.Version('7.0.0-beta.5+46.sha-56283ed');
 
     /**
      * @license
