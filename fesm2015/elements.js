7cd56e0173544e9cec107c0320b5254b3f4b6cd3
Ben Lesh <ben@benlesh.com>
2018-09-18 21:58:18 +0000

    110c81f build: ignore `aio`/`docs-infra` commits in `changelog` gulp task (#25838)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+19.sha-c693624
+ * @license Angular v6.1.7+21.sha-110c81f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.7+19.sha-c693624');
+const VERSION = new Version('6.1.7+21.sha-110c81f');
 
 /**
  * @license
