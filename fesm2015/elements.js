c4d8878c00040e3212e0ece84e9f60f56727e191
Ben Lesh <ben@benlesh.com>
2018-09-18 21:41:56 +0000

    c693624 build: update .nvmrc file to correct node version (#25992)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+17.sha-274dc1e
+ * @license Angular v6.1.7+19.sha-c693624
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.7+17.sha-274dc1e');
+const VERSION = new Version('6.1.7+19.sha-c693624');
 
 /**
  * @license
