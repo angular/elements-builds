1127b4ffa3c2a1bd576cc730b9a55028ff7ab459
Ben Lesh <ben@benlesh.com>
2018-09-19 22:35:26 +0000

    3988ebf release: cut the v6.1.8 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+39.sha-a41f331
+ * @license Angular v6.1.8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('6.1.7+39.sha-a41f331');
+var VERSION = new Version('6.1.8');
 
 /**
  * @license
