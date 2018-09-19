d1f4216fa8aecac8ce0fde493361e9df5729d01b
Ben Lesh <ben@benlesh.com>
2018-09-19 19:18:38 +0000

    5099b79 docs: copy edit (#25582)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+37.sha-df878a6
+ * @license Angular v6.1.7+42.sha-5099b79
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.7+37.sha-df878a6');
+    var VERSION = new core.Version('6.1.7+42.sha-5099b79');
 
     /**
      * @license
