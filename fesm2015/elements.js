6b6ac0778b60583405887366197fad0cde6e2368
Ben Lesh <ben@benlesh.com>
2018-09-19 19:20:20 +0000

    5099b79 docs: copy edit (#25582)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+37.sha-df878a6
+ * @license Angular v6.1.7+42.sha-5099b79
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.7+37.sha-df878a6');
+const VERSION = new Version('6.1.7+42.sha-5099b79');
 
 /**
  * @license
