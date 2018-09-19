8f5d0c36e7c0f64bc047793b5997e198d08ff0b7
Ben Lesh <ben@benlesh.com>
2018-09-19 17:21:49 +0000

    e8bab13 docs: delete extra sentence (#25984)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+51.sha-cbbad1b
+ * @license Angular v7.0.0-beta.5+54.sha-e8bab13
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.5+51.sha-cbbad1b');
+const VERSION = new Version('7.0.0-beta.5+54.sha-e8bab13');
 
 /**
  * @fileoverview added by tsickle
