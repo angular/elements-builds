d7bf3a653403f346d0f1a87ae2bb8474281807aa
Kara Erickson <karakara@google.com>
2018-09-21 18:49:34 +0000

    a135f48 docs: add Sajee to contributors (#26028)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+11.sha-f1cb460
+ * @license Angular v6.1.8+13.sha-a135f48
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.8+11.sha-f1cb460');
+    var VERSION = new core.Version('6.1.8+13.sha-a135f48');
 
     /**
      * @license
