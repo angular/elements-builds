f229256d64abc2e1e3f1903ad2dc46a480f0404c
Kara Erickson <karakara@google.com>
2018-09-21 18:51:06 +0000

    61b4c26 ci: correct github robot size check configuration (#26057)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+13.sha-a135f48
+ * @license Angular v6.1.8+12.sha-61b4c26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.8+13.sha-a135f48');
+    var VERSION = new core.Version('6.1.8+12.sha-61b4c26');
 
     /**
      * @license
