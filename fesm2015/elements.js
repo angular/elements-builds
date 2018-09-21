e20f84995b8b5ef0f9872f012770c48d6d3c51dc
Kara Erickson <karakara@google.com>
2018-09-21 18:55:38 +0000

    05d1b84 fix(docs-infra): ensure that only search is removed from URL on click (#26056)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+12.sha-61b4c26
+ * @license Angular v6.1.8+18.sha-05d1b84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.8+12.sha-61b4c26');
+const VERSION = new Version('6.1.8+18.sha-05d1b84');
 
 /**
  * @fileoverview added by tsickle
