ae86dfc0b7bd1d53afcea897723d29fe6e282aed
Kara Erickson <karakara@google.com>
2018-09-24 19:12:43 +0000

    82a14dc feat(ivy): provide groundwork for animations in core (#25234)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+25.sha-64aa670
+ * @license Angular v7.0.0-beta.6+35.sha-82a14dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.6+25.sha-64aa670');
+    var VERSION = new core.Version('7.0.0-beta.6+35.sha-82a14dc');
 
     /**
      * @license
