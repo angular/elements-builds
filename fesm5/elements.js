3af73c1e62a5afb8409dc2bcd9dbdb63da0dc000
Kara Erickson <karakara@google.com>
2018-09-24 19:14:11 +0000

    82a14dc feat(ivy): provide groundwork for animations in core (#25234)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+25.sha-64aa670
+ * @license Angular v7.0.0-beta.6+35.sha-82a14dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-beta.6+25.sha-64aa670');
+var VERSION = new Version('7.0.0-beta.6+35.sha-82a14dc');
 
 /**
  * @license
