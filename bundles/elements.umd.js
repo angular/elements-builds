42b6626f03690d7345d68ba37d3dea70a9d30bb2
Kara Erickson <karakara@google.com>
2018-09-24 17:27:49 +0000

    a730fc7 build(docs-infra): fail doc-gen if a content rule fails (#26039)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+37.sha-cea7fbe
+ * @license Angular v6.1.8+34.sha-a730fc7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.8+37.sha-cea7fbe');
+    var VERSION = new core.Version('6.1.8+34.sha-a730fc7');
 
     /**
      * @license
