cffdefacc1c086ec3fab5d4213cc745e9b60342c
Kara Erickson <karakara@google.com>
2018-09-24 16:40:52 +0000

    b8422b4 build(docs-infra): fail doc-gen if a content rule fails (#26039)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+37.sha-7f1cace
+ * @license Angular v7.0.0-beta.6+51.sha-b8422b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.6+37.sha-7f1cace');
+const VERSION = new Version('7.0.0-beta.6+51.sha-b8422b4');
 
 /**
  * @fileoverview added by tsickle
