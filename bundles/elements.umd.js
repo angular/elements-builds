d1c24e6446dde62da61245edb841c7185c6fa452
Kara Erickson <karakara@google.com>
2018-09-25 18:44:06 +0000

    0c34471 feat(ivy): expose a series of helpful application inspection tools (#25919)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+35.sha-82a14dc
+ * @license Angular v7.0.0-beta.6+58.sha-0c34471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-beta.6+35.sha-82a14dc');
+    var VERSION = new core.Version('7.0.0-beta.6+58.sha-0c34471');
 
     /**
      * @license
