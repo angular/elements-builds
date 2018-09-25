d018d10c8506831d2ad89b65d2564872496145dd
Kara Erickson <karakara@google.com>
2018-09-25 19:11:34 +0000

    42e2e7c build: remove obsolete comment in `env.sh` (#25819)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.8+39.sha-adad170');
+    var VERSION = new core.Version('6.1.8+40.sha-42e2e7c');
 
     /**
      * @license
