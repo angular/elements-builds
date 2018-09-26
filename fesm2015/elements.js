eecc17e19978acdc8c0ba1a84cfcb34cf1093513
Kara Erickson <karakara@google.com>
2018-09-26 02:22:11 +0000

    c7e2930 docs: fix issues related to tutorial. (#24445)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+60.sha-6a62ed2
+ * @license Angular v7.0.0-beta.6+61.sha-c7e2930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-beta.6+60.sha-6a62ed2');
+const VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
 
 /**
  * @fileoverview added by tsickle
