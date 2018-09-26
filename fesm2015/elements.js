d63c5f3c1b032163b4ebe692341e9fcb6f361b9a
Kara Erickson <karakara@google.com>
2018-09-26 21:14:58 +0000

    acffa22 release: cut the v6.1.9 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+65.sha-159e8b4
+ * @license Angular v6.1.9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.8+65.sha-159e8b4');
+const VERSION = new Version('6.1.9');
 
 /**
  * @license
