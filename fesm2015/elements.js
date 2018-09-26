e9038b3a570b29986301c9c63aa83291992aa44d
Kara Erickson <karakara@google.com>
2018-09-26 23:57:46 +0000

    5881f34 docs: firefox web components info (#26118)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9
+ * @license Angular v6.1.9+1.sha-5881f34
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.9');
+const VERSION = new Version('6.1.9+1.sha-5881f34');
 
 /**
  * @license
