4c903080201f83e80ab1a013c87c6a7283cb8fe0
Kara Erickson <karakara@google.com>
2018-09-26 23:57:46 +0000

    c12e553 fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+1.sha-5881f34
+ * @license Angular v6.1.9+18.sha-c12e553
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.9+1.sha-5881f34');
+    var VERSION = new core.Version('6.1.9+18.sha-c12e553');
 
     /**
      * @license
