10961b029607162a730c27d5b1e4392e969d0a9f
Kara Erickson <karakara@google.com>
2018-09-26 21:13:26 +0000

    acffa22 release: cut the v6.1.9 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+65.sha-159e8b4
+ * @license Angular v6.1.9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.8+65.sha-159e8b4');
+    var VERSION = new core.Version('6.1.9');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+65.sha-159e8b4
+ * @license Angular v6.1.9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -587,7 +587,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('6.1.8+65.sha-159e8b4');
+const VERSION = new Version('6.1.9');
 
 /**
  * @fileoverview added by tsickle
