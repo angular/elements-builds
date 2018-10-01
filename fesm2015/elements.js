a62545aaecb46deba6eeb7c5199f79bee078611a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 17:20:28 +0000

    ab379ab refactor(ivy): always use styling helper methods when comparing values (#26149)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0
+ * @license Angular v7.0.0-rc.0+5.sha-ab379ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-rc.0');
+const VERSION = new Version('7.0.0-rc.0+5.sha-ab379ab');
 
 /**
  * @license
