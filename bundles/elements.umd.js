4bc7d1e1dec1687d5a647728455d753580851990
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 23:11:13 +0000

    7306799 refactor(ivy): flatten LInjector into LViewData (#26220)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+25.sha-cb59d87
+ * @license Angular v7.0.0-rc.0+26.sha-7306799
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.0+25.sha-cb59d87');
+    var VERSION = new core.Version('7.0.0-rc.0+26.sha-7306799');
 
     /**
      * @license
