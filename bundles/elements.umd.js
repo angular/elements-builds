1c3b214035b2c832cbf39b67cb2015874ca4aa9d
Jason Aden <jasonaden@google.com>
2018-10-05 21:43:35 +0000

    4006c9b docs: fix spelling errors (#26213)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+48.sha-8f25321
+ * @license Angular v7.0.0-rc.0+55.sha-4006c9b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.0+48.sha-8f25321');
+    var VERSION = new core.Version('7.0.0-rc.0+55.sha-4006c9b');
 
     /**
      * @license
