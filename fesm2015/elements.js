2f931af5decd6712a6399178af1f4c058dffbb68
Jason Aden <jasonaden@google.com>
2018-10-05 23:23:18 +0000

    3b4d9dc fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+68.sha-3886bfa
+ * @license Angular v6.1.9+70.sha-3b4d9dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.9+68.sha-3886bfa');
+const VERSION = new Version('6.1.9+70.sha-3b4d9dc');
 
 /**
  * @license
