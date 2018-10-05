07924ba6e0fafb9e845ff6ddfdd1f14040c90fdf
Jason Aden <jasonaden@google.com>
2018-10-05 21:26:30 +0000

    8f25321 refactor(ivy): update context discovery to prep for dir merge (#26262)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+54.sha-245b85f
+ * @license Angular v7.0.0-rc.0+48.sha-8f25321
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.0+54.sha-245b85f');
+const VERSION = new Version('7.0.0-rc.0+48.sha-8f25321');
 
 /**
  * @fileoverview added by tsickle
