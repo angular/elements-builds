55cbc9e3078ce154ff954e8d34c5cb7b0c87ae3a
Jason Aden <jasonaden@google.com>
2018-10-05 21:49:45 +0000

    b4421bb docs: fix wording (#26207)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+62.sha-ecb28bf
+ * @license Angular v6.1.9+63.sha-b4421bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.9+62.sha-ecb28bf');
+    var VERSION = new core.Version('6.1.9+63.sha-b4421bb');
 
     /**
      * @license
