a8b3a29ca5598c5947d426ec959b252d49d5a095
Miško Hevery <misko@hevery.com>
2018-10-11 21:58:04 +0000

    50de03a docs: fix transpiles link in dependency injection (#26250)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+4.sha-f1e14a3
+ * @license Angular v6.1.10+3.sha-50de03a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.10+4.sha-f1e14a3');
+    var VERSION = new core.Version('6.1.10+3.sha-50de03a');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+4.sha-f1e14a3
+ * @license Angular v6.1.10+3.sha-50de03a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.10+4.sha-f1e14a3');
+const VERSION = new Version('6.1.10+3.sha-50de03a');
 
 /**
  * @license
