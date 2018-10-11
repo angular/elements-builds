6c12f1723240efc03dc2ff6b83cd5934281b559a
Miško Hevery <misko@hevery.com>
2018-10-11 21:50:31 +0000

    0a3f817 refactor(platform-browser): remove type assertion on console.profileEnd (#26303)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+18.sha-bd186c7
+ * @license Angular v7.0.0-rc.1+22.sha-0a3f817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+18.sha-bd186c7');
+    var VERSION = new core.Version('7.0.0-rc.1+22.sha-0a3f817');
 
     /**
      * @license
