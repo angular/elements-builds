e6028c2bb804b854e72ea9fb7419836cc6223f97
Miško Hevery <misko@hevery.com>
2018-10-11 20:29:06 +0000

    39f42ba feat(ivy): i18n compiler support for element attributes (#26280)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+105.sha-7acdad6
+ * @license Angular v7.0.0-rc.1+4.sha-39f42ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-rc.0+105.sha-7acdad6');
+const VERSION = new Version('7.0.0-rc.1+4.sha-39f42ba');
 
 /**
  * @license
