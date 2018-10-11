241ce04c29f582f71fc95ce26cfae5612982b9ea
Miško Hevery <misko@hevery.com>
2018-10-11 23:16:26 +0000

    0a3f817 refactor(platform-browser): remove type assertion on console.profileEnd (#26303)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+103.sha-912f3d1
+ * @license Angular v7.0.0-rc.1+22.sha-0a3f817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-var VERSION = new Version('7.0.0-rc.0+103.sha-912f3d1');
+var VERSION = new Version('7.0.0-rc.1+22.sha-0a3f817');
 
 /**
  * @license
