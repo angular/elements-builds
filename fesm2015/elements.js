980c7a9658fc0b8ea834fb2e1a260bb7512bd302
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:46 +0000

    b2db32b docs: getting started updated for accuracy and style (#26093)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+37.sha-a19b690
+ * @license Angular v7.0.0-rc.1+39.sha-b2db32b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+37.sha-a19b690');
+const VERSION = new Version('7.0.0-rc.1+39.sha-b2db32b');
 
 /**
  * @fileoverview added by tsickle
