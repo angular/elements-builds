8f83de5eef7e75bcbb1d87219f47d156cd2e3a71
Miško Hevery <misko@hevery.com>
2018-10-12 21:39:20 +0000

    bab5b68 fix(docs-infra): prevent unnecessary SideNav scrollbar (#26416)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+35.sha-3daeadd
+ * @license Angular v7.0.0-rc.1+36.sha-bab5b68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+35.sha-3daeadd');
+    var VERSION = new core.Version('7.0.0-rc.1+36.sha-bab5b68');
 
     /**
      * @license
