5b0c43d56052fc8004ce4d22017e4062eaea2d8c
Miško Hevery <misko@hevery.com>
2018-10-15 17:38:57 +0000

    45732e5 fix(ivy): fix reference to pipeBind instruction with 2 args (#26451)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+39.sha-b2db32b
+ * @license Angular v7.0.0-rc.1+40.sha-45732e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+39.sha-b2db32b');
+const VERSION = new Version('7.0.0-rc.1+40.sha-45732e5');
 
 /**
  * @fileoverview added by tsickle
