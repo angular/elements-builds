32e59f9eb4b3b9156fd1b5c81bd941605ad11977
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 20:12:11 +0000

    a720d8a build(docs-infra): upgrade cli command docs sources to 9b72649b8

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+40.sha-b821218
+ * @license Angular v7.0.0-rc.1+40.sha-a720d8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+40.sha-b821218');
+const VERSION = new Version('7.0.0-rc.1+40.sha-a720d8a');
 
 /**
  * @fileoverview added by tsickle
