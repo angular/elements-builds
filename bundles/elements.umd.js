6e7ec8ff2980db2cd91f5ef2f6340e54b29a56c5
Miško Hevery <misko@hevery.com>
2018-10-15 22:55:07 +0000

    05f279d test(docs-infra): improve logging output in `test-pwa-score[-localhost]` (#26459)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+14.sha-a251374
+ * @license Angular v6.1.10+17.sha-05f279d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('6.1.10+14.sha-a251374');
+    var VERSION = new core.Version('6.1.10+17.sha-05f279d');
 
     /**
      * @license
