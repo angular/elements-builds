c3c48a86cd7dcfced1920d28ca21ccc220d18524
Miško Hevery <misko@hevery.com>
2018-10-15 18:58:45 +0000

    5183bbf docs: update process for cli tool and restructure doc (#25752)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+42.sha-9afc9a7
+ * @license Angular v7.0.0-rc.1+44.sha-5183bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-rc.1+42.sha-9afc9a7');
+const VERSION = new Version('7.0.0-rc.1+44.sha-5183bbf');
 
 /**
  * @license
