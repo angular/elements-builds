7852a737b6a3ab873e392566b51983e1367857cc
Miško Hevery <misko@hevery.com>
2018-10-15 17:59:26 +0000

    81724f5 ci(docs-infra): allow `aio_local` builds to fail on Travis

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+11.sha-1f06b6c
+ * @license Angular v6.1.10+12.sha-81724f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.10+11.sha-1f06b6c');
+const VERSION = new Version('6.1.10+12.sha-81724f5');
 
 /**
  * @license
