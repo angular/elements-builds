73b7180f378d3344707afb33d05f2815eee6a444
Miško Hevery <misko@hevery.com>
2018-10-15 17:43:09 +0000

    931e603 refactor(ivy): revert LNode.data into LViewData[HOST] (#26424)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+40.sha-45732e5
+ * @license Angular v7.0.0-rc.1+41.sha-931e603
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+40.sha-45732e5');
+const VERSION = new Version('7.0.0-rc.1+41.sha-931e603');
 
 /**
  * @fileoverview added by tsickle
