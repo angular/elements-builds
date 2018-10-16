6b2d3bb126d36d73cda19c6231b688e82a33fb2a
Miško Hevery <misko@hevery.com>
2018-10-16 22:13:32 +0000

    ea2cfbb docs: remove setup for local dev and anatomy of setup docs from nav (#26380)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+55.sha-2326b9c
+ * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
+const VERSION = new Version('7.0.0-rc.1+56.sha-ea2cfbb');
 
 /**
  * @fileoverview added by tsickle
