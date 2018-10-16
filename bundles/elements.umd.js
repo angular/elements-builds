88e7c508e9ae4b6bcae109fae7cf926487aacc51
Miško Hevery <misko@hevery.com>
2018-10-16 00:16:02 +0000

    dd8a851 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+48.sha-164f79a
+ * @license Angular v7.0.0-rc.1+49.sha-dd8a851
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+48.sha-164f79a');
+    var VERSION = new core.Version('7.0.0-rc.1+49.sha-dd8a851');
 
     /**
      * @license
