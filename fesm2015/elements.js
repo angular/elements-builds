652ecf4cace42d1079dcfbc349d25234ede4daad
Miško Hevery <misko@hevery.com>
2018-10-16 00:21:44 +0000

    eeebe62 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('6.1.10+17.sha-05f279d');
+const VERSION = new Version('6.1.10+18.sha-eeebe62');
 
 /**
  * @license
