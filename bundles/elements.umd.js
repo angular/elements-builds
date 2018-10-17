520032fd9956089a66d0bb9e36bbc5fdba82eb01
Misko Hevery <misko@hevery.com>
2018-10-17 19:20:05 +0000

    3fa876c fix(docs-infra): fix top menu logo position (#26473)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+68.sha-516515d
+ * @license Angular v7.0.0-rc.1+69.sha-3fa876c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+68.sha-516515d');
+    var VERSION = new core.Version('7.0.0-rc.1+69.sha-3fa876c');
 
     /**
      * @license
