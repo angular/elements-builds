10ea10de8954e994e21769df37cc02649082b777
Miško Hevery <misko@hevery.com>
2018-10-17 20:42:19 +0000

    b49b274 fix(aio): add relative to app level routes section (#26504)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+72.sha-9997ab5
+ * @license Angular v7.0.0-rc.1+74.sha-b49b274
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+72.sha-9997ab5');
+    var VERSION = new core.Version('7.0.0-rc.1+74.sha-b49b274');
 
     /**
      * @license
