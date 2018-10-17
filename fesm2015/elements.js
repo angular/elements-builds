dc16f5070bde530913ad35d8983c84e5fafbcbf5
Misko Hevery <misko@hevery.com>
2018-10-17 19:20:47 +0000

    3808434 build(docs-infra): do not add extra space after links in tables (#26505)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+60.sha-4b52257
+ * @license Angular v7.0.0-rc.1+59.sha-3808434
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+60.sha-4b52257');
+const VERSION = new Version('7.0.0-rc.1+59.sha-3808434');
 
 /**
  * @fileoverview added by tsickle
