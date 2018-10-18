78a157e769c9bb449d25ec362e95f27a9cee9245
Miško Hevery <misko@hevery.com>
2018-10-18 20:54:58 +0000

    331989c docs: add release announcement URL

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+100.sha-bbf96db
+ * @license Angular v7.0.0-rc.1+101.sha-331989c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+100.sha-bbf96db');
+    var VERSION = new core.Version('7.0.0-rc.1+101.sha-331989c');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+100.sha-bbf96db
+ * @license Angular v7.0.0-rc.1+101.sha-331989c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-rc.1+100.sha-bbf96db');
+const VERSION = new Version('7.0.0-rc.1+101.sha-331989c');
 
 /**
  * @license
