4fa63f4949c65f7463fd8383e88a941cf5d02e0d
Miško Hevery <misko@hevery.com>
2018-10-18 18:16:20 +0000

    efe0c5f release: cut the v7.0.0 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+91.sha-d9d2260
+ * @license Angular v7.0.0-rc.1+92.sha-efe0c5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+91.sha-d9d2260');
+const VERSION = new Version('7.0.0-rc.1+92.sha-efe0c5f');
 
 /**
  * @fileoverview added by tsickle
