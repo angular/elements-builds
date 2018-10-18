f3aa980724eae0a8e4dc5576bd9cd85dd02faeee
Miško Hevery <misko@hevery.com>
2018-10-18 18:42:54 +0000

    bf656d6 release: cut the v7.0.0 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+78.sha-3b183ce
+ * @license Angular v7.0.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+78.sha-3b183ce');
+    var VERSION = new core.Version('7.0.0');
 
     /**
      * @license
