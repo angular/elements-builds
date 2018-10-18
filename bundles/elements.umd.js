2235f1834edea4b0796b800e0380053de6820baa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:03 +0000

    a403c4f docs: type fix (#26386)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0');
+    var VERSION = new core.Version('7.0.0+1.sha-a403c4f');
 
     /**
      * @license
