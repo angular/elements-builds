6922b33262d7ec27c4920713eac4823876b9dc5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:42:31 +0000

    624433c docs: removing errant double-quote (#26483) PR Close #26483

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+93.sha-09cc458
+ * @license Angular v7.0.0-rc.1+94.sha-624433c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+93.sha-09cc458');
+const VERSION = new Version('7.0.0-rc.1+94.sha-624433c');
 
 /**
  * @fileoverview added by tsickle
