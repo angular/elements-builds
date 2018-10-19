6b6a501e325a514421d4433dc8c817b57b380122
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:19:10 +0000

    716d887 docs: add Katerina Skroumpelou to GDE resources (#26547)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+110.sha-4c0ad52
+ * @license Angular v7.0.0-rc.1+112.sha-716d887
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -464,7 +464,7 @@ function createCustomElement(component, config) {
 /**
  * @experimental
  */
-const VERSION = new Version('7.0.0-rc.1+110.sha-4c0ad52');
+const VERSION = new Version('7.0.0-rc.1+112.sha-716d887');
 
 /**
  * @license
