aee5256dc2015493a18d03a19e55f9e0d5e2972a
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:13:04 +0000

    658a18c build(docs-infra): break long CLI options onto two lines (#26272)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+26.sha-5d90aed
+ * @license Angular v7.0.0+25.sha-658a18c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@experimental
   @type {?} */
-const VERSION = new Version('7.0.0+26.sha-5d90aed');
+const VERSION = new Version('7.0.0+25.sha-658a18c');
 
 /**
  * @fileoverview added by tsickle
