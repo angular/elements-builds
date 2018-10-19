81395251752c41e9622fc92604df3fc1e52e8955
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:17:29 +0000

    4c0ad52 build(docs-infra): display github links in CLI API docs (#26515)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+107.sha-07fc4c2
+ * @license Angular v7.0.0-rc.1+110.sha-4c0ad52
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @experimental
      */
-    var VERSION = new core.Version('7.0.0-rc.1+107.sha-07fc4c2');
+    var VERSION = new core.Version('7.0.0-rc.1+110.sha-4c0ad52');
 
     /**
      * @license
