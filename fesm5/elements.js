6535961ff39bbf17d8bbc4c846872e96f3e3356f
Kara Erickson <karakara@google.com>
2018-11-01 21:51:53 +0000

    5eb540c docs: Webcomponents activated as of firefox 63 (#26889) PR Close #26889

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+6.sha-7697650
+ * @license Angular v7.0.2+7.sha-5eb540c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+6.sha-7697650');
+var VERSION = new Version('7.0.2+7.sha-5eb540c');
 
 /**
  * @license
