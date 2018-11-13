6e453b2d925683f08a2bed0b4b14459c0e7802db
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:18:56 +0000

    2ec0541 docs: new doc for core directives (#26923)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+21.sha-188e9ce
+ * @license Angular v7.0.3+22.sha-2ec0541
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+21.sha-188e9ce');
+const VERSION = new Version('7.0.3+22.sha-2ec0541');
 
 /**
  * @fileoverview added by tsickle
