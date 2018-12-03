5f2978e9dc2bd959d7983dbb30110b842382df05
Filipe Silva <filipematossilva@gmail.com>
2018-12-03 15:34:04 +0000

    4fd3402 ci: test ts-api-guardian on windows

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+17.sha-e201a67.with-local-changes
+ * @license Angular v7.1.0+103.sha-4fd3402
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -537,7 +537,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-rc.0+17.sha-e201a67.with-local-changes');
+    var VERSION = new core.Version('7.1.0+103.sha-4fd3402');
 
     /**
      * @license
