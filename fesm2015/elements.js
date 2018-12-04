3a14598d35f79e481b47d719fb59259aef3aa9ce
Igor Minar <igor@angular.io>
2018-12-04 22:40:14 +0000

    1597886 fix(ivy): resolve resources using TS module resolution semantics (#27357)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+128.sha-8e644d9
+ * @license Angular v7.1.0+127.sha-1597886
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+128.sha-8e644d9');
+const VERSION = new Version('7.1.0+127.sha-1597886');
 
 /**
  * @fileoverview added by tsickle
