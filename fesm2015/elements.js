94ad35744bbecece0ed12ac2582905d1a2c034bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 18:32:17 +0000

    05cdfb9 fix(ivy): make ViewRef.detectChanges work with embedded views (FW-749) (#27521)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+12.sha-b0c7561
+ * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -589,7 +589,7 @@ function createCustomElement(component, config) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+12.sha-b0c7561');
+const VERSION = new Version('7.2.0-beta.1+22.sha-05cdfb9');
 
 /**
  * @fileoverview added by tsickle
