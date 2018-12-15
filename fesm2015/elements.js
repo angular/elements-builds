ca4430737632f5519863c6ddc13a3da04987ecd3
Miško Hevery <misko@hevery.com>
2018-12-15 00:06:57 +0000

    7dab458 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+43.sha-f9545d1
+ * @license Angular v7.2.0-beta.2+51.sha-7dab458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -593,7 +593,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+43.sha-f9545d1');
+const VERSION = new Version('7.2.0-beta.2+51.sha-7dab458');
 
 /**
  * @fileoverview added by tsickle
