b291b89efbca1a0009f18aaa7235c4abf0b6b185
Miško Hevery <misko@hevery.com>
2018-12-18 22:17:27 +0000

    7901cd8 fix(ivy): add missing AttributeMarker export (#27485) PR Close #27485

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+90.sha-7901cd8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+82.sha-1c93afe');
+var VERSION = new Version('7.2.0-beta.2+90.sha-7901cd8');
 
 /**
  * @license
