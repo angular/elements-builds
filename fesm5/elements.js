b5435bc9538e6d8dc02a96585f05529d4dd3f531
Kara Erickson <karakara@google.com>
2019-01-07 23:11:49 +0000

    a75c734 fix(ivy): restore NG defs after running tests in TestBed (#27786)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+9.sha-e775313
+ * @license Angular v7.2.0+10.sha-a75c734
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+9.sha-e775313');
+var VERSION = new Version('7.2.0+10.sha-a75c734');
 
 /**
  * @license
