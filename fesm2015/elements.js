65a32043b7280e2f25a2dc1b3b8c2aa129d1dd32
Kara Erickson <karakara@google.com>
2019-01-07 23:18:49 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+10.sha-a75c734
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -600,7 +600,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+10.sha-a75c734');
+const VERSION = new Version('7.2.0+15.sha-4613864');
 
 /**
  * @fileoverview added by tsickle
