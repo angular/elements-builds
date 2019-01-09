e1e1d0d14cf9b4b82729eca99d7638288e61b8f1
Kara Erickson <karakara@google.com>
2019-01-09 01:44:20 +0000

    c3aa24c fix(ivy): sanitization for Host Bindings (#27939)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+55.sha-1de4031
+ * @license Angular v7.2.0+56.sha-c3aa24c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -600,7 +600,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+55.sha-1de4031');
+const VERSION = new Version('7.2.0+56.sha-c3aa24c');
 
 /**
  * @fileoverview added by tsickle
