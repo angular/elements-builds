4dd909f6d03b43afc8771a2f96e20eb26b24985a
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:06:32 +0000

    b78351c build(bazel): Bump @bazel/typescript & @bazel/karma to 0.22.1 (#28031)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0+103.sha-885f1af
+ * @license Angular v7.2.0+104.sha-b78351c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { ComponentFactoryResolver, ApplicationRef, Injector, SimpleChange, Version } from '@angular/core';
-import { __extends, __spread } from 'tslib';
+import { __spread, __extends } from 'tslib';
+import { ComponentFactoryResolver, Injector, ApplicationRef, SimpleChange, Version } from '@angular/core';
 import { merge } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+103.sha-885f1af');
+var VERSION = new Version('7.2.0+104.sha-b78351c');
 
 /**
  * @license
