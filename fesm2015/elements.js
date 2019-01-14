f74d8a2281d075a67cff46d2081e627edacbe54d
Andrew Kushnir <akushnir@google.com>
2019-01-14 22:30:44 +0000

    d12db4e fix(bazel): incorrectly always uses ngc-wrapped from "npm" workspace (#28137)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+161.sha-e8495b4
+ * @license Angular v7.2.0+169.sha-d12db4e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -600,7 +600,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+161.sha-e8495b4');
+const VERSION = new Version('7.2.0+169.sha-d12db4e');
 
 /**
  * @fileoverview added by tsickle
