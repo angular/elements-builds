e359df75dd35a644f554b80d4c899828fb106c9e
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:51:05 +0000

    0516839 release: cut the v7.2.1 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-03293c4
+ * @license Angular v7.2.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+98.sha-03293c4');
+var VERSION = new Version('7.2.1');
 
 /**
  * @license
