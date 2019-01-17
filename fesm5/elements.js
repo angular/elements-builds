f00bd6598563db157a5b38817687638691e1715d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:07:05 +0000

    d8c75f1 docs: fix comments in services example (#26194)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+4.sha-6d40ef2
+ * @license Angular v7.2.1+5.sha-d8c75f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+4.sha-6d40ef2');
+var VERSION = new Version('7.2.1+5.sha-d8c75f1');
 
 /**
  * @license
