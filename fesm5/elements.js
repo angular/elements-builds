a2fd27fedcba03db0060d4ef5381c24c3da5b4d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:40 +0000

    4ea6c27 build(bazel): Use @angular/cli from root package.json (#28139)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1
+ * @license Angular v7.2.1+3.sha-4ea6c27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1');
+var VERSION = new Version('7.2.1+3.sha-4ea6c27');
 
 /**
  * @license
