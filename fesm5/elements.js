6146c784dd09d64425be9f4db6e246b78a4a63c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:22:37 +0000

    93d78c9 build(bazel): Use local rollup & build-optimizer for ng_rollup_bundle (#28215)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+6.sha-49bee4c
+ * @license Angular v7.2.1+7.sha-93d78c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+6.sha-49bee4c');
+var VERSION = new Version('7.2.1+7.sha-93d78c9');
 
 /**
  * @license
