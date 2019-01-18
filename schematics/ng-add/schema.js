cd5297bb624b2e2edf5772fa25068808c3cae244
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:34:53 +0000

    38343a2 build: set a default module_name for ts_library rules (#28051)

--- schematics/ng-add/schema.js
+++ schematics/ng-add/schema.js
@@ -11,7 +11,7 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define("angular/packages/elements/schematics/ng-add/schema", ["require", "exports"], factory);
+        define("@angular/elements/schematics/ng-add/schema", ["require", "exports"], factory);
     }
 })(function (require, exports) {
     "use strict";
