93a4082c8db5dfb18c9611e0436c33c95ec69829
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:18:18 +0000

    5877b3f refactor: remove unused parameter in _main method invocation (#28203)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2
+ * @license Angular v7.2.2+3.sha-5877b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2');
+var VERSION = new Version('7.2.2+3.sha-5877b3f');
 
 /**
  * @license
