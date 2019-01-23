e07cd776e19c017aa13767ce46be1f14d9f9c73e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:26:46 +0000

    f72319c docs(router): removed additional to (#25989) PR Close #25989

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+3.sha-5877b3f
+ * @license Angular v7.2.2+4.sha-f72319c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+3.sha-5877b3f');
+var VERSION = new Version('7.2.2+4.sha-f72319c');
 
 /**
  * @license
