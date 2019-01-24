628e025db848eabaedfbddcc4038213f15683f72
Jason Aden <jasonaden@google.com>
2019-01-24 23:58:37 +0000

    35f7ff0 docs: add api doc for ngif (#27376) (#28344)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+12.sha-bc10328
+ * @license Angular v7.2.2+15.sha-35f7ff0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+12.sha-bc10328');
+var VERSION = new Version('7.2.2+15.sha-35f7ff0');
 
 /**
  * @license
