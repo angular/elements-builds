53570173dba484d028e9b46c4e17f34222e811be
Jason Aden <jasonaden@google.com>
2019-01-25 21:12:24 +0000

    b2811e5 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
+ * @license Angular v8.0.0-beta.1+29.sha-b2811e5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+28.sha-e2c98fb');
+var VERSION = new Version('8.0.0-beta.1+29.sha-b2811e5');
 
 /**
  * @license
