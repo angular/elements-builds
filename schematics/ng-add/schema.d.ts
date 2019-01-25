ac95db873347ccafdc61a328af010f601f658777
Jason Aden <jasonaden@google.com>
2019-01-25 22:49:30 +0000

    3d5a919 refactor(ivy): clean up TNode not depending on LView (#28354)

--- schematics/ng-add/schema.d.ts
+++ schematics/ng-add/schema.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="angular/packages/elements/schematics/ng-add/schema" />
+/// <amd-module name="@angular/elements/schematics/ng-add/schema" />
 export interface Schema {
     /**
      * Skip package.json install.
