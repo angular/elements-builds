bb9c4492bcdfb1289f0300a12539244d9780648b
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:15:44 +0000

    c64e666 test: log cli version in `cli-hello-world[-ivy]` integration projects (#28421)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+9.sha-5f9d574
+ * @license Angular v7.2.3+16.sha-c64e666
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -484,7 +484,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.3+9.sha-5f9d574');
+var VERSION = new Version('7.2.3+16.sha-c64e666');
 
 /**
  * @license
