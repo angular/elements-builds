231f206689165e1ac792f64c3e36a5c19ece037d
Misko Hevery <misko@hevery.com>
2019-06-03 16:34:37 +0000

    680d385 fix(ivy): correctly project bare ICU expressions (#30696)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+18.sha-dd8cf19.with-local-changes
+ * @license Angular v8.1.0-beta.0+20.sha-680d385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -670,7 +670,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+18.sha-dd8cf19.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+20.sha-680d385.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
