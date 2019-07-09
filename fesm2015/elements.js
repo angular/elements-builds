c9be15c1eac061e007791623076f475bb28a026e
Jason Aden <jasonaden@google.com>
2019-07-09 22:46:21 +0000

    989ebcbb6 fix(ivy): inconsistent value returned by DebugNode.context (#31442)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+17.sha-7f2330a.with-local-changes
+ * @license Angular v8.2.0-next.0+23.sha-989ebcb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -670,7 +670,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.0+17.sha-7f2330a.with-local-changes');
+const VERSION = new Version('8.2.0-next.0+23.sha-989ebcb.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
