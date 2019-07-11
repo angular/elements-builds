dcbf313c25f3fc350cd77882813634ba2caaf4f1
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:54:57 +0000

    a395cb1a5 docs: add Renderer to deprecations doc (#31419)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -670,7 +670,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+12.sha-1863254.with-local-changes');
+const VERSION = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
