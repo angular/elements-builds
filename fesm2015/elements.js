8ace0ddbd44d66c955b44bf849a0041358a64b03
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:03:16 +0000

    b591035e4 docs: add angular-in-depth to community curations (#31212)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
+ * @license Angular v8.1.0+16.sha-b591035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -670,7 +670,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
+const VERSION = new Version('8.1.0+16.sha-b591035.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
