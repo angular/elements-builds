b17e6855d6cc82287619d003635c61adeff05e8c
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:42:11 +0000

    76503e65c release: cut the v9.0.0-next.0 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+127.sha-eac993d.with-local-changes
+ * @license Angular v9.0.0-next.0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -539,7 +539,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.2+127.sha-eac993d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0.with-local-changes');
 
     /**
      * @license
