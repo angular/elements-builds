80aa72b7e4baec24ad04d6af99f3cba56a7ba053
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:35:45 +0000

    afd76a80d docs: use proper case in styleguide sentence (#32039)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+18.sha-6ba31b9.with-local-changes
+ * @license Angular v8.2.0+21.sha-afd76a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0+18.sha-6ba31b9.with-local-changes');
+const VERSION = new Version('8.2.0+21.sha-afd76a8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
