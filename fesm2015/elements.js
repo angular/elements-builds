751bd340f53d0c3b5edf71dd4b9d973d68d35c8e
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:30:46 +0000

    497d6b132 ci: re-run flaky docs examples e2e tests in `test_docs_examples[_ivy]` jobs (#32497)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+14.sha-fed6b25.with-local-changes
+ * @license Angular v9.0.0-next.5+20.sha-497d6b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -834,7 +834,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+20.sha-497d6b1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
