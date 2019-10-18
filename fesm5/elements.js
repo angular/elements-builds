e8258d59a4bb9fd8fd6fbd59767895f0636f6886
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 17:38:20 +0000

    a0d16dcfea test: use @types/node compatible with TS 3.6 (#32946)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+59.sha-117ca7c.with-local-changes
+ * @license Angular v9.0.0-next.11+62.sha-a0d16dc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -492,7 +492,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+59.sha-117ca7c.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+62.sha-a0d16dc.with-local-changes');
 
 /**
  * @license
