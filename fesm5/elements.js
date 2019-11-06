b239c85debf846d622f868dbd84c9a686f149fe5
atscott <atscott01@gmail.com>
2019-11-06 20:21:35 +0000

    2ed04ffcdc build: update to latest material commit to pick up https://github.com/angular/components/pull/17620 (#33607)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
+ * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -492,7 +492,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+69.sha-41560b4.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+77.sha-2ed04ff.with-local-changes');
 
 /**
  * @license
@@ -515,5 +515,5 @@ var VERSION = new Version('9.0.0-rc.0+69.sha-41560b4.with-local-changes');
  * Generated bundle index. Do not edit.
  */
 
-export { NgElement, createCustomElement, VERSION };
+export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.js.map
