b0d04e1e9a78a50f3f4257cc1d1a9925638f5547
Kara Erickson <karakara@google.com>
2019-12-13 20:43:05 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+461.sha-b405942
+ * @license Angular v9.0.0-rc.1+463.sha-ea57587
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+461.sha-b405942');
+var VERSION = new Version('9.0.0-rc.1+463.sha-ea57587');
 
 /**
  * @license
