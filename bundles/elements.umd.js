9213cc6977fd67c43981f513d8656f1861586431
Kara Erickson <karakara@google.com>
2019-12-16 16:52:33 +0000

    d0a04bf309 ci: fix saucelabs_view_engine master-only failing (#34429)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+481.sha-ddc229b
+ * @license Angular v9.0.0-rc.1+482.sha-d0a04bf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+481.sha-ddc229b');
+    var VERSION = new core.Version('9.0.0-rc.1+482.sha-d0a04bf');
 
     /**
      * @license
