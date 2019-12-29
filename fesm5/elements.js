f68868c4fc3d51239884f2462d937e36ca9bd4d1
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:16:21 +0000

    61c805fa1b build(docs-infra): bump handlebars from 4.1.2 to 4.5.3 in /aio

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-22ac0e9
+ * @license Angular v9.0.0-rc.1+522.sha-61c805f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -492,7 +492,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+522.sha-22ac0e9');
+var VERSION = new Version('9.0.0-rc.1+522.sha-61c805f');
 
 /**
  * @license
