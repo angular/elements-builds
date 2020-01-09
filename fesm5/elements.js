ba96f3c7d7c3bfb4db9044fc12d65f6582614c85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 00:07:01 +0000

    8815ace418 fix(ngcc): insert definitions after statement (#34677)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+557.sha-02e4921
+ * @license Angular v9.0.0-rc.1+574.sha-8815ace
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+557.sha-02e4921');
+var VERSION = new Version('9.0.0-rc.1+574.sha-8815ace');
 
 /**
  * @license
