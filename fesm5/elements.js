1889641f9f7e7a665f81ce73ceb96e535872bf8c
atscott <atscott@google.com>
2020-01-13 18:21:42 +0000

    58f10026c4 docs: add deprecation info about default currency code (#32584)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+644.sha-cfbb1a1
+ * @license Angular v9.0.0-rc.1+649.sha-58f1002
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+644.sha-cfbb1a1');
+var VERSION = new Version('9.0.0-rc.1+649.sha-58f1002');
 
 /**
  * @license
