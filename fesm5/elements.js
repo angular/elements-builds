0922400a9665db0f694157d5922248367556b457
atscott <atscott@google.com>
2020-01-13 18:30:22 +0000

    7d401853b5 fix(ivy): Prevent errors when querying DebugElement roots that were outside angular context (#34687)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+649.sha-58f1002
+ * @license Angular v9.0.0-rc.1+650.sha-7d40185
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+649.sha-58f1002');
+var VERSION = new Version('9.0.0-rc.1+650.sha-7d40185');
 
 /**
  * @license
