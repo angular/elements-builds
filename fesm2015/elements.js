6a804d4ae43e3dc895c866510d70acf48e044daf
Miško Hevery <misko@hevery.com>
2020-01-24 20:09:00 +0000

    0eaf8747e6 refactor(ivy): change styling to use programmatic API on updates (#34804)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+745.sha-ad68b61
+ * @license Angular v9.0.0-rc.1+804.sha-0eaf874
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -840,7 +840,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('0.0.0');
+const VERSION = new Version('9.0.0-rc.1+804.sha-0eaf874');
 
 /**
  * @fileoverview added by tsickle
