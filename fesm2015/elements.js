023e8a3c80c6451b15d1f7aa9276de7b261e2ad4
Kara Erickson <karakara@google.com>
2020-02-08 01:43:03 +0000

    480a4c3061 fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+36.sha-18ed9dc
+ * @license Angular v9.0.0+37.sha-480a4c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -840,7 +840,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+36.sha-18ed9dc');
+const VERSION = new Version('9.0.0+37.sha-480a4c3');
 
 /**
  * @fileoverview added by tsickle
