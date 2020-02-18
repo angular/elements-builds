c6013c83215ed3cdda467ea151f1ca09fd29d004
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:19:35 +0000

    c013dd4ca6 fix(core): better handing of ICUs outside of i18n blocks (#35347)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+34.sha-c013dd4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION = new core.Version('9.1.0-next.0+34.sha-c013dd4');
 
     /**
      * @license
