4ec2b13e4ad2650b91ea4a98172527cd306e0888
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 17:33:39 +0000

    d0d36a5ebf ci: increase AIO payload size limit (#35538)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+38.sha-5fbfe69
+ * @license Angular v9.1.0-next.0+43.sha-d0d36a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.0+38.sha-5fbfe69');
+    var VERSION = new core.Version('9.1.0-next.0+43.sha-d0d36a5');
 
     /**
      * @license
