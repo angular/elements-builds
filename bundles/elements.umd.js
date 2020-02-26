9ad9272e70166cd3b87afce9497f5d6ced5013e0
Miško Hevery <misko@hevery.com>
2020-02-26 21:20:43 +0000

    d41988437e docs(common): update ngForTrackBy error URL (#34761)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+67.sha-d419884
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -690,7 +690,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.1+66.sha-a7ed74f');
+    var VERSION = new core.Version('9.1.0-next.1+67.sha-d419884');
 
     /**
      * @license
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+67.sha-d419884
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -492,7 +492,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+66.sha-a7ed74f');
+var VERSION = new Version('9.1.0-next.1+67.sha-d419884');
 
 /**
  * @license
