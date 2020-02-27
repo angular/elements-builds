b1ab084dd5decaa93a5efa268390c2d9b311f0d7
atscott <atscott@google.com>
2020-02-27 22:28:44 +0000

    287d841486 build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3+9.sha-9b53054
+ * @license Angular v9.0.4+3.sha-287d841
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -690,7 +690,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.3+9.sha-9b53054');
+    var VERSION = new core.Version('9.0.4+3.sha-287d841');
 
     /**
      * @license
