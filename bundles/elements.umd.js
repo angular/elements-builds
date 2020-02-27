4420c9b9dde39c9b76e9e5b3f2cd94a56e07faf9
atscott <atscott@google.com>
2020-02-27 19:26:54 +0000

    bf6fbf5a74 feat(docs-infra): add useful links if landed on 404 page and no search results found (#34978)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3
+ * @license Angular v9.0.3+6.sha-bf6fbf5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -690,7 +690,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.3');
+    var VERSION = new core.Version('9.0.3+6.sha-bf6fbf5');
 
     /**
      * @license
