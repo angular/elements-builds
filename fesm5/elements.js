5e9689c95549281c9e3fa001081cc04afc8e08bd
atscott <atscott@google.com>
2020-03-03 22:23:18 +0000

    218e82ebb4 fix(core): log error instead of warning for unknown properties and elements (#35798)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+38.sha-12e52a7
+ * @license Angular v9.0.4+44.sha-218e82e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -492,7 +492,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.4+38.sha-12e52a7');
+var VERSION = new Version('9.0.4+44.sha-218e82e');
 
 /**
  * @license
