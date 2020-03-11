f6fee15b50998e8a5e750b118909a7d112ca2b94
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:39:33 +0000

    13495c64f7 docs(dev-infra): update triage and contributing docs for dev-infra (#35995)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+3.sha-6f95bc9
+ * @license Angular v9.1.0-next.4+7.sha-13495c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -839,7 +839,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+3.sha-6f95bc9');
+const VERSION = new Version('9.1.0-next.4+7.sha-13495c6');
 
 /**
  * @fileoverview added by tsickle
