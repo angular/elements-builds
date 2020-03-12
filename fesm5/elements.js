aacb85c6aa49817b8559e72351a6511426267598
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:14:56 +0000

    43635a6c54 build: update ibazel to `0.12.2` (#36033)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+37.sha-37a4839
+ * @license Angular v9.1.0-next.4+38.sha-43635a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -491,7 +491,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+37.sha-37a4839');
+var VERSION = new Version('9.1.0-next.4+38.sha-43635a6');
 
 /**
  * @license
