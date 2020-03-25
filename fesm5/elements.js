d0142295718fa9211f99288b54ddb3a75adbb915
Misko Hevery <misko@hevery.com>
2020-03-25 17:07:36 +0000

    22710fc353 docs: release notes for the v9.1.0 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+35.sha-4f66250
+ * @license Angular v9.1.0-rc.0+36.sha-22710fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -501,7 +501,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+35.sha-4f66250');
+var VERSION = new Version('9.1.0-rc.0+36.sha-22710fc');
 
 /**
  * @license
