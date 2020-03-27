0c83ca5d6f2acab8de88e353260ee452e1cb6de1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:49:15 +0000

    0baab0f3db docs: update release nots with blog post (#36268)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+2.sha-294c629
+ * @license Angular v9.1.0+14.sha-0baab0f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -848,7 +848,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+2.sha-294c629');
+const VERSION = new Version('9.1.0+14.sha-0baab0f');
 
 /**
  * @fileoverview added by tsickle
