7d64998f66010cc34aacd4344e6c8bbe3ad97519
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:55:05 +0000

    0ab244f39e docs: fix typo in Schematics guide (#36328)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+20.sha-15cffa2
+ * @license Angular v9.1.0+22.sha-0ab244f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -848,7 +848,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+20.sha-15cffa2');
+const VERSION = new Version('9.1.0+22.sha-0ab244f');
 
 /**
  * @fileoverview added by tsickle
