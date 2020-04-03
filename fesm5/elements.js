ea9ea13b3cf3c2235d7f807e9a6389f70aa9dded
Kara Erickson <karakara@google.com>
2020-04-03 19:32:58 +0000

    568e9df1d6 fix(router): allow UrlMatcher to return null (#36402)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+105.sha-ca25c95
+ * @license Angular v9.1.0-rc.0+109.sha-568e9df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -501,7 +501,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+105.sha-ca25c95');
+var VERSION = new Version('9.1.0-rc.0+109.sha-568e9df');
 
 /**
  * @license
