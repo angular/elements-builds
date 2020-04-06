195c6f34784f8dabde59e6ccca0855f436802492
Kara Erickson <karakara@google.com>
2020-04-06 18:58:23 +0000

    f9fb8338f5 fix(ngcc): support ignoring deep-imports via package config (#36423)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+114.sha-1b4df64
+ * @license Angular v9.1.0-rc.0+118.sha-f9fb833
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -500,7 +500,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+114.sha-1b4df64');
+var VERSION = new Version('9.1.0-rc.0+118.sha-f9fb833');
 
 /**
  * @license
