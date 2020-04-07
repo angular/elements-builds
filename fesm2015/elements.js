cd2c55cfa50ad9dbabb525beb4864b5c4d8715fc
Kara Erickson <karakara@google.com>
2020-04-07 17:51:11 +0000

    af4269471d fix(core): ngOnDestroy on multi providers called with incorrect context (#35840)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+89.sha-f48a065
+ * @license Angular v9.1.0+95.sha-af42694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -848,7 +848,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+89.sha-f48a065');
+const VERSION = new Version('9.1.0+95.sha-af42694');
 
 /**
  * @fileoverview added by tsickle
