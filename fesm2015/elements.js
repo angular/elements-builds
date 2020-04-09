f788c8cf96520d36deee761d29a34d7c53e3a702
atscott <atscott@google.com>
2020-04-09 19:03:55 +0000

    e06512b22f perf(ngcc): only load  if it is needed (#36486)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+19.sha-66724fd
+ * @license Angular v9.1.1+25.sha-e06512b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -848,7 +848,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+19.sha-66724fd');
+const VERSION = new Version('9.1.1+25.sha-e06512b');
 
 /**
  * @fileoverview added by tsickle
