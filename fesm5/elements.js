ef39285be6700bd06e302635cdfa947daf2d4c92
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:16:31 +0000

    28995dba19 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+51.sha-51b0924
+ * @license Angular v10.0.0-next.2+54.sha-28995db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -508,7 +508,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+51.sha-51b0924');
+var VERSION = new Version('10.0.0-next.2+54.sha-28995db');
 
 /**
  * @license
