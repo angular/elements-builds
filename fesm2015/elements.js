9a946deaa89b87913ab2bfe153ef666f8e7e7a83
Andrew Kushnir <akushnir@google.com>
2020-04-24 18:24:27 +0000

    ad8c4cdd75 revert: "feat(dev-infra): exposed new rule 'component_benchmark' via dev_infra (#36434)" (#36798)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+27.sha-76190c7
+ * @license Angular v10.0.0-next.3+30.sha-ad8c4cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -855,7 +855,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+27.sha-76190c7');
+const VERSION = new Version('10.0.0-next.3+30.sha-ad8c4cd');
 
 /**
  * @fileoverview added by tsickle
