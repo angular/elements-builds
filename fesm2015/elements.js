2c03175b0a717e3ceaa1a052c78f76939f0dceee
Andrew Kushnir <akushnir@google.com>
2020-04-27 19:31:15 +0000

    b34fb04cbf build(docs-infra): upgrade cli command docs sources to 9459c8519 (#36808)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+32.sha-2365bb8
+ * @license Angular v10.0.0-next.3+33.sha-b34fb04
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -855,7 +855,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+32.sha-2365bb8');
+const VERSION = new Version('10.0.0-next.3+33.sha-b34fb04');
 
 /**
  * @fileoverview added by tsickle
