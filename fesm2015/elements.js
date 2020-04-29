63521aafa4e1de2967e4cc24588a6c2abde39d9d
Andrew Kushnir <akushnir@google.com>
2020-04-29 18:15:08 +0000

    62930aac7b Revert "refactor(localize): move `source_file_utils.ts` up (#36834)" (#36861)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+36.sha-d743331
+ * @license Angular v9.1.3+37.sha-62930aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -855,7 +855,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+36.sha-d743331');
+const VERSION = new Version('9.1.3+37.sha-62930aa');
 
 /**
  * @fileoverview added by tsickle
