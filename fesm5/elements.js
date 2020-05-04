0267e5d6c8b8d5bf2f817ae2eec4b1e4e7665ce3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:18:24 +0000

    75370107c2 docs(dev-infra): update format commands described in DEVELOPER.md (#36910)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+17.sha-a034276
+ * @license Angular v9.1.4+26.sha-7537010
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -508,7 +508,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.4+17.sha-a034276');
+var VERSION = new Version('9.1.4+26.sha-7537010');
 
 /**
  * @license
