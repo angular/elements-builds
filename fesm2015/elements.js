35d17c1b59111d9be49bd16be45b7f1c384212e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:24:20 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -855,7 +855,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
+const VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
 
 /**
  * @fileoverview added by tsickle
