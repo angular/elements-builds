8879ec476542665d50653b22fb1b30bf74c7041c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:07:24 +0000

    0be3792b5c refactor(docs-infra): take advantage of latest Jasmine features in preview server tests (#36837)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+43.sha-9e755f3
+ * @license Angular v9.1.4+55.sha-0be3792
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -508,7 +508,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.4+43.sha-9e755f3');
+var VERSION = new Version('9.1.4+55.sha-0be3792');
 
 /**
  * @license
