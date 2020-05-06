49c71c2bfa6e4d2ac856101649a9c7b10346ac08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:28:33 +0000

    f930e75a80 ci: update components-repo-unit-tests job commit (#36921)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+55.sha-20cc3ab
+ * @license Angular v10.0.0-next.5+61.sha-f930e75
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -722,7 +722,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.5+55.sha-20cc3ab');
+    var VERSION = new core.Version('10.0.0-next.5+61.sha-f930e75');
 
     /**
      * @license
