7a60de941b91f6be458fe210e20034b7606d3087
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:29:07 +0000

    d1b3af697c release: cut the v9.1.5 release

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+67.sha-dcf1dcb
+ * @license Angular v9.1.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -508,7 +508,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.4+67.sha-dcf1dcb');
+var VERSION = new Version('9.1.5');
 
 /**
  * @license
