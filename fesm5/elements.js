f4197b5fb55c6fc23f702e7900ed507375c6db69
Kara Erickson <karakara@google.com>
2020-05-14 20:25:01 +0000

    719ca5283b fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+35.sha-3813f54
+ * @license Angular v9.1.6+36.sha-719ca52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -508,7 +508,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+35.sha-3813f54');
+var VERSION = new Version('9.1.6+36.sha-719ca52');
 
 /**
  * @license
