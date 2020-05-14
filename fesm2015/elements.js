f328e55dc1e495cf2ccb798314fe5f3a7947301b
Kara Erickson <karakara@google.com>
2020-05-14 23:08:33 +0000

    ea971f7098 fix(core): inheritance delegate ctor regex updated to work on minified code (#36962)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+51.sha-ddaa124
+ * @license Angular v10.0.0-next.7+52.sha-ea971f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -858,7 +858,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+51.sha-ddaa124');
+const VERSION = new Version('10.0.0-next.7+52.sha-ea971f7');
 
 /**
  * @fileoverview added by tsickle
