11111432e4c0919665ad0bf46a8c0bcda6495800
Kara Erickson <karakara@google.com>
2020-05-16 00:20:31 +0000

    3f9b3d7666 build: add kara to public-api approvers (#37143)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+64.sha-7f58c57
+ * @license Angular v10.0.0-next.7+66.sha-3f9b3d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -857,7 +857,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+64.sha-7f58c57');
+const VERSION = new Version('10.0.0-next.7+66.sha-3f9b3d7');
 
 /**
  * @fileoverview added by tsickle
