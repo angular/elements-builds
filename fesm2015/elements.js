0768fcce7b474b768d5e735f1f94100f98c0c7fd
Kara Erickson <karakara@google.com>
2020-05-19 22:57:58 +0000

    362fba4bde fix(dev-infra): make commit-message-filter script executable for merge (#37209)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+30.sha-211d33a
+ * @license Angular v10.0.0-next.8+31.sha-362fba4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -857,7 +857,7 @@ function createCustomElement(component, config) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+30.sha-211d33a');
+const VERSION = new Version('10.0.0-next.8+31.sha-362fba4');
 
 /**
  * @fileoverview added by tsickle
