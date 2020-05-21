615ab2eaca315327ef415bb8c0d8b622e0f27932
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:59:36 +0000

    540c29cd6b feat(dev-infra): migrate release tool to use new logging system (#37232)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+3.sha-93be1f1
+ * @license Angular v10.0.0-rc.0+14.sha-540c29c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -520,7 +520,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+3.sha-93be1f1');
+const VERSION = new Version('10.0.0-rc.0+14.sha-540c29c');
 
 /**
  * @license
