502320323762da2d4a4ec44c855266f3b2c6bb8c
Kara Erickson <karakara@google.com>
2020-05-21 17:45:01 +0000

    cfb37b8994 release: cut the v10.0.0-rc.0 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+23.sha-a1001f2
+ * @license Angular v10.0.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -781,7 +781,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.9+23.sha-a1001f2');
+    var VERSION = new core.Version('10.0.0-rc.0');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+23.sha-a1001f2
+ * @license Angular v10.0.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -520,7 +520,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.9+23.sha-a1001f2');
+const VERSION = new Version('10.0.0-rc.0');
 
 /**
  * @license
