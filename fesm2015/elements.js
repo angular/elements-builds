bb072c71841e13b9b5f6ec9d1b0b73dde9b4cc88
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:55:24 +0000

    bd7393fc68 docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+23.sha-df08230
+ * @license Angular v10.0.0-rc.0+34.sha-bd7393f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -520,7 +520,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+23.sha-df08230');
+const VERSION = new Version('10.0.0-rc.0+34.sha-bd7393f');
 
 /**
  * @license
