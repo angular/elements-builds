643d6caa5259d58825fd0da76a84bf1d30f7e67b
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 21:41:54 +0000

    3b52f7066b docs: fix typo in deprecations (#37379)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+37.sha-8bb7411
+ * @license Angular v10.0.0-rc.0+38.sha-3b52f70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -520,7 +520,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+37.sha-8bb7411');
+const VERSION = new Version('10.0.0-rc.0+38.sha-3b52f70');
 
 /**
  * @license
