a60ab43b9af3122ce100c053762096c9112f835c
atscott <atscott@google.com>
2020-06-03 19:55:54 +0000

    c7c0c1f626 refactor(forms): use a type guard to get rid of casts (#32541)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
+ * @license Angular v10.0.0-rc.2+14.sha-c7c0c1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -520,7 +520,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+12.sha-2d86dbb');
+const VERSION = new Version('10.0.0-rc.2+14.sha-c7c0c1f');
 
 /**
  * @license
