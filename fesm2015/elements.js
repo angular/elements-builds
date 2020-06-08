21a4f036eff5949c40dac0e7bb8a3379b7a9b0a1
atscott <atscott@google.com>
2020-06-08 19:00:17 +0000

    8d1d6e8f70 docs: place download section in toh to the top (#36567)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+66.sha-8bead6b
+ * @license Angular v10.0.0-rc.2+69.sha-8d1d6e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+66.sha-8bead6b');
+const VERSION = new Version('10.0.0-rc.2+69.sha-8d1d6e8');
 
 /**
  * @license
