5c8771833ac0753e9781910b15667aaed8d3f768
atscott <atscott@google.com>
2020-06-08 22:25:22 +0000

    a1616ce181 docs: add note on publishing libraries in ivy (#36556)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+70.sha-1c22dff
+ * @license Angular v10.0.0-rc.2+71.sha-a1616ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
+const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
 
 /**
  * @license
