6c460f303480c142aa749390ba03b46a21f6c71d
atscott <atscott@google.com>
2020-06-08 16:50:22 +0000

    5e64c2b1df style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+86.sha-d887ba8
+ * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+86.sha-d887ba8');
+const VERSION = new Version('10.0.0-rc.0+89.sha-5e64c2b');
 
 /**
  * @license
