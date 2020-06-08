e6e6cb94cc4046c84d9a9b0285f47d6a9dacbd10
atscott <atscott@google.com>
2020-06-08 22:02:12 +0000

    1c22dff714 docs(docs-infra): fix small typo (#37258)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+69.sha-8d1d6e8
+ * @license Angular v10.0.0-rc.2+70.sha-1c22dff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+69.sha-8d1d6e8');
+const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
 
 /**
  * @license
