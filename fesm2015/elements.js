4713b7eb57938292f18a0bca7a0c248f61b96ac5
atscott <atscott@google.com>
2020-06-08 17:46:57 +0000

    8bead6bfdd test(language-service): Remove all markers from test project (#37475)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+65.sha-52dda73
+ * @license Angular v10.0.0-rc.2+66.sha-8bead6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+65.sha-52dda73');
+const VERSION = new Version('10.0.0-rc.2+66.sha-8bead6b');
 
 /**
  * @license
