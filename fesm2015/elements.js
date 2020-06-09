3dadc32fccae8c8112b5e00be4e77914b9c6178f
atscott <atscott@google.com>
2020-06-09 00:43:46 +0000

    bf2cb6fa48 feat(language-service): TS references from template items (#37437)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+71.sha-a1616ce
+ * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -526,7 +526,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
+const VERSION = new Version('10.0.0-rc.3+3.sha-bf2cb6f');
 
 /**
  * @license
