ad999ef0321336c5e54c27fddde53d4d603f7f47
atscott <atscott@google.com>
2020-06-09 00:47:20 +0000

    6280cf95b4 fix(language-service): Improve signature selection by finding exact match (#37494)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+98.sha-946f117
+ * @license Angular v10.0.0-rc.0+100.sha-6280cf9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -525,7 +525,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+98.sha-946f117');
+const VERSION = new Version('10.0.0-rc.0+100.sha-6280cf9');
 
 /**
  * @license
