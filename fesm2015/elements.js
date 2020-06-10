17208d713de138d4751e2ff959517c1062b1713d
atscott <atscott@google.com>
2020-06-10 19:12:41 +0000

    a398d6af45 docs: remove redundant web-worker nav (#37289)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+119.sha-ae36486
+ * @license Angular v10.0.0-rc.0+121.sha-a398d6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+119.sha-ae36486');
+const VERSION = new Version('10.0.0-rc.0+121.sha-a398d6a');
 
 /**
  * @license
