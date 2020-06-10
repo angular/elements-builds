171550f1b213d992f66feed1be7c2e59884cb77c
atscott <atscott@google.com>
2020-06-10 20:21:37 +0000

    c2f4a9bf71 build: remove ngcontainer Docker (#36421)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+5.sha-231095f
+ * @license Angular v10.0.0-rc.4+6.sha-c2f4a9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.4+5.sha-231095f');
+const VERSION = new Version('10.0.0-rc.4+6.sha-c2f4a9b');
 
 /**
  * @license
