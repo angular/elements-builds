85ba630394533ebc7d77fbebf4dabbeea1a04029
Misko Hevery <misko@hevery.com>
2020-06-12 16:09:55 +0000

    a075260751 docs: document how external link icons work (#37025)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+138.sha-284123c
+ * @license Angular v10.0.0-rc.0+159.sha-a075260
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+138.sha-284123c');
+const VERSION = new Version('10.0.0-rc.0+159.sha-a075260');
 
 /**
  * @license
