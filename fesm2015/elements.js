79be8ef9c99502b570e75aeec67a80cae36698c2
Misko Hevery <misko@hevery.com>
2020-06-15 21:54:31 +0000

    2a145f2463 perf(forms): optimize internal method _anyControls in FormGroup (#32534)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+184.sha-e0dfa42
+ * @license Angular v10.0.0-rc.0+185.sha-2a145f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+184.sha-e0dfa42');
+const VERSION = new Version('10.0.0-rc.0+185.sha-2a145f2');
 
 /**
  * @license
