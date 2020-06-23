2e00091631574ba7fbae18eae3f5a013703993c5
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:15:35 +0000

    82c8b44db7 docs: Initial commit of update guide for v10 release. (#37152)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+14.sha-4c1edd5
+ * @license Angular v10.0.0-rc.6+17.sha-82c8b44
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+14.sha-4c1edd5');
+const VERSION = new Version('10.0.0-rc.6+17.sha-82c8b44');
 
 /**
  * @license
