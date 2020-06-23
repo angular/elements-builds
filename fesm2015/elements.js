f93e32cf82c8fdc4675b99cb333576e93131507a
Misko Hevery <misko@hevery.com>
2020-06-23 20:39:26 +0000

    71c0363bb5 docs: add tslib update migration docs (#37402)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+222.sha-e03adaf
+ * @license Angular v10.0.0-rc.0+223.sha-71c0363
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+222.sha-e03adaf');
+const VERSION = new Version('10.0.0-rc.0+223.sha-71c0363');
 
 /**
  * @license
