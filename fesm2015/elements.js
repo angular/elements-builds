f877ce71cfb545b13d2f98f3a3ccfe5b51485e60
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:13:26 +0000

    1c1eb3045c docs: Add support schedule for v10 (#37745)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+235.sha-666e6d1
+ * @license Angular v10.0.0-rc.0+237.sha-1c1eb30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+235.sha-666e6d1');
+const VERSION = new Version('10.0.0-rc.0+237.sha-1c1eb30');
 
 /**
  * @license
