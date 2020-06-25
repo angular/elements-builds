c76629653b621eaa766d7fc9e1e32e1641180112
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:57:58 +0000

    64f2ffa166 fix(core): cleanup DOM elements when root view is removed (#37600)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+17.sha-2cbe53a
+ * @license Angular v10.0.0+20.sha-64f2ffa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+17.sha-2cbe53a');
+const VERSION = new Version('10.0.0+20.sha-64f2ffa');
 
 /**
  * @license
