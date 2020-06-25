5c7c7bb6be8f759a25827528213bc83597005ae8
Andrew Kushnir <akushnir@google.com>
2020-06-25 23:08:02 +0000

    8572e885b4 test(compiler-cli): fix assertion of diagnostic filename on Windows (#37758)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
+ * @license Angular v10.0.0-rc.0+264.sha-8572e88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -522,7 +522,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+263.sha-0e3e9cd');
+const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
 
 /**
  * @license
