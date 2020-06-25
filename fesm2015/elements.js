208106108e1c648eefefe7321c6da2c57cb9a382
Misko Hevery <misko@hevery.com>
2020-06-25 03:16:38 +0000

    4481bebb38 docs: add v9.angular.io to the angular.io version picker (#37719)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
+ * @license Angular v10.0.0-rc.0+228.sha-4481beb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+227.sha-f5bbfd7');
+const VERSION = new Version('10.0.0-rc.0+228.sha-4481beb');
 
 /**
  * @license
