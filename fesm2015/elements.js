a485431ee65225f7491dc5ec2618e10eb4ebb51f
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:28:07 +0000

    c00f4ab2ae fix(core): don't consider inherited NG_ELEMENT_ID during DI (#37574)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+287.sha-2038568
+ * @license Angular v10.0.0-rc.0+288.sha-c00f4ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+287.sha-2038568');
+const VERSION = new Version('10.0.0-rc.0+288.sha-c00f4ab');
 
 /**
  * @license
