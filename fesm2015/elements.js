eb2791c88b7eb4667e2f25654b4c3b3f877afae1
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:35:29 +0000

    67cfc4c9bc build: add wombot proxy for publish config for @angular/benchpress (#37752)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+21.sha-a9a4ede
+ * @license Angular v10.0.0+25.sha-67cfc4c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+21.sha-a9a4ede');
+const VERSION = new Version('10.0.0+25.sha-67cfc4c');
 
 /**
  * @license
