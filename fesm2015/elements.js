2d63d557d13a1882a55fa402054af7c2472ab5fb
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:37:21 +0000

    e7023726f4 ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+29.sha-ad6680f
+ * @license Angular v10.0.0+34.sha-e702372
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+29.sha-ad6680f');
+const VERSION = new Version('10.0.0+34.sha-e702372');
 
 /**
  * @license
