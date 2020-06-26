1e29037ae98cd10af624e2b2e5b9c69735ffa5f5
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:51:57 +0000

    ad6680f602 fix(language-service): reinstate getExternalFiles() (#37750)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+25.sha-67cfc4c
+ * @license Angular v10.0.0+29.sha-ad6680f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+25.sha-67cfc4c');
+const VERSION = new Version('10.0.0+29.sha-ad6680f');
 
 /**
  * @license
