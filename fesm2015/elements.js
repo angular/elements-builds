0dcefdc433c4b909fe2beba6eb20dd4a6b33cd78
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:21:59 +0000

    dbc2364d16 fix(dev-infra): support running scripts from within a detached head (#37737)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -523,7 +523,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
 /**
  * @license
