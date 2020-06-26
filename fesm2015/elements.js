000232a54948f72b804fd7ecf09aff40a4a002c8
Igor Minar <igor@angular.io>
2020-06-26 01:41:41 +0000

    a86a41210f fix(docs-infra): fix deploy-to-firebase.sh for master and v10.0.x branches (#37762)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+4.sha-20c44a6
+ * @license Angular v9.1.11+5.sha-a86a412
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function defineInputGettersSetters(inputs, target) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.11+4.sha-20c44a6');
+const VERSION = new Version('9.1.11+5.sha-a86a412');
 
 /**
  * @fileoverview added by tsickle
