018cddb12a761801e809d8ebe358c5194708e5ee
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:43:49 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -811,7 +811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.1+10.sha-89e16ed');
+    var VERSION = new core.Version('10.0.1+16.sha-7a91b23');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+10.sha-89e16ed');
+const VERSION = new Version('10.0.1+16.sha-7a91b23');
 
 /**
  * @license
