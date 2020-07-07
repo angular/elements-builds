a72a74fe916dabae8fa1aa144b11599b1458cd0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:43:23 +0000

    289a96b078 docs: minor fixes to docs related to updating to v10 (#37897)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
+ * @license Angular v10.0.0-rc.0+325.sha-289a96b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -547,7 +547,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
+const VERSION = new Version('10.0.0-rc.0+325.sha-289a96b');
 
 /**
  * @license
