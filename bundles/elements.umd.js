7225fd91c73c0e61a29cda0e46225bddb1b718c2
atscott <atscott@google.com>
2020-07-08 23:01:37 +0000

    879b2273c1 ci: decrease the minBodyLength commit message limit to 20 chars (#37949)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3
+ * @license Angular v10.0.3+1.sha-879b227
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -811,7 +811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.3');
+    var VERSION = new core.Version('10.0.3+1.sha-879b227');
 
     /**
      * @license
