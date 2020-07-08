91c2d32a5a09aee9007a104a185b2ca9e0560546
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:24:24 +0000

    1f0c1f3ff2 release: cut the v9.1.12 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+5.sha-a86a412
+ * @license Angular v9.1.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function defineInputGettersSetters(inputs, target) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.11+5.sha-a86a412');
+const VERSION = new Version('9.1.12');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+5.sha-a86a412
+ * @license Angular v9.1.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.11+5.sha-a86a412');
+var VERSION = new Version('9.1.12');
 
 /**
  * @license
