e1f2ce21916b3d35a96b313efeba7209d917bce0
Andrew Scott <atscott01@gmail.com>
2020-07-10 22:30:32 +0000

    0d38288078 docs: Move router tutorial (toh) from router.md to new file (#37979)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+37.sha-2e9b953
+ * @license Angular v10.0.3+38.sha-0d38288
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+37.sha-2e9b953');
+const VERSION = new Version('10.0.3+38.sha-0d38288');
 
 /**
  * @license
