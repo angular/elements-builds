930565d9af243e525ffe98014b7042f1d1e9e752
Andrew Scott <atscott01@gmail.com>
2020-07-10 17:30:15 +0000

    c776825fdd fix(dev-infra): include `bazel` utility files in npm package (#37891)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+20.sha-b76a2dc
+ * @license Angular v10.0.3+33.sha-c776825
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+20.sha-b76a2dc');
+const VERSION = new Version('10.0.3+33.sha-c776825');
 
 /**
  * @license
