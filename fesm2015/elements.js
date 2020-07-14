db810edaaf9743a54897d9a53601b92c310fe7a0
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:41:17 +0000

    d690eec88f docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+52.sha-9cf78d5
+ * @license Angular v10.0.3+55.sha-d690eec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+52.sha-9cf78d5');
+const VERSION = new Version('10.0.3+55.sha-d690eec');
 
 /**
  * @license
