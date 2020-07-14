90ce090a7f5679e68ea0a552edccce30c1515679
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:41:32 +0000

    f4fac406b9 docs(core): fixes minor typo in initNgDevMode function docs (#38042)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+46.sha-421e807
+ * @license Angular v10.1.0-next.0+48.sha-f4fac40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -547,7 +547,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+46.sha-421e807');
+const VERSION = new Version('10.1.0-next.0+48.sha-f4fac40');
 
 /**
  * @license
