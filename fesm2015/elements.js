f08b638b088b4de8b5c0dfe976dc40d3b9870b4e
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:25:39 +0000

    c1539b16ae docs: add Ajit Singh to the collaborators (#37792)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+14.sha-737506e
+ * @license Angular v10.1.0-next.1+15.sha-c1539b1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+14.sha-737506e');
+const VERSION = new Version('10.1.0-next.1+15.sha-c1539b1');
 
 /**
  * @license
