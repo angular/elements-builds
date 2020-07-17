c4e91d86aef67392475be7ea6b5ccdc8b15b12e0
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-17 22:59:56 +0000

    e056ca3575 docs: correct flag default values in `--strict`

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-7b7e3d9
+ * @license Angular v10.1.0-next.1+29.sha-e056ca3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+11.sha-7b7e3d9');
+const VERSION = new Version('10.1.0-next.1+29.sha-e056ca3');
 
 /**
  * @license
