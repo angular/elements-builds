fbbc961c8d8225caee7b0670d96315296359b1d6
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:40:03 +0000

    d88711c2fc docs: add Ivy and View Engine test scripts (#38149)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+8.sha-5a73362
+ * @license Angular v10.1.0-next.2+2.sha-d88711c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+8.sha-5a73362');
+const VERSION = new Version('10.1.0-next.2+2.sha-d88711c');
 
 /**
  * @license
