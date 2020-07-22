dbd29063f932ca7af2373d8bec5657c28c39d561
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:29:07 +0000

    970d10c671 release: cut the v10.1.0-next.2 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+46.sha-8851ba9');
+const VERSION = new Version('10.1.0-next.2');
 
 /**
  * @license
