25dfc901ed523a09b72172dd8ca3548a2348f092
Misko Hevery <misko@hevery.com>
2020-07-24 15:46:38 +0000

    307db74e93 docs: fixed that class attribute is not closed (#38219)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+17.sha-d0060dc
+ * @license Angular v10.1.0-next.2+21.sha-307db74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+17.sha-d0060dc');
+const VERSION = new Version('10.1.0-next.2+21.sha-307db74');
 
 /**
  * @license
