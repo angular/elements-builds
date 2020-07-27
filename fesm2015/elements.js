1757d9b481d81c483074c4d78bfb69d5a3d49105
Misko Hevery <misko@hevery.com>
2020-07-27 22:00:28 +0000

    5d3ba8dec7 test: update ts-api-guardian's strip_export_pattern to exclude Ivy instructions (#38224)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+39.sha-96aa14d
+ * @license Angular v10.1.0-next.2+42.sha-5d3ba8d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+39.sha-96aa14d');
+const VERSION = new Version('10.1.0-next.2+42.sha-5d3ba8d');
 
 /**
  * @license
