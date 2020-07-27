034ae94aa1deae9a59f87f838c35d19314483c62
Misko Hevery <misko@hevery.com>
2020-07-27 22:47:59 +0000

    03a6252123 docs: update api reference doc for router link directive (#38181)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+43.sha-174aac6
+ * @license Angular v10.1.0-next.2+44.sha-03a6252
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -890,7 +890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.2+43.sha-174aac6');
+    var VERSION = new core.Version('10.1.0-next.2+44.sha-03a6252');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+43.sha-174aac6
+ * @license Angular v10.1.0-next.2+44.sha-03a6252
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+43.sha-174aac6');
+const VERSION = new Version('10.1.0-next.2+44.sha-03a6252');
 
 /**
  * @license
