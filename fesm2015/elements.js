2b72bb03b59953bfc791ec71aa23272bd818f230
Misko Hevery <misko@hevery.com>
2020-07-27 22:48:41 +0000

    b1e7775a8a fix(compiler-cli): Add support for string literal class members (#38226)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+32.sha-776067c
+ * @license Angular v10.0.5+38.sha-b1e7775
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+32.sha-776067c');
+const VERSION = new Version('10.0.5+38.sha-b1e7775');
 
 /**
  * @license
