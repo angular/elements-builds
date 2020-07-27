b4f5e5af3b6cb229a67b846f55b7616ad8f7e2a9
Misko Hevery <misko@hevery.com>
2020-07-27 18:33:08 +0000

    b65b6163f7 docs: fix breaking URL for RxJS marble testing (#38209)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+36.sha-f74cfad
+ * @license Angular v10.1.0-next.2+37.sha-b65b616
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+36.sha-f74cfad');
+const VERSION = new Version('10.1.0-next.2+37.sha-b65b616');
 
 /**
  * @license
