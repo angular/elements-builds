6eb8127ccdd7ab08e35e46a2e4cc7206069cdca9
Misko Hevery <misko@hevery.com>
2020-07-28 19:55:46 +0000

    2a45b932e2 build: fix broken build (#38274)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+45.sha-65cc0c8
+ * @license Angular v10.1.0-next.2+58.sha-2a45b93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -734,6 +734,8 @@
      * static property to affect all newly created instances, or as a constructor argument for
      * one-off creations.
      *
+     * @see [Angular Elements Overview](guide/elements "Turning Angular components into custom elements")
+     *
      * @param component The component to transform.
      * @param config A configuration that provides initialization information to the created class.
      * @returns The custom-element construction class, which can be registered with
@@ -890,7 +892,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.2+45.sha-65cc0c8');
+    var VERSION = new core.Version('10.1.0-next.2+58.sha-2a45b93');
 
     /**
      * @license
