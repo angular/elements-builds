57ac3022e09b3494c8cf796c10efc04c34f4f4a0
Misko Hevery <misko@hevery.com>
2020-07-28 23:03:09 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
+const VERSION = new Version('10.1.0-next.3');
 
 /**
  * @license
