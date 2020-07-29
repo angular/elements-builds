6366d0ed9263953f4680e6d29fa2f7019a5be84d
Misko Hevery <misko@hevery.com>
2020-07-29 17:57:13 +0000

    b280d54470 refactor(docs-infra): simplify/improve `CopierService` hidden textarea creation (#38244)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+13.sha-1b17722
+ * @license Angular v10.1.0-next.3+12.sha-b280d54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+13.sha-1b17722');
+const VERSION = new Version('10.1.0-next.3+12.sha-b280d54');
 
 /**
  * @license
