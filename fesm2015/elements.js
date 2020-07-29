41d8a35bc71d8a2b8ceaa9e5c6875c71a351de57
Misko Hevery <misko@hevery.com>
2020-07-29 16:54:38 +0000

    0469d9240a Revert "refactor(platform-browser): specify return type of parseEventName (#38089)"

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6');
+const VERSION = new Version('10.0.6+1.sha-0469d92');
 
 /**
  * @license
