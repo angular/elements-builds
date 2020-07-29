43fe445b950e2d4aa092eb06cf069f716f5ee95f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 21:02:12 +0000

    7f8c2225f2 fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+17.sha-8effc83
+ * @license Angular v10.1.0-next.3+16.sha-7f8c222
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+17.sha-8effc83');
+const VERSION = new Version('10.1.0-next.3+16.sha-7f8c222');
 
 /**
  * @license
