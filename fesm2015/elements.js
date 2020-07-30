387b94ebd2443d6a96e3742ba0d2cd6b6136923f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:26:11 +0000

    4a6abbdfc6 fix(compiler-cli): correct type of makeDiagnostic()

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+1.sha-0469d92
+ * @license Angular v10.0.6+19.sha-4a6abbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+1.sha-0469d92');
+const VERSION = new Version('10.0.6+19.sha-4a6abbd');
 
 /**
  * @license
