affe78693cc556d3f5fb49b956f64ad3a20ae1c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:44:27 +0000

    879ff08f0e fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+20.sha-a15d7ac
+ * @license Angular v10.0.6+21.sha-879ff08f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+20.sha-a15d7ac');
+const VERSION = new Version('10.0.6+21.sha-879ff08f');
 
 /**
  * @license
