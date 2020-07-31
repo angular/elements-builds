e10625d16c495d796d7970ac6657cc758397a2bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:59:44 +0000

    cea46786a2 fix(compiler): update unparsable character reference entity error messages (#38319)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+17.sha-9c5fc16
+ * @license Angular v10.0.7+18.sha-cea4678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+17.sha-9c5fc16');
+const VERSION = new Version('10.0.7+18.sha-cea4678');
 
 /**
  * @license
