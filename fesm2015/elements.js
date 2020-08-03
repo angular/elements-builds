3f8dc92d5287a0c17d8fb935ca23d8eb4420b15d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:15:26 +0000

    e49b053dac build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+39.sha-ff9f4de
+ * @license Angular v10.1.0-next.3+41.sha-e49b053
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
+const VERSION = new Version('10.1.0-next.3+41.sha-e49b053');
 
 /**
  * @license
