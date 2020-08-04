49d5107d5473b6df2074925ecbfc9aa9b4215541
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:47:34 +0000

    2fcabe1557 release: cut the v10.1.0-next.4 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+44.sha-192ef42
+ * @license Angular v10.1.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+44.sha-192ef42');
+const VERSION = new Version('10.1.0-next.4');
 
 /**
  * @license
