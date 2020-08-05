b8856e800f3ee6e9fe6e467f94dc777c9076136c
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:24:24 +0000

    763023472b fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+4.sha-f0766a4
+ * @license Angular v10.1.0-next.4+6.sha-7630234
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+4.sha-f0766a4');
+const VERSION = new Version('10.1.0-next.4+6.sha-7630234');
 
 /**
  * @license
