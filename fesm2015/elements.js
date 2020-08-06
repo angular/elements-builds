fbc790a57d7bec192ef75a67f119325fff5e40dc
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:23:33 +0000

    696a9b01ef docs: remove https://angular.io from internal links (#38360)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+15.sha-dca4443
+ * @license Angular v10.1.0-next.4+12.sha-696a9b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+15.sha-dca4443');
+const VERSION = new Version('10.1.0-next.4+12.sha-696a9b0');
 
 /**
  * @license
