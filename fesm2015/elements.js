637704ec09cc5db2911cdeb72660cd3ae76a3fcd
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:44:11 +0000

    26be5b4994 refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+16.sha-0551fbd
+ * @license Angular v10.1.0-next.4+19.sha-26be5b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+16.sha-0551fbd');
+const VERSION = new Version('10.1.0-next.4+19.sha-26be5b4');
 
 /**
  * @license
