ecb1ae69ce150a60d4e32d351f8e722e949ae025
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:30:24 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+9.sha-bb88c9f');
+const VERSION = new Version('10.1.0-next.4+10.sha-0c24903');
 
 /**
  * @license
