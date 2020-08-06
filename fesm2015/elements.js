08668803783ece5c29ca644f63c83300f468e78b
Andrew Kushnir <akushnir@google.com>
2020-08-06 19:04:59 +0000

    18cd1a9937 docs(service-worker): describe how asset-/data-group order affects request handling (#38364)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+12.sha-696a9b0
+ * @license Angular v10.1.0-next.4+17.sha-18cd1a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+12.sha-696a9b0');
+const VERSION = new Version('10.1.0-next.4+17.sha-18cd1a9');
 
 /**
  * @license
