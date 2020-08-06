29097617b5aa37444f17cb71093693546157923c
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:27:37 +0000

    016a41b14d fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+10.sha-58f4b3a');
+const VERSION = new Version('10.0.8+14.sha-016a41b');
 
 /**
  * @license
