a9c69aeefb24de56d8ec8a0dc9c551df5f6b5e36
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:29:19 +0000

    42a649d80f docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+23.sha-47fbfb3
+ * @license Angular v10.0.8+24.sha-42a649d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+23.sha-47fbfb3');
+const VERSION = new Version('10.0.8+24.sha-42a649d');
 
 /**
  * @license
