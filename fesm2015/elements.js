85f962b746f565489d106720b1a24f996fc13865
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:34:14 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+27.sha-ee5123f
+ * @license Angular v10.0.8+28.sha-7f11149
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+27.sha-ee5123f');
+const VERSION = new Version('10.0.8+28.sha-7f11149');
 
 /**
  * @license
