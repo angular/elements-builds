0dd3b321ae048708fca9e1743f8b3c2b42c0e04b
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:25:31 +0000

    0de93fd402 docs: update team contributors page (#38384)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+28.sha-6ff28ac
+ * @license Angular v10.1.0-next.4+27.sha-0de93fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+28.sha-6ff28ac');
+const VERSION = new Version('10.1.0-next.4+27.sha-0de93fd');
 
 /**
  * @license
