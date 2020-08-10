95264de803bdf7c9349c3c15773e2b6561991de0
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:50:06 +0000

    6da9e5851a fix(compiler-cli): preserve quotes in class member names (#38387)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+32.sha-250e299
+ * @license Angular v10.1.0-next.4+33.sha-6da9e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+32.sha-250e299');
+const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
 
 /**
  * @license
