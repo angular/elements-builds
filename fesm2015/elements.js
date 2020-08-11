20ca01f717290a96ad51dc8c4f9a81b61233f4a9
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:54:12 +0000

    a2e069fdda build: run formatting automatically on pre-commit hook (#38402)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+43.sha-e2e5f83
+ * @license Angular v10.1.0-next.4+46.sha-a2e069f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+43.sha-e2e5f83');
+const VERSION = new Version('10.1.0-next.4+46.sha-a2e069f');
 
 /**
  * @license
