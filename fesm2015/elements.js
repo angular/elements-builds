55054bac4d7b539f2d2390f706fc28c3f5a92b14
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:51:54 +0000

    02aca135bd fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+31.sha-32109dc
+ * @license Angular v10.0.8+32.sha-02aca13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+31.sha-32109dc');
+const VERSION = new Version('10.0.8+32.sha-02aca13');
 
 /**
  * @license
