e7d297c2e03c3ba78f233c3196bd25a9158ad562
Andrew Kushnir <akushnir@google.com>
2020-08-11 18:26:04 +0000

    e2e5f83869 ci: update payload size limits for Closure tests (#38411)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+36.sha-df76a20
+ * @license Angular v10.1.0-next.4+43.sha-e2e5f83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+36.sha-df76a20');
+const VERSION = new Version('10.1.0-next.4+43.sha-e2e5f83');
 
 /**
  * @license
