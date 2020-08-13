fb4094ec94250c4f91e1efe18047669a08a8ea03
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:03:51 +0000

    aa816d3887 ci: disable closure size tracking test (#38449)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+9.sha-cb83b8a
+ * @license Angular v10.0.9+11.sha-aa816d3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+9.sha-cb83b8a');
+const VERSION = new Version('10.0.9+11.sha-aa816d3');
 
 /**
  * @license
