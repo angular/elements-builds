00bb66227fdbf90aa41f77adb0526eed0f2af4a8
Andrew Scott <atscott01@gmail.com>
2020-08-13 23:29:40 +0000

    ca798804b2 fix(router): export DefaultRouteReuseStrategy to Router public_api (#31575)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+18.sha-b071495
+ * @license Angular v10.1.0-next.5+19.sha-ca79880
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+18.sha-b071495');
+const VERSION = new Version('10.1.0-next.5+19.sha-ca79880');
 
 /**
  * @license
