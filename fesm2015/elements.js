924b646b8c8220770c5857e3e2fb99817b113930
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:02:23 +0000

    71079ce47e fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+19.sha-ca79880
+ * @license Angular v10.1.0-next.5+20.sha-71079ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+19.sha-ca79880');
+const VERSION = new Version('10.1.0-next.5+20.sha-71079ce');
 
 /**
  * @license
