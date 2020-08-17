5f5543bab1df031b3432c1044c0c997fe530a61e
atscott <atscott01@gmail.com>
2020-08-17 20:41:08 +0000

    3e80f0e526 release: cut the v10.0.10 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+21.sha-f3dd6c2');
+const VERSION = new Version('10.0.10');
 
 /**
  * @license
