c735abad87ba3771998e951a86929993d55cf23d
Andrew Scott <atscott@google.com>
2020-08-17 18:54:19 +0000

    84d1ba792b refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+17.sha-b32126c
+ * @license Angular v10.0.9+18.sha-84d1ba7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+17.sha-b32126c');
+const VERSION = new Version('10.0.9+18.sha-84d1ba7');
 
 /**
  * @license
