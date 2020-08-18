f12be5ce82819b7482414f56fc61e5809ff26ae2
Andrew Scott <atscott@google.com>
2020-08-18 17:45:04 +0000

    0af95332e9 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10
+ * @license Angular v10.0.10+6.sha-0af9533
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10');
+const VERSION = new Version('10.0.10+6.sha-0af9533');
 
 /**
  * @license
