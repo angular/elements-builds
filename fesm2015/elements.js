fe2ecc58cf3bbe5807de945777671d3dccdc948b
Andrew Scott <atscott@google.com>
2020-08-19 15:49:20 +0000

    0570c240e4 docs: Fix typo in the inputs and outputs guide (#38524)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+8.sha-398118f
+ * @license Angular v10.0.10+9.sha-0570c24
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+8.sha-398118f');
+const VERSION = new Version('10.0.10+9.sha-0570c24');
 
 /**
  * @license
