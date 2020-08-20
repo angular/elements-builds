89830a39e00183afa0a27e4d8a7df443316b1288
Misko Hevery <misko@hevery.com>
2020-08-20 20:23:18 +0000

    956b25a100 docs: apply code styling in template reference variables guide (#38522)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+12.sha-22f1ac3
+ * @license Angular v10.1.0-next.7+14.sha-956b25a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+12.sha-22f1ac3');
+const VERSION = new Version('10.1.0-next.7+14.sha-956b25a');
 
 /**
  * @license
