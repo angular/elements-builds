836681ee5a0c681c9f12475f1ce37cb97659118a
Misko Hevery <misko@hevery.com>
2020-08-20 16:21:24 +0000

    c90262e619 Revert "Revert "fix(core): remove closing body tag from inert DOM builder (#38454)""

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+6.sha-87bbf69
+ * @license Angular v10.0.11+7.sha-c90262e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+6.sha-87bbf69');
+const VERSION = new Version('10.0.11+7.sha-c90262e');
 
 /**
  * @license
