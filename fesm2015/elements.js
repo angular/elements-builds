d445115af011d32467e7ed4dbf26ca58c886bed5
Misko Hevery <misko@hevery.com>
2020-08-20 17:01:15 +0000

    8017ca4db3 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+10.sha-6442875
+ * @license Angular v10.1.0-next.7+13.sha-8017ca4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+10.sha-6442875');
+const VERSION = new Version('10.1.0-next.7+13.sha-8017ca4');
 
 /**
  * @license
