87e548ed1e0ac8a21e5dab9445c985d99fb04867
Misko Hevery <misko@hevery.com>
2020-08-21 19:53:21 +0000

    874792dc43 feat(compiler): support unary operators for more accurate type checking (#37918)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+17.sha-e7da404
+ * @license Angular v10.1.0-next.7+18.sha-874792d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+17.sha-e7da404');
+const VERSION = new Version('10.1.0-next.7+18.sha-874792d');
 
 /**
  * @license
