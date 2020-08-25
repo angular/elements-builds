d6e91912f58732f5904e533dbd7866a75f36fc2a
Misko Hevery <misko@hevery.com>
2020-08-25 17:18:43 +0000

    18e474f522 fix(zone.js): zone.js toString patch should check typeof Promise is function (#38350)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+28.sha-cb3db0d
+ * @license Angular v10.1.0-next.7+29.sha-18e474f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+28.sha-cb3db0d');
+const VERSION = new Version('10.1.0-next.7+29.sha-18e474f');
 
 /**
  * @license
