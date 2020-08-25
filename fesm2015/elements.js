fb519e99a7dca983cff735081375fab416701324
Misko Hevery <misko@hevery.com>
2020-08-25 22:39:07 +0000

    f0af387f6c fix(localize): ensure required XLIFF parameters are serialized (#38575)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+12.sha-841dfa6
+ * @license Angular v10.1.0-next.8+11.sha-f0af387
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.8+12.sha-841dfa6');
+const VERSION = new Version('10.1.0-next.8+11.sha-f0af387');
 
 /**
  * @license
