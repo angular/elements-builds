56746a9eafd26257cecaeb01328b676d032f60f8
Misko Hevery <misko@hevery.com>
2020-08-26 19:05:38 +0000

    80cab26023 docs: Minor fixes in NgModules section (#36177)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+11.sha-f0af387
+ * @license Angular v10.1.0-next.8+13.sha-80cab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.8+11.sha-f0af387');
+const VERSION = new Version('10.1.0-next.8+13.sha-80cab26');
 
 /**
  * @license
