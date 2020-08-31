906786c9b0430b7bbbd9b6a1f6ad477ab51648d5
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:56:16 +0000

    7d5ea554e9 refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+22.sha-b060837
+ * @license Angular v10.0.14+23.sha-7d5ea55
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+22.sha-b060837');
+const VERSION = new Version('10.0.14+23.sha-7d5ea55');
 
 /**
  * @license
