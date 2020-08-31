14b40d130d0c7c15d3714a70e320ada073dea732
Joey Perrott <josephperrott@gmail.com>
2020-08-31 20:48:08 +0000

    0dda97ea66 fix(compiler): incorrectly inferring namespace for HTML nodes inside SVG (#38477)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+16.sha-5e4aeaa
+ * @license Angular v10.1.0-rc.0+17.sha-0dda97e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+16.sha-5e4aeaa');
+const VERSION = new Version('10.1.0-rc.0+17.sha-0dda97e');
 
 /**
  * @license
