8fe11b5073f55474c6ae97a2fc34d376b8738787
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:29 +0000

    4e8592e610 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+13.sha-cb6bf40
+ * @license Angular v10.0.14+19.sha-4e8592e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+13.sha-cb6bf40');
+const VERSION = new Version('10.0.14+19.sha-4e8592e');
 
 /**
  * @license
