6a259d45bdfae34a13778262eabd587095bcc99a
Joey Perrott <josephperrott@gmail.com>
2020-09-01 00:32:31 +0000

    626ac45482 docs: Restructure table of contents to provide a more streamlined experience (#38658)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+24.sha-a938180
+ * @license Angular v10.0.14+25.sha-626ac45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+24.sha-a938180');
+const VERSION = new Version('10.0.14+25.sha-626ac45');
 
 /**
  * @license
