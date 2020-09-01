92064b44862f1d6734cfb52e62b220658ea7314e
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:31:20 +0000

    59c234cfb4 build: add configuration for the caretaker command (#38601)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+35.sha-d9fea85
+ * @license Angular v10.1.0-rc.0+37.sha-59c234c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+35.sha-d9fea85');
+const VERSION = new Version('10.1.0-rc.0+37.sha-59c234c');
 
 /**
  * @license
