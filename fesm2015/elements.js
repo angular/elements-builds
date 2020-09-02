f33057cef0a2831b6ba657d1f7968f1b56574eb1
atscott <atscott01@gmail.com>
2020-09-02 23:01:00 +0000

    6aac499ee7 docs: Restructure table of contents to provide a more streamlined experience (#38689)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+2.sha-32f33f0
+ * @license Angular v10.1.0+3.sha-6aac499
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+2.sha-32f33f0');
+const VERSION = new Version('10.1.0+3.sha-6aac499');
 
 /**
  * @license
