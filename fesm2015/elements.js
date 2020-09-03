8a50e9831a9e28ad2d7ec61cc3e8b245ce9e3c3b
atscott <atscott01@gmail.com>
2020-09-03 17:05:37 +0000

    29c89c9297 docs(dev-infra): fix typo in comment (arguements --> arguments) (#38653)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+3.sha-6aac499
+ * @license Angular v10.1.0+6.sha-29c89c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+3.sha-6aac499');
+const VERSION = new Version('10.1.0+6.sha-29c89c9');
 
 /**
  * @license
