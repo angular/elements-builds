5dbb2243bfe3d315b36c4ae23c5bd767e058da39
atscott <atscott01@gmail.com>
2020-09-03 00:10:21 +0000

    fdea1804d6 fix(core): remove CollectionChangeRecord symbol (#38668)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+2.sha-5da1934
+ * @license Angular v11.0.0-next.0+6.sha-fdea180
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+2.sha-5da1934');
+const VERSION = new Version('11.0.0-next.0+6.sha-fdea180');
 
 /**
  * @license
