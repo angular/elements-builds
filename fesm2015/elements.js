eb993a59d7409dc63cd8f59d5bf2e075b5bbd09f
Andrew Scott <atscott@google.com>
2020-09-03 21:29:53 +0000

    9bf32c4dcb refactor(compiler-cli): make template parsing errors into diagnostics (#38576)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+7.sha-25afbcc
+ * @license Angular v10.1.0+9.sha-9bf32c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+7.sha-25afbcc');
+const VERSION = new Version('10.1.0+9.sha-9bf32c4');
 
 /**
  * @license
