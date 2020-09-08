274f6f7a47f4c085d37dc4cd37c58997c4458b69
atscott <atscott01@gmail.com>
2020-09-08 17:29:02 +0000

    3e57ca1d98 docs: fix typos in deployment guide (#38727)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+18.sha-79d8795
+ * @license Angular v11.0.0-next.0+17.sha-3e57ca1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -891,7 +891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.0+18.sha-79d8795');
+    var VERSION = new core.Version('11.0.0-next.0+17.sha-3e57ca1');
 
     /**
      * @license
