b48e9883f75c82d609f7539f3874f6a71a9d87fb
atscott <atscott01@gmail.com>
2020-09-08 19:12:37 +0000

    a1c34c6f0a fix(compiler): correct confusion between field and property names (#38685)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+20.sha-5863537
+ * @license Angular v10.1.0+26.sha-a1c34c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+20.sha-5863537');
+const VERSION = new Version('10.1.0+26.sha-a1c34c6');
 
 /**
  * @license
