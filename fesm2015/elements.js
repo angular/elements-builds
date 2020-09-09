47dbee4dc437a020cea9f55f4ef66776333e015a
atscott <atscott01@gmail.com>
2020-09-09 23:47:47 +0000

    c6ebb77cec test(language-service): [ivy] remove all markers from test (#38777)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+2.sha-f942118
+ * @license Angular v11.0.0-next.1+1.sha-c6ebb77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+2.sha-f942118');
+const VERSION = new Version('11.0.0-next.1+1.sha-c6ebb77');
 
 /**
  * @license
