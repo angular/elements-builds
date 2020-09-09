910bf9856b08802fe15068460f1eac665acba847
atscott <atscott01@gmail.com>
2020-09-09 23:51:00 +0000

    7669bd856f feat(dev-infra): Allow local user ng-dev configuration to disable commit message wizard (#38701)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1
+ * @license Angular v10.1.1+5.sha-7669bd8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1');
+const VERSION = new Version('10.1.1+5.sha-7669bd8');
 
 /**
  * @license
