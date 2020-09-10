b14500fec58fbbfda0134c9ddc0d04ad7b30712e
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:22:08 +0000

    281865bbcf refactor(router): Remove `defer` as it's not needed anymore (#38781)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+5.sha-1373a98
+ * @license Angular v11.0.0-next.1+9.sha-281865b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+5.sha-1373a98');
+const VERSION = new Version('11.0.0-next.1+9.sha-281865b');
 
 /**
  * @license
