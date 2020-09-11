55a64cd1cb5ef302d7fdb53522a9f1dc12492106
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:44:39 +0000

    b05d79d14a build(router): update symbols for routing app (#38817)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+15.sha-8096c63
+ * @license Angular v10.1.1+18.sha-b05d79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+15.sha-8096c63');
+const VERSION = new Version('10.1.1+18.sha-b05d79d');
 
 /**
  * @license
