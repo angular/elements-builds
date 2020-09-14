befc89e696682e7b87c9d27d0473640bf739e5ab
Andrew Kushnir <akushnir@google.com>
2020-09-14 16:02:49 +0000

    c62d1cb80a refactor(dev-infra): do not validate config file multiple times (#38808)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+19.sha-aa43cbf
+ * @license Angular v10.1.1+20.sha-c62d1cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+19.sha-aa43cbf');
+const VERSION = new Version('10.1.1+20.sha-c62d1cb');
 
 /**
  * @license
