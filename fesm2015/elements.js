086d25b1ab2079b75a850d8b73467f7ea1b74e00
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:05:04 +0000

    84bd1a233d feat(dev-infra): include CI status check in the caretaker check (#38779)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+26.sha-dc4f858
+ * @license Angular v10.1.1+28.sha-84bd1a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+26.sha-dc4f858');
+const VERSION = new Version('10.1.1+28.sha-84bd1a2');
 
 /**
  * @license
