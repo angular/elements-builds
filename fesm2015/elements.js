6ed039bdd7ebce6629e6544421819dd5f8127f7c
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:51:45 +0000

    171a0d0696 docs: add ngc to glossary (#36781)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+43.sha-a1c1c45
+ * @license Angular v11.0.0-next.1+44.sha-171a0d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+43.sha-a1c1c45');
+const VERSION = new Version('11.0.0-next.1+44.sha-171a0d0');
 
 /**
  * @license
