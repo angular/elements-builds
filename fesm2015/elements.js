94c32d5a29bd79acc5efa8ebe75cd9fb675ceb96
Joey Perrott <josephperrott@gmail.com>
2020-09-16 22:15:15 +0000

    85a2626620 docs: release notes for the v10.1.2 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+46.sha-d192c87
+ * @license Angular v11.0.0-next.1+47.sha-85a2626
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+46.sha-d192c87');
+const VERSION = new Version('11.0.0-next.1+47.sha-85a2626');
 
 /**
  * @license
