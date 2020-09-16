40c180189d4176befdbf89cf91fb56609ced0a1a
mgechev <mgechev@gmail.com>
2020-09-16 23:03:50 +0000

    4a21eb84a7 docs: add upcoming angular conferences

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * @license Angular v11.0.0-next.2+12.sha-4a21eb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+47.sha-956e4f2');
+const VERSION = new Version('11.0.0-next.2+12.sha-4a21eb8');
 
 /**
  * @license
