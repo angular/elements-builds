3280a474f1c18a3f287eac676acc908a52a60780
Misko Hevery <misko@hevery.com>
2020-09-18 18:46:34 +0000

    3902ec0dbe fix(http): Fix error message when we call jsonp without importing HttpClientJsonpModule (#38756)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+16.sha-6b05dc4
+ * @license Angular v10.1.2+21.sha-3902ec0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.2+16.sha-6b05dc4');
+const VERSION = new Version('10.1.2+21.sha-3902ec0');
 
 /**
  * @license
