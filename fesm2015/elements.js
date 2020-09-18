6add648b6919741bf2628caae38a392f289872bc
Misko Hevery <misko@hevery.com>
2020-09-18 15:28:46 +0000

    7fb388f929 docs: updating the text of user input page to reflect real location of the forms page (#38802)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+21.sha-d3169c5
+ * @license Angular v11.0.0-next.2+18.sha-7fb388f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+21.sha-d3169c5');
+const VERSION = new Version('11.0.0-next.2+18.sha-7fb388f');
 
 /**
  * @license
