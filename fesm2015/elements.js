944de0c46055c2018334f4d516ecacbf8dab2406
Misko Hevery <misko@hevery.com>
2020-09-19 00:17:55 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+21.sha-3902ec0
+ * @license Angular v10.1.2+24.sha-7d5b515
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.2+21.sha-3902ec0');
+const VERSION = new Version('10.1.2+24.sha-7d5b515');
 
 /**
  * @license
