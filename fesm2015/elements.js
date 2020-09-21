96873c4bb4fad30ac579ce8241a5b4dbcec83332
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:48 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
+ * @license Angular v11.0.0-next.2+37.sha-2b1b718
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+36.sha-fdd4fa0');
+const VERSION = new Version('11.0.0-next.2+37.sha-2b1b718');
 
 /**
  * @license
