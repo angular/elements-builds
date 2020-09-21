90c491524e34afad645dc00dbb7ce35e8e20c916
Misko Hevery <misko@hevery.com>
2020-09-21 19:45:13 +0000

    f3f6a42342 test(docs-infra): replace deprecated `ReflectiveInjector` with `Injector` (#38897)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+31.sha-e498ea9
+ * @license Angular v11.0.0-next.2+32.sha-f3f6a42
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+31.sha-e498ea9');
+const VERSION = new Version('11.0.0-next.2+32.sha-f3f6a42');
 
 /**
  * @license
