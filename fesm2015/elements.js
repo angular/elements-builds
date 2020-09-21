4919225aeaf9b7eff5c55c30f8934e6b2379b5d5
Misko Hevery <misko@hevery.com>
2020-09-21 23:56:21 +0000

    145ab3d7e0 docs: Update titles to getting started topics (#38887)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+48.sha-3082f73
+ * @license Angular v11.0.0-next.2+49.sha-145ab3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+48.sha-3082f73');
+const VERSION = new Version('11.0.0-next.2+49.sha-145ab3d');
 
 /**
  * @license
