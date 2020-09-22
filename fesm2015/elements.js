f3ca80b10df02d27dfdcff7d591493dfb0b2306c
Misko Hevery <misko@hevery.com>
2020-09-22 16:53:24 +0000

    a2068523fd feat(service-worker): add the option to prefer network for navigation requests (#38565)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+49.sha-145ab3d
+ * @license Angular v11.0.0-next.2+50.sha-a206852
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+49.sha-145ab3d');
+const VERSION = new Version('11.0.0-next.2+50.sha-a206852');
 
 /**
  * @license
