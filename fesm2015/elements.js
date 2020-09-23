1d9fab7b45eeeb0363e8c49be47284ec4f0ef70c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:11:33 +0000

    ffe89fb07d test(docs-infra): remove usage of blacklist in test naming (#38927)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+1.sha-15ea811
+ * @license Angular v11.0.0-next.3+2.sha-ffe89fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+1.sha-15ea811');
+const VERSION = new Version('11.0.0-next.3+2.sha-ffe89fb');
 
 /**
  * @license
