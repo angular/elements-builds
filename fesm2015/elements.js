6f992375a8f1709bc57b61abfcf81e41fa1ded84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:59 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+17.sha-4ca1c73');
+const VERSION = new Version('11.0.0-next.3+14.sha-e790c85');
 
 /**
  * @license
