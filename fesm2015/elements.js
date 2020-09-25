6dcdb94d7cb3961b4da7f30d7eeb31e499202e2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:56:32 +0000

    4ca1c736bb fix(packaging): remove polyfills needed to run tests on IE9 and IE 10 (#38931)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+19.sha-cf9176e
+ * @license Angular v11.0.0-next.3+17.sha-4ca1c73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+19.sha-cf9176e');
+const VERSION = new Version('11.0.0-next.3+17.sha-4ca1c73');
 
 /**
  * @license
