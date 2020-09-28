3d71b5e2bdb4964cbdbd57ce3ff1dcbf89fd290e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:39:28 +0000

    f96dcc5ce0 feat(dev-infra): tool for staging and publishing releases (#38656)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+29.sha-4744c22
+ * @license Angular v11.0.0-next.3+46.sha-f96dcc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+29.sha-4744c22');
+const VERSION = new Version('11.0.0-next.3+46.sha-f96dcc5');
 
 /**
  * @license
