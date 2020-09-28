34fa9eeeea3d6e2fabffebb0dff64d5c4b724f8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:59:10 +0000

    e2997ed5ee build(docs-infra): update TypeScript and other deps to align with latest CLI (#39017)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+72.sha-06525cf
+ * @license Angular v11.0.0-next.3+76.sha-e2997ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
+const VERSION = new Version('11.0.0-next.3+76.sha-e2997ed');
 
 /**
  * @license
