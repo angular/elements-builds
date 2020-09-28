f731e9f887c73824c4352273d9d1f7ce0372d5a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:05 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+63.sha-4a9f452');
+const VERSION = new Version('11.0.0-next.3+69.sha-4fd635a');
 
 /**
  * @license
