d6027ac8a721e0dde6820196556ec9576ce669df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:17 +0000

    c5e178997c build(docs-infra): update docs examples to Angular v10 (#38993)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+69.sha-4fd635a
+ * @license Angular v11.0.0-next.3+65.sha-c5e1789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+69.sha-4fd635a');
+const VERSION = new Version('11.0.0-next.3+65.sha-c5e1789');
 
 /**
  * @license
