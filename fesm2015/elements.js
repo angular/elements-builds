4ac332c9891e01c64e93eab2f37dc63b3eaf2506
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:48:40 +0000

    4a9f4520ce docs: remove usage of whitelist in cli analytics docs (#38963)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+62.sha-914d709
+ * @license Angular v11.0.0-next.3+63.sha-4a9f452
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+62.sha-914d709');
+const VERSION = new Version('11.0.0-next.3+63.sha-4a9f452');
 
 /**
  * @license
