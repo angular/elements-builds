a9cdf4543f135f1bf20bb4f63f2e1f2c95762cad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:57:16 +0000

    1e3f810f8f fix(bazel): clean up outstanding failure message for usages of --define=compile (#39046)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+89.sha-2aeaedc
+ * @license Angular v11.0.0-next.3+94.sha-1e3f810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+89.sha-2aeaedc');
+const VERSION = new Version('11.0.0-next.3+94.sha-1e3f810');
 
 /**
  * @license
