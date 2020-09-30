6a56d1b4ceb5ac422810f5812581df8937d4de60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:41:51 +0000

    1b70dc931d docs: edit template-statements doc (#38742)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+80.sha-a6971ba
+ * @license Angular v11.0.0-next.3+78.sha-1b70dc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+80.sha-a6971ba');
+const VERSION = new Version('11.0.0-next.3+78.sha-1b70dc9');
 
 /**
  * @license
