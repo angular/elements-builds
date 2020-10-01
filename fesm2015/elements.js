8f5d5431f7e3a0a67033bf7cb3da48d1011045ce
Joey Perrott <josephperrott@gmail.com>
2020-10-01 17:01:43 +0000

    ac54019315 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+23.sha-8894706
+ * @license Angular v11.0.0-next.4+22.sha-ac54019
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+23.sha-8894706');
+const VERSION = new Version('11.0.0-next.4+22.sha-ac54019');
 
 /**
  * @license
