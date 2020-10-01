2bfb58236425cd76cadda4246f4baec0b32b5c4f
Joey Perrott <josephperrott@gmail.com>
2020-10-01 19:53:44 +0000

    bb19e61848 feat(dev-infra): add a command to verify NgBot YAML config syntax (#39071)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+14.sha-50e83e2
+ * @license Angular v10.1.4+15.sha-bb19e61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+14.sha-50e83e2');
+const VERSION = new Version('10.1.4+15.sha-bb19e61');
 
 /**
  * @license
