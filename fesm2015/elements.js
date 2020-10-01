fc7105c617d41dabbebfb5e32bee3f8cd34a4210
Joey Perrott <josephperrott@gmail.com>
2020-10-01 23:00:33 +0000

    db56cf18ba build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#37727)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+28.sha-f2341e2
+ * @license Angular v11.0.0-next.4+29.sha-db56cf1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+28.sha-f2341e2');
+const VERSION = new Version('11.0.0-next.4+29.sha-db56cf1');
 
 /**
  * @license
