b6b8328d5daa951006f3d434808ed68dd8c71652
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:42:52 +0000

    89c5255b8c refactor(compiler): iteratively parse interpolations (#38977)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+35.sha-5dbf357
+ * @license Angular v11.0.0-next.4+36.sha-89c5255
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+35.sha-5dbf357');
+const VERSION = new Version('11.0.0-next.4+36.sha-89c5255');
 
 /**
  * @license
