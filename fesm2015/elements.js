9280e17fed26667da080f9ab798e4119d12c170f
Joey Perrott <josephperrott@gmail.com>
2020-10-05 21:48:17 +0000

    57f8dd2978 test(compiler): add tests for parsing of malformed property reads (#38998)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+44.sha-bc69182
+ * @license Angular v11.0.0-next.4+45.sha-57f8dd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+44.sha-bc69182');
+const VERSION = new Version('11.0.0-next.4+45.sha-57f8dd2');
 
 /**
  * @license
