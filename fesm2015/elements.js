aed935a85eda38633467d61b0b51f7257db025ff
Joey Perrott <josephperrott@gmail.com>
2020-10-05 22:15:07 +0000

    e10b3e22ac refactor(compiler): Add ngModule to directive symbol (#39099)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+45.sha-57f8dd2
+ * @license Angular v11.0.0-next.4+46.sha-e10b3e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+45.sha-57f8dd2');
+const VERSION = new Version('11.0.0-next.4+46.sha-e10b3e2');
 
 /**
  * @license
