6638340b1b2e7aaeb5e1c5d9f5e29818768e0a4e
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:52:56 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+43.sha-82d54fe
+ * @license Angular v11.0.0-next.4+44.sha-bc69182
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+43.sha-82d54fe');
+const VERSION = new Version('11.0.0-next.4+44.sha-bc69182');
 
 /**
  * @license
