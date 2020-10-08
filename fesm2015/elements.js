4bacc028aec7d019ff66d72ba0346e19dd982e91
atscott <atscott01@gmail.com>
2020-10-08 17:35:07 +0000

    049b453149 fix(core): migrate relative link resolution with single quotes (#39102)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+6.sha-dace4aa
+ * @license Angular v11.0.0-next.5+7.sha-049b453
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -566,7 +566,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+6.sha-dace4aa');
+const VERSION = new Version('11.0.0-next.5+7.sha-049b453');
 
 /**
  * @license
