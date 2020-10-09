f3f26bdbdc4382b631cdb592d35d88383e0633a9
atscott <atscott01@gmail.com>
2020-10-09 15:59:05 +0000

    52a0c6b36e fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+7.sha-8913aee');
+const VERSION = new Version('10.1.5+8.sha-52a0c6b');
 
 /**
  * @license
