72968074b4f95ace237971c2695150e0ae976209
atscott <atscott01@gmail.com>
2020-10-09 15:22:15 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+6.sha-7628c36');
+const VERSION = new Version('10.1.5+7.sha-8913aee');
 
 /**
  * @license
