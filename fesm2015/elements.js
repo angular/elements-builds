b88006f0a8f39f3380159bb9e308efa96cb7019b
atscott <atscott01@gmail.com>
2020-10-12 20:03:43 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+17.sha-1efc75c
+ * @license Angular v10.1.5+18.sha-3487d5a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+17.sha-1efc75c');
+const VERSION = new Version('10.1.5+18.sha-3487d5a');
 
 /**
  * @license
