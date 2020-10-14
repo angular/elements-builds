17802bd99ce06691921899c6c918f8774a9d2152
atscott <atscott01@gmail.com>
2020-10-14 16:58:15 +0000

    5221df8e92 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+20.sha-23fc2b4
+ * @license Angular v10.1.5+22.sha-5221df8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+20.sha-23fc2b4');
+const VERSION = new Version('10.1.5+22.sha-5221df8');
 
 /**
  * @license
