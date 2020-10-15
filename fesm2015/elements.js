696efa9ba9e446c2c5cc838e7488141142c02e62
Andrew Kushnir <akushnir@google.com>
2020-10-15 23:32:07 +0000

    af170384b4 docs: fix typo at console.log (#39252)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+7.sha-72cdd42
+ * @license Angular v10.1.6+14.sha-af17038
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+7.sha-72cdd42');
+const VERSION = new Version('10.1.6+14.sha-af17038');
 
 /**
  * @license
