2f2d6ebe3953eb52000d3e6792d2b1b885edab3b
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:52:25 +0000

    85be258845 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+11.sha-497af77
+ * @license Angular v11.0.0-next.6+15.sha-85be258
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+11.sha-497af77');
+const VERSION = new Version('11.0.0-next.6+15.sha-85be258');
 
 /**
  * @license
