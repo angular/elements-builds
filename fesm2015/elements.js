f3e08449b63e704b94699a18a7925f2136a39610
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:10:32 +0000

    8fd59dabba ci: separate the windows CI tests into build and test (#39289)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+20.sha-d8e313b
+ * @license Angular v10.1.6+21.sha-8fd59da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+20.sha-d8e313b');
+const VERSION = new Version('10.1.6+21.sha-8fd59da');
 
 /**
  * @license
