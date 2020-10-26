2cc555c9c664e87c55e4502068eb262dd022356d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:14:42 +0000

    308b93058d docs: fix typo in component overview (#39425)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+1.sha-513ed49
+ * @license Angular v10.2.0+17.sha-308b930
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.0+1.sha-513ed49');
+const VERSION = new Version('10.2.0+17.sha-308b930');
 
 /**
  * @license
