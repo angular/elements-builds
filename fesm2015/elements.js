3e4714fc30d7de17aba12f4bc167af4449e562d4
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:07:32 +0000

    dd0ba3f4ab docs: rename `ng xi18n` to `ng extract-i18n` (#39337)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+149.sha-e3d2f46
+ * @license Angular v11.0.0-next.6+150.sha-dd0ba3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -550,7 +550,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+149.sha-e3d2f46');
+const VERSION = new Version('11.0.0-next.6+150.sha-dd0ba3f');
 
 /**
  * @license
