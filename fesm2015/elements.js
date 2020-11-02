2ae51f85a2174332d7bc48ae8c8e9cb2e2d95f09
Joey Perrott <josephperrott@gmail.com>
2020-11-02 16:20:15 +0000

    e9ff09ac48 build(docs-infra): upgrade `firebase-tools` to v8.14.1 (#39478)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12
+ * @license Angular v9.1.12+8.sha-e9ff09a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function defineInputGettersSetters(inputs, target) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.12');
+const VERSION = new Version('9.1.12+8.sha-e9ff09a');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12
+ * @license Angular v9.1.12+8.sha-e9ff09a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12');
+var VERSION = new Version('9.1.12+8.sha-e9ff09a');
 
 /**
  * @license
