ca3dfee5e257253a46d98b1432dfd8b295a9db51
Misko Hevery <misko@hevery.com>
2020-11-09 16:02:37 +0000

    ae71b76089 ci: log commands output when deploying angular.io to Firebase (#39596)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+8.sha-861e4fa
+ * @license Angular v10.2.2+10.sha-ae71b76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.2+8.sha-861e4fa');
+const VERSION = new Version('10.2.2+10.sha-ae71b76');
 
 /**
  * @license
