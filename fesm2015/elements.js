001f730dc457151b055fe297521ce490a6610fa0
atscott <atscott01@gmail.com>
2020-11-13 01:03:06 +0000

    06de803963 ci: fix payload size (#39666)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+7.sha-20db90a
+ * @license Angular v11.0.0+14.sha-06de803
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+7.sha-20db90a');
+const VERSION = new Version('11.0.0+14.sha-06de803');
 
 /**
  * @license
