2ca6cb6adf603d8e4324c1bb1abfda757bd05b20
atscott <atscott01@gmail.com>
2020-11-18 20:34:08 +0000

    26533a0954 docs: fixes on paragraph (#39664)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1');
+const VERSION = new Version('11.0.1+3.sha-26533a0');
 
 /**
  * @license
