604a156301b72ca1fee7e603048a710dc5ff2737
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:46:25 +0000

    be998e830b refactor(core): move `injectAttributeImpl` to avoid cycles (#37085)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+31.sha-a965589
+ * @license Angular v11.1.0-next.0+33.sha-be998e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+31.sha-a965589');
+const VERSION = new Version('11.1.0-next.0+33.sha-be998e8');
 
 /**
  * @license
