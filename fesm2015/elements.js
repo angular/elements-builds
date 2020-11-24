0977659510cfd8bfca81ed40c358afcfdb22560d
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:11:18 +0000

    8634611e37 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+43.sha-7bf73d7
+ * @license Angular v11.0.2+48.sha-8634611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+43.sha-7bf73d7');
+const VERSION = new Version('11.0.2+48.sha-8634611');
 
 /**
  * @license
