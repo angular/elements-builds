ed64eec0f971740bfb99d13f424f25c0e359bb88
Andrew Kushnir <akushnir@google.com>
2020-11-24 23:43:09 +0000

    9dc0a4edf6 test(docs-infra): disable the Selenium Promise Manager in docs examples e2e tests (#39818)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+51.sha-75b5369
+ * @license Angular v11.0.2+56.sha-9dc0a4e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+51.sha-75b5369');
+const VERSION = new Version('11.0.2+56.sha-9dc0a4e');
 
 /**
  * @license
