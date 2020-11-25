565fb5796dc37aa9705f5d9dcc2a4539408c4217
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:09:04 +0000

    68d4a74411 fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+103.sha-e148382
+ * @license Angular v11.1.0-next.0+104.sha-68d4a74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+103.sha-e148382');
+const VERSION = new Version('11.1.0-next.0+104.sha-68d4a74');
 
 /**
  * @license
