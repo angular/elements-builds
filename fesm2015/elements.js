69f9e0c2673f1ef13ff4119179968ebfe708de3e
Misko Hevery <misko@hevery.com>
2020-12-03 00:58:51 +0000

    3680ad147b fix(core): remove application from the testability registry when the root view is removed (#39876)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+5.sha-57642e8
+ * @license Angular v11.0.3+17.sha-3680ad1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+5.sha-57642e8');
+const VERSION = new Version('11.0.3+17.sha-3680ad1');
 
 /**
  * @license
