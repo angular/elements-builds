0a6bc53b5c6a007ac7fc5a572beb3f3acb437d79
Misko Hevery <misko@hevery.com>
2020-12-04 23:32:45 +0000

    1e3534f74f perf(core): use `ngDevMode` to tree-shake warnings (#39959)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+24.sha-4aeb65d
+ * @license Angular v11.0.3+26.sha-1e3534f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+24.sha-4aeb65d');
+const VERSION = new Version('11.0.3+26.sha-1e3534f');
 
 /**
  * @license
