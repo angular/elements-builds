e8580aead2768315f4dcd452e4f3d5bdc2de6699
Misko Hevery <misko@hevery.com>
2020-12-04 23:35:30 +0000

    d2042a0da2 refactor(compiler-cli): use semver range checking for partial versions (#39847)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+32.sha-c7c5b2f
+ * @license Angular v11.1.0-next.1+45.sha-d2042a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+32.sha-c7c5b2f');
+const VERSION = new Version('11.1.0-next.1+45.sha-d2042a0');
 
 /**
  * @license
