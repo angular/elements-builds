b683d6da9ff5d445867d1f8e59817fc666e92eb0
Misko Hevery <misko@hevery.com>
2020-12-07 21:45:10 +0000

    6bb6dfd616 test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+39.sha-6d62971
+ * @license Angular v11.0.3+41.sha-6bb6dfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+39.sha-6d62971');
+const VERSION = new Version('11.0.3+41.sha-6bb6dfd');
 
 /**
  * @license
