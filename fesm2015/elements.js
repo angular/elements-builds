e00b65d13aa7972f1eb399af5595b00b567199ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:02:09 +0000

    47d9b6d72d fix(core): fix possible XSS attack in development through SSR. (#40136)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+54.sha-2e7727c
+ * @license Angular v11.1.0-next.2+55.sha-47d9b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+54.sha-2e7727c');
+const VERSION = new Version('11.1.0-next.2+55.sha-47d9b6d');
 
 /**
  * @license
