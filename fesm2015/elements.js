ea6c4508d94ac7d53bfc7cd76d8d869152caafde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 22:56:34 +0000

    9ae216ced5 build: upgrade typescript (dev dep) in zone.js to 3.8.3 (#40159)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+10.sha-5b38050
+ * @license Angular v9.1.12+13.sha-9ae216c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function defineInputGettersSetters(inputs, target) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.12+10.sha-5b38050');
+const VERSION = new Version('9.1.12+13.sha-9ae216c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+10.sha-5b38050
+ * @license Angular v9.1.12+13.sha-9ae216c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+10.sha-5b38050');
+var VERSION = new Version('9.1.12+13.sha-9ae216c');
 
 /**
  * @license
