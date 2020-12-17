1ddb13adfb022bcd4f29a3cc3b9bd39bf128aea9
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:49:51 +0000

    31448197e4 release: cut the v9.1.13 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function defineInputGettersSetters(inputs, target) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.12+13.sha-9ae216c');
+const VERSION = new Version('9.1.13');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/elements.js
+++ fesm5/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ function defineInputGettersSetters(inputs, target) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+13.sha-9ae216c');
+var VERSION = new Version('9.1.13');
 
 /**
  * @license
