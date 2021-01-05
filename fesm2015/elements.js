7d4c72512da135af1cada80bdf5a26481387ebfe
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:28:30 +0000

    6abc13330b fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+51.sha-6057753
+ * @license Angular v11.1.0-next.3+55.sha-6abc133
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+51.sha-6057753');
+const VERSION = new Version('11.1.0-next.3+55.sha-6abc133');
 
 /**
  * @license
