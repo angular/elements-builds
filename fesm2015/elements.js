b66e750f3eac7f0b67731677b77729457bc5426a
GitHub <noreply@github.com>
2021-01-06 21:40:02 +0000

    5b9c981b6d release: cut the v11.0.6 release (#40328)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+55.sha-b6a9717
+ * @license Angular v11.0.5+56.sha-5b9c981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+55.sha-b6a9717');
+const VERSION = new Version('11.0.5+56.sha-5b9c981');
 
 /**
  * @license
