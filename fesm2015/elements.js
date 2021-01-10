245240d74c2d0f9de1fda83008a4a09bdda21a54
Andrew Scott <atscott@google.com>
2021-01-10 01:02:12 +0000

    c44dd84a71 fix(router): Remove usage of `Object.values` to avoid the need for a polyfill (#40370)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.7+11.sha-6ccdb2d
+ * @license Angular v11.0.7+13.sha-c44dd84
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.7+11.sha-6ccdb2d');
+const VERSION = new Version('11.0.7+13.sha-c44dd84');
 
 /**
  * @license
