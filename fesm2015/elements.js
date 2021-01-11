214b0b973a5384a5c41bf556696484016608f0a0
atscott <atscott01@gmail.com>
2021-01-11 22:38:50 +0000

    f8495ca1c4 docs(router): clarify the description of legacy relativeLinkResolution (#40384)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+19.sha-adf42da
+ * @license Angular v11.0.8+20.sha-f8495ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+19.sha-adf42da');
+const VERSION = new Version('11.0.8+20.sha-f8495ca');
 
 /**
  * @license
