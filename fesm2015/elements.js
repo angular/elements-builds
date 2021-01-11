a86bdcafaceb723ac8112841703f494f13832f0f
atscott <atscott01@gmail.com>
2021-01-11 19:35:33 +0000

    adf42daed6 fix(router): better ngZone checking for warning (#25839)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+6.sha-6438a29');
+const VERSION = new Version('11.0.8+19.sha-adf42da');
 
 /**
  * @license
