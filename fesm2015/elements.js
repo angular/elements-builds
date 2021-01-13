c6687048896a2697ba2dd2bd25f27eb65d72f714
atscott <atscott01@gmail.com>
2021-01-13 22:35:07 +0000

    3f6279d394 ci: add mgechev to docs-marketing approvers (#40396)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+31.sha-c6ca7b4
+ * @license Angular v11.0.9+7.sha-3f6279d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+31.sha-c6ca7b4');
+const VERSION = new Version('11.0.9+7.sha-3f6279d');
 
 /**
  * @license
