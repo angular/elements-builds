6a2fc6d197a1fe1f17203f5b0b2e3bf208efa1d9
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:57:17 +0000

    10994ae68c docs(core): change URL to shadow dom spec (#39797)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+97.sha-b3b8509
+ * @license Angular v11.1.0-next.4+98.sha-10994ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+97.sha-b3b8509');
+const VERSION = new Version('11.1.0-next.4+98.sha-10994ae');
 
 /**
  * @license
