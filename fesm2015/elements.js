ff1a040b1bf23a5af5810b5cd514e2047ea9e22d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 01:21:29 +0000

    e995d851cf test(core): verify that token IDs that exceed the bloom filter size are handled correctly (#40489)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+9.sha-a8a27ef
+ * @license Angular v11.1.0+11.sha-e995d85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+9.sha-a8a27ef');
+const VERSION = new Version('11.1.0+11.sha-e995d85');
 
 /**
  * @license
