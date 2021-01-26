db78a04627704551b676ccdf25a8d4898d811a4e
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:56:56 +0000

    97ec6e4849 fix(core): fix possible XSS attack in development through SSR (#40525)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+43.sha-a87e900
+ * @license Angular v11.1.0+45.sha-97ec6e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+43.sha-a87e900');
+const VERSION = new Version('11.1.0+45.sha-97ec6e4');
 
 /**
  * @license
