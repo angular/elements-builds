e1d430013817c85ddb86d878d9dcdc35241a925c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:32:58 +0000

    95e68c4f27 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+48.sha-67c4ab6
+ * @license Angular v11.1.0+49.sha-95e68c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+48.sha-67c4ab6');
+const VERSION = new Version('11.1.0+49.sha-95e68c4');
 
 /**
  * @license
