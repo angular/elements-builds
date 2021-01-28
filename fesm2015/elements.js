d0f8bccf04881ad17681457d6f8adfc256d9bfaa
Misko Hevery <misko@hevery.com>
2021-01-28 23:00:41 +0000

    e706f46a16 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+1.sha-de12485
+ * @license Angular v11.1.1+10.sha-e706f46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.1+1.sha-de12485');
+const VERSION = new Version('11.1.1+10.sha-e706f46');
 
 /**
  * @license
