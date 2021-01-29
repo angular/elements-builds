c23ada02b8dce6bc01363a8424c5bc030e5cb96c
Misko Hevery <misko@hevery.com>
2021-01-29 00:23:58 +0000

    cafd4f58c9 fix(common): parse `YYYY-MM` strings as UTC dates (#40620)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+10.sha-e706f46
+ * @license Angular v11.1.1+14.sha-cafd4f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.1+10.sha-e706f46');
+const VERSION = new Version('11.1.1+14.sha-cafd4f5');
 
 /**
  * @license
