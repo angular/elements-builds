1ac5b6a83f163c74848659c5c8277ae2df2f13b2
Misko Hevery <misko@hevery.com>
2021-02-03 18:17:36 +0000

    7c38a4a037 docs: clarify that fileReplacements only supports compilation source files (#40687)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+32.sha-4cc15fe
+ * @license Angular v11.2.0-next.0+37.sha-7c38a4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0-next.0+32.sha-4cc15fe');
+const VERSION = new Version('11.2.0-next.0+37.sha-7c38a4a');
 
 /**
  * @license
