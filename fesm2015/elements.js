94e122ab42a51ae9a0d835689ea5b63e572c18f0
Misko Hevery <misko@hevery.com>
2021-02-03 23:27:25 +0000

    9517ae3f23 release: cut the v11.1.2 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+37.sha-0ee9ea8
+ * @license Angular v11.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.1+37.sha-0ee9ea8');
+const VERSION = new Version('11.1.2');
 
 /**
  * @license
