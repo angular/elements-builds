e0b6bbcad6b7d05d3171d8ceef908ec0cff49124
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:19:08 +0000

    9482a4df4c docs: copy paste error (#40812)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+16.sha-4c6d557
+ * @license Angular v11.2.0+18.sha-9482a4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+16.sha-4c6d557');
+const VERSION = new Version('11.2.0+18.sha-9482a4d');
 
 /**
  * @license
