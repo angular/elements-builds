6600c7238768d1cc1c1c887cb2daff4d4f761d12
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:20:21 +0000

    8225bb5b57 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+18.sha-9482a4d
+ * @license Angular v11.2.0+20.sha-8225bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+18.sha-9482a4d');
+const VERSION = new Version('11.2.0+20.sha-8225bb5');
 
 /**
  * @license
