45b7f5612dc642101a3bf0d4b4933270f215aa0b
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:50:14 +0000

    ebbcf0cc04 docs: Update start-deployment.md (#40817)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+20.sha-8225bb5
+ * @license Angular v11.2.0+21.sha-ebbcf0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+20.sha-8225bb5');
+const VERSION = new Version('11.2.0+21.sha-ebbcf0c');
 
 /**
  * @license
