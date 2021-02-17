a5f903c67609bf1d46476cee946eca0955d0648a
atscott <atscott01@gmail.com>
2021-02-17 20:27:24 +0000

    b7a2d0da20 perf(core): use `ngDevMode` to tree-shake warning (#40876)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+3.sha-cdf1ea1
+ * @license Angular v12.0.0-next.1+2.sha-b7a2d0d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+3.sha-cdf1ea1');
+const VERSION = new Version('12.0.0-next.1+2.sha-b7a2d0d');
 
 /**
  * @license
