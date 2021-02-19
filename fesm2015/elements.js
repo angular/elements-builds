05bdab3c35d45cc08ddd13802ffbf06ff62faacc
Andrew Scott <atscott@google.com>
2021-02-19 16:52:47 +0000

    4e84b2f3ac docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+9.sha-170692c
+ * @license Angular v11.2.1+10.sha-4e84b2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+9.sha-170692c');
+const VERSION = new Version('11.2.1+10.sha-4e84b2f');
 
 /**
  * @license
