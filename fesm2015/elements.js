a68dca9bab7c18f86be54aa8ad08a6b931266990
Andrew Scott <atscott@google.com>
2021-02-22 20:56:39 +0000

    b84f719747 docs: update year (#40925)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v12.0.0-next.1+41.sha-dc9fd1a
- * (c) 2010-2020 Google LLC. https://angular.io/
+ * @license Angular v12.0.0-next.1+42.sha-b84f719
+ * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+41.sha-dc9fd1a');
+const VERSION = new Version('12.0.0-next.1+42.sha-b84f719');
 
 /**
  * @license
