ade9c284ae2f1e7ac1ff71201775665d049fa05b
atscott <atscott01@gmail.com>
2021-02-22 18:23:51 +0000

    dc9fd1aaef fix(core): NgZone coaleascing options should trigger onStable correctly (#40540)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+40.sha-d28197d
+ * @license Angular v12.0.0-next.1+41.sha-dc9fd1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+40.sha-d28197d');
+const VERSION = new Version('12.0.0-next.1+41.sha-dc9fd1a');
 
 /**
  * @license
