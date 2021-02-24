98bbfa4f3e2b5e77fbc426c1f3943fdd6eb9638b
atscott <atscott01@gmail.com>
2021-02-24 17:13:21 +0000

    3fe759eb6c test(language-service): remove circular dependency (#40966)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+15.sha-3551434
+ * @license Angular v11.2.2+25.sha-3fe759e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+15.sha-3551434');
+const VERSION = new Version('11.2.2+25.sha-3fe759e');
 
 /**
  * @license
