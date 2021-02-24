8f139c3f2b9f02047707342276f78cf49ba214a8
Zach Arend <zachzach@google.com>
2021-02-24 22:44:51 +0000

    ca271b84fc test(localize): ensure tests pass on Windows (#40952)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+26.sha-d7133a8
+ * @license Angular v11.2.3+3.sha-ca271b8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+26.sha-d7133a8');
+const VERSION = new Version('11.2.3+3.sha-ca271b8');
 
 /**
  * @license
