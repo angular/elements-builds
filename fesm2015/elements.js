8a4bc37ec3fba6211bd62d91f5d94fff5c8b8403
Zach Arend <zachzach@google.com>
2021-03-03 23:54:26 +0000

    4f4454d9ea docs: add ng-mocks to tooling resources (#41027)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4
+ * @license Angular v11.2.4+3.sha-4f4454d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4');
+const VERSION = new Version('11.2.4+3.sha-4f4454d');
 
 /**
  * @license
