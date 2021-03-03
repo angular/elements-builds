d2687e4185b7b0712322a210b132a641428e533e
Zach Arend <zachzach@google.com>
2021-03-03 18:23:28 +0000

    e4774da6c3 docs: update Angular Global Summit on event pages (#41050)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+38.sha-78bf697
+ * @license Angular v11.2.3+46.sha-e4774da
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+38.sha-78bf697');
+const VERSION = new Version('11.2.3+46.sha-e4774da');
 
 /**
  * @license
