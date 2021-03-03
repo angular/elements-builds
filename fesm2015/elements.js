1b1ef54187f447fb9534fde98575b5a70cd6dde7
Zach Arend <zachzach@google.com>
2021-03-03 23:12:45 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+49.sha-9322e6a');
+const VERSION = new Version('11.2.4');
 
 /**
  * @license
