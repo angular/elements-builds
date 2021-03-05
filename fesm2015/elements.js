ebada4a47ee2ba1459074034b39665d9924781af
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:05:14 +0000

    2de7eef92e docs: fix issue #40941 (#40942)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+12.sha-a05eb13
+ * @license Angular v11.2.4+14.sha-2de7eef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+12.sha-a05eb13');
+const VERSION = new Version('11.2.4+14.sha-2de7eef');
 
 /**
  * @license
