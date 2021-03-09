666a3cbf42d57bc611740b9fc35bdb45bbd94d6f
Andrew Kushnir <akushnir@google.com>
2021-03-09 21:43:57 +0000

    01c1677151 build: update the lock closed sha to the latest sha (#41140)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+52.sha-c4ea0ab
+ * @license Angular v11.2.4+53.sha-01c1677
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+52.sha-c4ea0ab');
+const VERSION = new Version('11.2.4+53.sha-01c1677');
 
 /**
  * @license
