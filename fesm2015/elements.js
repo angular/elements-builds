3f21d7ff04edc12e92c2fff82af66e57dab6ad5a
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:50:25 +0000

    c4ea0ab4e0 fix(docs-infra): print info to help debugging SW cache issue (#41106) (#41139)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+51.sha-16f90ca
+ * @license Angular v11.2.4+52.sha-c4ea0ab
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+51.sha-16f90ca');
+const VERSION = new Version('11.2.4+52.sha-c4ea0ab');
 
 /**
  * @license
