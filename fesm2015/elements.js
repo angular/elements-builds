bbfc311a2a9aa8e3706f7d7609485d3d62ec31c3
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:49:02 +0000

    ed6c09ae45 fix(localize): trim extracted `equiv-text` values (#41180)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+6.sha-78e6713
+ * @license Angular v11.2.5+7.sha-ed6c09a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+6.sha-78e6713');
+const VERSION = new Version('11.2.5+7.sha-ed6c09a');
 
 /**
  * @license
