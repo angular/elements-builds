a30f3470d18323ca4da36cb7a9f55a51ffa8a160
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:52:33 +0000

    78e67130f7 docs: add stepan suvorov to GDE resources (#41178)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+4.sha-2fa231a
+ * @license Angular v11.2.5+6.sha-78e6713
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+4.sha-2fa231a');
+    var VERSION = new core.Version('11.2.5+6.sha-78e6713');
 
     /**
      * @license
