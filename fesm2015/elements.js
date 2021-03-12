c707092a1932a63be1d0a6e14090b352e6ca08d9
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 21:47:56 +0000

    b37296ae64 ci: remove `CHANGELOG.md` from the `public-api` group (#41135)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+9.sha-74aba13
+ * @license Angular v11.2.5+16.sha-b37296a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+9.sha-74aba13');
+const VERSION = new Version('11.2.5+16.sha-b37296a');
 
 /**
  * @license
