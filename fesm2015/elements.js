1aa7ff2cf68c7a2c784bb84573c21a0d74de3d67
Misko Hevery <misko@hevery.com>
2021-03-18 17:39:12 +0000

    e23994191c docs: make `HeroService#gerHero()` in `toh-pt5` compatible with TS strict mode (#41234)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+7.sha-ac9a250
+ * @license Angular v12.0.0-next.5+5.sha-e239941
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+7.sha-ac9a250');
+const VERSION = new Version('12.0.0-next.5+5.sha-e239941');
 
 /**
  * @license
