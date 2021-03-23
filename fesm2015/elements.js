c1b6d3097dd3d7d7ddbf4f838afcf84d2c8f192e
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:12:17 +0000

    18cd7a0c69 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+17.sha-17c5dc7
+ * @license Angular v11.2.6+22.sha-18cd7a0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+17.sha-17c5dc7');
+const VERSION = new Version('11.2.6+22.sha-18cd7a0');
 
 /**
  * @license
