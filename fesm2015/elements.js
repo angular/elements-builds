65a1686ebe91779552e9e0727c46b07eaee5a2a4
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:28:02 +0000

    a68f6b0ca6 docs: format browser-support tables for GitHub markdown (#41327)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+24.sha-9cb1175
+ * @license Angular v11.2.6+25.sha-a68f6b0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+24.sha-9cb1175');
+const VERSION = new Version('11.2.6+25.sha-a68f6b0');
 
 /**
  * @license
