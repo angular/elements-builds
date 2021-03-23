298cdc6f67f4a19e1530e7e7e61acee68f817828
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:41:56 +0000

    9cb11758c1 docs: add link to fork repo how-to (#41266)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+22.sha-18cd7a0
+ * @license Angular v11.2.6+24.sha-9cb1175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+22.sha-18cd7a0');
+const VERSION = new Version('11.2.6+24.sha-9cb1175');
 
 /**
  * @license
