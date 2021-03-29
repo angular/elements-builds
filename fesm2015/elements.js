f293fd7bad9d1431b476a29a0afbc1998589f2ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-29 22:33:50 +0000

    81a88c009c fix(dev-infra): use all non-header content for checking commit body length (#41367)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+6.sha-95ff5ec
+ * @license Angular v12.0.0-next.6+7.sha-81a88c0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+6.sha-95ff5ec');
+const VERSION = new Version('12.0.0-next.6+7.sha-81a88c0');
 
 /**
  * @license
