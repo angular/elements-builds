e3821eb4cc99faa9dd2d6c50b5c06879996f50ab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:11:37 +0000

    2d3cd2b969 refactor(compiler): rename `R3FactoryTarget` to `FactoryTarget` (#41231)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+10.sha-d35751d
+ * @license Angular v12.0.0-next.6+18.sha-2d3cd2b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+10.sha-d35751d');
+const VERSION = new Version('12.0.0-next.6+18.sha-2d3cd2b');
 
 /**
  * @license
