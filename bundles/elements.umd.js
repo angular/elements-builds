e5ff8117e386d6f6e7832837b586cb84adcc3f42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:19:26 +0000

    c4628f29be refactor(common): remove `XhrFactory` re-export from `@angular/common/http` (#41370)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+21.sha-31370f4
+ * @license Angular v12.0.0-next.6+22.sha-c4628f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -894,7 +894,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+21.sha-31370f4');
+    var VERSION = new core.Version('12.0.0-next.6+22.sha-c4628f2');
 
     /**
      * @license
