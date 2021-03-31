9e77b70fa79b3a9a358f67e5464364b35ecde32d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:06:06 +0000

    d35751d442 docs(docs-infra): update docs on configuring project for Travis CI (#37473)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+9.sha-da22d49
+ * @license Angular v12.0.0-next.6+10.sha-d35751d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -894,7 +894,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+9.sha-da22d49');
+    var VERSION = new core.Version('12.0.0-next.6+10.sha-d35751d');
 
     /**
      * @license
