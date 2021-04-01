1a07a7cb9351c000e8255b56bf613d6a97eef342
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:44:45 +0000

    18bc9ffb51 ci: do not try to publish directories that are not packages (#41393)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+34.sha-6391a4a
+ * @license Angular v12.0.0-next.6+36.sha-18bc9ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+34.sha-6391a4a');
+const VERSION = new Version('12.0.0-next.6+36.sha-18bc9ff');
 
 /**
  * @license
