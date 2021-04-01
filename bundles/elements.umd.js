9517e55df765eb27253e8080af1e39e7e2b14865
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 22:05:35 +0000

    154129b9de release: cut the v11.2.8 release (#41419)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+32.sha-4bac297
+ * @license Angular v11.2.7+33.sha-154129b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.7+32.sha-4bac297');
+    var VERSION = new core.Version('11.2.7+33.sha-154129b');
 
     /**
      * @license
