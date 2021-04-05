5c4926a609b34917b5bb3366f26300dc7f57e857
atscott <atscott01@gmail.com>
2021-04-05 15:36:59 +0000

    103f05ad54 docs: fix grammar mistake in `glossary.md` ("can exported" --> "can be exported") (#41440)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+3.sha-d4f739f
+ * @license Angular v11.2.8+6.sha-103f05a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+3.sha-d4f739f');
+const VERSION = new Version('11.2.8+6.sha-103f05a');
 
 /**
  * @license
