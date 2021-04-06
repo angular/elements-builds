7811f0afd794085fbfe696c2ac76b4a215da98a6
atscott <atscott01@gmail.com>
2021-04-06 17:29:45 +0000

    0eecbed69f docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+11.sha-6eb3dc7
+ * @license Angular v11.2.8+17.sha-0eecbed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
+const VERSION = new Version('11.2.8+17.sha-0eecbed');
 
 /**
  * @license
