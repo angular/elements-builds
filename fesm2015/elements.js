4700902be80b16e1a1c5241431cec093d97d1d39
atscott <atscott01@gmail.com>
2021-04-06 00:00:47 +0000

    6eb3dc770d build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+10.sha-754247f
+ * @license Angular v11.2.8+11.sha-6eb3dc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+10.sha-754247f');
+const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
 
 /**
  * @license
