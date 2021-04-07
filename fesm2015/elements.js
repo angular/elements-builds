5f2895fcad740077ca5da8ec3d8dac53674430dc
atscott <atscott01@gmail.com>
2021-04-07 19:26:29 +0000

    41636e6f5c build: rely on engines to prevent using npm for dependency install (#41477)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+32.sha-9408af6
+ * @license Angular v11.2.9+8.sha-41636e6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+32.sha-9408af6');
+const VERSION = new Version('11.2.9+8.sha-41636e6');
 
 /**
  * @license
