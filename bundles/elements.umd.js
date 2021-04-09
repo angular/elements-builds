0f5ad4ccbd0c5b20181493f23e5cca36a09b3076
Zach Arend <zachzach@google.com>
2021-04-09 15:49:00 +0000

    7946f6385b feat(dev-infra): create ReleaseNotes class for generating release notes during publishing (#41476)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+14.sha-e34299a
+ * @license Angular v11.2.9+17.sha-7946f63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.9+14.sha-e34299a');
+    var VERSION = new core.Version('11.2.9+17.sha-7946f63');
 
     /**
      * @license
