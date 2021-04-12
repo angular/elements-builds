ad3c8e3544a2b0fee4ff1cb2f75cf1d3120371b7
Zach Arend <zachzach@google.com>
2021-04-12 17:51:59 +0000

    419c189ad3 fix(core): error if DebugRenderer2.destroyNode is called twice in a row (#41565)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+17.sha-7946f63
+ * @license Angular v11.2.9+21.sha-419c189
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.9+17.sha-7946f63');
+    var VERSION = new core.Version('11.2.9+21.sha-419c189');
 
     /**
      * @license
