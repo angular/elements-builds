85e9110d2ede2cf80addd492094630a1e7983b34
Zach Arend <zachzach@google.com>
2021-04-13 00:09:21 +0000

    528e7d787c test: update CLI integration tests to run only for Ivy (#41434)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+37.sha-9bf8e51
+ * @license Angular v12.0.0-next.8+45.sha-528e7d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+37.sha-9bf8e51');
+const VERSION = new Version('12.0.0-next.8+45.sha-528e7d7');
 
 /**
  * @license
