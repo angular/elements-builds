4e22099c2eb0a50cd011ad399c8b23b2abf86e68
Zach Arend <zachzach@google.com>
2021-04-13 04:33:20 +0000

    fe6002977e docs: change (+) operator to `Number` function to match code (#41570) PR Close #41570

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+54.sha-3a61bb8
+ * @license Angular v12.0.0-next.8+56.sha-fe60029
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+54.sha-3a61bb8');
+const VERSION = new Version('12.0.0-next.8+56.sha-fe60029');
 
 /**
  * @license
