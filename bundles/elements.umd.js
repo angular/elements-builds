e7c14b55a95e34d00a9b211669f2f4bddd714ee6
Zach Arend <zachzach@google.com>
2021-04-14 22:27:50 +0000

    3a2b5906ae release: cut the v11.2.10 release

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+41.sha-b8c4da9
+ * @license Angular v11.2.10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.9+41.sha-b8c4da9');
+    var VERSION = new core.Version('11.2.10');
 
     /**
      * @license
--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+41.sha-b8c4da9
+ * @license Angular v11.2.10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+41.sha-b8c4da9');
+const VERSION = new Version('11.2.10');
 
 /**
  * @license
