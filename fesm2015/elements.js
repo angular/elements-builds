91980ac40b20e67d917ca42c103c478f253079cb
Andrew Kushnir <akushnir@google.com>
2021-04-19 17:03:20 +0000

    8c831a257c test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+16.sha-095bb52
+ * @license Angular v11.2.10+29.sha-8c831a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+16.sha-095bb52');
+const VERSION = new Version('11.2.10+29.sha-8c831a2');
 
 /**
  * @license
