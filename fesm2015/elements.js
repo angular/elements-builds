0bfd19c0b0b1beab7184c85b16a6b1039415f17f
Andrew Kushnir <akushnir@google.com>
2021-04-19 20:58:04 +0000

    3631934f2d Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+29.sha-8c831a2
+ * @license Angular v11.2.10+31.sha-3631934
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+29.sha-8c831a2');
+const VERSION = new Version('11.2.10+31.sha-3631934');
 
 /**
  * @license
