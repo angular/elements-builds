ce0c0f0a7f249815ff523d6fb559d6e04e45fbfc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:34:17 +0000

    aa578f56bf docs: fix links to docs with ambiguous paths (#41788)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+15.sha-21f1466
+ * @license Angular v11.2.11+18.sha-aa578f5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+15.sha-21f1466');
+const VERSION = new Version('11.2.11+18.sha-aa578f5');
 
 /**
  * @license
