bc5d6970bf65daee7e7041abf380693417143fe4
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:56:26 +0000

    f555a557a3 docs: remove mention of scheduleTick in markForCheck (#41845)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12
+ * @license Angular v11.2.12+1.sha-f555a55
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.12');
+const VERSION = new Version('11.2.12+1.sha-f555a55');
 
 /**
  * @license
