49c94dfbfcb889fb6826652054746cc12507984f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:52:29 +0000

    fca8d7ae92 docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+1.sha-f555a55
+ * @license Angular v11.2.12+3.sha-fca8d7a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.12+1.sha-f555a55');
+    var VERSION = new core.Version('11.2.12+3.sha-fca8d7a');
 
     /**
      * @license
