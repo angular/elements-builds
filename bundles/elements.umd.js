0de909e5403291c09bb8492470cd47767a32f209
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:59:28 +0000

    336fe9124e Revert "ci: re-enable the `@angular/components` unit tests (#41816)" (#41863)

--- bundles/elements.umd.js
+++ bundles/elements.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+36.sha-7219d1b
+ * @license Angular v11.2.11+38.sha-336fe91
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.11+36.sha-7219d1b');
+    var VERSION = new core.Version('11.2.11+38.sha-336fe91');
 
     /**
      * @license
