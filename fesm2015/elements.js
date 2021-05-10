45948c7b1bce182d422aaea864468e5ad45320e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:56:12 +0000

    a529d4f4f8 fix(core): do not retain dynamically compiled components and modules (#42003)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+376.sha-23f6b76
+ * @license Angular v12.0.0-next.8+386.sha-a529d4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+376.sha-23f6b76');
+const VERSION = new Version('12.0.0-next.8+386.sha-a529d4f');
 
 /**
  * @license
