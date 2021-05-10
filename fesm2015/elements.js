6ef81a79346ed52df301288c4c39f60af2e32602
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 17:04:19 +0000

    b075481059 build(docs-infra): revert optional chaining usage (#42021)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+12.sha-ed20342
+ * @license Angular v11.2.13+24.sha-b075481
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13+12.sha-ed20342');
+const VERSION = new Version('11.2.13+24.sha-b075481');
 
 /**
  * @license
