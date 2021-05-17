c277c050bc1284d88d48b56a4279b85b4436b90f
atscott <atscott01@gmail.com>
2021-05-17 18:15:48 +0000

    c32bea6ccf docs: add link to the blog post in the changelog (#42109) PR Close #42109

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
+ * @license Angular v12.0.0-next.8+420.sha-c32bea6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+418.sha-fc1bc0e');
+const VERSION = new Version('12.0.0-next.8+420.sha-c32bea6');
 
 /**
  * @license
