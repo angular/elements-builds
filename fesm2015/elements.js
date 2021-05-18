891d4a354e0669334d512ec168248f650a35e4e6
Andrew Scott <atscott@google.com>
2021-05-18 18:38:56 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+26.sha-6861911
+ * @license Angular v12.0.0+32.sha-c58e15a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+26.sha-6861911');
+const VERSION = new Version('12.0.0+32.sha-c58e15a');
 
 /**
  * @license
