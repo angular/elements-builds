bef431ce85821da89122a3211e09a854787f2bbf
Zach Arend <zachzach@google.com>
2021-05-25 17:44:01 +0000

    12b1e87e6c build: update CLI packages to 12.0.1 (#42307)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+15.sha-99802dc
+ * @license Angular v12.0.1+38.sha-12b1e87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+15.sha-99802dc');
+const VERSION = new Version('12.0.1+38.sha-12b1e87');
 
 /**
  * @license
