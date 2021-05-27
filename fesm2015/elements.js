ea266c83b8b5b7c70712e31ab97d053ec4ffd584
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:12:59 +0000

    e87dbafba1 docs: fix a typo (#42384)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+25.sha-38378ce
+ * @license Angular v12.1.0-next.3+26.sha-e87dbaf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+25.sha-38378ce');
+const VERSION = new Version('12.1.0-next.3+26.sha-e87dbaf');
 
 /**
  * @license
