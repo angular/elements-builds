e42c0117847c13c7ed5850e140a1088391b88f73
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 20:42:06 +0000

    47270d9e63 feat(forms): add `ng-submitted` class to forms that have been submitted. (#42132)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+32.sha-fa84d19
+ * @license Angular v12.1.0-next.4+33.sha-47270d9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+32.sha-fa84d19');
+const VERSION = new Version('12.1.0-next.4+33.sha-47270d9');
 
 /**
  * @license
