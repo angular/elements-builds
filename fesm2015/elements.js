27026c3878d0d02fb1aae2e6fed48d5b762a9f1d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:54 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+21.sha-e7c69b8
+ * @license Angular v12.1.0-next.5+19.sha-56a0582
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+21.sha-e7c69b8');
+const VERSION = new Version('12.1.0-next.5+19.sha-56a0582');
 
 /**
  * @license
