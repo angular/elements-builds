094af066310474024d7ddd19937d4b9d43aeb08d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 21:46:04 +0000

    d71d521f9b release: cut the v12.1.0 release

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+59.sha-2203217
+ * @license Angular v12.1.0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+59.sha-2203217');
+const VERSION = new Version('12.1.0');
 
 /**
  * @license
