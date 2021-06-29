139eec374c5224f7ce82d877a4c8b43d108df0de
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:46:20 +0000

    2c490d5c6c docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+14.sha-41823ff
+ * @license Angular v12.2.0-next.0+15.sha-2c490d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+14.sha-41823ff');
+const VERSION = new Version('12.2.0-next.0+15.sha-2c490d5');
 
 /**
  * @license
