4cbac8b70d660c6457f9281c2d29fb2a8e2c03b5
atscott <atscott01@gmail.com>
2021-07-02 17:08:31 +0000

    19bcdfb028 fix(docs-infra): correctly configure top-menu nav-item in the sidenav (#42747)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+5.sha-ffeea63
+ * @license Angular v12.2.0-next.1+7.sha-19bcdfb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+5.sha-ffeea63');
+const VERSION = new Version('12.2.0-next.1+7.sha-19bcdfb');
 
 /**
  * @license
