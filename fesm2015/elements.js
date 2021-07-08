42d3bf47bc33f93fe0616eb3c6e284f3645d53ee
atscott <atscott01@gmail.com>
2021-07-08 22:20:18 +0000

    dfe9e1d077 build: lock file maintenance (#42770)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+44.sha-89084ff
+ * @license Angular v12.2.0-next.1+46.sha-dfe9e1d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+44.sha-89084ff');
+const VERSION = new Version('12.2.0-next.1+46.sha-dfe9e1d');
 
 /**
  * @license
