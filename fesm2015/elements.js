ee57ca39fecca1724b725a0f34e8884bb1a33281
Andrew Kushnir <akushnir@google.com>
2021-07-09 22:08:38 +0000

    762e057e85 build: update to rules_nodejs v4.0.0-beta.0 (#42760)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+53.sha-9d58ebf
+ * @license Angular v12.2.0-next.1+58.sha-762e057
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+53.sha-9d58ebf');
+const VERSION = new Version('12.2.0-next.1+58.sha-762e057');
 
 /**
  * @license
