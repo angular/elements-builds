8ab438f1e9c4ad63e6cf72f9f77a44ffced9f005
Andrew Kushnir <akushnir@google.com>
2021-08-10 18:03:41 +0000

    7da3570be8 build(dev-infra): exlude commit message filter from formatting (#43091)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+9.sha-8a3d2aa.with-local-changes
+ * @license Angular v12.2.0+12.sha-7da3570.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0+9.sha-8a3d2aa.with-local-changes');
+const VERSION = new Version('12.2.0+12.sha-7da3570.with-local-changes');
 
 /**
  * @license
