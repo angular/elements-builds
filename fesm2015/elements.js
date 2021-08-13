4a2f04439935621d22baabb3b32fef653eb7a234
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:18:12 +0000

    19fd2f4125 ci: add breaking changes auto labeling action (#43141)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+3.sha-7a67597.with-local-changes
+ * @license Angular v12.2.1+4.sha-19fd2f4.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.1+3.sha-7a67597.with-local-changes');
+const VERSION = new Version('12.2.1+4.sha-19fd2f4.with-local-changes');
 
 /**
  * @license
