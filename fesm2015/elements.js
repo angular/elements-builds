899e933b22e2b13ca87525f8009dd82b86f3a718
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:27:04 +0000

    fdcb420bd7 docs(router): Fix incorrect name for url match options (#43268)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+3.sha-fb64f71.with-local-changes
+ * @license Angular v12.2.3+5.sha-fdcb420.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.3+3.sha-fb64f71.with-local-changes');
+const VERSION = new Version('12.2.3+5.sha-fdcb420.with-local-changes');
 
 /**
  * @license
