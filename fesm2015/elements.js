4776b02588690362805c1ae40b189be9603e6cc3
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:41:58 +0000

    6c84c5f513 fix(animations): emit pure annotations to static property initializers (#43344)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+6.sha-4c307ad.with-local-changes
+ * @license Angular v13.0.0-next.5+8.sha-6c84c5f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.5+6.sha-4c307ad.with-local-changes');
+const VERSION = new Version('13.0.0-next.5+8.sha-6c84c5f.with-local-changes');
 
 /**
  * @license
