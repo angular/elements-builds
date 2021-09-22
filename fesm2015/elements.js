f4532728a359cf63cd53d5c6ab7db0dd086f19c7
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:38:40 +0000

    9b7cdaefe8 docs(http): get rid of not needed space at the end of the sentence (#43528)

--- fesm2015/elements.js
+++ fesm2015/elements.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+51.sha-3cc8c7e.with-local-changes
+ * @license Angular v12.2.6+55.sha-9b7cdae.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.6+51.sha-3cc8c7e.with-local-changes');
+const VERSION = new Version('12.2.6+55.sha-9b7cdae.with-local-changes');
 
 /**
  * @license
