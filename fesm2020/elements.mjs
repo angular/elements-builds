e6a2e1d6211c2da3e94dfe6a4870754f8de91b3e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 19:48:32 +0000

    28a40f3396 refactor(compiler-cli): extract error documentation base url into separate file (#43527)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+118.sha-a91fe6d.with-local-changes
+ * @license Angular v13.0.0-next.14+120.sha-28a40f3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+118.sha-a91fe6d.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+120.sha-28a40f3.with-local-changes');
 
 /**
  * @license
