e86b53f58501994661baef20cd1ebda743e30882
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-28 18:53:41 +0000

    f225bfde80 docs(common): describe behavior when reference changes (#42863) PR Close #42863

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.2+1.sha-8d89b28.with-local-changes
+ * @license Angular v13.0.0-rc.2+5.sha-f225bfd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.2+1.sha-8d89b28.with-local-changes');
+const VERSION = new Version('13.0.0-rc.2+5.sha-f225bfd.with-local-changes');
 
 /**
  * @license
