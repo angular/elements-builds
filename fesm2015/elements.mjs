88134563663793d972818471c3b2808f6b663d37
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 19:52:25 +0000

    48ca7dce4f fix(compiler-cli): interpret string concat calls (#44167)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+16.sha-7e121e4.with-local-changes
+ * @license Angular v13.1.0-next.2+21.sha-48ca7dc.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.2+16.sha-7e121e4.with-local-changes');
+const VERSION = new Version('13.1.0-next.2+21.sha-48ca7dc.with-local-changes');
 
 /**
  * @license
