37db9658fb0a954794b2deb3a12085adcac5fc6d
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 19:11:07 +0000

    d5719c2e0f fix(core): input coercion (#42803)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+40.sha-db6cf7e.with-local-changes
+ * @license Angular v14.0.0-next.1+41.sha-d5719c2.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+40.sha-db6cf7e.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+41.sha-d5719c2.with-local-changes');
 
 /**
  * @license
