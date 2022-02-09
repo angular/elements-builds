f071ea7c04602b4cda77a930f25ace4a8e5788a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 20:03:35 +0000

    bbababe590 refactor(http): deprecated symbol used (#44985)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+20.sha-4c778cd.with-local-changes
+ * @license Angular v14.0.0-next.2+21.sha-bbababe.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.2+20.sha-4c778cd.with-local-changes');
+const VERSION = new Version('14.0.0-next.2+21.sha-bbababe.with-local-changes');
 
 /**
  * @license
