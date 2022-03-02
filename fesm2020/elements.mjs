15223cc58504208b1408934ec1c210a59e22f323
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-02 16:57:28 +0000

    69018c9f42 feat(core): allow for injector to be specified when creating an embedded view (#45156)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+42.sha-05d50b8
+ * @license Angular v14.0.0-next.4+43.sha-69018c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.4+42.sha-05d50b8');
+const VERSION = new Version('14.0.0-next.4+43.sha-69018c9');
 
 /**
  * @license
