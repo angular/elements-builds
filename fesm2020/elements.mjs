f074a58768d0d08b462b1a023e850a0cd1925df7
Andrew Kushnir <akushnir@google.com>
2022-03-07 22:49:52 +0000

    43bbaf810d build: update dependency google-closure-compiler to v20220301 (#45260)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.5+13.sha-f261fa7
+ * @license Angular v13.2.5+15.sha-43bbaf8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.5+13.sha-f261fa7');
+const VERSION = new Version('13.2.5+15.sha-43bbaf8');
 
 /**
  * @license
