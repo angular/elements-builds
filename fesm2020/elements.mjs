3f11cb7370f73807647c3e4a480b38e7ac3f6335
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:53:35 +0000

    f261fa7c08 test(compiler): fix deprecation warning (#45285)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.5+11.sha-de67c9f
+ * @license Angular v13.2.5+13.sha-f261fa7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.5+11.sha-de67c9f');
+const VERSION = new Version('13.2.5+13.sha-f261fa7');
 
 /**
  * @license
