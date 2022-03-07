cb51dbcc1ba51e44a1a374e98434f74b1ffd8a7a
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:53:08 +0000

    d87f66c02f test(compiler): fix deprecation warning (#45285)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+15.sha-886cfe8
+ * @license Angular v14.0.0-next.5+16.sha-d87f66c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+15.sha-886cfe8');
+const VERSION = new Version('14.0.0-next.5+16.sha-d87f66c');
 
 /**
  * @license
