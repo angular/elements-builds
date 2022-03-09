e7d5731353cc9d2c6aa7f0d504998ab65b5ff0e1
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:12:37 +0000

    6336b8e5b3 refactor(router): Make a few adjustments to createUrlTree for clarity (#45306)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.5+24.sha-6f1c941
+ * @license Angular v13.2.6+2.sha-6336b8e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.5+24.sha-6f1c941');
+const VERSION = new Version('13.2.6+2.sha-6336b8e');
 
 /**
  * @license
