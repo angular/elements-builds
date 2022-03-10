83998c6556bbbbb2d190251cecde3f658d4c6cec
Andrew Scott <atscott01@gmail.com>
2022-03-10 20:25:28 +0000

    8848d2dd28 docs: add manual changelog edits from `13.3.x` to main branch (#45323)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.6+6.sha-79580bb
+ * @license Angular v14.0.0-next.6+7.sha-8848d2d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.6+6.sha-79580bb');
+const VERSION = new Version('14.0.0-next.6+7.sha-8848d2d');
 
 /**
  * @license
