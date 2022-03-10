015e88e2f5bb245422db97a70bebda716325e3a1
GitHub <noreply@github.com>
2022-03-10 19:49:32 +0000

    79580bba4c docs: release notes for the v13.3.0-rc.0 release (#45322)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.6+3.sha-c7cab69
+ * @license Angular v14.0.0-next.6+6.sha-79580bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.6+3.sha-c7cab69');
+const VERSION = new Version('14.0.0-next.6+6.sha-79580bb');
 
 /**
  * @license
