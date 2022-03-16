678a7008bf8c72e48d44294e2b810d52c1c89959
Andrew Scott <atscott01@gmail.com>
2022-03-16 20:12:20 +0000

    3700323e1c docs: changelog for v13.3.0 release (#45367)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.0-rc.0+9.sha-f00f8c4
+ * @license Angular v13.3.0+2.sha-3700323
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.0-rc.0+9.sha-f00f8c4');
+const VERSION = new Version('13.3.0+2.sha-3700323');
 
 /**
  * @license
