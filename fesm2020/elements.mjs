35e7c259b627771feed57e52470674460a3eda2c
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:14:08 +0000

    f671dd5815 fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+26.sha-dc72f30
+ * @license Angular v14.0.0-next.7+29.sha-f671dd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+26.sha-dc72f30');
+const VERSION = new Version('14.0.0-next.7+29.sha-f671dd5');
 
 /**
  * @license
