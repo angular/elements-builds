be1b07cb13831ed0c4d14d83c031dbc0310b91e7
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:06:23 +0000

    0c28997739 build: disable payload size uploading within bazel (#45446)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.0+28.sha-88dbeda
+ * @license Angular v13.3.0+31.sha-0c28997
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.0+28.sha-88dbeda');
+const VERSION = new Version('13.3.0+31.sha-0c28997');
 
 /**
  * @license
