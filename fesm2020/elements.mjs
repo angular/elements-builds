fb068cdcc1e4b3f250fb33c965e8a7b5d858730c
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:43:54 +0000

    6e1fce529a test: update aio payload size golden to reflect Angular update (#45461)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.9+19.sha-16e635c
+ * @license Angular v14.0.0-next.9+22.sha-6e1fce5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.9+19.sha-16e635c');
+const VERSION = new Version('14.0.0-next.9+22.sha-6e1fce5');
 
 /**
  * @license
