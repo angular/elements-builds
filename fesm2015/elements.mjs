82b8663d39ce4ca51f6926a54949fa84cb66b755
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 22:20:18 +0000

    60b5a3de9d refactor(core): validate property (#45528)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.11+6.sha-2a81e44
+ * @license Angular v14.0.0-next.11+7.sha-60b5a3d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.11+6.sha-2a81e44');
+const VERSION = new Version('14.0.0-next.11+7.sha-60b5a3d');
 
 /**
  * @license
