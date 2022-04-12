15d6621e4ea2cdd21c5992ffd54b4eb3255ffbc6
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:44:31 +0000

    3c1695e399 docs: make tutorial instructions consistent (#45372)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.12+6.sha-820077d
+ * @license Angular v14.0.0-next.12+9.sha-3c1695e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.12+6.sha-820077d');
+const VERSION = new Version('14.0.0-next.12+9.sha-3c1695e');
 
 /**
  * @license
