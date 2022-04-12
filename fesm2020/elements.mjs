ccd9e2943f595e461bfbfc9274cbecc9a81b652d
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:52 +0000

    222d8662a2 docs: fix `schematicCollections` option (#45602) PR Close #45602

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.11+35.sha-eb2f449
+ * @license Angular v14.0.0-next.11+34.sha-222d866
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.11+35.sha-eb2f449');
+const VERSION = new Version('14.0.0-next.11+34.sha-222d866');
 
 /**
  * @license
