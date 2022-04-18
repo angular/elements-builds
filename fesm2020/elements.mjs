2321d1f3900cb643141c47cc15aeda19754cdd06
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:42:21 +0000

    b29b95b0f8 fix(devtools): clean up menu layout (#45665)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+19.sha-6835710
+ * @license Angular v14.0.0-next.13+20.sha-b29b95b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.13+19.sha-6835710');
+const VERSION = new Version('14.0.0-next.13+20.sha-b29b95b');
 
 /**
  * @license
