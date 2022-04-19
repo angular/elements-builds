e754372a450ce089a1e04bed7ac7820415f1c746
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:44:37 +0000

    5d155950e3 Fix typo (#45680) PR Close #45680

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+38.sha-ceffded
+ * @license Angular v14.0.0-next.13+39.sha-5d15595
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.13+38.sha-ceffded');
+const VERSION = new Version('14.0.0-next.13+39.sha-5d15595');
 
 /**
  * @license
