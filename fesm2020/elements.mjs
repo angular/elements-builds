d14b8777b976fe519fbaf0a2a4e07cf397efd64f
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:24:38 +0000

    81611dbc9d fix(docs-infra): fix placement of "Edit source" button on errors and diagnostics pages (#45634)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.3+27.sha-2ea541f
+ * @license Angular v13.3.4+2.sha-81611db
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.3+27.sha-2ea541f');
+const VERSION = new Version('13.3.4+2.sha-81611db');
 
 /**
  * @license
