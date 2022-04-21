328cce45c98eea714791703eac095755ce925e8f
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:28:31 +0000

    612d6e0d3d test: update size golden for hello-world-ivy-compat integration test (#45405)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+9.sha-3dee3d1
+ * @license Angular v14.0.0-next.14+16.sha-612d6e0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+9.sha-3dee3d1');
+const VERSION = new Version('14.0.0-next.14+16.sha-612d6e0');
 
 /**
  * @license
