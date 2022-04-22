85c34ba993485ac7fe1a1c50decfcaa3fae8ffb8
Andrew Scott <atscott01@gmail.com>
2022-04-22 16:44:02 +0000

    4962a4a332 feat(router): Allow `loadChildren` to return a `Route` array (#45700)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+18.sha-3e46a42
+ * @license Angular v14.0.0-next.14+19.sha-4962a4a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+18.sha-3e46a42');
+const VERSION = new Version('14.0.0-next.14+19.sha-4962a4a');
 
 /**
  * @license
