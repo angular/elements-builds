e00bbcd9e6252275501140b4faf72b1f9de51870
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:29:44 +0000

    9d755fa55b build: update actions/checkout action to v3.0.2 (#45764)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+28.sha-284329e
+ * @license Angular v14.0.0-next.14+29.sha-9d755fa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+28.sha-284329e');
+const VERSION = new Version('14.0.0-next.14+29.sha-9d755fa');
 
 /**
  * @license
