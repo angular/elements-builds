386bc7356f9d930746e959a2b608a737fcf50bdf
Dylan Hunn <dylhunn@gmail.com>
2022-04-27 20:58:13 +0000

    f3eb7d9ecb fix(core): Ensure the `StandaloneService` is retained after closure minification (#45783)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+43.sha-4d6a1d6
+ * @license Angular v14.0.0-next.15+1.sha-f3eb7d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+43.sha-4d6a1d6');
+const VERSION = new Version('14.0.0-next.15+1.sha-f3eb7d9');
 
 /**
  * @license
