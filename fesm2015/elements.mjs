0a9f5f144dacbac96ccec2fd44603a23c96b9fc8
Joey Perrott <josephperrott@gmail.com>
2022-04-28 17:36:20 +0000

    b596a5087f build: update dev-infra to latest snapshot (#45800)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+6.sha-2941793
+ * @license Angular v14.0.0-next.15+sha-b596a50
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+6.sha-2941793');
+const VERSION = new Version('14.0.0-next.15+sha-b596a50');
 
 /**
  * @license
