51311b43e4953794b297b1418882a71c1d4808fb
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:06:15 +0000

    a6675925b0 feat(core): allow to throw on unknown properties in tests (#45853)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-410d81f
+ * @license Angular v14.0.0-next.15+sha-a667592
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-410d81f');
+const VERSION = new Version('14.0.0-next.15+sha-a667592');
 
 /**
  * @license
