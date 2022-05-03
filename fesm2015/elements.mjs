ea241dd62de921b1b9ef867aa5c23f53ee50a4b7
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 20:31:31 +0000

    410d81f0e2 refactor(core): disallow standalone components in importProvidersFrom calls (#45837)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-8ba4ddc
+ * @license Angular v14.0.0-next.15+sha-410d81f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-8ba4ddc');
+const VERSION = new Version('14.0.0-next.15+sha-410d81f');
 
 /**
  * @license
