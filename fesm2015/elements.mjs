0391bde66b68aef71b1cbe1666af8bad4e975b8b
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:30:34 +0000

    e79260e445 release: cut the v14.0.0-next.16 release (#45875)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-c988487
+ * @license Angular v14.0.0-next.16+sha-e79260e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-c988487');
+const VERSION = new Version('14.0.0-next.16+sha-e79260e');
 
 /**
  * @license
