e293d64efcead4e63b5f7911c83177694cf93230
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:24:09 +0000

    c988487f03 docs: release notes for the v13.3.6 release (#45874)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-2dbdebc
+ * @license Angular v14.0.0-next.15+sha-c988487
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-2dbdebc');
+const VERSION = new Version('14.0.0-next.15+sha-c988487');
 
 /**
  * @license
