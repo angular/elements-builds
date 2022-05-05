511af461b8ca72b0de5e0cd684e0089bec533c1e
Andrew Kushnir <akushnir@google.com>
2022-05-05 19:08:44 +0000

    719199c678 docs: fix a markdown syntax error in aio/content/marketing/analytics.md (#45898) PR Close #45898

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.6+1.sha-0928025
+ * @license Angular v13.3.6+2.sha-719199c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.6+1.sha-0928025');
+const VERSION = new Version('13.3.6+2.sha-719199c');
 
 /**
  * @license
