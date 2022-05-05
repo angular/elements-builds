a4a4becd420717ebd07b1ec3a2bebd8562c69b65
Andrew Kushnir <akushnir@google.com>
2022-05-05 18:13:35 +0000

    dcea70fab7 refactor(core): rename internal bootstrap function (#45896)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.16+sha-1e4e9ee
+ * @license Angular v14.0.0-next.16+sha-dcea70f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.16+sha-1e4e9ee');
+const VERSION = new Version('14.0.0-next.16+sha-dcea70f');
 
 /**
  * @license
