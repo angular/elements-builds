01e054cb0c3c748a3906f84d046de6e6c18385ab
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:41:45 +0000

    5056cfe839 docs(docs-infra): clarify toh-2 error message (#45878)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.6+8.sha-6644165
+ * @license Angular v13.3.6+12.sha-5056cfe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.6+8.sha-6644165');
+const VERSION = new Version('13.3.6+12.sha-5056cfe');
 
 /**
  * @license
