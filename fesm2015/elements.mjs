c604bc2d154b93c1bc9a9467ecff9f2aec452c92
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:22:05 +0000

    75b3d0f843 perf(core): allow `checkNoChanges` mode to be tree-shaken in production (#45913)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-b5d78f4
+ * @license Angular v14.1.0-next.0+sha-75b3d0f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-b5d78f4');
+const VERSION = new Version('14.1.0-next.0+sha-75b3d0f');
 
 /**
  * @license
