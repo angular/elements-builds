61bff5d3258f79a2712a409431e386d1ccee679a
Andrew Kushnir <akushnir@google.com>
2022-05-10 20:51:44 +0000

    3165fa3f4e perf(platform-browser): avoid including Testability by default in `bootstrapApplication` (#45885)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-e441ff4
+ * @license Angular v14.1.0-next.0+sha-3165fa3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-e441ff4');
+const VERSION = new Version('14.1.0-next.0+sha-3165fa3');
 
 /**
  * @license
