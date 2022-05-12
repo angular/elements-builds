5651231e3c5bae6bd2ad6c5acfdac7b5ca400797
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 16:20:03 +0000

    b056f1b823 fix(docs-infra): do not detect a running job as failed (#45948)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.0+sha-1c0d0f0
+ * @license Angular v14.0.0-rc.0+sha-b056f1b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.0+sha-1c0d0f0');
+const VERSION = new Version('14.0.0-rc.0+sha-b056f1b');
 
 /**
  * @license
