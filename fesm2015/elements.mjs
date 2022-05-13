e00ce93df1e3672ccd465361c319158dde6989d1
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:27:03 +0000

    e1a93e4d7c build: manually unshallow the repo for builds publishing, use absolute paths (#45979)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-0db2841
+ * @license Angular v14.1.0-next.0+sha-e1a93e4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-0db2841');
+const VERSION = new Version('14.1.0-next.0+sha-e1a93e4');
 
 /**
  * @license
