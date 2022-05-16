8768a6f627dc85d960adba38a6ed74022f1ef69f
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 23:25:50 +0000

    add2bb8e89 docs: add template overview doc (#45897)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.0+sha-3c690bc
+ * @license Angular v14.0.0-rc.0+sha-add2bb8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.0+sha-3c690bc');
+const VERSION = new Version('14.0.0-rc.0+sha-add2bb8');
 
 /**
  * @license
