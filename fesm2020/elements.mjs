e7ecaa21fc8e081afd282a7a37f274eaddf95610
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 17:23:06 +0000

    ca04ecedec refactor(core): avoid code duplication in standalone-related logic (#45949)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-cd04588
+ * @license Angular v14.1.0-next.0+sha-ca04ece
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-cd04588');
+const VERSION = new Version('14.1.0-next.0+sha-ca04ece');
 
 /**
  * @license
