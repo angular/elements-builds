0d96fcde04d05cdce9e69e48ced68f40cce36d64
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 21:29:53 +0000

    3c690bc1e9 build: use updated commit sha for github actions (#46010)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.0+sha-018550e
+ * @license Angular v14.0.0-rc.0+sha-3c690bc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.0+sha-018550e');
+const VERSION = new Version('14.0.0-rc.0+sha-3c690bc');
 
 /**
  * @license
