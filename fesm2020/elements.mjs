0b25528617e2d8e897d8d2ddd6b17052946e6b6e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:20:41 +0000

    985f8d721e fix(core): produce proper error message for unknown props on `<ng-template>`s (#46068)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-863c32e
+ * @license Angular v14.1.0-next.0+sha-985f8d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-863c32e');
+const VERSION = new Version('14.1.0-next.0+sha-985f8d7');
 
 /**
  * @license
