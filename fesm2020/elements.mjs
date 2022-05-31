28c4ff6e4cc51e3f8f13add1a48ad6f87597726f
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:17:37 +0000

    a46f3a1bab ci: github robot should consider `closure-locale.ts` file as g3-synced file (#46177)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-b7394bf
+ * @license Angular v14.1.0-next.0+sha-a46f3a1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-b7394bf');
+const VERSION = new Version('14.1.0-next.0+sha-a46f3a1');
 
 /**
  * @license
