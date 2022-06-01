d71e43519b8f9693026ba0b3d87ee29e9dff0862
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-01 23:27:20 +0000

    bf6b44be35 docs(core): mark the standalone APIs as Developer Preview (#46050)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.3+sha-536939e
+ * @license Angular v14.0.0-rc.3+sha-bf6b44b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.3+sha-536939e');
+const VERSION = new Version('14.0.0-rc.3+sha-bf6b44b');
 
 /**
  * @license
