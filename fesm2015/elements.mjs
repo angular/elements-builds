13a776b127bdd0a6fe284732973db2a662e25920
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:27:01 +0000

    6d1e6a8bc7 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-1f01bcc
+ * @license Angular v14.1.0-next.0+sha-6d1e6a8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-1f01bcc');
+const VERSION = new Version('14.1.0-next.0+sha-6d1e6a8');
 
 /**
  * @license
