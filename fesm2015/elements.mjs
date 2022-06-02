dfcb9073e2cc51e6c7afd5a024df8bf7b784e27a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 20:59:13 +0000

    8fb737cfa4 refactor(core): remove unused error handler logic (#46216)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-a006ede
+ * @license Angular v14.1.0-next.0+sha-8fb737c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-a006ede');
+const VERSION = new Version('14.1.0-next.0+sha-8fb737c');
 
 /**
  * @license
