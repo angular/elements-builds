021154b5ad5a539d5cedac7c3edfbd89d2113298
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-03 17:29:07 +0000

    6c44222fa1 refactor(core): don't use patched data in LifecycleHooksFeature (#46237)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-da0f53b
+ * @license Angular v14.1.0-next.0+sha-6c44222
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-da0f53b');
+const VERSION = new Version('14.1.0-next.0+sha-6c44222');
 
 /**
  * @license
