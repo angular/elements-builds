ae42790e034069e013725a7f42aee5ee0ae515e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 17:29:55 +0000

    f12cf2b89b docs(forms): Amend the FormGroupDirective docs to indicate it also works with FormRecord. (#46235)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-5cfde8b
+ * @license Angular v14.1.0-next.0+sha-f12cf2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-5cfde8b');
+const VERSION = new Version('14.1.0-next.0+sha-f12cf2b');
 
 /**
  * @license
