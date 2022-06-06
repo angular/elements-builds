b39c73cefe4072573e2e64d9107e23425f16cc27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 17:30:43 +0000

    d846bba678 refactor(core): improve code around element validation (#46175)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-f12cf2b
+ * @license Angular v14.1.0-next.0+sha-d846bba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-f12cf2b');
+const VERSION = new Version('14.1.0-next.0+sha-d846bba');
 
 /**
  * @license
