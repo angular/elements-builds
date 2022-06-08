d1b2d21236f652d3d076a165046e4da6680af1c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-08 19:59:01 +0000

    7391143dd3 docs: remove azure builder reference (#46285)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-34890fb
+ * @license Angular v14.1.0-next.0+sha-7391143
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-34890fb');
+const VERSION = new Version('14.1.0-next.0+sha-7391143');
 
 /**
  * @license
