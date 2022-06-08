2feee2ec459ad2ed49495fa2eacce1bed2adc3f9
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-08 21:10:17 +0000

    16c71fc56f docs(forms): fix FormRecord usage notes (#46299)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.1+sha-f0b94f5
+ * @license Angular v14.0.1+sha-16c71fc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.1+sha-f0b94f5');
+const VERSION = new Version('14.0.1+sha-16c71fc');
 
 /**
  * @license
