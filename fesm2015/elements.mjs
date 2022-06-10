d2baeee6e9c078169a88bdc2eb693ac7947282b3
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 18:17:46 +0000

    8ac3513b62 docs: Add kevindaviladev to contributors (#46330)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.1+sha-75c953f
+ * @license Angular v14.0.1+sha-8ac3513
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.1+sha-75c953f');
+const VERSION = new Version('14.0.1+sha-8ac3513');
 
 /**
  * @license
