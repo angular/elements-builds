b13e60ad8d433b5c6b3692e74287b259f00c2e11
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 15:42:58 +0000

    77ae8e1c20 refactor(router): clean up internal hooks (#46321)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.1+sha-8ecfd71
+ * @license Angular v14.0.1+sha-77ae8e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.1+sha-8ecfd71');
+const VERSION = new Version('14.0.1+sha-77ae8e1');
 
 /**
  * @license
