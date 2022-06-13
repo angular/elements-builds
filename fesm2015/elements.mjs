eb8f7a5331326ca70e9522766550c308cdba438a
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:57:38 +0000

    a0f3c81bb3 build: update angular version of in-memory-web-api (#46344)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.1+sha-3fd8948
+ * @license Angular v14.0.1+sha-a0f3c81
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.1+sha-3fd8948');
+const VERSION = new Version('14.0.1+sha-a0f3c81');
 
 /**
  * @license
