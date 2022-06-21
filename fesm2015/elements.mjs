ffb79be526702073efcc1096a16c76cab976f6d2
Andrew Kushnir <akushnir@google.com>
2022-06-21 20:01:04 +0000

    c086653655 fix(core): deduplicate imports of standalone components in JIT compiler (#46439)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.2+sha-79a973a
+ * @license Angular v14.0.2+sha-c086653
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.2+sha-79a973a');
+const VERSION = new Version('14.0.2+sha-c086653');
 
 /**
  * @license
