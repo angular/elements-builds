e94fa942a2143a56e8c4d3f03764d89cf6fe80c3
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:12:14 +0000

    51be9bbe29 fix(animations): cleanup DOM elements when the root view is removed (#45143)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.3+sha-a4c9e8d
+ * @license Angular v14.0.3+sha-51be9bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.3+sha-a4c9e8d');
+const VERSION = new Version('14.0.3+sha-51be9bb');
 
 /**
  * @license
