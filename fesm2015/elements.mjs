35b82aafbffdc351671efe6173ac3484b3779036
Dylan Hunn <dylhunn@gmail.com>
2022-06-25 00:22:29 +0000

    e9e1d8595f docs(core): add circleci insights metrics widget (#46496)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.3+sha-f930fc2
+ * @license Angular v14.0.3+sha-e9e1d85
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.3+sha-f930fc2');
+const VERSION = new Version('14.0.3+sha-e9e1d85');
 
 /**
  * @license
