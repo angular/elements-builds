f2c5641fdbf207cc81a80315f772bfa7f479e4b7
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 23:47:18 +0000

    98b41bf399 docs: add nicobytes to GDE resources and Platzi to Online Training (#46668)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.5+sha-023c218
+ * @license Angular v14.0.5+sha-98b41bf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.5+sha-023c218');
+const VERSION = new Version('14.0.5+sha-98b41bf');
 
 /**
  * @license
