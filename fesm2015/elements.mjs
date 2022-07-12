b89df0a3d08454096140d4a04d6c615138c27008
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 22:08:24 +0000

    99697dae66 fix(compiler-cli): only consider used pipes for inline type-check requirement (#46807)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.5+sha-4a2c6e3
+ * @license Angular v14.0.5+sha-99697da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.5+sha-4a2c6e3');
+const VERSION = new Version('14.0.5+sha-99697da');
 
 /**
  * @license
