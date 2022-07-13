b9779dca7dfe24dab314e9aaa83458e69ca8aa8d
GitHub <noreply@github.com>
2022-07-13 19:23:29 +0000

    79aa8be54d release: cut the v14.0.6 release (#46827)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.5+sha-dff82f2
+ * @license Angular v14.0.6+sha-79aa8be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.5+sha-dff82f2');
+const VERSION = new Version('14.0.6+sha-79aa8be');
 
 /**
  * @license
