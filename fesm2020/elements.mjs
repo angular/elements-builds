be44c22b2b87c897aa5ef1589f74da84bb633fd3
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 21:43:50 +0000

    3f2d16c823 fix(compiler): inputs/outputs incorrectly parsed in jit mode (#46813)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-rc.0+sha-4dca184
+ * @license Angular v14.1.0-rc.0+sha-3f2d16c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-rc.0+sha-4dca184');
+const VERSION = new Version('14.1.0-rc.0+sha-3f2d16c');
 
 /**
  * @license
