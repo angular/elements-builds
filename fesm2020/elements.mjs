da997001f62321ef1593b6f56baa4af422a02a30
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 20:22:51 +0000

    4dca1842b3 docs: describe smallest typo, "an" -> "a" (#46812) PR Close #46812

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-rc.0+sha-442b16c
+ * @license Angular v14.1.0-rc.0+sha-4dca184
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-rc.0+sha-442b16c');
+const VERSION = new Version('14.1.0-rc.0+sha-4dca184');
 
 /**
  * @license
