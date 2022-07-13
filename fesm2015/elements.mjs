01eb42e8a5f3f31c1e08c6571015024204fcbbeb
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 23:13:45 +0000

    676bf041c1 docs: correct debugging task step (#46832)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.6+sha-481dc8d
+ * @license Angular v14.0.6+sha-676bf04
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.6+sha-481dc8d');
+const VERSION = new Version('14.0.6+sha-676bf04');
 
 /**
  * @license
