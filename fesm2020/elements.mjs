8f5e9f0a6bea0a4d3c9ce43390e0ff8ab3cdbf9f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 22:18:19 +0000

    f9edb776ca refactor(forms): simplify group builder function (#46844)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-rc.0+sha-08fb7bc
+ * @license Angular v14.1.0-rc.0+sha-f9edb77
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-rc.0+sha-08fb7bc');
+const VERSION = new Version('14.1.0-rc.0+sha-f9edb77');
 
 /**
  * @license
