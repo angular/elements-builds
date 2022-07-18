e98386a0dbb06c71b14363ad327e2d97cf3fe1a0
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 22:22:01 +0000

    e95ee96562 refactor(router): Update TitleStrategy to useFactory (#46876)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-rc.0+sha-6b8e60c
+ * @license Angular v14.1.0-rc.0+sha-e95ee96
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-rc.0+sha-6b8e60c');
+const VERSION = new Version('14.1.0-rc.0+sha-e95ee96');
 
 /**
  * @license
