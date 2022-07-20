32f506328454262b761367504fedc28d75e4f40d
Andrew Kushnir <akushnir@google.com>
2022-07-20 19:38:14 +0000

    0cbbd6aeec refactor(router): Update Router to be `providedIn: 'root'` (#46914)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0+sha-a8e9247
+ * @license Angular v14.1.0+sha-0cbbd6a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0+sha-a8e9247');
+const VERSION = new Version('14.1.0+sha-0cbbd6a');
 
 /**
  * @license
