ce0a013c31a3c38d08bb95685302e479569c400c
Andrew Kushnir <akushnir@google.com>
2022-08-03 18:47:45 +0000

    cb0fe50b1d refactor(common): make NgOptimizedImage directive standalone

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-37ce31f
+ * @license Angular v14.2.0-next.0+sha-cb0fe50
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-37ce31f');
+const VERSION = new Version('14.2.0-next.0+sha-cb0fe50');
 
 /**
  * @license
