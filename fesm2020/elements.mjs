95d498e106538d6832f00a14b7abfcec6f51c743
Dylan Hunn <dylhunn@gmail.com>
2022-08-09 17:19:39 +0000

    e10aa8858f docs: add antoniocardenas to GDE resources (#47033)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.1+sha-874d78b
+ * @license Angular v14.1.1+sha-e10aa88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.1+sha-874d78b');
+const VERSION = new Version('14.1.1+sha-e10aa88');
 
 /**
  * @license
