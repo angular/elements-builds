ac581dcbeceb68ed084898de5607c5243e0c3e87
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-08-12 16:21:32 +0000

    757684b705 docs: set titles to sentence case (#47083)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-422323c
+ * @license Angular v14.2.0-next.1+sha-757684b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-422323c');
+const VERSION = new Version('14.2.0-next.1+sha-757684b');
 
 /**
  * @license
