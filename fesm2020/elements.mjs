a1f9bd8122da852d502bff25d97dd8ab8b538e42
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 16:28:35 +0000

    a5ca049fd9 refactor(router): Provide easily patchable file for assigning relativeLinkResolution (#47136)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.2+sha-5547ce0
+ * @license Angular v14.1.2+sha-a5ca049
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.2+sha-5547ce0');
+const VERSION = new Version('14.1.2+sha-a5ca049');
 
 /**
  * @license
