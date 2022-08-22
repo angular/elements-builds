0d636708560a79b088d626f787a40098882ced50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:16:08 +0000

    edf7f670f0 refactor(compiler): Add a new helper method `getOwningNgModule`. (#47166)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-7884837
+ * @license Angular v14.3.0-next.0+sha-edf7f67
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-7884837');
+const VERSION = new Version('14.3.0-next.0+sha-edf7f67');
 
 /**
  * @license
