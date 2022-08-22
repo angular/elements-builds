ec014ca2b2076fd853058fc82691722c4e8c4ec4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 23:33:33 +0000

    54ceed53e2 refactor(compiler): add support for host directives (#46868)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-b6fbbea
+ * @license Angular v14.3.0-next.0+sha-54ceed5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-b6fbbea');
+const VERSION = new Version('14.3.0-next.0+sha-54ceed5');
 
 /**
  * @license
