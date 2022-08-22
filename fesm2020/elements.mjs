80ced9b8d6f51c708ed3b65a34515bc9e8e55448
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 22:11:40 +0000

    b6fbbeacf2 Revert "fix(animations): fix stagger timing not handling params (#47208)" (#47222)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-a44b4bf
+ * @license Angular v14.3.0-next.0+sha-b6fbbea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-a44b4bf');
+const VERSION = new Version('14.3.0-next.0+sha-b6fbbea');
 
 /**
  * @license
