e013afe2a6a6466a826116480f9f08c37d7d0977
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:40:21 +0000

    4780f1bf26 ci: do not match dev-infra group for pullapprove owner changes (#47216)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-c5b3f1c
+ * @license Angular v14.3.0-next.0+sha-4780f1b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-c5b3f1c');
+const VERSION = new Version('14.3.0-next.0+sha-4780f1b');
 
 /**
  * @license
