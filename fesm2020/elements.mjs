826886965877bb56cab94eaa644d9851fab57972
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-02 02:37:39 +0000

    af975d5bef docs(http): replace http method return description (#47322)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0+sha-4ac5ea3
+ * @license Angular v14.2.0+sha-af975d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0+sha-4ac5ea3');
+const VERSION = new Version('14.2.0+sha-af975d5');
 
 /**
  * @license
