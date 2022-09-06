52f210c485ad139219c9df75e8e69b62518d8e80
Andrew Kushnir <akushnir@google.com>
2022-09-06 18:51:09 +0000

    1edb8e5c45 docs: add new words to vocabulary (#47344)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-e810402
+ * @license Angular v15.0.0-next.0+sha-1edb8e5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-e810402');
+const VERSION = new Version('15.0.0-next.0+sha-1edb8e5');
 
 /**
  * @license
