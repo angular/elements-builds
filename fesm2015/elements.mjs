ec1bf7769947e8ef5e2ad5f461fae2ef9a1e4817
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:50:09 +0000

    d3c2ca13ce docs: fix linting errors in change detection guide (#47324)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-a468865
+ * @license Angular v15.0.0-next.0+sha-d3c2ca1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-a468865');
+const VERSION = new Version('15.0.0-next.0+sha-d3c2ca1');
 
 /**
  * @license
