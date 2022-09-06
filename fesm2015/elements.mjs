b891f0dcbee84444f83a2a05b335d18765c5c6fc
Andrew Kushnir <akushnir@google.com>
2022-09-06 19:16:00 +0000

    0e35829580 fix(compiler): avoid errors for inputs with Object-builtin names (#47220)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0+sha-6091786
+ * @license Angular v14.2.0+sha-0e35829
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0+sha-6091786');
+const VERSION = new Version('14.2.0+sha-0e35829');
 
 /**
  * @license
