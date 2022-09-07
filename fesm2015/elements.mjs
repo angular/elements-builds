4cb4136c7ebf75f12a72c1169e7ab4ba87926bf4
Andrew Kushnir <akushnir@google.com>
2022-09-07 18:04:45 +0000

    eca3db5692 docs(docs-infra): fix minor typos in angular.io (#47295) (#47295)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-e25349c
+ * @license Angular v15.0.0-next.1+sha-eca3db5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-e25349c');
+const VERSION = new Version('15.0.0-next.1+sha-eca3db5');
 
 /**
  * @license
