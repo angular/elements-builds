75ab6b6ba6e2a6a947f81ab8b0efa049829c7e13
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:23:04 +0000

    710d1da4ed refactor(common): create an `NgFor` alias for `NgForOf` directive (#47309)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-7e0de5b
+ * @license Angular v15.0.0-next.1+sha-710d1da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-7e0de5b');
+const VERSION = new Version('15.0.0-next.1+sha-710d1da');
 
 /**
  * @license
