df81734ecf8dc107fae7e335589470325fc1ef53
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:47:04 +0000

    9e2d3ed2d1 refactor(common): drop unnecessary srcset sanitization (#47302)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-4a13210
+ * @license Angular v15.0.0-next.1+sha-9e2d3ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-4a13210');
+const VERSION = new Version('15.0.0-next.1+sha-9e2d3ed');
 
 /**
  * @license
