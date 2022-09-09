5e657d9a13373e4f614f82aa93734d851cd2bd93
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:46:55 +0000

    4a13210ecd fix(forms): don't prevent default behavior for forms with method="dialog" (#47308)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-ca7bf65
+ * @license Angular v15.0.0-next.1+sha-4a13210
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-ca7bf65');
+const VERSION = new Version('15.0.0-next.1+sha-4a13210');
 
 /**
  * @license
