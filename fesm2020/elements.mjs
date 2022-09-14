4a52369723b3a83a02563c20c689b2d03b1e1620
Dylan Hunn <dylhunn@gmail.com>
2022-09-14 06:03:01 +0000

    69ecbd5bcc ci: update payload size for Animations integration test (#47424)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-7ec196e
+ * @license Angular v15.0.0-next.1+sha-69ecbd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-7ec196e');
+const VERSION = new Version('15.0.0-next.1+sha-69ecbd5');
 
 /**
  * @license
