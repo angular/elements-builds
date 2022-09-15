074d91db5637dfcbdc4212fcb6273fb2ec36a909
GitHub <noreply@github.com>
2022-09-15 17:44:29 +0000

    1f88df0f8f build: update `CODE_OF_CONDUCT.md` to match the content of `angular/.github`

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-deaa05a
+ * @license Angular v15.0.0-next.2+sha-1f88df0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-deaa05a');
+const VERSION = new Version('15.0.0-next.2+sha-1f88df0');
 
 /**
  * @license
