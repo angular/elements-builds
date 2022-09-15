52c2bcee0805bd156f0e78cc87b016022b56cb26
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:39:24 +0000

    8deedc58a6 build: update cross-repo angular dependencies (#47407)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-19f189f
+ * @license Angular v15.0.0-next.2+sha-8deedc5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-19f189f');
+const VERSION = new Version('15.0.0-next.2+sha-8deedc5');
 
 /**
  * @license
