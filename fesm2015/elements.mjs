dca478aeab69e16b1c23fef8fe4009fbf48d0899
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 08:00:50 +0000

    3f4cbe4ca7 docs: update contribution docs for serving a local library/project (#47421)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.2+sha-db3b532
+ * @license Angular v14.2.2+sha-3f4cbe4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.2+sha-db3b532');
+const VERSION = new Version('14.2.2+sha-3f4cbe4');
 
 /**
  * @license
