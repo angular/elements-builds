3fdebc517cc23f25d3fac55ed8e53053bae37cb3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:39:15 +0000

    c9991ca49d build: update dependency google-closure-compiler to v20220905 (#47437)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-53db8ab
+ * @license Angular v15.0.0-next.2+sha-c9991ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-53db8ab');
+const VERSION = new Version('15.0.0-next.2+sha-c9991ca');
 
 /**
  * @license
