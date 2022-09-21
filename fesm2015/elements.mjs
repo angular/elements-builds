a02a7f9558b41986d142da749bfd4a1ce4cbfbec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 08:24:55 +0000

    06ca0dcf27 refactor(core): remove assumptions that component will be first in directives list (#47490)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-59aa2c0
+ * @license Angular v15.0.0-next.2+sha-06ca0dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-59aa2c0');
+const VERSION = new Version('15.0.0-next.2+sha-06ca0dc');
 
 /**
  * @license
