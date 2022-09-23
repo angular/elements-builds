675a8954d2ec40ab3b2758abf2df7fae52a7ca43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:23:43 +0000

    76f393b322 docs: visualize keyboard shortcut for search (#47507)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.3+sha-7e6fc10
+ * @license Angular v14.2.3+sha-76f393b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.3+sha-7e6fc10');
+const VERSION = new Version('14.2.3+sha-76f393b');
 
 /**
  * @license
