9ca1bf3924329fe6229f81071d4048ba6b9de678
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-26 17:18:11 +0000

    7e7769f869 docs: fixing standalone components sample code (#47542) PR Close #47542

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.3+sha-76f393b
+ * @license Angular v14.2.3+sha-7e7769f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.3+sha-76f393b');
+const VERSION = new Version('14.2.3+sha-7e7769f');
 
 /**
  * @license
