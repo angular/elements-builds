7320d698fd6dc3a9e49b96cbbc0fc9046c22e036
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:36:30 +0000

    10a3cefdb4 docs: update broken link (#47532) PR Close #47532

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.3+sha-2c3e2d6
+ * @license Angular v14.2.3+sha-10a3cef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.3+sha-2c3e2d6');
+const VERSION = new Version('14.2.3+sha-10a3cef');
 
 /**
  * @license
