3a80398ba384df316d1c2319b059ba2347e29a3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:27:53 +0000

    120555a626 feat(core): support object-based DI flags in TestBed.inject() (#46761)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.3+sha-85b5d12
+ * @license Angular v15.0.0-next.3+sha-120555a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.3+sha-85b5d12');
+const VERSION = new Version('15.0.0-next.3+sha-120555a');
 
 /**
  * @license
