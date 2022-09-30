1f071b3c044cf45144da911ed7346e3b8819b6e3
Andrew Kushnir <akushnir@google.com>
2022-09-30 18:12:29 +0000

    ce59f1a14d test: delete bazel_ngtsc_plugin integration test (#47584)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-382330c
+ * @license Angular v15.0.0-next.4+sha-ce59f1a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-382330c');
+const VERSION = new Version('15.0.0-next.4+sha-ce59f1a');
 
 /**
  * @license
