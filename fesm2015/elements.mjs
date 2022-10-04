02dec2a664b23c274fcaa6d4ea123402bd17fc02
Andrew Kushnir <akushnir@google.com>
2022-10-04 23:38:40 +0000

    ab4ef264ed refactor(router): drop own constructor from the `RouterLinkWithHref` class (#47619)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-2919463
+ * @license Angular v15.0.0-next.4+sha-ab4ef26
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-2919463');
+const VERSION = new Version('15.0.0-next.4+sha-ab4ef26');
 
 /**
  * @license
