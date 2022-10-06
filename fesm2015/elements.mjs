4ca8779d34e7031b89e25097bbfd3a1af15ce11b
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:20:21 +0000

    197e8b4963 docs: fixed typo in AsyncValidatorFn (#47465)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-1095dc5
+ * @license Angular v14.2.5+sha-197e8b4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-1095dc5');
+const VERSION = new Version('14.2.5+sha-197e8b4');
 
 /**
  * @license
