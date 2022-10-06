d9a26a607457848bd64998a2b77cbcbe8279bc7d
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 17:08:10 +0000

    fcea8fe377 docs: fix linter errors for libraries.md (#47668)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-5d002df
+ * @license Angular v15.0.0-next.5+sha-fcea8fe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-5d002df');
+const VERSION = new Version('15.0.0-next.5+sha-fcea8fe');
 
 /**
  * @license
