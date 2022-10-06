f12fd2945d18ae14487107941c66820736db72b1
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 18:33:07 +0000

    db28badfe6 feat(core): enable the new directive composition API (#47642)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-e0dd2c5
+ * @license Angular v15.0.0-next.5+sha-db28bad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-e0dd2c5');
+const VERSION = new Version('15.0.0-next.5+sha-db28bad');
 
 /**
  * @license
