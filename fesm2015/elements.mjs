f9b0d7fd67ffb39aa1006697f8eed5d6a81d9ee3
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 21:51:38 +0000

    bebef5fb43 feat(language-service): Quick fix to import a component when its selector is used (#47088)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-75e6297
+ * @license Angular v15.0.0-next.5+sha-bebef5f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-75e6297');
+const VERSION = new Version('15.0.0-next.5+sha-bebef5f');
 
 /**
  * @license
