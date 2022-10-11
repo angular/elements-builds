b9ab597c56f6f55b92de92bc0ad544e8a00a6221
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 23:05:19 +0000

    a910c8328a build(devtools): migrate to manifest v3 (#47575)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-bbbd4c3
+ * @license Angular v15.0.0-next.5+sha-a910c83
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-bbbd4c3');
+const VERSION = new Version('15.0.0-next.5+sha-a910c83');
 
 /**
  * @license
