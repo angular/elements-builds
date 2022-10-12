44ac64828c1c8fb53c6527eb3cbcd8fa14b5505f
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-12 16:18:23 +0000

    3fd176a905 fix(compiler-cli): add missing period to error message (#47744)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-b82da42
+ * @license Angular v14.2.5+sha-3fd176a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-b82da42');
+const VERSION = new Version('14.2.5+sha-3fd176a');
 
 /**
  * @license
