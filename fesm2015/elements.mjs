cdb6262ef7de3cfae4c8aed8257bc6b27f7a11b3
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-10-12 18:58:09 +0000

    755fe0ee3f docs: release notes for the v15.0.0-next.6 release

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-7ab0a26
+ * @license Angular v15.1.0-next.0+sha-755fe0e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-7ab0a26');
+const VERSION = new Version('15.1.0-next.0+sha-755fe0e');
 
 /**
  * @license
