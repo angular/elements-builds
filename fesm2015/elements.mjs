41a573c1759eefbab0b2ecfbb56e658c08af2a87
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 16:56:27 +0000

    2ca213747c docs: update of documentation contrib. guide (#47381)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.6+sha-24950dd
+ * @license Angular v14.2.6+sha-2ca2137
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.6+sha-24950dd');
+const VERSION = new Version('14.2.6+sha-2ca2137');
 
 /**
  * @license
