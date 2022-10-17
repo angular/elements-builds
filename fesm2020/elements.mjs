28149c5797fec676b3a0a4c73113a23f8a0baa02
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 10:34:28 +0000

    0d65e1de2c docs: declare Standalone APIs as stable (#47754)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-6140bae
+ * @license Angular v15.1.0-next.0+sha-0d65e1d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-6140bae');
+const VERSION = new Version('15.1.0-next.0+sha-0d65e1d');
 
 /**
  * @license
