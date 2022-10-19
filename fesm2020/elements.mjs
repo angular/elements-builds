595cd810c85f0c632273de23aacf3ee5e8828124
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:33:02 +0000

    15044737a6 docs: fix linter errors for accessibility.md (#47725)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.6+sha-64d6a4f
+ * @license Angular v14.2.6+sha-1504473
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.6+sha-64d6a4f');
+const VERSION = new Version('14.2.6+sha-1504473');
 
 /**
  * @license
