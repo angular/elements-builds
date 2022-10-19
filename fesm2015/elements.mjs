a515c319dc0de7e966de2194c98770e93d688d4c
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:31:00 +0000

    79e9e8ab77 fix(router): Delay router scroll event until navigated components have rendered (#47563)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.6+sha-716352c
+ * @license Angular v15.0.0-next.6+sha-79e9e8a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.6+sha-716352c');
+const VERSION = new Version('15.0.0-next.6+sha-79e9e8a');
 
 /**
  * @license
