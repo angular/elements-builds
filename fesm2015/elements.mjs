1fb3477e0135d6c5e0f29afa497c2891bfaac46a
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:29:10 +0000

    80da664b71 docs: Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47790)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-b31091b
+ * @license Angular v15.1.0-next.0+sha-80da664
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-b31091b');
+const VERSION = new Version('15.1.0-next.0+sha-80da664');
 
 /**
  * @license
