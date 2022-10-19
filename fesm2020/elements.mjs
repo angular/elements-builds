3cbe5b16dfae8c9ee467c3b0d571384ede5d86f4
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:43:55 +0000

    4d18ba1af7 release: cut the v14.2.7 release

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.6+sha-1504473
+ * @license Angular v14.2.7+sha-4d18ba1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.6+sha-1504473');
+const VERSION = new Version('14.2.7+sha-4d18ba1');
 
 /**
  * @license
