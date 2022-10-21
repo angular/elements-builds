45cfa2b357874384fd8e4ecf89d9527d2db017ca
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 17:32:55 +0000

    7c4f77a062 docs: fix linter errors in user-input.md (#47832)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.7+sha-a42c8cf
+ * @license Angular v14.2.7+sha-7c4f77a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.7+sha-a42c8cf');
+const VERSION = new Version('14.2.7+sha-7c4f77a');
 
 /**
  * @license
