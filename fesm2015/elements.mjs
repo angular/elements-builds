432d29696f1cd543a39bd98139ee85b97200bb52
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 14:00:56 +0000

    41709d8aff docs: removing http-server section in Service worker tour (#47613)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-e1bf693
+ * @license Angular v15.1.0-next.0+sha-41709d8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-e1bf693');
+const VERSION = new Version('15.1.0-next.0+sha-41709d8');
 
 /**
  * @license
