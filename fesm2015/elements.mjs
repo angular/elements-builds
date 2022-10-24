e7deec976cecad0c4f34c5e10a61a3072317c57b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:01:27 +0000

    6471eafde6 build: update recommended launch vscode config to use proper bazel dist path (#47831)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-a014ca1
+ * @license Angular v15.0.0-rc.0+sha-6471eaf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-a014ca1');
+const VERSION = new Version('15.0.0-rc.0+sha-6471eaf');
 
 /**
  * @license
