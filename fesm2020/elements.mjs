326d40519e9773e2ce8987ef61dbb9b78071a399
Andrew Kushnir <akushnir@google.com>
2022-11-04 16:47:17 +0000

    b8a6f7491b fix(router): fix redirectTo on named outlets - resolves #33783 (#47927)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.2+sha-e7b48da
+ * @license Angular v15.0.0-rc.2+sha-b8a6f74
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.2+sha-e7b48da');
+const VERSION = new Version('15.0.0-rc.2+sha-b8a6f74');
 
 /**
  * @license
