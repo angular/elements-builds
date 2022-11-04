52f92a48bbd74fb07a3e41df7f63ae8db9a72350
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:13 +0000

    ad8ccedeca build: enable the branch manager action in the repo (#47942)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-febf29d
+ * @license Angular v15.1.0-next.0+sha-ad8cced
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-febf29d');
+const VERSION = new Version('15.1.0-next.0+sha-ad8cced');
 
 /**
  * @license
