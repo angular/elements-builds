efa5d273aea151ac193386ef54c181cd2af526b5
Andrew Kushnir <akushnir@google.com>
2022-11-07 20:51:04 +0000

    3e490fab1b refactor(core): remove duplicate createRenderer call (#47981)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.2+sha-b8a6f74
+ * @license Angular v15.0.0-rc.2+sha-3e490fa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.2+sha-b8a6f74');
+const VERSION = new Version('15.0.0-rc.2+sha-3e490fa');
 
 /**
  * @license
