e97dcee1f3bbb06f28790c0b8913d83fdeca65c8
Andrew Kushnir <akushnir@google.com>
2022-11-08 18:52:56 +0000

    8e6ec72beb fix(forms): Runtime error pages must begin with leading zero (#47991)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-2ae3614
+ * @license Angular v15.1.0-next.0+sha-8e6ec72
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-2ae3614');
+const VERSION = new Version('15.1.0-next.0+sha-8e6ec72');
 
 /**
  * @license
