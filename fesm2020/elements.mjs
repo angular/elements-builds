eadacecbc8ffd8bf1dfa1ae5167279ebcbf441f2
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 19:46:30 +0000

    ec8b52af69 docs(core): add an error details page for unsafe <iframe> bindings (#48027)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-85d9b61
+ * @license Angular v15.1.0-next.0+sha-ec8b52a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-85d9b61');
+const VERSION = new Version('15.1.0-next.0+sha-ec8b52a');
 
 /**
  * @license
