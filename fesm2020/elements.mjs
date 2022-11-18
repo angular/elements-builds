68940a71166627290e9134c8067711c65874081d
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 18:38:07 +0000

    764fa3d9c3 fix(language-service): update packages/language-service/build.sh script to work with vscode-ng-language-service's new Bazel build (#48120)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-414b1b2
+ * @license Angular v15.1.0-next.0+sha-764fa3d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-414b1b2');
+const VERSION = new Version('15.1.0-next.0+sha-764fa3d');
 
 /**
  * @license
