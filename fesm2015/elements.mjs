f3048e9961463ab889e095a58ce186987b8d5496
Joey Perrott <josephperrott.github@gmail.com>
2022-11-22 21:08:52 +0000

    903917b13d build(bazel): remaining AIO Bazel migration fixes

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-2e2135b
+ * @license Angular v15.1.0-next.0+sha-903917b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-2e2135b');
+const VERSION = new Version('15.1.0-next.0+sha-903917b');
 
 /**
  * @license
