3266bc704adcaa8dbcb50292c735206295fc80aa
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:58:29 +0000

    2e2135bf18 docs: add missing documentation setup instructions (#48174)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-4f03a9c
+ * @license Angular v15.1.0-next.0+sha-2e2135b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-4f03a9c');
+const VERSION = new Version('15.1.0-next.0+sha-2e2135b');
 
 /**
  * @license
