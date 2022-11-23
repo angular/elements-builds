986650938ca6ff58eee00d5680ed80b56879177e
Joey Perrott <josephperrott.github@gmail.com>
2022-11-23 21:59:06 +0000

    b258b32f55 build(bazel): copy dist folder to aio for firebase deployment

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-e864254
+ * @license Angular v15.1.0-next.0+sha-b258b32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-e864254');
+const VERSION = new Version('15.1.0-next.0+sha-b258b32');
 
 /**
  * @license
