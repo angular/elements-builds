c001ed6e9bf0455f3b53a762a3f58bbe7ca0aa80
Andrew Kushnir <akushnir@google.com>
2022-12-02 17:19:48 +0000

    6706fab017 perf(docs-infra): remove usage of the NgClass directive (#48312)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-21952bb
+ * @license Angular v15.1.0-next.1+sha-6706fab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-21952bb');
+const VERSION = new Version('15.1.0-next.1+sha-6706fab');
 
 /**
  * @license
