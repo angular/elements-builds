9bf28cd121a1f76b5fd2545d454a531de6a4fabb
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:23:40 +0000

    e44b2b3fe0 docs: removed empty methods (#48343)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-0beeee4
+ * @license Angular v15.1.0-next.1+sha-e44b2b3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-0beeee4');
+const VERSION = new Version('15.1.0-next.1+sha-e44b2b3');
 
 /**
  * @license
