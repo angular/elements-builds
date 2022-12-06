c01756129a74b1e46fcd08fbfea4758b144b1eb7
Andrew Kushnir <akushnir@google.com>
2022-12-06 20:02:24 +0000

    5e2cd07a0b build: update dependency jasmine-spec-reporter to v7 (#48367)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-4c76f94
+ * @license Angular v15.0.2+sha-5e2cd07
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-4c76f94');
+const VERSION = new Version('15.0.2+sha-5e2cd07');
 
 /**
  * @license
