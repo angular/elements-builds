493fce6ae45c854ad1e4ce765b49249c5cc1c6c7
Andrew Kushnir <akushnir@google.com>
2022-12-06 18:18:10 +0000

    f443ee6af7 docs: wrap json property in double quotes (#48375)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-8189b64
+ * @license Angular v15.1.0-next.1+sha-f443ee6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-8189b64');
+const VERSION = new Version('15.1.0-next.1+sha-f443ee6');
 
 /**
  * @license
