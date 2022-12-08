595a8eef7f5bfdc7f659d39c1afd07da27e827c0
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 23:27:20 +0000

    5af7ec244d refactor(router): use `RegExp` to check if a URL is absolute (#48406)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-77ba4aa
+ * @license Angular v15.0.3+sha-5af7ec2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-77ba4aa');
+const VERSION = new Version('15.0.3+sha-5af7ec2');
 
 /**
  * @license
