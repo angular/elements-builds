b0601dff05afd2062d4b664ae889c1a5a7707fc1
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 18:47:55 +0000

    1f3e9d904a refactor(migrations): remove unused migrations (#48414)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-e7707db
+ * @license Angular v15.0.3+sha-1f3e9d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-e7707db');
+const VERSION = new Version('15.0.3+sha-1f3e9d9');
 
 /**
  * @license
