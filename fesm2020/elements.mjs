525790912bd73f16d105f2d0da6f3f81f2c623b7
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 22:27:32 +0000

    ff84c73603 docs(core): Document invalid multi token (#48267)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-279e7d4
+ * @license Angular v15.1.0-next.2+sha-ff84c73
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-279e7d4');
+const VERSION = new Version('15.1.0-next.2+sha-ff84c73');
 
 /**
  * @license
