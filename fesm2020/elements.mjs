082006ecd6a5e25d4df87350cfe546c3e6ac430b
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 21:57:31 +0000

    279e7d4465 refactor: cleanup polyfills by importing zone.js directly in angular.json (#48380)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-2e65a2b
+ * @license Angular v15.1.0-next.2+sha-279e7d4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-2e65a2b');
+const VERSION = new Version('15.1.0-next.2+sha-279e7d4');
 
 /**
  * @license
