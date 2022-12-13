9e77af6d3fbd510084a7be7ff65ca583d1e4d787
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 00:46:04 +0000

    70b2508d22 docs: add docgeni to tooling of resources list (#48061)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-6c1064c
+ * @license Angular v15.0.3+sha-70b2508
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -555,7 +555,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-6c1064c');
+const VERSION = new Version('15.0.3+sha-70b2508');
 
 /**
  * @license
