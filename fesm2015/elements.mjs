d0aa3ed31d43bde0fefc2205970db50258ae8d39
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 16:48:42 +0000

    caedef0f5b fix(compiler-cli): update `@babel/core` dependency and lock version (#48634)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-e38bc6b
+ * @license Angular v15.1.0-next.3+sha-caedef0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-e38bc6b');
+const VERSION = new Version('15.1.0-next.3+sha-caedef0');
 
 /**
  * @module
