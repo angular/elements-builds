ccab07020055e796fca8b97033d8cc75c60a66b9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 13:34:02 +0000

    e38bc6b806 build: switch from `ng-comp-dev` to canonical dev-infra preview Firebase project (#48630)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-50f95f8
+ * @license Angular v15.1.0-next.3+sha-e38bc6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-50f95f8');
+const VERSION = new Version('15.1.0-next.3+sha-e38bc6b');
 
 /**
  * @module
