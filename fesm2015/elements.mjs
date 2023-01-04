9919704e18b2cc335309ca07e864d6678738fc8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:36:36 +0000

    c51e3e33ce build: update eslint dependencies to v5.47.0 (#48548)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-a532d71
+ * @license Angular v15.1.0-next.3+sha-c51e3e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-a532d71');
+const VERSION = new Version('15.1.0-next.3+sha-c51e3e3');
 
 /**
  * @module
