4c679351c17ff605e9de5d38b44bf5c630560f0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 18:10:12 +0000

    3944aa75e9 build: remove unused dependencies (#48641)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-caedef0
+ * @license Angular v15.1.0-next.3+sha-3944aa7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-caedef0');
+const VERSION = new Version('15.1.0-next.3+sha-3944aa7');
 
 /**
  * @module
