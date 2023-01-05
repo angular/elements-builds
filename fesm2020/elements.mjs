603a874006e1e7a02ba0958a357d770f53db8875
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:06:44 +0000

    43ced455d6 docs: remove empty constructors & ngOnInit() (#48576)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-d887d69
+ * @license Angular v15.2.0-next.0+sha-43ced45
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-d887d69');
+const VERSION = new Version('15.2.0-next.0+sha-43ced45');
 
 /**
  * @module
