103f691766de7d17199e615e9814a3c9cbbc6568
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:25:13 +0000

    d4a56721a4 ci: use readonly github token for querying CLI snapshot repo (#48681)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-45029f1
+ * @license Angular v15.2.0-next.0+sha-d4a5672
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-45029f1');
+const VERSION = new Version('15.2.0-next.0+sha-d4a5672');
 
 /**
  * @module
