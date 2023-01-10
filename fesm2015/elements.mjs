fdfe11ef80634707f135522ca384e5d8b31fe3c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:33:54 +0000

    caf7228f8a fix(compiler): resolve deprecation warning (#48652)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-rc.0+sha-e615b59
+ * @license Angular v15.1.0-rc.0+sha-caf7228
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-rc.0+sha-e615b59');
+const VERSION = new Version('15.1.0-rc.0+sha-caf7228');
 
 /**
  * @module
