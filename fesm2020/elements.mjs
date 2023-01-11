e25962b0949b21e55f57a72df7a228a45b5bc20b
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:17:52 +0000

    926c35f4ac docs: Deprecate class and InjectionToken and resolvers (#47924)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-7f68a70
+ * @license Angular v15.2.0-next.0+sha-926c35f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-7f68a70');
+const VERSION = new Version('15.2.0-next.0+sha-926c35f');
 
 /**
  * @module
