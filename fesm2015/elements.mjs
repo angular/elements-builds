4bf43cdf7ea83bfd8a153222989dcdd7219bc0ef
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:19:32 +0000

    cc392e532b refactor(forms): replace type any for the providers (#48647)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-c3ce295
+ * @license Angular v15.1.0+sha-cc392e5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-c3ce295');
+const VERSION = new Version('15.1.0+sha-cc392e5');
 
 /**
  * @module
