ecacc5c5c4fd2bd3b0ea0894fc682413328849f5
Andrew Kushnir <akushnir@google.com>
2023-01-12 20:06:12 +0000

    68ce4f6ab4 fix(common): Update `Location` to get a normalized URL valid in case a represented URL starts with the substring equals `APP_BASE_HREF` (#48489)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-cc392e5
+ * @license Angular v15.1.0+sha-68ce4f6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-cc392e5');
+const VERSION = new Version('15.1.0+sha-68ce4f6');
 
 /**
  * @module
