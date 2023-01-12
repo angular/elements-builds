915dc5397ae0b91a57eeb82215bcdbeeb325aa61
Andrew Kushnir <akushnir@google.com>
2023-01-12 22:07:22 +0000

    dd54f6bd96 fix(core): makeEnvironmentProviders should accept EnvironmentProviders (#48720)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-68ce4f6
+ * @license Angular v15.1.0+sha-dd54f6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-68ce4f6');
+const VERSION = new Version('15.1.0+sha-dd54f6b');
 
 /**
  * @module
