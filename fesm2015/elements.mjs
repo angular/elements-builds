ca6984e9f0c5fcbebc408b69497d1c717b3c242a
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:59:32 +0000

    032b2bd689 perf(common): avoid excessive DOM mutation in NgClass (#48433)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-5978eed
+ * @license Angular v15.1.0+sha-032b2bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-5978eed');
+const VERSION = new Version('15.1.0+sha-032b2bd');
 
 /**
  * @module
