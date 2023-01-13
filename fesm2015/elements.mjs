79c046610ba0fda31fb27301ddd62e12fadb4269
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:57:45 +0000

    5bb1223737 refactor(router): remove unused parameter in navigation internal function (#48724)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-914e7b0
+ * @license Angular v15.2.0-next.0+sha-5bb1223
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-914e7b0');
+const VERSION = new Version('15.2.0-next.0+sha-5bb1223');
 
 /**
  * @module
