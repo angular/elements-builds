7f53bd22ba0bf80096a514546be5d827623311a8
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:38:49 +0000

    fb7b8a95f2 docs: Fix example in breaking changes to apply to correct sentence (#48758)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-7840352
+ * @license Angular v15.2.0-next.0+sha-fb7b8a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-7840352');
+const VERSION = new Version('15.2.0-next.0+sha-fb7b8a9');
 
 /**
  * @module
