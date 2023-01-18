42c3472e2d8bc0a32efa32a85a2b0953675aa4a0
Andrew Kushnir <akushnir@google.com>
2023-01-18 21:46:40 +0000

    9e1e5ccdfe ci: increase payload size for an integration test app (#48772)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-fe6a2bd
+ * @license Angular v15.2.0-next.1+sha-9e1e5cc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-fe6a2bd');
+const VERSION = new Version('15.2.0-next.1+sha-9e1e5cc');
 
 /**
  * @module
