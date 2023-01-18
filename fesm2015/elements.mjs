2b010b2123bb05ac2a5eceda9faa89132572a777
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:05:05 +0000

    fe6a2bd9e2 build: update dependency rimraf to v4 (#48726)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-3a688cb
+ * @license Angular v15.2.0-next.1+sha-fe6a2bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-3a688cb');
+const VERSION = new Version('15.2.0-next.1+sha-fe6a2bd');
 
 /**
  * @module
