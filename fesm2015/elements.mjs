9f883af89acce21ca1044fdbb3154ef02da3bb4b
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 17:54:03 +0000

    60a7214ab2 refactor(bazel): Extract the helper patchNgHostWithFileNameToModuleName to its own file for 1P use (#48739)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-97f6535
+ * @license Angular v15.1.1+sha-60a7214
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-97f6535');
+const VERSION = new Version('15.1.1+sha-60a7214');
 
 /**
  * @module
