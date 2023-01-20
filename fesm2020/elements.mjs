163d0921cfde30388af4026ca10442a609c7a441
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 18:55:39 +0000

    b6e3840ed4 refactor(bazel): remove unnecessary banner stamping code (#48798)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-840530f
+ * @license Angular v15.2.0-next.1+sha-b6e3840
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-840530f');
+const VERSION = new Version('15.2.0-next.1+sha-b6e3840');
 
 /**
  * @module
