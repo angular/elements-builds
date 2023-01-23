2a6a9b9c91ff2ea62c1b64bb6cc7018d6e795f22
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-23 13:52:29 +0000

    f54448c0e9 build: remove unused build optimizer dependency (#48797)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-b6e3840
+ * @license Angular v15.2.0-next.1+sha-f54448c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-b6e3840');
+const VERSION = new Version('15.2.0-next.1+sha-f54448c');
 
 /**
  * @module
