c7038e45fd85bbd4d863154e1af9577e9dc9b224
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 16:51:44 +0000

    82c8b7ae9e build(docs-infra): replace base64 images for stackblitz by svgs (#48774)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-a63983d
+ * @license Angular v15.2.0-next.1+sha-82c8b7a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-a63983d');
+const VERSION = new Version('15.2.0-next.1+sha-82c8b7a');
 
 /**
  * @module
