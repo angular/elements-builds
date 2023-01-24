122651f4b5812e6d12cdd1f4cf7a7de03b854038
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:01:38 +0000

    28da74cffc test: update size golden to reflect Angular cross dependency updates (#48809)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-09053f7
+ * @license Angular v15.2.0-next.1+sha-28da74c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-09053f7');
+const VERSION = new Version('15.2.0-next.1+sha-28da74c');
 
 /**
  * @module
