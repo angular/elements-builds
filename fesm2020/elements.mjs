71dd7529660b3709498920e67f116927c687ae65
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:06:32 +0000

    64416a6e4d docs: Use of $any() has no runtime incidence. (#48785) Some users are woried about `$any()` as function calls are to be avoided in templates. This sentence clarifies the situation.

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-2f83366
+ * @license Angular v15.1.1+sha-64416a6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-2f83366');
+const VERSION = new Version('15.1.1+sha-64416a6');
 
 /**
  * @module
