5c18f16962f02399ff87b3c2d375503ed09a3434
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-01-25 17:07:14 +0000

    0861471112 release: cut the v15.1.2 release

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-2403d3d
+ * @license Angular v15.1.2+sha-0861471
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-2403d3d');
+const VERSION = new Version('15.1.2+sha-0861471');
 
 /**
  * @module
