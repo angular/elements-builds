aefd95fd89cedc9680656371d03dc64ca210cbe4
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:52:31 +0000

    6e197520dd refactor(upgrade): rewrite the downgrade component adapter (#48715)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-0861471
+ * @license Angular v15.1.2+sha-6e19752
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-0861471');
+const VERSION = new Version('15.1.2+sha-6e19752');
 
 /**
  * @module
