633291dcd89a1412d8731b74540a05fd9cc9e2e4
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 19:46:09 +0000

    a334e4efbe fix(common): warn if using ngSrcset without a configured image loader (#48804)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-6e19752
+ * @license Angular v15.1.2+sha-a334e4e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-6e19752');
+const VERSION = new Version('15.1.2+sha-a334e4e');
 
 /**
  * @module
