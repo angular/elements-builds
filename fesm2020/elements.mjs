5924680f4ab6731ab9751039c97ca61ba41a6a36
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 17:15:46 +0000

    ed8f70125e docs(core): add readme for standalone migration (#48848)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-04e0ac3
+ * @license Angular v15.2.0-next.2+sha-ed8f701
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-04e0ac3');
+const VERSION = new Version('15.2.0-next.2+sha-ed8f701');
 
 /**
  * @module
