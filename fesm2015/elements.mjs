cd68b81d81f32822420fc5c6adcb01ec8d7f012d
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 17:00:18 +0000

    9e86dd231b fix(migrations): Fixed file format issue with lint (#48859)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-af31f98
+ * @license Angular v15.1.2+sha-9e86dd2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-af31f98');
+const VERSION = new Version('15.1.2+sha-9e86dd2');
 
 /**
  * @module
