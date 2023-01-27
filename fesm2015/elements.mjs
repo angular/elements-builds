9141865e6992e6f78654ccf9010e3d6aa1c94a8d
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 16:41:20 +0000

    ef6e0b8f65 docs: add this is angular to Community Curations (#48618)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-d36dfd4
+ * @license Angular v15.1.2+sha-ef6e0b8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-d36dfd4');
+const VERSION = new Version('15.1.2+sha-ef6e0b8');
 
 /**
  * @module
