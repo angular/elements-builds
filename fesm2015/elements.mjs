a8f18bd6163c6151bd1d8f7f5cf83a104e917639
Andrew Scott <atscott@google.com>
2023-02-03 17:58:58 +0000

    2ceff3f0c2 refactor(core): remove IE11 only sanitization code (#48438)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-ba38178
+ * @license Angular v15.2.0-next.3+sha-2ceff3f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-ba38178');
+const VERSION = new Version('15.2.0-next.3+sha-2ceff3f');
 
 /**
  * @module
