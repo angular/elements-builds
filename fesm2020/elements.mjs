9a1db3e67e538e0e19f9b95b7dafac8a1e8f2dc1
Andrew Scott <atscott@google.com>
2023-02-06 20:58:02 +0000

    57d0c03e17 docs: Fixing typo error (#48891)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-431ec6c
+ * @license Angular v15.2.0-next.3+sha-57d0c03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-431ec6c');
+const VERSION = new Version('15.2.0-next.3+sha-57d0c03');
 
 /**
  * @module
