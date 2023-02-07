446f0847d521d4ce9f56262199c9b3be1c59ef9c
Andrew Scott <atscott@google.com>
2023-02-07 20:03:16 +0000

    110300e16e build: update eslint dependencies to v5.51.0 (#48983)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.3+sha-03a946d
+ * @license Angular v15.1.3+sha-110300e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.3+sha-03a946d');
+const VERSION = new Version('15.1.3+sha-110300e');
 
 /**
  * @module
