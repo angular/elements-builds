6e36bfe9492ec8b89ab17d46ae08b77f1406aae3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:32:09 +0000

    c7926b5773 fix(migrations): move standalone migrations into imports (#48987)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-845ef71
+ * @license Angular v15.2.0-next.3+sha-c7926b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-845ef71');
+const VERSION = new Version('15.2.0-next.3+sha-c7926b5');
 
 /**
  * @module
