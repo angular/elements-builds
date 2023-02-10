7deb3403302b92b7df9117237f06625d0b95bd16
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:49:50 +0000

    993a43f87c test: update size goldens to reflect devkit update and terser (#48996)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-7af4ea6
+ * @license Angular v15.2.0-next.4+sha-993a43f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-7af4ea6');
+const VERSION = new Version('15.2.0-next.4+sha-993a43f');
 
 /**
  * @module
