1b0357fa05cd0782e8b58187110fb70e08794e3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 08:08:37 +0000

    84ad4d0391 docs: missing closing parenthesis in withNavigationErrorHandler() example (#49013)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-a28cf24
+ * @license Angular v15.2.0-next.4+sha-84ad4d0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-a28cf24');
+const VERSION = new Version('15.2.0-next.4+sha-84ad4d0');
 
 /**
  * @module
