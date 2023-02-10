187975aa371b800cbff8932c73c3e007eb07ee30
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:43:00 +0000

    b24cf027d3 build: remove unused webdriver-manager code (#49009)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-38257a3
+ * @license Angular v15.1.4+sha-b24cf02
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-38257a3');
+const VERSION = new Version('15.1.4+sha-b24cf02');
 
 /**
  * @module
