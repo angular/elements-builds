191ac27d32054426b5c47c25bdbfdcbba02d8e30
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 19:16:33 +0000

    c120fe5eab build: never cache monitoring bazel test target (#49039)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-b022d07
+ * @license Angular v15.1.4+sha-c120fe5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-b022d07');
+const VERSION = new Version('15.1.4+sha-c120fe5');
 
 /**
  * @module
