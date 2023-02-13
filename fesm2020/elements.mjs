5caa275be141318e7bbc30e7d9a2e688a9b972c3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 15:20:01 +0000

    b022d07344 build: update dependency eslint-plugin-jsdoc to v40 (#49037)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-0091947
+ * @license Angular v15.1.4+sha-b022d07
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-0091947');
+const VERSION = new Version('15.1.4+sha-b022d07');
 
 /**
  * @module
