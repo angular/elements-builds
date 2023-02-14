945fd69c2066a1012372dc93ed17f7db1c312d40
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 18:03:00 +0000

    6534a5e3ca docs: remove former team members from bios (#48999)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-f3e3f6e
+ * @license Angular v15.1.4+sha-6534a5e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-f3e3f6e');
+const VERSION = new Version('15.1.4+sha-6534a5e');
 
 /**
  * @module
