e90ef36293ed381f684d2195bb543e55ac693ca9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 17:21:56 +0000

    f3e3f6e455 build: update dependency json5 to v2.2.2 [security] (#48989)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-17c3bd4
+ * @license Angular v15.1.4+sha-f3e3f6e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-17c3bd4');
+const VERSION = new Version('15.1.4+sha-f3e3f6e');
 
 /**
  * @module
