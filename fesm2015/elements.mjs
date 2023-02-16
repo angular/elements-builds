7571577b3277ef4c57385bb7358b5f89f6476d68
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:49:03 +0000

    262895b81a docs: close the developer survey (#49092)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.5+sha-d69ef72
+ * @license Angular v15.1.5+sha-262895b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.5+sha-d69ef72');
+const VERSION = new Version('15.1.5+sha-262895b');
 
 /**
  * @module
