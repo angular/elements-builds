7597690d3776722ab1325d29d593dc68b766f4b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:08:01 +0000

    6b8636fff8 docs(core): typo adding missing comma (#49088) PR Close #49088

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-f3f1399
+ * @license Angular v16.0.0-next.0+sha-6b8636f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-f3f1399');
+const VERSION = new Version('16.0.0-next.0+sha-6b8636f');
 
 /**
  * @module
