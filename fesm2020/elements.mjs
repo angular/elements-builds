838b9553f70d2d5ea8eb518835170b1e1e4f598b
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:34:50 +0000

    9b9c818f99 perf(core): change `RendererType2.styles` to accept a only a flat array (#49072)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-18c7fac
+ * @license Angular v16.0.0-next.0+sha-9b9c818
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-18c7fac');
+const VERSION = new Version('16.0.0-next.0+sha-9b9c818');
 
 /**
  * @module
