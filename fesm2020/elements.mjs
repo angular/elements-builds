55d149e4e72bf06619c5fdbd30af5491d5603fe9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:09:24 +0000

    d2a7245ea7 docs: improve sentence in toh-pt5.md (#49109)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-918e6f0
+ * @license Angular v16.0.0-next.0+sha-d2a7245
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-918e6f0');
+const VERSION = new Version('16.0.0-next.0+sha-d2a7245');
 
 /**
  * @module
