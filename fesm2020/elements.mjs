e42739dc2ddd40065eaad4d9c2fa1633af2e5b58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:14:26 +0000

    07db67b4d7 refactor: restore tree shaking for a warning message (#49064)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-dcd82ba
+ * @license Angular v16.0.0-next.0+sha-07db67b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-dcd82ba');
+const VERSION = new Version('16.0.0-next.0+sha-07db67b');
 
 /**
  * @module
