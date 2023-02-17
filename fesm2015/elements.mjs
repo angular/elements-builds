7398ef1bcf0facffb81d53851f00a61cfa9a5204
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:20:04 +0000

    1f055b90b6 fix(router): Ensure anchor scrolling happens on ignored same URL navigations (#48025)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-48aa96e
+ * @license Angular v16.0.0-next.0+sha-1f055b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-48aa96e');
+const VERSION = new Version('16.0.0-next.0+sha-1f055b9');
 
 /**
  * @module
