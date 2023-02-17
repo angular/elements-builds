0e660bb37ca908a1eb568a2eb880356d539a257d
Andrew Kushnir <akushnir@google.com>
2023-02-17 20:06:56 +0000

    c872426fa9 ci: update AIO payload size tracking (#49130)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-9b9c818
+ * @license Angular v16.0.0-next.0+sha-c872426
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-9b9c818');
+const VERSION = new Version('16.0.0-next.0+sha-c872426');
 
 /**
  * @module
