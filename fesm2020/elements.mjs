cef7c3d0bdcfc306ec3e61ca2ca0a966abae72c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:12:15 +0000

    dcd82ba719 docs: fixing typo in angular compiler options (#49085)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-320f36f
+ * @license Angular v16.0.0-next.0+sha-dcd82ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-320f36f');
+const VERSION = new Version('16.0.0-next.0+sha-dcd82ba');
 
 /**
  * @module
