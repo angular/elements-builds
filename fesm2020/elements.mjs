b9532cda6176509aebdaf7ad2b3301f16c055b11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:21:48 +0000

    9105c41f44 fix(platform-server): avoid duplicate TransferState info after renderApplication call (#49094)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-07db67b
+ * @license Angular v16.0.0-next.0+sha-9105c41
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-07db67b');
+const VERSION = new Version('16.0.0-next.0+sha-9105c41');
 
 /**
  * @module
