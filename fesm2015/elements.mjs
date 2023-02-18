48ea9430476b17305baa94f3073f6af61891155d
Joey Perrott <josephperrott@gmail.com>
2023-02-18 02:53:53 +0000

    8ba8e0dcff build: update to latest remote build execution setup (#49133)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-c872426
+ * @license Angular v16.0.0-next.0+sha-8ba8e0d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-c872426');
+const VERSION = new Version('16.0.0-next.0+sha-8ba8e0d');
 
 /**
  * @module
