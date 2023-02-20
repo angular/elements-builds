a315afb697c4ddc812a4657886f1711aab9e9eb4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-20 15:28:16 +0000

    0950ef6a49 ci: attempt to fix node windows job flakiness (#49142)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-6187888
+ * @license Angular v15.2.0-rc.0+sha-0950ef6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-rc.0+sha-6187888');
+const VERSION = new Version('15.2.0-rc.0+sha-0950ef6');
 
 /**
  * @module
