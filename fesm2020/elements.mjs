e35958801c58360d767c40bdfe34d22b7ddce641
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:50:39 +0000

    03fcb36cfd fix(migrations): migrate HttpClientModule to provideHttpClient() (#48949)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-7b4d3c0
+ * @license Angular v15.2.0-rc.0+sha-03fcb36
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-rc.0+sha-7b4d3c0');
+const VERSION = new Version('15.2.0-rc.0+sha-03fcb36');
 
 /**
  * @module
