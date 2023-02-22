fb5bac936ea062a1ec68ff76e406f330cef59425
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:45:32 +0000

    bc5ddabdcb feat(core): add Angular Signals to the public API (#49150)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-1e32709
+ * @license Angular v16.0.0-next.0+sha-bc5ddab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-1e32709');
+const VERSION = new Version('16.0.0-next.0+sha-bc5ddab');
 
 /**
  * @module
