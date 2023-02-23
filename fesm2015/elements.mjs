1081b71cbfd4787baafddfae1b8f50824f30e55a
Andrew Kushnir <akushnir@google.com>
2023-02-23 18:58:40 +0000

    79cdfeb392 feat(compiler): drop support for TypeScript 4.8 (#49155)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-eb17657
+ * @license Angular v16.0.0-next.0+sha-79cdfeb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-eb17657');
+const VERSION = new Version('16.0.0-next.0+sha-79cdfeb');
 
 /**
  * @module
