54c32bd90e4e941d41b5973e834b43ab52a95cdc
Andrew Kushnir <akushnir@google.com>
2023-02-23 18:46:45 +0000

    760cf4ca9f build: update io_bazel_rules_sass digest to 1f27790 (#49132)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-daae4d8
+ * @license Angular v16.0.0-next.0+sha-760cf4c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-daae4d8');
+const VERSION = new Version('16.0.0-next.0+sha-760cf4c');
 
 /**
  * @module
