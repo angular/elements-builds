d47dac0ee7522d82fa986b3cdc6c0cca1b7ecb4c
Andrew Kushnir <akushnir@google.com>
2023-02-24 16:42:14 +0000

    b6c6dfd278 fix(compiler-cli): do not persist component analysis if template/styles are missing (#49184)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-7dd1957
+ * @license Angular v16.0.0-next.0+sha-b6c6dfd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-7dd1957');
+const VERSION = new Version('16.0.0-next.0+sha-b6c6dfd');
 
 /**
  * @module
