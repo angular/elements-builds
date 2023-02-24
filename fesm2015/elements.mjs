f504662e586b032992d962a97e01316e04e0335b
Andrew Kushnir <akushnir@google.com>
2023-02-24 17:52:58 +0000

    76731ae5c8 refactor(platform-server): Switch to using angular fork of domino (#49189)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-02f4599
+ * @license Angular v16.0.0-next.0+sha-76731ae
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-02f4599');
+const VERSION = new Version('16.0.0-next.0+sha-76731ae');
 
 /**
  * @module
