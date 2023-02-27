29f4bd21ee813e45ebd8b5af6e08a1bf912c0ea6
Andrew Kushnir <akushnir@google.com>
2023-02-27 18:27:17 +0000

    7e35a917c5 fix(router): add error message when using loadComponent with a NgModule (#49164)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-d420ab9
+ * @license Angular v16.0.0-next.0+sha-7e35a91
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-d420ab9');
+const VERSION = new Version('16.0.0-next.0+sha-7e35a91');
 
 /**
  * @module
