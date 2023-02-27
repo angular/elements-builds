a4b54a0b749c12da4bf6c9dce9409d34e9986f22
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:10 +0000

    d1ac3aa14e fix(zone.js): zone-node only patch Promise.prototype.then (#49144)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-27b986e
+ * @license Angular v16.0.0-next.0+sha-d1ac3aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-27b986e');
+const VERSION = new Version('16.0.0-next.0+sha-d1ac3aa');
 
 /**
  * @module
