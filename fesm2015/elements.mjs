92ca27f99b8b5ece1e453dc8a2923790db1a17e9
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:49 +0000

    47ef9e83c7 build: cleanup unused dependencies (#49227)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-d1ac3aa
+ * @license Angular v16.0.0-next.0+sha-47ef9e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-d1ac3aa');
+const VERSION = new Version('16.0.0-next.0+sha-47ef9e8');
 
 /**
  * @module
