1d26e58ecaa26f8e14edc65f98131f15f3f84efa
Andrew Kushnir <akushnir@google.com>
2023-02-27 23:41:34 +0000

    19a4cdec7d docs: New document extracted from the original Communicating with backend services using HTTP document, which is to be retired. (#47937)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-3062442
+ * @license Angular v15.2.0+sha-19a4cde
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-3062442');
+const VERSION = new Version('15.2.0+sha-19a4cde');
 
 /**
  * @module
