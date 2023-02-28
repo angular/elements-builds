f290f70919f3ca5616b19ce1553d825132d7ad28
Andrew Kushnir <akushnir@google.com>
2023-02-28 19:12:53 +0000

    0c805f128e build: update bazel setup (#48240)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-7b9a082
+ * @license Angular v16.0.0-next.0+sha-0c805f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-7b9a082');
+const VERSION = new Version('16.0.0-next.0+sha-0c805f1');
 
 /**
  * @module
