7b85d85248ba4fec75110b3aec550d09882d56d0
Andrew Kushnir <akushnir@google.com>
2023-02-28 21:48:44 +0000

    a08a8ff108 fix(platform-server): bundle @angular/domino in via esbuild (#49229)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-7fd650a
+ * @license Angular v16.0.0-next.0+sha-a08a8ff
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-7fd650a');
+const VERSION = new Version('16.0.0-next.0+sha-a08a8ff');
 
 /**
  * @module
