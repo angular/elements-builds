cf7cba2c652b28d41ab0f338dbfdf999c3dc1091
Andrew Kushnir <akushnir@google.com>
2023-02-28 18:19:39 +0000

    87affadb87 fix(migrations): avoid migrating the same class multiple times in standalone migration (#49245)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-dba747f
+ * @license Angular v16.0.0-next.0+sha-87affad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-dba747f');
+const VERSION = new Version('16.0.0-next.0+sha-87affad');
 
 /**
  * @module
