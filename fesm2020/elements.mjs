929f1108fb13386bbe347d9e16c2ef714e0cac4c
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:31:56 +0000

    75f48fe4a6 build: update eslint dependencies to v5.54.0 (#49243)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-1c64e2b
+ * @license Angular v16.0.0-next.0+sha-75f48fe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-1c64e2b');
+const VERSION = new Version('16.0.0-next.0+sha-75f48fe');
 
 /**
  * @module
