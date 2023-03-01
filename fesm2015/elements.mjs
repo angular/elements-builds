c70477acb8b699598dac941952867dca198a1023
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:08 +0000

    17abe6dc96 refactor(platform-server): remove deprecated `renderModuleFactory` (#49247)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-030060e
+ * @license Angular v16.0.0-next.1+sha-17abe6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-030060e');
+const VERSION = new Version('16.0.0-next.1+sha-17abe6d');
 
 /**
  * @module
