b3b22853e4fb3463f9639138d853cbc5c2420401
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:44:26 +0000

    030060e3dc refactor(core): use lower-level API to annotate `TransferState` class (#49272)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-4e9531f
+ * @license Angular v16.0.0-next.1+sha-030060e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-4e9531f');
+const VERSION = new Version('16.0.0-next.1+sha-030060e');
 
 /**
  * @module
