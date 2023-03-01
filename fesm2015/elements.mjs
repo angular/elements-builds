332908ab2e54cb842568ba500ab999122381a954
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:37:26 +0000

    4e9531f777 feat(core): add `mergeApplicationConfig` method (#49253)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-41bed34
+ * @license Angular v16.0.0-next.1+sha-4e9531f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-41bed34');
+const VERSION = new Version('16.0.0-next.1+sha-4e9531f');
 
 /**
  * @module
