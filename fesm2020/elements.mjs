1e28cbdcb17cf9330ff007c167270ce07ad5d695
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-01 22:57:10 +0000

    8f7fbddb51 refactor: remove duplicate key from component metadata (#49065)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-bca3aed
+ * @license Angular v15.2.1+sha-8f7fbdd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-bca3aed');
+const VERSION = new Version('15.2.1+sha-8f7fbdd');
 
 /**
  * @module
