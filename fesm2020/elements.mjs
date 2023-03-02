3c475c588505d9241a5ce7662c2538f5726d3e96
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 21:10:39 +0000

    d42032a380 refactor(core): Remove `isListLikeIterable` from private export. (#49297)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-ffc6843
+ * @license Angular v15.2.1+sha-d42032a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-ffc6843');
+const VERSION = new Version('15.2.1+sha-d42032a');
 
 /**
  * @module
