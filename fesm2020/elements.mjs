358900e6ed707b5ad82a0c091c0e09b750ad50da
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 20:21:03 +0000

    cc34d5b6ef refactor(core): Application ref/init code cleaning (#49206)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-3aec17e
+ * @license Angular v16.0.0-next.1+sha-cc34d5b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-3aec17e');
+const VERSION = new Version('16.0.0-next.1+sha-cc34d5b');
 
 /**
  * @module
