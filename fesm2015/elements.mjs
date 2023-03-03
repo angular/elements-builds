bd2605d1b636c58a1a40e63d86e04c1bfb14c1ce
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 22:22:49 +0000

    ac59054cd4 refactor(migrations): Move ChangeTracker to common utils (#49308)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-b90b329
+ * @license Angular v15.2.1+sha-ac59054
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-b90b329');
+const VERSION = new Version('15.2.1+sha-ac59054');
 
 /**
  * @module
