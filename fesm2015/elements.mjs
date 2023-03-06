3af8677d52f984c09d877b3c6b381b6f40072718
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 17:16:26 +0000

    27379d004e refactor(platform-browser): remove ununsed functions. (#49302)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-06b0003
+ * @license Angular v16.0.0-next.1+sha-27379d0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-06b0003');
+const VERSION = new Version('16.0.0-next.1+sha-27379d0');
 
 /**
  * @module
