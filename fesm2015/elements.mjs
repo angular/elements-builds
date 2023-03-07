7769e537e7d31de29d9083343e1da38c21b5c51e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 17:56:25 +0000

    e0be553abf docs(forms): change error code in filename to match enum value. (#49344)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-ffccb8e
+ * @license Angular v15.2.1+sha-e0be553
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-ffccb8e');
+const VERSION = new Version('15.2.1+sha-e0be553');
 
 /**
  * @module
