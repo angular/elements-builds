e5b5ffd0d87a92803df0a2f2e591a275a9985a1b
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 19:33:56 +0000

    436050fcaa docs(docs-infra): Split Error by categories & sort by code. (#49343)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-50a1f80
+ * @license Angular v16.0.0-next.1+sha-436050f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-50a1f80');
+const VERSION = new Version('16.0.0-next.1+sha-436050f');
 
 /**
  * @module
