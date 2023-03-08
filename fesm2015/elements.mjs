6c0d5a1a6f920737a18f7d7b3d6e7ae76135a825
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 18:08:39 +0000

    5944f5d21e refactor(core): skip hydration annotation (#49345)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-984d0f4
+ * @license Angular v16.0.0-next.1+sha-5944f5d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-984d0f4');
+const VERSION = new Version('16.0.0-next.1+sha-5944f5d');
 
 /**
  * @module
