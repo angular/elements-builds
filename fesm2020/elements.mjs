538f24102df818f47a3405a5e92ff03e4321fd10
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 22:10:29 +0000

    0e070dc223 docs: update events (#49366)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-3f210d8
+ * @license Angular v16.0.0-next.2+sha-0e070dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-3f210d8');
+const VERSION = new Version('16.0.0-next.2+sha-0e070dc');
 
 /**
  * @module
