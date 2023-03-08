8d6aa96e51131e5a902e52b073a3ef29b5b68947
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 18:16:15 +0000

    c241f63e8d refactor(compiler-cli): remove unused class decorator downlevel code (#49351)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-5944f5d
+ * @license Angular v16.0.0-next.1+sha-c241f63
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-5944f5d');
+const VERSION = new Version('16.0.0-next.1+sha-c241f63');
 
 /**
  * @module
