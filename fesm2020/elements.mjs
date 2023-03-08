385eb14546d003d0c716bee33d990a2df3ab706e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 21:18:40 +0000

    3f210d8720 build: update eslint dependencies to v5.54.1 (#49348)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-8525583
+ * @license Angular v16.0.0-next.2+sha-3f210d8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-8525583');
+const VERSION = new Version('16.0.0-next.2+sha-3f210d8');
 
 /**
  * @module
