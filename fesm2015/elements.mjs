4ab7fd1f916df02167a2fb144fcdbc4bd619b646
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 17:39:15 +0000

    1535ab90da docs: New doc: Understanding communicating with backend services using HTTP (#49294)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-7e23883
+ * @license Angular v15.2.2+sha-1535ab9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.2+sha-7e23883');
+const VERSION = new Version('15.2.2+sha-1535ab9');
 
 /**
  * @module
