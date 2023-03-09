30d4d1e11f464a9c08558a93e7a1ed182cf618d0
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 17:35:19 +0000

    7bde9db4d4 docs: New doc extract from original HTTP doc to be retired. (#48914)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-0a3ac9d
+ * @license Angular v16.0.0-next.2+sha-7bde9db
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-0a3ac9d');
+const VERSION = new Version('16.0.0-next.2+sha-7bde9db');
 
 /**
  * @module
