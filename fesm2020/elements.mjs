33e72e6955fcc8e36ac91ed87e5a0ce3abe761af
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 18:33:57 +0000

    5e7fc259ea feat(service-worker): add function to provide service worker (#48247)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-86fc4d3
+ * @license Angular v16.0.0-next.2+sha-5e7fc25
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-86fc4d3');
+const VERSION = new Version('16.0.0-next.2+sha-5e7fc25');
 
 /**
  * @module
