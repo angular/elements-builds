3abaec6a51e3f3814b977c9911193dd4220a3461
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-13 14:03:44 +0000

    2df92f7633 build: update cross-repo angular dependencies (#49410)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-1535ab9
+ * @license Angular v15.2.2+sha-2df92f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.2+sha-1535ab9');
+const VERSION = new Version('15.2.2+sha-2df92f7');
 
 /**
  * @module
