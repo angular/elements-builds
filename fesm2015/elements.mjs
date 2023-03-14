5b2590973929c779bc98f226dbbaa3f99a62afed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:37:37 +0000

    4e098fa8a7 refactor(core): move Zone providers to a single provider function (#49373)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-5a2b622
+ * @license Angular v16.0.0-next.2+sha-4e098fa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-5a2b622');
+const VERSION = new Version('16.0.0-next.2+sha-4e098fa');
 
 /**
  * @module
