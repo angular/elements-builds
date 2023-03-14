aa334642667abfeff8416fd750f5c16a665ca011
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:59:58 +0000

    f07533f371 docs(upgrade): mark lifecycle methods as nodoc (#49416) PR Close #49416

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-be97c87
+ * @license Angular v16.0.0-next.2+sha-f07533f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-be97c87');
+const VERSION = new Version('16.0.0-next.2+sha-f07533f');
 
 /**
  * @module
