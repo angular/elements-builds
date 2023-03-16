f9c84c9ea2be043b243b532bc7a4321858c6ff21
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:40:19 +0000

    92e41e9cb4 build: update actions/cache digest to 940f3d7 (#49377)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-250f602
+ * @license Angular v16.0.0-next.2+sha-92e41e9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-250f602');
+const VERSION = new Version('16.0.0-next.2+sha-92e41e9');
 
 /**
  * @module
