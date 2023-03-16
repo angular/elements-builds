d87f477ecec65b7bf44846101ecdc1ce8b57aa49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:26:07 +0000

    cf98777153 test: update tests to remove deprecated `withServerTransition` (#49422)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-64067a1
+ * @license Angular v16.0.0-next.2+sha-cf98777
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-64067a1');
+const VERSION = new Version('16.0.0-next.2+sha-cf98777');
 
 /**
  * @module
