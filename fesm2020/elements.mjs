431c732d75a7fccb80cf55bec48db996731afe97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:26:20 +0000

    bf2a5d5e04 build: update io_bazel_rules_sass digest to 3d2ad47 (#49428)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-cf98777
+ * @license Angular v16.0.0-next.2+sha-bf2a5d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-cf98777');
+const VERSION = new Version('16.0.0-next.2+sha-bf2a5d5');
 
 /**
  * @module
