52d6aaf1209bf5733af48c65556c199bcaa7d6dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 07:54:09 +0000

    c807a324d1 build: update actions/checkout digest to 24cb908 (#49436)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.3+sha-3d71d9d
+ * @license Angular v15.2.3+sha-c807a32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.3+sha-3d71d9d');
+const VERSION = new Version('15.2.3+sha-c807a32');
 
 /**
  * @module
