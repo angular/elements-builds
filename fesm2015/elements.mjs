eb36e1e3eda4e62605a1d2ddaa3912c554f23b51
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 15:31:33 +0000

    087f4412af fix(core): more accurate matching of classes during content projection (#48888)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.3+sha-0f0c0a9
+ * @license Angular v15.2.3+sha-087f441
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.3+sha-0f0c0a9');
+const VERSION = new Version('15.2.3+sha-087f441');
 
 /**
  * @module
