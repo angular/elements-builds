b57dd35609aca4e0194414e87bf7a14b4e202607
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 14:29:17 +0000

    e2d3da5c71 test: update i18n size golden to reflect `main` (#49514)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-81b9e85
+ * @license Angular v16.0.0-next.3+sha-e2d3da5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-81b9e85');
+const VERSION = new Version('16.0.0-next.3+sha-e2d3da5');
 
 /**
  * @module
