4c62b2a249aa6020586c80513e58fe5420d231a6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:18:18 +0000

    ab0b3622a5 docs: remove ticks in code sample to generate components (#49525)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-69a6e09
+ * @license Angular v16.0.0-next.3+sha-ab0b362
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-69a6e09');
+const VERSION = new Version('16.0.0-next.3+sha-ab0b362');
 
 /**
  * @module
