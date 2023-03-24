873b7be7507eb8997848ac57e66f248c555da098
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-24 13:05:56 +0000

    29c682ce5b ci: update github.com known host given recent RSA update (#49580)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.4+sha-3f8e4fb
+ * @license Angular v15.2.4+sha-29c682c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.4+sha-3f8e4fb');
+const VERSION = new Version('15.2.4+sha-29c682c');
 
 /**
  * @module
