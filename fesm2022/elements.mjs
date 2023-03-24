1bbba73b9c8039d0af8791e308272e4d07e19728
Andrew Scott <atscott01@gmail.com>
2023-03-24 17:47:20 +0000

    71def16598 build: continue publishing `@angular/bazel` snapshot builds (#49583)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-22a6412
+ * @license Angular v16.0.0-next.4+sha-71def16
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-22a6412');
+const VERSION = new Version('16.0.0-next.4+sha-71def16');
 
 /**
  * @module
