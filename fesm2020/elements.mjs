5afaad880275bf2431168100c9f3f84b88ea011b
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:59:05 +0000

    0fecbabfd2 ci: update pullapprove config to reflect current availability  (#49592)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.4+sha-978d37f
+ * @license Angular v15.2.4+sha-0fecbab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.4+sha-978d37f');
+const VERSION = new Version('15.2.4+sha-0fecbab');
 
 /**
  * @module
