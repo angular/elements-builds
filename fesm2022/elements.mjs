655610eb598243106348d6e06db12b1d0689d132
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:32:14 +0000

    e99460865e refactor(platform-server): deprecate `useAbsoluteUrl` and `baseUrl` (#49546)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-ef149de
+ * @license Angular v16.0.0-next.4+sha-e994608
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-ef149de');
+const VERSION = new Version('16.0.0-next.4+sha-e994608');
 
 /**
  * @module
