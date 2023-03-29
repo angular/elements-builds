741d4c904eec1690b1c834e1bffdf050f9998d6b
Andrew Scott <atscott01@gmail.com>
2023-03-29 01:58:07 +0000

    ef149defb9 refactor(core): expose `ReactiveNode` privately on signal functions (#49529)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-84a0fa3
+ * @license Angular v16.0.0-next.4+sha-ef149de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-84a0fa3');
+const VERSION = new Version('16.0.0-next.4+sha-ef149de');
 
 /**
  * @module
