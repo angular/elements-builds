7c462546b2978f13a3d58ea5a3d1a26ae31365bc
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:43:35 +0000

    e8e36811d5 fix(platform-browser): set nonce attribute in a platform compatible way (#49624)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-e994608
+ * @license Angular v16.0.0-next.4+sha-e8e3681
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-e994608');
+const VERSION = new Version('16.0.0-next.4+sha-e8e3681');
 
 /**
  * @module
