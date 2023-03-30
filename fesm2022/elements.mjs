5fb28f2bb1b2bb453036530b1d4485b0e17233f6
Andrew Scott <atscott01@gmail.com>
2023-03-30 16:48:23 +0000

    06e74a5583 refactor(core): run effects during change detection (#49641)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-f2ba192
+ * @license Angular v16.0.0-next.5+sha-06e74a5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-f2ba192');
+const VERSION = new Version('16.0.0-next.5+sha-06e74a5');
 
 /**
  * @module
