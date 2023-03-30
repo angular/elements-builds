1c399066eb1125604f32e1f7d9477ca7c546fc16
Andrew Scott <atscott01@gmail.com>
2023-03-30 16:55:59 +0000

    337922709a docs(core): fix view hierarchy links (#49629)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-d596ec0
+ * @license Angular v16.0.0-next.5+sha-3379227
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-d596ec0');
+const VERSION = new Version('16.0.0-next.5+sha-3379227');
 
 /**
  * @module
