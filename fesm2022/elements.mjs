13633de599678df7a17f53479386ae29a94a539d
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 18:54:13 +0000

    39a648f31c refactor(core): Use (where applicable), `ViewContainer.createComponent` instead of relying on the `ComponentFactoryResolver`. (#49614)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-9d96487
+ * @license Angular v16.0.0-next.5+sha-39a648f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-9d96487');
+const VERSION = new Version('16.0.0-next.5+sha-39a648f');
 
 /**
  * @module
