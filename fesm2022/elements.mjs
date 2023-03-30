e2ccb595916e43d279c9dd2aa75f29b8d09bbedc
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 23:27:05 +0000

    e9dd7f0028 refactor(core): Prevent reads and writes to signals at certain times (#49631)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-07a1aa3
+ * @license Angular v16.0.0-next.5+sha-e9dd7f0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-07a1aa3');
+const VERSION = new Version('16.0.0-next.5+sha-e9dd7f0');
 
 /**
  * @module
