08711ac1c57f85e8806540a1f6def6c8844112c9
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:05:48 +0000

    6c1895f5da refactor(core): avoid incrementing a dev mode counter unnecessarily (#49656)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-de55fdf
+ * @license Angular v16.0.0-next.5+sha-6c1895f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-de55fdf');
+const VERSION = new Version('16.0.0-next.5+sha-6c1895f');
 
 /**
  * @module
