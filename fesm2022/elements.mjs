3e32c4b61c34f73bc3ef92a947648191106174f6
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:09:48 +0000

    01d4a136ed build: update eslint dependencies to v5.57.0 (#49608)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-12b2caa
+ * @license Angular v16.0.0-next.5+sha-01d4a13
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -515,7 +515,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-12b2caa');
+const VERSION = new Version('16.0.0-next.5+sha-01d4a13');
 
 /**
  * @module
