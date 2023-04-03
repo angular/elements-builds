eb6f89bc0992399a3da93c94db8167e590d205fa
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 05:35:24 +0000

    a6def648f5 docs: fix typo (#49664) PR Close #49664

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.5+sha-9b61379
+ * @license Angular v15.2.5+sha-a6def64
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.5+sha-9b61379');
+const VERSION = new Version('15.2.5+sha-a6def64');
 
 /**
  * @module
