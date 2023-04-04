fa43390de8d1bea9bc3e0511e48f35ffd52ed41a
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:37:56 +0000

    61bedaf930 refactor(core): handle a case when ngSkipHydration is applied to the root node of an application (#49675)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-4b673d5
+ * @license Angular v16.0.0-next.6+sha-61bedaf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-4b673d5');
+const VERSION = new Version('16.0.0-next.6+sha-61bedaf');
 
 /**
  * @module
