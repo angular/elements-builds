e2022408bff2c2032b5b758515f32f9bf326cdd6
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:32:54 +0000

    aff1512950 feat(http): allow `HttpClient` to cache requests (#49509)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-ba3e9ea
+ * @license Angular v16.0.0-next.6+sha-aff1512
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-ba3e9ea');
+const VERSION = new Version('16.0.0-next.6+sha-aff1512');
 
 /**
  * @module
