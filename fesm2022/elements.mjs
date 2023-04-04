6730c96ad174d5256a345123161c5da6f8f7ab4a
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:28:27 +0000

    d11f5d70d3 build: update dependency google-closure-compiler to v20230228 (#49663)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-85ca1f9
+ * @license Angular v16.0.0-next.6+sha-d11f5d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-85ca1f9');
+const VERSION = new Version('16.0.0-next.6+sha-d11f5d7');
 
 /**
  * @module
