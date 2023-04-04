fb0309e86602823b2bda0e01155597133623087c
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:35:00 +0000

    cb23864472 docs: update Universal guide with hydration information (#49694)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-e7463dc
+ * @license Angular v16.0.0-next.6+sha-cb23864
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-e7463dc');
+const VERSION = new Version('16.0.0-next.6+sha-cb23864');
 
 /**
  * @module
