4238fe52e1c13dd1aa66b6b378c4f0ac3cab5ac9
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:32:31 +0000

    ba3e9eac92 feat(devtools): Improve Set support in component properties. (#49316)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-719f6a0
+ * @license Angular v16.0.0-next.6+sha-ba3e9ea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-719f6a0');
+const VERSION = new Version('16.0.0-next.6+sha-ba3e9ea');
 
 /**
  * @module
