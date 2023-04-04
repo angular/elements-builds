cd4dc3240ee8c0d99c8b0f255599a0c84b1f5a0c
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:29:18 +0000

    e7463dc80f docs: tweak hydration guide from feedback (#49705)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-edc3bb1
+ * @license Angular v16.0.0-next.6+sha-e7463dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-edc3bb1');
+const VERSION = new Version('16.0.0-next.6+sha-e7463dc');
 
 /**
  * @module
