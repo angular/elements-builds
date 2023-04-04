21b655bcfe2b2b0a04fea4844bf6972aa908b18c
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 18:39:30 +0000

    edc3bb180f Revert "feat(forms): Improve typings form (async)Validators" (#49706)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-761e02d
+ * @license Angular v16.0.0-next.6+sha-edc3bb1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-761e02d');
+const VERSION = new Version('16.0.0-next.6+sha-edc3bb1');
 
 /**
  * @module
