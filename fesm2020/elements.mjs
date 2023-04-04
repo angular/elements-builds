e4bae7a6c12405e521f1cd0b308771b26c31925e
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:40:52 +0000

    bd82fbeec4 docs: fix typo (#49669)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.5+sha-a6def64
+ * @license Angular v15.2.5+sha-bd82fbe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.5+sha-a6def64');
+const VERSION = new Version('15.2.5+sha-bd82fbe');
 
 /**
  * @module
