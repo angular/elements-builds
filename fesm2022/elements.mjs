df56d7e46ba23e84bbba4cb7cf4590dac690f3bd
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 00:00:02 +0000

    3b863ddc1e refactor(core): Remove `ReflectiveInjector` symbol (#48103)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-34b2d34
+ * @license Angular v16.0.0-next.6+sha-3b863dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-34b2d34');
+const VERSION = new Version('16.0.0-next.6+sha-3b863dd');
 
 /**
  * @module
