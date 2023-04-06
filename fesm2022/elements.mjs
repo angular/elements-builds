e3e95444d466a33c95fbc9d9dd5cb0b412b51207
Dylan Hunn <github@dylanhunn.com>
2023-04-06 02:56:52 +0000

    a7479545db docs: release notes for the v15.2.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-40ed152
+ * @license Angular v16.0.0-next.6+sha-a747954
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-40ed152');
+const VERSION = new Version('16.0.0-next.6+sha-a747954');
 
 /**
  * @module
