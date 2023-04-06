fdaeb5135b196ccbcfe71e5b038d79ee4ba609ca
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:19:50 +0000

    fe58f0a383 refactor(core): update the link in hydration stats message (#49728)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-e08d679
+ * @license Angular v16.1.0-next.0+sha-fe58f0a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-e08d679');
+const VERSION = new Version('16.1.0-next.0+sha-fe58f0a');
 
 /**
  * @module
