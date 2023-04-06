792218b6669befbae757d1d699f789b76324d84c
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:19:12 +0000

    e08d6797ad refactor(core): skip hydration for components that use ShadowDom encapsulation (#49722)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-bf1b5b4
+ * @license Angular v16.1.0-next.0+sha-e08d679
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-bf1b5b4');
+const VERSION = new Version('16.1.0-next.0+sha-e08d679');
 
 /**
  * @module
