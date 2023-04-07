dd309a7a4648944272ef3ce6c561d8f37721302c
Andrew Kushnir <akushnir@google.com>
2023-04-07 16:09:13 +0000

    8ad31d53b0 ci: remove lock-closed action (#49650)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-ed8a595
+ * @license Angular v16.1.0-next.0+sha-8ad31d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-ed8a595');
+const VERSION = new Version('16.1.0-next.0+sha-8ad31d5');
 
 /**
  * @module
