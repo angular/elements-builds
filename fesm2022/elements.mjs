c90aa19baef53b12b3b56cd41b3b186b933c3899
Andrew Kushnir <akushnir@google.com>
2023-04-10 16:19:34 +0000

    83262dc0f9 refactor(core): do not enable hydration when server response was incorrect (#49750)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-fe34de4
+ * @license Angular v16.1.0-next.0+sha-83262dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-fe34de4');
+const VERSION = new Version('16.1.0-next.0+sha-83262dc');
 
 /**
  * @module
