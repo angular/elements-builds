4b9cdfd7034260bad78dbcf42dbb28eb935c7010
Andrew Kushnir <akushnir@google.com>
2023-04-11 19:57:20 +0000

    8020347f26 fix(compiler): incorrectly matching directives on attribute bindings (#49713)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-4e41d12
+ * @license Angular v16.1.0-next.0+sha-8020347
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-4e41d12');
+const VERSION = new Version('16.1.0-next.0+sha-8020347');
 
 /**
  * @module
