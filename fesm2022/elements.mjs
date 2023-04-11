a7ef8705af82430be8ec86ed6ecbc3836dd2ecdb
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:02:56 +0000

    2eb9b8b402 fix(http): wait for all XHR requests to finish before stabilizing application (#49776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.7+sha-f8e2586
+ * @license Angular v16.0.0-next.7+sha-2eb9b8b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.7+sha-f8e2586');
+const VERSION = new Version('16.0.0-next.7+sha-2eb9b8b');
 
 /**
  * @module
