348b263227ed6f4c977f448871bb4333e5fe4fe3
Andrew Kushnir <akushnir@google.com>
2023-04-11 20:06:34 +0000

    f8e25864e8 fix(core): allow async functions in effects (#49783)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.7+sha-d980af6
+ * @license Angular v16.0.0-next.7+sha-f8e2586
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.7+sha-d980af6');
+const VERSION = new Version('16.0.0-next.7+sha-f8e2586');
 
 /**
  * @module
