ade747eaf669d14b3b8f57289ab2c11cb1182857
Andrew Kushnir <akushnir@google.com>
2023-04-11 18:03:06 +0000

    687f13884b ci: disable size tracking CI job in favor of integration tests size checks (#49788)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.7+sha-a46b761
+ * @license Angular v16.0.0-next.7+sha-687f138
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.7+sha-a46b761');
+const VERSION = new Version('16.0.0-next.7+sha-687f138');
 
 /**
  * @module
