4371dde490524fedeb17b0f1534eab91a393be12
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:57:15 +0000

    fd13e11e75 test: improve testing of HTTP and transfer state cache (#49810)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.7+sha-15c91a5
+ * @license Angular v16.0.0-next.7+sha-fd13e11
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.7+sha-15c91a5');
+const VERSION = new Version('16.0.0-next.7+sha-fd13e11');
 
 /**
  * @module
