1f430a47022276ce6c74218673641b0747f81e34
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:43:53 +0000

    2c140f4871 build: update github/codeql-action action to v2.2.11 (#49775)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.6+sha-b0c1a90
+ * @license Angular v15.2.6+sha-2c140f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.6+sha-b0c1a90');
+const VERSION = new Version('15.2.6+sha-2c140f4');
 
 /**
  * @module
