7bf280ab29794b15ed5d9dadf16b9e7b349231ff
Andrew Kushnir <akushnir@google.com>
2023-04-12 20:02:59 +0000

    5ac8ca4f55 fix(core): error if document body is null (#49818)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-577374f
+ * @license Angular v16.1.0-next.0+sha-5ac8ca4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-577374f');
+const VERSION = new Version('16.1.0-next.0+sha-5ac8ca4');
 
 /**
  * @module
