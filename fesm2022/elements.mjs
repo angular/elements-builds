c8d3f9ddd8ce03bfcdc34232abbf2fce90f9de3f
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:51:51 +0000

    d966fdd438 refactor(router): Ensure data is bound to components in change detection following navigation (#49741)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-c190fbb
+ * @license Angular v16.1.0-next.0+sha-d966fdd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-c190fbb');
+const VERSION = new Version('16.1.0-next.0+sha-d966fdd');
 
 /**
  * @module
