532efe65e944683e0f812d82a26b3541c4a87cca
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 21:04:17 +0000

    6ade3ec8c3 refactor(core): add asReadonly helper to writable signals (#49802)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.0+sha-87549af
+ * @license Angular v16.0.0-rc.0+sha-6ade3ec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.0+sha-87549af');
+const VERSION = new Version('16.0.0-rc.0+sha-6ade3ec');
 
 /**
  * @module
