cf4ef330736559f52c4a2bb8076d916900de8ea9
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:21:50 +0000

    294fae02eb refactor(core): fix privately imported symbol from signals package (#49812)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-7fd0b67
+ * @license Angular v16.1.0-next.0+sha-294fae0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-7fd0b67');
+const VERSION = new Version('16.1.0-next.0+sha-294fae0');
 
 /**
  * @module
