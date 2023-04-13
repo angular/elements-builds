f99c8102961057c5cc10a86bbfe4713633b1fb15
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:19:28 +0000

    7fd0b678f3 refactor(core): avoid deep links into `@angular/core` (#49823)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-54f18c8
+ * @license Angular v16.1.0-next.0+sha-7fd0b67
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-54f18c8');
+const VERSION = new Version('16.1.0-next.0+sha-7fd0b67');
 
 /**
  * @module
