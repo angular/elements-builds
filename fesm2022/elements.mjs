961221e91b1b9ce9d71f10531d245df4c067d94d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 19:27:08 +0000

    87549af73c fix(core): Fix capitalization of toObservableOptions (#49832)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.0+sha-970e8b4
+ * @license Angular v16.0.0-rc.0+sha-87549af
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.0+sha-970e8b4');
+const VERSION = new Version('16.0.0-rc.0+sha-87549af');
 
 /**
  * @module
