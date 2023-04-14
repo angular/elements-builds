e98fd8961bb7348118db87fdf0de04de15bea7df
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 18:25:50 +0000

    9e9b424332 test(router): enable a subset of bootstrapping test to ran on node (#49843)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-cbca581
+ * @license Angular v16.1.0-next.0+sha-9e9b424
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-cbca581');
+const VERSION = new Version('16.1.0-next.0+sha-9e9b424');
 
 /**
  * @module
