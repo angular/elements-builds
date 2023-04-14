430961db80fcab61a9a9e23412fd60ace280387a
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 15:11:20 +0000

    cbca5817d8 fix(router): canceledNavigationResolution: 'computed' with redirects to the current URL (#49793)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-b4e83ca
+ * @license Angular v16.1.0-next.0+sha-cbca581
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-b4e83ca');
+const VERSION = new Version('16.1.0-next.0+sha-cbca581');
 
 /**
  * @module
