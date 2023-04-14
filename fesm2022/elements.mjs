81bf1bf0e0fc7cdacb85b802a996b5cf441d7601
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 19:44:14 +0000

    449c011ae4 refactor(platform-server): simplify `_render` method (#49840)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-e594d1c
+ * @license Angular v16.0.0-rc.1+sha-449c011
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-e594d1c');
+const VERSION = new Version('16.0.0-rc.1+sha-449c011');
 
 /**
  * @module
