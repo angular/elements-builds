6e391deceedf689712fddb3c2a980f3a301a5c06
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 14:31:38 +0000

    b4e83caba9 refactor(core): Do not need to set lView slots to null in createLView (#49836)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-aebf115
+ * @license Angular v16.1.0-next.0+sha-b4e83ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-aebf115');
+const VERSION = new Version('16.1.0-next.0+sha-b4e83ca');
 
 /**
  * @module
