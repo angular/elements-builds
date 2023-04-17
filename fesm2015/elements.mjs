73ad62a9caa8f4788a2921784ed208f0224537b4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:50:10 +0000

    79a8b3c049 docs: Specify when an app is debuggable by the DevTools. (#48970)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-628c520
+ * @license Angular v15.2.7+sha-79a8b3c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-628c520');
+const VERSION = new Version('15.2.7+sha-79a8b3c');
 
 /**
  * @module
