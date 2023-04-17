ba8fb4e0862b8cfff174405b4136a5682a26db6a
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:54:03 +0000

    f37cb477a8 refactor(core): log hydration setup warning in dev mode only (#49876)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-d994f85
+ * @license Angular v16.1.0-next.0+sha-f37cb47
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-d994f85');
+const VERSION = new Version('16.1.0-next.0+sha-f37cb47');
 
 /**
  * @module
