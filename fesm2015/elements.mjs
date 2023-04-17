4a7d8f3d8884bdcfc0780d309a531fe51d06794b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:22:36 +0000

    1017630d04 refactor(docs-infra): cleanup deprecated code (#49671)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-a63d9a3
+ * @license Angular v15.2.7+sha-1017630
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-a63d9a3');
+const VERSION = new Version('15.2.7+sha-1017630');
 
 /**
  * @module
