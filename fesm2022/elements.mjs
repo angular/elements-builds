5a7346133252625bffc531c3e68461ce16ed794d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:43:00 +0000

    8f3fca03d2 build: update PullApprove config (#49878)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8926b55
+ * @license Angular v16.1.0-next.0+sha-8f3fca0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8926b55');
+const VERSION = new Version('16.1.0-next.0+sha-8f3fca0');
 
 /**
  * @module
