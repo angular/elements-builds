1bd0c15fffcd6162aa6c233e641d385bf24ebad0
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:39:18 +0000

    2aa6d6d616 refactor(compiler-cli): cleanup inferences (#49863)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8f28f19
+ * @license Angular v16.1.0-next.0+sha-2aa6d6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8f28f19');
+const VERSION = new Version('16.1.0-next.0+sha-2aa6d6d');
 
 /**
  * @module
