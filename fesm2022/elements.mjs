12968e19ecce8df3e5e02f9ad6384dce185180f4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:54:27 +0000

    bf2e11c035 refactor(core): throw an error when `APP_INITIALIZER` token is not an array. (#49860)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-f37cb47
+ * @license Angular v16.1.0-next.0+sha-bf2e11c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-f37cb47');
+const VERSION = new Version('16.1.0-next.0+sha-bf2e11c');
 
 /**
  * @module
