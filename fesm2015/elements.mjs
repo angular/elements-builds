ed76b12e5ebc87979f21ec7c661e59103fc5d5ca
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:42:50 +0000

    a63d9a30d8 build: update actions/checkout digest to 8e5e7e5 (#49815)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-09a42d9
+ * @license Angular v15.2.7+sha-a63d9a3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-09a42d9');
+const VERSION = new Version('15.2.7+sha-a63d9a3');
 
 /**
  * @module
