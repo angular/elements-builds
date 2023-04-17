f4da2d2b99f24f688088a1e396e84f4ae7892503
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 16:03:16 +0000

    a4c768cad6 build: update dependency eslint-plugin-jsdoc to v41 (#49885)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-28c789b
+ * @license Angular v16.0.0-rc.1+sha-a4c768c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-28c789b');
+const VERSION = new Version('16.0.0-rc.1+sha-a4c768c');
 
 /**
  * @module
