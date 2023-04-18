030348e5ef1a85bee6571d68b71ed41482645e29
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 21:51:45 +0000

    296459ac16 docs: add Artur Androsovych to GDE resources (#48520)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-279e9e7
+ * @license Angular v15.2.7+sha-296459a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-279e9e7');
+const VERSION = new Version('15.2.7+sha-296459a');
 
 /**
  * @module
