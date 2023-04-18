77a3aaa147b761be21d6bb6acf3160e906864cca
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:20:19 +0000

    fb1161f2ec docs: type clearTimer function as VoidFunction or undefined (#48480)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-0d9705b
+ * @license Angular v16.1.0-next.0+sha-fb1161f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-0d9705b');
+const VERSION = new Version('16.1.0-next.0+sha-fb1161f');
 
 /**
  * @module
