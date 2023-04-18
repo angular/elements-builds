6bc61777cc02c519a72afe8f68b4b76774eb06f1
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:18:33 +0000

    8219857821 refactor(platform-browser): cleanup references to dart (#49858)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-003e7d1
+ * @license Angular v16.0.0-rc.1+sha-8219857
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-003e7d1');
+const VERSION = new Version('16.0.0-rc.1+sha-8219857');
 
 /**
  * @module
