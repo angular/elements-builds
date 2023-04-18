08b99a5a60e49ed30b33bafedc236f9fbbbd219a
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 21:07:19 +0000

    279e9e7fdb build: update dependency build_bazel_rules_nodejs to v5.8.2 (#49910)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-6a93956
+ * @license Angular v15.2.7+sha-279e9e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-6a93956');
+const VERSION = new Version('15.2.7+sha-279e9e7');
 
 /**
  * @module
