49ed8ff5ea52ef674beff37a4ba7b8ad29271382
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:57:07 +0000

    ded7ede1d4 test: update platform-server integration tests to use the Angular CLI (#49900)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-5e4797c
+ * @license Angular v16.0.0-rc.1+sha-ded7ede
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-5e4797c');
+const VERSION = new Version('16.0.0-rc.1+sha-ded7ede');
 
 /**
  * @module
