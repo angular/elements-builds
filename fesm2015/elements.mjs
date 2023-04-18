f87e215ca485f1c417814d1c28a4fe9637eb310b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 17:16:58 +0000

    cab701d0ee docs: update README.md (#48423)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-5a13eb8
+ * @license Angular v15.2.7+sha-cab701d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-5a13eb8');
+const VERSION = new Version('15.2.7+sha-cab701d');
 
 /**
  * @module
