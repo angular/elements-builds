3cc187420f5037e100e379570782ec036c0ff1f7
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 17:18:12 +0000

    ead10aff41 refactor(compiler): next context merging in the template pipeline (#49797)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-27c624e
+ * @license Angular v16.0.0-rc.1+sha-ead10af
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-27c624e');
+const VERSION = new Version('16.0.0-rc.1+sha-ead10af');
 
 /**
  * @module
