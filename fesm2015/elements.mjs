c7992233b1840ef75d8b2f6ed4da85751c54a577
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:19:29 +0000

    bce7f6bfd9 refactor(core): Remove ununsed Zone mock from testing internals. (#49873)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-1d4cf95
+ * @license Angular v15.2.7+sha-bce7f6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-1d4cf95');
+const VERSION = new Version('15.2.7+sha-bce7f6b');
 
 /**
  * @module
