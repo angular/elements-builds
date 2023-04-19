be38489a2aaa5fd68c98143b5ce2d1b3b102bbfb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:44:03 +0000

    338c7d9125 test(core): adding extra tests for hydration of view containers located after <ng-content> slots (#49920)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-efb3c68
+ * @license Angular v16.1.0-next.0+sha-338c7d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-efb3c68');
+const VERSION = new Version('16.1.0-next.0+sha-338c7d9');
 
 /**
  * @module
