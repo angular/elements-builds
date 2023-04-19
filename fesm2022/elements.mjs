b53f624533796291883e75f8b68c768309cd6322
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 15:07:09 +0000

    a175ac5441 test: update platform-server tests to test both ngmodule and standalone application (#49927)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-21b834f
+ * @license Angular v16.0.0-rc.1+sha-a175ac5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-21b834f');
+const VERSION = new Version('16.0.0-rc.1+sha-a175ac5');
 
 /**
  * @module
