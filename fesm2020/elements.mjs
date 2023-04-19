dea39e3db041406c166015b528451d53222d8a7a
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 16:46:27 +0000

    2fff8fadbe fix(core): handle invalid classes in class array bindings (#49924)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-0454158
+ * @license Angular v15.2.7+sha-2fff8fa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-0454158');
+const VERSION = new Version('15.2.7+sha-2fff8fa');
 
 /**
  * @module
