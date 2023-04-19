bbce287596824a57510cac04b384ce222de608dc
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 18:29:42 +0000

    f8b95b9da6 fix(core): execute query setters in non-reactive context (#49906)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-a3c3ab7
+ * @license Angular v16.0.0-rc.1+sha-f8b95b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-a3c3ab7');
+const VERSION = new Version('16.0.0-rc.1+sha-f8b95b9');
 
 /**
  * @module
