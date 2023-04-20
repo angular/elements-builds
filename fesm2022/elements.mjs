3df057c4a4581b2cf95b0d81c9edddf1bea6c23f
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:05:17 +0000

    bbc2efcda2 refactor(core): hydrate components of the same type used with and without ngSkipHydration (#49943)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-da1ed98
+ * @license Angular v16.1.0-next.0+sha-bbc2efc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-da1ed98');
+const VERSION = new Version('16.1.0-next.0+sha-bbc2efc');
 
 /**
  * @module
