833274995138699295149a8bd733fb3d4fdb1546
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:47:56 +0000

    da681ee712 refactor(core): throw an error when `APP_INITIALIZER` token is not an array. (#49860)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.2+sha-0ade007
+ * @license Angular v16.0.0-rc.2+sha-da681ee
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.2+sha-0ade007');
+const VERSION = new Version('16.0.0-rc.2+sha-da681ee');
 
 /**
  * @module
