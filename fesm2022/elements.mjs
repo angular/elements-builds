40ca30f718942af9283245aee77128a29d363cfc
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:38:43 +0000

    eb5bc95dbf docs: remove the warning about shadowDom support (#49961)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-345dd6d
+ * @license Angular v16.1.0-next.0+sha-eb5bc95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-345dd6d');
+const VERSION = new Version('16.1.0-next.0+sha-eb5bc95');
 
 /**
  * @module
