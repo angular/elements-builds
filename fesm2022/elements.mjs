f1dc8fca146658c035dfe523cd8797d392387c48
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:38:32 +0000

    345dd6d81a refactor(compiler-cli): Add experimental local compilation mode. (#49846)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-2487d41
+ * @license Angular v16.1.0-next.0+sha-345dd6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-2487d41');
+const VERSION = new Version('16.1.0-next.0+sha-345dd6d');
 
 /**
  * @module
