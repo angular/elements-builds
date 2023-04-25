2961467011a21bea15d337140fa9220cc3d9a268
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:55 +0000

    f5dd3b4ede refactor(docs-infra): remove `semi` rule from eslintrc. (#49970)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-1090bf8
+ * @license Angular v16.1.0-next.0+sha-f5dd3b4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-1090bf8');
+const VERSION = new Version('16.1.0-next.0+sha-f5dd3b4');
 
 /**
  * @module
