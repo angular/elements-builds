b0e02d668ddb33a5522cf42652c56f4dc1d436b6
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:29 +0000

    bdf2aa1957 build: bump license year (#49984)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-1ef6205
+ * @license Angular v15.2.8+sha-bdf2aa1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-1ef6205');
+const VERSION = new Version('15.2.8+sha-bdf2aa1');
 
 /**
  * @module
