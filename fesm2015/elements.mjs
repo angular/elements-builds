eaad527dc32d6e17a152b756c0bdb7dfb3c968f4
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 19:51:53 +0000

    1584b32f09 build: update io_bazel_rules_sass digest to 236d2fc (#50006)

--- fesm2015/elements.mjs
+++ fesm2015/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-3599214
+ * @license Angular v15.2.8+sha-1584b32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-3599214');
+const VERSION = new Version('15.2.8+sha-1584b32');
 
 /**
  * @module
