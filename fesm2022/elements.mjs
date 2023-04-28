6092e4a16f296be6619a71ecc33cff40224750d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:25:36 +0000

    d5fb3ad645 refactor(core): finalize rxjs-interop options & docs (#50071)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-cb5c4e0
+ * @license Angular v16.0.0-rc.3+sha-d5fb3ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-cb5c4e0');
+const VERSION = new Version('16.0.0-rc.3+sha-d5fb3ad');
 
 /**
  * @module
