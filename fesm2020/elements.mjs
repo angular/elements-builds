84ebb6eacba747c47316624fe890284d8d0c81b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 09:52:36 +0000

    ea82d3b0e9 ci: update link to start google presubmit (#50060)

--- fesm2020/elements.mjs
+++ fesm2020/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-9107e93
+ * @license Angular v15.2.8+sha-ea82d3b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -521,7 +521,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-9107e93');
+const VERSION = new Version('15.2.8+sha-ea82d3b');
 
 /**
  * @module
