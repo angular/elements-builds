6e806ed6988ee97148e5c101507053aa22ec6cff
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 13:35:32 +0000

    6da1054a42 fix(compiler-cli): catch fatal diagnostic when getting diagnostics for components (#50046)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-4a5d4a9
+ * @license Angular v16.0.0-rc.3+sha-6da1054
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-4a5d4a9');
+const VERSION = new Version('16.0.0-rc.3+sha-6da1054');
 
 /**
  * @module
