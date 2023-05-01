e031a83646a4ed6b2d838d8e4de68de06a21c6d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 17:19:29 +0000

    952569c2c6 docs: update the intro page (#49980)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-d6d347f
+ * @license Angular v16.0.0-rc.3+sha-952569c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-d6d347f');
+const VERSION = new Version('16.0.0-rc.3+sha-952569c');
 
 /**
  * @module
