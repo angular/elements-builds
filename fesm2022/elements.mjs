84ecee7ff260e3b9d7bb0521424ef4848951b3dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:12:47 +0000

    d6d347fec5 docs: add developer preview guide for esbuild-based build system (#50068)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-6dc3add
+ * @license Angular v16.0.0-rc.3+sha-d6d347f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-6dc3add');
+const VERSION = new Version('16.0.0-rc.3+sha-d6d347f');
 
 /**
  * @module
