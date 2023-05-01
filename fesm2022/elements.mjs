5fe25c8b358466f1b118488e50c9b2e825394a52
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:56:05 +0000

    f49c6d0a2f refactor(common): Deprecate useless isPlatform functions (#50055)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-109eb88
+ * @license Angular v16.1.0-next.0+sha-f49c6d0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-109eb88');
+const VERSION = new Version('16.1.0-next.0+sha-f49c6d0');
 
 /**
  * @module
