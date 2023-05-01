feb93548ecc8680078a3d0ae08187731b6b1a409
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:55:11 +0000

    109eb88c6a docs: add v16 update guide (#50073)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-08b664d
+ * @license Angular v16.1.0-next.0+sha-109eb88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-08b664d');
+const VERSION = new Version('16.1.0-next.0+sha-109eb88');
 
 /**
  * @module
