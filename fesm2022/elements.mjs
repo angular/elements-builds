947fef8bfb2451be13df0ca3f6efae0373118710
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:52:45 +0000

    08b664d3c8 docs: Introduce RxJS and Signals interop guide (#50039)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-50a4a7c
+ * @license Angular v16.1.0-next.0+sha-08b664d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-50a4a7c');
+const VERSION = new Version('16.1.0-next.0+sha-08b664d');
 
 /**
  * @module
