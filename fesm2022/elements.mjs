424a580f4a735ee13343fa9459b42ccaac80dbcd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:49:49 +0000

    50a4a7cbee docs: Add `DestroyRef` and `takeUntilDestroyed` sections to lifecycle doc (#50042)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-73c6126
+ * @license Angular v16.1.0-next.0+sha-50a4a7c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-73c6126');
+const VERSION = new Version('16.1.0-next.0+sha-50a4a7c');
 
 /**
  * @module
