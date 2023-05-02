de2911fd846248563c14eea886065d45f4e462d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:46:42 +0000

    c9d4a6965c build: update github/codeql-action action to v2.3.2 (#50102)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.4+sha-6928618
+ * @license Angular v16.0.0-rc.4+sha-c9d4a69
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.4+sha-6928618');
+const VERSION = new Version('16.0.0-rc.4+sha-c9d4a69');
 
 /**
  * @module
