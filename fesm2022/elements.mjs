d49491d600bb2e7c839ec4e436e2c14739724213
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 14:22:12 +0000

    da2efbcef5 test: update payload golden for AIO to reflect reduction in size (#50107)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8741670
+ * @license Angular v16.1.0-next.0+sha-da2efbc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8741670');
+const VERSION = new Version('16.1.0-next.0+sha-da2efbc');
 
 /**
  * @module
