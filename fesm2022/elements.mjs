e79a4260796b4011255454db845fc0d2fec3aa2f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:16:53 +0000

    c68545e082 docs: cleanup team page (#50044)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-ee180a9
+ * @license Angular v16.1.0-next.0+sha-c68545e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-ee180a9');
+const VERSION = new Version('16.1.0-next.0+sha-c68545e');
 
 /**
  * @module
