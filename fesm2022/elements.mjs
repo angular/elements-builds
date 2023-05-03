f460e5e9c53147787457688563b1aeaa9886dd4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-03 20:16:33 +0000

    b3730081c3 docs: add the v16 blog post link to the changelog (#50132)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-612771e
+ * @license Angular v16.1.0-next.0+sha-b373008
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-612771e');
+const VERSION = new Version('16.1.0-next.0+sha-b373008');
 
 /**
  * @module
