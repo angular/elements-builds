54d00f4d8b01ff443b8ef77104e8d2f8d4ba2f8a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 10:40:52 +0000

    2f03eefd5c refactor(platform-browser): Throw tree-shakable errors. (#49341)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-f28580a
+ * @license Angular v16.0.0+sha-2f03eef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0+sha-f28580a');
+const VERSION = new Version('16.0.0+sha-2f03eef');
 
 /**
  * @module
