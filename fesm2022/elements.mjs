80472689a6776c301b6ef6eda63466255d64a11e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:10:36 +0000

    2180a692b0 refactor(core): minor updates to TestBed error messages (#50072)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-6024b66
+ * @license Angular v16.1.0-next.0+sha-2180a69
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-6024b66');
+const VERSION = new Version('16.1.0-next.0+sha-2180a69');
 
 /**
  * @module
