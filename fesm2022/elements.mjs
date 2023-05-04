7e8bfb47d510fcd02b8d5a783684ecb80809c900
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 06:49:53 +0000

    3c4b30711a docs: fix roadmap structure and remove redundant title (#50137)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-b373008
+ * @license Angular v16.1.0-next.0+sha-3c4b307
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-b373008');
+const VERSION = new Version('16.1.0-next.0+sha-3c4b307');
 
 /**
  * @module
