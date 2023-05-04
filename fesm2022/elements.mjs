a36a195748b4cfbbceb5c6d006b9e156bcd8804a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:25:49 +0000

    f28580a42b build: remove the webworker platform. (#50059)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-f3bb680
+ * @license Angular v16.0.0+sha-f28580a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0+sha-f3bb680');
+const VERSION = new Version('16.0.0+sha-f28580a');
 
 /**
  * @module
