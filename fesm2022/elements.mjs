d75391772bbcc43fbb4a73fac1a6b214d520c764
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:13:14 +0000

    cb8cdadd3b refactor(compiler): reflect arrow function definition (#50084)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-2180a69
+ * @license Angular v16.1.0-next.0+sha-cb8cdad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-2180a69');
+const VERSION = new Version('16.1.0-next.0+sha-cb8cdad');
 
 /**
  * @module
