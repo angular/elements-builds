fcdf230387dd0385a1e9d1d2e7d1a02f534452d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:08:06 +0000

    31123520ce refactor(core): Update CD traversal to use 'modes' (#50005)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-a648e30
+ * @license Angular v16.1.0-next.0+sha-3112352
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-a648e30');
+const VERSION = new Version('16.1.0-next.0+sha-3112352');
 
 /**
  * @module
