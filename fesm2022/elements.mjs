b8cbe5f7e05501ade2fc594003a25907c7d382e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 17:04:40 +0000

    da7ff37dc9 Revert "docs(docs-infra): Remove unused annotation template (#50114)" (#50206)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-2f03eef
+ * @license Angular v16.0.0+sha-da7ff37
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0+sha-2f03eef');
+const VERSION = new Version('16.0.0+sha-da7ff37');
 
 /**
  * @module
