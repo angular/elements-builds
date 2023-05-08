108170f26edbffbf5dd0cd0edb1c07d85c6e7408
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:49:26 +0000

    f780d6f202 refactor(compiler): extract save/restore view logic to separate phase (#50008)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-73fcf9f
+ * @license Angular v16.1.0-next.0+sha-f780d6f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-73fcf9f');
+const VERSION = new Version('16.1.0-next.0+sha-f780d6f');
 
 /**
  * @module
