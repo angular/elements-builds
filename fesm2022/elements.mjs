17f658b3f0013d1fe24fdb987f9674129f17858c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:08:56 +0000

    fe653c2141 docs: fix filename in first-app (#50207)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-da7ff37
+ * @license Angular v16.0.0+sha-fe653c2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0+sha-da7ff37');
+const VERSION = new Version('16.0.0+sha-fe653c2');
 
 /**
  * @module
