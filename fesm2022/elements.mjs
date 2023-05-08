1b33f76db60b87de074a723a30aabcda2452bbb2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:52:24 +0000

    59837f4749 refactor(platform-server): import `xhr2` dynamically in the ServerXhr class (#50095)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-e60b3d4
+ * @license Angular v16.1.0-next.0+sha-59837f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-e60b3d4');
+const VERSION = new Version('16.1.0-next.0+sha-59837f4');
 
 /**
  * @module
