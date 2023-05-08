45020b07ff7dcdf0b7f7e7a085960cfd7b4a4f2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:51:55 +0000

    e60b3d45b9 build: remove unused deps (#50116)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-f780d6f
+ * @license Angular v16.1.0-next.0+sha-e60b3d4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-f780d6f');
+const VERSION = new Version('16.1.0-next.0+sha-e60b3d4');
 
 /**
  * @module
