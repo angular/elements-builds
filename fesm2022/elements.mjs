abc2298deecd4a159b84d17f5bf8f4f92acd928c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:09:29 +0000

    570114e30f refactor(common): cleanup platformLocation (#50054)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-55dfe80
+ * @license Angular v16.1.0-next.0+sha-570114e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-55dfe80');
+const VERSION = new Version('16.1.0-next.0+sha-570114e');
 
 /**
  * @module
