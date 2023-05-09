eb5153b0721b6bf338110f42339a17b538363ee1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:07:14 +0000

    55dfe80700 docs: standalone component preloading config (#50193)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-822b307
+ * @license Angular v16.1.0-next.0+sha-55dfe80
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -511,7 +511,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-822b307');
+const VERSION = new Version('16.1.0-next.0+sha-55dfe80');
 
 /**
  * @module
