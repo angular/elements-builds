049b8c6198bc92c0896bbb79cb0da29d7745e21c
Andrew Kushnir <akushnir@google.com>
2023-05-11 21:47:54 +0000

    4a7ebf4037 build: lock file maintenance (#49879)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.1+sha-d5be23e
+ * @license Angular v16.0.1+sha-4a7ebf4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.1+sha-d5be23e');
+const VERSION = new Version('16.0.1+sha-4a7ebf4');
 
 /**
  * @module
