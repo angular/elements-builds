056b700f54d03141e542b2f0281926c864b12502
Andrew Kushnir <akushnir@google.com>
2023-05-12 17:32:59 +0000

    aad267ac07 docs: add missing "when" (#50262) PR Close #50262

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-06f19ce
+ * @license Angular v16.1.0-next.0+sha-aad267a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-06f19ce');
+const VERSION = new Version('16.1.0-next.0+sha-aad267a');
 
 /**
  * @module
