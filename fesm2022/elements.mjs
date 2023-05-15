4fe6d63126f66375528cd3fb6acabb1a9289ef46
Andrew Kushnir <akushnir@google.com>
2023-05-15 17:11:09 +0000

    a09f88934d docs: Glossary link to N (#50294)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-3e025d9
+ * @license Angular v16.1.0-next.0+sha-a09f889
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-3e025d9');
+const VERSION = new Version('16.1.0-next.0+sha-a09f889');
 
 /**
  * @module
