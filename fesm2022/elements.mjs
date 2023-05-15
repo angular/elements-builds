09d588180e3d8b10c3982435c6397c68f66cea61
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:45:57 +0000

    63f5dfdf52 docs: fix inconsistencies in getting started (#50275)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-d0f6d31
+ * @license Angular v16.1.0-next.0+sha-63f5dfd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-d0f6d31');
+const VERSION = new Version('16.1.0-next.0+sha-63f5dfd');
 
 /**
  * @module
