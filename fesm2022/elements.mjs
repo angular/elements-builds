24bf28f7c1e748be82a1ab62eb73ab2e2918d9fe
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:40:35 +0000

    239e7e6de3 refactor(core): remove legacy way of preventing default actions (#50257)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-d0262cd
+ * @license Angular v16.1.0-next.0+sha-239e7e6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-d0262cd');
+const VERSION = new Version('16.1.0-next.0+sha-239e7e6');
 
 /**
  * @module
