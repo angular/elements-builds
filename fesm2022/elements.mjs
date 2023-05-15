8731a1e456b032518592ae888dd74c45af72b7e1
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:32:25 +0000

    d0262cd0e4 build: don't run saucelabs tests yet on PRs... that will happen in a followup (#49200)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-85b4941
+ * @license Angular v16.1.0-next.0+sha-d0262cd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-85b4941');
+const VERSION = new Version('16.1.0-next.0+sha-d0262cd');
 
 /**
  * @module
