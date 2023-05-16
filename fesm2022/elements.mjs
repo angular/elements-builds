1714cca1fe05a002fb174108b0ea362eb0a7410b
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:38:49 +0000

    165b8b647c fix(core): allow passing value of any type to `isSignal` function (#50035)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-fab7f39
+ * @license Angular v16.1.0-next.0+sha-165b8b6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-fab7f39');
+const VERSION = new Version('16.1.0-next.0+sha-165b8b6');
 
 /**
  * @module
