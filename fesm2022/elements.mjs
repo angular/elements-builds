1c737ba870802bdf2453279614592a0d3a85a06e
Andrew Kushnir <akushnir@google.com>
2023-05-16 20:48:15 +0000

    3eafb4b470 Revert "fix(core): add additional component metadata to component ID generation (#50203)" (#50335)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.1+sha-b75625b
+ * @license Angular v16.0.1+sha-3eafb4b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.1+sha-b75625b');
+const VERSION = new Version('16.0.1+sha-3eafb4b');
 
 /**
  * @module
