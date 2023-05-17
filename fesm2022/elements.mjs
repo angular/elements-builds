a164b2e57acb6f7f5567f25fcd2120f17677992b
Andrew Kushnir <akushnir@google.com>
2023-05-17 16:03:13 +0000

    5bf1abffea refactor(core): Add a warning when `ApplicationRef.isStable` doesn't emit `true` (#50295)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.1+sha-656c953
+ * @license Angular v16.0.1+sha-5bf1abf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.1+sha-656c953');
+const VERSION = new Version('16.0.1+sha-5bf1abf');
 
 /**
  * @module
