9da1fe1d1a550ed1b8e2bb6147184211d43026c9
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 15:26:21 +0000

    8b44ba3170 fix(core): host directives incorrectly validating aliased bindings (#50364)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-46128fe
+ * @license Angular v16.1.0-next.1+sha-8b44ba3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-46128fe');
+const VERSION = new Version('16.1.0-next.1+sha-8b44ba3');
 
 /**
  * @module
