c9ce1a03660df2592df640a640480c698fcce544
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 15:27:40 +0000

    24d803b0a2 docs: forwardref usage for standalone components (#50366)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-8b44ba3
+ * @license Angular v16.1.0-next.1+sha-24d803b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-8b44ba3');
+const VERSION = new Version('16.1.0-next.1+sha-24d803b');
 
 /**
  * @module
