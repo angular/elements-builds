5fdf1ae311b3436b2c8ded2bae5342c3d91f8399
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 16:41:39 +0000

    d097844da9 docs: replace blossom emoji with tulip (#50349)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-24d803b
+ * @license Angular v16.1.0-next.1+sha-d097844
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-24d803b');
+const VERSION = new Version('16.1.0-next.1+sha-d097844');
 
 /**
  * @module
