f8c205b2865b4b6e919443610598d73fe0e59083
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 15:03:35 +0000

    f6da091228 refactor(compiler): introduce compiler infrastructure for input transforms (#50225)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-d0a5530
+ * @license Angular v16.1.0-next.1+sha-f6da091
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-d0a5530');
+const VERSION = new Version('16.1.0-next.1+sha-f6da091');
 
 /**
  * @module
