8ebcda5392367c98448c20260dff2c6cafee6108
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:46:55 +0000

    4dc528d402 build: remove unused bash utilities from legacy build (#50427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-f7b8fb3
+ * @license Angular v16.1.0-next.1+sha-4dc528d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-f7b8fb3');
+const VERSION = new Version('16.1.0-next.1+sha-4dc528d');
 
 /**
  * @module
