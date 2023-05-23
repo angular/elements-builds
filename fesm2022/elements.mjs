7bf6d9ef0653fdd2e90c8d621581f4a901cadba4
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:33:44 +0000

    f7b8fb3c97 build: remove unused gulp helper script (#50428)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-f4393f7
+ * @license Angular v16.1.0-next.1+sha-f7b8fb3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-f4393f7');
+const VERSION = new Version('16.1.0-next.1+sha-f7b8fb3');
 
 /**
  * @module
