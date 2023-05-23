f934f2942a97494029c81b7d234c857f94b551df
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:26:49 +0000

    3c093d767e build: update babel dependencies to v7.21.9 (#50416)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-34d7301
+ * @license Angular v16.1.0-next.1+sha-3c093d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-34d7301');
+const VERSION = new Version('16.1.0-next.1+sha-3c093d7');
 
 /**
  * @module
