a7a1530b5de6f03b239726414354e42bb1d0652c
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:28:00 +0000

    3152c4de92 fix(docs-infra): remove extra slash from JSON-LD data (#50140)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-3c093d7
+ * @license Angular v16.1.0-next.1+sha-3152c4d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-3c093d7');
+const VERSION = new Version('16.1.0-next.1+sha-3152c4d');
 
 /**
  * @module
