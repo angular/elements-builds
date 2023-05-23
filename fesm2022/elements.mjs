55e015da85154a6135c831e5bd12c3a7cad41086
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:30:49 +0000

    f79a5bc71c docs: fix code-example header path in dependency-injection-in-action-md (#50397)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-e516556
+ * @license Angular v16.1.0-next.1+sha-f79a5bc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-e516556');
+const VERSION = new Version('16.1.0-next.1+sha-f79a5bc');
 
 /**
  * @module
