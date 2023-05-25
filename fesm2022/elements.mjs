8a88c21718b2ce0048b7c81d2782c941ba4a1ece
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:57:02 +0000

    721bc72649 fix(compiler): resolve deprecation warning with TypeScript 5.1 (#50460)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-04553b9
+ * @license Angular v16.1.0-next.2+sha-721bc72
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-04553b9');
+const VERSION = new Version('16.1.0-next.2+sha-721bc72');
 
 /**
  * @module
