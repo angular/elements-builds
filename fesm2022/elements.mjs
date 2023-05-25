ee306a447d4581ba6e31aa632af33dea3c7818c6
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:57:17 +0000

    fc6b10cb15 build: update dependency https-proxy-agent to v7 (#50456)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-c0a8c8f
+ * @license Angular v16.0.3+sha-fc6b10c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-c0a8c8f');
+const VERSION = new Version('16.0.3+sha-fc6b10c');
 
 /**
  * @module
