e81ffdbb390f0ac41666d8d868a81ec6be18ac3b
Dylan Hunn <dylhunn@gmail.com>
2023-05-25 20:35:06 +0000

    6f5dabe0d2 Revert "fix(http): create macrotask during request handling instead of load start (#50406)" (#50475)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-bada919
+ * @license Angular v16.1.0-next.2+sha-6f5dabe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-bada919');
+const VERSION = new Version('16.1.0-next.2+sha-6f5dabe');
 
 /**
  * @module
