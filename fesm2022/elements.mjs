b397fc73c4abf61a667f7cadbe6c807d1ae1b39f
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:59:24 +0000

    3a05e4cca1 refactor(platform-server): exclude `provideHttpClient()` call from `provideServerRendering()` (#50459)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-721bc72
+ * @license Angular v16.1.0-next.2+sha-3a05e4c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-721bc72');
+const VERSION = new Version('16.1.0-next.2+sha-3a05e4c');
 
 /**
  * @module
