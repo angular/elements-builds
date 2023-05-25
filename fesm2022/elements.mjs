480779b8df1bc58189f6c79302896732e3deb481
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 15:00:58 +0000

    4f5f4969c5 refactor(core): Remove unused import from the graph.ts (#50372) This commit updates the code to remove unused import of the `throwInvalidWriteToSignalError` symbol.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-3a05e4c
+ * @license Angular v16.1.0-next.2+sha-4f5f496
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-3a05e4c');
+const VERSION = new Version('16.1.0-next.2+sha-4f5f496');
 
 /**
  * @module
