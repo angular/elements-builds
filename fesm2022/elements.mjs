5c60ed45e5a3f12ffda2f8e6eba8f943fdb88b29
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:54:34 +0000

    258c773188 docs: remove unused imports and improved the code (#50424)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-06b498f
+ * @license Angular v16.1.0-next.2+sha-258c773
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-06b498f');
+const VERSION = new Version('16.1.0-next.2+sha-258c773');
 
 /**
  * @module
