7d99abafbc59fc8c4a768c8a1f9d7655a3f0b245
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 16:31:10 +0000

    62e76d4d20 docs: update CLI command to include `--preserve-symlinks-main` (#50512)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-6f5dabe
+ * @license Angular v16.1.0-next.2+sha-62e76d4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-6f5dabe');
+const VERSION = new Version('16.1.0-next.2+sha-62e76d4');
 
 /**
  * @module
