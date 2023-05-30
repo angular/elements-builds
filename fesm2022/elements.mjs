d002e2f45b3e6cee9c282b3b224bb60480a440ee
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:14:12 +0000

    1ca6362c06 docs(docs-infra): rimraf requires the globflag. (#50493)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-0c80349
+ * @license Angular v16.1.0-next.2+sha-1ca6362
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-0c80349');
+const VERSION = new Version('16.1.0-next.2+sha-1ca6362');
 
 /**
  * @module
