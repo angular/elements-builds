b8e12880f0a6588b949012129fa610840a7fc66c
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 16:37:08 +0000

    9acc0e32c4 docs: refactored the markup tags (#50481)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-62e76d4
+ * @license Angular v16.1.0-next.2+sha-9acc0e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-62e76d4');
+const VERSION = new Version('16.1.0-next.2+sha-9acc0e3');
 
 /**
  * @module
