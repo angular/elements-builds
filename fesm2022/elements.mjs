92622ca889342dcd230e39d2b60dbc48854d844b
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:55:36 +0000

    7e468dffce docs: correct Signals documentation (#50518)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-ac447ed
+ * @license Angular v16.1.0-next.2+sha-7e468df
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-ac447ed');
+const VERSION = new Version('16.1.0-next.2+sha-7e468df');
 
 /**
  * @module
