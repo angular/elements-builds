1d6038fc151ebf6e001a7f21ec79d42cc3613aad
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:54:23 +0000

    ac447ede22 refactor(common): fix variable name (#50521)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-915abc6
+ * @license Angular v16.1.0-next.2+sha-ac447ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-915abc6');
+const VERSION = new Version('16.1.0-next.2+sha-ac447ed');
 
 /**
  * @module
