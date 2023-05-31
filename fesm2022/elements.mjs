86325f10962a32ef77d826ef6d0e284d6a50ed38
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-31 14:53:59 +0000

    277a473e53 build: Fix size tracking test (#50531)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-f26c9a2
+ * @license Angular v16.0.3+sha-277a473
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-f26c9a2');
+const VERSION = new Version('16.0.3+sha-277a473');
 
 /**
  * @module
