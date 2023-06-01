35c6691dee09ea5c829c92db1930800d0220cc8d
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:03:29 +0000

    703b8fcac1 fix(compiler): do not remove comments in component styles (#50346)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-9e68a62
+ * @license Angular v16.0.4+sha-703b8fc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-9e68a62');
+const VERSION = new Version('16.0.4+sha-703b8fc');
 
 /**
  * @module
