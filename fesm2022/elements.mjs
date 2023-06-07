8c9d4d293f594d0bd86e1aa0c0dc28102d9477a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:04:25 +0000

    4e663297c5 fix(compiler): error when reading compiled input transforms metadata in JIT mode (#50600)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-3044822
+ * @license Angular v16.1.0-next.3+sha-4e66329
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-3044822');
+const VERSION = new Version('16.1.0-next.3+sha-4e66329');
 
 /**
  * @module
