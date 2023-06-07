bb60dccad1a3976fc98ea417ef390440beaaa699
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:08:54 +0000

    9648fc49dd refactor(compiler-cli): Remove .d.ts files transformer in local compilation mode. (#50486)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-ab35596
+ * @license Angular v16.1.0-next.3+sha-9648fc4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-ab35596');
+const VERSION = new Version('16.1.0-next.3+sha-9648fc4');
 
 /**
  * @module
