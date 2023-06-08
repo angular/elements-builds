2f5a10a27810f2132f53b7d6660d71db6acc73e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-08 21:03:59 +0000

    e1a715da73 docs: remove reverted commit from the changelog

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-024d6a8
+ * @license Angular v16.2.0-next.0+sha-e1a715d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-024d6a8');
+const VERSION = new Version('16.2.0-next.0+sha-e1a715d');
 
 /**
  * @module
