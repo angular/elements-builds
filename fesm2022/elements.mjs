68cc9e70b5dd42fd4b1d658cae327fc2adc32984
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-09 15:57:18 +0000

    7ca5cb4969 docs: removed unused methods (#50631)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-a66608a
+ * @license Angular v16.1.0-rc.0+sha-7ca5cb4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-a66608a');
+const VERSION = new Version('16.1.0-rc.0+sha-7ca5cb4');
 
 /**
  * @module
