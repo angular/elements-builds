8472591ca2ce2668b30096ef1b46b62d0e6436e2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 12:40:40 +0000

    f8920dfd70 docs: update first-app-lesson-11.md to include info about routerLink (#50348)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-89a089b
+ * @license Angular v16.0.5+sha-f8920df
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-89a089b');
+const VERSION = new Version('16.0.5+sha-f8920df');
 
 /**
  * @module
