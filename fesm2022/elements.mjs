92460323c825f3d1663c6917d4c46fd9380cf71a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:54:05 +0000

    3b2f119f57 docs: add missing navigation item (#50113)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-7c7a4a2
+ * @license Angular v16.1.0-rc.0+sha-3b2f119
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-7c7a4a2');
+const VERSION = new Version('16.1.0-rc.0+sha-3b2f119');
 
 /**
  * @module
