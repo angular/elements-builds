a2351cf43049ad013455c0422c07fc118bb23ea3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 12:59:39 +0000

    fa3b7332a9 build: update actions/checkout action to v3.5.3 (#50689)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-b90f2b1
+ * @license Angular v16.1.0-rc.0+sha-fa3b733
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-b90f2b1');
+const VERSION = new Version('16.1.0-rc.0+sha-fa3b733');
 
 /**
  * @module
