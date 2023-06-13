55ef426be256254145adc51dd2d056ea0a37c33f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 10:02:43 +0000

    7abb71609b build: lock file maintenance (#50532)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-57f9efe
+ * @license Angular v16.1.0-rc.0+sha-7abb716
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-57f9efe');
+const VERSION = new Version('16.1.0-rc.0+sha-7abb716');
 
 /**
  * @module
