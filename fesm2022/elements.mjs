4110f76d11b61911d2817c9c4d7083db5326656b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 15:14:41 +0000

    f1e350cb59 release: cut the v16.0.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-c58bc97
+ * @license Angular v16.0.6+sha-f1e350c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-c58bc97');
+const VERSION = new Version('16.0.6+sha-f1e350c');
 
 /**
  * @module
