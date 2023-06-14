4ae055194bb385e7775807fcd2823b3a245f637a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 14:56:35 +0000

    71360b3a3e fix(compiler-cli): libraries compiled with v16.1+ breaking with Angular framework v16.0.x (#50715)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0+sha-5e1d844
+ * @license Angular v16.1.0+sha-71360b3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0+sha-5e1d844');
+const VERSION = new Version('16.1.0+sha-71360b3');
 
 /**
  * @module
