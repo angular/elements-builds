cf223dc11495a2a0e0924aea83bb7e2a96d13f5d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 10:53:40 +0000

    9ff5572b9f refactor(core): handle #24571 todos. (#49221)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0+sha-0a72df6
+ * @license Angular v16.1.0+sha-9ff5572
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0+sha-0a72df6');
+const VERSION = new Version('16.1.0+sha-9ff5572');
 
 /**
  * @module
