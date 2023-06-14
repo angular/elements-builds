244fa5da74565b6701e1c6b233d7e29ab35e96d4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 09:14:52 +0000

    f199305db6 docs(platform-server): Make links out of @see tags (#50110)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-1ad4d55
+ * @license Angular v16.2.0-next.0+sha-f199305
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-1ad4d55');
+const VERSION = new Version('16.2.0-next.0+sha-f199305');
 
 /**
  * @module
