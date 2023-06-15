88031b624e5d55250fc5f47e2ed3c2d8b6ab0c84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-15 13:45:16 +0000

    1d3e96b7d9 test: mark ngtsc test target as flaky (#50718)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.1+sha-fd519b0
+ * @license Angular v16.1.1+sha-1d3e96b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.1+sha-fd519b0');
+const VERSION = new Version('16.1.1+sha-1d3e96b');
 
 /**
  * @module
