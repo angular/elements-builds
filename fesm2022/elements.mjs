0e7082ed0c66b1486f0e85ea1a15fc52633973d0
Andrew Kushnir <akushnir@google.com>
2023-06-21 19:03:58 +0000

    7d7fc48e58 refactor(core): tweak runtime deps tracker to accommodate JIT requirements. (#50606)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-4596577
+ * @license Angular v16.2.0-next.0+sha-7d7fc48
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-4596577');
+const VERSION = new Version('16.2.0-next.0+sha-7d7fc48');
 
 /**
  * @module
