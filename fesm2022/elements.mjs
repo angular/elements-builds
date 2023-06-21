8d61ba0883795ba640c907d92b085db4697413c1
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-06-21 18:46:00 +0000

    5c3407507a release: cut the v16.2.0-next.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-a99b6ff
+ * @license Angular v16.2.0-next.0+sha-5c34075
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -506,7 +506,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-a99b6ff');
+const VERSION = new Version('16.2.0-next.0+sha-5c34075');
 
 /**
  * @module
