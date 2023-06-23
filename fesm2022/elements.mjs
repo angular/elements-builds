854d0b287109d808820871fb70c10d469b65c213
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:46:52 +0000

    e93708b864 docs: update bio for devversion (#50831)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-9f2fb7c
+ * @license Angular v16.2.0-next.0+sha-e93708b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-9f2fb7c');
+const VERSION = new Version('16.2.0-next.0+sha-e93708b');
 
 /**
  * @module
