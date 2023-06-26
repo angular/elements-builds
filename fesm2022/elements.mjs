ea1f002d72405c5fc88361b299fbef9beb4e5912
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:28:25 +0000

    a844249c7f docs: fix minor typo in tutorial (#50795)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-9b7bc80
+ * @license Angular v16.1.2+sha-a844249
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-9b7bc80');
+const VERSION = new Version('16.1.2+sha-a844249');
 
 /**
  * @module
