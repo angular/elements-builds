620dd8cb2f651cd96c2e8e2164e4567010796b3d
Dylan Hunn <dylhunn@gmail.com>
2023-07-10 14:27:49 +0000

    daea7dcd54 docs: remove mention of deprecated providedIn: module. (#50925)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-394ca3b
+ * @license Angular v16.2.0-next.1+sha-daea7dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-394ca3b');
+const VERSION = new Version('16.2.0-next.1+sha-daea7dc');
 
 /**
  * @module
