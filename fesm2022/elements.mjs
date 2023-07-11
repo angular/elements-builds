00f83c9e112001ff81bf510a287734e895afd571
Joey Perrott <josephperrott@gmail.com>
2023-07-11 18:13:55 +0000

    d9f832d55c ci: remove GHA lint job from required statuses (#51011)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-f1b6299
+ * @license Angular v16.2.0-next.1+sha-d9f832d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-f1b6299');
+const VERSION = new Version('16.2.0-next.1+sha-d9f832d');
 
 /**
  * @module
