2cc044a374dd0cc4ba87532a71039c0d1a66fb8c
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:48:57 +0000

    b14a78ebb0 refactor(compiler): implement block syntax in html ast (#50953)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-6cac41f
+ * @license Angular v16.2.0-next.1+sha-b14a78e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-6cac41f');
+const VERSION = new Version('16.2.0-next.1+sha-b14a78e');
 
 /**
  * @module
