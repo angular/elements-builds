f63033f8692b5308dfa9a4be7a9b02ce333fb8f7
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:46:38 +0000

    5bd530ab32 refactor(compiler-cli): add internal compiler option to control NgModule selector scope emit (#51007)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-25153e9
+ * @license Angular v16.2.0-next.1+sha-5bd530a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-25153e9');
+const VERSION = new Version('16.2.0-next.1+sha-5bd530a');
 
 /**
  * @module
