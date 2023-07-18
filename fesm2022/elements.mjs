46a4717c664c00b87850e9d2ce1d12e7de4f197d
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:19:26 +0000

    1ec3a2bc2b docs: fix typos and styling in lessons 7-14 (#51004)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-89c84bf
+ * @license Angular v16.2.0-next.2+sha-1ec3a2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-89c84bf');
+const VERSION = new Version('16.2.0-next.2+sha-1ec3a2b');
 
 /**
  * @module
