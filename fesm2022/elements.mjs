167273d6182d6e22fb16dfaba14a8a6c8d2fc9d3
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:19:17 +0000

    89c84bf2c1 test(core): update runtime error list to include deps tracker error (#50980)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-b76088d
+ * @license Angular v16.2.0-next.2+sha-89c84bf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-b76088d');
+const VERSION = new Version('16.2.0-next.2+sha-89c84bf');
 
 /**
  * @module
