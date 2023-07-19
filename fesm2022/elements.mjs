b4ec6196cbf808c6348e12b86d1d82b37c4a523a
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 18:58:44 +0000

    8c8e7210f4 Revert "docs: added the alt attribute in the img tag (#51102)" (#51107)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-4176f59
+ * @license Angular v16.1.5+sha-8c8e721
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-4176f59');
+const VERSION = new Version('16.1.5+sha-8c8e721');
 
 /**
  * @module
