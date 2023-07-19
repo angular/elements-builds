08e90b264fd3015e48b195a53cf1db9096c6f3a9
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 17:23:45 +0000

    4176f59670 docs(docs-infra): fix wrong links in first-app-lesson-13.md (#51094)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-7df4648
+ * @license Angular v16.1.5+sha-4176f59
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-7df4648');
+const VERSION = new Version('16.1.5+sha-4176f59');
 
 /**
  * @module
