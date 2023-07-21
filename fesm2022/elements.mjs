0c29063c1d333319283353a3721bbb1752292725
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:45:47 +0000

    7cfc36567f docs: fix typos in first app tutorial (#51115) PR Close #51115

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.6+sha-bfc6e48
+ * @license Angular v16.1.6+sha-7cfc365
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.6+sha-bfc6e48');
+const VERSION = new Version('16.1.6+sha-7cfc365');
 
 /**
  * @module
