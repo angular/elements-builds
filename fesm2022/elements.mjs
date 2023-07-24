229f0a828547a30ab87e43064cd72126aaf26ea3
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:37:04 +0000

    de0d3d704c docs: remove orphan migration docs (#51147)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.6+sha-7cfc365
+ * @license Angular v16.1.6+sha-de0d3d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.6+sha-7cfc365');
+const VERSION = new Version('16.1.6+sha-de0d3d7');
 
 /**
  * @module
