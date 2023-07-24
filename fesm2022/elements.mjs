caac0bc6011ac6c70467bfa33584e5a64a7219e3
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:29:31 +0000

    92ebfd1ca7 refactor(compiler): handle braces in block parameters (#51143)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-27d43a4
+ * @license Angular v16.2.0-next.3+sha-92ebfd1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-27d43a4');
+const VERSION = new Version('16.2.0-next.3+sha-92ebfd1');
 
 /**
  * @module
