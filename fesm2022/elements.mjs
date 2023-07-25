76ad32da1263e11726522a265fdd5e206a24cf3f
Andrew Kushnir <akushnir@google.com>
2023-07-25 17:59:01 +0000

    d6efd71278 ci: correct changed github action paths (#51168)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-2c5e5e4
+ * @license Angular v16.2.0-next.3+sha-d6efd71
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-2c5e5e4');
+const VERSION = new Version('16.2.0-next.3+sha-d6efd71');
 
 /**
  * @module
