67bb9031a2b7cceb41c01f299158a50b3b643558
Andrew Kushnir <akushnir@google.com>
2023-07-26 17:19:04 +0000

    bb0f3bc125 docs(docs-infra): Adding required and Transform Args into the usage and documentation of input (#51173)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-3a59de6
+ * @license Angular v16.2.0-next.4+sha-bb0f3bc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-3a59de6');
+const VERSION = new Version('16.2.0-next.4+sha-bb0f3bc');
 
 /**
  * @module
