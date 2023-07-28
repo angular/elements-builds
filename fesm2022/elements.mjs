f0e0d7c0b3ea75a67eddc007c9b0b7d3b0c7a7a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-28 21:52:22 +0000

    cdaa2a8a9e feat(core): support Provider type in Injector.create (#49587)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-5061311
+ * @license Angular v16.2.0-next.4+sha-cdaa2a8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-5061311');
+const VERSION = new Version('16.2.0-next.4+sha-cdaa2a8');
 
 /**
  * @module
