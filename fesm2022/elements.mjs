652744eeb2cbc306bf6b5e045ff33601418d22d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:21:27 +0000

    a24830777f docs: fix typos (#51201)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-6691c49
+ * @license Angular v16.2.0-next.4+sha-a248307
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-6691c49');
+const VERSION = new Version('16.2.0-next.4+sha-a248307');
 
 /**
  * @module
