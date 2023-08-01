18bbdb2f96fe78b17f659eeb0dce2196fc449a61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:31:28 +0000

    7d579b9db0 docs: add lang attribute to html files (#51235)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.7+sha-db0f653
+ * @license Angular v16.1.7+sha-7d579b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.7+sha-db0f653');
+const VERSION = new Version('16.1.7+sha-7d579b9');
 
 /**
  * @module
