ca495db8e460bd78f55192682889f27ff4e709da
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:45:00 +0000

    526e215dcd docs: fix missing space after full stop (#51252)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-6d05a68
+ * @license Angular v16.3.0-next.0+sha-526e215
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-6d05a68');
+const VERSION = new Version('16.3.0-next.0+sha-526e215');
 
 /**
  * @module
