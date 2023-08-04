3ae86d1c4b2c7f42f171f3676e799a06aff9f2a4
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:44:55 +0000

    6d05a68122 docs: add title tag to index file (#51266)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-6f506cd
+ * @license Angular v16.3.0-next.0+sha-6d05a68
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-6f506cd');
+const VERSION = new Version('16.3.0-next.0+sha-6d05a68');
 
 /**
  * @module
