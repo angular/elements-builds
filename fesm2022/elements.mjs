3a0d3e718b4ae89ca949832f31724e1b767df595
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:44:31 +0000

    6f506cdff0 refactor(compiler): drop regular imports when symbols can be defer-loaded (#51171)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-b1f9609
+ * @license Angular v16.3.0-next.0+sha-6f506cd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-b1f9609');
+const VERSION = new Version('16.3.0-next.0+sha-6f506cd');
 
 /**
  * @module
