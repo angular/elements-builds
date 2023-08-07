365e3d587c9a8e631bc275a84e4090476a390dd7
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:45:02 +0000

    4a8efbe287 refactor(animations): remove unecessary interface (#50662)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.8+sha-d4ffef2
+ * @license Angular v16.1.8+sha-4a8efbe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.8+sha-d4ffef2');
+const VERSION = new Version('16.1.8+sha-4a8efbe');
 
 /**
  * @module
