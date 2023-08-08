87c0805f7dec01fef03f726e83e64e6ca68829c6
Dylan Hunn <dylhunn@gmail.com>
2023-08-08 17:58:22 +0000

    3a647a5106 docs: Instead of CanActivateFn, CanActivateChildFn appears in CanActivateFn docs (#51283)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-6a7c1ab
+ * @license Angular v16.3.0-next.0+sha-3a647a5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-6a7c1ab');
+const VERSION = new Version('16.3.0-next.0+sha-3a647a5');
 
 /**
  * @module
