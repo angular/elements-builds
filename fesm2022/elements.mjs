d7f08c048953f6dd3f47baf3ca2dd50914028b6e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:33:22 +0000

    1a56763b85 docs: add legend tag to fieldset (#51259)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-79b3327
+ * @license Angular v16.3.0-next.0+sha-1a56763
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-79b3327');
+const VERSION = new Version('16.3.0-next.0+sha-1a56763');
 
 /**
  * @module
