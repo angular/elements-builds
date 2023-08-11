cf511ad303a81c69fc9fec4514a0e7d22152652c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 13:37:05 +0000

    f3f97f0fb5 docs: update Angular CLI help [main] (#51325)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-f67812c
+ * @license Angular v17.0.0-next.0+sha-f3f97f0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-f67812c');
+const VERSION = new Version('17.0.0-next.0+sha-f3f97f0');
 
 /**
  * @module
