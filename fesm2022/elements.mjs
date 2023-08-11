e13d92217977a60860eeb8d4b8a8b636ebe4a922
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 14:09:45 +0000

    5212b47bbf refactor(compiler): introduce defer trigger instructions (#51315)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-f3f97f0
+ * @license Angular v17.0.0-next.0+sha-5212b47
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-f3f97f0');
+const VERSION = new Version('17.0.0-next.0+sha-5212b47');
 
 /**
  * @module
