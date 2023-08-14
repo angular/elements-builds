d831ad0b6f4ddc849f8f196485cbd6c64a1d5955
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 22:38:18 +0000

    6a60a29ff2 docs: Fix Promise.then wrong parameter (#51322)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-ccf32f6
+ * @license Angular v17.0.0-next.0+sha-6a60a29
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-ccf32f6');
+const VERSION = new Version('17.0.0-next.0+sha-6a60a29');
 
 /**
  * @module
