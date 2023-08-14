736f0790db20c5570a78064b90f9cf1bbb505665
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 22:22:49 +0000

    074fd4a5ec build: update pullapprove (#51360)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-094d733
+ * @license Angular v16.2.0+sha-074fd4a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-094d733');
+const VERSION = new Version('16.2.0+sha-074fd4a');
 
 /**
  * @module
