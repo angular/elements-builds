778b56a55bdf00efab5e7a33aca960cf7a1d6d25
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:05:33 +0000

    83b708203e docs: Improve host binding docs (#51326)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-3348f88
+ * @license Angular v17.0.0-next.0+sha-83b7082
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-3348f88');
+const VERSION = new Version('17.0.0-next.0+sha-83b7082');
 
 /**
  * @module
