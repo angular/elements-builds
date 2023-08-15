533d22ff2583c4660bcbfb261f9882c8f71488c8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:18:51 +0000

    17c6ad7775 docs: Add usage information to `HttpInterceptorFn` (#51236)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-83b7082
+ * @license Angular v17.0.0-next.0+sha-17c6ad7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-83b7082');
+const VERSION = new Version('17.0.0-next.0+sha-17c6ad7');
 
 /**
  * @module
