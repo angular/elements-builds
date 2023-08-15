ccd47f22cd2ce3bd3a2b4e5429ddf2f6dfaf11aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 23:13:34 +0000

    6145cc1c0a refactor(core): Ensure hydration cleanup runs in the Angular zone (#51321)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-6791f41
+ * @license Angular v17.0.0-next.0+sha-6145cc1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-6791f41');
+const VERSION = new Version('17.0.0-next.0+sha-6145cc1');
 
 /**
  * @module
