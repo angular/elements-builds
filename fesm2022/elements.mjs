c734742f95a9e71722b14ff7cd7f7ebb84d0f7e6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:05:22 +0000

    3348f8820b docs: Add precisions about static/dynamic queries. (#51146)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-2174351
+ * @license Angular v17.0.0-next.0+sha-3348f88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-2174351');
+const VERSION = new Version('17.0.0-next.0+sha-3348f88');
 
 /**
  * @module
