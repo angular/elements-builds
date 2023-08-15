3864cf52223cf32628b2b1f4a7b903b2a0a56d66
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 00:26:24 +0000

    93675dc797 feat(core): conditional built-in control flow (#51346)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-6a60a29
+ * @license Angular v17.0.0-next.0+sha-93675dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-6a60a29');
+const VERSION = new Version('17.0.0-next.0+sha-93675dc');
 
 /**
  * @module
