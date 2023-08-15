4b52e1d0feb7cbc5bc2a0082eec7a29e22004b5b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 16:47:53 +0000

    9f977e0f34 docs(docs-infra): fix minor typo in standalone-migration.md (#51370) PR Close #51370

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-3e19890
+ * @license Angular v16.2.0+sha-9f977e0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-3e19890');
+const VERSION = new Version('16.2.0+sha-9f977e0');
 
 /**
  * @module
