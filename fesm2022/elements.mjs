f0c6b82a93ee2f85e7324222488cd8c5f4481611
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:22:56 +0000

    03619ada72 docs(common): revert format date_pipe (#51359)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-252b111
+ * @license Angular v16.2.0+sha-03619ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-252b111');
+const VERSION = new Version('16.2.0+sha-03619ad');
 
 /**
  * @module
