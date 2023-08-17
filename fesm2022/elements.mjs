87856b18a042d7b66f0ebafeac5d34cad32d6abc
Andrew Kushnir <akushnir@google.com>
2023-08-17 21:15:00 +0000

    0b901a814b refactor(compiler-cli): better error messages when external strings used for template and styles in local compilation mode (#51338)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-273d8e9
+ * @license Angular v17.0.0-next.0+sha-0b901a8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-273d8e9');
+const VERSION = new Version('17.0.0-next.0+sha-0b901a8');
 
 /**
  * @module
