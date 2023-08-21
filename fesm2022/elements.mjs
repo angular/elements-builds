329e6b4b47cbc998929375dee757cd7f677222c7
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:47:11 +0000

    d2346a601d docs: migrate rxjs-interop guide to standalone (#51447)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-9977727
+ * @license Angular v17.0.0-next.0+sha-d2346a6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-9977727');
+const VERSION = new Version('17.0.0-next.0+sha-d2346a6');
 
 /**
  * @module
