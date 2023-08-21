bd1bee546e924db73e27363a7bf6671279db4a9c
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:44:15 +0000

    4fe88cf9af docs: add lang attribute in html files (#51269)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-bf0f80e
+ * @license Angular v16.2.1+sha-4fe88cf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.1+sha-bf0f80e');
+const VERSION = new Version('16.2.1+sha-4fe88cf');
 
 /**
  * @module
