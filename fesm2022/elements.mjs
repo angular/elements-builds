84e2c58b71bb08df12632c67a81744f6e09b69c0
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:34:18 +0000

    3a686478ac docs(service-worker): Miss a comma in sample (#3) (#51336)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-a9b3c00
+ * @license Angular v17.0.0-next.0+sha-3a68647
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-a9b3c00');
+const VERSION = new Version('17.0.0-next.0+sha-3a68647');
 
 /**
  * @module
