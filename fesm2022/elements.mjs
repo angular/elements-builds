00663f1cfcb902e060a70cb01316de74b5b6ce7b
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:14:04 +0000

    2aae92b05f refactor(router): Have router subscribe to transition events to update state (#48427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-bd150a2
+ * @license Angular v17.0.0-next.0+sha-2aae92b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-bd150a2');
+const VERSION = new Version('17.0.0-next.0+sha-2aae92b');
 
 /**
  * @module
