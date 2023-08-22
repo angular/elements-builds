5022145740ae48be4a6532fb8bcbc15f506daa28
Andrew Kushnir <akushnir@google.com>
2023-08-22 18:52:11 +0000

    76860b54b9 ci: correctly check aio job results (#51464)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-fb296e4
+ * @license Angular v16.2.1+sha-76860b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.1+sha-fb296e4');
+const VERSION = new Version('16.2.1+sha-76860b5');
 
 /**
  * @module
