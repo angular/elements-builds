e03f3a9231851a6b0df2cd5f480f2dd5b47854b6
Andrew Kushnir <akushnir@google.com>
2023-08-22 17:49:40 +0000

    362e069364 docs: add MSYS2 to DEVELOPER.md (#51459)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-2aae92b
+ * @license Angular v17.0.0-next.0+sha-362e069
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-2aae92b');
+const VERSION = new Version('17.0.0-next.0+sha-362e069');
 
 /**
  * @module
