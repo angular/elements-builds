751a246b7bb63adef392de51be01306432ad026e
Joey Perrott <josephperrott@gmail.com>
2023-08-24 20:45:36 +0000

    c3349fb2a1 ci: fix escaping for determing current stable version (#51496)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-6d29ab7
+ * @license Angular v17.0.0-next.1+sha-c3349fb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-6d29ab7');
+const VERSION = new Version('17.0.0-next.1+sha-c3349fb');
 
 /**
  * @module
