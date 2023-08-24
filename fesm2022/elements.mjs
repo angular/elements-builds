e9a38d75c1f7aa248364c51f977a3000fda34ca6
Joey Perrott <josephperrott@gmail.com>
2023-08-24 16:54:48 +0000

    554305976b docs(docs-infra): set meta description per page (#51487)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-6f4a5c3
+ * @license Angular v16.2.2+sha-5543059
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-6f4a5c3');
+const VERSION = new Version('16.2.2+sha-5543059');
 
 /**
  * @module
