70ca8f055bf792d6779d98e25580da822a072936
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-24 16:18:51 +0000

    26ad6d50cc build: add AIO_PAYLOAD_TOKEN env variable (#51489)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-fa72384
+ * @license Angular v17.0.0-next.1+sha-26ad6d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-fa72384');
+const VERSION = new Version('17.0.0-next.1+sha-26ad6d5');
 
 /**
  * @module
