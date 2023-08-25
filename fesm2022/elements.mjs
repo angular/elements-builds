df4665722501f1b22ae5df400bf7bc2069722aa5
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-25 16:53:14 +0000

    440684ddb4 docs(router): Update the url matcher guide to use standalone (#51450)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c3349fb
+ * @license Angular v17.0.0-next.1+sha-440684d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c3349fb');
+const VERSION = new Version('17.0.0-next.1+sha-440684d');
 
 /**
  * @module
