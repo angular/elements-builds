b48b50aaf0bac904420fab4beba7e36ee261b0e7
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:14 +0000

    9753347f8a docs: Add standalone edits to "Testing Components" (#51451)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-3dc74f3
+ * @license Angular v17.0.0-next.1+sha-9753347
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-3dc74f3');
+const VERSION = new Version('17.0.0-next.1+sha-9753347');
 
 /**
  * @module
