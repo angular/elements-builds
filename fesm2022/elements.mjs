bf1d1277cfa0a7d501e04413f66a33a67ad15dfb
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 22:03:07 +0000

    c2d859241b refactor(compiler): Enable additional passing tests (#51498)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-cdcfa09
+ * @license Angular v17.0.0-next.1+sha-c2d8592
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-cdcfa09');
+const VERSION = new Version('17.0.0-next.1+sha-c2d8592');
 
 /**
  * @module
