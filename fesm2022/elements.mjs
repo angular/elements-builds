f6a3f9a1be7b16a0547cfffef9bfa04d916a58de
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:45:34 +0000

    3dc74f3ac5 docs(router): Update the basic router guide to use standalone (#51452)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c4deaac
+ * @license Angular v17.0.0-next.1+sha-3dc74f3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c4deaac');
+const VERSION = new Version('17.0.0-next.1+sha-3dc74f3');
 
 /**
  * @module
