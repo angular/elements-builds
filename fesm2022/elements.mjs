3a8b621e5590b6aa2680424fcc44d14264b15f7f
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 21:13:14 +0000

    cdcfa09ab3 refactor(core): built-in control flow - repeaters (#51422)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-88fcd27
+ * @license Angular v17.0.0-next.1+sha-cdcfa09
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-88fcd27');
+const VERSION = new Version('17.0.0-next.1+sha-cdcfa09');
 
 /**
  * @module
