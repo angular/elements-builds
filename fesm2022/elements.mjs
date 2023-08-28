1b1937b49057183588b48696b1b4998dd80fbd1e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:18 +0000

    14941b9b79 docs(docs-infra): remove inactive resource from community page (#51418)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-0ad7386
+ * @license Angular v16.2.2+sha-14941b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-0ad7386');
+const VERSION = new Version('16.2.2+sha-14941b9');
 
 /**
  * @module
