a83d40a491f688a84967e2103e4fd70682d1890c
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 19:13:17 +0000

    88fcd27c6d refactor(compiler): add a pointer to the const array in i18nStart (#51353)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-a28864d
+ * @license Angular v17.0.0-next.1+sha-88fcd27
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-a28864d');
+const VERSION = new Version('17.0.0-next.1+sha-88fcd27');
 
 /**
  * @module
