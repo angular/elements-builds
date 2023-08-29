501edfa6a7dfe2345b0dc5cb456287ea0cdb81c9
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 22:47:44 +0000

    f6fa690152 docs: migrate directives guide and examples to standalone (#51416)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-fbdca15
+ * @license Angular v17.0.0-next.1+sha-f6fa690
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-fbdca15');
+const VERSION = new Version('17.0.0-next.1+sha-f6fa690');
 
 /**
  * @module
