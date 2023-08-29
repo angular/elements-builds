bdb3c04bea097e542a09ba88f2af3e24f649c2da
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 16:58:06 +0000

    4dabcad557 test(core): convert for block tests to template (#51514)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c5daa6c
+ * @license Angular v17.0.0-next.1+sha-4dabcad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c5daa6c');
+const VERSION = new Version('17.0.0-next.1+sha-4dabcad');
 
 /**
  * @module
