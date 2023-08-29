6e0f280bb015a8ebf5654bb614af3c22d3060ac7
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 22:34:54 +0000

    5d1d3b3d9f docs: migrate angular compiler options code to standalone (#51467)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-1baeca8
+ * @license Angular v17.0.0-next.1+sha-5d1d3b3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-1baeca8');
+const VERSION = new Version('17.0.0-next.1+sha-5d1d3b3');
 
 /**
  * @module
