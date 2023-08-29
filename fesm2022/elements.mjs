367d42563c116d46a07f8d1839ccef54540d471e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:07:59 +0000

    7d4550b7a7 docs: migrate accessibility guide and example to standalone (#51445)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-f6fa690
+ * @license Angular v17.0.0-next.1+sha-7d4550b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-f6fa690');
+const VERSION = new Version('17.0.0-next.1+sha-7d4550b');
 
 /**
  * @module
