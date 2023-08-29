7c2fea4a9465f4ec1016cb8838dfaa708d0f4d5c
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:37:05 +0000

    49415e8221 build: update security related package version for aio (#51562)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c62e680
+ * @license Angular v17.0.0-next.1+sha-49415e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c62e680');
+const VERSION = new Version('17.0.0-next.1+sha-49415e8');
 
 /**
  * @module
