793b88ce684cb9aae49cb4676061a1b26a22b130
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 19:19:16 +0000

    0b6aae8d6d build: use pin for devcontainer base image (#51556)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-a610eb1
+ * @license Angular v17.0.0-next.1+sha-0b6aae8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-a610eb1');
+const VERSION = new Version('17.0.0-next.1+sha-0b6aae8');
 
 /**
  * @module
