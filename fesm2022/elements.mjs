1d22cf1ac9e56d3a6cfb65c5723a4d8d2a872790
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:42:49 +0000

    c99624d078 docs: fix casing of GitHub in navigation (#51427) PR Close #51427

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-a5e98aa
+ * @license Angular v16.2.2+sha-c99624d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-a5e98aa');
+const VERSION = new Version('16.2.2+sha-c99624d');
 
 /**
  * @module
