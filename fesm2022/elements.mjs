5f3e58685d41c65215ea2a0cdb404288192be59f
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:39:35 +0000

    629f0146b8 docs: fix typos in search results component (#51433) PR Close #51433

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-e027a59
+ * @license Angular v17.0.0-next.1+sha-629f014
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-e027a59');
+const VERSION = new Version('17.0.0-next.1+sha-629f014');
 
 /**
  * @module
