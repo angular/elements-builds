bb66012d558ecd9d5edc6189095e00bcd34a07e2
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 00:05:28 +0000

    ba32d6ffd1 refactor(core): add basic prefetching runtime mechanism for defer blocks (#51529)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-3a19d6b
+ * @license Angular v17.0.0-next.1+sha-ba32d6f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-3a19d6b');
+const VERSION = new Version('17.0.0-next.1+sha-ba32d6f');
 
 /**
  * @module
