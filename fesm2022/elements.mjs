2d5e548f0cd03a8d12480a26813ae1a4ffd34ad2
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 16:57:23 +0000

    20d62603c2 fix(core): handle hydration of view containers that use component hosts as anchors (#51456)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-14941b9
+ * @license Angular v16.2.2+sha-20d6260
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-14941b9');
+const VERSION = new Version('16.2.2+sha-20d6260');
 
 /**
  * @module
