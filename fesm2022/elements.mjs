2eac001fc0dfd98d9c3575b03e59d34fa6b0798e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 00:11:07 +0000

    0f86a0b9e7 docs: migrate elements guide and code to standalone (#51455)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-fa29eb8
+ * @license Angular v17.0.0-next.1+sha-0f86a0b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-fa29eb8');
+const VERSION = new Version('17.0.0-next.1+sha-0f86a0b');
 
 /**
  * @module
