b9196711bc55b9dce4917c1fb189fb09eb368c4f
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:47:51 +0000

    b710ab63b4 docs: migrate two-way-binding to standalone (#51574)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-cfcd2e3
+ * @license Angular v17.0.0-next.2+sha-b710ab6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-cfcd2e3');
+const VERSION = new Version('17.0.0-next.2+sha-b710ab6');
 
 /**
  * @module
