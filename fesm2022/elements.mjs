f47db4fb321625f3405a667728113675eab82434
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:42:29 +0000

    78afe889fd refactor(devtools): migrate material legacy components to mdc (#51590)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-240b7cd
+ * @license Angular v17.0.0-next.2+sha-78afe88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-240b7cd');
+const VERSION = new Version('17.0.0-next.2+sha-78afe88');
 
 /**
  * @module
