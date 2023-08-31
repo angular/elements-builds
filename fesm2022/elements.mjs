3de39b2d1ab43b01b6b30a9c86708a37eee54d50
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 20:35:21 +0000

    05e4dbb859 refactor(core): Use intersections on branded types. (#49702)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-3c28506
+ * @license Angular v17.0.0-next.2+sha-05e4dbb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-3c28506');
+const VERSION = new Version('17.0.0-next.2+sha-05e4dbb');
 
 /**
  * @module
