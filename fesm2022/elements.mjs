0bf5975c2548daa7480af741d6476b74655d7145
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:25:41 +0000

    8a3479b9e4 fix(docs-infra): fix incomplete escaping (#51604)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-acd59ad
+ * @license Angular v17.0.0-next.2+sha-8a3479b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-acd59ad');
+const VERSION = new Version('17.0.0-next.2+sha-8a3479b');
 
 /**
  * @module
