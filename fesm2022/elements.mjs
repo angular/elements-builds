ab5c9c36ff9252fc2f79995ba43028d7a717a0bd
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:55:05 +0000

    f464e39364 fix(router): Ensure title observable gets latest values (#51561)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-e4d2018
+ * @license Angular v17.0.0-next.2+sha-f464e39
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-e4d2018');
+const VERSION = new Version('17.0.0-next.2+sha-f464e39');
 
 /**
  * @module
