0e2147beb8ab0902ea4aa00a16d320bd9f849e20
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 15:31:16 +0000

    af9cd84c13 build: migrate to node 18 (#51609)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.3+sha-f56b655
+ * @license Angular v16.2.3+sha-af9cd84
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.3+sha-f56b655');
+const VERSION = new Version('16.2.3+sha-af9cd84');
 
 /**
  * @module
