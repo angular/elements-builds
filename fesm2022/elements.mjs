bd0d06fb62da7605e6ebe0980a73aefef143a6b0
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:30:36 +0000

    201ab9d247 refactor(core): switch signals to a refcounting algorithm (#51226)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-b9ba6e6
+ * @license Angular v17.0.0-next.2+sha-201ab9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-b9ba6e6');
+const VERSION = new Version('17.0.0-next.2+sha-201ab9d');
 
 /**
  * @module
