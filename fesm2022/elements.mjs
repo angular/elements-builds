881a1030d8e04e8260261fc792f4cb95aa7d92a3
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 16:28:24 +0000

    6e94e6d7ea docs: add viewContainerRef example (#51573)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-5a32326
+ * @license Angular v17.0.0-next.2+sha-6e94e6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-5a32326');
+const VERSION = new Version('17.0.0-next.2+sha-6e94e6d');
 
 /**
  * @module
