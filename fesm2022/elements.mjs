e9c65df492bf3c9761c6a92b5f15f9a760a2d49e
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 20:18:13 +0000

    ca1dac2ada docs: update Angular CLI help [main] (#51661)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-df54b1c
+ * @license Angular v17.0.0-next.2+sha-ca1dac2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-df54b1c');
+const VERSION = new Version('17.0.0-next.2+sha-ca1dac2');
 
 /**
  * @module
