4f7a6a28eedb98537508696d76a60399c2e63c5e
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:17:58 +0000

    2d96dc27b7 ci: remove test job from circleci in favor of GHA (#51660)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-e804075
+ * @license Angular v17.0.0-next.2+sha-2d96dc2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-e804075');
+const VERSION = new Version('17.0.0-next.2+sha-2d96dc2');
 
 /**
  * @module
