6f212bdab207b7468b55c80dc0a8529b78919b1b
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 17:21:12 +0000

    52ff8a56d9 docs: migrate toh-pt2 to standalone (#51580)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-cfe2f1d
+ * @license Angular v17.0.0-next.2+sha-52ff8a5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-cfe2f1d');
+const VERSION = new Version('17.0.0-next.2+sha-52ff8a5');
 
 /**
  * @module
