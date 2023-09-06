304d421000df50ecde73aa5ccf8f83be8a4e8cc7
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:43:26 +0000

    fe0b793fdc refactor(router): Share more code in the route matching utilities (#51543)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-88a0af6
+ * @license Angular v17.0.0-next.2+sha-fe0b793
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-88a0af6');
+const VERSION = new Version('17.0.0-next.2+sha-fe0b793');
 
 /**
  * @module
