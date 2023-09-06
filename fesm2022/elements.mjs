c49fb179036e8de465d2d30c4b976d4c152a0cb9
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:42:14 +0000

    8ad21ccfb8 docs: Add info about hydration caching. (#51648)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-970d68f
+ * @license Angular v17.0.0-next.2+sha-8ad21cc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-970d68f');
+const VERSION = new Version('17.0.0-next.2+sha-8ad21cc');
 
 /**
  * @module
