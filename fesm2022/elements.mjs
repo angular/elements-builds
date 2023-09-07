3e6e20d05c3716d91e242908e9585b26fcff89a8
Andrew Kushnir <akushnir@google.com>
2023-09-07 20:59:15 +0000

    2579592be6 build: migrate `project_id` option to use `bes_instance_name` (#51696)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.4+sha-7ce89a2
+ * @license Angular v16.2.4+sha-2579592
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.4+sha-7ce89a2');
+const VERSION = new Version('16.2.4+sha-2579592');
 
 /**
  * @module
