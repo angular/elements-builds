53d6a1646a702961a083eb937561ce6140f80ef3
Andrew Kushnir <akushnir@google.com>
2023-09-08 17:40:13 +0000

    5277cb408b refactor(core): delegate afterRender errors to an ErrorHandler (#51662)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-e866d85
+ * @license Angular v17.0.0-next.3+sha-5277cb4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-e866d85');
+const VERSION = new Version('17.0.0-next.3+sha-5277cb4');
 
 /**
  * @module
