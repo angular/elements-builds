d69e0c6a0a9dc734044b57f7ebebd0e8c30c74e5
Andrew Kushnir <akushnir@google.com>
2023-09-08 16:40:58 +0000

    e866d85e95 docs: typo in what is angular (#51703)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-00bf052
+ * @license Angular v17.0.0-next.3+sha-e866d85
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-00bf052');
+const VERSION = new Version('17.0.0-next.3+sha-e866d85');
 
 /**
  * @module
