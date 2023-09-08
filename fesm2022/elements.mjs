fd699d5b70ed86edba11fb7fd7b0565f6d59e478
Andrew Kushnir <akushnir@google.com>
2023-09-08 22:01:05 +0000

    05762b9fff refactor(localize): ignore code duplication error with compiler package (#51500) (#51558)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-0d9fd6d
+ * @license Angular v17.0.0-next.3+sha-05762b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-0d9fd6d');
+const VERSION = new Version('17.0.0-next.3+sha-05762b9');
 
 /**
  * @module
