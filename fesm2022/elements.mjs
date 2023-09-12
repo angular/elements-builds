5cf8ed6aad8f10c8f1ff572856c35de8e9baf0f9
Andrew Kushnir <akushnir@google.com>
2023-09-12 20:05:43 +0000

    635318fd61 refactor: add readonly to public InjectionToken types (#51407)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-92113d7
+ * @license Angular v17.0.0-next.3+sha-635318f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-92113d7');
+const VERSION = new Version('17.0.0-next.3+sha-635318f');
 
 /**
  * @module
