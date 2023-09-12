efe9ff43788624eb74aff12d000074c930d132c8
Andrew Kushnir <akushnir@google.com>
2023-09-12 18:26:45 +0000

    caaabd8efe release: cut the zone.js-0.13.3 release (#51742)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-38c9f08
+ * @license Angular v17.0.0-next.3+sha-caaabd8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-38c9f08');
+const VERSION = new Version('17.0.0-next.3+sha-caaabd8');
 
 /**
  * @module
