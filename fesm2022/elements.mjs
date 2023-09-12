f513b0db2338debe0f4b076041b7cf267fde3462
Andrew Kushnir <akushnir@google.com>
2023-09-12 18:49:45 +0000

    52cc7f839b build: align with internal tsconfig options (#51728)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-caaabd8
+ * @license Angular v17.0.0-next.3+sha-52cc7f8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-caaabd8');
+const VERSION = new Version('17.0.0-next.3+sha-52cc7f8');
 
 /**
  * @module
