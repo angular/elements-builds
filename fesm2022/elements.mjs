1c00f492578c46c56b73d3c14ee80ddcbf0b5ece
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:34:27 +0000

    cbeef95c29 docs: migrate form-validation to standalone (#51709)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-4e0a575
+ * @license Angular v17.0.0-next.3+sha-cbeef95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-4e0a575');
+const VERSION = new Version('17.0.0-next.3+sha-cbeef95');
 
 /**
  * @module
