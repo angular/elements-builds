5c20231a4a5866c807a5c966b10a2bdf0526c79e
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:33:10 +0000

    4e0a575262 docs: migrate forms-overview to standalone (#51699)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-7b0bfc1
+ * @license Angular v17.0.0-next.3+sha-4e0a575
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-7b0bfc1');
+const VERSION = new Version('17.0.0-next.3+sha-4e0a575');
 
 /**
  * @module
