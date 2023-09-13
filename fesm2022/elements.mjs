a3dc6fe837f25e8a260f918e1941b7546ba5f4c7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-09-13 17:46:22 +0000

    a8ccc6f3b4 docs: release notes for the v16.2.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-59387ee
+ * @license Angular v17.0.0-next.3+sha-a8ccc6f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-59387ee');
+const VERSION = new Version('17.0.0-next.3+sha-a8ccc6f');
 
 /**
  * @module
