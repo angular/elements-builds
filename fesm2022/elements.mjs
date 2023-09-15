8c4ef1c1a1995bfbe205e0927398c567eedde5ee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 14:03:18 +0000

    3b46066ff0 refactor: ensure top-level symbols are marked as side-effect free (#51776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.5+sha-b0b004e
+ * @license Angular v16.2.5+sha-3b46066
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.5+sha-b0b004e');
+const VERSION = new Version('16.2.5+sha-3b46066');
 
 /**
  * @module
