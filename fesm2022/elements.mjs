70ef8fae75c76ee0adba82a8efdb0295ba7ca334
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:15:18 +0000

    d79db4a9e1 build: update io_bazel_rules_sass digest to 63d77ff (#51243)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-25b9b86
+ * @license Angular v17.0.0-next.4+sha-d79db4a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-25b9b86');
+const VERSION = new Version('17.0.0-next.4+sha-d79db4a');
 
 /**
  * @module
