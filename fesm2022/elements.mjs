654703c1ef818fd7d5b6f8f1f73c29b71dbefd30
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:56:12 +0000

    bf1f9a0897 test(compiler): attempt to deflake windows tests (#51804)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.5+sha-3b46066
+ * @license Angular v16.2.5+sha-bf1f9a0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.5+sha-3b46066');
+const VERSION = new Version('16.2.5+sha-bf1f9a0');
 
 /**
  * @module
