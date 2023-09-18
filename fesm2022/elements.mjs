9b96d9c62497ebadd040c933be041bd321d64945
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 10:07:08 +0000

    9bd1551c3f release: cut the zone.js-0.14.0 release (#51774)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-75380bf
+ * @license Angular v17.0.0-next.4+sha-9bd1551
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-75380bf');
+const VERSION = new Version('17.0.0-next.4+sha-9bd1551');
 
 /**
  * @module
