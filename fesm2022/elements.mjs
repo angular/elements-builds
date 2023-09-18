f85828dc07b91e4119c2f020e9359b5abb0dcef5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 18:52:55 +0000

    0b3e6a41d0 fix(router): Remove malformedUriErrorHandler from `ExtraOptions` (#51745)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-873e80b
+ * @license Angular v17.0.0-next.4+sha-0b3e6a4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-873e80b');
+const VERSION = new Version('17.0.0-next.4+sha-0b3e6a4');
 
 /**
  * @module
