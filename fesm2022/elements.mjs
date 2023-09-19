b4807c0fca9cda38225ae0d8590d521e5b04fc81
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:12:02 +0000

    9f3b549cf6 docs: Migrate Service Worker to Standalone (#51687)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-0b3e6a4
+ * @license Angular v17.0.0-next.4+sha-9f3b549
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-0b3e6a4');
+const VERSION = new Version('17.0.0-next.4+sha-9f3b549');
 
 /**
  * @module
