0b9f93e736c6ea16a9baad5cdee901c149835a16
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 16:51:28 +0000

    0ee0f780e4 build: update dependency jasmine to v5 (#51820)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-ba64e9c
+ * @license Angular v17.0.0-next.4+sha-0ee0f78
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-ba64e9c');
+const VERSION = new Version('17.0.0-next.4+sha-0ee0f78');
 
 /**
  * @module
