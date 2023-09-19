2aec678a1f767dd8fd017c012896fe8040bb5aed
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:33:10 +0000

    20cdb7730e build: update dependency puppeteer-core to v21 (#51802)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-8a2dc0f
+ * @license Angular v17.0.0-next.4+sha-20cdb77
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-8a2dc0f');
+const VERSION = new Version('17.0.0-next.4+sha-20cdb77');
 
 /**
  * @module
