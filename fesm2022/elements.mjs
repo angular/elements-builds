ad8eee3b69bdd0e30172de28f38a539f5b271da0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 18:08:59 +0000

    bc296a17e5 docs: release notes for the v16.2.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-34495b3
+ * @license Angular v17.0.0-next.4+sha-bc296a1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-34495b3');
+const VERSION = new Version('17.0.0-next.4+sha-bc296a1');
 
 /**
  * @module
