8fcd2388a7df58a3e1223562b4796203fed2e73c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 10:36:33 +0000

    e3532291c7 refactor(core): print the errors related to computing component dependencies to the console in local compilation mode (#51824)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-b44533b
+ * @license Angular v17.0.0-next.4+sha-e353229
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-b44533b');
+const VERSION = new Version('17.0.0-next.4+sha-e353229');
 
 /**
  * @module
