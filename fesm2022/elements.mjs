f523c952be54c214c6a8c5c834ede4a4467952d6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 16:43:32 +0000

    4ccd845081 refactor(router): Refactor loadChildren callback execution and privately export for SSR (#51818)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.5+sha-6e4d8f6
+ * @license Angular v16.2.5+sha-4ccd845
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.5+sha-6e4d8f6');
+const VERSION = new Version('16.2.5+sha-4ccd845');
 
 /**
  * @module
