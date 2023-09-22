8ff487be6994a87c86a08c2f0b9bf60fdfe03e59
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 17:01:32 +0000

    077534ef0f refactor(core): enabled using deps tracker in JIT compilation (#51415)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-cb54580
+ * @license Angular v17.0.0-next.5+sha-077534e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-cb54580');
+const VERSION = new Version('17.0.0-next.5+sha-077534e');
 
 /**
  * @module
