e17d4a108bbff5e882812a8d27c40bd359ce612e
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 16:58:24 +0000

    3c0577f991 perf(platform-browser): disable styles of removed components instead of removing (#51808)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-86e9146
+ * @license Angular v17.0.0-next.5+sha-3c0577f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-86e9146');
+const VERSION = new Version('17.0.0-next.5+sha-3c0577f');
 
 /**
  * @module
