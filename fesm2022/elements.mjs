1e5e047fcee4203e7a08463d3dc208d5c1516bf7
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 16:57:27 +0000

    917203d991 refactor(core): cleanup prefetch triggers when resource loading starts (#51856)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-0a4f18a
+ * @license Angular v17.0.0-next.5+sha-917203d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-0a4f18a');
+const VERSION = new Version('17.0.0-next.5+sha-917203d');
 
 /**
  * @module
