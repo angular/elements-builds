e86f5b3c934d0775a1c70dfc35139ee0926f1b46
Dylan Hunn <dylhunn@gmail.com>
2023-09-25 23:58:17 +0000

    487b9e118a test(core): disable flaky tests temporarily (#51895)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-d6bfebe
+ * @license Angular v17.0.0-next.5+sha-487b9e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-d6bfebe');
+const VERSION = new Version('17.0.0-next.5+sha-487b9e1');
 
 /**
  * @module
