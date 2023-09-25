a68060b87a7efc7ea1f13ac0e17c4172488bd058
Dylan Hunn <dylhunn@gmail.com>
2023-09-25 16:26:53 +0000

    16f5fc40a4 feat(core): support deferred viewport triggers (#51874)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-3cbb2a8
+ * @license Angular v17.0.0-next.5+sha-16f5fc4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-3cbb2a8');
+const VERSION = new Version('17.0.0-next.5+sha-16f5fc4');
 
 /**
  * @module
