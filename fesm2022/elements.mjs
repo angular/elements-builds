8528c165c4b26f4e86bbcb2546c65b59c9478e3d
Dylan Hunn <dylhunn@gmail.com>
2023-09-25 16:28:02 +0000

    baaaa6daf6 refactor(core): add `on immediate` support for defer blocks (#51630)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-16f5fc4
+ * @license Angular v17.0.0-next.5+sha-baaaa6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-16f5fc4');
+const VERSION = new Version('17.0.0-next.5+sha-baaaa6d');
 
 /**
  * @module
