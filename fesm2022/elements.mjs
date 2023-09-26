8382ff7662e563cad82cb862f7ede7fcc6ad5a58
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 18:09:23 +0000

    c3cb26527e refactor(compiler): Add source maps in template pipeline for expressions and additional ops (#51877)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-f91f222
+ * @license Angular v17.0.0-next.5+sha-c3cb265
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-f91f222');
+const VERSION = new Version('17.0.0-next.5+sha-c3cb265');
 
 /**
  * @module
