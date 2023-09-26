77d505b3cf14a5e64a7e3183f7fcecfbe4f7d4b5
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 22:30:57 +0000

    4b38e9a1ab refactor(core): add batching for defer blocks with `on idle` conditions (#51750)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-ce1b915
+ * @license Angular v17.0.0-next.5+sha-4b38e9a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-ce1b915');
+const VERSION = new Version('17.0.0-next.5+sha-4b38e9a');
 
 /**
  * @module
