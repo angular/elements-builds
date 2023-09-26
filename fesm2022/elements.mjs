ec39d154da696537abec60b534e60372e6a0b7bd
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:20:44 +0000

    dcaad169ec fix(service-worker): throw a critical error when `handleFetch` fails (#51885)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-8be2c48
+ * @license Angular v17.0.0-next.5+sha-dcaad16
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-8be2c48');
+const VERSION = new Version('17.0.0-next.5+sha-dcaad16');
 
 /**
  * @module
