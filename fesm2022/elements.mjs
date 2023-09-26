7f4cb844fb042423b307054a00e6c09772443713
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:17:30 +0000

    18e34ad91f ci: use pull request head sha instead of the sha provided in the github context (#51908)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-487b9e1
+ * @license Angular v17.0.0-next.5+sha-18e34ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-487b9e1');
+const VERSION = new Version('17.0.0-next.5+sha-18e34ad');
 
 /**
  * @module
