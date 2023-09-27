3b38c6e96d303d4197261d09187826e8385a3184
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:29:31 +0000

    ba0effd494 refactor(migrations): rename migration to remove version prefix (#51926)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-c7127b9
+ * @license Angular v17.0.0-next.5+sha-ba0effd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-c7127b9');
+const VERSION = new Version('17.0.0-next.5+sha-ba0effd');
 
 /**
  * @module
