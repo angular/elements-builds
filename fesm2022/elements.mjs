887cb85f522d6f3e1b8f7fe1be51cfafd90d7feb
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:33:13 +0000

    aedd8de7dd build: update scorecard action dependencies (#51899)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.6+sha-c3d901e
+ * @license Angular v16.2.6+sha-aedd8de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.6+sha-c3d901e');
+const VERSION = new Version('16.2.6+sha-aedd8de');
 
 /**
  * @module
