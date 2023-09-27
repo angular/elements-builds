33c4ad9e6fbdbe16c56f9990446680ca77f45286
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:33:46 +0000

    c52995d9f1 refactor(compiler): Output i18n message parameter maps in sorted order (#51911)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-a39010f
+ * @license Angular v17.0.0-next.5+sha-c52995d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-a39010f');
+const VERSION = new Version('17.0.0-next.5+sha-c52995d');
 
 /**
  * @module
