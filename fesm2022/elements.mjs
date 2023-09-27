addb21e19e9278953d409bc23be8dee0138d3e8a
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 17:41:18 +0000

    7a731c9536 refactor(core): add hydration support for built-in `if` and `switch` (#51915)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-acfea3c
+ * @license Angular v17.0.0-next.5+sha-7a731c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-acfea3c');
+const VERSION = new Version('17.0.0-next.5+sha-7a731c9');
 
 /**
  * @module
