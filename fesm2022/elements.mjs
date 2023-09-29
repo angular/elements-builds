53be3761e65fbf0223d5e4915e0968a287ad3457
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 22:35:29 +0000

    c4d77fde12 docs: fix link to refer to RxJS pipes (#51851) PR Close #51851

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-aa4069d
+ * @license Angular v17.0.0-next.6+sha-c4d77fd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-aa4069d');
+const VERSION = new Version('17.0.0-next.6+sha-c4d77fd');
 
 /**
  * @module
