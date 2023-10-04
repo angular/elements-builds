f8d81752f22c22427d0394c15a37fc86c8b9eba7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 14:41:54 +0000

    334fe60c17 refactor(core): Update view flags so only one number needs adjustment (#51839)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.7+sha-96141c6
+ * @license Angular v16.2.7+sha-334fe60
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.7+sha-96141c6');
+const VERSION = new Version('16.2.7+sha-334fe60');
 
 /**
  * @module
