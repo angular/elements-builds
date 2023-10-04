bb467f5e8545e03c624b54530d329c6673f861d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:47:56 +0000

    dde3fdabbd feat(common): upgrade warning to logged error for lazy-loaded LCP images using NgOptimizedImage (#52004)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-4611b70
+ * @license Angular v17.0.0-next.6+sha-dde3fda
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-4611b70');
+const VERSION = new Version('17.0.0-next.6+sha-dde3fda');
 
 /**
  * @module
