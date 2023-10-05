f66028915043b71c16adbfc8b99b1f01db000459
Andrew Scott <atscott01@gmail.com>
2023-10-05 21:47:33 +0000

    5b88d136af feat(core): mark core signal APIs as stable (#51821)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-5464bea
+ * @license Angular v17.0.0-next.7+sha-5b88d13
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-5464bea');
+const VERSION = new Version('17.0.0-next.7+sha-5b88d13');
 
 /**
  * @module
