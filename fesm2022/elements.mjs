7c009b44ff8011e3c714609c19eba61aa6988e13
Andrew Scott <atscott01@gmail.com>
2023-10-05 23:27:53 +0000

    05d1fac41f test(core): more tests around built-in for (#52045)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-5b88d13
+ * @license Angular v17.0.0-next.7+sha-05d1fac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-5b88d13');
+const VERSION = new Version('17.0.0-next.7+sha-05d1fac');
 
 /**
  * @module
