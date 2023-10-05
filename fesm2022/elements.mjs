8caafaca40e94b909b90e85ad7ef1cf82eaa4c1b
Andrew Scott <atscott01@gmail.com>
2023-10-05 18:14:09 +0000

    ced66d4007 revert: fix(core): allow `toSignal` in reactive contexts (#52049)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-2da3551
+ * @license Angular v17.0.0-next.7+sha-ced66d4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-2da3551');
+const VERSION = new Version('17.0.0-next.7+sha-ced66d4');
 
 /**
  * @module
