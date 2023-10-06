109a6941e631500e9b9b4465d008a8a6a82edf63
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:40:34 +0000

    f06d97e86d test(core): await all async work to be completed before checking test results (#52064)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-22fa9fe
+ * @license Angular v17.0.0-next.7+sha-f06d97e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-22fa9fe');
+const VERSION = new Version('17.0.0-next.7+sha-f06d97e');
 
 /**
  * @module
