b526a38fddc46ab7a1d9b3860dd9ccb973b9af81
Andrew Scott <atscott01@gmail.com>
2023-10-06 19:21:16 +0000

    048f400efc feat(core): add warnings for oversized images and lazy-lcp (#51846)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-b6b9eae
+ * @license Angular v17.0.0-next.7+sha-048f400
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-b6b9eae');
+const VERSION = new Version('17.0.0-next.7+sha-048f400');
 
 /**
  * @module
