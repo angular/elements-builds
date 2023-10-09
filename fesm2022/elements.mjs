9281bf710ecb63e4cee79db87264522371930b47
Andrew Scott <atscott01@gmail.com>
2023-10-09 17:16:13 +0000

    28a5925f53 fix(common): use === operator to match NgSwitch cases (#51504)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-e25006b
+ * @license Angular v17.0.0-next.7+sha-28a5925
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-e25006b');
+const VERSION = new Version('17.0.0-next.7+sha-28a5925');
 
 /**
  * @module
