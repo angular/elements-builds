4b7fc7b7d18b65b521358f3557dea2dd0d25e234
Andrew Scott <atscott01@gmail.com>
2023-10-10 17:46:49 +0000

    d487014785 fix(core): Remove no longer needed build rule related to removed migration (#52143)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-7c052bb
+ * @license Angular v17.0.0-next.7+sha-d487014
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-7c052bb');
+const VERSION = new Version('17.0.0-next.7+sha-d487014');
 
 /**
  * @module
