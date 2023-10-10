51a1e80d5b47477d657bdc97f25b6d922f52d83a
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:43:48 +0000

    21b085d refactor(compiler): capture unknown blocks inside switch (#52136)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-9bf6495
+ * @license Angular v17.0.0-next.7+sha-21b085d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-9bf6495');
+const VERSION = new Version('17.0.0-next.7+sha-21b085d');
 
 /**
  * @module
