f6abb4258331766287f352af7242d3f229372ece
Andrew Scott <atscott01@gmail.com>
2023-10-10 22:42:09 +0000

    ddd7212 fix(forms): reset() call with null values on nested group (#48830)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-da056a1
+ * @license Angular v17.0.0-next.7+sha-ddd7212
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-da056a1');
+const VERSION = new Version('17.0.0-next.7+sha-ddd7212');
 
 /**
  * @module
