c16c66ff4c658a15121bddb7154557b8c3d63058
Andrew Scott <atscott01@gmail.com>
2023-10-10 19:48:48 +0000

    1934524 feat(compiler): add docs extraction for type aliases (#52118)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-75d610d
+ * @license Angular v17.0.0-next.7+sha-1934524
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-75d610d');
+const VERSION = new Version('17.0.0-next.7+sha-1934524');
 
 /**
  * @module
