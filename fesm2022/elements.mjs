2ff2450fe88dbf5f61ec8940d02387b11d96f207
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:32:08 +0000

    2bfd28c396 docs: correct wording for Directive's decorator host property key description (#51977)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-aeb7d03
+ * @license Angular v16.2.8+sha-2bfd28c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-aeb7d03');
+const VERSION = new Version('16.2.8+sha-2bfd28c');
 
 /**
  * @module
