e0e5959cf24d15e8470f1856d950f7c59f78c908
Andrew Scott <atscott01@gmail.com>
2023-10-10 00:06:59 +0000

    b9a4941a32 build: update babel dependencies (#51898)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-c2f270c
+ * @license Angular v17.0.0-next.7+sha-b9a4941
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-c2f270c');
+const VERSION = new Version('17.0.0-next.7+sha-b9a4941');
 
 /**
  * @module
