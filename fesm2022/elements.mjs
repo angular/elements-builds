53dda9652f85351ae59652cca5237c4cc211c583
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:05:14 +0000

    5a4ccdf build: update cross-repo angular dependencies (#52133)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-d487014
+ * @license Angular v17.0.0-next.7+sha-5a4ccdf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-d487014');
+const VERSION = new Version('17.0.0-next.7+sha-5a4ccdf');
 
 /**
  * @module
