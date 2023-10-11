8977087f6d3750acc28a66ce54dd6173a9f2b883
Andrew Scott <atscott01@gmail.com>
2023-10-11 19:42:49 +0000

    c2b1a24 refactor(compiler-cli): remove `MethodIdentifier` type (#49611)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-221f882
+ * @license Angular v17.1.0-next.0+sha-c2b1a24
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-221f882');
+const VERSION = new Version('17.1.0-next.0+sha-c2b1a24');
 
 /**
  * @module
