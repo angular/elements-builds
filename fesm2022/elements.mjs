6107a1e365a08f2f16e63cfc0766c3b9cdbaab87
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 17:34:07 +0000

    20e7e21 fix(core): do not remove used ng-template nodes in control flow migration (#52186)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-37c8fd7
+ * @license Angular v17.1.0-next.0+sha-20e7e21
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-37c8fd7');
+const VERSION = new Version('17.1.0-next.0+sha-20e7e21');
 
 /**
  * @module
