5aa24018834e3fbc1ddcc81a5814afdc3ca4c61c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 10:42:34 +0000

    2bf088e Revert "refactor(compiler-cli): remove `MethodIdentifier` type (#49611)" (#52174)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-2e5ee1c
+ * @license Angular v17.0.0-next.8+sha-2bf088e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-2e5ee1c');
+const VERSION = new Version('17.0.0-next.8+sha-2bf088e');
 
 /**
  * @module
