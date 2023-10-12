63fecd281655157e5e402857b2086730778ecf19
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 13:51:21 +0000

    65b4604 fix(common): missing space in ngSwitch equality warning (#52180)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-2bf088e
+ * @license Angular v17.0.0-next.8+sha-65b4604
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-2bf088e');
+const VERSION = new Version('17.0.0-next.8+sha-65b4604');
 
 /**
  * @module
