a20805b4891a4974b746f4a1cd9e79bb1d8dfe2d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 12:14:37 +0000

    d75eaa7375 refactor(core): remove unused helper for setting `LView` for reactive consumer (#52192)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.9+sha-630e906
+ * @license Angular v16.2.9+sha-d75eaa7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.9+sha-630e906');
+const VERSION = new Version('16.2.9+sha-d75eaa7');
 
 /**
  * @module
