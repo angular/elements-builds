1b9e12324d51aa356352b209457af5983b964fe5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 09:25:52 +0000

    e90f5e5 docs: Phase 2 Sample Code Conversions to Standalone (#51918)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-99e7629
+ * @license Angular v17.0.0-next.8+sha-e90f5e5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-99e7629');
+const VERSION = new Version('17.0.0-next.8+sha-e90f5e5');
 
 /**
  * @module
