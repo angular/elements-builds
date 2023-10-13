3aaee0c35ea09886b829ac6c42757f5d9f21387b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 10:30:41 +0000

    6fd863d refactor(core): cleanup `DebugElement` (#50896)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-e90f5e5
+ * @license Angular v17.0.0-next.8+sha-6fd863d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-e90f5e5');
+const VERSION = new Version('17.0.0-next.8+sha-6fd863d');
 
 /**
  * @module
