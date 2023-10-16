959a4b2e542745ccf22b27d198e51916d4b80cbb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 10:30:40 +0000

    9d8af45 refactor(core): run internal work outside of public afterRender phases (#52145)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-4e4ad5a
+ * @license Angular v17.1.0-next.0+sha-9d8af45
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-4e4ad5a');
+const VERSION = new Version('17.1.0-next.0+sha-9d8af45');
 
 /**
  * @module
