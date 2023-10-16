969401f736cc53fd4c8f5aaf63f331a7b024f0dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:28:59 +0000

    302ab34 fix(compiler): avoid error in template parser for tag names that can occur in object prototype (#52225)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-4ed6ff1
+ * @license Angular v17.1.0-next.0+sha-302ab34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-4ed6ff1');
+const VERSION = new Version('17.1.0-next.0+sha-302ab34');
 
 /**
  * @module
