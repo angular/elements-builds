da2620375d6f2c2b12767e6e3beabdd66df70de1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 17:31:58 +0000

    9f4927e refactor(compiler): Fix i18n placeholders for slef-closing elements (#52195)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-2003caf
+ * @license Angular v17.1.0-next.0+sha-9f4927e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-2003caf');
+const VERSION = new Version('17.1.0-next.0+sha-9f4927e');
 
 /**
  * @module
