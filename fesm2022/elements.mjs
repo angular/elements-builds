d602e3db7ffef3216d0df427f51a28e34422bc9c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 14:34:38 +0000

    81018d7562 docs: add missing next steps section (#52220)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.9+sha-6997c52
+ * @license Angular v16.2.9+sha-81018d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.9+sha-6997c52');
+const VERSION = new Version('16.2.9+sha-81018d7');
 
 /**
  * @module
