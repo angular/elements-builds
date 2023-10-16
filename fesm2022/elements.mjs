56aa1343d7ef71e5ca058434742f844ecdf19526
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 15:49:36 +0000

    4ed6ff1 refactor(core): remove hack on render3 (#51238)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-403e0f2
+ * @license Angular v17.1.0-next.0+sha-4ed6ff1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-403e0f2');
+const VERSION = new Version('17.1.0-next.0+sha-4ed6ff1');
 
 /**
  * @module
