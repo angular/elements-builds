7ae8f9490f9a214fe35654fa74d6ab967e008357
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 09:23:05 +0000

    7466004 perf(platform-browser): only append style element on creation (#52237)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-9bc9464
+ * @license Angular v17.1.0-next.0+sha-7466004
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-9bc9464');
+const VERSION = new Version('17.1.0-next.0+sha-7466004');
 
 /**
  * @module
