69efacef1a0ebb1e37f30b70a393c40b4e37f21a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 16:17:28 +0000

    d811d82 build: update babel dependencies to v7.23.2 (#52236)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-a318e6a
+ * @license Angular v17.0.0-next.8+sha-d811d82
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-a318e6a');
+const VERSION = new Version('17.0.0-next.8+sha-d811d82');
 
 /**
  * @module
