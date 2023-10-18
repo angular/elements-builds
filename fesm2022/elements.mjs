80221df20bbae61c567e06d50a211764d7373248
Dylan Hunn <dylhunn@gmail.com>
2023-10-18 19:18:46 +0000

    b4c1ffb refactor(forms): Log a warning when `FormGroup` keys include a dot. (#50642)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-866d0f4
+ * @license Angular v17.0.0-rc.0+sha-b4c1ffb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-866d0f4');
+const VERSION = new Version('17.0.0-rc.0+sha-b4c1ffb');
 
 /**
  * @module
