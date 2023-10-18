fc8096aac4897cc807a7c7d42135411bf607b470
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 16:21:35 +0000

    eccbf36 docs: Update BAZEL.md with correct bazelrc documentation link (#52090) PR Close #52090

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-37768da
+ * @license Angular v17.0.0-next.8+sha-eccbf36
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-37768da');
+const VERSION = new Version('17.0.0-next.8+sha-eccbf36');
 
 /**
  * @module
