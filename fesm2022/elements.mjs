de83ed91dbeed5c822848347b6d2d04cbddd65e0
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:34:22 +0000

    a207421 refactor(core): check "skip hydration" condition only when hydration info is present (#52221)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-b4c1ffb
+ * @license Angular v17.0.0-rc.0+sha-a207421
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-b4c1ffb');
+const VERSION = new Version('17.0.0-rc.0+sha-a207421');
 
 /**
  * @module
