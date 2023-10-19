c33477003ef079ab8af56524a1fb4604852fb557
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 17:32:23 +0000

    3278966 fix(router): Ensure newly resolved data is inherited by child routes (#52167)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-8fff07c
+ * @license Angular v17.1.0-next.0+sha-3278966
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-8fff07c');
+const VERSION = new Version('17.1.0-next.0+sha-3278966');
 
 /**
  * @module
