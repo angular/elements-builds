1c7af104c2987136322940656b38865b2b4e1e63
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:33:05 +0000

    fbf49b5e9c build: update all non-major dependencies (#52259)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.10+sha-bf70bf6
+ * @license Angular v16.2.10+sha-fbf49b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.10+sha-bf70bf6');
+const VERSION = new Version('16.2.10+sha-fbf49b5');
 
 /**
  * @module
