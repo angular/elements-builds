3f4274b995ed81f9d299915d656b3a6ed061c04b
Dylan Hunn <dylhunn@gmail.com>
2023-10-23 20:53:23 +0000

    47ab069 docs: update Angular CLI help [main] (#52330)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-67b9720
+ * @license Angular v17.1.0-next.0+sha-47ab069
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-67b9720');
+const VERSION = new Version('17.1.0-next.0+sha-47ab069');
 
 /**
  * @module
