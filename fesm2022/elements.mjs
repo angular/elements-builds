9cbe99ac21c75f5cb955449ebf2889aaae90ce98
Dylan Hunn <dylhunn@gmail.com>
2023-10-23 16:39:31 +0000

    569f57d build: update lockfile (#52270)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-4dce876
+ * @license Angular v17.0.0-rc.0+sha-569f57d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-4dce876');
+const VERSION = new Version('17.0.0-rc.0+sha-569f57d');
 
 /**
  * @module
