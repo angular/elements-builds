81db5408c3a7a4e62ad2beb795e78745f20041cc
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:41:51 +0000

    c993e9a fix(migrations): handle nested classes in control flow migration (#52309)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-d82d586
+ * @license Angular v17.1.0-next.0+sha-c993e9a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-d82d586');
+const VERSION = new Version('17.1.0-next.0+sha-c993e9a');
 
 /**
  * @module
