73c6e474308494607542a1eb600910dc765b5de6
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 16:34:19 +0000

    d2f8412 refactor(platform-server): replace `global` usages with `globalThis` (#52342)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-96ad3bf
+ * @license Angular v17.0.0-rc.0+sha-d2f8412
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-96ad3bf');
+const VERSION = new Version('17.0.0-rc.0+sha-d2f8412');
 
 /**
  * @module
