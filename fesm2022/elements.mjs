89388de51d0946c9f09bb33a307199cb679cbf45
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:57:37 +0000

    76152a5 fix(core): Ensure backwards-referenced transplanted views are refreshed (#51854)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-c993e9a
+ * @license Angular v17.1.0-next.0+sha-76152a5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-c993e9a');
+const VERSION = new Version('17.1.0-next.0+sha-76152a5');
 
 /**
  * @module
