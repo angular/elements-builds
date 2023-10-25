ae353f319e75664d9b3f8e0cd4bbea89cc4f68fb
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 23:59:21 +0000

    fc9ba39 refactor: fix a number of typos throughout the codebase (#52249)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-31c7bc1
+ * @license Angular v17.1.0-next.0+sha-fc9ba39
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-31c7bc1');
+const VERSION = new Version('17.1.0-next.0+sha-fc9ba39');
 
 /**
  * @module
