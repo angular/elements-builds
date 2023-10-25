371315b1d20f3addea27715f49812a331478d0b0
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:47:11 +0000

    16bf04b release: cut the zone.js-0.14.1 release (#52374)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-4ca2cea
+ * @license Angular v17.1.0-next.0+sha-16bf04b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-4ca2cea');
+const VERSION = new Version('17.1.0-next.0+sha-16bf04b');
 
 /**
  * @module
