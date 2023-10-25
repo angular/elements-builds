81dc516392e6631e2a16034d679d81b5d017589b
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 18:10:24 +0000

    b79b4aca91 fix(router): `RouterTestingHarness` should throw if a component is expected but navigation fails (#52357)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.10+sha-019a0f4
+ * @license Angular v16.2.10+sha-b79b4ac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.10+sha-019a0f4');
+const VERSION = new Version('16.2.10+sha-b79b4ac');
 
 /**
  * @module
