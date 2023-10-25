4cdf1f0c27abe1126c6450c32b5c75ffd40f8be5
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 17:58:32 +0000

    a803c89 fix(core): load global utils before creating platform injector in the standalone case (#52365)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-4c878f9
+ * @license Angular v17.0.0-rc.0+sha-a803c89
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-4c878f9');
+const VERSION = new Version('17.0.0-rc.0+sha-a803c89');
 
 /**
  * @module
