c5b366984e4ca7ceb67e20ebdbc65d5cac90ed03
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 16:46:26 +0000

    4c878f9 fix(migrations): Add support for nested structures inside a switch statement (#52358)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-1dd8558
+ * @license Angular v17.0.0-rc.0+sha-4c878f9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-1dd8558');
+const VERSION = new Version('17.0.0-rc.0+sha-4c878f9');
 
 /**
  * @module
