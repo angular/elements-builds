3975f5391c7f2db5fe49571659a3e01555d12268
Dylan Hunn <github@dylanhunn.com>
2023-10-25 21:35:21 +0000

    4ca2cea docs: release notes for the v16.2.11 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-0037c21
+ * @license Angular v17.1.0-next.0+sha-4ca2cea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-0037c21');
+const VERSION = new Version('17.1.0-next.0+sha-4ca2cea');
 
 /**
  * @module
