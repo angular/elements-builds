ad81efce3ffe26b29e1664dd92b88d375c142e3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 21:18:19 +0000

    7888819 build: update cross-repo angular dependencies (#52191)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-cb8a741
+ * @license Angular v17.1.0-next.0+sha-7888819
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-cb8a741');
+const VERSION = new Version('17.1.0-next.0+sha-7888819');
 
 /**
  * @module
