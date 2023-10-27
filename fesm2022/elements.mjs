67f40b90fcb306a63cb61e3708ecdc1cff9e2397
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:55:58 +0000

    a51bb6927d docs(docs-infra): fix typo in tap into the observable tutorial example (#52359)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-5ff99f2
+ * @license Angular v16.2.11+sha-a51bb69
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-5ff99f2');
+const VERSION = new Version('16.2.11+sha-a51bb69');
 
 /**
  * @module
