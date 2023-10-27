085ad91b65964fb4610bacc553d199cf64967590
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:24:35 +0000

    4461cef fix(core): handle trackBy and aliased index in control flow migration (#52423)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-8e17dd7
+ * @license Angular v17.0.0-rc.1+sha-4461cef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-8e17dd7');
+const VERSION = new Version('17.0.0-rc.1+sha-4461cef');
 
 /**
  * @module
