0c7bdfab6b511fb22062920a3bf3070f69efc972
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 20:07:39 +0000

    fa03f0a fix(migrations): Ensure control flow migration ignores new block syntax (#52402)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-e06b73f
+ * @license Angular v17.1.0-next.0+sha-fa03f0a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-e06b73f');
+const VERSION = new Version('17.1.0-next.0+sha-fa03f0a');
 
 /**
  * @module
