4dcf2cdd8b755d24825742e1ddec3d0b4873bc87
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 22:21:32 +0000

    07623ca docs: update SSR docs (#52408)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-fa03f0a
+ * @license Angular v17.1.0-next.0+sha-07623ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-fa03f0a');
+const VERSION = new Version('17.1.0-next.0+sha-07623ca');
 
 /**
  * @module
