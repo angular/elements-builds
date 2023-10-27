52b39313c750b2b34f299c783e3bc0cb315e9fb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:12:51 +0000

    b0bc25d refactor(localize): mark `TargetMessage` and `MessageId` as public. (#52418)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-07623ca
+ * @license Angular v17.1.0-next.0+sha-b0bc25d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-07623ca');
+const VERSION = new Version('17.1.0-next.0+sha-b0bc25d');
 
 /**
  * @module
