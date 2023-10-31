7f2840ff33049bb77843c5b92fab1c0326ea7744
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:05:31 +0000

    bf5bda4 refactor(language-service): Add quick info for built in control flow/blocks (#52386)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-73c5d1c
+ * @license Angular v17.1.0-next.0+sha-bf5bda4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-73c5d1c');
+const VERSION = new Version('17.1.0-next.0+sha-bf5bda4');
 
 /**
  * @module
