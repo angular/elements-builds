8e2474387578873ad879ac49e58ae19db43419f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:29:47 +0000

    d8280ac refactor(core): Remove RootViewRef<T> because it is the same as ViewRef<T> (#52430)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-e355f05
+ * @license Angular v17.0.0-rc.1+sha-d8280ac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-e355f05');
+const VERSION = new Version('17.0.0-rc.1+sha-d8280ac');
 
 /**
  * @module
