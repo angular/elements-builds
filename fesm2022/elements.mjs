7209166bc6dd745768169a85e8a3750593274c54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 19:54:05 +0000

    595009e refactor(compiler): Implement the remaining `defer on` triggers (#52387)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-e74b578
+ * @license Angular v17.0.0-rc.1+sha-595009e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-e74b578');
+const VERSION = new Version('17.0.0-rc.1+sha-595009e');
 
 /**
  * @module
