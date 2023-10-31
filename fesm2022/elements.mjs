7b89504f786f2c2611520293f266109e8cd4798f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 21:27:36 +0000

    078ebea fix(core): mutation bug in `getDependenciesFromInjectable` (#52450)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-c5980d6
+ * @license Angular v17.1.0-next.0+sha-078ebea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-c5980d6');
+const VERSION = new Version('17.1.0-next.0+sha-078ebea');
 
 /**
  * @module
