564da78268526924a7dd895c7f047d8f71df9f78
Andrew Scott <atscott01@gmail.com>
2023-10-31 19:04:36 +0000

    df0902d212 docs: update home page for live stream event (#52456)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-a51bb69
+ * @license Angular v16.2.11+sha-df0902d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-a51bb69');
+const VERSION = new Version('16.2.11+sha-df0902d');
 
 /**
  * @module
