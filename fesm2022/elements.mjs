d013a0350ff47145286fbea9388db1a2696468e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 23:30:33 +0000

    a6b8752 docs: update examples to use `buildTarget` instead of `browserTarget` (#52361)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-cce84d2
+ * @license Angular v17.1.0-next.0+sha-a6b8752
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-cce84d2');
+const VERSION = new Version('17.1.0-next.0+sha-a6b8752');
 
 /**
  * @module
