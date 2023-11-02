bb8b3ef70c64ae4e7acc752e1ae8cf915075a77f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:38:44 +0000

    0754122 build: update actions/checkout digest to b4ffde6 (#52252)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-356da9c
+ * @license Angular v17.0.0-rc.1+sha-0754122
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-356da9c');
+const VERSION = new Version('17.0.0-rc.1+sha-0754122');
 
 /**
  * @module
