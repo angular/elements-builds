5ba9f5d05eab62af9a2b510c6a07fb35a3e3c57a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 21:46:50 +0000

    ace0a683f6 release: cut the v16.2.12 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-e092184
+ * @license Angular v16.2.12+sha-ace0a68
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-e092184');
+const VERSION = new Version('16.2.12+sha-ace0a68');
 
 /**
  * @module
