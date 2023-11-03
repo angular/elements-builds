e50e9270cec1b867298d00d2dadf100cc2af96a0
Andrew Scott <atscott01@gmail.com>
2023-11-03 16:43:15 +0000

    a2ba548 fix(core): use TNode instead of LView for mapping injector providers (#52436)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-236b448
+ * @license Angular v17.0.0-rc.2+sha-a2ba548
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-236b448');
+const VERSION = new Version('17.0.0-rc.2+sha-a2ba548');
 
 /**
  * @module
