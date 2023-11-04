4d31f2ca6aa40964427d006118bd5bb64d89f330
Andrew Scott <atscott01@gmail.com>
2023-11-04 00:43:51 +0000

    1c92609 test(devtools): fix transformInjectorResolutionPathsIntoTree tests (#52489)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-de13610
+ * @license Angular v17.0.0-rc.2+sha-1c92609
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-de13610');
+const VERSION = new Version('17.0.0-rc.2+sha-1c92609');
 
 /**
  * @module
