fe698c710f99041408570ff7e1a82b971293dc6f
Andrew Scott <atscott01@gmail.com>
2023-11-06 20:39:38 +0000

    a86661f refactor(devtools): update Angular logo for extension and loading animation (#52546)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-590c618
+ * @license Angular v17.0.0-rc.2+sha-a86661f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-590c618');
+const VERSION = new Version('17.0.0-rc.2+sha-a86661f');
 
 /**
  * @module
