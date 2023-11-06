ef57eb234ddebb645621eff7c83957da03421cff
Andrew Scott <atscott01@gmail.com>
2023-11-06 17:56:08 +0000

    89cfadd refactor(compiler): Add more comments for i18n template pipeline phases (#52503)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-f2fbe86
+ * @license Angular v17.1.0-next.0+sha-89cfadd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-f2fbe86');
+const VERSION = new Version('17.1.0-next.0+sha-89cfadd');
 
 /**
  * @module
