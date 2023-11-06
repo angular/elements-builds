57b7e4a7fef7eaf30d8c47fd0603c8b453a16466
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:15:07 +0000

    499f09ac56 docs: add angular.dev banner (#52535) PR Close #52535

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.12+sha-ace0a68
+ * @license Angular v16.2.12+sha-499f09a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.12+sha-ace0a68');
+const VERSION = new Version('16.2.12+sha-499f09a');
 
 /**
  * @module
