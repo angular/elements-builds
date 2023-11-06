03b409d38d2de89d89383d198ae40e56efe20c51
Andrew Scott <atscott@google.com>
2023-11-06 19:15:23 +0000

    cab7e00 docs: add angular.dev (#52534)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-2c3daeb
+ * @license Angular v17.0.0-rc.2+sha-cab7e00
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-2c3daeb');
+const VERSION = new Version('17.0.0-rc.2+sha-cab7e00');
 
 /**
  * @module
