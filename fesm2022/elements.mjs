2ea69f03cba396588d626bcacfb2ff4023f48aa4
Andrew Scott <atscott01@gmail.com>
2023-11-06 23:58:23 +0000

    d4c20ac refactor(core): Add warning when signal equality is false for object.is (#52532)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-a86661f
+ * @license Angular v17.0.0-rc.2+sha-d4c20ac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-a86661f');
+const VERSION = new Version('17.0.0-rc.2+sha-d4c20ac');
 
 /**
  * @module
