bb42343f83777d31b0515061acbbd684772ca161
Andrew Scott <atscott01@gmail.com>
2023-11-07 00:12:01 +0000

    6b83328 release: cut the v17.0.0-rc.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-d4c20ac
+ * @license Angular v17.0.0-rc.3+sha-6b83328
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-d4c20ac');
+const VERSION = new Version('17.0.0-rc.3+sha-6b83328');
 
 /**
  * @module
