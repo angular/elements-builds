d933df379d1ac85d1a60341d645c0b5eb1735dca
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-11-08 21:00:40 +0000

    9cac4ee docs: release notes for the v17.0.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-291ba38
+ * @license Angular v17.1.0-next.0+sha-9cac4ee
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-291ba38');
+const VERSION = new Version('17.1.0-next.0+sha-9cac4ee');
 
 /**
  * @module
