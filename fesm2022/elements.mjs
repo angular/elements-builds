c00fd7aae90ca4cd7a7cf2ba66eb0419f2d2e800
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 15:27:30 +0000

    fc6b2f2 docs: add the v17 blog post link to the changelog (#52675)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0+sha-fecd141
+ * @license Angular v17.0.0+sha-fc6b2f2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0+sha-fecd141');
+const VERSION = new Version('17.0.0+sha-fc6b2f2');
 
 /**
  * @module
