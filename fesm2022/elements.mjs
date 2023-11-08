d5523e5d2ef5bbc025debb2dfe973b561d8693d8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 16:12:22 +0000

    3f2501e build: bump in-memory-web-api to v17 (#52676)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-1e87ce2
+ * @license Angular v17.1.0-next.0+sha-3f2501e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-1e87ce2');
+const VERSION = new Version('17.1.0-next.0+sha-3f2501e');
 
 /**
  * @module
