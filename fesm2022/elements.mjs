e63eaf872ba40f259e41073354b74c58e62455e5
Andrew Scott <atscott01@gmail.com>
2023-11-08 16:56:57 +0000

    1d31a0c refactor(core): sanitize reactivity tests (#52632)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0+sha-c30dde2
+ * @license Angular v17.0.0+sha-1d31a0c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0+sha-c30dde2');
+const VERSION = new Version('17.0.0+sha-1d31a0c');
 
 /**
  * @module
