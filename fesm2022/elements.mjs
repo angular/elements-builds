48375badfd01bbebb6ba522bdce36b0f9c64dd71
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:55:13 +0000

    a548c03 fix(compiler-cli): incorrect inferred type of for loop implicit variables (#52732)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-7d6752a
+ * @license Angular v17.0.1+sha-a548c03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-7d6752a');
+const VERSION = new Version('17.0.1+sha-a548c03');
 
 /**
  * @module
