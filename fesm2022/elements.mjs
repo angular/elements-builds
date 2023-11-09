f00ca417ecc666d45937db343463b266be2b92f4
Joey Perrott <josephperrott@gmail.com>
2023-11-09 16:03:06 +0000

    b651648 docs: make homepade example accessible (#52578) PR Close #52578

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-a548c03
+ * @license Angular v17.0.1+sha-b651648
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-a548c03');
+const VERSION = new Version('17.0.1+sha-b651648');
 
 /**
  * @module
