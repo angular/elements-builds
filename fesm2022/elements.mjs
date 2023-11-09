b6eadb4ae23472b0831494c653f391e172e04c41
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 17:03:52 +0000

    f61519a refactor(core): delay applying component metadata until it's needed in tests (#52708)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-b651648
+ * @license Angular v17.0.1+sha-f61519a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-b651648');
+const VERSION = new Version('17.0.1+sha-f61519a');
 
 /**
  * @module
