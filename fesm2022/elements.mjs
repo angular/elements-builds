1951aaa986827fc2ed7ee03418a66de00b15aded
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:41:42 +0000

    7d6752a docs(docs-infra): improve decorator deprecated property display in documentation (#52041)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-96d554d
+ * @license Angular v17.0.1+sha-7d6752a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-96d554d');
+const VERSION = new Version('17.0.1+sha-7d6752a');
 
 /**
  * @module
