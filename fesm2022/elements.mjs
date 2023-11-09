19fdded6df1d28e3f6865f5ebc4053998bce39ab
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:10:48 +0000

    27bf371 docs: removed the broken link (#52743)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-56ec37d
+ * @license Angular v17.0.1+sha-27bf371
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-56ec37d');
+const VERSION = new Version('17.0.1+sha-27bf371');
 
 /**
  * @module
