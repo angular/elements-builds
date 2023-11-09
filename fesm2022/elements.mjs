24aa011d46ad9c97eca3cc172af16cbfb1dd5bc3
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:05:31 +0000

    96d554d docs: fix incorrect tag reference (#52586)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-4626691
+ * @license Angular v17.0.1+sha-96d554d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-4626691');
+const VERSION = new Version('17.0.1+sha-96d554d');
 
 /**
  * @module
