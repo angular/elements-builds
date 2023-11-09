353aac805848c384586b7c4af8d27662adf4c5a5
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 23:00:06 +0000

    c01af43 refactor(migrations): code clean up and add comments on exported functions (#52755)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-01efb1b
+ * @license Angular v17.0.2+sha-c01af43
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-01efb1b');
+const VERSION = new Version('17.0.2+sha-c01af43');
 
 /**
  * @module
