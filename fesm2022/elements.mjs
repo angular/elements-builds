018fe68741fc8433a9778935c355a0d1476f62ca
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-10 17:09:11 +0000

    70fe5e6 refactor(compiler): Handle trailing spaces in ICU placeholders (#52698)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-94096c6
+ * @license Angular v17.1.0-next.0+sha-70fe5e6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-94096c6');
+const VERSION = new Version('17.1.0-next.0+sha-70fe5e6');
 
 /**
  * @module
