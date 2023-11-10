bb36b5f24fd88f2e71ab1b55c271567d91d170bd
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-10 18:41:35 +0000

    3cf18bb fix(devtools): check for all new DI debug APIs before trying to determine resolution path providers (#52791)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-70fe5e6
+ * @license Angular v17.1.0-next.0+sha-3cf18bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-70fe5e6');
+const VERSION = new Version('17.1.0-next.0+sha-3cf18bb');
 
 /**
  * @module
