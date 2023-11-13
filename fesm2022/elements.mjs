4172797556f928b8e18adeb3e74910ea975b50b4
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 15:28:07 +0000

    e1f84a3 fix(migrations): passed in paths will be respected in nx workspaces (#52796)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-54a7b33
+ * @license Angular v17.0.2+sha-e1f84a3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-54a7b33');
+const VERSION = new Version('17.0.2+sha-e1f84a3');
 
 /**
  * @module
