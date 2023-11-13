5d12b23451352d4272c72ce5bc07e2a054a9b9ed
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:00:55 +0000

    e46162d docs: update readme links to target angular.dev documentation (#52772)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-e1f84a3
+ * @license Angular v17.0.2+sha-e46162d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-e1f84a3');
+const VERSION = new Version('17.0.2+sha-e46162d');
 
 /**
  * @module
