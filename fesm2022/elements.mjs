e14609a0f7e5469d16644b9dcc56021892760196
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 22:38:11 +0000

    d033540 fix(migrations): Add support for bound versions of NgIfElse and NgIfThenElse (#52869)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-c8ff0be
+ * @license Angular v17.0.2+sha-d033540
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-c8ff0be');
+const VERSION = new Version('17.0.2+sha-d033540');
 
 /**
  * @module
