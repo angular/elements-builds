ef919e18aae13db03ee227c51c87de56c64ad9da
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 17:32:34 +0000

    30757bd refactor(animations): `EnvironmentProviders` for `provideAnimationsAsync` (#52862)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-f5872c9
+ * @license Angular v17.0.2+sha-30757bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-f5872c9');
+const VERSION = new Version('17.0.2+sha-30757bd');
 
 /**
  * @module
