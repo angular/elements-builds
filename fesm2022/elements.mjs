9ba19265180ed0070969cd0b3b9f7220dded2187
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 19:03:45 +0000

    9e2c3bb docs(forms): replace fb with formBuilder (#52795)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-726530a
+ * @license Angular v17.1.0-next.0+sha-9e2c3bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-726530a');
+const VERSION = new Version('17.1.0-next.0+sha-9e2c3bb');
 
 /**
  * @module
