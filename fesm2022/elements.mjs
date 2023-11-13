1755145710e727f54a669b7328788322ef8eb355
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 23:30:58 +0000

    15a825c docs: fix provideRouter typo (#52798)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-42805e3
+ * @license Angular v17.1.0-next.0+sha-15a825c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-42805e3');
+const VERSION = new Version('17.1.0-next.0+sha-15a825c');
 
 /**
  * @module
