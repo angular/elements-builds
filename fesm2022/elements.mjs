eda57dd1acb46ea6de216129abd8d55618ee9076
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 22:14:36 +0000

    c8ff0be ci: correct label for bazel-saucelabs job (#52873)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-a00beec
+ * @license Angular v17.0.2+sha-c8ff0be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-a00beec');
+const VERSION = new Version('17.0.2+sha-c8ff0be');
 
 /**
  * @module
