397d3dc0a022fca9c78f7ab0cc088aa92ed9ae7c
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 20:22:53 +0000

    e639bf4 release: bump DevTools version to 1.0.9 (#52938)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-8f5124e
+ * @license Angular v17.1.0-next.0+sha-e639bf4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-8f5124e');
+const VERSION = new Version('17.1.0-next.0+sha-e639bf4');
 
 /**
  * @module
