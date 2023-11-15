cd92053defd28ffbc83507162531eee118b75876
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 21:18:27 +0000

    ec2d6e7 fix(compiler): changed after checked error in for loops (#52935)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-93c0dd8
+ * @license Angular v17.1.0-next.0+sha-ec2d6e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-93c0dd8');
+const VERSION = new Version('17.1.0-next.0+sha-ec2d6e7');
 
 /**
  * @module
