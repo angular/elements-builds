f310491098cf0e95c1486978eec680039068942e
Andrew Kushnir <akushnir@google.com>
2023-11-16 20:25:27 +0000

    31a1575 fix(core): handle local refs when `getDeferBlocks` is invoked in tests (#52973)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.3+sha-baeb671
+ * @license Angular v17.0.3+sha-31a1575
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.3+sha-baeb671');
+const VERSION = new Version('17.0.3+sha-31a1575');
 
 /**
  * @module
