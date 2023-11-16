1736dacab44af92c8432a8ac890371e904682905
Andrew Kushnir <akushnir@google.com>
2023-11-16 19:21:15 +0000

    baeb671 refactor(migrations): Switch control flow migration reformat default to true (#52971)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.3+sha-f763d26
+ * @license Angular v17.0.3+sha-baeb671
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.3+sha-f763d26');
+const VERSION = new Version('17.0.3+sha-baeb671');
 
 /**
  * @module
