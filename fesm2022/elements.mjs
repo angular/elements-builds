c40901d472926f86c362185ea640484dca218b88
Andrew Kushnir <akushnir@google.com>
2023-11-17 19:57:28 +0000

    6df5f0e Revert "fix(compiler-cli): add diagnostic for control flow that prevents content projection (#52726)" (#53012)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.3+sha-31a047f
+ * @license Angular v17.0.3+sha-6df5f0e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.3+sha-31a047f');
+const VERSION = new Version('17.0.3+sha-6df5f0e');
 
 /**
  * @module
