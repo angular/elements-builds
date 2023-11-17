41f3531539901b2244a6b41be6c621ac136548d4
Andrew Kushnir <akushnir@google.com>
2023-11-17 20:23:42 +0000

    060ea83 docs: fix broken link for view encapsulation (#52995)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-f63c0f6
+ * @license Angular v17.1.0-next.0+sha-060ea83
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-f63c0f6');
+const VERSION = new Version('17.1.0-next.0+sha-060ea83');
 
 /**
  * @module
