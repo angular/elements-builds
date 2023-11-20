eab3ea299a749fdcccff024f136affe46bae63f8
Andrew Kushnir <akushnir@google.com>
2023-11-20 21:22:54 +0000

    dee50f1 fix(core): inherit host directives (#52992)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-91486aa
+ * @license Angular v17.0.4+sha-dee50f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-91486aa');
+const VERSION = new Version('17.0.4+sha-dee50f1');
 
 /**
  * @module
