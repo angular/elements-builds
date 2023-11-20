153aec42571e753df84240ab8b758b74a9782343
Andrew Kushnir <akushnir@google.com>
2023-11-20 18:51:29 +0000

    0fad36e fix(migrations): tweaks to formatting in control flow migration (#53058)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.3+sha-291deac
+ * @license Angular v17.0.3+sha-0fad36e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.3+sha-291deac');
+const VERSION = new Version('17.0.3+sha-0fad36e');
 
 /**
  * @module
