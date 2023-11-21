b9ead111b9c737002bb3d8f48eaf559806107dd7
Andrew Kushnir <akushnir@google.com>
2023-11-21 18:49:58 +0000

    c62b2da build: update to TypeScript 5.3 final (#53087)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-cb2db54
+ * @license Angular v17.1.0-next.1+sha-c62b2da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-cb2db54');
+const VERSION = new Version('17.1.0-next.1+sha-c62b2da');
 
 /**
  * @module
