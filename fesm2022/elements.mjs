393497448dbebe507771bd2c67dbad9b667644c6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 10:04:25 +0000

    4604be2 refactor(platform-browser): clean up legacy way of getting a relative path (#53097)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-6d61f2d
+ * @license Angular v17.0.4+sha-4604be2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-6d61f2d');
+const VERSION = new Version('17.0.4+sha-4604be2');
 
 /**
  * @module
