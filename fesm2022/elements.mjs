724bb573ee7951e8eb99f7e9e0a847363356f0b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:08:24 +0000

    dbd6f38 fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-92a28a7
+ * @license Angular v17.0.4+sha-dbd6f38
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-92a28a7');
+const VERSION = new Version('17.0.4+sha-dbd6f38');
 
 /**
  * @module
