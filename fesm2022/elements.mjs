398d37b393f94bd66417a1d0c5209719384be6e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:24:07 +0000

    4c1d69e fix(compiler-cli): add diagnostic for control flow that prevents content projection (#53190)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-e8ad51a
+ * @license Angular v17.1.0-next.1+sha-4c1d69e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-e8ad51a');
+const VERSION = new Version('17.1.0-next.1+sha-4c1d69e');
 
 /**
  * @module
