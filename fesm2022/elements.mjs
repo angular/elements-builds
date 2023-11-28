34d0d1db6b67bf2de541ca4e1e8946e4e3771e5f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 18:49:01 +0000

    b2e3b3e docs: suggestions applied (#53117)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-df01a5c
+ * @license Angular v17.0.4+sha-b2e3b3e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-df01a5c');
+const VERSION = new Version('17.0.4+sha-b2e3b3e');
 
 /**
  * @module
