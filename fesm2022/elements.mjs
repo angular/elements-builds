bc5e61f6805666bcb2c49ada371efd4a4d08acb8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:11:41 +0000

    92a28a7 build: update dependency jsdom to v23 (#53194)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-cf3de83
+ * @license Angular v17.0.4+sha-92a28a7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-cf3de83');
+const VERSION = new Version('17.0.4+sha-92a28a7');
 
 /**
  * @module
