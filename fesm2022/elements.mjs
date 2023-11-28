8af6b7c1d0d98a6b51ef6322a6710546f1a83ed7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 18:33:39 +0000

    df01a5c test: refactor platform-server integration tests to use application builder (#53219)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-ba6d7fe
+ * @license Angular v17.0.4+sha-df01a5c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-ba6d7fe');
+const VERSION = new Version('17.0.4+sha-df01a5c');
 
 /**
  * @module
