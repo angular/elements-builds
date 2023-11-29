d9a26ee67d9a8990353562b1e1d55aa77f913b28
Joey Perrott <josephperrott@gmail.com>
2023-11-29 20:45:19 +0000

    81e9489 build: set up initial adev guide (#53208)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-5f6323c
+ * @license Angular v17.0.5+sha-81e9489
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-5f6323c');
+const VERSION = new Version('17.0.5+sha-81e9489');
 
 /**
  * @module
