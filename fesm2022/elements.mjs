787ce77afd1da5af0fb73ff482edaf447f4a946b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 08:53:28 +0000

    58cf389 fix(core): avoid stale provider info when TestBed.overrideProvider is used (#52918)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-e3dbadd
+ * @license Angular v17.1.0-next.1+sha-58cf389
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-e3dbadd');
+const VERSION = new Version('17.1.0-next.1+sha-58cf389');
 
 /**
  * @module
