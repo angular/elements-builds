acbb763e30bc881c8cf4f4f8f50815aad41a9f8e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 09:36:27 +0000

    c9f8e75 test(compiler): Update golden partial file (#53209)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-58cf389
+ * @license Angular v17.1.0-next.1+sha-c9f8e75
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-58cf389');
+const VERSION = new Version('17.1.0-next.1+sha-c9f8e75');
 
 /**
  * @module
