e7fb305552e6b083abbabbc632c8fb3fc2b48e3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 16:39:13 +0000

    6ae4088 fix(migrations): properly handle ngIfThen cases in CF migration (#53256)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-5b9f896
+ * @license Angular v17.0.4+sha-6ae4088
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-5b9f896');
+const VERSION = new Version('17.0.4+sha-6ae4088');
 
 /**
  * @module
