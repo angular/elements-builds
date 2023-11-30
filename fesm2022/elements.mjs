377c3e958d779134ae0e387aebdcd813b489d645
Dylan Hunn <dylhunn@gmail.com>
2023-11-30 17:50:44 +0000

    13ade13 fix(router): Ensure canMatch guards run on wildcard routes (#53239)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-81e9489
+ * @license Angular v17.0.5+sha-13ade13
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-81e9489');
+const VERSION = new Version('17.0.5+sha-13ade13');
 
 /**
  * @module
