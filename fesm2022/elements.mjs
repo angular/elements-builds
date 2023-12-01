ac128cf11ef169cfc63cbb9c96b5e6f130f5537d
Dylan Hunn <dylhunn@gmail.com>
2023-12-01 18:42:19 +0000

    af5da6d docs: fix the colspan and colSpan link in the property binding page (#53180)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-a6275cf
+ * @license Angular v17.0.5+sha-af5da6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-a6275cf');
+const VERSION = new Version('17.0.5+sha-af5da6d');
 
 /**
  * @module
