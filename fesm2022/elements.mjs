9158f8ff4abcba2c1233724c5a65ec5d558b98ee
Dylan Hunn <dylhunn@gmail.com>
2023-12-05 05:55:18 +0000

    2d2eaec docs: fixed wrong template string placeholder (#53286) PR Close #53286

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-dc27bea
+ * @license Angular v17.0.5+sha-2d2eaec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-dc27bea');
+const VERSION = new Version('17.0.5+sha-2d2eaec');
 
 /**
  * @module
