b8fb9a235ed6964d493099c72f7827ac9baeddff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-06 15:28:06 +0000

    580af8e Revert "refactor(core): output an error guide link in prod mode (#53324)" (#53392)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.2+sha-e620b3a
+ * @license Angular v17.1.0-next.2+sha-580af8e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.2+sha-e620b3a');
+const VERSION = new Version('17.1.0-next.2+sha-580af8e');
 
 /**
  * @module
