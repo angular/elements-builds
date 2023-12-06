490357a5b170e08c5e6e904e6dadf6ab03935866
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 01:24:42 +0000

    7c863d7 refactor(devtools): run control flow migration. (#53353)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-20ea149
+ * @license Angular v17.0.5+sha-7c863d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-20ea149');
+const VERSION = new Version('17.0.5+sha-7c863d7');
 
 /**
  * @module
