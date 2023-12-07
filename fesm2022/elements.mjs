6b6326edf99f15d32918ac8d4d0b00f38ebbd9d8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-07 17:41:43 +0000

    543df3d fix(migrations): ensure we do not overwrite prior template replacements in migration (#53393)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-049e4ab
+ * @license Angular v17.0.6+sha-543df3d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-049e4ab');
+const VERSION = new Version('17.0.6+sha-543df3d');
 
 /**
  * @module
