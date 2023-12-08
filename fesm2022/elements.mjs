54b56f0d72b6e2ae800a6e0860f455eb8dd4a8f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 22:41:52 +0000

    f88b77b build: migrate adev generation to @angular/docs package (#53458)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-a398c55
+ * @license Angular v17.0.6+sha-f88b77b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-a398c55');
+const VERSION = new Version('17.0.6+sha-f88b77b');
 
 /**
  * @module
