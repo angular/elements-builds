129a591ce0be8e044f309501f6b3a3934ef80ec4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 15:10:28 +0000

    42f4f70 fix(core): remove signal equality check short-circuit (#53446)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-299eae4
+ * @license Angular v17.1.0-next.3+sha-42f4f70
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-299eae4');
+const VERSION = new Version('17.1.0-next.3+sha-42f4f70');
 
 /**
  * @module
