44217c5e4db99ad79121b7d8c49a2fa0cc2f77bf
Joey Perrott <josephperrott@gmail.com>
2023-12-11 19:27:22 +0000

    1ea8a14 refactor(docs-infra): move versions information into the application (#53501)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-24d8aa6
+ * @license Angular v17.0.6+sha-1ea8a14
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-24d8aa6');
+const VERSION = new Version('17.0.6+sha-1ea8a14');
 
 /**
  * @module
