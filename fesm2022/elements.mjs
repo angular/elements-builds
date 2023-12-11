534f89f2fd53d8aab34b2c9efe4b96adf7b5e4e5
Definitely not me <something@something.com>
2023-12-11 21:40:23 +0000

    cc75fab build: drop rxjs_umd_modules as it is unused (#53506)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-0ea8864
+ * @license Angular v17.1.0-next.3+sha-cc75fab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-0ea8864');
+const VERSION = new Version('17.1.0-next.3+sha-cc75fab');
 
 /**
  * @module
