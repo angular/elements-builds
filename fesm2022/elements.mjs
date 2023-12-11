b68c8d684f3fb91e66f666affe445bfb3948746e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 19:00:33 +0000

    24d8aa6 fix(zone.js): Promise.resolve(subPromise) should return subPromise (#53423)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-044cb55
+ * @license Angular v17.0.6+sha-24d8aa6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-044cb55');
+const VERSION = new Version('17.0.6+sha-24d8aa6');
 
 /**
  * @module
