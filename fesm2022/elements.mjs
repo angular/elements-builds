633fb33f0468b116a86e84f6aa502bf4a6ab4580
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-12 22:37:11 +0000

    63fd649 docs: add state of js survey link (#53537)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-44f9f01
+ * @license Angular v17.1.0-next.3+sha-63fd649
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-44f9f01');
+const VERSION = new Version('17.1.0-next.3+sha-63fd649');
 
 /**
  * @module
