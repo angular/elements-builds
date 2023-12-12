8d6ac89258a44b2e22f3960eb791d6679eaeae9b
Definitely not me <something@something.com>
2023-12-12 23:53:28 +0000

    d9348be feat(docs-infra): put the editor back in angular/angular (#53540)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-63fd649
+ * @license Angular v17.1.0-next.3+sha-d9348be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-63fd649');
+const VERSION = new Version('17.1.0-next.3+sha-d9348be');
 
 /**
  * @module
