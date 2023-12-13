8cdc0c736bd97a73d338144814bf791003023f68
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-13 23:49:29 +0000

    4866fce refactor(compiler-cli): capture signal inputs in semantic graph for directives (#53521)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-629343f
+ * @license Angular v17.1.0-next.3+sha-4866fce
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-629343f');
+const VERSION = new Version('17.1.0-next.3+sha-4866fce');
 
 /**
  * @module
