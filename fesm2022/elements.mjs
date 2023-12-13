87fa1898bcc8da12df95e78d12b7060da2ca8be4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-13 17:34:31 +0000

    fdb9cb7 fix(platform-browser): Get correct base path when using "." as base href when serving from the file:// protocol. (#53547)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-d9348be
+ * @license Angular v17.1.0-next.3+sha-fdb9cb7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-d9348be');
+const VERSION = new Version('17.1.0-next.3+sha-fdb9cb7');
 
 /**
  * @module
