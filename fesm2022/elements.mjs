e0bd8e4550710d6b9058570cab2d64ac012d38a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-13 19:32:10 +0000

    c16b5e8 fix(core): Multiple subscribers to ApplicationRef.isStable should all see values (#53541)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-1c55c49
+ * @license Angular v17.0.6+sha-c16b5e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-1c55c49');
+const VERSION = new Version('17.0.6+sha-c16b5e8');
 
 /**
  * @module
