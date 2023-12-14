e36ae52e69dec56750c1df1ec2328afeea8b024e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-14 00:46:38 +0000

    bb93f6c docs: release notes for the v17.0.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-4866fce
+ * @license Angular v17.1.0-next.3+sha-bb93f6c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-4866fce');
+const VERSION = new Version('17.1.0-next.3+sha-bb93f6c');
 
 /**
  * @module
