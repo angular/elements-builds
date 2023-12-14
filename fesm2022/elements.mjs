28464358a924264d5f45521e5ab87ae79751fdc9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-14 01:18:22 +0000

    be7beca release: cut the v17.1.0-next.4 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-bb93f6c
+ * @license Angular v17.1.0-next.4+sha-be7beca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-bb93f6c');
+const VERSION = new Version('17.1.0-next.4+sha-be7beca');
 
 /**
  * @module
