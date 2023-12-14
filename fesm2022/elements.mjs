7282a2860cc3c36ffd0b0674efe6b252404547e8
Andrew Scott <atscott01@gmail.com>
2023-12-14 17:39:49 +0000

    512a19b refactor(compiler): Ensure correct context kind on ICU contexts (#53557)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-be58dba
+ * @license Angular v17.0.7+sha-512a19b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-be58dba');
+const VERSION = new Version('17.0.7+sha-512a19b');
 
 /**
  * @module
