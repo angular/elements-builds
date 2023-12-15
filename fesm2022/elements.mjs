11b861c3c56baf841205bb799142ed869217a9e6
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-15 19:57:13 +0000

    e73205f fix(migrations): cf migration - stop removing empty newlines from i18n blocks (#53578)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-512a19b
+ * @license Angular v17.0.7+sha-e73205f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-512a19b');
+const VERSION = new Version('17.0.7+sha-e73205f');
 
 /**
  * @module
