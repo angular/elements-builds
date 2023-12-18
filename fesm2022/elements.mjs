8ce838484b3bcd9fece3d596a760d9a7289389bc
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 15:40:03 +0000

    b06b24b fix(zone.js): handle fetch with AbortSignal (#49595)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-1892904
+ * @license Angular v17.1.0-next.4+sha-b06b24b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-1892904');
+const VERSION = new Version('17.1.0-next.4+sha-b06b24b');
 
 /**
  * @module
