d71a91b4033a59ccc1d53917e3587f3db59aec73
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 16:33:29 +0000

    de5c9ca fix(compiler): correctly intercept index in loop tracking function (#53604)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-4a892a3
+ * @license Angular v17.0.7+sha-de5c9ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-4a892a3');
+const VERSION = new Version('17.0.7+sha-de5c9ca');
 
 /**
  * @module
