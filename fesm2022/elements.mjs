760ff7a73e0fcc0bef9b7b65de60f5ddebfe2693
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 17:25:17 +0000

    b394125 build: update rxjs build version to v7 (#53621)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-de5c9ca
+ * @license Angular v17.0.7+sha-b394125
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-de5c9ca');
+const VERSION = new Version('17.0.7+sha-b394125');
 
 /**
  * @module
