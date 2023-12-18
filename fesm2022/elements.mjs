03c13d8a74baebc7ab40dbddb4a877fa9a900051
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 22:19:00 +0000

    7ac60ba fix(migrations): cf migration - improve import declaration handling (#53622)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-b394125
+ * @license Angular v17.0.7+sha-7ac60ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-b394125');
+const VERSION = new Version('17.0.7+sha-7ac60ba');
 
 /**
  * @module
