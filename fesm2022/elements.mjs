b375ada5daf1decd71ae9198cbe3d8fd0263db8b
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:26:56 +0000

    ed936ba fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638) (#53639)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-eb7c29c
+ * @license Angular v17.0.7+sha-ed936ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-eb7c29c');
+const VERSION = new Version('17.0.7+sha-ed936ba');
 
 /**
  * @module
