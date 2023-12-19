0ac8d7e602b37b30289f8023777cb6f02fff32f8
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:47:37 +0000

    12181b9 refactor(core): Use single source of truth for `ApplicationRef.isStable` (#53576)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-d49333e
+ * @license Angular v17.1.0-next.4+sha-12181b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-d49333e');
+const VERSION = new Version('17.1.0-next.4+sha-12181b9');
 
 /**
  * @module
