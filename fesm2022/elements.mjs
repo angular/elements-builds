f51f8e96dc1022b4999d1d847c89a4491d179016
Andrew Scott <atscott01@gmail.com>
2023-12-19 18:04:24 +0000

    c99491e refactor(core): No default for zone stableness token (#53505)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-0696ab6
+ * @license Angular v17.0.7+sha-c99491e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-0696ab6');
+const VERSION = new Version('17.0.7+sha-c99491e');
 
 /**
  * @module
