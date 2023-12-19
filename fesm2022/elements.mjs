2e99019738a0a402fd8854c00446e3c9e44ab500
Andrew Scott <atscott01@gmail.com>
2023-12-19 18:22:26 +0000

    22b95de fix(migrations): cf migration - fix bug in attribute formatting (#53636)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-c16e9a7
+ * @license Angular v17.1.0-next.4+sha-22b95de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-c16e9a7');
+const VERSION = new Version('17.1.0-next.4+sha-22b95de');
 
 /**
  * @module
