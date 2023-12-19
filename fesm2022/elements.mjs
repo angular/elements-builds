d151cc233200a85e3d412e1553ac508c63015eda
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:25:44 +0000

    0696ab6 fix(router): Should not freeze original object used for route data (#53635)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-b131c3b
+ * @license Angular v17.0.7+sha-0696ab6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-b131c3b');
+const VERSION = new Version('17.0.7+sha-0696ab6');
 
 /**
  * @module
