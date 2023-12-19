4560a915ca07e67b7e666937cd6f2b1d1c81616e
Andrew Scott <atscott01@gmail.com>
2023-12-19 16:43:52 +0000

    b131c3b docs: Add v18 dates (#53549)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-c3f85e5
+ * @license Angular v17.0.7+sha-b131c3b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-c3f85e5');
+const VERSION = new Version('17.0.7+sha-b131c3b');
 
 /**
  * @module
