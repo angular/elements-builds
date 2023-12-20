1a49d0f54d45521617f68c8ac11d97c5af10b64c
Andrew Scott <atscott01@gmail.com>
2023-12-20 15:28:50 +0000

    cc74ebf refactor(compiler): Rework how ICU placeholders are handled (#53643)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-cafc3b0
+ * @license Angular v17.1.0-next.4+sha-cc74ebf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-cafc3b0');
+const VERSION = new Version('17.1.0-next.4+sha-cc74ebf');
 
 /**
  * @module
