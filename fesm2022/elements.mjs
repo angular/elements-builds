1940dbac42f5d9c4ca5c4ca12d2cb4c107250901
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:38:55 +0000

    d315e2c release: cut the v17.1.0-next.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-c4de4e1
+ * @license Angular v17.1.0-next.5+sha-d315e2c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-c4de4e1');
+const VERSION = new Version('17.1.0-next.5+sha-d315e2c');
 
 /**
  * @module
