bbf0261dc25d34ec2950f3b83a491fbb95a7a4e2
Andrew Scott <atscott@google.com>
2024-01-03 19:34:25 +0000

    7bf11ea refactor(core): Use NoopNgZone in componentFixture (#53670)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-7c19dc2
+ * @license Angular v17.0.8+sha-7bf11ea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-7c19dc2');
+const VERSION = new Version('17.0.8+sha-7bf11ea');
 
 /**
  * @module
