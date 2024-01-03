4e6423d9b4502d438137863c31754939ac1ae3c3
Andrew Scott <atscott@google.com>
2024-01-03 18:39:48 +0000

    fed831f docs: fix incorrect glossary entry (#53609)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.5+sha-33b5707
+ * @license Angular v17.1.0-next.5+sha-fed831f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.5+sha-33b5707');
+const VERSION = new Version('17.1.0-next.5+sha-fed831f');
 
 /**
  * @module
