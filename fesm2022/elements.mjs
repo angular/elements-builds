3965b65e153165c30636dfb9feacc32691c7b873
Andrew Scott <atscott@google.com>
2024-01-04 22:03:30 +0000

    8fa5ebb refactor(router): replace `last` helper with native `Array.at(-1)` (#53749)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-91cb16f
+ * @license Angular v17.0.8+sha-8fa5ebb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-91cb16f');
+const VERSION = new Version('17.0.8+sha-8fa5ebb');
 
 /**
  * @module
