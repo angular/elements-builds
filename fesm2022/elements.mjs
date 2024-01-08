24e73035b73092f03d4b07d61239f0e5d1189242
Andrew Scott <atscott@google.com>
2024-01-08 22:43:55 +0000

    c22b513 fix(common): remove unused parameters from the ngClass constructor (#53831)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-8e49e38
+ * @license Angular v17.0.8+sha-c22b513
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-8e49e38');
+const VERSION = new Version('17.0.8+sha-c22b513');
 
 /**
  * @module
