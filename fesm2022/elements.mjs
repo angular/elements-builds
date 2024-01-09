1d16e696d012c6f901a39c73f38c4ce857cf3354
Andrew Scott <atscott@google.com>
2024-01-09 16:56:33 +0000

    9c58717 refactor(core): node removal should notify the scheduler (#53812)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-c22b513
+ * @license Angular v17.0.8+sha-9c58717
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-c22b513');
+const VERSION = new Version('17.0.8+sha-9c58717');
 
 /**
  * @module
