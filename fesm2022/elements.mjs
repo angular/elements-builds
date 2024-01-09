e0bc2d05158f01c2dd9a9de82e150e6526a847a3
Andrew Scott <atscott@google.com>
2024-01-09 20:23:09 +0000

    c4b266c refactor(core): type-safe global ng (#53439)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-9c58717
+ * @license Angular v17.0.8+sha-c4b266c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-9c58717');
+const VERSION = new Version('17.0.8+sha-c4b266c');
 
 /**
  * @module
