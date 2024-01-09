f72c0dbbd480d12eb134cb3085b4e14abbe06e1b
Andrew Scott <atscott@google.com>
2024-01-09 21:27:40 +0000

    f4b1e64 refactor(zone.js): delete zone.js externs (#53445)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-92fd6cc
+ * @license Angular v17.0.8+sha-f4b1e64
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-92fd6cc');
+const VERSION = new Version('17.0.8+sha-f4b1e64');
 
 /**
  * @module
