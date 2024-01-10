fc179143babbb613b5825e260af1186e33086a86
Andrew Scott <atscott@google.com>
2024-01-10 23:30:53 +0000

    425a8d3 build: set up adev testing (#53854)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-37e2769
+ * @license Angular v17.0.9+sha-425a8d3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-37e2769');
+const VERSION = new Version('17.0.9+sha-425a8d3');
 
 /**
  * @module
