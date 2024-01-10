977e6a54868435a59a0a49a8c28e2cc7c873cecb
Andrew Scott <atscott@google.com>
2024-01-10 20:38:27 +0000

    863be4b feat(core): expose new `input` API for signal-based inputs (#53872)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.5+sha-f6a32c0
+ * @license Angular v17.1.0-next.5+sha-863be4b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.5+sha-f6a32c0');
+const VERSION = new Version('17.1.0-next.5+sha-863be4b');
 
 /**
  * @module
