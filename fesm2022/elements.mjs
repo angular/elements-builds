0b33d8047b86e4cbf32723b504070fa08805bde6
Andrew Scott <atscott@google.com>
2024-01-10 18:48:19 +0000

    74094e1 docs: fix & update apf links to pkg examples (#53866)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-b7cddf4
+ * @license Angular v17.0.8+sha-74094e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-b7cddf4');
+const VERSION = new Version('17.0.8+sha-74094e1');
 
 /**
  * @module
