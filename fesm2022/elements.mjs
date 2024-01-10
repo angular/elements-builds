0381c920921b13f6440914efebfeda9d42833ad3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-01-10 12:25:55 +0000

    7862686 test(core): add acceptance unit tests for signal inputs (#53808)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.5+sha-5978b3d
+ * @license Angular v17.1.0-next.5+sha-7862686
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.5+sha-5978b3d');
+const VERSION = new Version('17.1.0-next.5+sha-7862686');
 
 /**
  * @module
