1fc6195d25edf85202b134272e4a89d43352391b
Andrew Scott <atscott@google.com>
2024-01-11 20:10:37 +0000

    0daca45 fix(core): TestBed should still use the microtask queue to schedule effects (#53843)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-12f9ed0
+ * @license Angular v17.1.0-rc.0+sha-0daca45
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-12f9ed0');
+const VERSION = new Version('17.1.0-rc.0+sha-0daca45');
 
 /**
  * @module
