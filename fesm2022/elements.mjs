c600d1d82c1724db9a8d23ad80b214a72eb9665b
Dylan Hunn <dylhunn@gmail.com>
2024-01-16 17:42:29 +0000

    e9c659a docs: fix-build-path-in-http-server-command (#53226)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-a2087a1
+ * @license Angular v17.2.0-next.0+sha-e9c659a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-a2087a1');
+const VERSION = new Version('17.2.0-next.0+sha-e9c659a');
 
 /**
  * @module
