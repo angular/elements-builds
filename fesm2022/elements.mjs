03c2e09fe6b5fa942809e2fbf135f61a7a9bbdb0
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:14 +0000

    3363295 refactor: migrate modules to prettier formatting (#53954)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-23f19f8
+ * @license Angular v17.0.9+sha-3363295
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-23f19f8');
+const VERSION = new Version('17.0.9+sha-3363295');
 
 /**
  * @module
