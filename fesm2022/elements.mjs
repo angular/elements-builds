a6f7133629c6da9752cc1796643cbe6d52428ced
Dylan Hunn <github@dylanhunn.com>
2024-01-17 22:49:42 +0000

    51d53fc release: cut the v17.1.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-be882c3
+ * @license Angular v17.1.0+sha-51d53fc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-be882c3');
+const VERSION = new Version('17.1.0+sha-51d53fc');
 
 /**
  * @module
