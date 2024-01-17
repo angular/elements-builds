1c733380966fb4b2ae193d9df61495c400343a76
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:19:01 +0000

    4d4858f docs: add `platform-browser/animations/async` entry to AIO (#53914)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-b07c549
+ * @license Angular v17.1.0-rc.0+sha-4d4858f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-b07c549');
+const VERSION = new Version('17.1.0-rc.0+sha-4d4858f');
 
 /**
  * @module
