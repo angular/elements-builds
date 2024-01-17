a3d4f99f73cd0dcd763e8b83d4d94f001a0e95b8
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 18:52:16 +0000

    79e8539 refactor(compiler): extra diagnostics for `@defer` in local compilation mode (#53899)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-6616019
+ * @license Angular v17.1.0-rc.0+sha-79e8539
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-6616019');
+const VERSION = new Version('17.1.0-rc.0+sha-79e8539');
 
 /**
  * @module
