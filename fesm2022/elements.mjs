5a0284b425254446ea7ef0e8626750e78da013a0
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:41:50 +0000

    a7f8d9d refactor(compiler-cli): interpolatedSignalNotInvoked diagnostic for input signals (#53883)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-455b645
+ * @license Angular v17.2.0-next.0+sha-a7f8d9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-455b645');
+const VERSION = new Version('17.2.0-next.0+sha-a7f8d9d');
 
 /**
  * @module
