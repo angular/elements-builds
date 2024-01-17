da33193689990da43b0469c9d36d9369b171b261
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 03:23:43 +0000

    b607757 ci: set up previews of adev (#53941)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-14bbd88
+ * @license Angular v17.0.9+sha-b607757
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-14bbd88');
+const VERSION = new Version('17.0.9+sha-b607757');
 
 /**
  * @module
