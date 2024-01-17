14661082772323b4278dd9d6e70635fc3c941e9f
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:09:35 +0000

    b07c549 refactor(compiler): enable `register` and `resolve` phases for local compilation (#53901)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-6f6ad02
+ * @license Angular v17.1.0-rc.0+sha-b07c549
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-6f6ad02');
+const VERSION = new Version('17.1.0-rc.0+sha-b07c549');
 
 /**
  * @module
