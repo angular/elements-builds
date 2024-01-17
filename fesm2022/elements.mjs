9e2711ffa74cb9b682a926ec3577507494a041b6
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:40:32 +0000

    8ef626e refactor(router): Use Nullish coalescing assignment when possible. (#53930)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-4373889
+ * @license Angular v17.0.9+sha-8ef626e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-4373889');
+const VERSION = new Version('17.0.9+sha-8ef626e');
 
 /**
  * @module
