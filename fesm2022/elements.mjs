3f1908906946c5723c4a7483c3d3491c726596a3
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 22:12:46 +0000

    3c3a947 Revert "refactor(core): improve forwardRef typings"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-8ef626e
+ * @license Angular v17.0.9+sha-3c3a947
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-8ef626e');
+const VERSION = new Version('17.0.9+sha-3c3a947');
 
 /**
  * @module
