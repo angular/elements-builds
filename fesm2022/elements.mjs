4a8f79b459fdba82c476ac24b711ba3795b22d8b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:21:21 +0000

    dc228c6 docs: fix broken code example (#53531)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-9ff4893
+ * @license Angular v17.1.0+sha-dc228c6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-9ff4893');
+const VERSION = new Version('17.1.0+sha-dc228c6');
 
 /**
  * @module
