5b8cb559f87081debfafbdc491b831990e592cbc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:16:51 +0000

    ee68df9 build: update all non-major dependencies (#53979)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-a057b61
+ * @license Angular v17.2.0-next.0+sha-ee68df9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-a057b61');
+const VERSION = new Version('17.2.0-next.0+sha-ee68df9');
 
 /**
  * @module
