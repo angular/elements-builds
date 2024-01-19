a16fb1fa27914df9c649fd68d8101e9b87ef24dd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:26:59 +0000

    62ad2f0 refactor: migrate scripts to prettier formatting (#53976)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-424a26b
+ * @license Angular v17.1.0+sha-62ad2f0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-424a26b');
+const VERSION = new Version('17.1.0+sha-62ad2f0');
 
 /**
  * @module
