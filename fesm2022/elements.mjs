43273479954e0c23a722198dc19f969a3164f244
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 18:14:33 +0000

    711cb41 refactor(devtools): migrate devtools to prettier formatting (#53945)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-4be2534
+ * @license Angular v17.2.0-next.0+sha-711cb41
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-4be2534');
+const VERSION = new Version('17.2.0-next.0+sha-711cb41');
 
 /**
  * @module
