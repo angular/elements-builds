bb192161a1c14ca191911761e18089fc7455941a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:22:49 +0000

    c8ee528 build: update g3 sync config to match internal one (#53987)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-5ae85e4
+ * @license Angular v17.2.0-next.0+sha-c8ee528
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-5ae85e4');
+const VERSION = new Version('17.2.0-next.0+sha-c8ee528');
 
 /**
  * @module
