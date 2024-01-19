f4f693bc94e28d98da9de538ac0bd1fe3d7b8d83
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:40:54 +0000

    c0f0baa refactor(devtools): improving type safety (#53436)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-0fe9331
+ * @license Angular v17.1.0+sha-c0f0baa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -509,7 +509,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-0fe9331');
+const VERSION = new Version('17.1.0+sha-c0f0baa');
 
 /**
  * @module
