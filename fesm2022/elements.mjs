a59336986a6ab7d4efe3b65405b3f09366d11e41
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 18:49:36 +0000

    20811aa docs: move import statement of ngmodule from standalone component to module code #53952 (#54016)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-df453c3
+ * @license Angular v17.1.0+sha-20811aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-df453c3');
+const VERSION = new Version('17.1.0+sha-20811aa');
 
 /**
  * @module
