752824e050d5b005247fc37e46c935cbe844ad47
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 13:26:01 +0000

    b774e22 feat(compiler-cli): make it configurable to generate alias reexports (#53937)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-ec03e46
+ * @license Angular v17.2.0-next.0+sha-b774e22
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-ec03e46');
+const VERSION = new Version('17.2.0-next.0+sha-b774e22');
 
 /**
  * @module
