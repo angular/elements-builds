9b872c7b5f666f2b0ce8e9a72a60ce7d70566014
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 14:15:58 +0000

    9778326 refactor(compiler): ICUs should roll up to the root i18n block for application (#54026)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-fac5a7c
+ * @license Angular v17.1.0+sha-9778326
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-fac5a7c');
+const VERSION = new Version('17.1.0+sha-9778326');
 
 /**
  * @module
