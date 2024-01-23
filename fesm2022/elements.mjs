7fbf1c4299f87340b77fa755e2365dab9ea5725e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 08:58:49 +0000

    ae145d1 refactor(devtools): migrate to standalone (#53998)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-20811aa
+ * @license Angular v17.1.0+sha-ae145d1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-20811aa');
+const VERSION = new Version('17.1.0+sha-ae145d1');
 
 /**
  * @module
