6f892fd9f617d5520c6f41860ec618b1c4e1e37d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 18:01:20 +0000

    9a9a00e docs: update docs to reflect changes in Angular CLI version 17.1 (#53949)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-5f9cac0
+ * @license Angular v17.1.0+sha-9a9a00e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-5f9cac0');
+const VERSION = new Version('17.1.0+sha-9a9a00e');
 
 /**
  * @module
