fd725b5b4ce65c7ff84aa01f46f2e6bfade30d85
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-24 23:46:18 +0000

    854b839 refactor(core): race rAF and setTimeout in zoneless scheduler (#54023)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-a4f024f
+ * @license Angular v17.1.1+sha-854b839
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-a4f024f');
+const VERSION = new Version('17.1.1+sha-854b839');
 
 /**
  * @module
