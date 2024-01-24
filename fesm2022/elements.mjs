2f92988c5fc180c153e4ad1091476f3959ee1954
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:37:33 +0000

    09f9423 docs: fix typo in Custom directives section (#54044)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-25f91e3
+ * @license Angular v17.2.0-next.0+sha-09f9423
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-25f91e3');
+const VERSION = new Version('17.2.0-next.0+sha-09f9423');
 
 /**
  * @module
