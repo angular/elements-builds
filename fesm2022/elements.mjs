a4d7c3a3bf819f9fae7bfcc328464bf9751d544e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-01-24 21:05:22 +0000

    fad1354 release: cut the v17.2.0-next.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-9e10af4
+ * @license Angular v17.2.0-next.0+sha-fad1354
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-9e10af4');
+const VERSION = new Version('17.2.0-next.0+sha-fad1354');
 
 /**
  * @module
