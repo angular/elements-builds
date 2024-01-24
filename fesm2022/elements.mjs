17b75742392f562f4832debc0287e98ddefab687
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 20:28:56 +0000

    a4f024f docs: typo Chat to chart fix (#53892)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-e2e0f08
+ * @license Angular v17.1.1+sha-a4f024f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-e2e0f08');
+const VERSION = new Version('17.1.1+sha-a4f024f');
 
 /**
  * @module
