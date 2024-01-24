1373b4860826c64f2bc6dc80c6a77cdd36a051e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:02:06 +0000

    8ad1ee7 build: ensure zone.js typing test is hermetic (#54048)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0+sha-f222bee
+ * @license Angular v17.1.0+sha-8ad1ee7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0+sha-f222bee');
+const VERSION = new Version('17.1.0+sha-8ad1ee7');
 
 /**
  * @module
