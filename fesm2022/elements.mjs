71050d6a781172997af03f5e15939c0626443ede
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 09:14:15 +0000

    aeaec86 refactor(compiler-cli): rephrase an error message related to `@defer` and local compilation (#54030)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-a3787bd
+ * @license Angular v17.2.0-next.0+sha-aeaec86
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-a3787bd');
+const VERSION = new Version('17.2.0-next.0+sha-aeaec86');
 
 /**
  * @module
