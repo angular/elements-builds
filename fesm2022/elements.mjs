06f9e795991eec3aedd9decb2b61e5d1bb5f2b0c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:39:43 +0000

    d8d6478 docs: testing-components-basics.md and components-basics.md links to css selector documentation are dead (#53586)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-293dc3b
+ * @license Angular v17.1.1+sha-d8d6478
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-293dc3b');
+const VERSION = new Version('17.1.1+sha-d8d6478');
 
 /**
  * @module
