a5f1556138141b605ecd32f6e3d2c75d5e93d4b9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 16:38:35 +0000

    75aeae4 fix(animations): cleanup DOM elements when root view is removed with async animations (#53033)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-bd9c2c5
+ * @license Angular v17.2.0-next.0+sha-75aeae4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-bd9c2c5');
+const VERSION = new Version('17.2.0-next.0+sha-75aeae4');
 
 /**
  * @module
