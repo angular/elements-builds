73923f80b27ae4b30db4d7686cd2627ca4b9d953
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:53:34 +0000

    ccddacf fix: cta clickability issue in adev homepage. (#52905) PR Close #52905

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-b216e76
+ * @license Angular v17.1.1+sha-ccddacf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-b216e76');
+const VERSION = new Version('17.1.1+sha-ccddacf');
 
 /**
  * @module
