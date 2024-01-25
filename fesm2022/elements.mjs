cd2cedbc8418849ceff407e6b2879a568840e048
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:22:27 +0000

    293dc3b docs: fix reference to forwardRef in ForwardRefFn JSdoc (#53556)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-8dca5a1
+ * @license Angular v17.1.1+sha-293dc3b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-8dca5a1');
+const VERSION = new Version('17.1.1+sha-293dc3b');
 
 /**
  * @module
