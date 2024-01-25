9d862016ecb2b9d8824e7eec8251ea6e23d8c92c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:45:03 +0000

    a6dad59 ci: update adev approvers list (#54081)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-d5c0743
+ * @license Angular v17.2.0-next.0+sha-a6dad59
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-d5c0743');
+const VERSION = new Version('17.2.0-next.0+sha-a6dad59');
 
 /**
  * @module
