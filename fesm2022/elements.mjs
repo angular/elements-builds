1c8bba341f5f16e214ecd74199147440a048ef47
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:45:29 +0000

    b216e76 docs: Fix open link in new tab (#53647)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-50b7901
+ * @license Angular v17.1.1+sha-b216e76
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-50b7901');
+const VERSION = new Version('17.1.1+sha-b216e76');
 
 /**
  * @module
