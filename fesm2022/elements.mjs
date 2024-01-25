d65796e29e889347c12a6e7b421faa4799ca30b0
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:51:16 +0000

    259707e docs: fix type at change detection with pipes section (#53940)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-d17ba9b
+ * @license Angular v17.1.1+sha-259707e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-d17ba9b');
+const VERSION = new Version('17.1.1+sha-259707e');
 
 /**
  * @module
