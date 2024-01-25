3b1a726c23679f3ee0889cad71a01b2f74230753
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 21:30:04 +0000

    7ac94dd docs: change the file of the provideHttpClient standalone example to match cli generated project (#52806)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-259707e
+ * @license Angular v17.1.1+sha-7ac94dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-259707e');
+const VERSION = new Version('17.1.1+sha-7ac94dd');
 
 /**
  * @module
