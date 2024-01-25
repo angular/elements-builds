89be54386fabd760909ec9615628e5071ecaa6b5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:53:41 +0000

    ecd0daf docs(docs-infra): move comma in Angular Language Service section (#53759)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-d8d6478
+ * @license Angular v17.1.1+sha-ecd0daf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-d8d6478');
+const VERSION = new Version('17.1.1+sha-ecd0daf');
 
 /**
  * @module
