69711dff44b8827e522c951e173cccdd5f9e982a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:07:19 +0000

    4a5f37d docs(router): fix variable reference in text (#54099) PR Close #54099

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-74567f1
+ * @license Angular v17.1.1+sha-4a5f37d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-74567f1');
+const VERSION = new Version('17.1.1+sha-4a5f37d');
 
 /**
  * @module
