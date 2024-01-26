b5564f1c452c962b5d4b9433f11618c6d4512bf0
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:33:13 +0000

    64399b2 docs: add 2 new diagnostics created recently. (#52329)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-8109013
+ * @license Angular v17.1.1+sha-64399b2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-8109013');
+const VERSION = new Version('17.1.1+sha-64399b2');
 
 /**
  * @module
