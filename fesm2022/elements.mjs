56a1bb3227ead5a4926b4ef24c83fa562af0e027
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:19:08 +0000

    b0632bd docs: Update update-to-version-16.md (#51857)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-16fbe85
+ * @license Angular v17.2.0-next.0+sha-b0632bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-16fbe85');
+const VERSION = new Version('17.2.0-next.0+sha-b0632bd');
 
 /**
  * @module
