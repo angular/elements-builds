990921c45356eb9fa564b7264b8c77a67231ce4f
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 20:44:39 +0000

    c213a4e InMemoryBackendConfigArgs.passThruUnknownUrl: fix tsdoc (#52273)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-b0632bd
+ * @license Angular v17.2.0-next.0+sha-c213a4e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-b0632bd');
+const VERSION = new Version('17.2.0-next.0+sha-c213a4e');
 
 /**
  * @module
