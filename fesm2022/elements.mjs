317078aa97125ea63d95c1fe3424c1b69cc1f267
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:43:23 +0000

    4105ab5 docs: update i18n-common-merge.md (#52400)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-1a2b569
+ * @license Angular v17.2.0-next.0+sha-4105ab5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-1a2b569');
+const VERSION = new Version('17.2.0-next.0+sha-4105ab5');
 
 /**
  * @module
