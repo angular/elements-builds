cdaeb09e993d52cedce61b5a0e7b3386e94fa80f
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 16:08:25 +0000

    271755d docs(docs-infra): cleanup repeated scss from typography (#51579)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-1c49160
+ * @license Angular v17.2.0-next.0+sha-271755d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-1c49160');
+const VERSION = new Version('17.2.0-next.0+sha-271755d');
 
 /**
  * @module
