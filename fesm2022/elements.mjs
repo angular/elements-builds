8db4e6c8a08f6b8fb4b35797b40977498780ae2c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 20:27:08 +0000

    c5a0e90 refactor(core): Update `ComponentFixture` behavior when using zoneless scheduler (#54024)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-d07ce61
+ * @license Angular v17.1.1+sha-c5a0e90
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-d07ce61');
+const VERSION = new Version('17.1.1+sha-c5a0e90');
 
 /**
  * @module
