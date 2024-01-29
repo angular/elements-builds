36f5ab59303f779e833507281b355bbf8584798d
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:05:19 +0000

    f5c520b feat(common): add placeholder to NgOptimizedImage (#53783)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-45288f7
+ * @license Angular v17.2.0-next.0+sha-f5c520b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-45288f7');
+const VERSION = new Version('17.2.0-next.0+sha-f5c520b');
 
 /**
  * @module
