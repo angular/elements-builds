da58a61a40fce4a5ce1d5b76b0cd2bc644a27c22
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 15:47:30 +0000

    45288f7 docs: update input/output guide links (#54135)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-c213a4e
+ * @license Angular v17.2.0-next.0+sha-45288f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-c213a4e');
+const VERSION = new Version('17.2.0-next.0+sha-45288f7');
 
 /**
  * @module
