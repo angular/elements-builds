3de30263b1191dd3aaec4fc3bc289f230061d90a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 19:35:30 +0000

    d07ce61 docs: Update component-overview.md (#52282)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-8650f0d
+ * @license Angular v17.1.1+sha-d07ce61
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-8650f0d');
+const VERSION = new Version('17.1.1+sha-d07ce61');
 
 /**
  * @module
