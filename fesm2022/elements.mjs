6ee216d185b940e1b7c90966be913f03c426001d
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:46:39 +0000

    8650f0d docs: correct grammar error on start page (#53993)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-030d770
+ * @license Angular v17.1.1+sha-8650f0d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-030d770');
+const VERSION = new Version('17.1.1+sha-8650f0d');
 
 /**
  * @module
