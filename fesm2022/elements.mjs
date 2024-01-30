98522b684e7e0c970528682b44b569a1f42b2cf5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 16:14:17 +0000

    4c2b0d9 refactor: migrate common to prettier formatting (#54159)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-57174bb
+ * @license Angular v17.1.1+sha-4c2b0d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-57174bb');
+const VERSION = new Version('17.1.1+sha-4c2b0d9');
 
 /**
  * @module
