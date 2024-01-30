5759e5d49df30deba1019cf2282f4cd8482827b7
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 15:11:32 +0000

    f3567bb refactor(common): simplify i18n plural and select pipe metadata (#53939)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-7b4d275
+ * @license Angular v17.2.0-next.0+sha-f3567bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-7b4d275');
+const VERSION = new Version('17.2.0-next.0+sha-f3567bb');
 
 /**
  * @module
