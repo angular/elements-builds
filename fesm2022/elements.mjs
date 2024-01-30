4c3b659e1c41485fa543471ced126f221d5feacf
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 17:54:43 +0000

    274a489 docs: fix path to housing location css file in lesson 03 (#54106)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-0460a9d
+ * @license Angular v17.2.0-next.0+sha-274a489
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-0460a9d');
+const VERSION = new Version('17.2.0-next.0+sha-274a489');
 
 /**
  * @module
