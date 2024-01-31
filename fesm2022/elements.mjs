8bb572e42ff2f65041a2a8c51dc695b8c89fe62d
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:08:25 +0000

    ca7456e docs(docs-infra): @input getter setter syntax change (#51023)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-b353ad5
+ * @license Angular v17.1.1+sha-ca7456e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-b353ad5');
+const VERSION = new Version('17.1.1+sha-ca7456e');
 
 /**
  * @module
