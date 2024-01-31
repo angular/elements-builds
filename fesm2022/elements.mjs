922e55f92f645bb979a23a0cc4c1477f3393b9bb
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:27:58 +0000

    3b218a7 docs(core): fix example sections (#54121) PR Close #54121

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-4367c44
+ * @license Angular v17.1.1+sha-3b218a7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-4367c44');
+const VERSION = new Version('17.1.1+sha-3b218a7');
 
 /**
  * @module
