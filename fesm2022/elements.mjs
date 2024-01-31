9e14d7fb257f8067af5c34c62f6f3137f3a5d812
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:52:13 +0000

    948fb5f build: update slackapi/slack-github-action digest to 2a8087d (#52000)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-ed8e7c7
+ * @license Angular v17.1.1+sha-948fb5f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-ed8e7c7');
+const VERSION = new Version('17.1.1+sha-948fb5f');
 
 /**
  * @module
