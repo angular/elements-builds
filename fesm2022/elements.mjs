a6f06a2a3c6d3bbd675687738dc16d3840a34ce7
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:35:37 +0000

    a297012 docs(docs-infra): add tab titles to home, playground and tutorial (#53026)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-2e2e8aa
+ * @license Angular v17.2.0-next.0+sha-a297012
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-2e2e8aa');
+const VERSION = new Version('17.2.0-next.0+sha-a297012');
 
 /**
  * @module
