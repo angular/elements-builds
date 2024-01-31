29586b694a4ff8e524d1615e0b539c028f2395f7
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 17:41:43 +0000

    0bd668c refactor(router): RouterLink read urlTree only once (#53817)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-948fb5f
+ * @license Angular v17.1.1+sha-0bd668c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-948fb5f');
+const VERSION = new Version('17.1.1+sha-0bd668c');
 
 /**
  * @module
