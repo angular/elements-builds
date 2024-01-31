f3da844f9759729d5efe46657030f1ce3c4a791a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:13:09 +0000

    4367c44 docs: Update docs about equality. (#54158)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-b69bf37
+ * @license Angular v17.1.1+sha-4367c44
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-b69bf37');
+const VERSION = new Version('17.1.1+sha-4367c44');
 
 /**
  * @module
