9f34afaa64d075a39f3c3313d3b36d714888a3a8
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 19:50:08 +0000

    4883ae6 build: update actions/cache action to v4 (#54179)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-57cb448
+ * @license Angular v17.1.2+sha-4883ae6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-57cb448');
+const VERSION = new Version('17.1.2+sha-4883ae6');
 
 /**
  * @module
