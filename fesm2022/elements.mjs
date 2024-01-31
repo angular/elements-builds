de8fe26928a199336c50f2f6e01b6f83ee42a802
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:49:42 +0000

    b353ad5 fix(docs-infra): sort api reference items (#54116)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-bd6d18b
+ * @license Angular v17.1.1+sha-b353ad5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-bd6d18b');
+const VERSION = new Version('17.1.1+sha-b353ad5');
 
 /**
  * @module
