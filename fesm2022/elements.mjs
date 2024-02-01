21a2654918aac933809a4c934c1adbb7c1357379
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:24:25 +0000

    8608616 [aio/src/styles/custom-themes/{dark,light}-theme.scss] Change theme definitions to resolve warning "Angular Material themes should be created from a map" (#53717)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-99a0b90
+ * @license Angular v17.1.2+sha-8608616
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-99a0b90');
+const VERSION = new Version('17.1.2+sha-8608616');
 
 /**
  * @module
