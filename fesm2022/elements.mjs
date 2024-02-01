4cb1c8b1bfab7dd964a7ab329665f186a7348a9e
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 21:56:25 +0000

    afc72b0 ci: fix size tracking for aio (#54207)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-0fb1142
+ * @license Angular v17.1.2+sha-afc72b0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-0fb1142');
+const VERSION = new Version('17.1.2+sha-afc72b0');
 
 /**
  * @module
