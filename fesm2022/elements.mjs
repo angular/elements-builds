fa37e941a349b854d0d667b2495b3186d382404c
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:12:11 +0000

    99a0b90 docs: correct TypeScript version range for 17.0.x (#53213)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-52a31d0
+ * @license Angular v17.1.2+sha-99a0b90
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-52a31d0');
+const VERSION = new Version('17.1.2+sha-99a0b90');
 
 /**
  * @module
