7991e4f52e43c061fe3b435adea82c085da23d92
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 16:09:17 +0000

    9e3d54c build: update actions/checkout action to v4 (#54183)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-924a276
+ * @license Angular v17.1.2+sha-9e3d54c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-924a276');
+const VERSION = new Version('17.1.2+sha-9e3d54c');
 
 /**
  * @module
