d9ce5fec22fe73d0d6cf46e0c24726829179dcd2
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 18:59:55 +0000

    bc81440 docs(docs-infra): Remove the edit button from AIO (#54204)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-5d98a02
+ * @license Angular v17.2.0-next.1+sha-bc81440
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-5d98a02');
+const VERSION = new Version('17.2.0-next.1+sha-bc81440');
 
 /**
  * @module
