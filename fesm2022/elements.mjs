1552413ad8af494c55c32b29b99d2afc6d813768
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 23:05:03 +0000

    cd88f14 docs:  remove outdated info about ng serve (#53890)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-37a6da3
+ * @license Angular v17.1.2+sha-cd88f14
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-37a6da3');
+const VERSION = new Version('17.1.2+sha-cd88f14');
 
 /**
  * @module
