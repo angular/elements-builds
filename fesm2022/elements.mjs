84c5e5596e2ca7b3afe67cdae9dd19da60ec43e7
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 14:43:51 +0000

    3b892e9 refactor(compiler): update ordering in template pipeline (#54154)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-70d0fb0
+ * @license Angular v17.2.0-next.1+sha-3b892e9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-70d0fb0');
+const VERSION = new Version('17.2.0-next.1+sha-3b892e9');
 
 /**
  * @module
