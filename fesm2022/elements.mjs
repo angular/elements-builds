e0b0892e3111451c3f8a2770a0bac5547b4aa18d
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 14:55:55 +0000

    74aa8a3 test(core): ExpressionChanged... error does not happen with signals (#54206)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-bdf6927
+ * @license Angular v17.2.0-next.1+sha-74aa8a3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-bdf6927');
+const VERSION = new Version('17.2.0-next.1+sha-74aa8a3');
 
 /**
  * @module
