1524d7052b92a02b6326266b5a6fdd49c4ee2c27
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 20:56:52 +0000

    f600f0f refactor(compiler): Show an error when a property binding duplicates an i18n attribute (#54063)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-74aa8a3
+ * @license Angular v17.2.0-next.1+sha-f600f0f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-74aa8a3');
+const VERSION = new Version('17.2.0-next.1+sha-f600f0f');
 
 /**
  * @module
