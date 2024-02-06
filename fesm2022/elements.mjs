cac1fca526a6333da274bb409a8dc9958eeca728
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:50:01 +0000

    6ef60c0 docs: replace event word with property for binding properties link (#54281)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-bfbb306
+ * @license Angular v17.2.0-next.1+sha-6ef60c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-bfbb306');
+const VERSION = new Version('17.2.0-next.1+sha-6ef60c0');
 
 /**
  * @module
