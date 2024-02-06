d514a6a615482c326ee1e246f6e45450c9f35b2e
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 16:10:40 +0000

    19fae76 fix(zone.js): patch `fs.realpath.native` as macrotask (#54208)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-4851453
+ * @license Angular v17.2.0-next.1+sha-19fae76
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-4851453');
+const VERSION = new Version('17.2.0-next.1+sha-19fae76');
 
 /**
  * @module
