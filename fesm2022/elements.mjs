8999f9b55d6e26fe94fea05318c2ce8ec84946c9
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 19:39:36 +0000

    c8a973e build: update dependency husky to v9 (#54289)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-4d29174
+ * @license Angular v17.2.0-next.1+sha-c8a973e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-4d29174');
+const VERSION = new Version('17.2.0-next.1+sha-c8a973e');
 
 /**
  * @module
