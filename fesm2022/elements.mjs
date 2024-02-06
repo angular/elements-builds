251464bc49564d6393a3937d25d4895885565032
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 21:19:51 +0000

    f3f1f85 build: update dependency marked to v12 (#54296)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-c6896a3
+ * @license Angular v17.1.2+sha-f3f1f85
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-c6896a3');
+const VERSION = new Version('17.1.2+sha-f3f1f85');
 
 /**
  * @module
