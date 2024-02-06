9f1a97e8a26cbc327ae314a598cb4d45f9cd5580
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 21:11:41 +0000

    c6896a3 docs: remove v17 banner from adev (#54287)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-db78666
+ * @license Angular v17.1.2+sha-c6896a3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-db78666');
+const VERSION = new Version('17.1.2+sha-c6896a3');
 
 /**
  * @module
