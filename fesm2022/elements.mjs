49d5b0012a9c074f240929437fd72fd1f1465b6e
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:41:24 +0000

    bc4a6a9 fix(compiler-cli): do not error due to multiple components named equally (#54273)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-4297005
+ * @license Angular v17.1.2+sha-bc4a6a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-4297005');
+const VERSION = new Version('17.1.2+sha-bc4a6a9');
 
 /**
  * @module
