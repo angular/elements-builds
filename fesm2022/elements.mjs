59050330b213a8a815a7414223712fec972a1f52
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 22:26:43 +0000

    ca239e8 refactor(core): share refresh logic for cd and signal queries (#54322)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-38c0084
+ * @license Angular v17.2.0-next.1+sha-ca239e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-38c0084');
+const VERSION = new Version('17.2.0-next.1+sha-ca239e8');
 
 /**
  * @module
