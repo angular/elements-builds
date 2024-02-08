6a51adcff38e61b5c4e5fc1ec62b07c45bc2df62
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:50:33 +0000

    744e206 fix(core): Fix possible infinite loop with `markForCheck` by partially reverting #54074 (#54329)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-63a9027
+ * @license Angular v17.1.2+sha-744e206
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-63a9027');
+const VERSION = new Version('17.1.2+sha-744e206');
 
 /**
  * @module
