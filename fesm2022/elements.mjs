7a40c5e37818e6e7a1eb366eeb88b285876e3d95
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:24:05 +0000

    9e2c140 refactor(devtools): show tooltip for hydration icon on directive forest (#54326)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-rc.0+sha-ed41bfc
+ * @license Angular v17.2.0-rc.0+sha-9e2c140
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-rc.0+sha-ed41bfc');
+const VERSION = new Version('17.2.0-rc.0+sha-9e2c140');
 
 /**
  * @module
