45680555dc0516ed20c0c99aed69d66537f5fc52
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-02-08 18:22:17 +0000

    dd8490f release: cut the v17.1.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-2e9a21a
+ * @license Angular v17.1.3+sha-dd8490f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-2e9a21a');
+const VERSION = new Version('17.1.3+sha-dd8490f');
 
 /**
  * @module
