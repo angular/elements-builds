0fa149409a72f2fa4c557b1791c0a36e6d5e8b70
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 15:06:32 +0000

    ef55d48 refactor(compiler-cli): avoid naming conflict with built-in global variable (#54333)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-2e3fa28
+ * @license Angular v17.1.2+sha-ef55d48
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-2e3fa28');
+const VERSION = new Version('17.1.2+sha-ef55d48');
 
 /**
  * @module
