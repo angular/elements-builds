1a85eb42341ea1ab522360b224b4dae029d44245
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 18:07:47 +0000

    2e9a21a Revert "fix(compiler-cli): interpolatedSignalNotInvoked diagnostic for model signals (#54338)" (#54346)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-744e206
+ * @license Angular v17.1.2+sha-2e9a21a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-744e206');
+const VERSION = new Version('17.1.2+sha-2e9a21a');
 
 /**
  * @module
