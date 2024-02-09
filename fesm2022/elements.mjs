a374556821f5865b9d17f6b72732eef32c3a561a
Andrew Kushnir <akushnir@google.com>
2024-02-09 19:03:56 +0000

    9bc386e refactor(docs-infra): create fast mode (#54364)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-abf6371
+ * @license Angular v17.3.0-next.0+sha-9bc386e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-abf6371');
+const VERSION = new Version('17.3.0-next.0+sha-9bc386e');
 
 /**
  * @module
