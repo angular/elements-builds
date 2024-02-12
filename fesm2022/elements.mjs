aeb87a8b09dde2f22e2d075b682c185ba98f0dc8
Andrew Kushnir <akushnir@google.com>
2024-02-12 16:56:16 +0000

    017825b fix(zone.js): patch form-associated custom element callbacks (#50686)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.3+sha-58365e0
+ * @license Angular v17.1.3+sha-017825b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.3+sha-58365e0');
+const VERSION = new Version('17.1.3+sha-017825b');
 
 /**
  * @module
