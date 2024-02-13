fb5436f15518f5c11d69321a18d0d10906d19fef
Andrew Kushnir <akushnir@google.com>
2024-02-13 19:59:14 +0000

    9c486c9 fix(http): Use string body to generate transfer cache key. (#54379)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.3+sha-da93046
+ * @license Angular v17.1.3+sha-9c486c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.3+sha-da93046');
+const VERSION = new Version('17.1.3+sha-9c486c9');
 
 /**
  * @module
