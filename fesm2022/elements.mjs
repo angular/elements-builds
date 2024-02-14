71403a3f1b4671753c3b84187571190ff3f9ec41
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:27:36 +0000

    44a0ea4 docs: add guide for signal inputs (#54316)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-3eda005
+ * @license Angular v17.2.1+sha-44a0ea4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-3eda005');
+const VERSION = new Version('17.2.1+sha-44a0ea4');
 
 /**
  * @module
