40366a78cb24f2cdbeb48c0f51e8775957f55e7f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-02-14 23:02:50 +0000

    ed0d5ec docs: release notes for the v17.2.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-9ca8c68
+ * @license Angular v17.3.0-next.0+sha-ed0d5ec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-9ca8c68');
+const VERSION = new Version('17.3.0-next.0+sha-ed0d5ec');
 
 /**
  * @module
