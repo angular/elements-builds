464cd37186d297cf408c260d0a08e34ce23b0fe0
Andrew Kushnir <akushnir@google.com>
2024-02-15 20:26:21 +0000

    3bd5860 fix(core): properly execute content queries for root components (#54457)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-ba686cc
+ * @license Angular v17.2.1+sha-3bd5860
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-ba686cc');
+const VERSION = new Version('17.2.1+sha-3bd5860');
 
 /**
  * @module
