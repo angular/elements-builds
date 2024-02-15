ad5fe86580937047b86de2e86ebc17d5f7b0de8c
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:59:08 +0000

    ba686cc docs: correct default application builder for new apps (#54386)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-a7e4b99
+ * @license Angular v17.2.1+sha-ba686cc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-a7e4b99');
+const VERSION = new Version('17.2.1+sha-ba686cc');
 
 /**
  * @module
