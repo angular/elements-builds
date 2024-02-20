3b15b80d7fd8a070f280e571f5ac4834c8126aa1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:34:52 +0000

    f8a5b40 docs: enhance Dependency injection description (#54464)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-3bd5860
+ * @license Angular v17.2.1+sha-f8a5b40
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-3bd5860');
+const VERSION = new Version('17.2.1+sha-f8a5b40');
 
 /**
  * @module
