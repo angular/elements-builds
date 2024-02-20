2fa5b491f35e9c4309bc12e579768f411138b911
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:38:01 +0000

    a9741e6 docs(router): deprecate RouterTestingModule (#54466)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-70345b1
+ * @license Angular v17.3.0-next.0+sha-a9741e6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-70345b1');
+const VERSION = new Version('17.3.0-next.0+sha-a9741e6');
 
 /**
  * @module
