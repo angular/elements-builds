34242abc794cc57d36fbb0cac9d15d30978e2b5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:26:12 +0000

    70345b1 docs: override piscina dependency for webcontainers projects in adev (#54494)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-d9a1a7d
+ * @license Angular v17.3.0-next.0+sha-70345b1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-d9a1a7d');
+const VERSION = new Version('17.3.0-next.0+sha-70345b1');
 
 /**
  * @module
