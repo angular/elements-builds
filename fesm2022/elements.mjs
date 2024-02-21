cf3fef341d38bbbeffceefe37198168e4302a615
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-21 22:16:29 +0000

    3e31f1a fix(router): Clear internal transition when navigation finalizes (#54261)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-d411ee7
+ * @license Angular v17.2.1+sha-3e31f1a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-d411ee7');
+const VERSION = new Version('17.2.1+sha-3e31f1a');
 
 /**
  * @module
