175fbdd63a949c9ea6c9733d12ac6c31e60f6cd0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-21 23:33:09 +0000

    bb57d34 fix(migrations): Fix cf migration regular expression to include underscores (#54533)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-3e31f1a
+ * @license Angular v17.2.1+sha-bb57d34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-3e31f1a');
+const VERSION = new Version('17.2.1+sha-bb57d34');
 
 /**
  * @module
