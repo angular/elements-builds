f09ffe5591717d1d6c1afce8f34d8f3c96238dca
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:04:06 +0000

    d411ee7 docs: typos in signal queries guide (#54540)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-d34e329
+ * @license Angular v17.2.1+sha-d411ee7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-d34e329');
+const VERSION = new Version('17.2.1+sha-d411ee7');
 
 /**
  * @module
