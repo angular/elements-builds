35ed82f324fa0ab6a4866dfe2eaf7e5846700c06
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-22 01:58:58 +0000

    600a8cd release: cut the v17.2.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-bb57d34
+ * @license Angular v17.2.2+sha-600a8cd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-bb57d34');
+const VERSION = new Version('17.2.2+sha-600a8cd');
 
 /**
  * @module
