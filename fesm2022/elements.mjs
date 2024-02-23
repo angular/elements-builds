3f5c07a34cd93a2f8db35f838448490cea31b48e
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 19:54:22 +0000

    fbb0398 docs: add a guide for model inputs (#54514)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-1a526f2
+ * @license Angular v17.2.2+sha-fbb0398
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-1a526f2');
+const VERSION = new Version('17.2.2+sha-fbb0398');
 
 /**
  * @module
