b4319302d97a81fa84ace35ee17c0e2f5c48b2f1
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:36:17 +0000

    5712352 fix(core): collect providers from NgModules while rendering `@defer` block (#52881)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-dcd6c8c
+ * @license Angular v17.2.2+sha-5712352
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-dcd6c8c');
+const VERSION = new Version('17.2.2+sha-5712352');
 
 /**
  * @module
