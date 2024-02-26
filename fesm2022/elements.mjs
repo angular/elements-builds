5934aff594a9842838787f1a14e4404e59c6aff5
Dylan Hunn <dylhunn@gmail.com>
2024-02-26 16:43:32 +0000

    c116353 Revert "fix(compiler-cli): identify aliased initializer functions (#54480)" (#54595)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-5712352
+ * @license Angular v17.2.2+sha-c116353
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-5712352');
+const VERSION = new Version('17.2.2+sha-c116353');
 
 /**
  * @module
