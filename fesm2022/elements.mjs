62b7b485944f29eab385df6d69bfe008083cfc2b
Dylan Hunn <dylhunn@gmail.com>
2024-02-27 22:13:08 +0000

    11a0a5d build: update io_bazel_rules_sass digest to 888dd9c (#54615)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-986b9c3
+ * @license Angular v17.2.2+sha-11a0a5d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-986b9c3');
+const VERSION = new Version('17.2.2+sha-11a0a5d');
 
 /**
  * @module
