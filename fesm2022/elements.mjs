fc5f2fe7d5b9034ffc2521fd7cf0c48a8522c14b
Dylan Hunn <dylhunn@gmail.com>
2024-02-27 23:25:28 +0000

    79a3281 fix(core): fix typo in injectors.svg file (#54596)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-11a0a5d
+ * @license Angular v17.2.2+sha-79a3281
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-11a0a5d');
+const VERSION = new Version('17.2.2+sha-79a3281');
 
 /**
  * @module
