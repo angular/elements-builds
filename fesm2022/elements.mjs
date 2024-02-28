e3547225ee411876a0529904625ec9dc7eed1d36
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:24:27 +0000

    52970e3 docs: add updating to v17 guide (#52410)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-2e401c7
+ * @license Angular v17.3.0-next.1+sha-52970e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-2e401c7');
+const VERSION = new Version('17.3.0-next.1+sha-52970e3');
 
 /**
  * @module
