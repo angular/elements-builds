6ceb375cdae666cc93301e6c6353f2c31afd2367
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:19:56 +0000

    c19c44d docs: fix broken link in AIO (#54649)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-b0f434a
+ * @license Angular v17.2.3+sha-c19c44d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-b0f434a');
+const VERSION = new Version('17.2.3+sha-c19c44d');
 
 /**
  * @module
