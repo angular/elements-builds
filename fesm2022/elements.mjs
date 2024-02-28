710f010d2d96e77c01027a09b00f73f3b07fa156
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:39:52 +0000

    7ca08aa docs: updated setting up Zone.js instructions (#52409)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-c19c44d
+ * @license Angular v17.2.3+sha-7ca08aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-c19c44d');
+const VERSION = new Version('17.2.3+sha-7ca08aa');
 
 /**
  * @module
