6cefa20fbc40db7f6f7ba32e7018bf91da2d9ea7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 14:20:54 +0000

    b0f434a docs: fix broken markdown syntax (#53567)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-c05bef0
+ * @license Angular v17.2.3+sha-b0f434a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-c05bef0');
+const VERSION = new Version('17.2.3+sha-b0f434a');
 
 /**
  * @module
