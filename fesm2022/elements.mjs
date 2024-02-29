ad53deedb36ed1fbc0d6ae26cfe32c4410546144
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 14:00:05 +0000

    37dcf8d refactor(core): remove unused `EffectRef` import statement (#53863)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-ffbafc7
+ * @license Angular v17.2.3+sha-37dcf8d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-ffbafc7');
+const VERSION = new Version('17.2.3+sha-37dcf8d');
 
 /**
  * @module
