a8e84b743e838d5e563a87cb6f6581194bd52eee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 18:13:25 +0000

    78e6911 fix(core): generic inference for signal inputs may break with `--strictFunctionTypes` (#54652)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-5e32a77
+ * @license Angular v17.3.0-next.1+sha-78e6911
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-5e32a77');
+const VERSION = new Version('17.3.0-next.1+sha-78e6911');
 
 /**
  * @module
