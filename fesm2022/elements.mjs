9a347e110cdc1d9d0ecc2cf0af5312b2e388d664
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:07:55 +0000

    342737e docs(docs-infra): Update tutorial deps (#54677)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-0767adc
+ * @license Angular v17.2.3+sha-342737e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-0767adc');
+const VERSION = new Version('17.2.3+sha-342737e');
 
 /**
  * @module
