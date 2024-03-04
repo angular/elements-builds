c276f88bf7ef0994437a3f67503a8cfd3d0d026b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 17:26:21 +0000

    2fc1971 ci: Add primitives configurations for ng-dev tools (#54663)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-7ead0fc
+ * @license Angular v17.2.3+sha-2fc1971
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-7ead0fc');
+const VERSION = new Version('17.2.3+sha-2fc1971');
 
 /**
  * @module
