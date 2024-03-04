c0f160dd490d624b302e623bce657c6a2c5bf409
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:42:23 +0000

    7ead0fc docs: revise documentation for @for control flow, 'track $index' performance issues explanation, and confusing 'trackBy' mention (#53806)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-6ca8eca
+ * @license Angular v17.2.3+sha-7ead0fc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-6ca8eca');
+const VERSION = new Version('17.2.3+sha-7ead0fc');
 
 /**
  * @module
