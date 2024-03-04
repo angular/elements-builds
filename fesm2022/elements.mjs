73b7d3128378996f8cb5e552e18f311eee92ddf4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:41:52 +0000

    e3bdab1 refactor(core): Pending effects should make the application unstable (#53835)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-342737e
+ * @license Angular v17.2.3+sha-e3bdab1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-342737e');
+const VERSION = new Version('17.2.3+sha-e3bdab1');
 
 /**
  * @module
