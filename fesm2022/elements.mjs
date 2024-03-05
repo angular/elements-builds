076e54ecb9e4f9941190fe39f462e72755e8f53c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 08:26:18 +0000

    33a6fab fix(core): apply TestBed provider overrides to `@defer` dependencies (#54667)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-78e6911
+ * @license Angular v17.3.0-next.1+sha-33a6fab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-78e6911');
+const VERSION = new Version('17.3.0-next.1+sha-33a6fab');
 
 /**
  * @module
