451716f97f0ba8ea39363f999228e91697994f41
Andrew Scott <atscott@google.com>
2024-03-06 21:40:14 +0000

    4d708b2 refactor(core): Fix internal test util types (#54736)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.4+sha-50623ea
+ * @license Angular v17.2.4+sha-4d708b2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.4+sha-50623ea');
+const VERSION = new Version('17.2.4+sha-4d708b2');
 
 /**
  * @module
