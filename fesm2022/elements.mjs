d88841c90bcb2d107392aae741067542305b56f5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-06 12:21:14 +0000

    e5885fa build: update ng-dev to avoid node-fetch usages (#54715)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-d4154f9
+ * @license Angular v17.3.0-next.1+sha-e5885fa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-d4154f9');
+const VERSION = new Version('17.3.0-next.1+sha-e5885fa');
 
 /**
  * @module
