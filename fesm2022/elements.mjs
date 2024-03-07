0c411dec56b53c11308c5f152fa45f2b816fb639
Andrew Scott <atscott@google.com>
2024-03-07 17:12:03 +0000

    cd242a1 refactor(core): Remove isInternal flag (#54740)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-ad045ef
+ * @license Angular v18.0.0-next.0+sha-cd242a1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-ad045ef');
+const VERSION = new Version('18.0.0-next.0+sha-cd242a1');
 
 /**
  * @module
