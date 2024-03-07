8ca7c81037095abc959d3775b821510840bc69dd
Andrew Scott <atscott@google.com>
2024-03-07 19:18:11 +0000

    d888da4 fix(core): `ApplicationRef.tick` should respect OnPush for host bindings (#53718)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-8bf842d
+ * @license Angular v18.0.0-next.0+sha-d888da4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-8bf842d');
+const VERSION = new Version('18.0.0-next.0+sha-d888da4');
 
 /**
  * @module
