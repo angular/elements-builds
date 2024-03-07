d7dd25c428fecaa265ba33a7e08abf2e54895d65
Andrew Scott <atscott@google.com>
2024-03-07 21:11:41 +0000

    67bc88f fix(router): Navigations triggered by cancellation events should cancel previous navigation (#54710)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.4+sha-b9a0d02
+ * @license Angular v17.2.4+sha-67bc88f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.4+sha-b9a0d02');
+const VERSION = new Version('17.2.4+sha-67bc88f');
 
 /**
  * @module
