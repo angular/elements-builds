dafe0bbd1f1d543267a029f90e6fc5198e70e8cc
Andrew Scott <atscott@google.com>
2024-03-08 00:18:44 +0000

    8180c39 build: fix size failure (#54756)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-115ee88
+ * @license Angular v18.0.0-next.0+sha-8180c39
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-115ee88');
+const VERSION = new Version('18.0.0-next.0+sha-8180c39');
 
 /**
  * @module
