0ba14c196f573976f4b922c611e6f14089383cd1
Andrew Scott <atscott@google.com>
2024-03-11 16:26:37 +0000

    81ccf5d fix(compiler): not catching for loop empty tracking expressions (#54772)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-d269c88
+ * @license Angular v18.0.0-next.0+sha-81ccf5d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-d269c88');
+const VERSION = new Version('18.0.0-next.0+sha-81ccf5d');
 
 /**
  * @module
