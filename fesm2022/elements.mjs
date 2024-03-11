b7ed5f41e45781271a8f301815f522557eb79e72
Andrew Scott <atscott@google.com>
2024-03-11 23:39:14 +0000

    47f79e7 refactor(core): assert presence of the track function (#54814)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-6ea208e
+ * @license Angular v18.0.0-next.0+sha-47f79e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-6ea208e');
+const VERSION = new Version('18.0.0-next.0+sha-47f79e7');
 
 /**
  * @module
