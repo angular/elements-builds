ab617e81c5494439f3afba791e80d5632a1b5bb7
Andrew Scott <atscott@google.com>
2024-03-11 20:44:28 +0000

    d714e99 docs: bump to 2024 for copyright text (#54822)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-eaff724
+ * @license Angular v18.0.0-next.0+sha-d714e99
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-eaff724');
+const VERSION = new Version('18.0.0-next.0+sha-d714e99');
 
 /**
  * @module
