47f25ef745861aa01b18bbc91a837b6b33c4ee61
Andrew Scott <atscott@google.com>
2024-03-12 18:37:59 +0000

    db2f9a9 refactor(compiler-cli): cleanup unused code (#54775)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-4ff94aa
+ * @license Angular v18.0.0-next.0+sha-db2f9a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-4ff94aa');
+const VERSION = new Version('18.0.0-next.0+sha-db2f9a9');
 
 /**
  * @module
