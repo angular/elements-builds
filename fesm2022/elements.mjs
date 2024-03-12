b3a5cb80db8bf32592d038ef4d6ab0bf99d9aaf2
Andrew Scott <atscott@google.com>
2024-03-12 18:45:10 +0000

    456f18b refactor(core): add internal API to enable i18n hydration (#54784)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-db2f9a9
+ * @license Angular v18.0.0-next.0+sha-456f18b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-db2f9a9');
+const VERSION = new Version('18.0.0-next.0+sha-456f18b');
 
 /**
  * @module
