afda93b1eefd486b0eb46aad4958bdbf2b32cfee
Andrew Scott <atscott@google.com>
2024-03-12 16:23:47 +0000

    8735af0 feat(router): Add ability to return `UrlTree` with `NavigationBehaviorOptions` from guards (#45023)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-018f826
+ * @license Angular v18.0.0-next.0+sha-8735af0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-018f826');
+const VERSION = new Version('18.0.0-next.0+sha-8735af0');
 
 /**
  * @module
