4c3f9115ed825c68ea40840a6e5951364bc8063f
Andrew Scott <atscott@google.com>
2024-03-12 16:12:37 +0000

    39a50f9 fix(core): ensure all initializer functions run in an injection context (#54761)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-rc.0+sha-1f77083
+ * @license Angular v17.3.0-rc.0+sha-39a50f9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-rc.0+sha-1f77083');
+const VERSION = new Version('17.3.0-rc.0+sha-39a50f9');
 
 /**
  * @module
