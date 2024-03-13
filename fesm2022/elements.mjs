f84efc5c5374186311c04a0358e3c610afec38e4
Andrew Scott <atscott@google.com>
2024-03-13 18:41:04 +0000

    2d47329 build: update symbol goldens (#54843)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-0dd441d
+ * @license Angular v18.0.0-next.0+sha-2d47329
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-0dd441d');
+const VERSION = new Version('18.0.0-next.0+sha-2d47329');
 
 /**
  * @module
