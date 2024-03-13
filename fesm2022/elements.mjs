a1000c333127eb85df4d0903cca89f02a12a729b
Andrew Scott <atscott@google.com>
2024-03-13 18:14:26 +0000

    0dd441d refactor(core): Remove cancelation funtion from scheduler return value (#54578)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-8cad4e8
+ * @license Angular v18.0.0-next.0+sha-0dd441d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-8cad4e8');
+const VERSION = new Version('18.0.0-next.0+sha-0dd441d');
 
 /**
  * @module
