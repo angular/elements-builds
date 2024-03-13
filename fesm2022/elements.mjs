2948ffc8165627baf3856955abd47af36910f09c
Andrew Scott <atscott01@gmail.com>
2024-03-13 21:09:42 +0000

    c22c5c4 release: cut the v17.3.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-rc.0+sha-5d41ab9
+ * @license Angular v17.3.0+sha-c22c5c4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-rc.0+sha-5d41ab9');
+const VERSION = new Version('17.3.0+sha-c22c5c4');
 
 /**
  * @module
