361a8262df825ede67f20bf11eee485ff1145baa
Andrew Scott <atscott@google.com>
2024-03-14 16:00:32 +0000

    b71a323 docs: update Angular CLI help [main] (#54861)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-77a397b
+ * @license Angular v18.0.0-next.0+sha-b71a323
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-77a397b');
+const VERSION = new Version('18.0.0-next.0+sha-b71a323');
 
 /**
  * @module
