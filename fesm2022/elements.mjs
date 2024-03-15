058e2a4cb102a882cc4e191bfc75ce2764745638
Joey Perrott <josephperrott@gmail.com>
2024-03-15 17:34:41 +0000

    e78dd31 build: update actions/cache digest to ab5e6d0 (#54886)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-64862cb
+ * @license Angular v17.3.0+sha-e78dd31
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-64862cb');
+const VERSION = new Version('17.3.0+sha-e78dd31');
 
 /**
  * @module
