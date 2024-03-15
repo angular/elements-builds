73bf71018a1a4e2f0feb5ebe79f0cb393b6d89cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-15 22:20:41 +0000

    80e5a0a test(compiler-cli): add unit tests for `output()` JIT transform (#54841)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-33dc072
+ * @license Angular v17.3.0+sha-80e5a0a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-33dc072');
+const VERSION = new Version('17.3.0+sha-80e5a0a');
 
 /**
  * @module
