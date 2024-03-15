1169638bf670adccf53681d16b6ed99431739470
Joey Perrott <josephperrott@gmail.com>
2024-03-15 16:47:48 +0000

    a5cc82e docs: update events (#54749)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-c22c5c4
+ * @license Angular v17.3.0+sha-a5cc82e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-c22c5c4');
+const VERSION = new Version('17.3.0+sha-a5cc82e');
 
 /**
  * @module
