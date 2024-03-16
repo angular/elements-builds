2000135ac1773dc9db4ca45c78b39517fd91e8f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-16 01:19:39 +0000

    5328be6 refactor(zone.js): formatting recently changed files (#53443)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-5a63a47
+ * @license Angular v18.0.0-next.0+sha-5328be6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-5a63a47');
+const VERSION = new Version('18.0.0-next.0+sha-5328be6');
 
 /**
  * @module
