7507c65b4b7b7c5b4dc4d587e81345a93df175d2
Joey Perrott <josephperrott@gmail.com>
2024-03-17 00:46:56 +0000

    e61ccd2 build: update to latest angular/dev-infra/github-actions/create-pr-for-changes (#54904)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-80e5a0a
+ * @license Angular v17.3.0+sha-e61ccd2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-80e5a0a');
+const VERSION = new Version('17.3.0+sha-e61ccd2');
 
 /**
  * @module
