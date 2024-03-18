d39287149e6c03c48d3713f85d1b82f9ce666268
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 23:43:01 +0000

    89eca1d refactor(core): allow passing an environment injector while creating a view (#54903)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-0be64c1
+ * @license Angular v18.0.0-next.0+sha-89eca1d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-0be64c1');
+const VERSION = new Version('18.0.0-next.0+sha-89eca1d');
 
 /**
  * @module
