a5e001b0aeb6020cde92f23f5640674d83bea25c
Joey Perrott <josephperrott@gmail.com>
2024-03-18 19:11:04 +0000

    60dbc2b fix(docs-infra): remove part aio infra

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-ce5bb97
+ * @license Angular v18.0.0-next.0+sha-60dbc2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-ce5bb97');
+const VERSION = new Version('18.0.0-next.0+sha-60dbc2b');
 
 /**
  * @module
