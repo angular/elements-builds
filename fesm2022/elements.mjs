82804b8f2e9b9dda3f115443964bccd447b06f36
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 23:59:55 +0000

    314112d fix(core): Prevent `markForCheck` during change detection from causing infinite loops (#54900)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-89eca1d
+ * @license Angular v18.0.0-next.0+sha-314112d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-89eca1d');
+const VERSION = new Version('18.0.0-next.0+sha-314112d');
 
 /**
  * @module
