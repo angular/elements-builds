638e00dc1d6154713cbe94ad7b4982a5534941da
Joey Perrott <josephperrott@gmail.com>
2024-03-19 17:07:26 +0000

    353eae5 build: update babel dependencies to v7.24.1 (#54933)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-314112d
+ * @license Angular v18.0.0-next.0+sha-353eae5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-314112d');
+const VERSION = new Version('18.0.0-next.0+sha-353eae5');
 
 /**
  * @module
