2ef81dab5a64a8d165e8499f768437c7715208bb
Andrew Scott <atscott@google.com>
2024-03-20 20:36:12 +0000

    78d4ad2 ci: temporarily disable failing test (#54970)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.1+sha-c2149df
+ * @license Angular v17.3.1+sha-78d4ad2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.1+sha-c2149df');
+const VERSION = new Version('17.3.1+sha-78d4ad2');
 
 /**
  * @module
