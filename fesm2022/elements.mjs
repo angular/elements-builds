4d8b2e9a6779f1e36c120c5faf7414ac2f2c9fa3
Dylan Hunn <dylhunn@gmail.com>
2024-03-22 05:23:35 +0000

    5bd188a feat(compiler-cli): add partial compilation support for deferred blocks (#54908)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-0461bff
+ * @license Angular v18.0.0-next.1+sha-5bd188a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-0461bff');
+const VERSION = new Version('18.0.0-next.1+sha-5bd188a');
 
 /**
  * @module
