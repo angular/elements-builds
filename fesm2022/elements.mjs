473290ec788a58704bb2e3d588f6c5b5d86d2cb7
Dylan Hunn <dylhunn@gmail.com>
2024-03-26 16:14:55 +0000

    e1650e3 fix(compiler): throw error if item name and context variables conflict (#55045)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-ee3bb81
+ * @license Angular v18.0.0-next.1+sha-e1650e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-ee3bb81');
+const VERSION = new Version('18.0.0-next.1+sha-e1650e3');
 
 /**
  * @module
