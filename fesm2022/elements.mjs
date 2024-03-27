37c9c8b75473a2cdb554453913bc38d87db5fd97
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:24:45 +0000

    93ce4d0 refactor(compiler-cli): properly preserve file overview comments (#54983)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.1+sha-d1d9f55
+ * @license Angular v17.3.1+sha-93ce4d0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.1+sha-d1d9f55');
+const VERSION = new Version('17.3.1+sha-93ce4d0');
 
 /**
  * @module
