30db29f7dfe6ec483d71ed460b2f65d6f6705ac2
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:02:40 +0000

    14235b7 docs: update compat table with 17.3 (#54882)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-adf3ee8
+ * @license Angular v18.0.0-next.1+sha-14235b7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-adf3ee8');
+const VERSION = new Version('18.0.0-next.1+sha-14235b7');
 
 /**
  * @module
