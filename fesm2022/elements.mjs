704e4890fe062d8ab72ed104a3c4b99743495187
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:20:46 +0000

    eae75ff fix(router): RouterLinkActive will always remove active classes when links are not active (#54982)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-14235b7
+ * @license Angular v18.0.0-next.1+sha-eae75ff
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-14235b7');
+const VERSION = new Version('18.0.0-next.1+sha-eae75ff');
 
 /**
  * @module
