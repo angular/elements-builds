21f25364000f7257c5b791093dfb93a602e441a3
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 18:42:59 +0000

    80f96e7 refactor(core): Provide scheduler for use and coordination with zone change detection (#54952)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-9e60609
+ * @license Angular v18.0.0-next.1+sha-80f96e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-9e60609');
+const VERSION = new Version('18.0.0-next.1+sha-80f96e7');
 
 /**
  * @module
