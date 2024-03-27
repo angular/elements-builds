049d122c0684bf952caa2612f323f915c79a5caf
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:56:15 +0000

    1af2be9 docs(docs-infra): replace link by HTTPsecurity tab (#55029)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.1+sha-a893f0b
+ * @license Angular v17.3.1+sha-1af2be9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.1+sha-a893f0b');
+const VERSION = new Version('17.3.1+sha-1af2be9');
 
 /**
  * @module
