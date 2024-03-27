cf81d3106a0c793a48b25336235390747122980e
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 18:13:44 +0000

    9e60609 docs: explain difference between @placeholder and @loading (#54779)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-658cf8c
+ * @license Angular v18.0.0-next.1+sha-9e60609
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-658cf8c');
+const VERSION = new Version('18.0.0-next.1+sha-9e60609');
 
 /**
  * @module
