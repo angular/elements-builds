bd4cc40102ae90a1ab8a38a4e9fccc7359218cd8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-27 12:03:56 +0000

    adf3ee8 ci: fix build failures in patch branch (#55064)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-d15dca0
+ * @license Angular v18.0.0-next.1+sha-adf3ee8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-d15dca0');
+const VERSION = new Version('18.0.0-next.1+sha-adf3ee8');
 
 /**
  * @module
