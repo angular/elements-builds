b5fac7bd567d58f181de439c0f54350f9019bdc6
Andrew Scott <atscott@google.com>
2024-03-29 17:19:49 +0000

    a122bd1 build: update dependency gulp to v5 (#55108)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-367b3ee
+ * @license Angular v18.0.0-next.2+sha-a122bd1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-367b3ee');
+const VERSION = new Version('18.0.0-next.2+sha-a122bd1');
 
 /**
  * @module
