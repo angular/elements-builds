d6be355a2b3a9014c277b757bc1c187a4d07b713
Andrew Scott <atscott@google.com>
2024-03-29 17:08:04 +0000

    367b3ee refactor(compiler): add support for the new search element (#54945)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-218c5bd
+ * @license Angular v18.0.0-next.2+sha-367b3ee
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-218c5bd');
+const VERSION = new Version('18.0.0-next.2+sha-367b3ee');
 
 /**
  * @module
