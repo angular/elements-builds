91a148da440824d2b9e1792dc69a75add03c619b
Andrew Scott <atscott@google.com>
2024-03-29 19:53:34 +0000

    cb95e2f Revert "ci: Update workflows to use latest dev infra hash (#55123)" (#55126)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.2+sha-eb0ccfc
+ * @license Angular v17.3.2+sha-cb95e2f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.2+sha-eb0ccfc');
+const VERSION = new Version('17.3.2+sha-cb95e2f');
 
 /**
  * @module
