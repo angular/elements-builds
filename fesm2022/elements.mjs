49d2c46e37489b71057c8ec66aef38455b63f3bd
Andrew Scott <atscott@google.com>
2024-03-29 16:27:11 +0000

    0bcea6c docs: document why `undefined` is usefull and does not remove classes (#55112)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.2+sha-2c325d4
+ * @license Angular v17.3.2+sha-0bcea6c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.2+sha-2c325d4');
+const VERSION = new Version('17.3.2+sha-0bcea6c');
 
 /**
  * @module
