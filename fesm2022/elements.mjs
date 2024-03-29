ca79951d2a968f2e7690e4317f4451717dae33fb
Andrew Scott <atscott@google.com>
2024-03-29 16:21:58 +0000

    860de66 refactor(router): Navigation promise should resolve `false` instead of `null` on skip (#55068)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-0b450ff
+ * @license Angular v18.0.0-next.2+sha-860de66
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-0b450ff');
+const VERSION = new Version('18.0.0-next.2+sha-860de66');
 
 /**
  * @module
