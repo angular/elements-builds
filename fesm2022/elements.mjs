fe84f170d1cdc3f532292ad65256b67199f25d0a
Andrew Scott <atscott@google.com>
2024-03-29 23:50:52 +0000

    bc7f140 docs(docs-infra): Use monospace font for API reference (#54558)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-7d4dd08
+ * @license Angular v18.0.0-next.2+sha-bc7f140
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-7d4dd08');
+const VERSION = new Version('18.0.0-next.2+sha-bc7f140');
 
 /**
  * @module
