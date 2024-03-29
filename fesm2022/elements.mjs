b2bc160544dfcd965b1eaea512262035648e75cb
Andrew Scott <atscott@google.com>
2024-03-29 16:50:48 +0000

    218c5bd build: update dependency @babel/core to v7.24.3 (#55038)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-1c5f158
+ * @license Angular v18.0.0-next.2+sha-218c5bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-1c5f158');
+const VERSION = new Version('18.0.0-next.2+sha-218c5bd');
 
 /**
  * @module
