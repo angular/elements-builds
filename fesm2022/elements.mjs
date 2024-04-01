07c120e4958b78f4b1225e6c841fd47fce85ab21
Andrew Scott <atscott@google.com>
2024-04-01 17:57:04 +0000

    e18a0ed fix(devtools): fix padding in property tree view (#54648)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-0ceefae
+ * @license Angular v18.0.0-next.2+sha-e18a0ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-0ceefae');
+const VERSION = new Version('18.0.0-next.2+sha-e18a0ed');
 
 /**
  * @module
