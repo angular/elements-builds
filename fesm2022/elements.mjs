fcd3739ec3b5af707c5b7556ecf28f162996c458
Andrew Scott <atscott@google.com>
2024-04-01 18:04:33 +0000

    840c375 fix(core): do not save point-in-time `setTimeout` and `rAF` references (#55124)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-e18a0ed
+ * @license Angular v18.0.0-next.2+sha-840c375
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-e18a0ed');
+const VERSION = new Version('18.0.0-next.2+sha-840c375');
 
 /**
  * @module
