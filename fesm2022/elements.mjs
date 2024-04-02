140129ed4ce9338d4785258e343a12d9376215ff
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 16:13:37 +0000

    666d646 feat(core): Add event delegation library to queue up events and replay them when the application is ready (#55121)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-840c375
+ * @license Angular v18.0.0-next.2+sha-666d646
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-840c375');
+const VERSION = new Version('18.0.0-next.2+sha-666d646');
 
 /**
  * @module
