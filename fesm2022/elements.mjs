0b70d1c71221bb8f1bbfdb42913aa0e569bf33fd
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 16:26:01 +0000

    7b67ad2 refactor(language-service): support showing tags info in the completion (#51140)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.2+sha-129c973
+ * @license Angular v17.3.2+sha-7b67ad2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.2+sha-129c973');
+const VERSION = new Version('17.3.2+sha-7b67ad2');
 
 /**
  * @module
