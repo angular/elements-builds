e2d5480bee9ff8397cc9c0bb4d7f4be85567b411
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:50:20 +0000

    4ac78fb ci: remove event-dispatch in pullapprove (#55200)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.2+sha-5ba2a48
+ * @license Angular v17.3.2+sha-4ac78fb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.2+sha-5ba2a48');
+const VERSION = new Version('17.3.2+sha-4ac78fb');
 
 /**
  * @module
