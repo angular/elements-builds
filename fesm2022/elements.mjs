64996d2e867c12a9ae121f06bf5cdbb2ed3dd496
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:13:25 +0000

    88f0545 refactor(core): remove unnecessary folder (#55197)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.2+sha-eb9a5f5
+ * @license Angular v17.3.2+sha-88f0545
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.2+sha-eb9a5f5');
+const VERSION = new Version('17.3.2+sha-88f0545');
 
 /**
  * @module
