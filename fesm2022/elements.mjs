915d2ee688487a6a1ce7a0d5f0f03b8613ffc0d1
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 15:32:40 +0000

    45ae7a6 feat(platform-browser): add withI18nSupport() in developer preview (#55130)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-f577319
+ * @license Angular v18.0.0-next.2+sha-45ae7a6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-f577319');
+const VERSION = new Version('18.0.0-next.2+sha-45ae7a6');
 
 /**
  * @module
