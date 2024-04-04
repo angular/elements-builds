1131e90e51169370f7c0e59154b48bbb5ba691b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-04 16:07:40 +0000

    ee0412f docs: use structural directives workflow for structural directives docs. (#55212)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-05e7bf8
+ * @license Angular v17.3.3+sha-ee0412f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-05e7bf8');
+const VERSION = new Version('17.3.3+sha-ee0412f');
 
 /**
  * @module
