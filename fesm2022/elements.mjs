57865454f2a8f0f5ce73a89ce34caf30ee998e6d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-04 18:18:33 +0000

    39624c6 fix(compiler): output input flags as a literal (#55215)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-9ad4ed1
+ * @license Angular v18.0.0-next.3+sha-39624c6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-9ad4ed1');
+const VERSION = new Version('18.0.0-next.3+sha-39624c6');
 
 /**
  * @module
