c3f07fe7e0c0771866ebdd006fffe5ef3cfe233e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 04:31:34 +0000

    8cd1b23 ci: pullapprove is case sensitive (#55220)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-39624c6
+ * @license Angular v18.0.0-next.3+sha-8cd1b23
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-39624c6');
+const VERSION = new Version('18.0.0-next.3+sha-8cd1b23');
 
 /**
  * @module
