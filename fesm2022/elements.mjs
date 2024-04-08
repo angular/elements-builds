ecc865517def2b23a9966d7b259249d77097edd4
Andrew Scott <atscott@google.com>
2024-04-08 18:54:28 +0000

    9f06915 docs: replace `code-example` with `docs-code` (#55186)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-4d2a800
+ * @license Angular v17.3.3+sha-9f06915
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-4d2a800');
+const VERSION = new Version('17.3.3+sha-9f06915');
 
 /**
  * @module
