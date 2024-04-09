0a19518c418b7c16504198febf83e77dd2c5171a
Andrew Scott <atscott@google.com>
2024-04-09 18:26:33 +0000

    d0ae556 docs: fix input name (#55141)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-9d6effc
+ * @license Angular v17.3.3+sha-d0ae556
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-9d6effc');
+const VERSION = new Version('17.3.3+sha-d0ae556');
 
 /**
  * @module
