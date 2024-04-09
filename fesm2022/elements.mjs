2bd6685afcfe3b95dcedd6a09f099d98ec0b8de3
Andrew Scott <atscott@google.com>
2024-04-09 21:50:12 +0000

    11705f5 fix(common): skip transfer cache on client (#55012)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-bd236cc
+ * @license Angular v18.0.0-next.3+sha-11705f5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-bd236cc');
+const VERSION = new Version('18.0.0-next.3+sha-11705f5');
 
 /**
  * @module
