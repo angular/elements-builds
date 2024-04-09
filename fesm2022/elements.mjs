c45fcf915aa2a9a9e8a77ecad47019a8d357d7be
Andrew Scott <atscott@google.com>
2024-04-09 19:22:19 +0000

    545fb0d docs(forms): validate email field with proper validator (#55142)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-d0ae556
+ * @license Angular v17.3.3+sha-545fb0d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-d0ae556');
+const VERSION = new Version('17.3.3+sha-545fb0d');
 
 /**
  * @module
