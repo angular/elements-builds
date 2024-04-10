68fd606841fab2351048e093c9cc23911becf1a5
Andrew Scott <atscott@google.com>
2024-04-10 17:45:11 +0000

    e742787 docs: add guide for the `output()` function (#54946)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-6993146
+ * @license Angular v18.0.0-next.3+sha-e742787
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-6993146');
+const VERSION = new Version('18.0.0-next.3+sha-e742787');
 
 /**
  * @module
