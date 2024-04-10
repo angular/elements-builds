aa14b1c2fba6ebdc4c27aa6805d48a3b3a87fd1b
Andrew Scott <atscott@google.com>
2024-04-10 20:50:04 +0000

    19a238d Revert "refactor(core): warn about duplicated keys when using built-in @for (#55243)" (#55293)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-92debf4
+ * @license Angular v18.0.0-next.3+sha-19a238d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-92debf4');
+const VERSION = new Version('18.0.0-next.3+sha-19a238d');
 
 /**
  * @module
