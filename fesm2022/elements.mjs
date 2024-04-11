1205f971b53dcebfd5486773ecab30c51db31235
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 09:25:41 +0000

    b955999 refactor(compiler-cli): move config initialization into constructor (#54975)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.4+sha-268a26f
+ * @license Angular v17.3.4+sha-b955999
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.4+sha-268a26f');
+const VERSION = new Version('17.3.4+sha-b955999');
 
 /**
  * @module
