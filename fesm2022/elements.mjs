e69114515dea2ef077137b98a94c037e48136daa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 18:54:57 +0000

    ab910e2 build(http): Mark the `http` tests as flaky because of one particular transferCache test. (#55255)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-1ee9f32
+ * @license Angular v18.0.0-next.4+sha-ab910e2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-1ee9f32');
+const VERSION = new Version('18.0.0-next.4+sha-ab910e2');
 
 /**
  * @module
