d405db9c2712df83ebd03af7787df5071cd2f4bd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 09:14:00 +0000

    3af54d5 docs: fix links to output function (#55335)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-571a61b
+ * @license Angular v18.0.0-next.4+sha-3af54d5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-571a61b');
+const VERSION = new Version('18.0.0-next.4+sha-3af54d5');
 
 /**
  * @module
