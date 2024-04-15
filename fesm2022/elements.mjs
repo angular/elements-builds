1610d66bf2af100df7cd1b773d6057652363cf3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 15:33:34 +0000

    43cf239 refactor(migrations): exit developer preview for built-in control flow (#55348)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-3471c41
+ * @license Angular v18.0.0-next.4+sha-43cf239
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-3471c41');
+const VERSION = new Version('18.0.0-next.4+sha-43cf239');
 
 /**
  * @module
