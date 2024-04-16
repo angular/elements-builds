82694f2390d424f5d01a90244276858ea34a3660
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-16 13:41:09 +0000

    d28614b feat(core): Modify EventType from an enum to an object. (#55323)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-bce5e23
+ * @license Angular v18.0.0-next.4+sha-d28614b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-bce5e23');
+const VERSION = new Version('18.0.0-next.4+sha-d28614b');
 
 /**
  * @module
