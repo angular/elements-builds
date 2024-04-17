2497711508a20f06edbd4174dd95417e23b789c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-17 23:00:51 +0000

    a5b5b7d refactor: migrate core/primitives to prettier formatting (#55387)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.5+sha-e1eae84
+ * @license Angular v18.0.0-next.5+sha-a5b5b7d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.5+sha-e1eae84');
+const VERSION = new Version('18.0.0-next.5+sha-a5b5b7d');
 
 /**
  * @module
