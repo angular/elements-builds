4e4d2dd206c43a3384b230e038fcd118a420209f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 15:48:00 +0000

    cc57d4c release: cut the v17.3.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.4+sha-8c6f261
+ * @license Angular v17.3.5+sha-cc57d4c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.4+sha-8c6f261');
+const VERSION = new Version('17.3.5+sha-cc57d4c');
 
 /**
  * @module
