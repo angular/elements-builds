75a46ae5a0fd81c0a6e5233dc5c3f4ee0890f387
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 08:41:54 +0000

    9f5e90e build: make JSA contract binary accessible (#55361)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-ff686f3
+ * @license Angular v18.0.0-next.4+sha-9f5e90e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-ff686f3');
+const VERSION = new Version('18.0.0-next.4+sha-9f5e90e');
 
 /**
  * @module
