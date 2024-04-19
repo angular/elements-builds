5898d3ee436c2470c500dc43b5250d4d7863e90f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-19 20:16:39 +0000

    a730f09 feat(core): Add a public API to establish events to be replayed and an attribute to mark an element with an event handler. (#55356)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.5+sha-b67e11a
+ * @license Angular v18.0.0-next.5+sha-a730f09
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.5+sha-b67e11a');
+const VERSION = new Version('18.0.0-next.5+sha-a730f09');
 
 /**
  * @module
