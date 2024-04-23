9c8207493cd42a08452769980ddbdbdc63506fda
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 18:10:13 +0000

    3d84c98 refactor(core): Make some minor changes to facilitate testing and usage of event-dispatch. (#55411)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.5+sha-f84d7dd
+ * @license Angular v18.0.0-next.5+sha-3d84c98
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.5+sha-f84d7dd');
+const VERSION = new Version('18.0.0-next.5+sha-3d84c98');
 
 /**
  * @module
