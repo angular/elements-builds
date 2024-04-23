36e2cf2ae5cbd3ce7e550bd4aecb7c53bc902497
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 20:43:54 +0000

    86967af build: lock file maintenance (#55343)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.5+sha-5a10f40
+ * @license Angular v18.0.0-next.5+sha-86967af
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.5+sha-5a10f40');
+const VERSION = new Version('18.0.0-next.5+sha-86967af');
 
 /**
  * @module
