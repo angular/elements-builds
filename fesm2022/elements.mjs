f9c0eddc19fe14e36701e994681c8a913f8fc5c3
Andrew Kushnir <akushnir@google.com>
2024-04-25 16:11:52 +0000

    a177abe fix(core): render hooks should not specifically run outside the Angular zone (#55399)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.6+sha-5ad2f5f
+ * @license Angular v18.0.0-next.6+sha-a177abe
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.6+sha-5ad2f5f');
+const VERSION = new Version('18.0.0-next.6+sha-a177abe');
 
 /**
  * @module
