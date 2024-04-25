1763608bc5a83edeb57a8c6854a986bc32902b97
Andrew Kushnir <akushnir@google.com>
2024-04-25 15:16:34 +0000

    add11c6 build: make scripts for compatibility with Node.js 18.20+ (#55526)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.5+sha-5083d89
+ * @license Angular v17.3.5+sha-add11c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.5+sha-5083d89');
+const VERSION = new Version('17.3.5+sha-add11c6');
 
 /**
  * @module
