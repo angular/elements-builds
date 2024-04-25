babbbc76627f1893bfabb385dffed399c4b52622
Andrew Kushnir <akushnir@google.com>
2024-04-25 16:02:01 +0000

    e54c950 refactor(forms): replace errors with hasError() (#55379)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-3667352
+ * @license Angular v18.1.0-next.0+sha-e54c950
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-3667352');
+const VERSION = new Version('18.1.0-next.0+sha-e54c950');
 
 /**
  * @module
