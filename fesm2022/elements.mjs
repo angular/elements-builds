714a34f9f41207d88c2dd86e4835f3f966c286bd
Andrew Kushnir <akushnir@google.com>
2024-04-25 22:02:27 +0000

    507d83d refactor(devtools): hide hydration error when the component tree is collapsed (#54912)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.6+sha-9258160
+ * @license Angular v17.3.6+sha-507d83d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.6+sha-9258160');
+const VERSION = new Version('17.3.6+sha-507d83d');
 
 /**
  * @module
