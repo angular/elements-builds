3b3c5742737c0c90366ca19c771a80d5f016db5b
Andrew Kushnir <akushnir@google.com>
2024-04-25 22:03:02 +0000

    5b4970b refactor(platform-server): Update event_replay_spec to more match production code. (#55517)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-356ec65
+ * @license Angular v18.1.0-next.0+sha-5b4970b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-356ec65');
+const VERSION = new Version('18.1.0-next.0+sha-5b4970b');
 
 /**
  * @module
