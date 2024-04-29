81c6b63a04086284ecca40e98001af2bbdc0aa7b
Andrew Kushnir <akushnir@google.com>
2024-04-29 16:57:50 +0000

    9d89898 refactor: migrate zone.js to prettier formatting (#55427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.6+sha-b5ec30a
+ * @license Angular v18.0.0-next.6+sha-9d89898
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.6+sha-b5ec30a');
+const VERSION = new Version('18.0.0-next.6+sha-9d89898');
 
 /**
  * @module
