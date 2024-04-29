7666820ce8f22812a6dead55714f66ca3a7bb6f8
Andrew Kushnir <akushnir@google.com>
2024-04-29 17:30:09 +0000

    89dfb24 refactor: migrate compiler-cli to prettier formatting (#55485)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.6+sha-9d89898
+ * @license Angular v18.0.0-next.6+sha-89dfb24
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.6+sha-9d89898');
+const VERSION = new Version('18.0.0-next.6+sha-89dfb24');
 
 /**
  * @module
