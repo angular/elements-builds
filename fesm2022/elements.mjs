85cf714e873b32867a0a6f51da6fb6701f3a6947
Andrew Kushnir <akushnir@google.com>
2024-04-30 21:13:43 +0000

    3312727 refactor(zone.js): use `Object.prototype.toString` directly for improved tree shakability (#55412)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-ff65669
+ * @license Angular v18.1.0-next.0+sha-3312727
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-ff65669');
+const VERSION = new Version('18.1.0-next.0+sha-3312727');
 
 /**
  * @module
