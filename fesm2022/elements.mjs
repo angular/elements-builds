901a2d59ca09838f4443213cca98f0790473fc49
Andrew Kushnir <akushnir@google.com>
2024-04-30 22:45:07 +0000

    122478c refactor(core): Ensure animations are flushed before running render hooks (#55564)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.6+sha-354cd9c
+ * @license Angular v18.0.0-next.6+sha-122478c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.6+sha-354cd9c');
+const VERSION = new Version('18.0.0-next.6+sha-122478c');
 
 /**
  * @module
