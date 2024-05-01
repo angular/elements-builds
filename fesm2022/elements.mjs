bdac794627c7ffbc95b7e502e20d65b70b85982f
Andrew Kushnir <akushnir@google.com>
2024-05-01 00:06:22 +0000

    82bbcde release: bump DevTools to 1.0.14 (#55545)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.6+sha-266ad40
+ * @license Angular v17.3.6+sha-82bbcde
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.6+sha-266ad40');
+const VERSION = new Version('17.3.6+sha-82bbcde');
 
 /**
  * @module
