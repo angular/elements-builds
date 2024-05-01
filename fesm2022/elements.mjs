6a570916de30b3b0669262837ad0adb75c17045e
Andrew Kushnir <akushnir@google.com>
2024-05-01 22:14:50 +0000

    a0ec2d8 fix(core): don't schedule timer triggers on the server (#55605)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-bf25dfa
+ * @license Angular v18.1.0-next.0+sha-a0ec2d8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-bf25dfa');
+const VERSION = new Version('18.1.0-next.0+sha-a0ec2d8');
 
 /**
  * @module
