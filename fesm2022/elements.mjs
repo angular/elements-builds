5e6feada9cf204f30aeb53b479528b2efbcd617e
Andrew Kushnir <akushnir@google.com>
2024-05-02 20:43:24 +0000

    c21b459 fix(compiler): add math elements to schema (#55631) (#55645)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.7+sha-f978e9c
+ * @license Angular v17.3.7+sha-c21b459
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.7+sha-f978e9c');
+const VERSION = new Version('17.3.7+sha-c21b459');
 
 /**
  * @module
