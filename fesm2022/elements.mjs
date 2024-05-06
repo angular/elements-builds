5ddbd09abbc34cb2eb2e7e6e32e663c17b7a3b21
Andrew Kushnir <akushnir@google.com>
2024-05-06 23:08:13 +0000

    a5f29f9 refactor(devtools): remove git sha stamping from devtools build (#55694)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.0+sha-8483721
+ * @license Angular v18.0.0-rc.0+sha-a5f29f9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.0+sha-8483721');
+const VERSION = new Version('18.0.0-rc.0+sha-a5f29f9');
 
 /**
  * @module
