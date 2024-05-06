e91c2863100ac99f14f0674dc1592be712701be3
Joey Perrott <josephperrott@gmail.com>
2024-05-06 16:09:41 +0000

    aea3b57 WIP

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-8f273ce
+ * @license Angular v18.1.0-next.0+sha-aea3b57
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-8f273ce');
+const VERSION = new Version('18.1.0-next.0+sha-aea3b57');
 
 /**
  * @module
