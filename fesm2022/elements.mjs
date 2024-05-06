7d972d2ace78bced9ad37026248ddba1419a25d9
Andrew Kushnir <akushnir@google.com>
2024-05-06 15:21:14 +0000

    6baa3bc refactor(core): Allow the container and the listenable element to be configurable for early event contract. (#55586)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.0+sha-f1dc74f
+ * @license Angular v18.0.0-rc.0+sha-6baa3bc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.0+sha-f1dc74f');
+const VERSION = new Version('18.0.0-rc.0+sha-6baa3bc');
 
 /**
  * @module
