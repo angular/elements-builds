d61830d8aa5d91d50b1c7f521a112a76f0069d22
Andrew Kushnir <akushnir@google.com>
2024-05-07 20:45:37 +0000

    8305e50 build: trigger error when size-tracking is enabled but not in limit file (#55708)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-0fb455f
+ * @license Angular v18.1.0-next.0+sha-8305e50
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-0fb455f');
+const VERSION = new Version('18.1.0-next.0+sha-8305e50');
 
 /**
  * @module
