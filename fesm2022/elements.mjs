71d613487f8151a568b93760083459bd8dc7d653
Andrew Kushnir <akushnir@google.com>
2024-05-07 15:12:42 +0000

    a9460d0 fix(zone.js): remove `abort` listener on a signal when actual event is removed (#55339)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-77ae5a3
+ * @license Angular v18.1.0-next.0+sha-a9460d0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-77ae5a3');
+const VERSION = new Version('18.1.0-next.0+sha-a9460d0');
 
 /**
  * @module
