403fd0932730296692fa512976cc3866a91dcfc3
Andrew Scott <atscott@google.com>
2024-05-08 21:04:55 +0000

    c001286 refactor(core): a couple of minor changes to the early-event-contract (#55704)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-fb54863
+ * @license Angular v18.1.0-next.0+sha-c001286
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-fb54863');
+const VERSION = new Version('18.1.0-next.0+sha-c001286');
 
 /**
  * @module
