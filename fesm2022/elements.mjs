7d6496ffaa27f30fea0e2bb0962818e246128bce
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-08 16:07:30 +0000

    a9dd754 release: cut the v18.0.0-rc.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.0+sha-e701672
+ * @license Angular v18.0.0-rc.1+sha-a9dd754
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.0+sha-e701672');
+const VERSION = new Version('18.0.0-rc.1+sha-a9dd754');
 
 /**
  * @module
