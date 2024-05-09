e2e50dfcf217473072be581f36c44432ea615cff
Andrew Scott <atscott@google.com>
2024-05-09 21:39:27 +0000

    72b107b refactor(core): Use early event contract instead of the event contract in bootstrap. (#55587)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-61007dc
+ * @license Angular v18.0.0-rc.1+sha-72b107b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-61007dc');
+const VERSION = new Version('18.0.0-rc.1+sha-72b107b');
 
 /**
  * @module
