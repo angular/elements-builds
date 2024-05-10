e755b176b2f5ac02eb2a4d5db9ddbaa615212862
Andrew Scott <atscott@google.com>
2024-05-10 02:17:54 +0000

    85ac2de ci: update payload size for the `event-dispatch-contract` script (#55748)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-28fb385
+ * @license Angular v18.1.0-next.0+sha-85ac2de
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-28fb385');
+const VERSION = new Version('18.1.0-next.0+sha-85ac2de');
 
 /**
  * @module
