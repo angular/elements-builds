73f22499fa96436e040c8dd2a126bc7c93844799
Andrew Scott <atscott@google.com>
2024-05-13 16:41:31 +0000

    76d0aea refactor(core): Remove custom event and replay behavior. (#55695)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-3068de4
+ * @license Angular v18.1.0-next.0+sha-76d0aea
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-3068de4');
+const VERSION = new Version('18.1.0-next.0+sha-76d0aea');
 
 /**
  * @module
