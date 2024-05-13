15dd525c6344e64009c1fa5dd7c8160ddf470c8a
Andrew Scott <atscott@google.com>
2024-05-13 19:35:23 +0000

    0cb5031 refactor(core): Rename `BaseDispatcher` to `Dispatcher`. (#55721)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-7187394
+ * @license Angular v18.1.0-next.0+sha-0cb5031
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-7187394');
+const VERSION = new Version('18.1.0-next.0+sha-0cb5031');
 
 /**
  * @module
