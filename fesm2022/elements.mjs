552cd09fe0dc39c83273ecf99fe696686502fe94
Andrew Scott <atscott@google.com>
2024-05-13 16:18:58 +0000

    7c9d37d refactor(core): Remove enums from event-dispatch. (#55421)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-897e8db
+ * @license Angular v18.0.0-rc.1+sha-7c9d37d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-897e8db');
+const VERSION = new Version('18.0.0-rc.1+sha-7c9d37d');
 
 /**
  * @module
