36d07aee3746387bbc91857e9ddb1b08323dfc7f
Andrew Scott <atscott@google.com>
2024-05-14 18:18:09 +0000

    e205dcb ci: release angular.dev on each commit (#55792)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-beedb68
+ * @license Angular v18.0.0-rc.1+sha-e205dcb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-beedb68');
+const VERSION = new Version('18.0.0-rc.1+sha-e205dcb');
 
 /**
  * @module
