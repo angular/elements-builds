c293c7f1229276c078fd50edf004a3f665e97a8d
Andrew Scott <atscott@google.com>
2024-05-14 22:21:21 +0000

    f1e3ec2 refactor(core): Add an ActionResolver option to Dispatcher. (#55757)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-caedd10
+ * @license Angular v18.1.0-next.0+sha-f1e3ec2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-caedd10');
+const VERSION = new Version('18.1.0-next.0+sha-f1e3ec2');
 
 /**
  * @module
