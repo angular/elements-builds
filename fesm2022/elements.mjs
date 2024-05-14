1fd6550b5dec63236b7c667c4f9bb34923c7296d
Andrew Scott <atscott@google.com>
2024-05-14 19:56:41 +0000

    6850ff1 ci: properly rely on adev for the adev-deploy job (#55797)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-55d6942
+ * @license Angular v18.0.0-rc.1+sha-6850ff1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-55d6942');
+const VERSION = new Version('18.0.0-rc.1+sha-6850ff1');
 
 /**
  * @module
