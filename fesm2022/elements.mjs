a8e29a35ac541b6a87327c50245b489d93e37eca
Andrew Scott <atscott@google.com>
2024-05-16 16:26:12 +0000

    20a530a fix(zone.js): correctly bundle `zone-patch-rxjs` (#55826)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-f3b0e2f
+ * @license Angular v18.1.0-next.0+sha-20a530a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-f3b0e2f');
+const VERSION = new Version('18.1.0-next.0+sha-20a530a');
 
 /**
  * @module
