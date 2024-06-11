5dcac57abdb8dcae3e034e1108880701181c0368
Joey Perrott <josephperrott@gmail.com>
2024-06-11 17:29:32 +0000

    25a5373 docs(docs-infra): guard access to `navigator` for prerendering (#56333)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-0287ae5
+ * @license Angular v18.1.0-next.1+sha-25a5373
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.1+sha-0287ae5');
+const VERSION = new Version('18.1.0-next.1+sha-25a5373');
 
 /**
  * @module
