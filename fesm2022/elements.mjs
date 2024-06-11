ead34fce5deff291a16c2db92cf112e17d423b11
Joey Perrott <josephperrott@gmail.com>
2024-06-11 18:09:37 +0000

    65bf5ec ci: remove tmp directory after publishing artifacts (#56385)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-5717ff6
+ * @license Angular v18.0.2+sha-65bf5ec
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-5717ff6');
+const VERSION = new Version('18.0.2+sha-65bf5ec');
 
 /**
  * @module
