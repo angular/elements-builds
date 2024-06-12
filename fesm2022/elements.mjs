ab98896be1c6d7ac90bc54370611fdbbe7fd5dad
Andrew Kushnir <akushnir@google.com>
2024-06-12 00:11:20 +0000

    4204bee ci: update payload size for a couple integration apps (#56391)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-dbd0fa0
+ * @license Angular v18.0.2+sha-4204bee
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-dbd0fa0');
+const VERSION = new Version('18.0.2+sha-4204bee');
 
 /**
  * @module
