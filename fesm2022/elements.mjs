d82e658097d552ac0d56aba53922457ddb47e721
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 18:50:35 +0000

    96d3ed8 ci: use depth 100 to allow for some history in artifact repos (#56414)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-160171e
+ * @license Angular v18.1.0-next.1+sha-96d3ed8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.1+sha-160171e');
+const VERSION = new Version('18.1.0-next.1+sha-96d3ed8');
 
 /**
  * @module
