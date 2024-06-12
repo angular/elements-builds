580c0be4b3a9bd12b257114392b23800fdcdc80f
Andrew Kushnir <akushnir@google.com>
2024-06-12 00:39:05 +0000

    160171e docs(docs-infra): generate valid HTML for the update guide. (#56390)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-b3780b8
+ * @license Angular v18.1.0-next.1+sha-160171e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.1+sha-b3780b8');
+const VERSION = new Version('18.1.0-next.1+sha-160171e');
 
 /**
  * @module
