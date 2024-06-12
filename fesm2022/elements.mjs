318284ddb4d659575df1c766131a321e0242e975
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 19:06:19 +0000

    e49a81b docs(service-worker): fix formatting of directory structure example (#56412)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-882cae1
+ * @license Angular v18.0.2+sha-e49a81b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-882cae1');
+const VERSION = new Version('18.0.2+sha-e49a81b');
 
 /**
  * @module
