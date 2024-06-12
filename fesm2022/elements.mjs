5aa669985b3e1c23dc71b5378bde64162c1b4ac2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 23:31:39 +0000

    a114eba release: cut the v18.1.0-next.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-38b8400
+ * @license Angular v18.1.0-next.2+sha-a114eba
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.1+sha-38b8400');
+const VERSION = new Version('18.1.0-next.2+sha-a114eba');
 
 /**
  * @module
