2813dc82b80cd94a61b568eeef0f4d35edced778
Andrew Kushnir <akushnir@google.com>
2024-06-13 19:22:58 +0000

    a620231 Revert "refactor(core): Use `ActionResolver` in `Dispatcher`. (#56369)" (#56440)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-ec0d1bf
+ * @license Angular v18.0.3+sha-a620231
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-ec0d1bf');
+const VERSION = new Version('18.0.3+sha-a620231');
 
 /**
  * @module
