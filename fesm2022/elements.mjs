f6c5ece33c93a8113f0a63bc5545f9df494b811c
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:28:49 +0000

    34dc700 docs: language-service - Fix quick info URLs (#56416)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-7c76eb5
+ * @license Angular v18.0.3+sha-34dc700
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-7c76eb5');
+const VERSION = new Version('18.0.3+sha-34dc700');
 
 /**
  * @module
