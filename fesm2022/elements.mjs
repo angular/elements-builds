caccb66bc4bb8cf5b9397213b09c3a9d24b42eda
Andrew Kushnir <akushnir@google.com>
2024-06-13 17:22:12 +0000

    0b867e8 fix(compiler-cli): insert constant statements after the first group of imports (#56431)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.2+sha-c698668
+ * @license Angular v18.1.0-next.2+sha-0b867e8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.2+sha-c698668');
+const VERSION = new Version('18.1.0-next.2+sha-0b867e8');
 
 /**
  * @module
