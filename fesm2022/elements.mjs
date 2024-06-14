1616f917e36cc20528d1110a4acbc586fd728561
Andrew Kushnir <akushnir@google.com>
2024-06-14 20:54:46 +0000

    4c0882a ci: begin checking licenses of upstream dependencies to ensure they are approved (#56460)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-34e7d14
+ * @license Angular v18.0.3+sha-4c0882a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-34e7d14');
+const VERSION = new Version('18.0.3+sha-4c0882a');
 
 /**
  * @module
