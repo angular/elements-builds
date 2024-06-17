9a826128212124e3dd5c650ae2df20663d9c107a
Andrew Kushnir <akushnir@google.com>
2024-06-17 15:59:29 +0000

    01b82a3 docs: update security guide link to adev (#56469)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-4c0882a
+ * @license Angular v18.0.3+sha-01b82a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-4c0882a');
+const VERSION = new Version('18.0.3+sha-01b82a3');
 
 /**
  * @module
