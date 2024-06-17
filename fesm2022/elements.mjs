f6405b2b623bfb8f57ca4161690c77a265a2f56c
Andrew Kushnir <akushnir@google.com>
2024-06-17 15:53:24 +0000

    5a45d2b docs: fix location strategy link (#56475)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.2+sha-ee8ecfa
+ * @license Angular v18.1.0-next.2+sha-5a45d2b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.2+sha-ee8ecfa');
+const VERSION = new Version('18.1.0-next.2+sha-5a45d2b');
 
 /**
  * @module
