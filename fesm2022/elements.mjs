41a4d3c376c10439734104fa002053a333573080
Andrew Kushnir <akushnir@google.com>
2024-06-18 16:36:23 +0000

    2eaf04d docs: add `ng generate directive` page (#56417)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-841476d
+ * @license Angular v18.0.3+sha-2eaf04d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-841476d');
+const VERSION = new Version('18.0.3+sha-2eaf04d');
 
 /**
  * @module
