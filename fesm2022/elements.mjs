2ed341ab70550cdc55f46f5667d958599adf8587
Andrew Kushnir <akushnir@google.com>
2024-06-18 19:32:04 +0000

    cc6cd08 refactor(docs-infra): complete removal of aio directory (#56496)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-2eaf04d
+ * @license Angular v18.0.3+sha-cc6cd08
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-2eaf04d');
+const VERSION = new Version('18.0.3+sha-cc6cd08');
 
 /**
  * @module
