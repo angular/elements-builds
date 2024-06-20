32c06e6c19a6a962ee466cc3b15d853f71a0ce43
Andrew Kushnir <akushnir@google.com>
2024-06-20 17:48:16 +0000

    5f8f18d refactor(docs-infra): use an older version of the `afterRender` API (#56526)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-cc6cd08
+ * @license Angular v18.0.3+sha-5f8f18d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-cc6cd08');
+const VERSION = new Version('18.0.3+sha-5f8f18d');
 
 /**
  * @module
