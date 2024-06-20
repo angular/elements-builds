dfae9f9a087611ed9e7f646efc124002205153df
Andrew Kushnir <akushnir@google.com>
2024-06-20 22:05:31 +0000

    29ca6d1 fix(core): improve support for i18n hydration of projected content (#56192)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.3+sha-29ecb39
+ * @license Angular v18.1.0-next.3+sha-29ca6d1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.3+sha-29ecb39');
+const VERSION = new Version('18.1.0-next.3+sha-29ca6d1');
 
 /**
  * @module
