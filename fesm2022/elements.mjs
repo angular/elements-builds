8a371106764e119bdc9ad6c7c4a3411d9dada5d1
Dylan Hunn <dylhunn@gmail.com>
2024-06-26 16:00:25 +0000

    f49a2a4 docs: Move the background image migration to its own section. (#56588)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.4+sha-cfe1f38
+ * @license Angular v18.0.4+sha-f49a2a4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.4+sha-cfe1f38');
+const VERSION = new Version('18.0.4+sha-f49a2a4');
 
 /**
  * @module
