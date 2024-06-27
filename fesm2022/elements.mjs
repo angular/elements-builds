70946b0df720fdbd4cc7eea9574aaa453d0d38fc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 21:22:56 +0000

    2dcb698 build: update to latest docs tooling to resolve missing answers (#56746)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-fa9da56
+ * @license Angular v18.0.5+sha-2dcb698
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-fa9da56');
+const VERSION = new Version('18.0.5+sha-2dcb698');
 
 /**
  * @module
