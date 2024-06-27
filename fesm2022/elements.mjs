19cdf6cd222a7baf0a3e0971f33df4afb184a538
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:20:05 +0000

    3bfba7e docs: release notes for the v18.0.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.4+sha-5dc0de1
+ * @license Angular v18.1.0-next.4+sha-3bfba7e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.4+sha-5dc0de1');
+const VERSION = new Version('18.1.0-next.4+sha-3bfba7e');
 
 /**
  * @module
