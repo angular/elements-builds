3d547215df18ee11d1252e1e1ac81940c4fb9203
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 14:30:18 +0000

    aaa00b3 refactor(core): Add global event delegation provider (#56247)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-a59496c
+ * @license Angular v18.0.5+sha-aaa00b3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-a59496c');
+const VERSION = new Version('18.0.5+sha-aaa00b3');
 
 /**
  * @module
