14a7f7455b97b1ffec075447551a0faad0a30910
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:03:00 +0000

    81e6a54 docs: Add mention of `picture` support in FAQ (#56692)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.4+sha-95f118b
+ * @license Angular v18.0.4+sha-81e6a54
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.4+sha-95f118b');
+const VERSION = new Version('18.0.4+sha-81e6a54');
 
 /**
  * @module
