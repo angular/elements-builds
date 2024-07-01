62a0c866ee0a774b61b4680583766c3fc92ca263
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 18:53:53 +0000

    b4cc8d4 ci: upload build artifacts on each pr run (#56787)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.4+sha-f407e3c
+ * @license Angular v18.1.0-next.4+sha-b4cc8d4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.4+sha-f407e3c');
+const VERSION = new Version('18.1.0-next.4+sha-b4cc8d4');
 
 /**
  * @module
