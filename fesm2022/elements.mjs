3e6299db848d4a3d9139e64f5310b366f1774a5b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 14:44:57 +0000

    46b0292 refactor(core): export additional methods from primitives/signals (#56759)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.4+sha-2a1291e
+ * @license Angular v18.1.0-next.4+sha-46b0292
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.4+sha-2a1291e');
+const VERSION = new Version('18.1.0-next.4+sha-46b0292');
 
 /**
  * @module
