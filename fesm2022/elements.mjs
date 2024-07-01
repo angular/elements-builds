872dd0be7a027e2e1a0c785bee7e31fbdad0f9db
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 19:42:21 +0000

    9cbfc6b docs: update 18.1.0 release date (#56789)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-1156b19
+ * @license Angular v18.0.5+sha-9cbfc6b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-1156b19');
+const VERSION = new Version('18.0.5+sha-9cbfc6b');
 
 /**
  * @module
