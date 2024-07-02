e9c406b04bb9994f904b37717ba44f05c166cdd1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:02:13 +0000

    68464a7 docs: update playground app to use hello world for easier onboarding (#56813)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-0479c35
+ * @license Angular v18.0.5+sha-68464a7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-0479c35');
+const VERSION = new Version('18.0.5+sha-68464a7');
 
 /**
  * @module
