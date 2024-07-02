adc411b01a0535665251c66a6726f714943a04cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:09:19 +0000

    445dd96 build: update github/codeql-action action to v3.25.11 (#56803)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-68464a7
+ * @license Angular v18.0.5+sha-445dd96
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-68464a7');
+const VERSION = new Version('18.0.5+sha-445dd96');
 
 /**
  * @module
