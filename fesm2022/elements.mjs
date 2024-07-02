deb95306e95c34e3f926cbd5a7af901dc77c7a69
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 16:48:59 +0000

    9acccb1 docs: Update interceptors.md to replace `public class` with `export class` in DI section (#56807)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-58be8e6
+ * @license Angular v18.1.0-rc.0+sha-9acccb1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-58be8e6');
+const VERSION = new Version('18.1.0-rc.0+sha-9acccb1');
 
 /**
  * @module
