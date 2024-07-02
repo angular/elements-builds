257dd91caf09e181746bd43d4b76adaf6d65e9a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:39:04 +0000

    fec5b80 fix(core): support injection of object with null constructor. (#56553)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-b7d3ecc
+ * @license Angular v18.0.5+sha-fec5b80
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-b7d3ecc');
+const VERSION = new Version('18.0.5+sha-fec5b80');
 
 /**
  * @module
