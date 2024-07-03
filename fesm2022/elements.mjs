cebd28786ac4d447886bf181a610c6095a7641fd
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 15:41:42 +0000

    c8e2885 feat(compiler): Add extended diagnostic to warn when there are uncalled functions in event bindings (#56295) (#56295)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-d9fe17a
+ * @license Angular v18.2.0-next.0+sha-c8e2885
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-d9fe17a');
+const VERSION = new Version('18.2.0-next.0+sha-c8e2885');
 
 /**
  * @module
