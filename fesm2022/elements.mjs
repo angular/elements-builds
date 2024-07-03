44267a057aec7d40492fef32ab5c1351e01981ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:45:50 +0000

    d9fe17a refactor(common): Fire priority check on stable. (#56776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-00d9cd2
+ * @license Angular v18.2.0-next.0+sha-d9fe17a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-00d9cd2');
+const VERSION = new Version('18.2.0-next.0+sha-d9fe17a');
 
 /**
  * @module
