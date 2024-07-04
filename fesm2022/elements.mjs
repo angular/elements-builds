be9648f0aad59747203daf644509d1c81b7cb761
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 08:00:40 +0000

    a2d93dc docs: Update image-optimization.md (#56819)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-4e36bf8
+ * @license Angular v18.2.0-next.0+sha-a2d93dc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-4e36bf8');
+const VERSION = new Version('18.2.0-next.0+sha-a2d93dc');
 
 /**
  * @module
