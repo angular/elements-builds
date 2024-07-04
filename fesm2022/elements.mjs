f4ead4eb18b5a45c4cd1e68a91972c9ecf466762
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 09:36:52 +0000

    66bf467 docs: update app-shell docs to reflect v17 (#56836)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-b5500f8
+ * @license Angular v18.0.6+sha-66bf467
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.6+sha-b5500f8');
+const VERSION = new Version('18.0.6+sha-66bf467');
 
 /**
  * @module
