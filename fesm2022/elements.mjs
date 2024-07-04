965d8d49c1781b7d3c26d7fabfbdf81293a7b994
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 12:34:23 +0000

    97d718d docs: add window title to reference pages (#56840)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-6e1f78f
+ * @license Angular v18.2.0-next.0+sha-97d718d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-6e1f78f');
+const VERSION = new Version('18.2.0-next.0+sha-97d718d');
 
 /**
  * @module
