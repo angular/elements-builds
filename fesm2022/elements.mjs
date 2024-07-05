7328bf0513bb4f4975dce26711a23752e86b5421
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 11:08:36 +0000

    229dd83 fix(common): typo in warning for NgOptimizedDirective (#56817)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-e7da8c2
+ * @license Angular v18.2.0-next.0+sha-229dd83
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-e7da8c2');
+const VERSION = new Version('18.2.0-next.0+sha-229dd83');
 
 /**
  * @module
