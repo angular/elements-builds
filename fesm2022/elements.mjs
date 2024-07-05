dc8f44b8bb84c3f6d5545f9eb47cf030f66cde26
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 10:31:56 +0000

    e7da8c2 docs: fix broken link (#56848)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-97d718d
+ * @license Angular v18.2.0-next.0+sha-e7da8c2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-97d718d');
+const VERSION = new Version('18.2.0-next.0+sha-e7da8c2');
 
 /**
  * @module
