c16dde97605856e78c653198f4ea362cfea4bcc2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 16:08:29 +0000

    35fa9b0 docs(docs-infra): add missing context with link (#56896)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-56ec97d
+ * @license Angular v18.1.0-rc.0+sha-35fa9b0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-56ec97d');
+const VERSION = new Version('18.1.0-rc.0+sha-35fa9b0');
 
 /**
  * @module
