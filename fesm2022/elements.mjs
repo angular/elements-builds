674ea0599ef6bbb41b1ed58281086836a1c608a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 07:58:56 +0000

    176b26f docs: remove private symbol from docs (#56851)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-229dd83
+ * @license Angular v18.2.0-next.0+sha-176b26f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-229dd83');
+const VERSION = new Version('18.2.0-next.0+sha-176b26f');
 
 /**
  * @module
