47d3e4c562f2a3c9f8dd04519faab148a5aa0bef
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 14:32:05 +0000

    56ec97d refactor(compiler-cli): do not truncate/reduce types in API docs (#56572)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-ba325c5
+ * @license Angular v18.1.0-rc.0+sha-56ec97d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-ba325c5');
+const VERSION = new Version('18.1.0-rc.0+sha-56ec97d');
 
 /**
  * @module
