70bcf85aa0aa7e1dbfde5f7ccd7ad9be586719cb
Andrew Scott <atscott@google.com>
2024-07-08 16:38:37 +0000

    5dcdbfc fix(core): rename the equality function option in toSignal (#56769)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-d5ce323
+ * @license Angular v18.2.0-next.0+sha-5dcdbfc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-d5ce323');
+const VERSION = new Version('18.2.0-next.0+sha-5dcdbfc');
 
 /**
  * @module
