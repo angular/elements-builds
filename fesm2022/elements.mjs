d5149279b85af54700309ea14990f7f5ab45c6cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:43:36 +0000

    02a31c5 docs: add documentation page for let declarations (#56886)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-5dcdbfc
+ * @license Angular v18.2.0-next.0+sha-02a31c5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-5dcdbfc');
+const VERSION = new Version('18.2.0-next.0+sha-02a31c5');
 
 /**
  * @module
