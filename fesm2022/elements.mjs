f82978c46b10f587df0a6be2880f7c0c38bdbe04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:52:01 +0000

    aef166f fix(compiler): fix CSS animation rule scope (#56800)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-02f5e73
+ * @license Angular v18.1.0-rc.0+sha-aef166f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-02f5e73');
+const VERSION = new Version('18.1.0-rc.0+sha-aef166f');
 
 /**
  * @module
