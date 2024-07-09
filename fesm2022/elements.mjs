1a25fb1e44a27da9e3994fe1644fb84444526457
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:19:12 +0000

    260531d refactor(compiler-cli):  escape missing decorators. (#56794)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-fe52d8f
+ * @license Angular v18.2.0-next.0+sha-260531d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-fe52d8f');
+const VERSION = new Version('18.2.0-next.0+sha-260531d');
 
 /**
  * @module
