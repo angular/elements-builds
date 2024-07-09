257491bdf44af3b52b9d5118606265af3ab28f99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:10:17 +0000

    fe52d8f docs: update dependency-injection-context.md (#56736)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-31b235b
+ * @license Angular v18.2.0-next.0+sha-fe52d8f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-31b235b');
+const VERSION = new Version('18.2.0-next.0+sha-fe52d8f');
 
 /**
  * @module
