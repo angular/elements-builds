52ffe010b334e608d3acc0010e1e12a43887450d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 10:17:42 +0000

    f689214 docs: fix wrong link in template overview (#56911)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-aef166f
+ * @license Angular v18.1.0-rc.0+sha-f689214
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-aef166f');
+const VERSION = new Version('18.1.0-rc.0+sha-f689214');
 
 /**
  * @module
