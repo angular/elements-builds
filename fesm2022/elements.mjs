e5f75befbaf2baefe3beda7ffe1e5cada28e1cc7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 10:52:12 +0000

    ea86754 refactor(platform-server): Add private profiler. (#56274)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-85f77b5
+ * @license Angular v18.0.6+sha-ea86754
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.6+sha-85f77b5');
+const VERSION = new Version('18.0.6+sha-ea86754');
 
 /**
  * @module
