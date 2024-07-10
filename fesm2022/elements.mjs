7bac6d0180403e7a9d975b55815b3fa8c4d737f0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:15:59 +0000

    9fd2ef9 docs: Update component testing scenarios (#56388)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-5d75b1d
+ * @license Angular v18.2.0-next.0+sha-9fd2ef9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-5d75b1d');
+const VERSION = new Version('18.2.0-next.0+sha-9fd2ef9');
 
 /**
  * @module
