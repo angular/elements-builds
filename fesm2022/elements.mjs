da0ba14d8c81da59095e4571182874cbb4d45f9d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 09:33:42 +0000

    1c2415e docs: Update components-scenarios.md (#56921)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-a7d021f
+ * @license Angular v18.0.6+sha-1c2415e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.6+sha-a7d021f');
+const VERSION = new Version('18.0.6+sha-1c2415e');
 
 /**
  * @module
