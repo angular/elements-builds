8c0ee3cde1c55ab5c82a538dbca1b05e3bf62579
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:39:16 +0000

    a7d021f docs: update version table for 18.1 (#56920)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-619e181
+ * @license Angular v18.0.6+sha-a7d021f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.6+sha-619e181');
+const VERSION = new Version('18.0.6+sha-a7d021f');
 
 /**
  * @module
