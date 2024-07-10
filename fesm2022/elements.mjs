01fe2ea190b7683fcef5a92d4a9a9f71b492b0f7
Andrew Scott <atscott@google.com>
2024-07-10 20:58:22 +0000

    a7b774c refactor(core): Refactor `EarlyEventContract` to prepare for using it as a container. (#56900)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0+sha-aedf832
+ * @license Angular v18.1.0+sha-a7b774c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0+sha-aedf832');
+const VERSION = new Version('18.1.0+sha-a7b774c');
 
 /**
  * @module
