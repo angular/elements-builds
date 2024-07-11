6014a5c7ecf3bb58bd5b4f101380d6c0e95e1fa9
Andrew Scott <atscott@google.com>
2024-07-11 16:03:31 +0000

    a531106 build: fix rate limits when deploying angular.dev site (#56929)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0+sha-94c6ec0
+ * @license Angular v18.1.0+sha-a531106
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0+sha-94c6ec0');
+const VERSION = new Version('18.1.0+sha-a531106');
 
 /**
  * @module
