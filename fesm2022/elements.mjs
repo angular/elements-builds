5337274f894920951f1c08621cafdd7a054115ad
Andrew Scott <atscott@google.com>
2024-07-12 00:08:36 +0000

    8f08d04 ci: add an entry for aio in the pullapprove config (#56952)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-0953d65
+ * @license Angular v18.2.0-next.0+sha-8f08d04
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-0953d65');
+const VERSION = new Version('18.2.0-next.0+sha-8f08d04');
 
 /**
  * @module
