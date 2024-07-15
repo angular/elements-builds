1d3e372c931a17218526ae3d6b525af13ba83fd0
Andrew Scott <atscott@google.com>
2024-07-15 18:45:37 +0000

    bbe655f refactor(compiler): include public constructor paramters to class properties. (#56315)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0+sha-c0855f0
+ * @license Angular v18.1.0+sha-bbe655f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0+sha-c0855f0');
+const VERSION = new Version('18.1.0+sha-bbe655f');
 
 /**
  * @module
