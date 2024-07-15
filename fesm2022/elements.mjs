63d14c580f6ea17dc68bf8d1898c8b21c91b914b
Andrew Scott <atscott@google.com>
2024-07-15 18:55:18 +0000

    da9149e build: update dependency inquirer to v10 (#56883)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0+sha-bbe655f
+ * @license Angular v18.1.0+sha-da9149e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0+sha-bbe655f');
+const VERSION = new Version('18.1.0+sha-da9149e');
 
 /**
  * @module
