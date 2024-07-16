22122c4f07d0cb020328815b5c370a9be26d808a
Andrew Scott <atscott@google.com>
2024-07-16 16:58:39 +0000

    2c4613a fix(common): Don't run preconnect assertion on the server. (#56213)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-e5e1f49
+ * @license Angular v18.2.0-next.0+sha-2c4613a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-e5e1f49');
+const VERSION = new Version('18.2.0-next.0+sha-2c4613a');
 
 /**
  * @module
