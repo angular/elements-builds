1afbdfa4f91196687d14aea1ebaa419c44a7f475
Andrew Scott <atscott@google.com>
2024-07-17 21:07:28 +0000

    d97aee6 release: cut the zone.js 0.14.8 release (#57030)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-89550e7
+ * @license Angular v18.2.0-next.1+sha-d97aee6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-89550e7');
+const VERSION = new Version('18.2.0-next.1+sha-d97aee6');
 
 /**
  * @module
