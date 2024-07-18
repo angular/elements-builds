a357fba845f25c70fe085ba9601415e3941ccf82
Andrew Scott <atscott@google.com>
2024-07-18 22:31:59 +0000

    f3a88ad docs(docs-infra): update tutorial deps to 18.1 (#57024)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-425f44c
+ * @license Angular v18.2.0-next.1+sha-f3a88ad
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-425f44c');
+const VERSION = new Version('18.2.0-next.1+sha-f3a88ad');
 
 /**
  * @module
