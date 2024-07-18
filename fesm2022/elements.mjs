519094ebed5ba344ba562fb64faaba772a70de23
Andrew Scott <atscott@google.com>
2024-07-18 18:17:43 +0000

    658c7fe docs: fix changelog entry (#57038)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-c8fa616
+ * @license Angular v18.2.0-next.1+sha-658c7fe
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-c8fa616');
+const VERSION = new Version('18.2.0-next.1+sha-658c7fe');
 
 /**
  * @module
