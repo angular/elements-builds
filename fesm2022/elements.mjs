ba2e5c734ea64530cf2df1b71b6563141f303f72
Andrew Scott <atscott@google.com>
2024-07-18 18:12:09 +0000

    bbabf54 docs: fix headings and code font in `@for` docs (#57007)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-edbb403
+ * @license Angular v18.1.1+sha-bbabf54
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-edbb403');
+const VERSION = new Version('18.1.1+sha-bbabf54');
 
 /**
  * @module
