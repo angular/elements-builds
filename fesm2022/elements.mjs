0e54eb24929e17336a032cc4e73d2e9c824dc0d3
Andrew Scott <atscott@google.com>
2024-07-18 15:46:34 +0000

    5e2b29c docs(docs-infra): add open in StackBlitz button to code editor (#57018)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-d97aee6
+ * @license Angular v18.2.0-next.1+sha-5e2b29c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-d97aee6');
+const VERSION = new Version('18.2.0-next.1+sha-5e2b29c');
 
 /**
  * @module
