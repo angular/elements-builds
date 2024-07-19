4bf7dd668fb9eaee8b3667f3ef9e343606f8a6f2
Andrew Scott <atscott@google.com>
2024-07-19 18:55:34 +0000

    c76b440 fix(compiler-cli): add warning for unused let declarations (#57033)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-244c50d
+ * @license Angular v18.2.0-next.1+sha-c76b440
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-244c50d');
+const VERSION = new Version('18.2.0-next.1+sha-c76b440');
 
 /**
  * @module
