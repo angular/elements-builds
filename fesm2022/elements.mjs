f5f1a276ac648be82225499a640ecddc183d083d
Andrew Scott <atscott@google.com>
2024-07-22 18:56:06 +0000

    4639450 fix(compiler): limit the number of chained instructions (#57069)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-06ae226
+ * @license Angular v18.1.1+sha-4639450
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-06ae226');
+const VERSION = new Version('18.1.1+sha-4639450');
 
 /**
  * @module
