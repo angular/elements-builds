9eeb876630bdc05a6487b708bc319f6d623cc365
Andrew Scott <atscott@google.com>
2024-07-22 22:33:42 +0000

    915b0e0 Revert "fix(compiler-cli): add warning for unused let declarations (#57033)" (#57088)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-4639450
+ * @license Angular v18.1.1+sha-915b0e0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-4639450');
+const VERSION = new Version('18.1.1+sha-915b0e0');
 
 /**
  * @module
