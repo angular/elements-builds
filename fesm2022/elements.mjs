a0430d339c5483ff03745529599e353818d18e99
Andrew Scott <atscott@google.com>
2024-07-23 22:58:44 +0000

    83117f1 refactor(core): Switch to using concat rather than spread operator for events. (#57103)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-43515a3
+ * @license Angular v18.2.0-next.1+sha-83117f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-43515a3');
+const VERSION = new Version('18.2.0-next.1+sha-83117f1');
 
 /**
  * @module
