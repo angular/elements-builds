77dd657ad8037901dbeea7a5ef12d2ac7278a771
Andrew Scott <atscott@google.com>
2024-07-23 21:55:30 +0000

    889a02b test(docs-infra): update adev tests to be zoneless (#57100)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-23fa43d
+ * @license Angular v18.1.1+sha-889a02b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-23fa43d');
+const VERSION = new Version('18.1.1+sha-889a02b');
 
 /**
  * @module
