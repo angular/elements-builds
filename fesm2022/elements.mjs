8384d81c477eb22d7c4513c1b01860ab7d971700
Andrew Scott <atscott@google.com>
2024-07-23 17:33:52 +0000

    c979dfd build: update github/codeql-action action to v3.25.13 (#57091)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-915b0e0
+ * @license Angular v18.1.1+sha-c979dfd
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-915b0e0');
+const VERSION = new Version('18.1.1+sha-c979dfd');
 
 /**
  * @module
