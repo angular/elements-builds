7d04c1b21b2f405cbb5061c788b0d1b4c35ff8bd
Andrew Scott <atscott@google.com>
2024-07-24 17:09:28 +0000

    0e439d7 Revert "fix(router): Scroller should scroll as soon as change detection completes (#55105)" (#57115)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-83117f1
+ * @license Angular v18.2.0-next.1+sha-0e439d7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-83117f1');
+const VERSION = new Version('18.2.0-next.1+sha-0e439d7');
 
 /**
  * @module
