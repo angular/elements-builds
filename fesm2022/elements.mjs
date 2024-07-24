b2d0b98e39cbfbc193796caafd1950aa7a9b0054
Andrew Scott <atscott@google.com>
2024-07-24 17:30:11 +0000

    a7b973e docs(docs-infra): Use shiki for code highlighting (#57059)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-d5fbe11
+ * @license Angular v18.2.0-next.1+sha-a7b973e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-d5fbe11');
+const VERSION = new Version('18.2.0-next.1+sha-a7b973e');
 
 /**
  * @module
