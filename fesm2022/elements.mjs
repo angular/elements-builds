05c7a677dd9e225e82df995b851bdf1a0a3ec8eb
Dylan Hunn <dylhunn@gmail.com>
2024-07-26 17:10:18 +0000

    ca8bd5b docs: add open in IDX support (#57099)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.2+sha-ca89ef9
+ * @license Angular v18.2.0-next.2+sha-ca8bd5b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.2+sha-ca89ef9');
+const VERSION = new Version('18.2.0-next.2+sha-ca8bd5b');
 
 /**
  * @module
