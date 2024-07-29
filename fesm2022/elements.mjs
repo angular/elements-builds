7bf291ff93c51a22fe29a7bdc5f1726ac5b0914a
GitHub <noreply@github.com>
2024-07-29 08:55:09 +0000

    d108320 ci: replace `matchDepPatterns` with `matchPackageNames` (#57175)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.2+sha-ca8bd5b
+ * @license Angular v18.2.0-next.2+sha-d108320
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.2+sha-ca8bd5b');
+const VERSION = new Version('18.2.0-next.2+sha-d108320');
 
 /**
  * @module
