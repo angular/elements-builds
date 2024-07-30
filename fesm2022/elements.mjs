17fabfcd5d3870ca8cf61772e9d884b9a733f4dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 16:50:31 +0000

    130ff94 build: update all non-major dependencies (#57178)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.2+sha-81b30dc
+ * @license Angular v18.1.2+sha-130ff94
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.2+sha-81b30dc');
+const VERSION = new Version('18.1.2+sha-130ff94');
 
 /**
  * @module
