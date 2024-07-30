ac345fdf7c0dd6a5a1db7b712ff716f373c98315
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 15:56:38 +0000

    81b30dc refactor(docs-infra): migrate @angular/docs from dev-infra into adev directory (#57132)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.2+sha-9304c45
+ * @license Angular v18.1.2+sha-81b30dc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.2+sha-9304c45');
+const VERSION = new Version('18.1.2+sha-81b30dc');
 
 /**
  * @module
