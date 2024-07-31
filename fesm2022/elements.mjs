439bf146cd5b888a114c734146d897450ab25c23
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 16:14:13 +0000

    69b5a31 refactor(devtools): Use Chrome DevTools Performance extension API (#55805)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.2+sha-eadfa39
+ * @license Angular v18.1.2+sha-69b5a31
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.2+sha-eadfa39');
+const VERSION = new Version('18.1.2+sha-69b5a31');
 
 /**
  * @module
