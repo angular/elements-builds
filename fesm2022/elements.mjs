724b6fb7d28c1343805077e26efb909da3f8827e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-01 19:22:15 +0000

    f125cd8 fix(docs-infra): replace the uses of replaceAll with replace using regex with g flag (#57232)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.3+sha-f5960d0
+ * @license Angular v18.2.0-next.3+sha-f125cd8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.3+sha-f5960d0');
+const VERSION = new Version('18.2.0-next.3+sha-f125cd8');
 
 /**
  * @module
