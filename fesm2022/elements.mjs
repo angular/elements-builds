c56c78bfdff89e4bde905151550db0f8a93bdda2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 14:26:13 +0000

    2a915d1 refactor(core): Remove clickmod support from Angular. (#57201)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.3+sha-d73a374
+ * @license Angular v18.2.0-next.3+sha-2a915d1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.3+sha-d73a374');
+const VERSION = new Version('18.2.0-next.3+sha-2a915d1');
 
 /**
  * @module
