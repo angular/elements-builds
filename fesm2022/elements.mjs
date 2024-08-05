dd0a42fef38e6ac4b7feae01010340ddd7f00c45
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:04:40 +0000

    de99d3e release: cut the zone.js-0.14.10 release (#57264)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.3+sha-00a6c41
+ * @license Angular v18.2.0-next.3+sha-de99d3e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.3+sha-00a6c41');
+const VERSION = new Version('18.2.0-next.3+sha-de99d3e');
 
 /**
  * @module
