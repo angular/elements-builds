f009c7f3e3cffeecb9dc45fa31b97cad272889a4
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:13:44 +0000

    6eac1b3 refactor(docs-infra): migrate api-gen from dev-infra into the repo (#57241)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.3+sha-afb05ff
+ * @license Angular v18.1.3+sha-6eac1b3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.3+sha-afb05ff');
+const VERSION = new Version('18.1.3+sha-6eac1b3');
 
 /**
  * @module
