a8feb05499c0003247c0b1bc6a9fb3c9fe5bbd16
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-06 13:55:30 +0000

    8c029c6 build: update actions/upload-artifact action to v4.3.5 (#57271)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.3+sha-6b2a4c0
+ * @license Angular v18.1.3+sha-8c029c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.3+sha-6b2a4c0');
+const VERSION = new Version('18.1.3+sha-8c029c6');
 
 /**
  * @module
