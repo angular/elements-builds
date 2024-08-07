f4fbbd504bd16ab6976a863c5237558822d8d04e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 17:30:11 +0000

    d9d68e7 fix(compiler): reduce chance of conflicts between generated factory and local variables (#57181)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.4+sha-0b1dd39
+ * @license Angular v18.2.0-next.4+sha-d9d68e7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.4+sha-0b1dd39');
+const VERSION = new Version('18.2.0-next.4+sha-d9d68e7');
 
 /**
  * @module
