19fad1f2fb9953f0449dcb711c01edc08c159b15
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-07 19:33:45 +0000

    0761e9a docs: release notes for the v18.2.0-rc.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.4+sha-ccbce8f
+ * @license Angular v18.3.0-next.0+sha-0761e9a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.4+sha-ccbce8f');
+const VERSION = new Version('18.3.0-next.0+sha-0761e9a');
 
 /**
  * @module
