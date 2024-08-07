fcf2b0964c337398b78b5076e4fb85695a3c4c70
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 18:18:14 +0000

    e1240c6 fix(zone.js): Add support for addition jest functions. (#57280)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.4+sha-d9d68e7
+ * @license Angular v18.2.0-next.4+sha-e1240c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.4+sha-d9d68e7');
+const VERSION = new Version('18.2.0-next.4+sha-e1240c6');
 
 /**
  * @module
