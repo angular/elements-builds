362cb5874cb6754b00ac63d254c2d8f592a2d88b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 16:53:01 +0000

    0b1dd39 perf(compiler-cli): improve performance of `interpolatedSignalNotInvoked` extended diagnostic (#57291)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.4+sha-7919982
+ * @license Angular v18.2.0-next.4+sha-0b1dd39
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.4+sha-7919982');
+const VERSION = new Version('18.2.0-next.4+sha-0b1dd39');
 
 /**
  * @module
