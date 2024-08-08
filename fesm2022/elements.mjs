555556904dd5c93880635be33f2c796a3dddd2be
Andrew Kushnir <akushnir@google.com>
2024-08-08 15:37:50 +0000

    9af760e fix(core): Account for addEventListener to be passed a Window or Document. (#57282)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-rc.0+sha-02d613e
+ * @license Angular v18.2.0-rc.0+sha-9af760e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-rc.0+sha-02d613e');
+const VERSION = new Version('18.2.0-rc.0+sha-9af760e');
 
 /**
  * @module
