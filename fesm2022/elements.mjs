ccdebbfbc7157692276548a999f35efd944312d3
Andrew Kushnir <akushnir@google.com>
2024-08-12 19:17:36 +0000

    52673e6 refactor(migrations): handle safe property reads in signal input migration (#57318)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-468d3fb
+ * @license Angular v19.0.0-next.0+sha-52673e6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-468d3fb');
+const VERSION = new Version('19.0.0-next.0+sha-52673e6');
 
 /**
  * @module
