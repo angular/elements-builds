4072d0c7f2bf821f1538691b0f96937fd5d99ea2
Andrew Kushnir <akushnir@google.com>
2024-08-12 18:29:41 +0000

    468d3fb fix(core): rethrow errors during ApplicationRef.tick in TestBed (#57200)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-b279081
+ * @license Angular v19.0.0-next.0+sha-468d3fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-b279081');
+const VERSION = new Version('19.0.0-next.0+sha-468d3fb');
 
 /**
  * @module
