4b16d616f3934a57ee0d295dbe7d9e31039578ff
Andrew Kushnir <akushnir@google.com>
2024-08-13 19:15:51 +0000

    6882cc7 refactor(core): Add experimental support to have one event contract when there are multiple apps on the page. (#57355)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-e4a6198
+ * @license Angular v19.0.0-next.0+sha-6882cc7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-e4a6198');
+const VERSION = new Version('19.0.0-next.0+sha-6882cc7');
 
 /**
  * @module
