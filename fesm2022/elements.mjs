0bd1bb5ab602e78ea5fd023b6547b266f6a52127
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:48:26 +0000

    822db64 fix(core): skip hydration for i18n nodes that were not projected (#57356)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.4+sha-9814e9b
+ * @license Angular v18.1.4+sha-822db64
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.4+sha-9814e9b');
+const VERSION = new Version('18.1.4+sha-822db64');
 
 /**
  * @module
