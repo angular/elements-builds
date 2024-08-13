2b0a263e59e323c91dc408e69651ca2259c00e25
Andrew Kushnir <akushnir@google.com>
2024-08-13 22:58:47 +0000

    9857570 docs(docs-infra): fix markdown link rendering (#57377)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-6882cc7
+ * @license Angular v19.0.0-next.0+sha-9857570
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-6882cc7');
+const VERSION = new Version('19.0.0-next.0+sha-9857570');
 
 /**
  * @module
