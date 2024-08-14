159cad6505173208ee7f6fecfdf7c42a6d427a30
Andrew Kushnir <akushnir@google.com>
2024-08-14 01:27:31 +0000

    bf3235c Revert "docs(docs-infra): fix markdown link rendering (#57377)" (#57380)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.4+sha-95af661
+ * @license Angular v18.1.4+sha-bf3235c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.4+sha-95af661');
+const VERSION = new Version('18.1.4+sha-bf3235c');
 
 /**
  * @module
