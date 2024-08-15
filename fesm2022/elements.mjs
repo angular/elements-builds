5a7ddea49b700a5435131da53699fb92a7b13144
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 19:57:06 +0000

    9c73943 docs(docs-infra): move link tag for correct view (#57395)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-f03d274
+ * @license Angular v19.0.0-next.0+sha-9c73943
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-f03d274');
+const VERSION = new Version('19.0.0-next.0+sha-9c73943');
 
 /**
  * @module
