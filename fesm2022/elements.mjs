2d98a1fb91d33083f6d43f7b1c8aa828c4f7b25a
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 14:51:02 +0000

    9155f75 docs(common): fix example code (#57406)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0+sha-d49e083
+ * @license Angular v18.2.0+sha-9155f75
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0+sha-d49e083');
+const VERSION = new Version('18.2.0+sha-9155f75');
 
 /**
  * @module
