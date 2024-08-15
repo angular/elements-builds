9bb4d926f615c2bb539be8868d33822af5a72146
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 16:17:04 +0000

    58a79b6 fix(migrations): preserve type when using inject decorator (#57389)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-d4449fc
+ * @license Angular v19.0.0-next.0+sha-58a79b6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-d4449fc');
+const VERSION = new Version('19.0.0-next.0+sha-58a79b6');
 
 /**
  * @module
