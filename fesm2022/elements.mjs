1c2f05212749a36febdc9b1f47daa71567bbbc5d
Dylan Hunn <dylhunn@gmail.com>
2024-08-17 13:58:35 +0000

    368f36d refactor(migrations): framework to build batchable migrations (#57396)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-baa1254
+ * @license Angular v19.0.0-next.0+sha-368f36d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-baa1254');
+const VERSION = new Version('19.0.0-next.0+sha-368f36d');
 
 /**
  * @module
