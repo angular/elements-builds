224a2ba81fe7325313101992b9042abc66393d9f
Dylan Hunn <dylhunn@gmail.com>
2024-08-20 13:45:01 +0000

    9e87593 feat(compiler-cli): ensure template style elements are preprocessed as inline styles (#57429)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-b063468
+ * @license Angular v19.0.0-next.0+sha-9e87593
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-b063468');
+const VERSION = new Version('19.0.0-next.0+sha-9e87593');
 
 /**
  * @module
