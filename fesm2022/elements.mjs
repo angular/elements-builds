84592a7c22a6fbbc47997ac919eb73399c0b0593
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-20 20:25:32 +0000

    1a5f5ee build: update github/codeql-action action to v3.26.3 (#57455)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0+sha-980c64a
+ * @license Angular v18.2.0+sha-1a5f5ee
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0+sha-980c64a');
+const VERSION = new Version('18.2.0+sha-1a5f5ee');
 
 /**
  * @module
