7dc4cb105f718eef315cbde2d1dc8a72be3952f9
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 20:22:16 +0000

    5cc63c2 build: update zone.js peer dependency for core package to 0.15.0 (#57431)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-d1d4adc
+ * @license Angular v19.0.0-next.0+sha-5cc63c2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-d1d4adc');
+const VERSION = new Version('19.0.0-next.0+sha-5cc63c2');
 
 /**
  * @module
