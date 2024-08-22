d8b9e2964595b6fd8ce66730abe89342d75816f5
Dylan Hunn <dylhunn@gmail.com>
2024-08-22 16:17:11 +0000

    09deb24 docs: fix indentation of file tree in app-shell (#57436)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0+sha-69b20d8
+ * @license Angular v18.2.1+sha-09deb24
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0+sha-69b20d8');
+const VERSION = new Version('18.2.1+sha-09deb24');
 
 /**
  * @module
