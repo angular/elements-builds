79201c5515395a90cc71beb202186ac790ce9201
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 19:52:37 +0000

    6059ca8 refactor(core): restructure `AfterRenderManager` to connect related phases (#57453) (#57504)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-87c594b
+ * @license Angular v18.2.1+sha-6059ca8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.1+sha-87c594b');
+const VERSION = new Version('18.2.1+sha-6059ca8');
 
 /**
  * @module
