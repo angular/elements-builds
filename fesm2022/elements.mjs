af46e572d79f5a18a5932f4a3164144872f6c3c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 21:51:46 +0000

    6d3a2af fix(core): Do not bubble capture events. (#57476)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-223b785
+ * @license Angular v18.2.1+sha-6d3a2af
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.1+sha-223b785');
+const VERSION = new Version('18.2.1+sha-6d3a2af');
 
 /**
  * @module
