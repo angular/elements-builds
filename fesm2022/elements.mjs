851da33392084e974f47de5df6886d1c8aa305b7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-08-23 12:06:58 +0000

    4e803ae refactor(migrations): add support for simpler variant of tsurge migration (#57484)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.1+sha-ed920c3
+ * @license Angular v19.0.0-next.1+sha-4e803ae
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.1+sha-ed920c3');
+const VERSION = new Version('19.0.0-next.1+sha-4e803ae');
 
 /**
  * @module
