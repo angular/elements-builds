b1278ad6838a9e097eba5533822693bb8e107f0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 22:12:59 +0000

    226a67d fix(core): Schedulers run in zone above Angular rather than root (#57553)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.1+sha-76b9e2b
+ * @license Angular v19.0.0-next.1+sha-226a67d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.1+sha-76b9e2b');
+const VERSION = new Version('19.0.0-next.1+sha-226a67d');
 
 /**
  * @module
