845db647446178eca2e4dff4badc0181979dd07e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 19:46:52 +0000

    013d02d Revert "fix(http): Dynamicaly call the global fetch implementation (#57531)" (#57571)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.1+sha-b7f3baf
+ * @license Angular v19.0.0-next.1+sha-013d02d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.1+sha-b7f3baf');
+const VERSION = new Version('19.0.0-next.1+sha-013d02d');
 
 /**
  * @module
