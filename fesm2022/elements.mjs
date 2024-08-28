9d19e45f52f888521737b291f44eefdfba9e2702
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 16:22:02 +0000

    36aa3af refactor(migrations): forward-fix conflicts with generic of `ProgramInfo`. (#57565)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-adf3274
+ * @license Angular v18.2.1+sha-36aa3af
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.1+sha-adf3274');
+const VERSION = new Version('18.2.1+sha-36aa3af');
 
 /**
  * @module
