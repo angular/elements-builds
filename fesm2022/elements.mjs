e1c77765357d9081474db3d6f81bfeabbbcaf18c
Andrew Kushnir <akushnir@google.com>
2024-08-29 14:58:20 +0000

    d9c1004 refactor(migrations): ensure control flow analysis of signal input migration works with all TS versions (#57566)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.2+sha-98c6c05
+ * @license Angular v18.2.2+sha-d9c1004
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.2+sha-98c6c05');
+const VERSION = new Version('18.2.2+sha-d9c1004');
 
 /**
  * @module
