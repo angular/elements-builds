1ed4177acf9ceb5105cb32a7b21ca0f674e88a68
Andrew Kushnir <akushnir@google.com>
2024-08-29 21:29:55 +0000

    5007970 docs: revert changes on API entries urls (#57589)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.2+sha-c068045
+ * @license Angular v18.2.2+sha-5007970
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.2+sha-c068045');
+const VERSION = new Version('18.2.2+sha-5007970');
 
 /**
  * @module
