a758ee51a31fa5fd3750d67e010ab3861fe79404
Andrew Kushnir <akushnir@google.com>
2024-09-03 15:05:30 +0000

    aa8eb15 build: delete v18 migrations (#57603)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.2+sha-3bafda2
+ * @license Angular v19.0.0-next.2+sha-aa8eb15
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.2+sha-3bafda2');
+const VERSION = new Version('19.0.0-next.2+sha-aa8eb15');
 
 /**
  * @module
