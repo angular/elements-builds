3f35c4268a361d1d8bc58f6d08dba5cae9b3ced1
Andrew Kushnir <akushnir@google.com>
2024-09-03 21:35:40 +0000

    8da9fb4 feat(language-service): add code fix for unused standalone imports (#57605)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.2+sha-1f87cba
+ * @license Angular v19.0.0-next.2+sha-8da9fb4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.2+sha-1f87cba');
+const VERSION = new Version('19.0.0-next.2+sha-8da9fb4');
 
 /**
  * @module
