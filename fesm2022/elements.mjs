f8f9b92f24c21388439bd93a8f340fe8d27546ac
Andrew Kushnir <akushnir@google.com>
2024-09-03 16:15:47 +0000

    de68e04 fix(http): Dynamicaly call the global fetch implementation (#57531)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.2+sha-2324d9b
+ * @license Angular v18.2.2+sha-de68e04
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.2+sha-2324d9b');
+const VERSION = new Version('18.2.2+sha-de68e04');
 
 /**
  * @module
