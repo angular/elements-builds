5d1d72bd83ac385ad7cd4790b6f96102898b4a44
Andrew Kushnir <akushnir@google.com>
2024-09-03 14:58:08 +0000

    b494e9b build: update babel dependencies to v7.25.6 (#57634)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.2+sha-5256016
+ * @license Angular v18.2.2+sha-b494e9b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.2+sha-5256016');
+const VERSION = new Version('18.2.2+sha-b494e9b');
 
 /**
  * @module
