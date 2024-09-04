0047aef1ebca4920be51e486bd26b49ad0cd73c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 17:10:42 +0000

    9da21f7 feat(migrations): replace .next usage on outputs (#57654)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.3+sha-3c75684
+ * @license Angular v19.0.0-next.3+sha-9da21f7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.3+sha-3c75684');
+const VERSION = new Version('19.0.0-next.3+sha-9da21f7');
 
 /**
  * @module
