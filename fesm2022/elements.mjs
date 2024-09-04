5b6437ee950982f4dc5b6f4daaea97ab143540c3
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 20:13:14 +0000

    f6c40f1 refactor(migrations): expose all input migration helpers (#57659)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.3+sha-9da21f7
+ * @license Angular v19.0.0-next.3+sha-f6c40f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.3+sha-9da21f7');
+const VERSION = new Version('19.0.0-next.3+sha-f6c40f1');
 
 /**
  * @module
