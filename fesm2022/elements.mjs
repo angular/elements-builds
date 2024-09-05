cc47d921a686859758984e03cbbf1d01866e6095
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 14:18:13 +0000

    68e5370 feat(migrations): remove complete calls for migrated outputs (#57671)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.3+sha-7a4199a
+ * @license Angular v19.0.0-next.3+sha-68e5370
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.3+sha-7a4199a');
+const VERSION = new Version('19.0.0-next.3+sha-68e5370');
 
 /**
  * @module
