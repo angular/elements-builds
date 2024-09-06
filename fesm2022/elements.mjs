b80469932f0a86f3ad871d3584475f4a452d5ce0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 16:11:47 +0000

    0843f31 docs: Enable direct links to class/interface methods for `@link` (#57615)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.3+sha-bb8e7ce
+ * @license Angular v18.2.3+sha-0843f31
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.3+sha-bb8e7ce');
+const VERSION = new Version('18.2.3+sha-0843f31');
 
 /**
  * @module
