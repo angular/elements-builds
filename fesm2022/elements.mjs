48f19366c411596569fac1a98dde375332bcdea0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 14:21:45 +0000

    fc95a9a fix(migrations): replace leftover modules with their exports during pruning (#57684)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.3+sha-227d13b
+ * @license Angular v19.0.0-next.3+sha-fc95a9a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.3+sha-227d13b');
+const VERSION = new Version('19.0.0-next.3+sha-fc95a9a');
 
 /**
  * @module
