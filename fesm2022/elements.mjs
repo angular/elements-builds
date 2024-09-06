d1ec6f8eaaeb60aa1d4c2d0fd4a49a1699dbcd9b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 15:14:54 +0000

    bb8e7ce docs(docs-infra): fix extraction tests (#57694)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.3+sha-9895e44
+ * @license Angular v18.2.3+sha-bb8e7ce
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.3+sha-9895e44');
+const VERSION = new Version('18.2.3+sha-bb8e7ce');
 
 /**
  * @module
