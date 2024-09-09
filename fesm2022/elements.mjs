a5ae79797a252c93f8faddc118feb8707157bc05
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:44:51 +0000

    8d27839 docs: fix broken link in roadmap page (#57722)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.3+sha-b619d69
+ * @license Angular v18.2.3+sha-8d27839
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.3+sha-b619d69');
+const VERSION = new Version('18.2.3+sha-8d27839');
 
 /**
  * @module
