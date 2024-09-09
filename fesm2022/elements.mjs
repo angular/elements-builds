e6b0aa95e7d0f93bc95501556b1db1ea3c109e2f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 18:28:10 +0000

    d0fe538 docs(core): fix typo (#57741)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.3+sha-8d27839
+ * @license Angular v18.2.3+sha-d0fe538
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.3+sha-8d27839');
+const VERSION = new Version('18.2.3+sha-d0fe538');
 
 /**
  * @module
