881bc4ae35433699450eaa22e1a05c5931b3090c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:38:20 +0000

    bc03f53 docs: add examples for localize (#57534)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.4+sha-45575b5
+ * @license Angular v19.0.0-next.4+sha-bc03f53
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.4+sha-45575b5');
+const VERSION = new Version('19.0.0-next.4+sha-bc03f53');
 
 /**
  * @module
