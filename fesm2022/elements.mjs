76f7e5e1e67684ec76119e72e1b9ae81e406488f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 09:53:03 +0000

    156f0d0 docs: fix example for defer on hover on an alternative element (#57788)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.4+sha-fab6462
+ * @license Angular v18.2.4+sha-156f0d0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.4+sha-fab6462');
+const VERSION = new Version('18.2.4+sha-156f0d0');
 
 /**
  * @module
