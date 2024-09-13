95cbf85d8e924834bc88fda5a9076b4d202e57c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-13 11:33:02 +0000

    d5a27b8 docs: Update guide for version 18 - unclear (possibly incorrect?) instruction for opting out of caching HTTP requests that require authentication (#57799)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.4+sha-5c86694
+ * @license Angular v18.2.4+sha-d5a27b8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.4+sha-5c86694');
+const VERSION = new Version('18.2.4+sha-d5a27b8');
 
 /**
  * @module
