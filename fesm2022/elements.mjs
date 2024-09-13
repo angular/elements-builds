8683a29fef742f6c18c038839d3de189749bed51
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-13 12:18:31 +0000

    fd081e8 refactor(migrations): add compiler filesystem for devkit virtual tree (#57794)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-1b0a6a0
+ * @license Angular v19.0.0-next.5+sha-fd081e8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-1b0a6a0');
+const VERSION = new Version('19.0.0-next.5+sha-fd081e8');
 
 /**
  * @module
