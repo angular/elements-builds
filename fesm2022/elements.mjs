00a566269968b5020c2f6a0bdb8167ca3fbcaa3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:43:33 +0000

    6b066f3 build: update cross-repo angular dependencies (#57810)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-7526687
+ * @license Angular v19.0.0-next.5+sha-6b066f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-7526687');
+const VERSION = new Version('19.0.0-next.5+sha-6b066f3');
 
 /**
  * @module
