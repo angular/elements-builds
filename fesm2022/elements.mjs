24d5e48186f87060daec3662808979f83bbe1c7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 15:29:41 +0000

    8e01734 build: update dependency @inquirer/prompts to v6 (#57821)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-e5adf92
+ * @license Angular v19.0.0-next.5+sha-8e01734
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-e5adf92');
+const VERSION = new Version('19.0.0-next.5+sha-8e01734');
 
 /**
  * @module
