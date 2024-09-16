ddd3c2f5a7b3fe6547177f810cf4b5d9b26930b6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 16:09:48 +0000

    cd97054 docs: change spelling to correctly params (#57832) PR Close #57832

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.4+sha-e16b8a3
+ * @license Angular v18.2.4+sha-cd97054
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.4+sha-e16b8a3');
+const VERSION = new Version('18.2.4+sha-cd97054');
 
 /**
  * @module
