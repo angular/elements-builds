403e9eedfe5dc1239696407aab5d04df66497246
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:30:50 +0000

    e16b8a3 docs: correct key usage in sectionStyles and update template binding (#57820) PR Close #57820

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.4+sha-d5a27b8
+ * @license Angular v18.2.4+sha-e16b8a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.4+sha-d5a27b8');
+const VERSION = new Version('18.2.4+sha-e16b8a3');
 
 /**
  * @module
