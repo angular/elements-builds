3bbf34b1474b635522e7a481f720d3364ff0cd2a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 14:35:19 +0000

    4231e8f fix(core): Handle @let declaration with array when `preparingForHydration` (#57816)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-87e025a
+ * @license Angular v19.0.0-next.5+sha-4231e8f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-87e025a');
+const VERSION = new Version('19.0.0-next.5+sha-4231e8f');
 
 /**
  * @module
