8aa279ec255431d91e08beaac99e741a264c2d49
Andrew Scott <atscott@google.com>
2024-09-18 21:56:57 +0000

    4e890cc refactor(core): add support for new effect scheduling. (#56501)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-c6039b5
+ * @license Angular v19.0.0-next.6+sha-4e890cc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-c6039b5');
+const VERSION = new Version('19.0.0-next.6+sha-4e890cc');
 
 /**
  * @module
