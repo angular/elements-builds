1a6f7918083d25b77ea73213eb5fa901da042720
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 09:19:08 +0000

    6f0042e test: disable cache and analytics (#57863)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-ac3fc9a
+ * @license Angular v18.2.5+sha-6f0042e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-ac3fc9a');
+const VERSION = new Version('18.2.5+sha-6f0042e');
 
 /**
  * @module
