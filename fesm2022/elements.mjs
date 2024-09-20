281012979fdd90ec1b15d4e251fae284fdb22f17
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 13:34:13 +0000

    1ee7ec5 docs(http): heads up about provider ordering (#56518)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-e0ead92
+ * @license Angular v18.2.5+sha-1ee7ec5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-e0ead92');
+const VERSION = new Version('18.2.5+sha-1ee7ec5');
 
 /**
  * @module
