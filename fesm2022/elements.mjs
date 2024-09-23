4faf89576bbd21a72c4c977000d92059e8379f34
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 12:28:59 +0000

    7b2fda1 refactor(platform-browser): reduce runtime code size of shared style host (#57834)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-04a4873
+ * @license Angular v19.0.0-next.6+sha-7b2fda1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-04a4873');
+const VERSION = new Version('19.0.0-next.6+sha-7b2fda1');
 
 /**
  * @module
