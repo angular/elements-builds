2a4f3be729b8e300b883295463380f6a6a5cc3e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 11:47:03 +0000

    04a4873 refactor(migrations): add TODO for special output of signal input migration (#57918)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-d1391ce
+ * @license Angular v19.0.0-next.6+sha-04a4873
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-d1391ce');
+const VERSION = new Version('19.0.0-next.6+sha-04a4873');
 
 /**
  * @module
