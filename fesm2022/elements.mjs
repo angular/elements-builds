4a0bf873ac21a2d016b55d9351569a7134530872
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:13:54 +0000

    9f4063b refactor(devtools): provide user with suggestions on why their angular application is not being detected in dev mode (#57861)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-7b2fda1
+ * @license Angular v19.0.0-next.6+sha-9f4063b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-7b2fda1');
+const VERSION = new Version('19.0.0-next.6+sha-9f4063b');
 
 /**
  * @module
