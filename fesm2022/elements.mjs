3c29cefac8704c3941bba4e9ce16093e635c43e4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:29:50 +0000

    6c9abd3 refactor(forms): lift circular deps (#55881)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-9f4063b
+ * @license Angular v19.0.0-next.6+sha-6c9abd3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-9f4063b');
+const VERSION = new Version('19.0.0-next.6+sha-6c9abd3');
 
 /**
  * @module
