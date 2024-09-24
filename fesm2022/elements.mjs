b81b9b0edeef0b99e5c3d1cad0e6bd5d234f8bec
Andrew Scott <atscott@google.com>
2024-09-24 20:07:58 +0000

    dbf15c8 fix(docs-infra): extend the timeout for jasmine tests of mermaid (#57948)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-03ac3c2
+ * @license Angular v18.2.5+sha-dbf15c8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-03ac3c2');
+const VERSION = new Version('18.2.5+sha-dbf15c8');
 
 /**
  * @module
