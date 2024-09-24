4c13271edea134d6d400719b3ecf995d078b84b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 12:52:26 +0000

    9db9213 docs: use X link instead of twitter (#57608) PR Close #57608

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-97e2841
+ * @license Angular v18.2.5+sha-9db9213
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-97e2841');
+const VERSION = new Version('18.2.5+sha-9db9213');
 
 /**
  * @module
