34797262e885c23a9555aca8d9b32da4f6c9cb72
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:58:14 +0000

    97e2841 refactor(core): avoid producing zone-related warnings during hydration when in zoneless mode (#57911)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-b7b0052
+ * @license Angular v18.2.5+sha-97e2841
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-b7b0052');
+const VERSION = new Version('18.2.5+sha-97e2841');
 
 /**
  * @module
