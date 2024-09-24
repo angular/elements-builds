f75abcb84106a2b3b45996c7c8deff338be5725c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 13:07:03 +0000

    8f6c326 docs: fix image src in feature modules (#57607)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-9db9213
+ * @license Angular v18.2.5+sha-8f6c326
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-9db9213');
+const VERSION = new Version('18.2.5+sha-8f6c326');
 
 /**
  * @module
