7be3fce6ec696e13d5a8aad359f8fc260ea50372
Andrew Scott <atscott01@gmail.com>
2024-09-25 17:19:37 +0000

    1549afe release: cut the v19.0.0-next.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-95e8fb4
+ * @license Angular v19.0.0-next.7+sha-1549afe
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-95e8fb4');
+const VERSION = new Version('19.0.0-next.7+sha-1549afe');
 
 /**
  * @module
