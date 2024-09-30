fb01abfdbb5dc75c702ded72b265e0f78fc12eb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-30 20:45:35 +0000

    b9d846d fix(migrations): delete constructor if it only has super call (#58013)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.6+sha-add5c25
+ * @license Angular v18.2.6+sha-b9d846d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.6+sha-add5c25');
+const VERSION = new Version('18.2.6+sha-b9d846d');
 
 /**
  * @module
