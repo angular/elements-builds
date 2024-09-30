e4fe89a2f069f4bc4973e2bbf3523d62f0fefa93
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-30 19:56:20 +0000

    7d1998f docs: set expiry date to the developer survey (#57981)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-fbd5579
+ * @license Angular v19.0.0-next.7+sha-7d1998f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-fbd5579');
+const VERSION = new Version('19.0.0-next.7+sha-7d1998f');
 
 /**
  * @module
