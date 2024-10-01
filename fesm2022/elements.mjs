49579d381ee277c209f2dfd3b73ccd8379e2bfbb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 18:11:05 +0000

    00a79d0 refactor(migrations): properly migrate inputs marked as optional via question mark (#58031)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-d60bb51
+ * @license Angular v19.0.0-next.7+sha-00a79d0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-d60bb51');
+const VERSION = new Version('19.0.0-next.7+sha-00a79d0');
 
 /**
  * @module
