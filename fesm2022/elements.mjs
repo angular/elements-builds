da2251e56ed8dec8fe2eed2d9471989202f66479
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 11:34:07 +0000

    9ce839f test(migrations): add test for statistic collection tsurge (#58019)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-e77a163
+ * @license Angular v19.0.0-next.7+sha-9ce839f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-e77a163');
+const VERSION = new Version('19.0.0-next.7+sha-9ce839f');
 
 /**
  * @module
