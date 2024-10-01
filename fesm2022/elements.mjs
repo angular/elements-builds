f070e19a2f5aa24dbc71b842091de6e14d410bac
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 07:12:35 +0000

    b84e2d3 docs: add deferrable views tutorial (#57934)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.6+sha-b9d846d
+ * @license Angular v18.2.6+sha-b84e2d3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.6+sha-b9d846d');
+const VERSION = new Version('18.2.6+sha-b84e2d3');
 
 /**
  * @module
