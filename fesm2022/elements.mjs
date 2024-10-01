2633f7589c769f3c6de1755486b040362fe31075
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:16:35 +0000

    2ece9df docs(router): Document missing input for route binding (#57697)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-7bfe62a
+ * @license Angular v19.0.0-next.7+sha-2ece9df
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-7bfe62a');
+const VERSION = new Version('19.0.0-next.7+sha-2ece9df');
 
 /**
  * @module
