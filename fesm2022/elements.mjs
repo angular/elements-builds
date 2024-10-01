0f6b75bd180f280132c4219a08b713e21ab70021
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 16:15:32 +0000

    70591e7 docs: use proper workdir for tutorial and playground (#56063)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.6+sha-a21867d
+ * @license Angular v18.2.6+sha-70591e7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.6+sha-a21867d');
+const VERSION = new Version('18.2.6+sha-70591e7');
 
 /**
  * @module
