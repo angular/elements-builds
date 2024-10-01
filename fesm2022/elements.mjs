43ae8e8737292abf7c1807125cbbb15128352d4c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:37:34 +0000

    8d8fb44 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.6+sha-6b67cb5
+ * @license Angular v18.2.6+sha-8d8fb44
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.6+sha-6b67cb5');
+const VERSION = new Version('18.2.6+sha-8d8fb44');
 
 /**
  * @module
