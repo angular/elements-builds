79f61e5e485e07619525471666f0946f8da65cd0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 14:00:01 +0000

    3b35c4e fix(docs-infra): allow tutorial component to be GCed (#58028)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-9ce839f
+ * @license Angular v19.0.0-next.7+sha-3b35c4e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-9ce839f');
+const VERSION = new Version('19.0.0-next.7+sha-3b35c4e');
 
 /**
  * @module
