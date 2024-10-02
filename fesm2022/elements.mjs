6768acfa5c6a51d11544ecd9f6eef6a5896f10cc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 18:07:31 +0000

    dc911c6 refactor(docs-infra): cleanup `AfterRenderSequence` for reference list (#58030)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.7+sha-46b088a
+ * @license Angular v18.2.7+sha-dc911c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.7+sha-46b088a');
+const VERSION = new Version('18.2.7+sha-dc911c6');
 
 /**
  * @module
