4bc2d42e9ecba1544ea0ad6b4d165588507ac680
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 11:51:30 +0000

    46a2ad3 fix(common): prevent warning about oversize image twice (#58021)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.6+sha-c48816e
+ * @license Angular v18.2.6+sha-46a2ad3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.6+sha-c48816e');
+const VERSION = new Version('18.2.6+sha-46a2ad3');
 
 /**
  * @module
