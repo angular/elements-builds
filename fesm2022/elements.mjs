a7481df171f6abffb442a38cdfaf6b430ff416fe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-04 13:39:53 +0000

    de03e93 build: update ts target to ES2022 and module to ES2022 (#58022)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.7+sha-694fe3f
+ * @license Angular v18.2.7+sha-de03e93
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.7+sha-694fe3f');
+const VERSION = new Version('18.2.7+sha-de03e93');
 
 /**
  * @module
