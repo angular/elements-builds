ebf909b04ddf1ffecaf704864c16757beeb60e76
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:44:05 +0000

    0eb1c5c docs(common): Minor typo in code example (#58085)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-06600d2
+ * @license Angular v19.0.0-next.8+sha-0eb1c5c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-06600d2');
+const VERSION = new Version('19.0.0-next.8+sha-0eb1c5c');
 
 /**
  * @module
