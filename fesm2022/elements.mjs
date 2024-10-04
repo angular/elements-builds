09c60e2649af56be7e157e4b038f20b8f065f429
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:53:44 +0000

    10081a9 docs: move JSDoc before functions (#58087)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.7+sha-ccf2c7f
+ * @license Angular v18.2.7+sha-10081a9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.7+sha-ccf2c7f');
+const VERSION = new Version('18.2.7+sha-10081a9');
 
 /**
  * @module
