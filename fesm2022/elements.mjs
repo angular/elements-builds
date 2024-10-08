29337200e59820f44e5f5dd7f44cb43bc891dd35
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-08 06:23:45 +0000

    7b13d34 build: update scorecard action dependencies (#58116)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.7+sha-bf83801
+ * @license Angular v18.2.7+sha-7b13d34
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.7+sha-bf83801');
+const VERSION = new Version('18.2.7+sha-7b13d34');
 
 /**
  * @module
