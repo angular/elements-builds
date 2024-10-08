0544584aff624f0efa7ee86a6ed90f48f9ac7cca
Andrew Kushnir <akushnir@google.com>
2024-10-08 16:35:55 +0000

    97fb86d perf(core): set encapsulation to `None` for empty component styles (#57130)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-09f589f
+ * @license Angular v19.0.0-next.8+sha-97fb86d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-09f589f');
+const VERSION = new Version('19.0.0-next.8+sha-97fb86d');
 
 /**
  * @module
