cf14c26a77dce41f3a958a6449213ddb6c3d492c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-08 06:26:41 +0000

    bc83fc1 feat(language-service): support converting to signal queries in VSCode extension (#58106)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-5c63fc4
+ * @license Angular v19.0.0-next.8+sha-bc83fc1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-5c63fc4');
+const VERSION = new Version('19.0.0-next.8+sha-bc83fc1');
 
 /**
  * @module
