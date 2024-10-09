0bfc5da18dcb55c6f64d8ef5659ab3cbf9001751
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 09:02:44 +0000

    2213263 refactor(migrations): fix unique name generation not marking generated identifiers (#58126)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-306443d
+ * @license Angular v19.0.0-next.8+sha-2213263
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-306443d');
+const VERSION = new Version('19.0.0-next.8+sha-2213263');
 
 /**
  * @module
