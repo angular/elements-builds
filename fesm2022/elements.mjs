eb1d6ef7e0b4163530552d4d2e531358c41d553f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 13:55:46 +0000

    e00775a docs: mention autoDetectChanges parameter default value (#58092)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-9e82559
+ * @license Angular v19.0.0-next.8+sha-e00775a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-9e82559');
+const VERSION = new Version('19.0.0-next.8+sha-e00775a');
 
 /**
  * @module
