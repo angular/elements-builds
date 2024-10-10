cff0f4fc4d4f0a307a6d993cd1de501f6904e896
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:05:09 +0000

    2532ffa release: cut the v18.2.8 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.7+sha-21be258
+ * @license Angular v18.2.8+sha-2532ffa
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.7+sha-21be258');
+const VERSION = new Version('18.2.8+sha-2532ffa');
 
 /**
  * @module
