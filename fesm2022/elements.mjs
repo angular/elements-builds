9aadf3c374c12a864f5bde35baa7bf192ea1fc71
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:29:38 +0000

    da0e6b1 docs(core): fix description about signal value equality (#56472)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-2532ffa
+ * @license Angular v18.2.8+sha-da0e6b1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-2532ffa');
+const VERSION = new Version('18.2.8+sha-da0e6b1');
 
 /**
  * @module
