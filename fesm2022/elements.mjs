c5f77bbdd08350d3fb4d19b29adbaffb384f6dbe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:00:01 +0000

    46a6324 fix(compiler): scope :host-context inside pseudo selectors, do not decrease specificity (#57796)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-1479af9
+ * @license Angular v19.0.0-next.8+sha-46a6324
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-1479af9');
+const VERSION = new Version('19.0.0-next.8+sha-46a6324');
 
 /**
  * @module
