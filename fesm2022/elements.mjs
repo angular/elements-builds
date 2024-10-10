63d0a148adf9d941bfd511e90a17f5b497b7f754
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 12:43:36 +0000

    4746794 docs: move JSDoc below constructor (#57187)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-a9e440d
+ * @license Angular v18.2.8+sha-4746794
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-a9e440d');
+const VERSION = new Version('18.2.8+sha-4746794');
 
 /**
  * @module
