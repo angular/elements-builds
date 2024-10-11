abc518b80f07fd4e2dee7318cc395246b14d0a72
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 10:43:17 +0000

    d9bb74f build: update io_bazel_rules_sass digest to 583fc8c (#58155)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-4746794
+ * @license Angular v18.2.8+sha-d9bb74f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-4746794');
+const VERSION = new Version('18.2.8+sha-d9bb74f');
 
 /**
  * @module
