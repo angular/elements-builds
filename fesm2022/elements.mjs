350db51d7affaaf115434268278cd31105d4074f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-14 15:27:21 +0000

    bbca205 refactor(compiler): adjust HMR initializer block for improved Vite support (#58173)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.9+sha-c42759b
+ * @license Angular v19.0.0-next.9+sha-bbca205
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.9+sha-c42759b');
+const VERSION = new Version('19.0.0-next.9+sha-bbca205');
 
 /**
  * @module
