3f04d5f1e3a23fde928f4f364ff12381f22e2421
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 12:57:20 +0000

    97c44a1 feat(core): Promote `takeUntilDestroyed` to stable. (#58200)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.9+sha-b4de7ac
+ * @license Angular v19.0.0-next.9+sha-97c44a1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.9+sha-b4de7ac');
+const VERSION = new Version('19.0.0-next.9+sha-97c44a1');
 
 /**
  * @module
