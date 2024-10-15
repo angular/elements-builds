ecd40e24dbfc0464ff197f4f83f7d210324525e9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 16:54:42 +0000

    06f45c3 docs: update browserslist config to include last 2 Android major versions (#58186)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-4049d77
+ * @license Angular v18.2.8+sha-06f45c3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-4049d77');
+const VERSION = new Version('18.2.8+sha-06f45c3');
 
 /**
  * @module
