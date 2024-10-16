074e3045cbe488dfb79d77d9c6dea8f3754770cf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-16 06:50:47 +0000

    4288ea8 docs: update Angular CLI help [main] (#58210)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.9+sha-7f0b49d
+ * @license Angular v19.0.0-next.9+sha-4288ea8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.9+sha-7f0b49d');
+const VERSION = new Version('19.0.0-next.9+sha-4288ea8');
 
 /**
  * @module
