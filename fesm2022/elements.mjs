fe1e04dce9812a4e09c05ec248d682b60e85ea4a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 14:40:43 +0000

    b63c2ed docs: update default asset paths (#58246)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-d1cfbb1
+ * @license Angular v18.2.8+sha-b63c2ed
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-d1cfbb1');
+const VERSION = new Version('18.2.8+sha-b63c2ed');
 
 /**
  * @module
