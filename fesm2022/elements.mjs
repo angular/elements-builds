38f6070109b5297debddc34a5dd5f42bd14ddcc7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 08:17:42 +0000

    8311f00 feat(core): introduce the reactive linkedSignal (#58189)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.10+sha-e603e56
+ * @license Angular v19.0.0-next.10+sha-8311f00
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.10+sha-e603e56');
+const VERSION = new Version('19.0.0-next.10+sha-8311f00');
 
 /**
  * @module
