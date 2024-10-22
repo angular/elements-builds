860cb4fafd3f67b1074bc91aebeb0ed2e296e6f9
Andrew Kushnir <akushnir@google.com>
2024-10-22 16:49:58 +0000

    713a14f build: update actions/cache digest to 6849a64 (#58305)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-7e933d8
+ * @license Angular v18.2.8+sha-713a14f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-7e933d8');
+const VERSION = new Version('18.2.8+sha-713a14f');
 
 /**
  * @module
