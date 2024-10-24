99ae945bfe596223accd9748845da725852406dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 19:46:57 +0000

    e638b29 docs(router): document segment consumption during route evaluation (#58069)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-fae3c83
+ * @license Angular v18.2.9+sha-e638b29
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-fae3c83');
+const VERSION = new Version('18.2.9+sha-e638b29');
 
 /**
  * @module
