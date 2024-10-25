c013b00461dd006628d6bfb84ca8b81bbcbabc3c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-25 18:53:00 +0000

    35d7ca5 test: support parallel tsurge unit combining in batch test infra (#58280)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-2aa9f8b
+ * @license Angular v19.1.0-next.0+sha-35d7ca5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-2aa9f8b');
+const VERSION = new Version('19.1.0-next.0+sha-35d7ca5');
 
 /**
  * @module
