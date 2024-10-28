ee42e4827133008b8732028420fd3398975e5567
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 19:56:14 +0000

    98f54cb docs: update Angular CLI help [19.0.x] (#58395)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.11+sha-f815d7b
+ * @license Angular v19.0.0-next.11+sha-98f54cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.11+sha-f815d7b');
+const VERSION = new Version('19.0.0-next.11+sha-98f54cb');
 
 /**
  * @module
