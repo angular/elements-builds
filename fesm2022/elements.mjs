b00496960339e9b301dedb8c3326dd3bdb471e2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 19:39:33 +0000

    ddda355 refactor(core): remove globalApi tag (#58375)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-3b989ac
+ * @license Angular v18.2.9+sha-ddda355
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-3b989ac');
+const VERSION = new Version('18.2.9+sha-ddda355');
 
 /**
  * @module
