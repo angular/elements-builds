44bc7de371d62fe7db734c0d10a427da8a368262
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-29 12:18:16 +0000

    1f13a5e build: update scorecard action dependencies (#58400)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-29660f2
+ * @license Angular v18.2.9+sha-1f13a5e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-29660f2');
+const VERSION = new Version('18.2.9+sha-1f13a5e');
 
 /**
  * @module
