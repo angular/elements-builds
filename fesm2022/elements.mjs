5018d554d6b6e05305275f897cbab4ee61bf4f61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 16:31:23 +0000

    69dce38 Revert fix(compiler): transform pseudo selectors correctly for the encapsulated view. (#58417)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-1f13a5e
+ * @license Angular v18.2.9+sha-69dce38
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-1f13a5e');
+const VERSION = new Version('18.2.9+sha-69dce38');
 
 /**
  * @module
