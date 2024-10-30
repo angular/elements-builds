a2c70f9e72444bf8d8f9e1bfdec2a2d022e23062
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 19:00:07 +0000

    183af09 refactor(migrations): Make the explicit standalone migration idempotent (#58418)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-378284f
+ * @license Angular v19.1.0-next.0+sha-183af09
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-378284f');
+const VERSION = new Version('19.1.0-next.0+sha-183af09');
 
 /**
  * @module
