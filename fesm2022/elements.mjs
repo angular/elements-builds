e4a5b9490a7288b51e62ec03dce7fe368c685232
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 16:29:46 +0000

    db7ed20 fix(migrations): properly replace imports across files (#58414)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-929db81
+ * @license Angular v19.1.0-next.0+sha-db7ed20
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-929db81');
+const VERSION = new Version('19.1.0-next.0+sha-db7ed20');
 
 /**
  * @module
