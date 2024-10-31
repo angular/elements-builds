55ec9658874b1baa805c3ac47cbb0363e24a4042
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-10-31 18:11:37 +0000

    127023f build: update `yarn` to `1.22.22` (#58448)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.10+sha-5828585
+ * @license Angular v18.2.10+sha-127023f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.10+sha-5828585');
+const VERSION = new Version('18.2.10+sha-127023f');
 
 /**
  * @module
