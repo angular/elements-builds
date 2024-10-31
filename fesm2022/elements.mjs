0bd43fc3afea451500cacaf50df77be2d7fb3669
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-10-31 17:17:42 +0000

    89db5f7 refactor(core): tree-shake defer block registry (#58424)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-32a5388
+ * @license Angular v19.1.0-next.0+sha-89db5f7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-32a5388');
+const VERSION = new Version('19.1.0-next.0+sha-89db5f7');
 
 /**
  * @module
