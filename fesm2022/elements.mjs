2b0630dae6c0876f15a25627d5d917868bb46620
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 14:47:11 +0000

    9d9abf2 build: Update publish-build-artifacts to honor ORG (#58408)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.10+sha-a249d0b
+ * @license Angular v18.2.10+sha-9d9abf2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.10+sha-a249d0b');
+const VERSION = new Version('18.2.10+sha-9d9abf2');
 
 /**
  * @module
