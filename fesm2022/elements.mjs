39983547225e5ac31826a7050b4a1ba69e70a768
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-03 18:12:15 +0000

    c5e676b fix(migrations): flip the default standalone flag in route-lazy-loading migration (#58474)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.0+sha-86d8f6b
+ * @license Angular v19.0.0-rc.0+sha-c5e676b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.0+sha-86d8f6b');
+const VERSION = new Version('19.0.0-rc.0+sha-c5e676b');
 
 /**
  * @module
