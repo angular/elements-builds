81a94913b258201cbe884c359aa5e45013aab1a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 14:36:18 +0000

    d076c32 refactor(core): always create new renderer when applying HMR metadata update (#58527)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-b371384
+ * @license Angular v19.1.0-next.0+sha-d076c32
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-b371384');
+const VERSION = new Version('19.1.0-next.0+sha-d076c32');
 
 /**
  * @module
