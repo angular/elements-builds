d7486a5465a37e22c5d8ccdcfc214eaac76d725d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-08 17:33:48 +0000

    2fbc7f0 docs: Fix menu label for ng cache clean, clear -> clean (#58560) PR Close #58560

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.11+sha-f3567cc
+ * @license Angular v18.2.11+sha-2fbc7f0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.11+sha-f3567cc');
+const VERSION = new Version('18.2.11+sha-2fbc7f0');
 
 /**
  * @module
