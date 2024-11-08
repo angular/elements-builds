ddc6381751757a579224ce3d7f6df7ab6ebd1ea5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-08 19:01:51 +0000

    bc5aa3c docs: update hybrid rendering docs to remove `RenderMode.AppShell` (#58564)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-ff11551
+ * @license Angular v19.1.0-next.0+sha-bc5aa3c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-ff11551');
+const VERSION = new Version('19.1.0-next.0+sha-bc5aa3c');
 
 /**
  * @module
