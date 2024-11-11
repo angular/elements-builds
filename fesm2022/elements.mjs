6848617f4ebc90e3b6f15b9ef6fbf914bb36e9e5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 14:42:25 +0000

    b6a998b docs: add define option and prebundling info for application builder (#58569)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-9395aae
+ * @license Angular v19.0.0-rc.1+sha-b6a998b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-9395aae');
+const VERSION = new Version('19.0.0-rc.1+sha-b6a998b');
 
 /**
  * @module
