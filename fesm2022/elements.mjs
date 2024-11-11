b73df6994dcebb8d6536c527c5f1780d6d79b1ef
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 14:56:05 +0000

    7263917 ci: re-enable adev tests (#58574)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4327aa8
+ * @license Angular v19.1.0-next.0+sha-7263917
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4327aa8');
+const VERSION = new Version('19.1.0-next.0+sha-7263917');
 
 /**
  * @module
