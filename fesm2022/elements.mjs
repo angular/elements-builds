5739f410d1ca5435820219579ba2cd7dbe0e148a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 15:37:02 +0000

    9bbb01c fix(compiler-cli): report individual diagnostics for unused imports (#58589)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-64661dd
+ * @license Angular v19.0.0-rc.1+sha-9bbb01c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-64661dd');
+const VERSION = new Version('19.0.0-rc.1+sha-9bbb01c');
 
 /**
  * @module
