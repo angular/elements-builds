e8f31e5b8f9a1a5a2b7ae93aea800185cc24a409
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 18:11:09 +0000

    c421ffd fix(compiler): control flow nodes with root at the end projected incorrectly (#58607)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-f2bda8e
+ * @license Angular v19.1.0-next.0+sha-c421ffd
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-f2bda8e');
+const VERSION = new Version('19.1.0-next.0+sha-c421ffd');
 
 /**
  * @module
