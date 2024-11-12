880c783200b5788f48949bbe31daede6494ec964
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 14:55:05 +0000

    d190f82 test(core): add incremental hydration tests (#58601)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-c77b53b
+ * @license Angular v19.0.0-rc.1+sha-d190f82
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-c77b53b');
+const VERSION = new Version('19.0.0-rc.1+sha-d190f82');
 
 /**
  * @module
