a1fdd1c13439bd535d491fe4ba743492b5ba8c3e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 17:30:57 +0000

    f2bda8e fix(core): prevent errors on contract cleanup (#58614)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4df352b
+ * @license Angular v19.1.0-next.0+sha-f2bda8e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4df352b');
+const VERSION = new Version('19.1.0-next.0+sha-f2bda8e');
 
 /**
  * @module
