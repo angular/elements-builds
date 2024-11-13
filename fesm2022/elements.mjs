a3fa7fad6a9ccc8d1459a8d415e9982262dffa27
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 18:54:44 +0000

    4f7ad3a build: update io_bazel_rules_sass digest to b442a44 (#58617)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-ee320ee
+ * @license Angular v19.1.0-next.0+sha-4f7ad3a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-ee320ee');
+const VERSION = new Version('19.1.0-next.0+sha-4f7ad3a');
 
 /**
  * @module
