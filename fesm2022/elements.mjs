ac8437f3f18087961ac21bda68829efcbe05d10a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 15:45:48 +0000

    8f558b6 docs: update components guide for default standalone (#58605)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-f3d9316
+ * @license Angular v19.1.0-next.0+sha-8f558b6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-f3d9316');
+const VERSION = new Version('19.1.0-next.0+sha-8f558b6');
 
 /**
  * @module
