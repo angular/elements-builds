aacc6d496bd75ce6685c766c2ce0622faa1588f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 20:09:26 +0000

    84c543a docs: update NgModule overview for standalone default (#58606)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4f7ad3a
+ * @license Angular v19.1.0-next.0+sha-84c543a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4f7ad3a');
+const VERSION = new Version('19.1.0-next.0+sha-84c543a');
 
 /**
  * @module
