8113faebb10a1b49bec51879c2359617dd5e5551
Andrew Kushnir <akushnir@google.com>
2024-11-14 22:26:40 +0000

    91a616f refactor(core): add `REQUEST`, `RESPONSE_INIT` and `REQUEST_CONTEXT` tokens (#58669)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.2+sha-ee49947
+ * @license Angular v19.0.0-rc.2+sha-91a616f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.2+sha-ee49947');
+const VERSION = new Version('19.0.0-rc.2+sha-91a616f');
 
 /**
  * @module
