51c171f49008cb089bfedd0df4f38d3637134d9a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-15 11:58:55 +0000

    e33b195 fix(bazel): allow strictStandalone to be passed through tsconfig (#58683)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.2+sha-806a61b
+ * @license Angular v19.0.0-rc.2+sha-e33b195
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.2+sha-806a61b');
+const VERSION = new Version('19.0.0-rc.2+sha-e33b195');
 
 /**
  * @module
