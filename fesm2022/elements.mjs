9b473c87ada7d09ba25718704b4072b7299f13bf
Andrew Kushnir <akushnir@google.com>
2024-11-18 21:41:15 +0000

    281f747 docs(docs-infra): redirect to the root of the app. (#58700)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-7d161b9
+ * @license Angular v19.1.0-next.0+sha-281f747
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-7d161b9');
+const VERSION = new Version('19.1.0-next.0+sha-281f747');
 
 /**
  * @module
