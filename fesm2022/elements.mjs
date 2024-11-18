781f69d23da0c1b54f2fc321674c3205eb76700e
Andrew Kushnir <akushnir@google.com>
2024-11-18 20:54:37 +0000

    7d161b9 docs: add v19 developer event banner (#58721)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-92f30a7
+ * @license Angular v19.1.0-next.0+sha-7d161b9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-92f30a7');
+const VERSION = new Version('19.1.0-next.0+sha-7d161b9');
 
 /**
  * @module
