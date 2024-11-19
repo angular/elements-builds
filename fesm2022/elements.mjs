589437beeb2fd638bd167693ba488e9693ba11ea
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 14:26:59 +0000

    e7566c3 release: cut the v19.0.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.3+sha-b2802e5
+ * @license Angular v19.0.0+sha-e7566c3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.3+sha-b2802e5');
+const VERSION = new Version('19.0.0+sha-e7566c3');
 
 /**
  * @module
