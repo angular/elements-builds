d77a3236eef2ee7e5f2d4b4e46aee59412d9c468
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 17:00:59 +0000

    7bd56fd build: publish `angular-in-memory-web-api@0.19.0` for v19 compatibility (#58741)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-e7566c3
+ * @license Angular v19.0.0+sha-7bd56fd
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-e7566c3');
+const VERSION = new Version('19.0.0+sha-7bd56fd');
 
 /**
  * @module
