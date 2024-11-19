64c01fc666021c1c0f479035dd8e14e30e1596a8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 17:32:19 +0000

    c4357eb docs: update tutorials and playground to v19.0.0 (#58742)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-7bd56fd
+ * @license Angular v19.0.0+sha-c4357eb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-7bd56fd');
+const VERSION = new Version('19.0.0+sha-c4357eb');
 
 /**
  * @module
