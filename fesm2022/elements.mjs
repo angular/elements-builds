5efbf4c9f184cc8909a3d07c41b1978b80dec9ae
Andrew Kushnir <akushnir@google.com>
2024-11-20 20:03:38 +0000

    7c9f425 ci: update to latest actions from angular/dev-infra (#58781)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-678f333
+ * @license Angular v19.0.0+sha-7c9f425
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-678f333');
+const VERSION = new Version('19.0.0+sha-7c9f425');
 
 /**
  * @module
