968e462e34279076166cc63d3ed24408b6c5d237
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-21 16:42:06 +0000

    6b057bc refactor(forms): work around another TypeScript 5.7 issue (#58782)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-ec674c6
+ * @license Angular v19.0.0+sha-6b057bc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-ec674c6');
+const VERSION = new Version('19.0.0+sha-6b057bc');
 
 /**
  * @module
