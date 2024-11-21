5443c30474aedc0d61241b6d6e8d6f123c3391f0
Joey Perrott <josephperrott@gmail.com>
2024-11-21 15:06:52 +0000

    ec674c6 ci: update to latest actions from angular/dev-infra (#58799)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-a3be5b0
+ * @license Angular v19.0.0+sha-ec674c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-a3be5b0');
+const VERSION = new Version('19.0.0+sha-ec674c6');
 
 /**
  * @module
