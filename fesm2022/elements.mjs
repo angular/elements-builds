0d8cd698112ab2280a610bbc2ad82efc6d926279
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 14:53:33 +0000

    aca8cc0 build: lock file maintenance (#58711)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-bd08d1d
+ * @license Angular v19.1.0-next.0+sha-aca8cc0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-bd08d1d');
+const VERSION = new Version('19.1.0-next.0+sha-aca8cc0');
 
 /**
  * @module
