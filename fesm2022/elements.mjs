a6103126dd6a0e738a48da3efacad2ed6d8c8f29
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 15:02:31 +0000

    7fd034e build: remove unused patch (#58821)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-aca8cc0
+ * @license Angular v19.1.0-next.0+sha-7fd034e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-aca8cc0');
+const VERSION = new Version('19.1.0-next.0+sha-7fd034e');
 
 /**
  * @module
