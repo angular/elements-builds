95ba3d18e480045c547a5ae4525c5b7661068587
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 17:18:42 +0000

    e395364 build: pin google-github-actions/auth action to 6fc4af4 (#58879)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-c56ec5e
+ * @license Angular v19.1.0-next.0+sha-e395364
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-c56ec5e');
+const VERSION = new Version('19.1.0-next.0+sha-e395364');
 
 /**
  * @module
