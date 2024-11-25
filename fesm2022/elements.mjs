98553e24b0ee8b713211043b52c463b74263d705
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 15:36:19 +0000

    08b9452 fix(core): Ensure resource sets an error (#58855)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-a41ac83
+ * @license Angular v19.0.0+sha-08b9452
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-a41ac83');
+const VERSION = new Version('19.0.0+sha-08b9452');
 
 /**
  * @module
