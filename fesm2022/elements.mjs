b8eb9323c96ee539f80af70f0be7a7577c2ce9a1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-26 18:09:48 +0000

    e1e419a release: cut the v18.2.13 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.12+sha-c6cec5e
+ * @license Angular v18.2.13+sha-e1e419a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.12+sha-c6cec5e');
+const VERSION = new Version('18.2.13+sha-e1e419a');
 
 /**
  * @module
