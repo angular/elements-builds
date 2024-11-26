4b0467ad619c7caf658dc976cf61c2ae4c987863
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 14:55:26 +0000

    61ae17a refactor(router): remove unused default error handler function (#58819)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-3e2851b
+ * @license Angular v19.1.0-next.0+sha-61ae17a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-3e2851b');
+const VERSION = new Version('19.1.0-next.0+sha-61ae17a');
 
 /**
  * @module
