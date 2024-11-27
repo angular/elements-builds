0e8a62213e74fb8374abc605a750e6d60b670717
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 17:17:02 +0000

    3b76536 fix(core): Explicitly manage TracingSnapshot lifecycle and dispose of it once it's been used. (#58929)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-30891d8
+ * @license Angular v19.1.0-next.0+sha-3b76536
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-30891d8');
+const VERSION = new Version('19.1.0-next.0+sha-3b76536');
 
 /**
  * @module
