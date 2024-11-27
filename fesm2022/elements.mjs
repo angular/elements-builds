2ec59501c3956b4ff902e531fd26590940879dd9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 16:05:11 +0000

    30891d8 refactor(core): Consolidates shouldTrigger* methods down to one (#58833)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4a874bf
+ * @license Angular v19.1.0-next.0+sha-30891d8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4a874bf');
+const VERSION = new Version('19.1.0-next.0+sha-30891d8');
 
 /**
  * @module
