d0c703ac686b412d965deeab0c392389c386888f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 15:48:13 +0000

    66abff6 docs(docs-infra): don't use `URL` to check for external links (#58955)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-a4b86b2
+ * @license Angular v19.1.0-next.0+sha-66abff6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-a4b86b2');
+const VERSION = new Version('19.1.0-next.0+sha-66abff6');
 
 /**
  * @module
