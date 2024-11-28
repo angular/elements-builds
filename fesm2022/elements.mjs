b0c80aaeaa76be7b155142daaabf9715454264d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 14:23:09 +0000

    e86b179 refactor(core): preventing resolving renderer factory every tick (#58618)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-293dce9
+ * @license Angular v19.0.1+sha-e86b179
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-293dce9');
+const VERSION = new Version('19.0.1+sha-e86b179');
 
 /**
  * @module
