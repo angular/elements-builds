aa0f8ff600d6b0be505b219881f67069f82135a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-29 14:01:49 +0000

    db7ea92 docs: update two-way binding example in documentation (#58963)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-9bee4a3
+ * @license Angular v19.0.1+sha-db7ea92
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-9bee4a3');
+const VERSION = new Version('19.0.1+sha-db7ea92');
 
 /**
  * @module
