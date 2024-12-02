8aeb27a3da22a2512afe9e79bc5af587d305143c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 10:40:32 +0000

    1b6204d docs(core): fix eventCoalescing comment (#57097)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-16ab166
+ * @license Angular v19.1.0-next.0+sha-1b6204d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-16ab166');
+const VERSION = new Version('19.1.0-next.0+sha-1b6204d');
 
 /**
  * @module
