44183ab472967a48b9493d3fa5442e04c5647939
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 11:26:40 +0000

    a107bf8 build: update dependency mocha to v11 (#59012)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-bc5ca99
+ * @license Angular v19.0.1+sha-a107bf8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-bc5ca99');
+const VERSION = new Version('19.0.1+sha-a107bf8');
 
 /**
  * @module
