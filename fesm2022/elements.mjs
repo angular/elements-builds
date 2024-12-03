046f6493ee6d440e171b40546ae066027af475c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 15:19:34 +0000

    5cd35cb docs: remove extra spacing from DI token note (#59023)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-a107bf8
+ * @license Angular v19.0.1+sha-5cd35cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-a107bf8');
+const VERSION = new Version('19.0.1+sha-5cd35cb');
 
 /**
  * @module
