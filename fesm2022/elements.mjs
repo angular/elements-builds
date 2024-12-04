e00ea162398d0fa3f0dc3577676f4b6971a1d360
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 16:35:24 +0000

    0513fbc docs: set syntax highlighting of code examples MD code blocks (#59026)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.1+sha-dc7d555
+ * @license Angular v19.1.0-next.1+sha-0513fbc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.1+sha-dc7d555');
+const VERSION = new Version('19.1.0-next.1+sha-0513fbc');
 
 /**
  * @module
