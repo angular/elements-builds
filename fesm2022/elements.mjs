e5addc0bb3be4a411e7cfd1c3b8d2406dbbbda92
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 18:16:42 +0000

    04e074b Revert "fix(platform-server): remove peer dependency on animations (#58997)" (#59051)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.1+sha-60e5900
+ * @license Angular v19.1.0-next.1+sha-04e074b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.1+sha-60e5900');
+const VERSION = new Version('19.1.0-next.1+sha-04e074b');
 
 /**
  * @module
