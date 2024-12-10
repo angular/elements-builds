f8bcd7311019b2e6fdc98d6899976ca977630f77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-10 21:55:21 +0000

    f3729ce ci: Update caretaker config for merge queue (#59095)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.2+sha-46f00f9
+ * @license Angular v19.1.0-next.2+sha-f3729ce
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.2+sha-46f00f9');
+const VERSION = new Version('19.1.0-next.2+sha-f3729ce');
 
 /**
  * @module
