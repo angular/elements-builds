24036ac92c57b48eb4c88e3a0aa58109664f3add
Andrew Kushnir <akushnir@google.com>
2024-12-13 18:16:10 +0000

    3793218 fix(core): avoid triggering `on timer` and `on idle` on the server (#59177)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.4+sha-c8b1926
+ * @license Angular v19.0.4+sha-3793218
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.4+sha-c8b1926');
+const VERSION = new Version('19.0.4+sha-3793218');
 
 /**
  * @module
