acabff41955da25add82110acadc6b29581c1049
Andrew Kushnir <akushnir@google.com>
2024-12-13 00:25:20 +0000

    9203276 docs: update i18n deployment docs to use subPath option (#59166)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.3+sha-8230388
+ * @license Angular v19.1.0-next.3+sha-9203276
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.3+sha-8230388');
+const VERSION = new Version('19.1.0-next.3+sha-9203276');
 
 /**
  * @module
