034bf3901ae2e7225c46e6b93e8eca403ff2218b
Andrew Kushnir <akushnir@google.com>
2024-12-16 19:40:40 +0000

    b74e06e docs: Fix Dependency Injection Readme guide link (#59193) PR Close #59193

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.4+sha-4d74f86
+ * @license Angular v19.0.4+sha-b74e06e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.4+sha-4d74f86');
+const VERSION = new Version('19.0.4+sha-b74e06e');
 
 /**
  * @module
