88e9171af69f0db3a048f8523e03c521a64a84b4
Andrew Kushnir <akushnir@google.com>
2024-12-17 17:44:23 +0000

    f1ab35c refactor(compiler-cli): fix instanceof for error not working (#59219)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.4+sha-f0df010
+ * @license Angular v19.0.4+sha-f1ab35c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.4+sha-f0df010');
+const VERSION = new Version('19.0.4+sha-f1ab35c');
 
 /**
  * @module
