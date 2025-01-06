9430cd80dca7dec5ab0aa1e913039d7c7370fa13
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 17:36:03 +0000

    cc7634f fix(bazel): handle module names and ES output in hybrid `ts_project` mode (#59325)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-0bf6f76
+ * @license Angular v19.1.0-next.4+sha-cc7634f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-0bf6f76');
+const VERSION = new Version('19.1.0-next.4+sha-cc7634f');
 
 /**
  * @module
