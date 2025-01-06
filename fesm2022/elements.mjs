4231cf98e87e2821e58237fbf644646374945afb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 20:58:33 +0000

    fdfaabb refactor(compiler-cli): extract function overload separatly (#56489)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.5+sha-66f2aa3
+ * @license Angular v19.0.5+sha-fdfaabb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.5+sha-66f2aa3');
+const VERSION = new Version('19.0.5+sha-fdfaabb');
 
 /**
  * @module
