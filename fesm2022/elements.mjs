c1077565f7640084d3e9d44a770ff64a74f91ded
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 20:19:55 +0000

    7353054 refactor(core): drop `DEFER_BLOCK_DEPENDENCY_INTERCEPTOR` in production (#59381)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.5+sha-c82ad87
+ * @license Angular v19.0.5+sha-7353054
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.5+sha-c82ad87');
+const VERSION = new Version('19.0.5+sha-7353054');
 
 /**
  * @module
