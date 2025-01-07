80886494b68df8b900c7db0e45be37874e421ef1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 16:12:13 +0000

    9b8f699 refactor: remove unnecessary `// tslint:disable` rule flags in devtools (#59365)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-0e23f20
+ * @license Angular v19.1.0-next.4+sha-9b8f699
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-0e23f20');
+const VERSION = new Version('19.1.0-next.4+sha-9b8f699');
 
 /**
  * @module
