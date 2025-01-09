07aa0d208d28c8ecc0f8c0c4c20405387f3021cb
kirjs <kirjs@users.noreply.github.com>
2025-01-09 17:43:17 +0000

    f2e293d refactor(platform-browser): drop Hammer token names in production (#59438)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-3638f93
+ * @license Angular v19.2.0-next.0+sha-f2e293d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-3638f93');
+const VERSION = new Version('19.2.0-next.0+sha-f2e293d');
 
 /**
  * @module
