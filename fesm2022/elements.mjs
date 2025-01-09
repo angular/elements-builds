54b7754c052c2c445458767cb80f855737d570d1
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:36:48 +0000

    fb67b10 ci: Add self (Ethan Cline) as primitives-shared reviewer. (#59437)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-28a5a89
+ * @license Angular v19.0.6+sha-fb67b10
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-28a5a89');
+const VERSION = new Version('19.0.6+sha-fb67b10');
 
 /**
  * @module
