a56ad1d178183077ff98594df2b62fb557bb9147
kirjs <kirjs@users.noreply.github.com>
2025-01-09 20:46:44 +0000

    2ec7664 release: bump Angular DevTools version to 1.0.20 (#59454)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-679d4d1
+ * @license Angular v19.1.0-rc.0+sha-2ec7664
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-679d4d1');
+const VERSION = new Version('19.1.0-rc.0+sha-2ec7664');
 
 /**
  * @module
