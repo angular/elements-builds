b2b32c0b2186d469c3f606cf87e9f6a54e394a08
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:01:13 +0000

    534368c docs: update component scenarios guide (#59461) PR Close #59461

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-178c619
+ * @license Angular v19.0.6+sha-534368c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-178c619');
+const VERSION = new Version('19.0.6+sha-534368c');
 
 /**
  * @module
