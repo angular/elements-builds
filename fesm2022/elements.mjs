e589d68620d61f5845aec36bde0c69a9a69ae454
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:57:03 +0000

    eb2fcd1 fix(migrations): incorrect stats when migrating queries with best effort mode (#59463)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-534368c
+ * @license Angular v19.0.6+sha-eb2fcd1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-534368c');
+const VERSION = new Version('19.0.6+sha-eb2fcd1');
 
 /**
  * @module
