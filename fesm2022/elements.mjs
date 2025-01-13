2cb3d5ee00bbf6ed7ba681d8c59a57d730d0f8bd
kirjs <kirjs@users.noreply.github.com>
2025-01-13 19:52:59 +0000

    8a1dcaa fix(devtools): remove property tab css that is hiding directive metadata (#59493)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-7d273a1
+ * @license Angular v19.0.6+sha-8a1dcaa
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-7d273a1');
+const VERSION = new Version('19.0.6+sha-8a1dcaa');
 
 /**
  * @module
