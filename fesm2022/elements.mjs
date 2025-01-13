5c510caf09fdb1a29835d012e605f862d9e0b2cf
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:43:19 +0000

    cb30fec docs: fix typo example code in pipes documentation (#59312)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-40e7904
+ * @license Angular v19.2.0-next.0+sha-cb30fec
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-40e7904');
+const VERSION = new Version('19.2.0-next.0+sha-cb30fec');
 
 /**
  * @module
