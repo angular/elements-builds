fef6a4e90be56eaeccca0aa08838351620982cff
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:52:14 +0000

    790221b docs: add component harnesses guides (#59078)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-cb30fec
+ * @license Angular v19.2.0-next.0+sha-790221b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-cb30fec');
+const VERSION = new Version('19.2.0-next.0+sha-790221b');
 
 /**
  * @module
