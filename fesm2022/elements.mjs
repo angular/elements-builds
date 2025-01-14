62a3ce3708f6c9c959e8a1994db346aab8cb5b58
kirjs <kirjs@users.noreply.github.com>
2025-01-14 17:41:09 +0000

    815f1b1 refactor(core): add profiler calls for tne newly introduced events (#59233)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-2ca2587
+ * @license Angular v19.2.0-next.0+sha-815f1b1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-2ca2587');
+const VERSION = new Version('19.2.0-next.0+sha-815f1b1');
 
 /**
  * @module
