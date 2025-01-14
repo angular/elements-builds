b80d07f9be1bd7c53152e4cfea799686a8e0b49e
kirjs <kirjs@users.noreply.github.com>
2025-01-14 20:21:53 +0000

    3e7ba3d docs: set syntax highlighting to the remaining Markdown code examples blocks (#59088)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-99c542a
+ * @license Angular v19.1.0-rc.0+sha-3e7ba3d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-99c542a');
+const VERSION = new Version('19.1.0-rc.0+sha-3e7ba3d');
 
 /**
  * @module
