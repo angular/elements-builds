6c76a787785a12945168a4cb971a347deaf61cac
kirjs <kirjs@users.noreply.github.com>
2025-01-14 22:53:44 +0000

    990ce59 Revert "refactor: initialize headers map directly in HttpHeaders class (#59268)" (#59523)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-601994d
+ * @license Angular v19.0.6+sha-990ce59
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-601994d');
+const VERSION = new Version('19.0.6+sha-990ce59');
 
 /**
  * @module
