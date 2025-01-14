41b57ed648d791fb4568f4020f1932eee8513b5f
kirjs <kirjs@users.noreply.github.com>
2025-01-14 20:03:08 +0000

    265265a docs(docs-infra): fix scrolling issues in the API reference (#59207)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-19d2241
+ * @license Angular v19.2.0-next.0+sha-265265a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-19d2241');
+const VERSION = new Version('19.2.0-next.0+sha-265265a');
 
 /**
  * @module
