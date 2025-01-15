9f82728d74348e23e8909dc7c902560df48c5dad
kirjs <kirjs@users.noreply.github.com>
2025-01-15 17:35:04 +0000

    681f5e4 refactor(common): drop error message in production (#59471)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-9bd5b4a
+ * @license Angular v19.0.6+sha-681f5e4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-9bd5b4a');
+const VERSION = new Version('19.0.6+sha-681f5e4');
 
 /**
  * @module
