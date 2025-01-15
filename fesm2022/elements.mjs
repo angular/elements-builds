206748c5d1e11b763b1ecd7a2e2d53154b2588cb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-01-15 19:52:30 +0000

    b223f7e docs: release notes for the v19.0.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-ed705a8
+ * @license Angular v19.2.0-next.0+sha-b223f7e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-ed705a8');
+const VERSION = new Version('19.2.0-next.0+sha-b223f7e');
 
 /**
  * @module
