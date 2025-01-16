b1bc0d7820d56a13e5658f95037c9fd1a063facb
Andrew Kushnir <akushnir@google.com>
2025-01-16 17:28:53 +0000

    81f8fe6 refactor(core): port resource() to linkedSignal() (#59024)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.1+sha-d12a186
+ * @license Angular v19.1.1+sha-81f8fe6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.1+sha-d12a186');
+const VERSION = new Version('19.1.1+sha-81f8fe6');
 
 /**
  * @module
