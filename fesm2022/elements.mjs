cbab7f0c1714c8a740ab6d9ca78801229a4f82ae
Andrew Kushnir <akushnir@google.com>
2025-01-16 19:42:13 +0000

    b76be83 fix(core): animation sometimes renderer not being destroyed during HMR (#59574)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-884e0b4
+ * @license Angular v19.2.0-next.0+sha-b76be83
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-884e0b4');
+const VERSION = new Version('19.2.0-next.0+sha-b76be83');
 
 /**
  * @module
