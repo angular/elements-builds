c710590ceabb02260c7693610f13b6c01f02b8f1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:41:59 +0000

    357795c fix(core): run HMR replacement in the zone (#59562)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0+sha-eb0b185
+ * @license Angular v19.1.0+sha-357795c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0+sha-eb0b185');
+const VERSION = new Version('19.1.0+sha-357795c');
 
 /**
  * @module
