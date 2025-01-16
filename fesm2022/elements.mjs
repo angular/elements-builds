243f113b60d9f7fc521960adba4cddf3ddb8351a
Andrew Kushnir <akushnir@google.com>
2025-01-16 00:52:49 +0000

    c9eaf53 release: bump Angular DevTools version to 1.0.21 (#59548)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-d0ad030
+ * @license Angular v19.2.0-next.0+sha-c9eaf53
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-d0ad030');
+const VERSION = new Version('19.2.0-next.0+sha-c9eaf53');
 
 /**
  * @module
