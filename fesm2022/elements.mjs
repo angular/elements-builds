a7b055b25bb9c85586e1c3d464950b3b8a3ef9f9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:25:05 +0000

    326248c test(devtools): disable flaky highlighter test (#59561)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0+sha-74daef1
+ * @license Angular v19.1.0+sha-326248c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0+sha-74daef1');
+const VERSION = new Version('19.1.0+sha-326248c');
 
 /**
  * @module
