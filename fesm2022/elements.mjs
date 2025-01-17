10522e94849d43df692a0e6969269653e28f604b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-17 09:50:48 +0000

    4e6017a test(platform-server): fix flaky incremental hydration test (#59584)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-8ea691a
+ * @license Angular v19.2.0-next.0+sha-4e6017a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-8ea691a');
+const VERSION = new Version('19.2.0-next.0+sha-4e6017a');
 
 /**
  * @module
