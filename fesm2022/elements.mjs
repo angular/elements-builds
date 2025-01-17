4f8c64012f5367b530b0d4c0f57c79e9754fe3fd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-17 09:35:46 +0000

    55837de test(platform-server): fix flaky hydration test (#59579)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.1+sha-4eb5418
+ * @license Angular v19.1.1+sha-55837de
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.1+sha-4eb5418');
+const VERSION = new Version('19.1.1+sha-55837de');
 
 /**
  * @module
