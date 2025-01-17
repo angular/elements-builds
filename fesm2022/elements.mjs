fa0ca46eaf8e7e44854a9a2b168c6cfbdbc91bea
Andrew Kushnir <akushnir@google.com>
2025-01-17 21:26:47 +0000

    16b0885 docs(router): Fix small grammatical errors (#59568)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-14791f3
+ * @license Angular v19.2.0-next.0+sha-16b0885
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-14791f3');
+const VERSION = new Version('19.2.0-next.0+sha-16b0885');
 
 /**
  * @module
