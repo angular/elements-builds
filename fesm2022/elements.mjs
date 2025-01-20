1b7c4723e67a6446411d29c224fcd6466aa437b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 08:16:32 +0000

    30c4404 fix(compiler): update `@ng/component` URL to be relative (#59620)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-f9b13e4
+ * @license Angular v19.2.0-next.0+sha-30c4404
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-f9b13e4');
+const VERSION = new Version('19.2.0-next.0+sha-30c4404');
 
 /**
  * @module
