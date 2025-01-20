ecf0137942b50116d321d4828d5cc46a2010c8ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 08:05:44 +0000

    95a05bb fix(compiler-cli): disable tree shaking during HMR (#59595)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.1+sha-70c4b83
+ * @license Angular v19.1.1+sha-95a05bb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.1+sha-70c4b83');
+const VERSION = new Version('19.1.1+sha-95a05bb');
 
 /**
  * @module
