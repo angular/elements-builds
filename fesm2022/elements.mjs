8a5d8581b13bf7e073cd66d45ac3830ce7da0491
Andrew Kushnir <akushnir@google.com>
2025-01-21 18:03:37 +0000

    26f6d4c fix(core): skip component ID collision warning during SSR (#59625)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.2+sha-53160e5
+ * @license Angular v19.1.2+sha-26f6d4c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.2+sha-53160e5');
+const VERSION = new Version('19.1.2+sha-26f6d4c');
 
 /**
  * @module
