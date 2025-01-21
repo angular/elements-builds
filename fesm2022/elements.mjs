e39cba7941e7777aa4fdf336a7003394ce5bc5b6
Andrew Kushnir <akushnir@google.com>
2025-01-21 18:55:04 +0000

    0de5049 refactor(core): move TNode manipulation logic to its own file (#59601)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.2+sha-26f6d4c
+ * @license Angular v19.1.2+sha-0de5049
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.2+sha-26f6d4c');
+const VERSION = new Version('19.1.2+sha-0de5049');
 
 /**
  * @module
