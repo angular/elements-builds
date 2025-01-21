752c9bf6770d4d037ca7ceeac5f1b93fb0f3427d
Andrew Kushnir <akushnir@google.com>
2025-01-21 16:04:25 +0000

    6483621 refactor(core): move linkedSignal implementation to primitives (#59501)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.2+sha-2ec826d
+ * @license Angular v19.1.2+sha-6483621
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.2+sha-2ec826d');
+const VERSION = new Version('19.1.2+sha-6483621');
 
 /**
  * @module
