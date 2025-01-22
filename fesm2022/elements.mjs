538bc0fa809e330f04d7bc77c054e2acbae37524
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-01-22 19:49:21 +0000

    ec8c288 docs: release notes for the v19.1.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-61302cb
+ * @license Angular v19.2.0-next.0+sha-ec8c288
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-61302cb');
+const VERSION = new Version('19.2.0-next.0+sha-ec8c288');
 
 /**
  * @module
