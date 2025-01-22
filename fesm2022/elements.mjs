3a45581df5e8bbf3c12a010b880dade77d5f897d
Andrew Kushnir <akushnir@google.com>
2025-01-22 21:40:25 +0000

    1de822c refactor(devtools): select the root element by default (#59626)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-c243670
+ * @license Angular v19.1.3+sha-1de822c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-c243670');
+const VERSION = new Version('19.1.3+sha-1de822c');
 
 /**
  * @module
