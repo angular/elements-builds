f451a807ad81cb80cb2935fde7a222b3fbe46b86
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 10:56:49 +0000

    f8b8a3e refactor(forms): wrap `_checkParentType` with `ngDevMode` (#59489)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-4574ece
+ * @license Angular v19.1.3+sha-f8b8a3e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-4574ece');
+const VERSION = new Version('19.1.3+sha-f8b8a3e');
 
 /**
  * @module
