157781f03b499f0f3960aa0c8b1c317ff4e6d980
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 11:49:42 +0000

    b7b8da5 refactor(common): simplify null/undefined check in `keyvalue` pipe (#59696)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-f8b8a3e
+ * @license Angular v19.1.3+sha-b7b8da5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-f8b8a3e');
+const VERSION = new Version('19.1.3+sha-b7b8da5');
 
 /**
  * @module
