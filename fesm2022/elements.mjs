2704b4d8be2c98379a93e338ec5f8bfca921b6de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 16:29:05 +0000

    2c770a2 refactor(common): drop platform check in `PreconnectLinkChecker` (#59714)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-e7d0ca1
+ * @license Angular v19.1.3+sha-2c770a2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-e7d0ca1');
+const VERSION = new Version('19.1.3+sha-2c770a2');
 
 /**
  * @module
