6e599356ae4498a883c9a52f2ba57c2a327c98f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:34:11 +0000

    fd7ee47 fix(platform-browser): Update pseudoevent created by createMouseSpecialEvent to populate `_originalEvent` property (#59690)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-6d6708c
+ * @license Angular v19.2.0-next.0+sha-fd7ee47
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-6d6708c');
+const VERSION = new Version('19.2.0-next.0+sha-fd7ee47');
 
 /**
  * @module
