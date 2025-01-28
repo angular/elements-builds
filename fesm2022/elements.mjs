b71927f91a770b1610863ae07042cc6f2cf6791d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 11:47:52 +0000

    3554dbc build: revert back to @bazel/ibazel v0.16 (#59748)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-c202682
+ * @license Angular v19.2.0-next.0+sha-3554dbc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-c202682');
+const VERSION = new Version('19.2.0-next.0+sha-3554dbc');
 
 /**
  * @module
