b0a0d0307b304e56ed3592c313f778b0e21729f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 19:40:30 +0000

    68dbaf5 docs(docs-infra): remove non-necessary files from playground zip. (#59728)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-1a06eb2
+ * @license Angular v19.1.3+sha-68dbaf5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-1a06eb2');
+const VERSION = new Version('19.1.3+sha-68dbaf5');
 
 /**
  * @module
