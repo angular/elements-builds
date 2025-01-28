916cc683571625f65b5e571a1a0780010118ccb7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 18:23:14 +0000

    1a06eb2 ci: prevent renovate from updating @bazel/ibazel (#59761)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-522acbf
+ * @license Angular v19.1.3+sha-1a06eb2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-522acbf');
+const VERSION = new Version('19.1.3+sha-1a06eb2');
 
 /**
  * @module
