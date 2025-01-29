389c5e3f6a5ed8b469b34f00350542dfa5c43b4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 23:17:08 +0000

    eb9dca3 docs: release notes for the v19.1.4 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-b5f9018
+ * @license Angular v19.2.0-next.0+sha-eb9dca3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-b5f9018');
+const VERSION = new Version('19.2.0-next.0+sha-eb9dca3');
 
 /**
  * @module
