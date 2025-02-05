dab0f7d7c24aad1d3d9681f222e4daa11a06a6e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-05 17:08:34 +0000

    dbbd0d3 docs: Add v18 Angular version link to versions configuration (#58933)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.4+sha-2115cca
+ * @license Angular v19.1.4+sha-dbbd0d3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.4+sha-2115cca');
+const VERSION = new Version('19.1.4+sha-dbbd0d3');
 
 /**
  * @module
