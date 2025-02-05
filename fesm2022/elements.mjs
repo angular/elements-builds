66182def67b87f8cb6d7cfff04bedd5438429e8a
Andrew Scott <atscott@google.com>
2025-02-05 20:24:57 +0000

    87588ec build: remove self from unavailable list (#59858)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.4+sha-dbbd0d3
+ * @license Angular v19.1.4+sha-87588ec
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.4+sha-dbbd0d3');
+const VERSION = new Version('19.1.4+sha-87588ec');
 
 /**
  * @module
