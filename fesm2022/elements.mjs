5450ed96a190f87b5a5273e2f456e1d35b75c4d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-06 20:39:17 +0000

    27c39f5 release: cut the v19.1.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.4+sha-6217e45
+ * @license Angular v19.1.5+sha-27c39f5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.4+sha-6217e45');
+const VERSION = new Version('19.1.5+sha-27c39f5');
 
 /**
  * @module
