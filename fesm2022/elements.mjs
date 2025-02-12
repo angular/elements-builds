9d3fae4128606d3f6ad5e0ea22d6a92d8df2712a
Andrew Scott <atscott@google.com>
2025-02-12 17:12:02 +0000

    cab7a9b fix(core): invalidate HMR component if replacement throws an error (#59854)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.5+sha-27c39f5
+ * @license Angular v19.1.5+sha-cab7a9b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.5+sha-27c39f5');
+const VERSION = new Version('19.1.5+sha-cab7a9b');
 
 /**
  * @module
