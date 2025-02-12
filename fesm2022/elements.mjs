7771f43e384319fc4f93f1914f1423d58ac6c544
Andrew Scott <atscott@google.com>
2025-02-12 17:53:50 +0000

    b41a263 build: update io_bazel_rules_sass digest to d829b6a (#59862)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.5+sha-f9e5900
+ * @license Angular v19.1.5+sha-b41a263
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.5+sha-f9e5900');
+const VERSION = new Version('19.1.5+sha-b41a263');
 
 /**
  * @module
