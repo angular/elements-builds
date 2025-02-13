fa2e3971ab171f0fdbb7d6e40d4b0512f1a6315b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 20:25:07 +0000

    3fd69d7 refactor(core): Check in some interfaces for the DI package (#59921)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.6+sha-defe672
+ * @license Angular v19.1.6+sha-3fd69d7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.6+sha-defe672');
+const VERSION = new Version('19.1.6+sha-3fd69d7');
 
 /**
  * @module
