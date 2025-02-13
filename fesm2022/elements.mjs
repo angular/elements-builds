27ea0a1ec872b46d6447ae9b796af599d5c6f2e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 14:56:10 +0000

    22c2dfa release: bump Angular DevTools version to 1.0.22 (#59791)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.6+sha-20df2cd
+ * @license Angular v19.1.6+sha-22c2dfa
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.6+sha-20df2cd');
+const VERSION = new Version('19.1.6+sha-22c2dfa');
 
 /**
  * @module
