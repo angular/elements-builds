3aea87ab0f3b205f3c966be31c2219237c913666
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 20:58:52 +0000

    4ec0e66 build: update actions/cache digest to 0c907a7 (#60002)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.6+sha-ec1e4c3
+ * @license Angular v19.1.6+sha-4ec0e66
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.6+sha-ec1e4c3');
+const VERSION = new Version('19.1.6+sha-4ec0e66');
 
 /**
  * @module
