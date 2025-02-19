cb8dd96d015716fa16a28cac9be82ee2fbd5cdc1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-19 17:07:26 +0000

    164facb docs(docs-infra): disable CodeEditor tests (#60015)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.6+sha-e16394a
+ * @license Angular v19.1.6+sha-164facb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.6+sha-e16394a');
+const VERSION = new Version('19.1.6+sha-164facb');
 
 /**
  * @module
