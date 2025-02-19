7cf53cc361faf3e10587284f8d5c7a0cb639da0a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-19 18:48:49 +0000

    5a66f60 release: cut the v19.1.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.6+sha-164facb
+ * @license Angular v19.1.7+sha-5a66f60
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.6+sha-164facb');
+const VERSION = new Version('19.1.7+sha-5a66f60');
 
 /**
  * @module
