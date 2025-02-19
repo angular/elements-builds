2c8dc2d80682cb8edcdacafb0231bd19dfd00019
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-19 20:17:02 +0000

    c2102c5 refactor(core): Add fake navigation to primitives for code sharing (#59857)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-5a66f60
+ * @license Angular v19.1.7+sha-c2102c5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-5a66f60');
+const VERSION = new Version('19.1.7+sha-c2102c5');
 
 /**
  * @module
