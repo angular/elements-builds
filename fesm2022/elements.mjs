dddf244523354a4530a9b3b8fcdf2371815f2f8a
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:09:49 +0000

    da25071 docs: Fix typo ')' in banner-initial.component.spec.ts (#60032)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-c2102c5
+ * @license Angular v19.1.7+sha-da25071
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-c2102c5');
+const VERSION = new Version('19.1.7+sha-da25071');
 
 /**
  * @module
