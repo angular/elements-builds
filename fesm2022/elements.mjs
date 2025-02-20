595d026ed9e6758903f438d0704e3d4821704956
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:02:33 +0000

    1dc5c39 refactor(devtools): fix component inspector highlighting (#59995)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-f0990c6
+ * @license Angular v19.1.7+sha-1dc5c39
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-f0990c6');
+const VERSION = new Version('19.1.7+sha-1dc5c39');
 
 /**
  * @module
