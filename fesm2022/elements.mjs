a3a1bc521ff233dee60eaae587dfc411c333f3a7
kirjs <kirjs@users.noreply.github.com>
2025-02-21 19:13:00 +0000

    628ab40 refactor(core): do not store input/output public names (#60036)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.3.0-next.0+sha-aa58339
+ * @license Angular v19.3.0-next.0+sha-628ab40
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.3.0-next.0+sha-aa58339');
+const VERSION = new Version('19.3.0-next.0+sha-628ab40');
 
 /**
  * @module
