7b49e2f84fee9e2c3478ebaf4a0c1be255dd0753
kirjs <kirjs@users.noreply.github.com>
2025-02-25 17:40:44 +0000

    7c12cb1 feat(router): Allow resolvers to read resolved data from ancestors (#59860)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-491b0a4
+ * @license Angular v20.0.0-next.0+sha-7c12cb1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-491b0a4');
+const VERSION = new Version('20.0.0-next.0+sha-7c12cb1');
 
 /**
  * @module
