bf48c18f1369d6b3e1ee5eba0c72433de085c450
kirjs <kirjs@users.noreply.github.com>
2025-02-25 18:07:26 +0000

    1fbaeab fix(common): make types for HttpClient more readable (#59901)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-69b6d65
+ * @license Angular v19.1.7+sha-1fbaeab
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-69b6d65');
+const VERSION = new Version('19.1.7+sha-1fbaeab');
 
 /**
  * @module
