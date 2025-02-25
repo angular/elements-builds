dc86b57438c2f588cf50a850c69c41c375f56610
kirjs <kirjs@users.noreply.github.com>
2025-02-25 17:26:13 +0000

    491b0a4 fix(core): Remove duplicate reporting of errors in `CDR.detectChanges` (#60056)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-af2e79e
+ * @license Angular v20.0.0-next.0+sha-491b0a4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-af2e79e');
+const VERSION = new Version('20.0.0-next.0+sha-491b0a4');
 
 /**
  * @module
