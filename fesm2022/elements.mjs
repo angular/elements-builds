0c4a27d2fa740ca9446385c9b344b31549792f65
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:08:37 +0000

    4f38429 docs: move release date for v20 by one week (#60111)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-33769c2
+ * @license Angular v19.1.7+sha-4f38429
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-33769c2');
+const VERSION = new Version('19.1.7+sha-4f38429');
 
 /**
  * @module
