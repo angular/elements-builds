e1ae46b1a4073216f0ff2c1c460ad2d8f390888f
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:24:26 +0000

    567d48f refactor(core): tree-shake `REF_EXTRACTOR_REGEXP` (#60081)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-adf4987
+ * @license Angular v19.1.7+sha-567d48f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-adf4987');
+const VERSION = new Version('19.1.7+sha-567d48f');
 
 /**
  * @module
