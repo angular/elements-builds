0e1434f35b028b4c49e3873ecf11fb6e3a87fefe
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:26:18 +0000

    9ed4b54 feat(devtools): add the onpush label for marked onpush components in the components tree view (#60059)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-11ab5db
+ * @license Angular v20.0.0-next.0+sha-9ed4b54
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-11ab5db');
+const VERSION = new Version('20.0.0-next.0+sha-9ed4b54');
 
 /**
  * @module
