99ae0203cf9f0c1bd36c825138056b0504d05cf3
kirjs <kirjs@users.noreply.github.com>
2025-02-26 21:25:09 +0000

    e012cf0 build: add ignore revs file for git blame (#60117)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-8e9463b
+ * @license Angular v19.2.0+sha-e012cf0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0+sha-8e9463b');
+const VERSION = new Version('19.2.0+sha-e012cf0');
 
 /**
  * @module
