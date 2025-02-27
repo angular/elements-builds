8e695db324e71c56b57bd2f224fffb376c44b77a
Miles Malerba <mmalerba@google.com>
2025-02-27 17:59:20 +0000

    e549bde refactor(animations): tree-shake regular expressions (#60095)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-16ca676
+ * @license Angular v19.2.0+sha-e549bde
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0+sha-16ca676');
+const VERSION = new Version('19.2.0+sha-e549bde');
 
 /**
  * @module
