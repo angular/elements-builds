db7a75e745505595eb9aa1e3534de5fab4dfca13
Miles Malerba <mmalerba@google.com>
2025-02-27 00:32:52 +0000

    4078895 docs: fix mermaid event node text visibility in light mode (#60124) PR Close #60124

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-ab2ee22
+ * @license Angular v20.0.0-next.0+sha-4078895
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-ab2ee22');
+const VERSION = new Version('20.0.0-next.0+sha-4078895');
 
 /**
  * @module
