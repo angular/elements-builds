5c7e2cd0bd29c31c04839a4da6d497fcbb80457c
Miles Malerba <mmalerba@google.com>
2025-02-27 18:31:18 +0000

    b09f4a5 docs(docs-infra): add a tooltip to show the full text when nav item text is lengthy (#59038)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-e549bde
+ * @license Angular v19.2.0+sha-b09f4a5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0+sha-e549bde');
+const VERSION = new Version('19.2.0+sha-b09f4a5');
 
 /**
  * @module
