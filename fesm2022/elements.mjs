04fe78973dd768cccaffa9ce0000ed64e418dacc
Miles Malerba <mmalerba@google.com>
2025-02-28 18:53:58 +0000

    78b27a8 refactor(core): Move getCurrentInjector/setCurrentInjector to primitives package. (#60090)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-af02914
+ * @license Angular v20.0.0-next.0+sha-78b27a8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-af02914');
+const VERSION = new Version('20.0.0-next.0+sha-78b27a8');
 
 /**
  * @module
