e2ae9bf6120fe963cbdb0441b34d58fa121bfa1f
Miles Malerba <mmalerba@google.com>
2025-03-03 17:20:36 +0000

    36b60a9 fix(common): clean up `onUrlChange` listener when root scope is destroyed (#60004)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-51b8ff2
+ * @license Angular v20.0.0-next.0+sha-36b60a9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-51b8ff2');
+const VERSION = new Version('20.0.0-next.0+sha-36b60a9');
 
 /**
  * @module
