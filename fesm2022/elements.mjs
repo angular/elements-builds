aa3349f6ab2b6f8989089e3a58beb3cf4efa10ae
Miles Malerba <mmalerba@google.com>
2025-03-04 20:13:28 +0000

    31bbbe9 docs(docs-infra): Throw error at build time for invalid links (#59162)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-26b3a8f
+ * @license Angular v19.2.0+sha-31bbbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0+sha-26b3a8f');
+const VERSION = new Version('19.2.0+sha-31bbbe9');
 
 /**
  * @module
