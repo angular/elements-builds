369d31f92139fc53340beae925a03460713a47a0
Miles Malerba <mmalerba@google.com>
2025-03-04 20:03:37 +0000

    bb14fe8 refactor(http): add `context` property to `httpResource` (#60188)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-6f6d9f1
+ * @license Angular v20.0.0-next.0+sha-bb14fe8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-6f6d9f1');
+const VERSION = new Version('20.0.0-next.0+sha-bb14fe8');
 
 /**
  * @module
