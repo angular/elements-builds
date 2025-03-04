19f852317135085f72bfcc32f1afb42a087e3ada
Miles Malerba <mmalerba@google.com>
2025-03-04 17:19:58 +0000

    2e03a86 docs(docs-infra): Improve support for @link within the jsdoc. (#60201)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-a58b038
+ * @license Angular v20.0.0-next.0+sha-2e03a86
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-a58b038');
+const VERSION = new Version('20.0.0-next.0+sha-2e03a86');
 
 /**
  * @module
