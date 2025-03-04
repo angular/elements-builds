8714c3bb95828ddc0dba835754f667b6f8f9cf10
Miles Malerba <mmalerba@google.com>
2025-03-04 22:26:26 +0000

    30ede6b refactor(docs-infra): AppComponent cleanups (#58897)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-ff46280
+ * @license Angular v20.0.0-next.0+sha-30ede6b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-ff46280');
+const VERSION = new Version('20.0.0-next.0+sha-30ede6b');
 
 /**
  * @module
