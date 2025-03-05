cfcc6b76934f8ddfa18049cc23dc68cbacc81f73
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-05 10:10:30 +0000

    a2db87e docs(docs-infra): fix secondary menu (#60212)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-a64a575
+ * @license Angular v19.2.0+sha-a2db87e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0+sha-a64a575');
+const VERSION = new Version('19.2.0+sha-a2db87e');
 
 /**
  * @module
