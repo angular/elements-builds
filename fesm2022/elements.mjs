884aafe6cd6ab7ffafba81f081a461b5f0cd1ede
Andrew Kushnir <akushnir@google.com>
2025-03-05 23:39:38 +0000

    cae1fe5 fix(router): update symbols (#60233)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-a8ec7be
+ * @license Angular v20.0.0-next.1+sha-cae1fe5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-a8ec7be');
+const VERSION = new Version('20.0.0-next.1+sha-cae1fe5');
 
 /**
  * @module
