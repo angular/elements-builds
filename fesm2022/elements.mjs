26bc8e977f219d1b46be11d34f8eb9f634f93f88
Miles Malerba <mmalerba@google.com>
2025-03-05 19:19:48 +0000

    8eebec9 docs: release notes for the v19.2.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-9b0c9ff
+ * @license Angular v20.0.0-next.0+sha-8eebec9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-9b0c9ff');
+const VERSION = new Version('20.0.0-next.0+sha-8eebec9');
 
 /**
  * @module
