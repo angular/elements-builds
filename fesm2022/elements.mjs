e63d7e3fe31895b21887799688dd587874cc782d
Andrew Kushnir <akushnir@google.com>
2025-03-06 22:40:50 +0000

    aa1ab41 docs(docs-infra): enable & fix tests (#60220)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-978a0b8
+ * @license Angular v20.0.0-next.1+sha-aa1ab41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-978a0b8');
+const VERSION = new Version('20.0.0-next.1+sha-aa1ab41');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
