09bfae0e76148b86ae49a707f013c1c927a9e60d
Andrew Kushnir <akushnir@google.com>
2025-03-06 17:26:13 +0000

    81fe053 fix(forms): Make sure statusChanges is emitted (#57098)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-2668bd9
+ * @license Angular v20.0.0-next.1+sha-81fe053
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -426,7 +426,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-2668bd9');
+const VERSION = new Version('20.0.0-next.1+sha-81fe053');
 
 /**
  * @module
