e1f713191bcfe86a8f2d3cdb32e784b5757a690c
Andrew Kushnir <akushnir@google.com>
2025-03-07 19:08:28 +0000

    bde1b37 build: properly compile tests in `core` with Angular compiler (#60268)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-70fb3bb
+ * @license Angular v19.2.1+sha-bde1b37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-70fb3bb');
+const VERSION = new Version('19.2.1+sha-bde1b37');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
