b867626daa583757ae9214a6c405b126e5083de9
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:52:54 +0000

    3089ab4 build: update minimum development Node.js versions (#60080)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-07dc806
+ * @license Angular v20.0.0-next.1+sha-3089ab4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-07dc806');
+const VERSION = new Version('20.0.0-next.1+sha-3089ab4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
