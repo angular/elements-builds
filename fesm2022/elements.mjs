9e72b43e094cfc8bdb2d312651ef2ae2192bd9df
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:39:23 +0000

    07dc806 refactor(bazel): remove primary_bundle_name as attr for ng_package (#60306)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-338818c
+ * @license Angular v20.0.0-next.1+sha-07dc806
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-338818c');
+const VERSION = new Version('20.0.0-next.1+sha-07dc806');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
