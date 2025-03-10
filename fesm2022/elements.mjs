db40f2fa7d476f5c1b0354f2ad7fcaded331d916
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:29:19 +0000

    5e2f312 fix(docs-infra): terminal light mode text color (#60246)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-bde1b37
+ * @license Angular v19.2.1+sha-5e2f312
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-bde1b37');
+const VERSION = new Version('19.2.1+sha-5e2f312');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
