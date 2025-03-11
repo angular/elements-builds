ab6958ac4dc30e0217658cb4cbe87ef5e2dd989b
Andrew Kushnir <akushnir@google.com>
2025-03-11 23:27:11 +0000

    0a9403e docs(docs-infra): Bump tutorial/playground to 19.2 (#60336)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-b27b7ef
+ * @license Angular v20.0.0-next.1+sha-0a9403e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-b27b7ef');
+const VERSION = new Version('20.0.0-next.1+sha-0a9403e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
