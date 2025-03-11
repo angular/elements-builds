354ae643872133a9406ba132ae429670249e1c0c
Andrew Kushnir <akushnir@google.com>
2025-03-11 22:08:25 +0000

    56b551d fix(compiler): incorrect spans for template literals (#60323) (#60331)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-705fb0d
+ * @license Angular v19.2.1+sha-56b551d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -408,7 +408,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-705fb0d');
+const VERSION = new Version('19.2.1+sha-56b551d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
