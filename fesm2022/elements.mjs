154c243991a712ce17d3f0ff3531acdc440be2c8
Andrew Kushnir <akushnir@google.com>
2025-03-12 19:36:50 +0000

    f5f2549 refactor(http): replace overriden method with a ts `declare` (#60278)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-817021b
+ * @license Angular v20.0.0-next.2+sha-f5f2549
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.2+sha-817021b');
+const VERSION = new Version('20.0.0-next.2+sha-f5f2549');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
