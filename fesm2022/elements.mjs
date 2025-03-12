5c2ff5447350c5d3438f45c51ac599e3234447c0
Andrew Kushnir <akushnir@google.com>
2025-03-12 00:18:58 +0000

    05729f4 refactor(router): split remainder of describes in integration test file (#60313)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-41fd776
+ * @license Angular v19.2.1+sha-05729f4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-41fd776');
+const VERSION = new Version('19.2.1+sha-05729f4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
