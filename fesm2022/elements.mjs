929377371bdaf07f4d9fb3920289f5513a779b1c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-03-12 16:51:07 +0000

    0788b0c docs: release notes for the v19.2.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-0b6084e
+ * @license Angular v20.0.0-next.1+sha-0788b0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-0b6084e');
+const VERSION = new Version('20.0.0-next.1+sha-0788b0c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
