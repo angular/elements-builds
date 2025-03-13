238da609120989b6b2b0736634829f5f0d6033c8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:43:07 +0000

    9e1a103 fix(upgrade): handle output emitters when downgrading a component (#60369)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-3856629
+ * @license Angular v19.2.2+sha-9e1a103
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-3856629');
+const VERSION = new Version('19.2.2+sha-9e1a103');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
