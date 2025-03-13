906f76721903822ef4171c0193d73f0cb7595e4c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:10:27 +0000

    55ba1ed docs: added short explanation about event reply (#60349)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-7a39585
+ * @license Angular v20.0.0-next.2+sha-55ba1ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.2+sha-7a39585');
+const VERSION = new Version('20.0.0-next.2+sha-55ba1ed');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
