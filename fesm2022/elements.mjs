b210ee8ae4f69571949175546c3e07074497bcbb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 14:56:35 +0000

    8b888b2 docs: note that CSS is also taken into account in the max bundle size (#60362)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-4a30a4e
+ * @license Angular v19.2.2+sha-8b888b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-4a30a4e');
+const VERSION = new Version('19.2.2+sha-8b888b2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
