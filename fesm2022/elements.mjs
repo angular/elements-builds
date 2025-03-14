522f33c8b1d1aefa933550419f0e7f4da248cc66
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 18:57:46 +0000

    9b8781a build: upload grpc remote logs for debugging (#60393)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-296aded
+ * @license Angular v19.2.2+sha-9b8781a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-296aded');
+const VERSION = new Version('19.2.2+sha-9b8781a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
