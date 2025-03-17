08c07707131d10f8677bdb59b584fe009ead2a7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-17 17:40:51 +0000

    450c1fc docs: revise and consolidate server rendering guides (#60383)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-b154fb3
+ * @license Angular v20.0.0-next.2+sha-450c1fc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.2+sha-b154fb3');
+const VERSION = new Version('20.0.0-next.2+sha-450c1fc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
