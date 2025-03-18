0afbef5e6f1b64da7e1ee9fc185d249562cabb49
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-18 07:16:33 +0000

    2364be9 build: improve partial compliance golden generation and add debug logging (#60427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-0615ffb
+ * @license Angular v19.2.2+sha-2364be9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-0615ffb');
+const VERSION = new Version('19.2.2+sha-2364be9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
