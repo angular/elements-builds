2e4bb3e1675f14149fa8d76a643c51e1904c699e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:37:56 +0000

    bf5d995 refactor(platform-server): expose internal `render` method for extensibility (#60416)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-4930ed3
+ * @license Angular v20.0.0-next.2+sha-bf5d995
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.2+sha-4930ed3');
+const VERSION = new Version('20.0.0-next.2+sha-bf5d995');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
