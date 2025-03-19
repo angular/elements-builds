b87adfb83d7a197b23d81ae87c5e7c059a812731
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 19:33:19 +0000

    2be0089 release: cut the v19.2.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-2466fc9
+ * @license Angular v19.2.3+sha-2be0089
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-2466fc9');
+const VERSION = new Version('19.2.3+sha-2be0089');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
