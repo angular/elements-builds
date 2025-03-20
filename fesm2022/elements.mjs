0ce2e7b58cf7a37e88eaca82d13e2241d592c645
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-20 07:46:28 +0000

    9426e83 build: disable RBE for compliance partial goldens (#60473)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-6df6aa1
+ * @license Angular v19.2.3+sha-9426e83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-6df6aa1');
+const VERSION = new Version('19.2.3+sha-9426e83');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
