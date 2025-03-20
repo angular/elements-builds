4af9d066c1c78514e853ecdc440412407fa925a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:40:39 +0000

    8e8ccc7 fix(platform-browser): ensure `platformBrowserTesting` includes `platformBrowser` providers (#60480)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-c13c53f
+ * @license Angular v19.2.3+sha-8e8ccc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-c13c53f');
+const VERSION = new Version('19.2.3+sha-8e8ccc7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
