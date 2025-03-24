dc2d4540a04b96c04c86e569227581ab2bb820c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 16:14:23 +0000

    00a9331 docs: fix templates bindings link (#60521) PR Close #60521

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-9ac4057
+ * @license Angular v19.2.3+sha-00a9331
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-9ac4057');
+const VERSION = new Version('19.2.3+sha-00a9331');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
