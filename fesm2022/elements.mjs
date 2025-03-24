a30332587566bfcfc520235bfc9a659f7e821221
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 14:13:53 +0000

    9ac4057 refactor(core): specify reactive node kind for linked signal (#60451)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-d6a1a3a
+ * @license Angular v19.2.3+sha-9ac4057
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-d6a1a3a');
+const VERSION = new Version('19.2.3+sha-9ac4057');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
