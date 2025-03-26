21488e56e194ee3d610c4cc84727c93a7a077f46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-26 14:11:27 +0000

    7a65b06 refactor(core): updates to golden files (#60227) (#60556)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-00a9331
+ * @license Angular v19.2.3+sha-7a65b06
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-00a9331');
+const VERSION = new Version('19.2.3+sha-7a65b06');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
