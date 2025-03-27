5c29e73bfbc59461e394873b6f12975619c46e80
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-27 20:35:28 +0000

    f4c4b10 fix(compiler-cli): Produce fatal diagnostic on duplicate decorated properties (#60376)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-b53220a
+ * @license Angular v19.2.4+sha-f4c4b10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-b53220a');
+const VERSION = new Version('19.2.4+sha-f4c4b10');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
