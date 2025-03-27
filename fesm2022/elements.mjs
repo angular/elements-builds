32481f2fd5d3a0594c41127ae0005c4de1930c38
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-27 19:50:03 +0000

    76c60a6 refactor(core): move signal toString to primitives (#60365)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-f7385b4
+ * @license Angular v20.0.0-next.4+sha-76c60a6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-f7385b4');
+const VERSION = new Version('20.0.0-next.4+sha-76c60a6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
