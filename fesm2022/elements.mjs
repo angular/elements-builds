3f8c2624c5a0dd7728a37469aacec662d9029c5f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 19:54:03 +0000

    43d03f8 ci: fix over extraction of init symbols in symbol tests (#60493) (#60614)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-882f96f
+ * @license Angular v19.2.4+sha-43d03f8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-882f96f');
+const VERSION = new Version('19.2.4+sha-43d03f8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
