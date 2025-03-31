26972a35f58f51fbfd29dd7dfe4a6a79fd83f54f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:52:50 +0000

    4387b37 refactor(router): drop special handling of the OutletInjector (#58351)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-3855730
+ * @license Angular v19.2.4+sha-4387b37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-3855730');
+const VERSION = new Version('19.2.4+sha-4387b37');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
