36a126ea4d5001b005ace62d7bb3305424b8301b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 16:45:17 +0000

    599c45c build: update lock files for integration test apps (#60594)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-52dfce6
+ * @license Angular v20.0.0-next.4+sha-599c45c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-52dfce6');
+const VERSION = new Version('20.0.0-next.4+sha-599c45c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
