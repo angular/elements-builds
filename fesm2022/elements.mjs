2448d9d49fe75e78d6932c932eb1d3291041a8db
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 13:22:12 +0000

    fdde642 docs: fix private modifier for the injected service (#60619)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-0252da9
+ * @license Angular v19.2.4+sha-fdde642
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-0252da9');
+const VERSION = new Version('19.2.4+sha-fdde642');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
