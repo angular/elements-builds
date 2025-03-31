42deee94206d8914f0c1b7b379f924ddd9fabcb9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:23:29 +0000

    4e478af docs(docs-infra): add search via queryparam all adev routes (#57567)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-302cb83
+ * @license Angular v20.0.0-next.4+sha-4e478af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-302cb83');
+const VERSION = new Version('20.0.0-next.4+sha-4e478af');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
