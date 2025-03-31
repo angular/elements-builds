9bef757c565d96b066830cee6ea20d95e7eea7a6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 13:23:19 +0000

    9c106f4 refactor(core): introduce domProperty instruction (#60608)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-cbd6ec8
+ * @license Angular v20.0.0-next.4+sha-9c106f4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-cbd6ec8');
+const VERSION = new Version('20.0.0-next.4+sha-9c106f4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
