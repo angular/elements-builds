8a445b5f6482e0667bb0212e156822fefad7f779
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:03:29 +0000

    086b4a9 refactor(devtools): fix firefox breadcrumbs (#60552)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-84f3ba0
+ * @license Angular v19.2.4+sha-086b4a9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-84f3ba0');
+const VERSION = new Version('19.2.4+sha-086b4a9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
