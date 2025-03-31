0dd6b8fc5482cedc568cc151648afee5d5a5551a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:16:45 +0000

    139fead fix(devtools): catch firefox non-top level frame error case when using utilities APIs (#60430)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-086b4a9
+ * @license Angular v19.2.4+sha-139fead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-086b4a9');
+const VERSION = new Version('19.2.4+sha-139fead');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
