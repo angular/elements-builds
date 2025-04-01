f3f1efb4746a31b00cbdccd860ceaa5644d90a55
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 22:29:25 +0000

    da9f509 build: exclude relative imports commits via .git-blame-ignore-revs (#60685)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-d0c979e
+ * @license Angular v19.2.4+sha-da9f509
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-d0c979e');
+const VERSION = new Version('19.2.4+sha-da9f509');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
