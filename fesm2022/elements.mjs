b227c9e69e3e3709082647bfa75720210c04eba4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 16:05:33 +0000

    eb97303 test(bazel): update `common` package contents test to skip chunks (#60623)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-5db7e4c
+ * @license Angular v20.0.0-next.4+sha-eb97303
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-5db7e4c');
+const VERSION = new Version('20.0.0-next.4+sha-eb97303');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
