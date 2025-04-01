ac5be6c4d885b8f7545b1ad7d617674731c56ffd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 18:21:52 +0000

    b55f4de ci: update to latest dev-infra action versions (#60675)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-b927ce8
+ * @license Angular v20.0.0-next.4+sha-b55f4de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-b927ce8');
+const VERSION = new Version('20.0.0-next.4+sha-b55f4de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
