631be95a9a0e53148fbccf2200c0684a7ccc3971
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:32:27 +0000

    8f68d1b fix(core): fix ng generate @angular/core:output-migration (#60626)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-0c5b697
+ * @license Angular v19.2.4+sha-8f68d1b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-0c5b697');
+const VERSION = new Version('19.2.4+sha-8f68d1b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
