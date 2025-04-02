126c523b639881323923239a164335e2b833d736
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:25:59 +0000

    4f86f3f docs(docs-infra): fix algolia icon in the search dialog on safari (#60702)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-5f7f046
+ * @license Angular v20.0.0-next.4+sha-4f86f3f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-5f7f046');
+const VERSION = new Version('20.0.0-next.4+sha-4f86f3f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
