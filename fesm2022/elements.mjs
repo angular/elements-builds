9f2ea8ff5e57f3c0b02bfe409c4061da2e7beb54
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:39:42 +0000

    84ca0ef refactor: added typing for Response (#60541)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-4f86f3f
+ * @license Angular v20.0.0-next.4+sha-84ca0ef
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-4f86f3f');
+const VERSION = new Version('20.0.0-next.4+sha-84ca0ef');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
