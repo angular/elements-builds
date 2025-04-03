eab635a7901e88c2adc0e3e842e134de693d86d5
Andrew Scott <atscott@google.com>
2025-04-03 17:42:20 +0000

    0e82d42 fix(language-service): Do not provide element completions in end tag (#60616)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-8d183ae
+ * @license Angular v19.2.5+sha-0e82d42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-8d183ae');
+const VERSION = new Version('19.2.5+sha-0e82d42');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
