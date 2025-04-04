4881dbbe5fcd593cfecc5d29fbd8b8e35911bae5
Andrew Scott <atscott@google.com>
2025-04-04 18:52:15 +0000

    9228a73 feat(docs-infra): Add Navigation API adapter to adev (#60722)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-92c4123
+ * @license Angular v20.0.0-next.5+sha-9228a73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-92c4123');
+const VERSION = new Version('20.0.0-next.5+sha-9228a73');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
