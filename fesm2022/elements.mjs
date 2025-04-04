427c69818c23d6941526d9282070e2a3c531c004
Andrew Scott <atscott@google.com>
2025-04-04 16:55:26 +0000

    e9a719e build: avoid export internal renames (#60739)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-1b36f6e
+ * @license Angular v19.2.5+sha-e9a719e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-1b36f6e');
+const VERSION = new Version('19.2.5+sha-e9a719e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
