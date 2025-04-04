c8a7c8a04b6d337577db3785e8146f74b8257b7c
Andrew Scott <atscott@google.com>
2025-04-04 00:03:46 +0000

    1b36f6e refactor(common): Add hashchange event for traversals (#60682)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-d7089d2
+ * @license Angular v19.2.5+sha-1b36f6e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-d7089d2');
+const VERSION = new Version('19.2.5+sha-1b36f6e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
