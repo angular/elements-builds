4d3d1e46165b7de0dc5df529a2079461472be344
Andrew Scott <atscott@google.com>
2025-04-07 18:38:07 +0000

    0ae1889 fix(core): run `ApplicationRef.prototype.bootstrap` in `NgZone` (#60720)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-5039ed0
+ * @license Angular v20.0.0-next.5+sha-0ae1889
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-5039ed0');
+const VERSION = new Version('20.0.0-next.5+sha-0ae1889');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
