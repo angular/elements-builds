297f6fb775d23821410ea006d6aa4be7a4195cd5
Andrew Scott <atscott@google.com>
2025-04-07 17:37:08 +0000

    5039ed0 build: update cross-repo angular dependencies (#60747)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-d8e941b
+ * @license Angular v20.0.0-next.5+sha-5039ed0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-d8e941b');
+const VERSION = new Version('20.0.0-next.5+sha-5039ed0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
