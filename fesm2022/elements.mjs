8cf9bd7c729dd8e40a91643243c92438735d6682
Andrew Scott <atscott@google.com>
2025-04-08 21:11:10 +0000

    b8d9f95 Revert "refactor(core): allow multiple DI profilers (#60562)" (#60793)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-0506a0e
+ * @license Angular v20.0.0-next.5+sha-b8d9f95
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-0506a0e');
+const VERSION = new Version('20.0.0-next.5+sha-b8d9f95');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
