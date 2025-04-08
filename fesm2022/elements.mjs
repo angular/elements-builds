f023ea6e7b07cb83a4348823a9e3b7035fd2bce8
Andrew Scott <atscott@google.com>
2025-04-08 23:19:04 +0000

    9241615 fix(core): reduce total memory usage of various migration schematics (#60776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-dc193a7
+ * @license Angular v19.2.5+sha-9241615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-dc193a7');
+const VERSION = new Version('19.2.5+sha-9241615');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
