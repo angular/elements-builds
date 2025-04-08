1e4e09071ee1518dc650baa82b7153c4557c49b3
Andrew Scott <atscott@google.com>
2025-04-08 17:25:40 +0000

    56db79a refactor(core): use stream in rxResource instead of loader (#59910)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-8b9df44
+ * @license Angular v19.2.5+sha-56db79a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-8b9df44');
+const VERSION = new Version('19.2.5+sha-56db79a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
