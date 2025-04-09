dc2a1e3baf5e183fa371ed8ea3bb2c3b7870196b
Andrew Scott <atscott01@gmail.com>
2025-04-09 20:08:12 +0000

    ea58402 release: cut the v20.0.0-next.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-c3113b9
+ * @license Angular v20.0.0-next.6+sha-ea58402
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-c3113b9');
+const VERSION = new Version('20.0.0-next.6+sha-ea58402');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
