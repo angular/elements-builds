600bc1782aa5e73492f5b54ddd4a3762a3de0944
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:31:57 +0000

    54f9269 release: cut the v19.2.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-fc946c5
+ * @license Angular v19.2.6+sha-54f9269
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-fc946c5');
+const VERSION = new Version('19.2.6+sha-54f9269');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
