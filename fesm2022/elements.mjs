f16cbcfe29c9a638b2b0fee6aa4412f215b4b074
Andrew Scott <atscott@google.com>
2025-04-09 21:31:17 +0000

    d39e09d fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-54f9269
+ * @license Angular v19.2.6+sha-d39e09d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-54f9269');
+const VERSION = new Version('19.2.6+sha-d39e09d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
