8cc783f20f3ac88f7a44e581ee1ad4231796131a
kirjs <kirjs@users.noreply.github.com>
2025-04-10 15:25:55 +0000

    77c6041 fix(docs-infra): update docs-alert and tests (#60829)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-d39e09d
+ * @license Angular v19.2.6+sha-77c6041
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-d39e09d');
+const VERSION = new Version('19.2.6+sha-77c6041');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
