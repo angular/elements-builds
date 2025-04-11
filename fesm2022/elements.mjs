2f6f997c259c3dd918f5f57392e95999dc698b11
kirjs <kirjs@users.noreply.github.com>
2025-04-11 18:59:09 +0000

    72dd133 refactor(router): Use `NavigationTrigger` more consistently and move events helper next to definitions (#60736)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-1522e6c
+ * @license Angular v20.0.0-next.6+sha-72dd133
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-1522e6c');
+const VERSION = new Version('20.0.0-next.6+sha-72dd133');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
