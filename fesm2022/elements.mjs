af82f1f6c12c25dafbab5fb53a9ca02a6a9f761f
kirjs <kirjs@users.noreply.github.com>
2025-04-15 22:46:46 +0000

    e90daf3 refactor(core): Update Transition finished promise to not throw unhandled rejection (#60816)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-388c749
+ * @license Angular v20.0.0-next.6+sha-e90daf3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-388c749');
+const VERSION = new Version('20.0.0-next.6+sha-e90daf3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
