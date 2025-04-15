4c6edc05f7ab3c5eed8d69faba171b8ba5146dc9
kirjs <kirjs@users.noreply.github.com>
2025-04-15 13:16:39 +0000

    9ebe59f build: update all non-major dependencies (#60869)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-c80f24d
+ * @license Angular v19.2.6+sha-9ebe59f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-c80f24d');
+const VERSION = new Version('19.2.6+sha-9ebe59f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
