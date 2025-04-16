937d19dd599a1ac409cd825e8c72b3c0727cbe63
Kirill Cherkashin <kirill@kcherkashin.org>
2025-04-16 20:47:27 +0000

    2c1f348 release: cut the v19.2.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-c90eea2
+ * @license Angular v19.2.7+sha-2c1f348
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-c90eea2');
+const VERSION = new Version('19.2.7+sha-2c1f348');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
