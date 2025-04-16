ce7b5a6c743dc35cf4dfa15c97d7dd5195e8ffbe
kirjs <kirjs@users.noreply.github.com>
2025-04-16 17:07:08 +0000

    5675223 feat(core): stabilize incremental hydration api (#60888)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-bf8492b
+ * @license Angular v20.0.0-next.6+sha-5675223
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-bf8492b');
+const VERSION = new Version('20.0.0-next.6+sha-5675223');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
