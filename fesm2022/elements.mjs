f97eeb180422e95880047eb0e3a50dad5e088fd5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 10:39:27 +0000

    2f3f8d6 refactor(core): promote `pendingUntilEvent` to developerPreview (#60904)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-f838726
+ * @license Angular v20.0.0-next.7+sha-2f3f8d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-f838726');
+const VERSION = new Version('20.0.0-next.7+sha-2f3f8d6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
