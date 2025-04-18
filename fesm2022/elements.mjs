7014dce8bf7e05cdbe42175238bb0a46a990a956
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 19:19:53 +0000

    037dede refactor(devtools): improve components tab side pane UI (#60901)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-3a6121f
+ * @license Angular v20.0.0-next.7+sha-037dede
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-3a6121f');
+const VERSION = new Version('20.0.0-next.7+sha-037dede');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
