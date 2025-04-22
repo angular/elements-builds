05d0d827a130d8a3cb456b247318f05bd7f5665f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 08:49:20 +0000

    0162ceb fix(core): inject migration should treat @Attribute as optional (#60916)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-037dede
+ * @license Angular v20.0.0-next.7+sha-0162ceb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-037dede');
+const VERSION = new Version('20.0.0-next.7+sha-0162ceb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
