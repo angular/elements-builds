a6fa2552a74d4cd4c55a09c305cddf79f1e85718
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 11:45:58 +0000

    62de7d9 feat(router): add asynchronous redirects (#60863)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-953c4b2
+ * @license Angular v20.0.0-next.7+sha-62de7d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-953c4b2');
+const VERSION = new Version('20.0.0-next.7+sha-62de7d9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
