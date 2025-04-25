b866e73f55c128b3c93f75c245b780e3fdd5043d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-25 10:59:18 +0000

    d5fd734 feat(core): introduce TestBed.tick() (#60993)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-ccc5cc0
+ * @license Angular v20.0.0-next.8+sha-d5fd734
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-ccc5cc0');
+const VERSION = new Version('20.0.0-next.8+sha-d5fd734');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
