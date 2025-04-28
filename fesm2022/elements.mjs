3c70bf494a4d9ee27e6608958e56034e6889c310
Miles Malerba <mmalerba@google.com>
2025-04-28 21:42:17 +0000

    2445946 refactor(core): Update FakeNavigation to extract some Angular quirks (#60961)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-7680a27
+ * @license Angular v20.0.0-next.8+sha-2445946
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-7680a27');
+const VERSION = new Version('20.0.0-next.8+sha-2445946');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
