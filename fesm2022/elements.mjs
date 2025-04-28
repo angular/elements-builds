2b21797a22e6827e57db886d77d7b5f302d3571c
Miles Malerba <mmalerba@google.com>
2025-04-28 17:22:24 +0000

    7680a27 refactor(core): Ignore errors related to updating exhaustive check value (#61027)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-d8204bf
+ * @license Angular v20.0.0-next.8+sha-7680a27
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-d8204bf');
+const VERSION = new Version('20.0.0-next.8+sha-7680a27');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
