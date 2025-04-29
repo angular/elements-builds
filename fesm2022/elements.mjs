0bb5ffc73fd959b3c2e9294655d0f2763d7f619e
Miles Malerba <mmalerba@google.com>
2025-04-29 19:23:37 +0000

    222ae18 build(devtools): cleanup dependencies (#60936)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-46c1ade
+ * @license Angular v19.2.8+sha-222ae18
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-46c1ade');
+const VERSION = new Version('19.2.8+sha-222ae18');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
