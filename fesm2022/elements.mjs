5c11659a4c06c2c8e67cb22d864ae261822bad17
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:50:42 +0000

    c6148bd refactor(service-worker): drop error messages in production (#59702)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-400dbc5
+ * @license Angular v19.2.9+sha-c6148bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-400dbc5');
+const VERSION = new Version('19.2.9+sha-c6148bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
