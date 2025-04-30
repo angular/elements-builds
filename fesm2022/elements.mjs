2948a5f57473f32306dcc1b553a7dbb260082731
Miles Malerba <mmalerba@google.com>
2025-04-30 05:20:08 +0000

    946b844 fix(core): async EventEmitter error should not prevent stability (#61028)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-222ae18
+ * @license Angular v19.2.8+sha-946b844
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-222ae18');
+const VERSION = new Version('19.2.8+sha-946b844');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
