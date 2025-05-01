5c031be81151d64968605bf02152028dadc98aa3
Andrew Kushnir <akushnir@google.com>
2025-05-01 15:52:49 +0000

    d2cb0b9 refactor(core): tree-shake `getDirectiveDef` error (#61075)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-33a336f
+ * @license Angular v20.1.0-next.0+sha-d2cb0b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-33a336f');
+const VERSION = new Version('20.1.0-next.0+sha-d2cb0b9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
