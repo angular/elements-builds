a1b6ef63cea7d1dfd16b5b010e20d1133fc7f06e
Andrew Kushnir <akushnir@google.com>
2025-05-02 16:20:44 +0000

    1e79d47 build: enable rules_js interop mode in ng-dev release (#61087)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5fe726d
+ * @license Angular v20.1.0-next.0+sha-1e79d47
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5fe726d');
+const VERSION = new Version('20.1.0-next.0+sha-1e79d47');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
