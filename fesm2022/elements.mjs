7bf6ecf34cb9abf38375724ee35183b9fbaf2457
Andrew Kushnir <akushnir@google.com>
2025-05-05 22:42:50 +0000

    3b3466a build: update rules_angular digest to 42d4791 (#61126)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-63d10dc
+ * @license Angular v20.0.0-next.9+sha-3b3466a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-63d10dc');
+const VERSION = new Version('20.0.0-next.9+sha-3b3466a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
