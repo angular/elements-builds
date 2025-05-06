1474b0a77c5798e1dc07a7a9ca4afc525a1b66f4
Andrew Kushnir <akushnir@google.com>
2025-05-06 21:18:37 +0000

    c081e04 docs: fix non-working link  (#61131)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-e9e1c43
+ * @license Angular v19.2.9+sha-c081e04
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-e9e1c43');
+const VERSION = new Version('19.2.9+sha-c081e04');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
