496173760ff58c01548698dbd8d7336be8af18b5
Andrew Kushnir <akushnir@google.com>
2025-05-06 20:42:08 +0000

    e9e1c43 build: update tslint configuration (#61146)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-93e2735
+ * @license Angular v19.2.9+sha-e9e1c43
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-93e2735');
+const VERSION = new Version('19.2.9+sha-e9e1c43');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
