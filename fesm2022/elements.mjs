a0e1c1a5e5010f650d0abb724d07d9ecc134dde9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:47:59 +0000

    2f7e4eb ci: remove redundant dash in workflow file (#61206)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-1dece6c
+ * @license Angular v20.0.0-rc.0+sha-2f7e4eb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-1dece6c');
+const VERSION = new Version('20.0.0-rc.0+sha-2f7e4eb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
