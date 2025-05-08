135f9a499daa6fbc86d47f6430ca227e0f11c4dd
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:44:19 +0000

    3b9440b ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-6255954
+ * @license Angular v19.2.10+sha-3b9440b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-6255954');
+const VERSION = new Version('19.2.10+sha-3b9440b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
