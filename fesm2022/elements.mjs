5146b22ac46a78ca3fb9561cfe06ce9a7e29539c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:51:03 +0000

    684af21 docs: swap the anchor to the correct one (#61200)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-655954a
+ * @license Angular v19.2.10+sha-684af21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-655954a');
+const VERSION = new Version('19.2.10+sha-684af21');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
