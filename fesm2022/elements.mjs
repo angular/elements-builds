1675aaa5e346964b16d1438e895ab9e3b4df4d8a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 18:40:51 +0000

    adffb2e ci: use ` --force-with-lease`  when pushing to remote branch (#61222)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-11727dd-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-adffb2e-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-11727dd-with-local-changes');
+const VERSION = new Version('20.1.0-next.0+sha-adffb2e-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
