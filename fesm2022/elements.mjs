50e3bcf72fca81d73509e43ec7f4166a480dc4dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:55:40 +0000

    1aeb71c docs: update Angular CDK apis [20.0.x] (#61203)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-5666d25
+ * @license Angular v20.0.0-rc.0+sha-1aeb71c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-5666d25');
+const VERSION = new Version('20.0.0-rc.0+sha-1aeb71c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
