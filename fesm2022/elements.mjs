e8bdc11fa5dc92f2abb023ea4144922176038a8a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:36:35 +0000

    e9c9cfd ci: add workflow to sync non-default branches (#61201)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-4292d8a
+ * @license Angular v19.2.10+sha-e9c9cfd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-4292d8a');
+const VERSION = new Version('19.2.10+sha-e9c9cfd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
