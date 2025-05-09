f25b9a58fa503a141a857f6d1d5767d282c1b0a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-09 17:30:52 +0000

    223ca9f docs(docs-infra): limit the flexible width of the main content for better UX (#58831)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-f0dd8f8
+ * @license Angular v19.2.10+sha-223ca9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-f0dd8f8');
+const VERSION = new Version('19.2.10+sha-223ca9f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
