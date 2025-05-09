d85f4c94aec7dceebafbde8a52eebac1fe48bd56
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:11:28 +0000

    69bfece build: migrate adev devtools package to use ts_project (#61219)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-83c98ad
+ * @license Angular v20.0.0-rc.0+sha-69bfece
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-83c98ad');
+const VERSION = new Version('20.0.0-rc.0+sha-69bfece');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
