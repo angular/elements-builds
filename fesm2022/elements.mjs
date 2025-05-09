9d9b084cee2b8e6a92b95c86731afaa174a5d513
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:38:13 +0000

    345d3d8 build: migrate adev shared-docs package to use ts_project (#61218)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-69bfece
+ * @license Angular v20.0.0-rc.0+sha-345d3d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-69bfece');
+const VERSION = new Version('20.0.0-rc.0+sha-345d3d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
