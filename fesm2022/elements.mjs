3f427da80b135f07c91daf26f5ba027c7afa33ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-09 14:32:15 +0000

    83c98ad ci: restore `.npmrc` before executing `postUpgradeTasks` (#61239)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-5cecf0d
+ * @license Angular v20.0.0-rc.0+sha-83c98ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-5cecf0d');
+const VERSION = new Version('20.0.0-rc.0+sha-83c98ad');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
