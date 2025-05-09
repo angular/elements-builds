f007196fd97a89194b44ebda15f6f3f6487edb2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-09 17:47:14 +0000

    3c9b8d9 refactor(docs-infra): Remove rethrowing error handler (#61243)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-12a5b7b
+ * @license Angular v20.1.0-next.0+sha-3c9b8d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-12a5b7b');
+const VERSION = new Version('20.1.0-next.0+sha-3c9b8d9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
