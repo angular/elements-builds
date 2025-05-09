e54c3faf262841b96a7e68ed434b53429ba96cb1
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:08:34 +0000

    6cd2df0 build: remove unused debugging leftover from RBE investigation (#61237)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-5f46be0
+ * @license Angular v19.2.10+sha-6cd2df0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-5f46be0');
+const VERSION = new Version('19.2.10+sha-6cd2df0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
