ccfa546dc1ebfe1d16ef972fdc8d581299e6261d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-12 22:41:14 +0000

    7be6e5d refactor(migrations): remove unused code (#61260)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-223ca9f');
+const VERSION = new Version('19.2.10+sha-7be6e5d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
