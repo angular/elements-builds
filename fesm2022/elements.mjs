9c74f0090f0f63299e0429c3da06ba4fb28d1a05
Joey Perrott <josephperrott@gmail.com>
2025-05-13 22:54:31 +0000

    1b4b44e refactor: add explicit types for exports relying on inferred call return type (#61312)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-c1b9d3e
+ * @license Angular v20.0.0-rc.0+sha-1b4b44e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-c1b9d3e');
+const VERSION = new Version('20.0.0-rc.0+sha-1b4b44e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
