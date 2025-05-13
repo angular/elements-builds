dfd148f19a97eeae52e92b996d6844dc1d66e596
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 23:42:26 +0000

    41db650 docs: update references to the cdk/testing apis (#60853)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-1b4b44e
+ * @license Angular v20.0.0-rc.0+sha-41db650
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-1b4b44e');
+const VERSION = new Version('20.0.0-rc.0+sha-41db650');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
