498dc3323bb9908904a50184a4a2199c746d99ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 22:34:05 +0000

    c1b9d3e refactor(core): remove `USE_RUNTIME_DEPS_TRACKER_FOR_JIT` flag. (#61265)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-ed9a1d4
+ * @license Angular v20.0.0-rc.0+sha-c1b9d3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-ed9a1d4');
+const VERSION = new Version('20.0.0-rc.0+sha-c1b9d3e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
