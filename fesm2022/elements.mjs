d568be73e0844de54616111c0234ee732334aab3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 13:42:46 +0000

    c028918 docs: document the use of `this` in template expression syntax (#61250)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-48dc810
+ * @license Angular v20.0.0-rc.0+sha-c028918
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-48dc810');
+const VERSION = new Version('20.0.0-rc.0+sha-c028918');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
