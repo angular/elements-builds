577c9861916c0e8fad1618083ae9e5df40ff53a8
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-14 09:12:07 +0000

    d7a1c19 refactor: support arbitrary stats/metrics in tsurge (#61272)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-12afed8
+ * @license Angular v20.0.0-rc.0+sha-d7a1c19
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-12afed8');
+const VERSION = new Version('20.0.0-rc.0+sha-d7a1c19');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
