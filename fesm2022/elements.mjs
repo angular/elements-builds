0fc0dce540fcff1cb5edeae9708d924c9b9994e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 00:22:24 +0000

    42f203e docs: rename `@nodoc` to `@docs-private` (#61196)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-c92c34d
+ * @license Angular v19.2.10+sha-42f203e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-c92c34d');
+const VERSION = new Version('19.2.10+sha-42f203e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
