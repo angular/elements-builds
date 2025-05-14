e1a8d83f4f850f2f2e3530d871f27e6e49a122ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:41:52 +0000

    d835a44 build: migrate upgrade to use ng_project instead of ng_module (#61320)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-a07413a
+ * @license Angular v20.1.0-next.0+sha-d835a44
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-a07413a');
+const VERSION = new Version('20.1.0-next.0+sha-d835a44');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
