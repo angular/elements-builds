0ffb7dafa4a5f376b653f0df348e1f86c59de251
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 16:53:07 +0000

    1191e62 fix(compiler-cli): avoid ECMAScript private field metadata emit (#61227)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-ae503b7
+ * @license Angular v19.2.11+sha-1191e62
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-ae503b7');
+const VERSION = new Version('19.2.11+sha-1191e62');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
