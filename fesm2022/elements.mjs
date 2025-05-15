cb0f9c84239120cfecd8cc250300491ee722ee43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:23:51 +0000

    ca6295e fix(core): handle different DI token types in Chrome DevTools integration (#61333)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-bc2cab7
+ * @license Angular v20.0.0-rc.1+sha-ca6295e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-bc2cab7');
+const VERSION = new Version('20.0.0-rc.1+sha-ca6295e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
