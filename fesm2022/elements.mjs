eca6fbb863556425c76dfc9c77d1bbde6442ba32
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-15 12:56:55 +0000

    f2d6e59 refactor: explicitly ensure `ngDevMode` types are available (#61365)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-dfd068d
+ * @license Angular v19.2.10+sha-f2d6e59
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-dfd068d');
+const VERSION = new Version('19.2.10+sha-f2d6e59');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
