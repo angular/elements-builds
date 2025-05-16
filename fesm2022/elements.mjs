60c627f33755e2733f67ec82aca16a4f74e37c63
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:44:24 +0000

    378606e docs: fix incomplete sentence (#61391)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-c0fb34b
+ * @license Angular v20.0.0-rc.1+sha-378606e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-c0fb34b');
+const VERSION = new Version('20.0.0-rc.1+sha-378606e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
