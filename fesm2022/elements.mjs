6defcac88d9c3dffc509b8837d090b0e27a0a77e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 07:49:20 +0000

    15dcb2b ci: use current branch as base for pull request (#61390)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-7034b74
+ * @license Angular v20.0.0-rc.1+sha-15dcb2b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-7034b74');
+const VERSION = new Version('20.0.0-rc.1+sha-15dcb2b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
