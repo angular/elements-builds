e2d646e289e38e326720f2c29a4325a716e81527
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 14:06:57 +0000

    fd29dab build: fix golden approval script (#61407)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-b2ade23
+ * @license Angular v19.2.11+sha-fd29dab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-b2ade23');
+const VERSION = new Version('19.2.11+sha-fd29dab');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
