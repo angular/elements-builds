0c438f2195ceb6ec42da7a9a67219d5062604442
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:50:49 +0000

    11589d4 ci: streamline adev CDK APIs and CLI help workflows (#61396)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-94f5a4b
+ * @license Angular v19.2.11+sha-11589d4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-94f5a4b');
+const VERSION = new Version('19.2.11+sha-11589d4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
