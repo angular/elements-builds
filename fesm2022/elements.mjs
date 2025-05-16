b71f4ecffe88c03cd6bcfef829a9d2055e7a81f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 16:04:48 +0000

    13e3966 docs(docs-infra): only show clear button of textfield when there is a text (#61427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-693006e
+ * @license Angular v20.1.0-next.0+sha-13e3966
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-693006e');
+const VERSION = new Version('20.1.0-next.0+sha-13e3966');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
