373dc8c80b296a11e69563f6376b6fa4c5ef85e5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:15:42 +0000

    d3f667a Revert "docs: update Angular CDK apis [main] (#61346)" (#61394)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8e2ca25
+ * @license Angular v20.1.0-next.0+sha-d3f667a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8e2ca25');
+const VERSION = new Version('20.1.0-next.0+sha-d3f667a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
