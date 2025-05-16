f42dd3bad11f4a8f73d2dc2abbd44c355a0a3d07
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:39:11 +0000

    333a0f3 ci: update step name in workflow (#61393)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-d3f667a
+ * @license Angular v20.1.0-next.0+sha-333a0f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-d3f667a');
+const VERSION = new Version('20.1.0-next.0+sha-333a0f3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
