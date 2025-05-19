1f33b8c46daefd5c7a6ccde0f84ba6824065d465
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 14:34:43 +0000

    fcd4355 refactor(core): expose enableProfiling() on from @angular/core (#61465)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-c5177a6
+ * @license Angular v20.0.0-rc.1+sha-fcd4355
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-c5177a6');
+const VERSION = new Version('20.0.0-rc.1+sha-fcd4355');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
