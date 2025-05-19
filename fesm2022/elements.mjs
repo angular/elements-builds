f30e4e36b0241d7b3514be47a8b16ccb74a2477f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:43:25 +0000

    0a25aba build: `--config=debug` should not disable sandbox for all actions (#61468)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-bd2b454
+ * @license Angular v19.2.11+sha-0a25aba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-bd2b454');
+const VERSION = new Version('19.2.11+sha-0a25aba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
