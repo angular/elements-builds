e318e1bb7ea96588d41b55ca90039aa9d4508171
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 16:10:27 +0000

    de602b4 docs: point cdk drag-drop doc links to adev api pages (#61437)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-fcd4355
+ * @license Angular v20.0.0-rc.1+sha-de602b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-fcd4355');
+const VERSION = new Version('20.0.0-rc.1+sha-de602b4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
