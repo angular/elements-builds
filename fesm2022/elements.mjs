5a63f4747064f72033f140de712d73c5d56a1a98
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:47:38 +0000

    f6b4f72 Revert "test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)" (#61510)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-4430423
+ * @license Angular v19.2.11+sha-f6b4f72
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-4430423');
+const VERSION = new Version('19.2.11+sha-f6b4f72');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
