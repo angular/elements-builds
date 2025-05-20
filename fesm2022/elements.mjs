41b16f2e96adbdeeae698c299a8da97bbd3ad7be
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 09:01:01 +0000

    dbd66ec refactor(devtools): switch default font to material symbols (#61281)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1360e4b
+ * @license Angular v19.2.11+sha-dbd66ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-1360e4b');
+const VERSION = new Version('19.2.11+sha-dbd66ec');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
