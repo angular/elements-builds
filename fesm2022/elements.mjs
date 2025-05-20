93d9340a2e6e544ad2c58e0fbd15741251a4e81f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:08:04 +0000

    de7d8be test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-82b51c6
+ * @license Angular v20.0.0-rc.1+sha-de7d8be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-82b51c6');
+const VERSION = new Version('20.0.0-rc.1+sha-de7d8be');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
