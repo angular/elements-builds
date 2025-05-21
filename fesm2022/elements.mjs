5c38d9932b0674679af803dc51ce285f9f7ca935
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:12:21 +0000

    b37fd97 build: migrate language service to use rules_js (#61568)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-f68001d
+ * @license Angular v20.1.0-next.0+sha-b37fd97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-f68001d');
+const VERSION = new Version('20.1.0-next.0+sha-b37fd97');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
