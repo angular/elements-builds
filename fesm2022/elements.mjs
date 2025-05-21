6f2a2f9c85d67c668985515eee705efc5d779c6d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:15:07 +0000

    8f9b05e fix(core): cleanup testability subscriptions (#61261)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-0104a69
+ * @license Angular v19.2.11+sha-8f9b05e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-0104a69');
+const VERSION = new Version('19.2.11+sha-8f9b05e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
