b6e3fce2af93d3d64819cbebc0a9f1436b072596
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 16:22:49 +0000

    f68001d docs: fix descendants default explanation for contentChild (#61580)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-373480c
+ * @license Angular v20.1.0-next.0+sha-f68001d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-373480c');
+const VERSION = new Version('20.1.0-next.0+sha-f68001d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
