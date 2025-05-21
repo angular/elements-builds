391c1fd531463a2c073128a73ec2f74b6e5605b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:51:04 +0000

    47f4a8e refactor(core): drop `rxResource` error messages (#61565)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-e74266b
+ * @license Angular v20.0.0-rc.1+sha-47f4a8e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-e74266b');
+const VERSION = new Version('20.0.0-rc.1+sha-47f4a8e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
