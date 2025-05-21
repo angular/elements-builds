7337cd583e88b1d61f7fac93c4b748e2b159602f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 13:59:07 +0000

    1123b31 refactor(core): consolidate TNode creation checks (#61563)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-e7656b8
+ * @license Angular v20.1.0-next.0+sha-1123b31
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-e7656b8');
+const VERSION = new Version('20.1.0-next.0+sha-1123b31');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
