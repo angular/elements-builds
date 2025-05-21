59ce459514ba6d6dc9fca404f46fcf1d92a41252
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:35:54 +0000

    46af023 fix(compiler): avoid conflicts between HMR code and local symbols (#61550)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-6bf6dbc
+ * @license Angular v20.1.0-next.0+sha-46af023
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-6bf6dbc');
+const VERSION = new Version('20.1.0-next.0+sha-46af023');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
