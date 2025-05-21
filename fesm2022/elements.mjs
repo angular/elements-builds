410d3723e7f9c8d5e42d45afa06326be3032375a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 15:14:12 +0000

    126efc9 fix(common): cancel reader when app is destroyed (#61528)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-c78e75d
+ * @license Angular v19.2.11+sha-126efc9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-c78e75d');
+const VERSION = new Version('19.2.11+sha-126efc9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
