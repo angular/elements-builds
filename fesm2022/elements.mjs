e3e0e66ac9001a3741bd76b877a2d925c54a952b
Andrew Scott <atscott@google.com>
2025-05-22 19:20:26 +0000

    c895115 fix(core): mark `zone.js` as an optional peer dependency (#61616)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-14f2648
+ * @license Angular v20.0.0-rc.2+sha-c895115
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-14f2648');
+const VERSION = new Version('20.0.0-rc.2+sha-c895115');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
