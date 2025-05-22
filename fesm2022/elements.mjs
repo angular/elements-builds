0ed187c0254b5a4d5634e7c3cf92b6a62c2205de
Andrew Scott <atscott@google.com>
2025-05-22 22:41:16 +0000

    a6d5479 build: migrate platform-server to rules_js (#61619)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-e38cc41
+ * @license Angular v19.2.12+sha-a6d5479
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-e38cc41');
+const VERSION = new Version('19.2.12+sha-a6d5479');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
