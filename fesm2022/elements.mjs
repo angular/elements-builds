1ecd4a713cbd096b2ee036a14e2fc76acb73f119
Andrew Scott <atscott@google.com>
2025-05-22 17:59:11 +0000

    114253e release: bump Angular DevTools to version 1.0.32 (#61620)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-959258d
+ * @license Angular v20.0.0-rc.2+sha-114253e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-959258d');
+const VERSION = new Version('20.0.0-rc.2+sha-114253e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
