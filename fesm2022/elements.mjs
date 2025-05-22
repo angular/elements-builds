54c8cbffb812d849c13f15f9c27d3c85fdf0d95f
Andrew Scott <atscott@google.com>
2025-05-22 20:52:20 +0000

    d4d4be8 build: migrate localize package to use rules_js (#61615)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-2ae69f7
+ * @license Angular v19.2.12+sha-d4d4be8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-2ae69f7');
+const VERSION = new Version('19.2.12+sha-d4d4be8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
