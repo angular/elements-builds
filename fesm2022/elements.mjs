8cee066b6d5aaa940245f9ab955bd4245e06595e
Andrew Scott <atscott@google.com>
2025-05-22 03:50:00 +0000

    5f1bd07 fix(zone.js): classes that extend Error should retain cause property (#61599)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-2f5912e
+ * @license Angular v19.2.12+sha-5f1bd07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-2f5912e');
+const VERSION = new Version('19.2.12+sha-5f1bd07');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
