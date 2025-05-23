096d22999195e3ce28410f542f08d5e64e22d573
Andrew Scott <atscott@google.com>
2025-05-23 17:12:31 +0000

    2c876b4 fix(common): avoid injecting ApplicationRef in FetchBackend (#61649)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-a6d5479
+ * @license Angular v19.2.12+sha-2c876b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-a6d5479');
+const VERSION = new Version('19.2.12+sha-2c876b4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
