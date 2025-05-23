9f66bdfe0d18277fe1acc2e642b061c1ea883df7
Andrew Scott <atscott@google.com>
2025-05-23 16:51:51 +0000

    4356e85 fix(core): fakeAsync should not depend on module import order (#61375)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-e33444e
+ * @license Angular v20.1.0-next.0+sha-4356e85
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-e33444e');
+const VERSION = new Version('20.1.0-next.0+sha-4356e85');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
