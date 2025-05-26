0854da9500d2b0ca0bc7c399b55bc3e8f2b434c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 12:04:08 +0000

    73d3e00 build: fix failing test (#61683)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.13+sha-6fea9c8
+ * @license Angular v19.2.13+sha-73d3e00
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.13+sha-6fea9c8');
+const VERSION = new Version('19.2.13+sha-73d3e00');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
