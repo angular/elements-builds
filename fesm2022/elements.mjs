e5b23000392097e252fce9f7f46e40798df9105b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 10:03:54 +0000

    d366a98 build: update tools directory to use rules_js for building (#61657)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-556413d
+ * @license Angular v20.0.0-rc.2+sha-d366a98
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-556413d');
+const VERSION = new Version('20.0.0-rc.2+sha-d366a98');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
