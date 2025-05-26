458a28d0da701fb6a12dffb04a104d9e44aea3c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 09:30:36 +0000

    556413d refactor(compiler-cli): remove hardcoded config for unused standalone imports rule enablement (#61622)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-2e5362a
+ * @license Angular v20.0.0-rc.2+sha-556413d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-2e5362a');
+const VERSION = new Version('20.0.0-rc.2+sha-556413d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
