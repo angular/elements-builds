3ac72235a971097042661b89616ea21f35fc566c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 10:34:27 +0000

    9e1cd49 fix(migrations): preserve comments when removing unused imports (#61674)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.13+sha-61c731a
+ * @license Angular v19.2.13+sha-9e1cd49
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.13+sha-61c731a');
+const VERSION = new Version('19.2.13+sha-9e1cd49');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
