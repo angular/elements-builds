31f73a4ecc090464d84701a1e3f343a1210e93a8
Andrew Scott <atscott@google.com>
2025-05-28 17:36:02 +0000

    d5ac380 build: bump peer deps for angular-in-memory-web-api (#61729)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8cf7e23
+ * @license Angular v20.1.0-next.0+sha-d5ac380
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8cf7e23');
+const VERSION = new Version('20.1.0-next.0+sha-d5ac380');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
