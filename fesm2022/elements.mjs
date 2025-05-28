afb7536262cf7e2a934cbfb8f7371a207e8fb265
Andrew Scott <atscott@google.com>
2025-05-28 18:17:55 +0000

    aed8a77 docs: update versions.json with v20 (#61732)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-1035240
+ * @license Angular v20.0.0+sha-aed8a77
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-1035240');
+const VERSION = new Version('20.0.0+sha-aed8a77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
