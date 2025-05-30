2abfa81cb8d939da0c80a737464b3ab72b78589d
kirjs <kirts@google.com>
2025-05-30 14:02:50 +0000

    b561a50 refactor: expand DevTools reviewer list (#61709)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-e538862
+ * @license Angular v20.0.0+sha-b561a50
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-e538862');
+const VERSION = new Version('20.0.0+sha-b561a50');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
