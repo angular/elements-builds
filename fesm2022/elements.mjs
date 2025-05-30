6474e3146d7e0b519ac50e660cc4facc90a83353
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-30 08:07:56 +0000

    e538862 build: do not run ssr-benchmarks when using `//...`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-6113fae
+ * @license Angular v20.0.0+sha-e538862
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-6113fae');
+const VERSION = new Version('20.0.0+sha-e538862');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
