70577df2e65e30703569d303ebd8853bc04c717a
kirjs <kirts@google.com>
2025-06-02 18:07:35 +0000

    1e84061 build: remove ng_update_migration integration test. (#61822)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-cb1b167
+ * @license Angular v19.2.14+sha-1e84061
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.14+sha-cb1b167');
+const VERSION = new Version('19.2.14+sha-1e84061');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
