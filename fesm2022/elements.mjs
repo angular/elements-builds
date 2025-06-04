5c234a17dfdf146218e8997f5dcac6cba80d63d1
kirjs <kirts@google.com>
2025-06-04 13:44:56 +0000

    5c31e7e fix(core): unregister `onDestroy` when observable errors in `toSignal` (#61596)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-eb62a58
+ * @license Angular v20.0.0+sha-5c31e7e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-eb62a58');
+const VERSION = new Version('20.0.0+sha-5c31e7e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
