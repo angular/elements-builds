9c78d37f0d1d4c3a0009555420814cc8e3348c80
kirjs <kirts@google.com>
2025-06-04 14:33:20 +0000

    3710b12 build: update cross-repo angular dependencies (#61855)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-5c31e7e
+ * @license Angular v20.0.0+sha-3710b12
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-5c31e7e');
+const VERSION = new Version('20.0.0+sha-3710b12');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
