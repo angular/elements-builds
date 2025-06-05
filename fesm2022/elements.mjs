8e483d3124bb029918046ab90983755df7e9d010
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 08:59:17 +0000

    967c8fe docs(docs-infra): use application builder for adev karma tests (#58291)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ab5b7df
+ * @license Angular v20.1.0-next.0+sha-967c8fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-ab5b7df');
+const VERSION = new Version('20.1.0-next.0+sha-967c8fe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
