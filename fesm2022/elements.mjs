1e31c2871aca49fd53a433344bb9e7418482beb4
Andrew Kushnir <akushnir@google.com>
2025-06-06 01:57:05 +0000

    8f9d13e fix(docs-infra): fix wrong `visibleLines` numbers in `docs-code` blocks (#61909)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-cfb0dc2
+ * @license Angular v20.1.0-next.0+sha-8f9d13e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
+const VERSION = new Version('20.1.0-next.0+sha-8f9d13e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
