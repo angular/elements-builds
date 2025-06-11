93dd48a4594e2c6ed5f7f5fc39efb0c89608148f
Andrew Kushnir <akushnir@google.com>
2025-06-11 00:38:38 +0000

    229c6b4 build: update dependency typescript to v5.8.3 (#61958)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-eef6faf
+ * @license Angular v20.0.2+sha-229c6b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-eef6faf');
+const VERSION = new Version('20.0.2+sha-229c6b4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
