3e3180f9f6f2fcbce5c91f9d342e443956ad2745
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 09:46:04 +0000

    06979c6 docs: navigate-to-routes.md to include RouterLink in imports. (#61965)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-e73f248
+ * @license Angular v20.0.2+sha-06979c6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-e73f248');
+const VERSION = new Version('20.0.2+sha-06979c6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
