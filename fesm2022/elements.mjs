670fd44d8c52a129d7f0a3f8d3827f00c1f66151
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 11:46:09 +0000

    554b81e docs(docs-infra): retain the results in the search dialog while typing (#61935)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ff27f21
+ * @license Angular v20.0.2+sha-554b81e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-ff27f21');
+const VERSION = new Version('20.0.2+sha-554b81e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
