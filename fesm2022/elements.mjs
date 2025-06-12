7f7c2ba706441f9d0dfe9f86f458442cfd0d29bc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 12:00:10 +0000

    38f8705 docs: adds `--jobs 4` to Firefox release flags (#62015)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-8424b3b
+ * @license Angular v20.1.0-next.1+sha-38f8705
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-8424b3b');
+const VERSION = new Version('20.1.0-next.1+sha-38f8705');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
