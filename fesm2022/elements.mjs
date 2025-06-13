c1d040a521f66f4105650babaaa388cdf48aba74
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-13 10:43:58 +0000

    9b33089 docs: remove developer preview hint for withI18nSupport (#62039)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-67f657e
+ * @license Angular v20.0.3+sha-9b33089
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-67f657e');
+const VERSION = new Version('20.0.3+sha-9b33089');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
