286d9654486d07fd8b64455a112434f3bccea576
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 10:00:38 +0000

    2e0c98b feat(core): support bindings in TestBed (#62040)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-89efd27
+ * @license Angular v20.1.0-next.1+sha-2e0c98b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-89efd27');
+const VERSION = new Version('20.1.0-next.1+sha-2e0c98b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
