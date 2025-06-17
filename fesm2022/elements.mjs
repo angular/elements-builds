c39dd6985de333fa6dcaa41394bbe938fd4a8ebb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:41:43 +0000

    1c73f54 docs: add update AI instructions (#62089)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-a6c5742
+ * @license Angular v20.0.3+sha-1c73f54
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-a6c5742');
+const VERSION = new Version('20.0.3+sha-1c73f54');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
