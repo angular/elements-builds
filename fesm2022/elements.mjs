6229d10e2d3fc25f9de6532f7ac899c30f8f9663
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 16:11:01 +0000

    f201df9 build: migrate to using new jasmine_test (#62106)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-b40613f
+ * @license Angular v20.0.3+sha-f201df9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-b40613f');
+const VERSION = new Version('20.0.3+sha-f201df9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
