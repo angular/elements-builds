7a5585147a71ed7d516e16ede9bc0ab339cc40a5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 11:21:59 +0000

    acfedce docs: Update define-routes.md (#62069)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-a7e4b3e
+ * @license Angular v20.1.0-next.2+sha-acfedce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-a7e4b3e');
+const VERSION = new Version('20.1.0-next.2+sha-acfedce');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
