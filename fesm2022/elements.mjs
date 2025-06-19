2508e9afaad73d149c3fd32fdddc744cdc3e3d2a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 08:19:00 +0000

    288700c docs(docs-infra): fix spelling mistake (#62126)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-127120a
+ * @license Angular v20.0.4+sha-288700c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-127120a');
+const VERSION = new Version('20.0.4+sha-288700c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
