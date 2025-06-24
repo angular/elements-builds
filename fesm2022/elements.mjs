0cc22e68946cfad9cae69f35f156efd17811a3f3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 10:57:56 +0000

    4f1a5ab build: move dependencies on npm package of dev-infra to workspace dependency (#62083)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-76dbd72
+ * @license Angular v20.0.4+sha-4f1a5ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-76dbd72');
+const VERSION = new Version('20.0.4+sha-4f1a5ab');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
