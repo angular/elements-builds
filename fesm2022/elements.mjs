e71dbbd7c6ec989650245206374caf1916a30a13
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 12:58:09 +0000

    17a8ca3 ci: Update pullapprove ownership for compiler and zonejs (#62261)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-c43f632
+ * @license Angular v20.0.4+sha-17a8ca3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-c43f632');
+const VERSION = new Version('20.0.4+sha-17a8ca3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
