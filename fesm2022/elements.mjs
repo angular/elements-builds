48acca47d79c088638dee8b297478fd107e1340f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 17:14:30 +0000

    a7a6a5b refactor(devtools): inject number and symbol values with injection tokens (#56898)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-c6f0f54
+ * @license Angular v20.0.5+sha-a7a6a5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-c6f0f54');
+const VERSION = new Version('20.0.5+sha-a7a6a5b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
