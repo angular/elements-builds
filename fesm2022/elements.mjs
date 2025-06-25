fcd1d9021b369a85a0d98eab55decbbe7f9f0465
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:25:54 +0000

    c63a256 docs: add llms context files entries (#62224)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-767e1dd
+ * @license Angular v20.0.4+sha-c63a256
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-767e1dd');
+const VERSION = new Version('20.0.4+sha-c63a256');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
