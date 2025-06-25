5248ae9a0edd9fede1dea34f82a2bbcba4a98502
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 13:32:26 +0000

    a2e6f31 fix(core): allow to set a resource in an error state (#62253)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-17a8ca3
+ * @license Angular v20.0.4+sha-a2e6f31
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-17a8ca3');
+const VERSION = new Version('20.0.4+sha-a2e6f31');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
