51f565b94227f262013241e13a08ff29597edf73
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 14:45:59 +0000

    8955ed2 docs: fix deferrable views doc links (#62293)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-a3c246a
+ * @license Angular v20.0.5+sha-8955ed2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-a3c246a');
+const VERSION = new Version('20.0.5+sha-8955ed2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
