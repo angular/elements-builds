5e5931c1e2b6fcc143ba3db7234eeaddaae21aa0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:26:54 +0000

    f10e1bc fix(devtools): profiler records only the first frames from the stream (#62335)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-f943505
+ * @license Angular v20.1.0-next.3+sha-f10e1bc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-f943505');
+const VERSION = new Version('20.1.0-next.3+sha-f10e1bc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
