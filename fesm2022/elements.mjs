532a24ad554f4b31dbfe82f301b77568cc90dcf2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 18:43:44 +0000

    0866077 ci: add jkrems to pullapprove (public-api) (#62340)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-c0532f0
+ * @license Angular v20.0.5+sha-0866077
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-c0532f0');
+const VERSION = new Version('20.0.5+sha-0866077');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
