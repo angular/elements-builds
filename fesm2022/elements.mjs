3c8855005e3efcc427547fb8e7869940136e53fd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 08:43:29 +0000

    df1a61f build: update all non-major dependencies (#62323)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-9afc00f
+ * @license Angular v20.1.0-next.3+sha-df1a61f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-9afc00f');
+const VERSION = new Version('20.1.0-next.3+sha-df1a61f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
