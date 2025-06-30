de8dc6afbe8258550e9fd47860b9d49a6e93d350
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:13:07 +0000

    e1856da docs: update router read-route-state.md file (#62367)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-08e2d9d
+ * @license Angular v20.1.0-next.3+sha-e1856da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-08e2d9d');
+const VERSION = new Version('20.1.0-next.3+sha-e1856da');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
