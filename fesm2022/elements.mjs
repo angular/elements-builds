2d2338e54b7fb9169cdac1935e41e36a89ba61cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:23:26 +0000

    664ed95 refactor(core): update NG0303 error message (#62363)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-e1856da
+ * @license Angular v20.1.0-next.3+sha-664ed95
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-e1856da');
+const VERSION = new Version('20.1.0-next.3+sha-664ed95');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
