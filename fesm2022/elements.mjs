b097635347cf0607557b485022ec287d90e494a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 09:41:39 +0000

    9cd1c42 docs: update breaking change for nullish coalesching + parenthesis (#62382)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-bf881cc
+ * @license Angular v20.0.5+sha-9cd1c42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-bf881cc');
+const VERSION = new Version('20.0.5+sha-9cd1c42');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
