be9bd34960979119b6efa2ca0bd410f04b0b40be
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-01 17:11:31 +0000

    41955cf release: cut the v20.1.0-rc.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-b8c6cfa
+ * @license Angular v20.1.0-rc.0+sha-41955cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-b8c6cfa');
+const VERSION = new Version('20.1.0-rc.0+sha-41955cf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
