5cbb9103b7ac35403fbb485b80b0952a3a3c16ea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:14:54 +0000

    b49f2db refactor(compiler-cli): do not rely on `url` when loading file system logic (#62431)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-567597a
+ * @license Angular v20.2.0-next.0+sha-b49f2db
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-567597a');
+const VERSION = new Version('20.2.0-next.0+sha-b49f2db');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
