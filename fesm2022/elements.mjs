9c9e8ca6272ea1c64c916b1d9a7aab1935c80b33
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:23:26 +0000

    f5f80d8 ci: update pullapprove reviewers (#62448)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-f5a1020
+ * @license Angular v20.1.0-rc.0+sha-f5f80d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-f5a1020');
+const VERSION = new Version('20.1.0-rc.0+sha-f5f80d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
