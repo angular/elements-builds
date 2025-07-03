8c7b24160156d03162d8c820c85238f5ec5bd6c9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:44:33 +0000

    902121e refactor(migrations): ensure tsurge can properly emit references in g3 (#62447)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-aad1e74
+ * @license Angular v20.2.0-next.0+sha-902121e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-aad1e74');
+const VERSION = new Version('20.2.0-next.0+sha-902121e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
