e4b713e6d07c9babfa9467e8127fcfc271af1c63
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:43:05 +0000

    5901a23 docs: display deprecation message for top most symbol (#62427)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-a7ce3cc
+ * @license Angular v20.0.6+sha-5901a23
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-a7ce3cc');
+const VERSION = new Version('20.0.6+sha-5901a23');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
