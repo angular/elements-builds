c4d6654da548a409fa117cd6bcdbe85867abb941
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 08:55:46 +0000

    9ba5903 refactor(devtools): remove the standalone demo (#62455)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-720717e
+ * @license Angular v20.2.0-next.0+sha-9ba5903
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-720717e');
+const VERSION = new Version('20.2.0-next.0+sha-9ba5903');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
