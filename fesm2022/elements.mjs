be1110c0ccf2ec566d393b1ec34066df0b348ac2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 11:09:21 +0000

    da99b9a docs: fix typo on guidelines file (#62468)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-2943591
+ * @license Angular v20.1.0-rc.0+sha-da99b9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-2943591');
+const VERSION = new Version('20.1.0-rc.0+sha-da99b9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
