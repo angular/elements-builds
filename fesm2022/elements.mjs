cb355dda6da7d47b39e836dff2dc8f1b3d06a4c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 21:03:01 +0000

    efc6405 build: update devinfra digest to 6f54d14 (#62508)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-aee83d1
+ * @license Angular v20.1.0-rc.0+sha-efc6405
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-aee83d1');
+const VERSION = new Version('20.1.0-rc.0+sha-efc6405');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
