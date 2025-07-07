859e69cfc95ec375ab0d5890d79facafbc0e73bd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:01:25 +0000

    65b9adf docs: update comment (#62482) PR Close #62482

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-633d01b
+ * @license Angular v20.2.0-next.0+sha-65b9adf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-633d01b');
+const VERSION = new Version('20.2.0-next.0+sha-65b9adf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
