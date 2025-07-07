0f5884d23519dd8cfd8a495f6b13c8a056c76634
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-07-07 11:07:36 +0000

    d774314 refactor(compiler-cli): ensure mock file system can be loaded in the browser (#62493)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-da99b9a
+ * @license Angular v20.1.0-rc.0+sha-d774314
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-da99b9a');
+const VERSION = new Version('20.1.0-rc.0+sha-d774314');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
