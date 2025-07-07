286d71747461dc9d392d8244d53fc176158ab82a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 20:51:41 +0000

    22b8e22 build: migrate modules to use dependencies from //modules:package.json (#62506)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-ce8156a
+ * @license Angular v20.2.0-next.0+sha-22b8e22
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-ce8156a');
+const VERSION = new Version('20.2.0-next.0+sha-22b8e22');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
