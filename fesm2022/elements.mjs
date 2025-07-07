3d0fcb574651062180a0ceedc9db09d43dfe52d5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 16:23:19 +0000

    633d01b refactor(compiler): ensure compiler passes closure conformance checks (#62500)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-c6a720b
+ * @license Angular v20.2.0-next.0+sha-633d01b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-c6a720b');
+const VERSION = new Version('20.2.0-next.0+sha-633d01b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
