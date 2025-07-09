62d128ebeaed234569b5d41885aab0cb626c64a0
Andrew Kushnir <akushnir@google.com>
2025-07-09 21:46:41 +0000

    9c45c32 fix(router): ensure preloaded components are properly activated (#62502)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-9d5e248
+ * @license Angular v20.2.0-next.0+sha-9c45c32
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-9d5e248');
+const VERSION = new Version('20.2.0-next.0+sha-9c45c32');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
