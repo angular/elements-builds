224cc21562662c19584867ce42c2fb3053b5415a
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:52:16 +0000

    4f1e88e build: use esbuild from aspect rules (#62568)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-aea0a70
+ * @license Angular v20.1.0+sha-4f1e88e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-aea0a70');
+const VERSION = new Version('20.1.0+sha-4f1e88e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
