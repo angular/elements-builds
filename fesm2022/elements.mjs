db5e819521709267a9b45f9419593b6973f28c1e
Andrew Kushnir <akushnir@google.com>
2025-07-10 17:45:20 +0000

    6549418 refactor(core): include DI path into cyclic dependency error message (#50902)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-66a85ab
+ * @license Angular v20.1.0+sha-6549418
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-66a85ab');
+const VERSION = new Version('20.1.0+sha-6549418');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
