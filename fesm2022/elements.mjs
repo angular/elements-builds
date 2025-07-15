a880dd441f0432067d011cf7ce118bc6e854b84f
Andrew Kushnir <akushnir@google.com>
2025-07-15 20:34:20 +0000

    70c8780 fix(compiler): more permissive parsing of @ characters (#62644)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-acf8b74
+ * @license Angular v20.1.0+sha-70c8780
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-acf8b74');
+const VERSION = new Version('20.1.0+sha-70c8780');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
