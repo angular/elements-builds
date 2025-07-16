609098514789d8c7f3d40eed1fee5385bee468c3
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:20:27 +0000

    f5f8f76 release: bump Angular DevTools version to 1.0.39 (#62670)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a484a11
+ * @license Angular v20.2.0-next.1+sha-f5f8f76
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a484a11');
+const VERSION = new Version('20.2.0-next.1+sha-f5f8f76');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
