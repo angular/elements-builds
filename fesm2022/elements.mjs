26c0f832d3c71e2957d24e300bb4855fa90c0469
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-16 10:48:45 +0000

    527bd77 fix(compiler-cli): infer type of event target for void elements (#62648)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-18a6750
+ * @license Angular v20.2.0-next.0+sha-527bd77
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-18a6750');
+const VERSION = new Version('20.2.0-next.0+sha-527bd77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
