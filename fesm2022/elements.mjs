dd561389b276070153fefb43892f7b6e7258a26b
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:29:27 +0000

    8ad10fd fix(compiler): fix detection of directive deps in JIT (#62666)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-dcab8c9
+ * @license Angular v20.1.1+sha-8ad10fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-dcab8c9');
+const VERSION = new Version('20.1.1+sha-8ad10fd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
