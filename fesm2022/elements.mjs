2d9a64065aaece682c4cfd6a82b4c799e6e14b7c
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:13:55 +0000

    2f7f956 refactor(devtools): rearrange main toolbar menu buttons (#62684)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-2e6c2ff
+ * @license Angular v20.2.0-next.1+sha-2f7f956
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-2e6c2ff');
+const VERSION = new Version('20.2.0-next.1+sha-2f7f956');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
