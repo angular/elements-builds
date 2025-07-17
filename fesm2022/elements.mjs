59a31e1cc6e535bc346077463de461d0dd3513e0
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:34:39 +0000

    1930e99 docs: Update versions.md to use canonical Baseline site (#62680)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-0db3613
+ * @license Angular v20.1.1+sha-1930e99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-0db3613');
+const VERSION = new Version('20.1.1+sha-1930e99');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
