a78e026592bfed36a7e30c5261547d0e5a2e23c3
Kirill Cherkashin <kirjs@google.com>
2025-07-18 11:28:41 +0000

    ba8a5d8 refactor(core): Use promise.finally for task removal (#62674)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-1e9bfe5
+ * @license Angular v20.1.2+sha-ba8a5d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-1e9bfe5');
+const VERSION = new Version('20.1.2+sha-ba8a5d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
