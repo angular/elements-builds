b40e5ce7e55a2f32efae6d86dfe346535ea26710
Kirill Cherkashin <kirjs@google.com>
2025-07-18 14:27:26 +0000

    c36de9e build: update cross-repo angular dependencies (#62651)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-409287a
+ * @license Angular v20.1.2+sha-c36de9e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-409287a');
+const VERSION = new Version('20.1.2+sha-c36de9e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
