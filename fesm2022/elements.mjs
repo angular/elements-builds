7a65a3533610f757c3a4ebdb0afccef25adea22c
Kirill Cherkashin <kirjs@google.com>
2025-07-21 21:09:19 +0000

    38b7ab2 refactor(core): add a `getTransferState` for the devtools (#62722)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-45d3eef
+ * @license Angular v20.1.2+sha-38b7ab2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-45d3eef');
+const VERSION = new Version('20.1.2+sha-38b7ab2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
