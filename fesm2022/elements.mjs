47002988c7eaa69188ff342ed925ea98d487b286
Kirill Cherkashin <kirjs@google.com>
2025-07-21 13:44:34 +0000

    cf48b13 docs: fix code snippet (#62720)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-c36de9e
+ * @license Angular v20.1.2+sha-cf48b13
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-c36de9e');
+const VERSION = new Version('20.1.2+sha-cf48b13');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
