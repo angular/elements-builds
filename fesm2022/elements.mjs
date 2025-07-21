b4b55e31e9bc304906551e5af4e838576775a8d8
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:55:02 +0000

    8b5f814 build: update all non-major dependencies (#62701)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a6b2d7d
+ * @license Angular v20.2.0-next.1+sha-8b5f814
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a6b2d7d');
+const VERSION = new Version('20.2.0-next.1+sha-8b5f814');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
