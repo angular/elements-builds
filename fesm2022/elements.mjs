3a5343537e951f344289630075976469704e0ff1
Kirill Cherkashin <kirjs@google.com>
2025-07-21 15:01:38 +0000

    bca8a3c build: update dependency node to v22.17.1 (#62715)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-a1a78b2
+ * @license Angular v20.1.2+sha-bca8a3c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-a1a78b2');
+const VERSION = new Version('20.1.2+sha-bca8a3c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
