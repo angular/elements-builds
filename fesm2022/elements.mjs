4f462931bd3dd07eb9ff682f0773d3df4fb1cd14
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:15:59 +0000

    a6b2d7d fix(devtools): add event tagging to prevent DDOS (#62645)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-97b1bd1
+ * @license Angular v20.2.0-next.1+sha-a6b2d7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-97b1bd1');
+const VERSION = new Version('20.2.0-next.1+sha-a6b2d7d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
