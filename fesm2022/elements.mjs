9e3b15111184738ffafbee85b682c8308fb5d8ad
Kirill Cherkashin <kirjs@google.com>
2025-07-22 18:26:47 +0000

    0a9f55d build: update dependency angular-split to v20 (#62740)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-92e03e1
+ * @license Angular v20.2.0-next.1+sha-0a9f55d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-92e03e1');
+const VERSION = new Version('20.2.0-next.1+sha-0a9f55d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
