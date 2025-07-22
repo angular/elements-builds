f6d3a02b6ae5fe7c809db2c1829ece0e239166dd
Kirill Cherkashin <kirjs@google.com>
2025-07-22 11:06:00 +0000

    4138aca feat(core): render ARIA property bindings as attributes (#62630)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-db3c928
+ * @license Angular v20.2.0-next.1+sha-4138aca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-db3c928');
+const VERSION = new Version('20.2.0-next.1+sha-4138aca');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
