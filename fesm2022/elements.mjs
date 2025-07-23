441217d086697bd44423fe58a86bb6389283786d
Kirill Cherkashin <kirjs@google.com>
2025-07-23 11:32:03 +0000

    b7c745b docs: create MCP server setup guide (#62746)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-91bdd17
+ * @license Angular v20.1.2+sha-b7c745b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-91bdd17');
+const VERSION = new Version('20.1.2+sha-b7c745b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
