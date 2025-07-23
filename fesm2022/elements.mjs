7885de15a9cd0e0ee178c923be593b063dacf78c
Kirill Cherkashin <kirjs@google.com>
2025-07-23 18:08:25 +0000

    9562b63 docs: guard a resource error with `hasValue()` (#62708)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-593cc8a
+ * @license Angular v20.2.0-next.1+sha-9562b63
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-593cc8a');
+const VERSION = new Version('20.2.0-next.1+sha-9562b63');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
