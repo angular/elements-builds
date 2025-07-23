47dab6a21838d4218d35958195110fa9c2dee7a4
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:49:29 +0000

    7f9d37d build: update bazel rules_browsers dependency (#62772)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-063b5e1
+ * @license Angular v20.2.0-next.1+sha-7f9d37d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-063b5e1');
+const VERSION = new Version('20.2.0-next.1+sha-7f9d37d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
