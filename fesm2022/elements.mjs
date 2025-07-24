20582de0373e16e092e0bfbacee4faab4ccfed9e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 10:12:36 +0000

    92b0802 test(router): Use autoTick feature in router tests (#62776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-9d8c48d
+ * @license Angular v20.2.0-next.2+sha-92b0802
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-9d8c48d');
+const VERSION = new Version('20.2.0-next.2+sha-92b0802');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
