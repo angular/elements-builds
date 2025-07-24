02b4da7c213e5c1b722bcce9cdf455d18a4c26a5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 16:20:48 +0000

    82862de docs(docs-infra): correct typos in adev and wording in resource api (#62801) PR Close #62801

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-da0fb33
+ * @license Angular v20.1.3+sha-82862de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-da0fb33');
+const VERSION = new Version('20.1.3+sha-82862de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
