d9678ef5414e758c1b06d82dc717f3b7c362cc0f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 12:38:36 +0000

    63a6b5a build: update cross-repo angular dependencies (#62806)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-632eba5
+ * @license Angular v20.2.0-next.2+sha-63a6b5a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-632eba5');
+const VERSION = new Version('20.2.0-next.2+sha-63a6b5a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
