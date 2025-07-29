910472bfe8c09dd906cea7b55041e9785a1ebfc2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 12:23:22 +0000

    0b9f53a docs: fix ActivateRoute example (#62855)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-f0fca61
+ * @license Angular v20.1.3+sha-0b9f53a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-f0fca61');
+const VERSION = new Version('20.1.3+sha-0b9f53a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
