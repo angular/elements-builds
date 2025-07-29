fe0853d6a3db9aee0dc0745e39f23054850d4b5c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 17:20:01 +0000

    29195f1 build: use ts_project based extract_types (#62875)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-793ff35
+ * @license Angular v20.2.0-next.2+sha-29195f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-793ff35');
+const VERSION = new Version('20.2.0-next.2+sha-29195f1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
