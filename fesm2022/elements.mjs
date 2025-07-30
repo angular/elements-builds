8ec4544966bd0a534860af0e0c9c7bf777081b4f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 14:40:53 +0000

    82fcc9c Revert "fix(http): add missing http options allowed in fetch API (#62881)" (#62899)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-55e575a
+ * @license Angular v20.1.3+sha-82fcc9c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-55e575a');
+const VERSION = new Version('20.1.3+sha-82fcc9c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
