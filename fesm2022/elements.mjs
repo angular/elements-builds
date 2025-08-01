8c77fce4eb74790a9b6b78057d277cc2cf047c53
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:26:22 +0000

    c2b48be build: move to pnpm version 9.15.9 (#62951)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-f700bd9
+ * @license Angular v20.1.4+sha-c2b48be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-f700bd9');
+const VERSION = new Version('20.1.4+sha-c2b48be');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
