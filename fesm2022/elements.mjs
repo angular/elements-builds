48e66132de19453352431da0cbea88e7d22cc886
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 13:30:10 +0000

    f700bd9 build: update pnpm to v10.14.0 (#62946)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-3a8fa72
+ * @license Angular v20.1.4+sha-f700bd9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-3a8fa72');
+const VERSION = new Version('20.1.4+sha-f700bd9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
