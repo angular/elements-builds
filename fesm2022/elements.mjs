516fe692b830618c506d3f4644f1b6537d617488
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:17:39 +0000

    e5d6fb5 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-d05138a
+ * @license Angular v20.2.0-next.3+sha-e5d6fb5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-d05138a');
+const VERSION = new Version('20.2.0-next.3+sha-e5d6fb5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
