6a47b3f769336e5905662494af5fb8116992889f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 16:24:35 +0000

    52b8e07 feat(platform-browser): Warns on conflicting hydration and blocking navigation (#62963)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-ddf779e
+ * @license Angular v20.2.0-next.3+sha-52b8e07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-ddf779e');
+const VERSION = new Version('20.2.0-next.3+sha-52b8e07');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
