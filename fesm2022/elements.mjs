6260c146586948db8283c2b71495fb81ad72b4ff
Doug Parker <dgp1130@users.noreply.github.com>
2025-08-06 22:00:31 +0000

    3a320bd release: bump Angular DevTools version to 1.0.42 (#63036)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.5+sha-90b0227
+ * @license Angular v20.2.0-next.5+sha-3a320bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.5+sha-90b0227');
+const VERSION = new Version('20.2.0-next.5+sha-3a320bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
