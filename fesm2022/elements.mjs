d15ec38a2ee410153752b8f0451c08fc95a5b011
Andrew Scott <atscott@google.com>
2025-08-07 16:15:34 +0000

    8f234ff docs(devtools): update `pnpm` install command (#63040)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-0430a56
+ * @license Angular v20.1.6+sha-8f234ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-0430a56');
+const VERSION = new Version('20.1.6+sha-8f234ff');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
