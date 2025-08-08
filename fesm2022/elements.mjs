c504742098f0dbb5a80c5fe40df4afa97ecce45a
Andrew Scott <atscott@google.com>
2025-08-08 15:55:41 +0000

    322042c fix(core): destroying the effect on `afterRenderEffect` (#63001)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-bba7c6f
+ * @license Angular v20.1.6+sha-322042c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-bba7c6f');
+const VERSION = new Version('20.1.6+sha-322042c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
