73b99e48da4313e81b68ed59267ea04e2b9534cb
Andrew Scott <atscott@google.com>
2025-08-08 18:11:47 +0000

    3b214d2 feat(service-worker): Logs unhandled promise rejections in service worker (#63059)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-6a11846
+ * @license Angular v20.2.0-next.6+sha-3b214d2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-6a11846');
+const VERSION = new Version('20.2.0-next.6+sha-3b214d2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
