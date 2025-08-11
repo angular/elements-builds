7e53e493a3b1f3c2cf3f81c813ad6db9b700d392
Andrew Scott <atscott@google.com>
2025-08-11 19:20:25 +0000

    499fa8f docs: remove router testing from the testing scenarios guide (#63078)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-79b9159
+ * @license Angular v20.2.0-rc.0+sha-499fa8f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-79b9159');
+const VERSION = new Version('20.2.0-rc.0+sha-499fa8f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
