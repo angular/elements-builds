949290e5b58eb40f298a9150b002a1e729d21a1c
Andrew Scott <atscott@google.com>
2025-08-11 17:39:22 +0000

    c4dcfdf build: update to the latest version of devinfra and rename npm2 workspace (#63093)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-57184d0
+ * @license Angular v20.3.0-next.0+sha-c4dcfdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-57184d0');
+const VERSION = new Version('20.3.0-next.0+sha-c4dcfdf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
