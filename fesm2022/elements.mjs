ee80330eed6e68c4c396f25717f35065bfc58da4
Andrew Scott <atscott@google.com>
2025-08-11 23:45:31 +0000

    e6c84a4 refactor(zone.js): remove unused jasmine globalerror monkey patching. (#63077)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-1dd6fd0
+ * @license Angular v20.1.6+sha-e6c84a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-1dd6fd0');
+const VERSION = new Version('20.1.6+sha-e6c84a4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
