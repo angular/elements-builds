042607331699d3a68fe382d2ebff1ab3f58b2f61
Andrew Scott <atscott01@gmail.com>
2025-08-12 20:34:45 +0000

    ada3c37 refactor(compiler-cli): ensure JIT transforms with queries work with closure property renaming (#63123)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-4635095
+ * @license Angular v20.2.0-rc.0+sha-ada3c37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-4635095');
+const VERSION = new Version('20.2.0-rc.0+sha-ada3c37');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
