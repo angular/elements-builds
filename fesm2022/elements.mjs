155445c40a952e50becf1ca515b8ea57790b0aa0
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-12 08:05:49 +0000

    856dd50 ci: correctly configure ignored filenames (#63105)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-b38c43b
+ * @license Angular v20.2.0-rc.0+sha-856dd50
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-b38c43b');
+const VERSION = new Version('20.2.0-rc.0+sha-856dd50');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
