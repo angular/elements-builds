23154abb1d3ec1c1df20ea54e08d95a45f63dc63
Andrew Scott <atscott@google.com>
2025-08-12 16:43:58 +0000

    f251c7d docs: add missing supported `!=` operator (#63108)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-e6c84a4
+ * @license Angular v20.1.6+sha-f251c7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-e6c84a4');
+const VERSION = new Version('20.1.6+sha-f251c7d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
