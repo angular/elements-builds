fa7a0d80841e0449662dfa83e6244332d5b8715b
Andrew Scott <atscott01@gmail.com>
2025-08-12 23:57:37 +0000

    d7a5ead build: update pnpm to v10 (#63087)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-ea78278
+ * @license Angular v20.3.0-next.0+sha-d7a5ead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-ea78278');
+const VERSION = new Version('20.3.0-next.0+sha-d7a5ead');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
