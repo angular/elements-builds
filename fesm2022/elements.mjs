bf61cda7a6aa9aa734ff67727f1ff00b3ceb1c59
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:24 +0000

    55cd8c2 docs: add rxjs-interop section for `rxResource` (#63125)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-21b0fd3
+ * @license Angular v21.0.0-next.0+sha-55cd8c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-21b0fd3');
+const VERSION = new Version('21.0.0-next.0+sha-55cd8c2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
