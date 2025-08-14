4104fb8dc69d3b8b4292516d7fbfb9dfc871b6c5
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:23 +0000

    5129dae ci: redirect adev to `index.csr.html` (#63168)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d733c69
+ * @license Angular v21.0.0-next.0+sha-5129dae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d733c69');
+const VERSION = new Version('21.0.0-next.0+sha-5129dae');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
