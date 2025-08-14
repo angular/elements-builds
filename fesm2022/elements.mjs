81e61c1acd412658dd34d94c2a7b170a8d60a2e4
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:16:39 +0000

    4bed062 feat(http): Provide http services in root (#56212)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-e7cc89e
+ * @license Angular v21.0.0-next.0+sha-4bed062
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-e7cc89e');
+const VERSION = new Version('21.0.0-next.0+sha-4bed062');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
