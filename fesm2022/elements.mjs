10b06cc2ee3616382839eb3ed10f111dcf8f9814
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:14:58 +0000

    8747f9a docs: fix edit links for block/element api entries (#62766)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-1cf4d34
+ * @license Angular v20.2.0-rc.1+sha-8747f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-1cf4d34');
+const VERSION = new Version('20.2.0-rc.1+sha-8747f9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
