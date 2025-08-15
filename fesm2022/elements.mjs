f9e1c80ed94404c48e0e35b67dd2b7132708a367
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 07:59:00 +0000

    691f5ed fix(compiler): error when ng-content fallback has translated children (#63156)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-39e4f93
+ * @license Angular v20.1.7+sha-691f5ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-39e4f93');
+const VERSION = new Version('20.1.7+sha-691f5ed');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
