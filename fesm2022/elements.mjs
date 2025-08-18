2e502731b55db0fddbfd23cb6af48a06254922db
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:18:39 +0000

    a7e62b0 docs: revert remove of `build-and-link` (#63187)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-f43f8bb
+ * @license Angular v20.1.7+sha-a7e62b0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-f43f8bb');
+const VERSION = new Version('20.1.7+sha-a7e62b0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
