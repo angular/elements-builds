b6202a8e0bc9da4e4037cdb316926d641bfa294b
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 10:56:55 +0000

    6c8d000 ci: mark myself as unavailable during OOO (#63214)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-ab067a3
+ * @license Angular v21.0.0-next.0+sha-6c8d000
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-ab067a3');
+const VERSION = new Version('21.0.0-next.0+sha-6c8d000');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
