e1871b95b06cc3e50807feaa907b5d75df03afa0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:12:46 +0000

    1aa0a88 build: update the failure message with the correct command (#63219)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a7cfb42
+ * @license Angular v21.0.0-next.0+sha-1aa0a88
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a7cfb42');
+const VERSION = new Version('21.0.0-next.0+sha-1aa0a88');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
