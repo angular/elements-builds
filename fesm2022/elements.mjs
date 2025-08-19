ec9af2a9a2dd1cbdae0bbd6bbe150f65e7b9c912
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:31:11 +0000

    6712456 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-101f072
+ * @license Angular v21.0.0-next.0+sha-6712456
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-101f072');
+const VERSION = new Version('21.0.0-next.0+sha-6712456');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
