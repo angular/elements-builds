48097e815b1ae67297e95e1653a817eadfa49c89
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 13:09:43 +0000

    4dfa6f1 docs(docs-infra): auto-links to API entries (#63162)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a0ee6bd
+ * @license Angular v21.0.0-next.0+sha-4dfa6f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a0ee6bd');
+const VERSION = new Version('21.0.0-next.0+sha-4dfa6f1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
