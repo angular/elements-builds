bcc2a95eaa06fc9506d74f1af3e3457ea614f249
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:24:49 +0000

    8a6cc39 build: update all non-major dependencies (#63256)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-b7f2a8b
+ * @license Angular v21.0.0-next.0+sha-8a6cc39
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-b7f2a8b');
+const VERSION = new Version('21.0.0-next.0+sha-8a6cc39');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
