c8245b5a9ced280cb1882d038180ec7c7297a6a0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:34:33 +0000

    1feadba docs: remove redundant Angular docs link in tailwind guide (#63205)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-8a6cc39
+ * @license Angular v21.0.0-next.0+sha-1feadba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-8a6cc39');
+const VERSION = new Version('21.0.0-next.0+sha-1feadba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
