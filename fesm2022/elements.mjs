413da2d62d684b490e5ebc92e06573d8a9b0bf84
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:08:49 +0000

    39febe1 docs: rewording heading for `define` option (#63009)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-c935d42
+ * @license Angular v20.1.7+sha-39febe1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-c935d42');
+const VERSION = new Version('20.1.7+sha-39febe1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
