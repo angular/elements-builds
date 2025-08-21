9108545c68af3eb43a47a8fef35a7464b38555ab
Andrew Kushnir <akushnir@google.com>
2025-08-21 17:20:06 +0000

    49e00b3 docs(docs-infra): fix mermaid rendering (#63311)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-1c27fa9
+ * @license Angular v20.2.1+sha-49e00b3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-1c27fa9');
+const VERSION = new Version('20.2.1+sha-49e00b3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
