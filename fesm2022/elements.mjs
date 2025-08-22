676d26339538e374e55aa3f28ac417d07f586e92
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:31:06 +0000

    983cc2f ci: add myself to review fw-general (#63337)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a842272
+ * @license Angular v21.0.0-next.0+sha-983cc2f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a842272');
+const VERSION = new Version('21.0.0-next.0+sha-983cc2f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
