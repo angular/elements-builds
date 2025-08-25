8f513c8055283af45554c2701a9fd95b1e5b1f00
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:22:18 +0000

    650ad1f docs: add important service worker note (#63364)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-10b0927
+ * @license Angular v20.2.1+sha-650ad1f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-10b0927');
+const VERSION = new Version('20.2.1+sha-650ad1f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
