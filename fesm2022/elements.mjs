813a633380dc1d766c00c323458290166d2c2120
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:49:20 +0000

    9a27c5b build: rename defaults2.bzl to defaults.bzl (#63384)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-a26d94d
+ * @license Angular v20.2.1+sha-9a27c5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-a26d94d');
+const VERSION = new Version('20.2.1+sha-9a27c5b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
