056bf57a3f75a437026a8b45df3281c9ef3115cf
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:57:09 +0000

    23d1ace refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-e220a61
+ * @license Angular v20.2.1+sha-23d1ace
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-e220a61');
+const VERSION = new Version('20.2.1+sha-23d1ace');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
