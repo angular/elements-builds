8b22a6b74a9603d7aef034a9463e4e3d63f4dc60
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:54:02 +0000

    e220a61 docs(docs-infra): patch port changes from `main` (#63394)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-c9b0f45
+ * @license Angular v20.2.1+sha-e220a61
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-c9b0f45');
+const VERSION = new Version('20.2.1+sha-e220a61');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
