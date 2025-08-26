a507b02a6b1f266c84234928304902d90b28a8b5
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:50:20 +0000

    a038840 fix(compiler): fixes animations on elements with structural directives (#63390)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-aff8bb2
+ * @license Angular v21.0.0-next.0+sha-a038840
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-aff8bb2');
+const VERSION = new Version('21.0.0-next.0+sha-a038840');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
