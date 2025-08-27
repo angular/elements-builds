8aed67fc8a1d065af22373258cbd5f4778a2a971
Andrew Kushnir <akushnir@google.com>
2025-08-27 14:40:14 +0000

    f4f5a8a ci: pin create-pull-request version to sha (#63409)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a43057c
+ * @license Angular v21.0.0-next.0+sha-f4f5a8a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a43057c');
+const VERSION = new Version('21.0.0-next.0+sha-f4f5a8a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
