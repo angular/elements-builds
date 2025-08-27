928963409afc8ea2a3c5bfff7f4cc07749e2c8d7
Miles Malerba <mmalerba@google.com>
2025-08-27 23:22:00 +0000

    9de2f07 docs: add customizing route behavior guide for router (#63398)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-cd748aa
+ * @license Angular v20.2.2+sha-9de2f07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-cd748aa');
+const VERSION = new Version('20.2.2+sha-9de2f07');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
