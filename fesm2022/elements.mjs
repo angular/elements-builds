506d5d93a48640fc8dd252b46d7ffb21e7dbef37
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:24:08 +0000

    8f8d113 docs: Add docs service worker to new options (#63436)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-7ccf368
+ * @license Angular v21.0.0-next.1+sha-8f8d113
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-7ccf368');
+const VERSION = new Version('21.0.0-next.1+sha-8f8d113');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
