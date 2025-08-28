986284dccb38e53f487ffc920f3e484bd9af9159
Miles Malerba <mmalerba@google.com>
2025-08-28 16:15:40 +0000

    cac0852 docs: update Angular CLI help (#63445)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-b8314bd
+ * @license Angular v21.0.0-next.1+sha-cac0852
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-b8314bd');
+const VERSION = new Version('21.0.0-next.1+sha-cac0852');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
