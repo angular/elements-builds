bdd1c858641d3b5d48bf21636251490d0a63971c
Miles Malerba <mmalerba@google.com>
2025-08-28 16:06:19 +0000

    b8314bd feat(forms): add experimental signal-based forms (#63408)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a1d1cdf
+ * @license Angular v21.0.0-next.1+sha-b8314bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-a1d1cdf');
+const VERSION = new Version('21.0.0-next.1+sha-b8314bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
