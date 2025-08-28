9f4d6056930486dcba7f56484a9197f812f840ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:10:11 +0000

    2e05dea ci: remove old options from workflows (#63441)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-690405b
+ * @license Angular v20.2.2+sha-2e05dea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-690405b');
+const VERSION = new Version('20.2.2+sha-2e05dea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
