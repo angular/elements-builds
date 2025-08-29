63b54433ea7c6a31d490a402ef74b27b835ec8ce
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:23:22 +0000

    3fb02ed build: update google-github-actions/auth action to v3 (#63461)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a3938d5
+ * @license Angular v21.0.0-next.1+sha-3fb02ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-a3938d5');
+const VERSION = new Version('21.0.0-next.1+sha-3fb02ed');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
