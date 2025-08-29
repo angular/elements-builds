ebce40442f3eb3ded4666635772783413dc2e217
Miles Malerba <mmalerba@google.com>
2025-08-29 21:35:05 +0000

    862495c build: configure signal forms for release (#63458)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-e75b04f
+ * @license Angular v21.0.0-next.1+sha-862495c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-e75b04f');
+const VERSION = new Version('21.0.0-next.1+sha-862495c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
