1905a208b7df949be5976c3e183b60a8c5ac16a6
Miles Malerba <mmalerba@google.com>
2025-08-29 21:10:01 +0000

    e9096b9 docs(docs-infra): properties should be readonly (#63492)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-5bb4b75
+ * @license Angular v20.2.3+sha-e9096b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-5bb4b75');
+const VERSION = new Version('20.2.3+sha-e9096b9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
