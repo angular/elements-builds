30af4637557de1cd5958f03deca8482669089b0b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:36:43 +0000

    64ddde2 release: cut the v20.2.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-479a919
+ * @license Angular v20.2.3+sha-64ddde2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-479a919');
+const VERSION = new Version('20.2.3+sha-64ddde2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
