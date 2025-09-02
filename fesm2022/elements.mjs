d916a95f4c8ec6c8506f843a0b925c0ddb3def3b
Miles Malerba <mmalerba@google.com>
2025-09-02 18:56:26 +0000

    4c36659 refactor(router): Add handling for empty observables in guards (#63546)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-92e09ad
+ * @license Angular v21.0.0-next.1+sha-4c36659
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-92e09ad');
+const VERSION = new Version('21.0.0-next.1+sha-4c36659');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
