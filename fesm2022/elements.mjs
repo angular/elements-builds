b7bcb1c7b4b63cae9d5f30d93d96373b0e374dfd
Miles Malerba <mmalerba@google.com>
2025-09-02 18:58:34 +0000

    1e47a87 build: update cross-repo angular dependencies (#63553)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-dc64f3e
+ * @license Angular v20.2.3+sha-1e47a87
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-dc64f3e');
+const VERSION = new Version('20.2.3+sha-1e47a87');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
