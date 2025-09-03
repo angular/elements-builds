736a87a61f63f061d9274cfc27d8d538a2f6e3ad
Miles Malerba <mmalerba@google.com>
2025-09-03 16:04:42 +0000

    e6949ad docs: fix #63555 (#63560)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-d006721
+ * @license Angular v21.0.0-next.1+sha-e6949ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-d006721');
+const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
