18fb5b8de81c99e21ded8bf4a909a06274b22545
Miles Malerba <mmalerba@google.com>
2025-09-03 03:56:04 +0000

    475ed59 refactor(core): export zoneless provider token privately (#63558)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-6148a40
+ * @license Angular v21.0.0-next.1+sha-475ed59
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-6148a40');
+const VERSION = new Version('21.0.0-next.1+sha-475ed59');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
