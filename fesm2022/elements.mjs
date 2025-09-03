7a777d3f3f65a178ca986960d47aeb1b7ae3554e
Miles Malerba <mmalerba@google.com>
2025-09-03 16:19:23 +0000

    49d5de4 refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-e6949ad
+ * @license Angular v21.0.0-next.1+sha-49d5de4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
+const VERSION = new Version('21.0.0-next.1+sha-49d5de4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
