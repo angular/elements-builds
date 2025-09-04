ee7d20b027383355aec040adc082ad0b0ef29738
Andrew Scott <atscott@google.com>
2025-09-04 22:28:55 +0000

    8401f89 docs(docs-infra): allow collapse code example (#63559)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-3099f91
+ * @license Angular v21.0.0-next.2+sha-8401f89
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-3099f91');
+const VERSION = new Version('21.0.0-next.2+sha-8401f89');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
