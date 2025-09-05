9468d118f07459ad51006284d417d0b296e9e175
Andrew Scott <atscott@google.com>
2025-09-05 21:04:15 +0000

    bacda4f refactor(forms): add support for binding number fields to date inputs (#63585)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c0791e1
+ * @license Angular v21.0.0-next.2+sha-bacda4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c0791e1');
+const VERSION = new Version('21.0.0-next.2+sha-bacda4f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
