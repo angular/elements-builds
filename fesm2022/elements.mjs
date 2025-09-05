382d849be4d1609b142666dc1090fc841b351848
Andrew Scott <atscott@google.com>
2025-09-05 19:57:01 +0000

    c3303a5 test: increase mermaid spec timeout to reduce flakes (#63610)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f697995
+ * @license Angular v20.2.4+sha-c3303a5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f697995');
+const VERSION = new Version('20.2.4+sha-c3303a5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
