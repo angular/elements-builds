3c7aaa39ed53d4e34db7dd3c13f7150d73a18457
Andrew Scott <atscott@google.com>
2025-09-08 19:49:34 +0000

    a6ed784 build: properly override the repo for rules_angular (#63584)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-d124fba
+ * @license Angular v20.2.4+sha-a6ed784
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-d124fba');
+const VERSION = new Version('20.2.4+sha-a6ed784');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
