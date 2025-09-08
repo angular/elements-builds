c43cfb7daa5023d487547309d753658214e643e3
Andrew Scott <atscott@google.com>
2025-09-08 23:35:12 +0000

    c7b84f5 docs: Update Language Service IDE list (#63647)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a857f72
+ * @license Angular v20.2.4+sha-c7b84f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a857f72');
+const VERSION = new Version('20.2.4+sha-c7b84f5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
