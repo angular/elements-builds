e04b8841d5c3ae286ad234c577c937b5961ba26b
Andrew Scott <atscott@google.com>
2025-09-08 21:56:50 +0000

    efb9474 docs: should not conditionally include `<ng-content>` in IMPORTANT block (#63580)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c8c4399
+ * @license Angular v21.0.0-next.2+sha-efb9474
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c8c4399');
+const VERSION = new Version('21.0.0-next.2+sha-efb9474');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
