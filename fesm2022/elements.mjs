108fcbc30c42c4bbd232abb9f44540f266dd95a0
Andrew Scott <atscott@google.com>
2025-09-08 19:13:54 +0000

    1f21de8 docs: improve headings of docs (#63604)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-160e065
+ * @license Angular v21.0.0-next.2+sha-1f21de8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-160e065');
+const VERSION = new Version('21.0.0-next.2+sha-1f21de8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
