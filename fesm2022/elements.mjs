cb2f73c8bcf6ce98bdbb7f661d24d2702fe6ee6b
Andrew Scott <atscott@google.com>
2025-09-09 20:00:57 +0000

    9ffc83e docs: add signals tutorial (#62750)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-6d12f10
+ * @license Angular v21.0.0-next.2+sha-9ffc83e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-6d12f10');
+const VERSION = new Version('21.0.0-next.2+sha-9ffc83e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
