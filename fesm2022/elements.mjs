f4d896099aa8a7b669f37ccf8ba71ce6f2eea374
Andrew Scott <atscott@google.com>
2025-09-09 18:02:34 +0000

    28926ba feat(core): introduce `BootstrapContext` for improved server bootstrapping (#63562)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-3dc5056
+ * @license Angular v21.0.0-next.2+sha-28926ba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-3dc5056');
+const VERSION = new Version('21.0.0-next.2+sha-28926ba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
