a639680ba13421a594a539d4587aa2c2788607f8
Andrew Scott <atscott@google.com>
2025-09-09 21:33:01 +0000

    1e614ec docs: add missing component harness docs (#63620)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-9ffc83e
+ * @license Angular v21.0.0-next.2+sha-1e614ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-9ffc83e');
+const VERSION = new Version('21.0.0-next.2+sha-1e614ec');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
