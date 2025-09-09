4da993f91c549b5fc1d5a1a312afca8bc8062a2d
Andrew Scott <atscott@google.com>
2025-09-09 22:49:12 +0000

    97d3615 build: create a script and ci setup for managing and uploading known synonyms for algolia (#63663)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c357650
+ * @license Angular v21.0.0-next.2+sha-97d3615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c357650');
+const VERSION = new Version('21.0.0-next.2+sha-97d3615');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
