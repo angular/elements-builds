47d479362c04345bcc40ae4f75ff699ada2676d8
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-09 06:42:14 +0000

    3dc5056 Revert "test(devtools): create e2e test cases for injector tree feature (#62844)" (#63653)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-17d5479
+ * @license Angular v21.0.0-next.2+sha-3dc5056
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-17d5479');
+const VERSION = new Version('21.0.0-next.2+sha-3dc5056');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
