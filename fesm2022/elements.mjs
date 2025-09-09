e9f1f5c0b230b54e98e7538c7d4fd2db4221017d
Andrew Scott <atscott@google.com>
2025-09-09 19:17:17 +0000

    bf0f257 ci: update renovate base branch to 20.3.x (#63666)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-dce31fb
+ * @license Angular v21.0.0-next.2+sha-bf0f257
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-dce31fb');
+const VERSION = new Version('21.0.0-next.2+sha-bf0f257');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
