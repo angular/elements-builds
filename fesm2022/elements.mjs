7ef592e0eb95f2aecf1a4e37a054f9a1f0d06ea2
Andrew Scott <atscott@google.com>
2025-09-09 19:13:27 +0000

    dce31fb docs: add missing drag & drop docs (#63619)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-28926ba
+ * @license Angular v21.0.0-next.2+sha-dce31fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-28926ba');
+const VERSION = new Version('21.0.0-next.2+sha-dce31fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
