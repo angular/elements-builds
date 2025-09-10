ff2fee8eda184f6040185aa5b0ac2e3e28f650a4
Andrew Scott <atscott@google.com>
2025-09-10 20:33:47 +0000

    fb3082f build: fix symbol tests (#63709)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-f642fcf
+ * @license Angular v21.0.0-next.3+sha-fb3082f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-f642fcf');
+const VERSION = new Version('21.0.0-next.3+sha-fb3082f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
