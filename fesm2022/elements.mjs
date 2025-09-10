45e549e58af021725ea53274e9b50dc5981ed857
Andrew Scott <atscott@google.com>
2025-09-10 17:13:05 +0000

    802dbcc fix(core): prevent animation events from being cleaned up on destroy (#63414)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-9096d45
+ * @license Angular v20.3.0+sha-802dbcc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-9096d45');
+const VERSION = new Version('20.3.0+sha-802dbcc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
