786f6437785a31cd711dab89a78e0feb5dd6c3ef
Andrew Scott <atscott01@gmail.com>
2025-09-10 15:11:36 +0000

    9586d48 docs: release notes for the v20.3.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-0102575
+ * @license Angular v21.0.0-next.2+sha-9586d48
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-0102575');
+const VERSION = new Version('21.0.0-next.2+sha-9586d48');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
