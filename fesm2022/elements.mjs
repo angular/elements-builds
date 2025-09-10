40b2e7654c38155f5d02fa830d5aa282f5bb865b
Andrew Scott <atscott01@gmail.com>
2025-09-10 18:54:47 +0000

    98db109 release: cut the v18.2.14 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.13+sha-1d86de1
+ * @license Angular v18.2.14+sha-98db109
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.13+sha-1d86de1');
+const VERSION = new Version('18.2.14+sha-98db109');
 
 /**
  * @module
