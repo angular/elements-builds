a9248ca35c7edb7075fa0583b815237a10eb293e
Andrew Scott <atscott@google.com>
2025-09-10 18:48:09 +0000

    1d86de1 build: ignore node_modules (#63701)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.13+sha-08f01ac
+ * @license Angular v18.2.13+sha-1d86de1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -513,7 +513,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.13+sha-08f01ac');
+const VERSION = new Version('18.2.13+sha-1d86de1');
 
 /**
  * @module
