4323f396489b2102e2ae7ff610ae7d92554acb22
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:42:52 +0000

    b2c7979 build: update lock files (#63719)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-70d0639
+ * @license Angular v19.2.15+sha-b2c7979
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.14+sha-70d0639');
+const VERSION = new Version('19.2.15+sha-b2c7979');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
