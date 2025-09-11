208fb14d95f9716c0b15e1059ab44a8d39653267
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:39:49 +0000

    37e1eda build: mark `pnpapi` as external for esbuild (#63723)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-cdf5d67
+ * @license Angular v21.0.0-next.3+sha-37e1eda
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-cdf5d67');
+const VERSION = new Version('21.0.0-next.3+sha-37e1eda');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
