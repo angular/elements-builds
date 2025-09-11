5ec6098676261513d7e1157545fff09a7b4641ec
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 16:59:20 +0000

    bd54106 refactor(router): update error checks extending error (#63487)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-a53a15c
+ * @license Angular v21.0.0-next.3+sha-bd54106
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-a53a15c');
+const VERSION = new Version('21.0.0-next.3+sha-bd54106');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
