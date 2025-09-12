4162cff061a3aff95cb79fbd0f9db6a2d8c02e03
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:54:41 +0000

    895f0fb docs: update Creating service with CLI (#63714)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-3ec8a5c
+ * @license Angular v20.3.0+sha-895f0fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-3ec8a5c');
+const VERSION = new Version('20.3.0+sha-895f0fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
