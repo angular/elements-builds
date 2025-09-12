6ff1ead44adc4df0df565a9dc58ec3dc77ea92fe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:53:02 +0000

    8449001 docs: update signal-queries.md (#63726) PR Close #63726

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-9de50b1
+ * @license Angular v20.3.0+sha-8449001
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-9de50b1');
+const VERSION = new Version('20.3.0+sha-8449001');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
