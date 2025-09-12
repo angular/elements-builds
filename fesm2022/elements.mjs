5e40055fac73981c1c47b218b83896b6a2ff3c5c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:48:59 +0000

    85994fb build: update branches and versioning information (#63772)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-62ad4db
+ * @license Angular v21.0.0-next.3+sha-85994fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-62ad4db');
+const VERSION = new Version('21.0.0-next.3+sha-85994fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
