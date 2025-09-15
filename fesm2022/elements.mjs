b9b5bba1cecd9155a782a6b93f4d633cbc43e2a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:07:05 +0000

    2b440f4 docs: correct typos in AI design patterns guide (#63788)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-2327c3b
+ * @license Angular v21.0.0-next.3+sha-2b440f4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-2327c3b');
+const VERSION = new Version('21.0.0-next.3+sha-2b440f4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
