a1745be30d3a26b1b1ad07dac1e2d85063f01d25
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:55:52 +0000

    0a60e35 fix(compiler-cli): signal not invoked diagnostic not raised when input has same name in template (#63754)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-cfe0f03
+ * @license Angular v21.0.0-next.3+sha-0a60e35
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-cfe0f03');
+const VERSION = new Version('21.0.0-next.3+sha-0a60e35');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
