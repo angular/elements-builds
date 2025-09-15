9a9fbc19cc71d3221f7ad73d09504026f2ec1c9d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 21:16:19 +0000

    7e0cf41 refactor(core): wrap `checkNoChanges` body in `if (ngDevMode)` for better tree-shaking (#63387)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-321eecc
+ * @license Angular v21.0.0-next.3+sha-7e0cf41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-321eecc');
+const VERSION = new Version('21.0.0-next.3+sha-7e0cf41');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
