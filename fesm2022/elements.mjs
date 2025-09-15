bb622e9d7ae2634f93e95df838ed36290bb1ac0f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:38:23 +0000

    653089d test(platform-server): add test for transfer state data in nested defer blocks (#63801)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-13e4255
+ * @license Angular v20.3.0+sha-653089d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-13e4255');
+const VERSION = new Version('20.3.0+sha-653089d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
