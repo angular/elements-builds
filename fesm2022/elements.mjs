01a9355668105bc862798a02cf3fcab924fb3baf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:46:40 +0000

    803dc8e fix(compiler): Add support for `aria-invalid` (#63748)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-a5e5dbb
+ * @license Angular v21.0.0-next.3+sha-803dc8e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-a5e5dbb');
+const VERSION = new Version('21.0.0-next.3+sha-803dc8e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
