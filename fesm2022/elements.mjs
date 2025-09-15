290ff29bb35daeb7a08673734dd8ded2cfcaba97
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:29:18 +0000

    13e4255 fix(docs-infra): fix guide content width (#63798)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-c536035
+ * @license Angular v20.3.0+sha-13e4255
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-c536035');
+const VERSION = new Version('20.3.0+sha-13e4255');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
