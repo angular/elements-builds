ccf96f682437a81b12bfdee083ed6fdc31bd9d0f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:45:20 +0000

    cfe0f03 refactor(devtools): add a close button to the injector-provides pane (#63808)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d2163f3
+ * @license Angular v21.0.0-next.3+sha-cfe0f03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-d2163f3');
+const VERSION = new Version('21.0.0-next.3+sha-cfe0f03');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
