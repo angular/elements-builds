04db799ba8d77fc2398fa0318021f12deef0c8f0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:29:32 +0000

    a5e5dbb refactor(core): remove `moduleId` from Component metadata (#63482)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-2473931
+ * @license Angular v21.0.0-next.3+sha-a5e5dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-2473931');
+const VERSION = new Version('21.0.0-next.3+sha-a5e5dbb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
