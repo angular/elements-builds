51663ce7377673000bbd6f6dc0871f4bd47ee795
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 20:39:54 +0000

    321eecc docs(benchpress): Remove TODOs in Statistic (#63534)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-8dc8914
+ * @license Angular v21.0.0-next.3+sha-321eecc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-8dc8914');
+const VERSION = new Version('21.0.0-next.3+sha-321eecc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
