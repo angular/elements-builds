0d8f2fc0101ca8cd7ce0b2ba09148dfa369b794d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 16:25:20 +0000

    049b672 docs(docs-infra): new home page (#63662)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-6e54bdf
+ * @license Angular v20.3.0+sha-049b672
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-6e54bdf');
+const VERSION = new Version('20.3.0+sha-049b672');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
