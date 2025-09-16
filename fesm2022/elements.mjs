6947d84d2d4062d5f273f3f092df473d070a55af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 20:52:17 +0000

    1fec2a2 build: remove bazel flag usage for adev (#63845)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-1352fbd
+ * @license Angular v21.0.0-next.3+sha-1fec2a2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-1352fbd');
+const VERSION = new Version('21.0.0-next.3+sha-1fec2a2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
