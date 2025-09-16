1172b21b047442ba071762e51e3d2a0835125bb7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:57:38 +0000

    7eb5f9a docs: remove leading `$` from script examples (#63795)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-4a23f6e
+ * @license Angular v20.3.0+sha-7eb5f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-4a23f6e');
+const VERSION = new Version('20.3.0+sha-7eb5f9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
