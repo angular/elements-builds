2112cc42bf3546a3d04274417d6ed5eea53b3b13
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:21:56 +0000

    444fd01 docs(docs-infra): Use port 4201 for adev (#63834)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-27b2a4a
+ * @license Angular v20.3.0+sha-444fd01
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-27b2a4a');
+const VERSION = new Version('20.3.0+sha-444fd01');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
