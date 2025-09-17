f4db02a56e79cb311638647e7e93463b0d588e25
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 16:19:35 +0000

    32c98e0 docs: add regular expressions to table of supported literals (#63857)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-af33647
+ * @license Angular v21.0.0-next.3+sha-32c98e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-af33647');
+const VERSION = new Version('21.0.0-next.3+sha-32c98e0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
