aa7af295bc7382a83768dfb770b5964acd36cfaa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:40:21 +0000

    36b542d Revert "build: update dependency @types/node to v24.5.1 (#63853)" (#63878)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-32c98e0
+ * @license Angular v21.0.0-next.3+sha-36b542d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-32c98e0');
+const VERSION = new Version('21.0.0-next.3+sha-36b542d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
