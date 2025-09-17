e12308c2416f189c5f4ddb3a28fafd3a09dfa3f0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 19:39:29 +0000

    a53ef1d Revert "refactor(compiler): tokenize regular expression literals (#63857)" (#63883)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-7d3919e
+ * @license Angular v21.0.0-next.3+sha-a53ef1d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-7d3919e');
+const VERSION = new Version('21.0.0-next.3+sha-a53ef1d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
