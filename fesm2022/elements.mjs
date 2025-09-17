d405edeed855f11b98299744f8544910bb2132b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 14:56:24 +0000

    5f41f14 docs: fix pluralization error in templates guide (#63854)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5fa2c05
+ * @license Angular v21.0.0-next.3+sha-5f41f14
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-5fa2c05');
+const VERSION = new Version('21.0.0-next.3+sha-5f41f14');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
