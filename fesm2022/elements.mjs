e70d730b79bde2b8256c3c8b246bbf0464f3f9eb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:27:14 +0000

    af33647 refactor(core): remove usages of Promise.withResolvers (#63856)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-e523384
+ * @license Angular v21.0.0-next.3+sha-af33647
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-e523384');
+const VERSION = new Version('21.0.0-next.3+sha-af33647');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
