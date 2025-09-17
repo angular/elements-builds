606578d80c8c031b7fcd3a4d8342217e2f3294c4
Jan Martin <jan.krems@gmail.com>
2025-09-17 18:00:59 +0000

    7d3919e release: bump Angular DevTools version to 1.2.1 (#63879)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-dee6ef8
+ * @license Angular v21.0.0-next.3+sha-7d3919e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-dee6ef8');
+const VERSION = new Version('21.0.0-next.3+sha-7d3919e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
