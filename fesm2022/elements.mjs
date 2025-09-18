5ea610c0140a2e88868e5a490190b0de5680f3bc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 15:03:06 +0000

    5cb1bbb build: update rules_sass digest to 4a54e0e (#63898)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-950ffd0
+ * @license Angular v21.0.0-next.4+sha-5cb1bbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-950ffd0');
+const VERSION = new Version('21.0.0-next.4+sha-5cb1bbb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
