a1be2f32e909fdf06c523a218f0ab0554158a1b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 22:16:58 +0000

    6782f71 docs: remove api grouping toggle (#63961)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-40ae5af
+ * @license Angular v21.0.0-next.4+sha-6782f71
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-40ae5af');
+const VERSION = new Version('21.0.0-next.4+sha-6782f71');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
