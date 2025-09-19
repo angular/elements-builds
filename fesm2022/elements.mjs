266ad97c71ffd9cede47db8c25cd22fa882ddf29
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:57:35 +0000

    8891ee4 build: update dependency concurrently to v9 (#63918)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-4328ea8
+ * @license Angular v21.0.0-next.4+sha-8891ee4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-4328ea8');
+const VERSION = new Version('21.0.0-next.4+sha-8891ee4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
