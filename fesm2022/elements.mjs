0b357e954b33ca0bfd0425f06a1161003713323e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:45:06 +0000

    a2d98cc docs: add missing `signal` function (#63974)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-6782f71
+ * @license Angular v21.0.0-next.4+sha-a2d98cc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-6782f71');
+const VERSION = new Version('21.0.0-next.4+sha-a2d98cc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
