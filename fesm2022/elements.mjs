25485ea92cf5e04961a8aa40ffa785cebbae4f61
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:58:44 +0000

    5406e1a refactor(core): use version>0 instead of hasRun (#62467)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-3bd91de
+ * @license Angular v21.0.0-next.4+sha-5406e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-3bd91de');
+const VERSION = new Version('21.0.0-next.4+sha-5406e1a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
