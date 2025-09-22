62452c5f12b4827e4f80405ed0f9d6777a65c657
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:02:08 +0000

    1d4f81c fix(compiler-cli): resolve import alias in defer blocks (#63966)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-2d21524
+ * @license Angular v20.3.1+sha-1d4f81c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-2d21524');
+const VERSION = new Version('20.3.1+sha-1d4f81c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
