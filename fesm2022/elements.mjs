5ae1e1961a039fa0504b431972a81d884d75bf91
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 14:25:56 +0000

    9515a70 fix(core): fix narrowing of `Resource.hasValue()` (#63994)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-2d07610
+ * @license Angular v20.3.1+sha-9515a70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-2d07610');
+const VERSION = new Version('20.3.1+sha-9515a70');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
