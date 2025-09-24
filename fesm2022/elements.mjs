f1886f3fe570aa78fc43087d3313d6403f3a10be
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:52:15 +0000

    c0d88c3 fix(forms): Emit `FormResetEvent` when resetting control (#64024)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-fffc7cf
+ * @license Angular v21.0.0-next.4+sha-c0d88c3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-fffc7cf');
+const VERSION = new Version('21.0.0-next.4+sha-c0d88c3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
