4c6edba817a4b5b09f2fb7309d63bd629a3b1ed9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:29:28 +0000

    521c2a7 feat(devtools): Change lazy/eager visualization by using dashed lines and correctly positioned edge arrows (#63980)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-f36a504
+ * @license Angular v21.0.0-next.4+sha-521c2a7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-f36a504');
+const VERSION = new Version('21.0.0-next.4+sha-521c2a7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
