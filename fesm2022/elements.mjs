cb2ed726feefbd63c17f16db3394eb7ec2984c8f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-24 23:30:44 +0000

    0e8e7ba release: cut the v21.0.0-next.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-b1d9373
+ * @license Angular v21.0.0-next.5+sha-0e8e7ba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-b1d9373');
+const VERSION = new Version('21.0.0-next.5+sha-0e8e7ba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
