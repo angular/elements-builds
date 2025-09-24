e08255b950c745d7cb73e73196b9761ec0228ba4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 18:35:06 +0000

    0b2018d docs(docs-infra): align docs-step number to the heading (#64035)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-c0d88c3
+ * @license Angular v21.0.0-next.4+sha-0b2018d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-c0d88c3');
+const VERSION = new Version('21.0.0-next.4+sha-0b2018d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
