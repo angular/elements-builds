8cc57f650af857610621554fa7791d2a802b97ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 20:28:29 +0000

    863c7ea build: migrate vscode extension into repo (#63924)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-0b2018d
+ * @license Angular v21.0.0-next.4+sha-863c7ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-0b2018d');
+const VERSION = new Version('21.0.0-next.4+sha-863c7ea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
