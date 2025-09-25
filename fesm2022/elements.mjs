22748001d7fa18c0f0b2f69816ce1d989722bf23
kirjs <kirts@google.com>
2025-09-25 18:39:25 +0000

    497dce9 ci: update pullapprove config (#64086)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-d7dead3
+ * @license Angular v21.0.0-next.5+sha-497dce9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-d7dead3');
+const VERSION = new Version('21.0.0-next.5+sha-497dce9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
