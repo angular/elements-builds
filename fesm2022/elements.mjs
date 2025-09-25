6e0a3a0b46f455928076afe84d5d9372dec165b1
kirjs <kirts@google.com>
2025-09-25 20:23:24 +0000

    6fee508 refactor: break circular dependency in integration tests (#64091)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-3d2e1a6
+ * @license Angular v21.0.0-next.5+sha-6fee508
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-3d2e1a6');
+const VERSION = new Version('21.0.0-next.5+sha-6fee508');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
