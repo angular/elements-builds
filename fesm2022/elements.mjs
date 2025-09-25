feb075c1d32aa2d881cee784d46d0e80b8ba92d7
kirjs <kirts@google.com>
2025-09-25 14:30:07 +0000

    95fa943 refactor: update dependencies and build configurations (#64073)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0e8e7ba');
+const VERSION = new Version('21.0.0-next.5+sha-95fa943');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
