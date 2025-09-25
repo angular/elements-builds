93eedf183d7b0961e426b5e01728406aa0565759
kirjs <kirts@google.com>
2025-09-25 19:14:58 +0000

    566077f build: update all github actions (#64079)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-a45e6fd
+ * @license Angular v21.0.0-next.5+sha-566077f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-a45e6fd');
+const VERSION = new Version('21.0.0-next.5+sha-566077f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
