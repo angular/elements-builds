dd991fad8723a2e2fe2ca36a533bdbeae0568bf4
kirjs <kirts@google.com>
2025-09-25 19:07:05 +0000

    a45e6fd fix(devtools): signal graph effect node (#64076)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-497dce9
+ * @license Angular v21.0.0-next.5+sha-a45e6fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-497dce9');
+const VERSION = new Version('21.0.0-next.5+sha-a45e6fd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
