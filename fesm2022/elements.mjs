d30a9681bcc132625f54e336e1cad0fc50a0b869
kirjs <kirts@google.com>
2025-09-26 15:59:45 +0000

    8a1e36b build: update dependency @lezer/html to v1.3.11 (#64097)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-238ccc7
+ * @license Angular v21.0.0-next.5+sha-8a1e36b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-238ccc7');
+const VERSION = new Version('21.0.0-next.5+sha-8a1e36b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
