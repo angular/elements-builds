d40eb504329091f73da7706d2fafaa205077614e
kirjs <kirts@google.com>
2025-09-26 13:41:18 +0000

    597d6e7 docs: add StateOfJS survey banner (#64083)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-8ff536b
+ * @license Angular v21.0.0-next.5+sha-597d6e7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-8ff536b');
+const VERSION = new Version('21.0.0-next.5+sha-597d6e7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
