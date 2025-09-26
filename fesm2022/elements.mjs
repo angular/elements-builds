c579c6c014a50b180401b312245382a6369fd470
kirjs <kirts@google.com>
2025-09-26 13:37:08 +0000

    8ff536b docs: update text (#64095)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-14d5b0e
+ * @license Angular v21.0.0-next.5+sha-8ff536b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-14d5b0e');
+const VERSION = new Version('21.0.0-next.5+sha-8ff536b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
