21be98542f02484a73ea90fb5a99bd2786fb51bb
kirjs <kirts@google.com>
2025-09-29 18:28:38 +0000

    fc643c9 build: adopt `moduleResolution: "bundler"` (#64125)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-377d1a0
+ * @license Angular v21.0.0-next.5+sha-fc643c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-377d1a0');
+const VERSION = new Version('21.0.0-next.5+sha-fc643c9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
