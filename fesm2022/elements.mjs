787e1fe63d8fc5d8f09d7f0d68c37d231f0e8d12
kirjs <kirts@google.com>
2025-09-29 13:28:20 +0000

    4952110 docs: remove note about node.js 17+ (#64129)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-c680927
+ * @license Angular v20.3.2+sha-4952110
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-c680927');
+const VERSION = new Version('20.3.2+sha-4952110');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
