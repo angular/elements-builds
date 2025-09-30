55de93fc32471c083a1a15d64066d5ea47e23942
kirjs <kirts@google.com>
2025-09-30 19:37:19 +0000

    77b6305 fix(migrations): skip migration for inputs with 'this' references (#64142)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-612ce99
+ * @license Angular v20.3.2+sha-77b6305
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-612ce99');
+const VERSION = new Version('20.3.2+sha-77b6305');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
