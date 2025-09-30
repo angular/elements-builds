edd59677b8d61505125a61c7a34b5a9a2a11d5a9
kirjs <kirts@google.com>
2025-09-30 15:09:03 +0000

    563bc38 docs(docs-infra): fix app-scroller offset. (#64144)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-a10eb38
+ * @license Angular v21.0.0-next.5+sha-563bc38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-a10eb38');
+const VERSION = new Version('21.0.0-next.5+sha-563bc38');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
