0be677d8a697ac659c873f9edab4bd2c0eebd903
kirjs <kirts@google.com>
2025-09-30 17:55:03 +0000

    8e80083 refactor(core): Add custom formatters for Signals (#64000)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-c1a922a
+ * @license Angular v20.3.2+sha-8e80083
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-c1a922a');
+const VERSION = new Version('20.3.2+sha-8e80083');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
