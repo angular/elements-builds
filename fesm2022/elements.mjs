a014b04d1e5c1059584386bd5c03afc8eda8cedb
kirjs <kirts@google.com>
2025-10-01 17:26:14 +0000

    61624fd refactor(common): update NgIf, ngComponentOutlet and ngTemplateOutlet examples and remove redundant standalone flag (#64155)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-ee7d49a
+ * @license Angular v21.0.0-next.5+sha-61624fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-ee7d49a');
+const VERSION = new Version('21.0.0-next.5+sha-61624fd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
