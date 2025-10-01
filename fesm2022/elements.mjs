58faa32078e509dab0cc8ed54e00a7ca45dc8b28
kirjs <kirts@google.com>
2025-10-01 17:17:30 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0537083
+ * @license Angular v21.0.0-next.5+sha-ee7d49a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0537083');
+const VERSION = new Version('21.0.0-next.5+sha-ee7d49a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
