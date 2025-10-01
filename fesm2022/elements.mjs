acb69dee324d05118db0d7183f9384ef6fd2eb75
kirjs <kirts@google.com>
2025-10-01 16:48:49 +0000

    0537083 Revert "feat(common):  Support of optional keys for the KeyValue pipe (#48814)" (#64179)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1b102c2
+ * @license Angular v21.0.0-next.5+sha-0537083
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-1b102c2');
+const VERSION = new Version('21.0.0-next.5+sha-0537083');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
