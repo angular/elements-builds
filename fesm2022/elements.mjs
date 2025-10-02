34eda61d65a338f21cfb0f0929e6079b595b5980
Miles Malerba <mmalerba@google.com>
2025-10-02 17:02:13 +0000

    6fed986 fix(core): Fixes animations in conjunction with content projection (#63776)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-76fe559
+ * @license Angular v20.3.3+sha-6fed986
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-76fe559');
+const VERSION = new Version('20.3.3+sha-6fed986');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
