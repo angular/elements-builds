d1b067eb3e512667001a489ca3644819c5c6682b
Miles Malerba <mmalerba@google.com>
2025-10-02 20:59:28 +0000

    d3e76b1 fix(migrations): preserve component imports when pruning NgModules in standalone migration (#64186)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e23815b
+ * @license Angular v21.0.0-next.6+sha-d3e76b1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-e23815b');
+const VERSION = new Version('21.0.0-next.6+sha-d3e76b1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
