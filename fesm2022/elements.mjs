3b5a215f5d233bc7b650c2b6c6888473b2f7f74f
Miles Malerba <mmalerba@google.com>
2025-10-02 15:03:28 +0000

    595c668 docs: update website URL in license banners  (#64183)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,6 +1,6 @@
 /**
- * @license Angular v21.0.0-next.6+sha-6169fa5
- * (c) 2010-2025 Google LLC. https://angular.io/
+ * @license Angular v21.0.0-next.6+sha-595c668
+ * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-6169fa5');
+const VERSION = new Version('21.0.0-next.6+sha-595c668');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
