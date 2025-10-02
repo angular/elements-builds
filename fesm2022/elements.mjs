2431967c7ec56944a1c3d3a486d9acf9aaf77a2d
Kirill Cherkashin <kirill@kcherkashin.org>
2025-10-02 12:11:58 +0000

    4699ccf docs: release notes for the v20.3.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-7bc4ec4
+ * @license Angular v21.0.0-next.5+sha-4699ccf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-7bc4ec4');
+const VERSION = new Version('21.0.0-next.5+sha-4699ccf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
