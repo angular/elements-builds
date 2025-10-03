662285b47ab25ae0494fb4cd9b7bc8d690051edd
Miles Malerba <mmalerba@google.com>
2025-10-03 17:22:43 +0000

    ab780a7 docs: add info about disabling request caching (#64216)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e403078
+ * @license Angular v21.0.0-next.6+sha-ab780a7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-e403078');
+const VERSION = new Version('21.0.0-next.6+sha-ab780a7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
