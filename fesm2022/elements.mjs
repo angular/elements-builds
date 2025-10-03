54c0263841446f38ca3107f59e7f9993a9206dba
Miles Malerba <mmalerba@google.com>
2025-10-03 15:09:49 +0000

    e403078 refactor(devtools): visual separation between directives in the property tab (#64222)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e0b9d9b
+ * @license Angular v21.0.0-next.6+sha-e403078
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-e0b9d9b');
+const VERSION = new Version('21.0.0-next.6+sha-e403078');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
