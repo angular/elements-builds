2e917dbd80c04e9e426521e2ba9b459423b0403e
Miles Malerba <mmalerba@google.com>
2025-10-04 20:28:09 +0000

    effccff refactor(core): add framework support for binding form controls (#63773)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-548ea02
+ * @license Angular v21.0.0-next.6+sha-effccff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-548ea02');
+const VERSION = new Version('21.0.0-next.6+sha-effccff');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
