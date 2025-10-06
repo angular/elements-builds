8fb57704dc4c8d493fdc22b908506ef852c9b7dd
Miles Malerba <mmalerba@google.com>
2025-10-06 19:24:13 +0000

    c63de70 docs: add documentation for routerOutletData input and its usage (#64242)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-8528bb5
+ * @license Angular v20.3.3+sha-c63de70
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-8528bb5');
+const VERSION = new Version('20.3.3+sha-c63de70');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
