833d41b81220c499eef93adb84626402b281c4e8
Miles Malerba <mmalerba@google.com>
2025-10-06 19:26:51 +0000

    470dca1 build: lock file maintenance (#64252)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-1ffc643
+ * @license Angular v21.0.0-next.6+sha-470dca1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-1ffc643');
+const VERSION = new Version('21.0.0-next.6+sha-470dca1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
