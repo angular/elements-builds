de7650481d0133100f1363d3ce023876c8a42892
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 08:23:04 +0000

    47b66b1 release: bump Angular DevTools version to 1.4.0 (#64287)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5cbdefc
+ * @license Angular v21.0.0-next.6+sha-47b66b1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-5cbdefc');
+const VERSION = new Version('21.0.0-next.6+sha-47b66b1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
