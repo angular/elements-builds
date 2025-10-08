484d6d6919f3de40e030e1168b21c7f1402e7300
Miles Malerba <mmalerba@google.com>
2025-10-08 00:28:21 +0000

    a15688c build: update pnpm to v10.18.1 (#64264)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ac0eb23
+ * @license Angular v21.0.0-next.6+sha-a15688c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-ac0eb23');
+const VERSION = new Version('21.0.0-next.6+sha-a15688c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
