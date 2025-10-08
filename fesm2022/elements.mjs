6beea5fdaf05e73551d11a3581ff62495b1b0721
Miles Malerba <mmalerba@google.com>
2025-10-08 15:02:53 +0000

    808d6ae release: cut the v21.0.0-next.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ec4775d
+ * @license Angular v21.0.0-next.7+sha-808d6ae
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-ec4775d');
+const VERSION = new Version('21.0.0-next.7+sha-808d6ae');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
