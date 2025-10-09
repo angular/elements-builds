bb7f15c8107b14b66d284b177b5f3a4d77d43bc5
Andrew Kushnir <akushnir@google.com>
2025-10-09 18:16:48 +0000

    6e4bcc7 fix(router): Scroll restoration should use instant scroll behavior for traversals (#64299)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-fa25dad
+ * @license Angular v20.3.4+sha-6e4bcc7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-fa25dad');
+const VERSION = new Version('20.3.4+sha-6e4bcc7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
