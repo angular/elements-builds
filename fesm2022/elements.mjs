4996de05176e67b7892d5ae12954d5328afa1fce
Andrew Kushnir <akushnir@google.com>
2025-10-09 12:29:39 +0000

    f521c1f build: update github/codeql-action action to v3.30.7 (#64284)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e873c22
+ * @license Angular v21.0.0-next.7+sha-f521c1f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-e873c22');
+const VERSION = new Version('21.0.0-next.7+sha-f521c1f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
