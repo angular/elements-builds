b437adf9e65b69e54e1f3baa25c1ad6a818ee373
Andrew Kushnir <akushnir@google.com>
2025-10-09 17:05:11 +0000

    9af3cf7 build: migrate to in-repo ts_project with strict deps (#64306)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-22a1001
+ * @license Angular v21.0.0-next.7+sha-9af3cf7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-22a1001');
+const VERSION = new Version('21.0.0-next.7+sha-9af3cf7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
