6e2afe38bf3ca3638da652f1214c9184234aa2f2
Andrew Kushnir <akushnir@google.com>
2025-10-10 13:57:19 +0000

    249fbb3 docs: add example for assertInInjectionContext (#64325)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-d73d9ac
+ * @license Angular v21.0.0-next.7+sha-249fbb3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-d73d9ac');
+const VERSION = new Version('21.0.0-next.7+sha-249fbb3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
