62c827ab88bc410dabc549854085230ea8fb4f38
Andrew Kushnir <akushnir@google.com>
2025-10-10 15:17:24 +0000

    da50733 docs: add a callout that adding/removing to formArray doesn't not mark dirty (#64337)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-da38cd9
+ * @license Angular v20.3.4+sha-da50733
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-da38cd9');
+const VERSION = new Version('20.3.4+sha-da50733');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
