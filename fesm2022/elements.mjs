6ad4cdb25d40e5f1be1b9223417bfffe6e45028a
Andrew Kushnir <akushnir@google.com>
2025-10-13 18:16:44 +0000

    eb80c30 build: update symbol extractor tests to stabalize the order of the symbols listed (#64389)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-eee8eab
+ * @license Angular v21.0.0-next.7+sha-eb80c30
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-eee8eab');
+const VERSION = new Version('21.0.0-next.7+sha-eb80c30');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
