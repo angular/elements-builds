254502384d4f8086c960cb5522b0447122d98393
Andrew Kushnir <akushnir@google.com>
2025-10-13 15:48:30 +0000

    9cbe5b6 build: update pnpm to v10.18.2 (#64359)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-da50733
+ * @license Angular v20.3.4+sha-9cbe5b6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-da50733');
+const VERSION = new Version('20.3.4+sha-9cbe5b6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
