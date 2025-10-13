4bf3337f88671308deb1356c7cf42c76b9099a49
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:01:47 +0000

    c0510cd docs: Removes standalone flag from drag-drop examples (#64395)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-2a7a5de
+ * @license Angular v21.0.0-next.7+sha-c0510cd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-2a7a5de');
+const VERSION = new Version('21.0.0-next.7+sha-c0510cd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
