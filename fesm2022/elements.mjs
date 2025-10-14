eda25cdb9c60f33c362171d1372c17bfb3bbbb1e
Andrew Kushnir <akushnir@google.com>
2025-10-14 19:04:16 +0000

    d03970f refactor(compiler-cli): remove `canVisitStructuralAttributes` from `TemplateCheckWithVisitor` (#63809)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e941e6b
+ * @license Angular v21.0.0-next.7+sha-d03970f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-e941e6b');
+const VERSION = new Version('21.0.0-next.7+sha-d03970f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
