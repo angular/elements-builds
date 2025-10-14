4ec579ccb8ddb0a823f46e1bef4c41de5f833ee7
Andrew Kushnir <akushnir@google.com>
2025-10-14 21:33:32 +0000

    5f4f624 Revert "refactor(core): add debug name to resource (#64172)" (#64418)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-c2e817b
+ * @license Angular v20.3.4+sha-5f4f624
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-c2e817b');
+const VERSION = new Version('20.3.4+sha-5f4f624');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
