b375692fe3e1e042c52e7ef9f4bef7634b4273f8
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:11:17 +0000

    6a7c27e release: cut the v20.3.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-07d8c60
+ * @license Angular v20.3.5+sha-6a7c27e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-07d8c60');
+const VERSION = new Version('20.3.5+sha-6a7c27e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
