87a431be68649584e9092caef92f8161a0b68842
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-15 21:17:30 +0000

    2ece428 fix(platform-browser): `DomEventsPlugin` should always be the last plugin to be called for `supports()`. (#50394)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-e6c0223
+ * @license Angular v20.3.5+sha-2ece428
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-e6c0223');
+const VERSION = new Version('20.3.5+sha-2ece428');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
