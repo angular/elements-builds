9795c0a6270adf6d928f480dcd844524c52ef0da
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:19:54 +0000

    013effd release: cut the v21.0.0-next.8 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e3fa24d
+ * @license Angular v21.0.0-next.8+sha-013effd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-e3fa24d');
+const VERSION = new Version('21.0.0-next.8+sha-013effd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
