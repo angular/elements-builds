d139b8a2b90e916a3af16d13b5be3bebaa92a6fc
Andrew Kushnir <akushnir@google.com>
2025-10-15 18:51:15 +0000

    e6c0223 docs: update Angular CLI help (#64434)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-43fe755
+ * @license Angular v20.3.5+sha-e6c0223
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-43fe755');
+const VERSION = new Version('20.3.5+sha-e6c0223');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
