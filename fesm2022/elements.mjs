c5cb6c2c51bf4692df1398d4144a6c2e2249631c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:58:36 +0000

    2d159af docs: Add docs for EVENT_MANAGER_PLUGINS (#64453)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-136e3de
+ * @license Angular v20.3.5+sha-2d159af
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-136e3de');
+const VERSION = new Version('20.3.5+sha-2d159af');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
