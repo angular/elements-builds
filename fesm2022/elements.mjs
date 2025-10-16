27095800d3ec1681d7b85358d343c9026fd1d959
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:58:16 +0000

    df780fa docs: Add docs for EVENT_MANAGER_PLUGINS (#64453)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-6af231e
+ * @license Angular v21.0.0-next.8+sha-df780fa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-6af231e');
+const VERSION = new Version('21.0.0-next.8+sha-df780fa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
