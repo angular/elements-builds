40e89a3f275d0312903a2c7eaca71b5196373994
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 20:05:19 +0000

    371274b fix(compiler-cli): missingStructuralDirective diagnostic produces false negatives (#64470)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-891f180
+ * @license Angular v20.3.6+sha-371274b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-891f180');
+const VERSION = new Version('20.3.6+sha-371274b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
