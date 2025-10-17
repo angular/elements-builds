8aa2a4d4b538db0d50c56a3cdfe3c8bc524b9a3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:49:33 +0000

    7d7b4be docs: add matrix parameters docs and examples (#64459)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-10aa4f9
+ * @license Angular v21.0.0-next.8+sha-7d7b4be
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-10aa4f9');
+const VERSION = new Version('21.0.0-next.8+sha-7d7b4be');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
