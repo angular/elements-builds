db9c16d102257345d99dd6987a0c9ed230753234
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 14:48:59 +0000

    3bb4b3d docs: add more details on critical inlining. (#64292)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-371274b
+ * @license Angular v20.3.6+sha-3bb4b3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-371274b');
+const VERSION = new Version('20.3.6+sha-3bb4b3d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
