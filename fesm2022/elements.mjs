39139e7a285e1bb93384e1e25782f685de5ceb24
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 14:54:39 +0000

    4073dc5 build: update rules_browsers digest to 0e0949d (#64537)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-3bb4b3d
+ * @license Angular v20.3.6+sha-4073dc5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-3bb4b3d');
+const VERSION = new Version('20.3.6+sha-4073dc5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
