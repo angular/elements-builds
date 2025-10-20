2e3d0ba5edf2c78560e77032815ac291ba39c94c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 18:46:19 +0000

    f6a73f1 fix(router): Respect custom `UrlSerializer` handling of query parameters (#64449)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-7fb7511
+ * @license Angular v21.0.0-next.8+sha-f6a73f1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-7fb7511');
+const VERSION = new Version('21.0.0-next.8+sha-f6a73f1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
