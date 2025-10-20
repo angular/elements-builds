3701cb49e34b0e640c4bfe025b6dcfbacafc94a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 17:59:56 +0000

    7fb7511 fix(router): Surface parse errors in Router.parseUrl (#64503)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-25d45ba
+ * @license Angular v21.0.0-next.8+sha-7fb7511
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-25d45ba');
+const VERSION = new Version('21.0.0-next.8+sha-7fb7511');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
