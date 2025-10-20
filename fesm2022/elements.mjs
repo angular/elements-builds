44d719b723f5d10c2f0b23eb5acc2239005d50e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 17:15:15 +0000

    25d45ba refactor(router): Use promise array for loadComponent (#64450)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-fad6e13
+ * @license Angular v21.0.0-next.8+sha-25d45ba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-fad6e13');
+const VERSION = new Version('21.0.0-next.8+sha-25d45ba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
