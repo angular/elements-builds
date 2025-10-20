5c92febc9db126320bffb613a791cf6d3f923215
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 15:44:15 +0000

    e464aac build: bump leftover dependency of typescript (#64543)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-9c7029c
+ * @license Angular v21.0.0-next.8+sha-e464aac
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-9c7029c');
+const VERSION = new Version('21.0.0-next.8+sha-e464aac');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
