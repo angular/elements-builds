f842f66be1833532f9595cf83b753cd2fd01b121
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 15:30:34 +0000

    196fa50 fix(common): properly type ngComponentOutlet (#64561)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-bc9c814
+ * @license Angular v21.0.0-next.8+sha-196fa50
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-bc9c814');
+const VERSION = new Version('21.0.0-next.8+sha-196fa50');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
