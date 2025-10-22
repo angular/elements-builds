7eb72dc510f914de1f859093ec71534933323eda
Andrew Scott <atscott01@gmail.com>
2025-10-22 23:30:25 +0000

    ced2fa5 refactor(zone.js): Improve missing proxy zone error for jest imported (#64497) test functions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f0debd5
+ * @license Angular v21.1.0-next.0+sha-ced2fa5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-f0debd5');
+const VERSION = new Version('21.1.0-next.0+sha-ced2fa5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
