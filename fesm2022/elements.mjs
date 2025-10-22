da38bcaf3d4aa1234745101fb81cf802863daa03
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 22:01:24 +0000

    de2ee6e docs: update Angular CDK apis (#64599)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-d183153
+ * @license Angular v21.0.0-next.9+sha-de2ee6e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-d183153');
+const VERSION = new Version('21.0.0-next.9+sha-de2ee6e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
