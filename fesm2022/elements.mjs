81522e8a467d90b94f5e37f9cf1eec362c6fc85a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 21:50:15 +0000

    f0debd5 build: update cross-repo angular dependencies (#64597)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-be1598f
+ * @license Angular v21.1.0-next.0+sha-f0debd5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-be1598f');
+const VERSION = new Version('21.1.0-next.0+sha-f0debd5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
