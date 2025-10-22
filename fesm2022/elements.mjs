78da626582785f20697a7cd5e31a5db89f400943
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:32:00 +0000

    a3e91b2 refactor(forms): Add a test better documenting behavior of the error function in built-in validators (#63993)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-e039c6b
+ * @license Angular v21.0.0-next.8+sha-a3e91b2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-e039c6b');
+const VERSION = new Version('21.0.0-next.8+sha-a3e91b2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
