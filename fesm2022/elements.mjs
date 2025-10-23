d6b935ae94e25760ce942c180fe8190621210b2c
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:16:59 +0000

    ef34e39 refactor(forms): rename files related to metadata (#64603)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6a3e881
+ * @license Angular v21.1.0-next.0+sha-ef34e39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-6a3e881');
+const VERSION = new Version('21.1.0-next.0+sha-ef34e39');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
