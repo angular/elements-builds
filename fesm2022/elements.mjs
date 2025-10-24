b8cf744190c69008a67b4b2a86ce712bf07646c2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 13:30:27 +0000

    1a0d2da build: update all non-major dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0d38cbf
+ * @license Angular v21.1.0-next.0+sha-1a0d2da
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-0d38cbf');
+const VERSION = new Version('21.1.0-next.0+sha-1a0d2da');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
