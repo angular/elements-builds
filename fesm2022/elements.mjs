2d1e6313962ca91e28e8f98bd8b096fef6a71037
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 17:06:33 +0000

    620e2c4 Revert "build: refactor location of best-practices.md"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-d7e5aea
+ * @license Angular v21.1.0-next.0+sha-620e2c4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-d7e5aea');
+const VERSION = new Version('21.1.0-next.0+sha-620e2c4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
