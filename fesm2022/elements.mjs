7b950d035f3fefd67045110493ba72d3904a741b
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 07:42:18 +0000

    0d38cbf docs: update Angular CDK apis (#64643)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e47ef3e
+ * @license Angular v21.1.0-next.0+sha-0d38cbf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-e47ef3e');
+const VERSION = new Version('21.1.0-next.0+sha-0d38cbf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
