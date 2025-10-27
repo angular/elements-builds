6c91ce72272751188f706627f1615d93e35192cf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:18:32 +0000

    a5678f6 refactor(forms): add onError callback to validateHttp for HTTP errors

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b478e91
+ * @license Angular v21.1.0-next.0+sha-a5678f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-b478e91');
+const VERSION = new Version('21.1.0-next.0+sha-a5678f6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
