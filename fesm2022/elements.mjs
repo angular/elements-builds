dc53ca3fcdd858632208eda47f8773cc00bf2df6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 12:47:49 +0000

    04dd75b fix(compiler): support arbitrary nesting in :host-context()

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-a1b4e38
+ * @license Angular v21.0.0-next.9+sha-04dd75b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -411,7 +411,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-a1b4e38');
+const VERSION = new Version('21.0.0-next.9+sha-04dd75b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
