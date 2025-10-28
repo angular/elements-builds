14ec1fabbcdfe08b5cfa6c68d9de44892476488d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 14:13:54 +0000

    3fbaaa0 refactor(http): add hooks for propagating traces across XHR callbacks.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-81b8765
+ * @license Angular v21.0.0-next.9+sha-3fbaaa0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-next.9+sha-81b8765');
+const VERSION = new Version('21.0.0-next.9+sha-3fbaaa0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
