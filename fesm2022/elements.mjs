631c20b07a817874020d2a5dc5b1a841be5d5f12
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 09:35:38 +0000

    81b8765 build: lock file maintenance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-54ad230
+ * @license Angular v21.0.0-next.9+sha-81b8765
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-next.9+sha-54ad230');
+const VERSION = new Version('21.0.0-next.9+sha-81b8765');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
