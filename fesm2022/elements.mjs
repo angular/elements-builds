0d9fac554c989be28e2f019e922f43aab15b4a4f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 20:02:13 +0000

    f80b51a refactor(forms): track arrays in a parent array by index

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5a93eeb
+ * @license Angular v21.1.0-next.0+sha-f80b51a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-5a93eeb');
+const VERSION = new Version('21.1.0-next.0+sha-f80b51a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
