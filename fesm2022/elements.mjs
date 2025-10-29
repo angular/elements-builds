c8e5e55fb95cdbe2b8bbcb0fde124461312fcacd
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 17:53:57 +0000

    2a06e2b docs: Rename TodoListItem to UserProfile and fix usage of `isValidUserId`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.10+sha-db72aef
+ * @license Angular v21.0.0-rc.0+sha-2a06e2b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-next.10+sha-db72aef');
+const VERSION = new Version('21.0.0-rc.0+sha-2a06e2b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
