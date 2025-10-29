47efca1e93aaf5d1125290cb3554231b8a6a26b2
Andrew Scott <atscott01@gmail.com>
2025-10-29 20:36:28 +0000

    76adbbc refactor(core): expose isWritableSignal to the publicApi

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-662f0e5
+ * @license Angular v21.1.0-next.0+sha-76adbbc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-662f0e5');
+const VERSION = new Version('21.1.0-next.0+sha-76adbbc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
