284e13ff2e1c40189aa3f4bcfb735350e1f0678b
Andrew Scott <atscott@google.com>
2025-10-29 20:37:08 +0000

    20b4b2c refactor(core): expose isWritableSignal to the publicApi

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-981fabc
+ * @license Angular v21.0.0-rc.0+sha-20b4b2c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-981fabc');
+const VERSION = new Version('21.0.0-rc.0+sha-20b4b2c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
