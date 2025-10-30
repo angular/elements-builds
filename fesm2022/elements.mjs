87e498ef1c2aeda348f2b68483f035488017433f
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:28:47 +0000

    2123cc3 docs: add essential signal forms guide (#64665)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c1dee66
+ * @license Angular v21.1.0-next.0+sha-2123cc3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-c1dee66');
+const VERSION = new Version('21.1.0-next.0+sha-2123cc3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
