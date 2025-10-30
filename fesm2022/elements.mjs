ebaf45b7a05137e3cfcefb68366819d8c652c9a0
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:54:10 +0000

    56f26f1 docs(docs-infra): allow cors for json files (#64783)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-fd09a69
+ * @license Angular v21.1.0-next.0+sha-56f26f1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-fd09a69');
+const VERSION = new Version('21.1.0-next.0+sha-56f26f1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
