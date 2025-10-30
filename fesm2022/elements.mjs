69c02ec5d624b5c48cd9d0988f0336c362401425
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:28:26 +0000

    040585d ci: make the github action fail when a firebase command fails (#64801)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-54ad3f6
+ * @license Angular v20.3.9+sha-040585d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-54ad3f6');
+const VERSION = new Version('20.3.9+sha-040585d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
