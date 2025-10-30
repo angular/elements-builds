fb5f374997d301e2a8c6d4247381cf5dedc6f4dc
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:21:58 +0000

    c1dee66 fix(devtools): signal graph value preview mat-tree row indentation (#64798)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c84d372
+ * @license Angular v21.1.0-next.0+sha-c1dee66
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-c84d372');
+const VERSION = new Version('21.1.0-next.0+sha-c1dee66');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
