ee7c6348c9c17c89c2c34bf348a789f3d71a40b7
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:37:43 +0000

    a4fe078 refactor(common): remove unused import (#64699)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-fee4baf
+ * @license Angular v21.0.0-rc.0+sha-a4fe078
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-fee4baf');
+const VERSION = new Version('21.0.0-rc.0+sha-a4fe078');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
