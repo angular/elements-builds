11ad053aa76e17fef3ec62e2da3d52c9f6602ad4
Andrew Scott <atscott@google.com>
2025-11-03 20:11:30 +0000

    5d75d0e refactor(core): Replace usages of the `Function` type for animations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-303bff6
+ * @license Angular v21.0.0-rc.0+sha-5d75d0e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-303bff6');
+const VERSION = new Version('21.0.0-rc.0+sha-5d75d0e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
