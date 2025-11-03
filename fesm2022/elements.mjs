18e17f88c2b2d92c25662f562a1e9bba95797303
Andrew Scott <atscott01@gmail.com>
2025-11-03 23:33:54 +0000

    6b3fdf3 docs: remove feature from changelog.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d66384d
+ * @license Angular v21.0.0-rc.0+sha-6b3fdf3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-d66384d');
+const VERSION = new Version('21.0.0-rc.0+sha-6b3fdf3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
