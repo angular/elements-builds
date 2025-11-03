ace974747763ca69355b98677892b7f35ade9b04
Andrew Scott <atscott01@gmail.com>
2025-11-03 22:31:25 +0000

    97c31ae build: update dependency bazel to v7.7.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-92f7052
+ * @license Angular v21.0.0-rc.0+sha-97c31ae
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-92f7052');
+const VERSION = new Version('21.0.0-rc.0+sha-97c31ae');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
