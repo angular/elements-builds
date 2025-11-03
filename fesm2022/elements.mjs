7c314ac0ed1d118e787ede5547c51cd8baf3598f
Andrew Scott <atscott@google.com>
2025-11-03 21:05:49 +0000

    dc99f4c docs(docs-infra): ensure all redirections are absolute

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-32a166b
+ * @license Angular v21.0.0-rc.0+sha-dc99f4c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-32a166b');
+const VERSION = new Version('21.0.0-rc.0+sha-dc99f4c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
