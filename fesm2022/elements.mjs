78f2eab6dba6704ed1a8e351b0fd9b9f2967099a
Andrew Scott <atscott01@gmail.com>
2025-11-03 21:39:44 +0000

    429672d docs(docs-infra): Updates page title strategy to sync meta tags

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-434ddbc
+ * @license Angular v21.1.0-next.0+sha-429672d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-434ddbc');
+const VERSION = new Version('21.1.0-next.0+sha-429672d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
