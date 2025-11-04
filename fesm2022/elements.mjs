b346e4c15d1527c6cd1ed04a16013c6eda7e029b
Andrew Scott <atscott01@gmail.com>
2025-11-04 04:25:19 +0000

    2ad1b59 refactor(core): Removes unused flag for onDestroy

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b61847f
+ * @license Angular v21.1.0-next.0+sha-2ad1b59
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-b61847f');
+const VERSION = new Version('21.1.0-next.0+sha-2ad1b59');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
