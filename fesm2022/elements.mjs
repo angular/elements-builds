6a8f7097cf84871a5aae1a778ff097495d80253e
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:41:35 +0000

    b61847f docs: simplify tutorial signals with self-closing component tags

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c4c7fb8
+ * @license Angular v21.1.0-next.0+sha-b61847f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-c4c7fb8');
+const VERSION = new Version('21.1.0-next.0+sha-b61847f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
