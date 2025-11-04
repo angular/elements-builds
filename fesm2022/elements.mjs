824c39147e310b8037554339e5776db50464162b
Andrew Scott <atscott@google.com>
2025-11-04 17:43:33 +0000

    3ccda42 docs(router): uniformize lazy loading syntax

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-36f4c9b
+ * @license Angular v21.0.0-rc.0+sha-3ccda42
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-36f4c9b');
+const VERSION = new Version('21.0.0-rc.0+sha-3ccda42');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
