475954503ac4aa4efb18cfdba535b5a6adc25d80
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:30:06 +0000

    5b210e9 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c277214
+ * @license Angular v21.1.0-next.0+sha-5b210e9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-c277214');
+const VERSION = new Version('21.1.0-next.0+sha-5b210e9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
