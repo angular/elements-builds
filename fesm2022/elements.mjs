e06067d0801ecb5423dd57d4864c88ccd09c00b7
Andrew Scott <atscott@google.com>
2025-11-04 00:30:41 +0000

    34f53aa docs: remove standalone: true since it's now the default

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-a702c87
+ * @license Angular v21.0.0-rc.0+sha-34f53aa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-a702c87');
+const VERSION = new Version('21.0.0-rc.0+sha-34f53aa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
