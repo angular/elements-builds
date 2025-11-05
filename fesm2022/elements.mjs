015cd5aaef8b47491c8daaffa89966e6280203c5
Andrew Scott <atscott01@gmail.com>
2025-11-05 17:40:54 +0000

    748caf9 test(compiler-cli): set up TCB tests for signal forms

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7c49c53
+ * @license Angular v21.1.0-next.0+sha-748caf9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-7c49c53');
+const VERSION = new Version('21.1.0-next.0+sha-748caf9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
