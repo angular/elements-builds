34cf8b299f0d1e2230dd6ef6cd0871d8db1690f5
GitHub <noreply@github.com>
2025-11-05 19:59:43 +0000

    e1dfe32 build: add VSCode extension release script

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e18aac8
+ * @license Angular v21.1.0-next.0+sha-e1dfe32
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-e18aac8');
+const VERSION = new Version('21.1.0-next.0+sha-e1dfe32');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
