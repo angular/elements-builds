313807d07bcca06454e68a72c534488a2d422eae
Andrew Scott <atscott@google.com>
2025-11-05 19:51:11 +0000

    bf19fd4 docs: Add the missing function call to use the signal's actual value

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-24f9d76
+ * @license Angular v21.0.0-rc.1+sha-bf19fd4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-24f9d76');
+const VERSION = new Version('21.0.0-rc.1+sha-bf19fd4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
