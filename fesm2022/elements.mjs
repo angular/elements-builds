c299ce748a7b18703e8e3c87ca71726b2d275fe6
Alan Agius <alan.agius4@gmail.com>
2025-11-05 14:56:06 +0000

    1cd5b44 release: bump Angular DevTools version to 1.5.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-41be02d
+ * @license Angular v21.1.0-next.0+sha-1cd5b44
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-41be02d');
+const VERSION = new Version('21.1.0-next.0+sha-1cd5b44');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
