c186026a8010026523572d0f7207f0d1f7c51f34
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-05 23:28:46 +0000

    de1f22c build: update diff release package script

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-9ae1f44
+ * @license Angular v21.1.0-next.0+sha-de1f22c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-9ae1f44');
+const VERSION = new Version('21.1.0-next.0+sha-de1f22c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
