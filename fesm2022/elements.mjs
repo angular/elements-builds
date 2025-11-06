3723ad7c621a672d37f6cc26a47068f4af7c0b46
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 21:47:42 +0000

    6d6fda8 refactor(forms): allow FieldTree of recursive type

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-86a9feb
+ * @license Angular v21.0.0-rc.1+sha-6d6fda8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-86a9feb');
+const VERSION = new Version('21.0.0-rc.1+sha-6d6fda8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
