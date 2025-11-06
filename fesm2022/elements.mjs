922d04ca864f842d37cf477923c6337ea40862b7
GitHub <noreply@github.com>
2025-11-06 23:05:35 +0000

    85d9abb refactor(router): Intercept navigate events (unless it's a rollback)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-cf47ce2
+ * @license Angular v21.1.0-next.0+sha-85d9abb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-cf47ce2');
+const VERSION = new Version('21.1.0-next.0+sha-85d9abb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
