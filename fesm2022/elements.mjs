ed6c2389e42143ae5af834e4977f8136082ca3ec
GitHub <noreply@github.com>
2025-11-06 21:48:06 +0000

    faccf03 refactor(forms): allow FieldTree of recursive type

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-88f55b4
+ * @license Angular v21.1.0-next.0+sha-faccf03
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-88f55b4');
+const VERSION = new Version('21.1.0-next.0+sha-faccf03');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
