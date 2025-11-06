415c4e80578d025f9cc19a07f955a66b7229f59f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 22:28:57 +0000

    e69ada4 refactor(core): move profile_types.ts to primtives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-6d6fda8
+ * @license Angular v21.0.0-rc.1+sha-e69ada4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-6d6fda8');
+const VERSION = new Version('21.0.0-rc.1+sha-e69ada4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
