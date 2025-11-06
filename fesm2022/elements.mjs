2a08082940e4ca00ef89666571196f335dc8ae44
GitHub <noreply@github.com>
2025-11-06 22:28:34 +0000

    cf47ce2 refactor(core): move profile_types.ts to primtives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-faccf03
+ * @license Angular v21.1.0-next.0+sha-cf47ce2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-faccf03');
+const VERSION = new Version('21.1.0-next.0+sha-cf47ce2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
