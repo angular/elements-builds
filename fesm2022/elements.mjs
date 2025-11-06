1f8ddf368d6931117678ef6c8bdd403d2fbe69d9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 19:02:23 +0000

    036c5d2 fix(compiler): support one additional level of nesting in :host()

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-ee578d3
+ * @license Angular v20.3.10+sha-036c5d2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-ee578d3');
+const VERSION = new Version('20.3.10+sha-036c5d2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
