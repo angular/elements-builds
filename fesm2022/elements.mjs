89334fc0bc1e86303bbeef98f8d6e0d9445ac553
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 20:07:17 +0000

    850f0d6 perf(forms): only update interop controls when bound field changes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-033ea75
+ * @license Angular v21.1.0-next.0+sha-850f0d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-033ea75');
+const VERSION = new Version('21.1.0-next.0+sha-850f0d6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
