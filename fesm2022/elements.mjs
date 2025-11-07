664544d47382fbcea5a3a842c8281c8fd7c3b13d
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:50:39 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-0552e72');
+const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
