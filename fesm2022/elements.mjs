8b6604fa7112dfcdb4fe31cc9fb985239eddfafc
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:17:54 +0000

    46c1a80 docs(docs-infra): refactor update to use host property and refactor isWindows

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-a3cb1df
+ * @license Angular v21.0.0-rc.1+sha-46c1a80
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-a3cb1df');
+const VERSION = new Version('21.0.0-rc.1+sha-46c1a80');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
