da57e4cba80ec0515c8c424b3075652dda17af1f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 19:48:03 +0000

    aada582 build: update `rules_angular` to latest

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-1e53f4e
+ * @license Angular v21.0.0-rc.1+sha-aada582
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-1e53f4e');
+const VERSION = new Version('21.0.0-rc.1+sha-aada582');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
