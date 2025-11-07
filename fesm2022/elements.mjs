f9c91b7c4a334f422836f41588652cbbb4922a31
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 23:32:47 +0000

    d2b854b docs(core): add a11y considerations related to `@defer()`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1651908
+ * @license Angular v21.1.0-next.0+sha-d2b854b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-1651908');
+const VERSION = new Version('21.1.0-next.0+sha-d2b854b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
