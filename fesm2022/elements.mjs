2f64560be5bea39c9b58dfacd66f9b7406c948b3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-07 15:48:46 +0000

    a3cb1df refactor(forms): nicer type errors

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-8e73f54
+ * @license Angular v21.0.0-rc.1+sha-a3cb1df
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-8e73f54');
+const VERSION = new Version('21.0.0-rc.1+sha-a3cb1df');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
