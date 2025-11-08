103ae9fa6b3bba1e5cb197f51fc546b9d9dfb22c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-08 00:06:28 +0000

    b47054c Revert "fix(compiler): support commas in :host() argument"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-143883a
+ * @license Angular v20.3.10+sha-b47054c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-143883a');
+const VERSION = new Version('20.3.10+sha-b47054c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
