7b2441e6375b4bd6b66328d60f0faff00f36bc70
Andrew Kushnir <akushnir@google.com>
2025-11-10 17:58:37 +0000

    650af71 refactor(http): migrate XSRF classes to use inject() function

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-afa2ac2
+ * @license Angular v21.0.0-rc.1+sha-650af71
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-afa2ac2');
+const VERSION = new Version('21.0.0-rc.1+sha-650af71');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
