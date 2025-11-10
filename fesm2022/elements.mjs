8dff639dd89833752f2cd9b3af5acfb3eac1c6c2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 18:39:04 +0000

    63418fb docs: update cross-repo adev docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-650af71
+ * @license Angular v21.0.0-rc.1+sha-63418fb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-650af71');
+const VERSION = new Version('21.0.0-rc.1+sha-63418fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
