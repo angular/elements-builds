9cfcd6fd9db7e398d7457179e0a7784f7324e879
Andrew Kushnir <akushnir@google.com>
2025-11-10 15:57:19 +0000

    fedb083 fix(docs-infra): avoid double slash in sitemap urls

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-b47054c
+ * @license Angular v20.3.10+sha-fedb083
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-b47054c');
+const VERSION = new Version('20.3.10+sha-fedb083');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
