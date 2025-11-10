adafa1c109995c8f46415d370a484d45f445b80a
Andrew Kushnir <akushnir@google.com>
2025-11-10 22:20:06 +0000

    e0407c0 docs: add links to SDUI example and livestream

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-8922cae
+ * @license Angular v20.3.10+sha-e0407c0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-8922cae');
+const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-e0407c0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
