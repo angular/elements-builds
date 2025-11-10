1618027eb7dbde77d8d4fbd7caf172ea225f31f6
Andrew Kushnir <akushnir@google.com>
2025-11-10 20:15:24 +0000

    ab98e71 fix(common): remove placeholder image listeners once view is removed

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-63418fb
+ * @license Angular v21.0.0-rc.1+sha-ab98e71
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-63418fb');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-ab98e71');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
