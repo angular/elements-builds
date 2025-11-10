ebc6c1a8373d215538d142cfe955dcf34b086c73
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:06:14 +0000

    5092a89 docs: add shell language to CLI installation examples

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-590244a
+ * @license Angular v21.0.0-rc.1+sha-5092a89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-590244a');
+const VERSION = new Version('21.0.0-rc.1+sha-5092a89');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
