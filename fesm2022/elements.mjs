21404706ed8986390953d84b099edd45f1f1dc9b
Andrew Kushnir <akushnir@google.com>
2025-11-11 22:13:00 +0000

    9419ea3 fix(compiler): support complex selectors in :nth-child()

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-f00fb46
+ * @license Angular v20.3.10+sha-9419ea3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-f00fb46');
+const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-9419ea3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
