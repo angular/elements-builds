6faa990b2c9d7343f038f4218d106f3ee103f8e6
Andrew Kushnir <akushnir@google.com>
2025-11-11 16:41:09 +0000

    557213e docs: correct mermaid code block formatting

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-2019940
+ * @license Angular v21.0.0-rc.1+sha-557213e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-2019940');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-557213e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
