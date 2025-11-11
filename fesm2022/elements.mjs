24ba1ca45fece7d891b0f65e71992ab5f95ee703
Andrew Kushnir <akushnir@google.com>
2025-11-11 20:57:53 +0000

    7811d95 refactor(core): improve resource loading with async/await

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-a278ee3
+ * @license Angular v21.0.0-rc.1+sha-7811d95
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-a278ee3');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-7811d95');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
