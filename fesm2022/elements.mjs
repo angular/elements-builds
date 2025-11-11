755085b09c0af39712373b81b81e7bb874b2c029
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 22:16:03 +0000

    c3e82fb refactor(core): render additional WAI-ARIA 1.3 property bindings as attributes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4ed8781
+ * @license Angular v21.1.0-next.0+sha-c3e82fb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4ed8781');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c3e82fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
