e657866a7bfa599506b4eba21ac51e09077c4565
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 16:46:41 +0000

    cc095d4 docs: release notes for the v21.0.0-rc.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1f6de17
+ * @license Angular v21.1.0-next.0+sha-cc095d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1f6de17');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cc095d4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
