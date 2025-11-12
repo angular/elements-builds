4300bc18c0e97cd595b25e0ca350c7dbdd9bff0d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 21:16:54 +0000

    1c4fb90 docs: update tutorials with self-closing tags, standalone router directives, and fix syntax highlighting

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-2e7623c
+ * @license Angular v20.3.11+sha-1c4fb90
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-2e7623c');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-1c4fb90');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
