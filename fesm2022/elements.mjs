3a2dd9e4b29c5a72c8ebffcfb2b7381e0caf86ff
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-12 23:06:57 +0000

    b6a91d4 docs: Fixed mermaid diagram node label contrast in dark mode

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f7e5857
+ * @license Angular v21.1.0-next.0+sha-b6a91d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7e5857');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b6a91d4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
