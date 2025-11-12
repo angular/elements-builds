3a9a34377ab6946a7470e72cf1d9d363850a15f6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:38:05 +0000

    3737724 docs: correct code block language in error handling and signals interop

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-3bafe23
+ * @license Angular v20.3.11+sha-3737724
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-3bafe23');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-3737724');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
