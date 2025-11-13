129e93bb8eea0fdf1ae3a266896987ea57174152
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 18:07:47 +0000

    16d51f6 refactor(core): flip ngDevMode condition for formatter

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-2406172
+ * @license Angular v21.0.0-rc.2+sha-16d51f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-2406172');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-16d51f6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
