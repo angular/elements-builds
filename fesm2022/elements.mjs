f42d7434c0520a0c7ea784aeb893bdbd6aaae7c8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 17:22:20 +0000

    a02b957 refactor(core): remove resource flag.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-a586bd2
+ * @license Angular v20.3.11+sha-a02b957
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-a586bd2');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-a02b957');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
