f54ee6e316c42dfe89cdffb28ca5e7b1ae8351c4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:08:47 +0000

    70da36f docs(docs-infra): improve theme picker accessibility and add animations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-06bc146
+ * @license Angular v20.3.11+sha-70da36f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-06bc146');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-70da36f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
