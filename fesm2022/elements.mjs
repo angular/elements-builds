a9b72c073a76839851aba6685cf2d912e7b67085
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 23:34:36 +0000

    c1057c6 docs: add formArray clear() example clarifies array manipulation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-722292f
+ * @license Angular v21.1.0-next.0+sha-c1057c6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-722292f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c1057c6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
