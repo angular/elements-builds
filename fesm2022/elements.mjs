ec2ebabf47d2a938cc125b0b5dade3e2acffc469
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 19:30:49 +0000

    8a5b284 docs: add draft of aria component guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-966bfae
+ * @license Angular v21.0.0-rc.2+sha-8a5b284
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-966bfae');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-8a5b284');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
