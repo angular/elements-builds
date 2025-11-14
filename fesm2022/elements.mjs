70d909ab5844f28554440d11dff2350234b9134c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:47:39 +0000

    f550f4a docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-c2a52fd');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-f550f4a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
