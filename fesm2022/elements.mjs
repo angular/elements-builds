c7405b5cc0a49a4fd55faffee2d968c8503a707e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:46:56 +0000

    755b360 docs: replace legacy <docs-code> with fenced code blocks with highlight metadata

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-2e00074
+ * @license Angular v20.3.12+sha-755b360
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-2e00074');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-755b360');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
