39e3ff9925226b5c2475dac8681bb743b771563f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 23:12:33 +0000

    72b19d8 docs: replace @HostListener with host event bindings in attribute directives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-81b6e45
+ * @license Angular v20.3.12+sha-72b19d8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-81b6e45');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-72b19d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
