24936259cd53be32512acc3ab96f2e966cb171c4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:15:20 +0000

    59543d6 docs: add bun package manager and standardize order in testing guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-63ca0de
+ * @license Angular v21.0.0-rc.2+sha-59543d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-63ca0de');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-59543d6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
