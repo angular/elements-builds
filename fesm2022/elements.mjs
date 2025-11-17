5f8945ade17f187d35aa595ce83f2a608e810347
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 19:21:18 +0000

    6551b65 docs: update examples to latest Angular

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-b7e9183
+ * @license Angular v21.0.0-rc.2+sha-6551b65
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-b7e9183');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-6551b65');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
