797b882cd6bc5b579378f0cd939e99c872c8c949
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:53:18 +0000

    a7dddcc docs: Adds links to relevant guides for APIs in core package

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-59543d6
+ * @license Angular v21.0.0-rc.2+sha-a7dddcc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-59543d6');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-a7dddcc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
