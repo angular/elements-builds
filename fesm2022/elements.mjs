8f75367087e7c7d1b9e726ae94334e4ddede7ca3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:33:07 +0000

    4a1a703 docs(docs-infra): set max-height to code example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-daae263
+ * @license Angular v20.3.12+sha-4a1a703
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-daae263');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-4a1a703');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
