a35e3af620a98332b0893b271a164d2bdb1d6e19
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:53:38 +0000

    ecea909 fix(compiler): don't choke on unbalanced parens in declaration block

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-cc621ea
+ * @license Angular v21.0.0-rc.2+sha-ecea909
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-cc621ea');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-ecea909');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
