855f33b76570c66215a4b68da6a6b401a6e81869
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 16:04:31 +0000

    63ca0de build: update dependency bazel to v7.7.1

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-74b95de
+ * @license Angular v21.0.0-rc.2+sha-63ca0de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-74b95de');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-63ca0de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
