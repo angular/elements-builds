bbe936a6be433b1618d67fa94b2a8cc48eaee531
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:43:30 +0000

    cb481a5 docs: aria listbox examples (#65273)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-88dfd96
+ * @license Angular v21.1.0-next.0+sha-cb481a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-88dfd96');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cb481a5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
