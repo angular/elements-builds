c3e506ebf2e15b4905f0194a8faf2534cf4ca127
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 16:21:16 +0000

    56ba2c8 build: lock file maintenance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-1b43bfb
+ * @license Angular v21.0.0-rc.3+sha-56ba2c8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-1b43bfb');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-56ba2c8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
