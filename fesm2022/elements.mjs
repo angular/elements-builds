fc8f60a2617a271f337df466f4627912969e1bbf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 19:19:05 +0000

    d0877a5 docs: add aria grid examples

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ed0647d
+ * @license Angular v21.1.0-next.0+sha-d0877a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ed0647d');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-d0877a5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
