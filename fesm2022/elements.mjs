301d1f748309958aff2a520075923bf660153d79
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 22:16:39 +0000

    27fa993 docs: format examples with replacing <docs-code> typescript examples with fenced ts code blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ceb2d71
+ * @license Angular v21.1.0-next.0+sha-27fa993
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ceb2d71');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-27fa993');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
