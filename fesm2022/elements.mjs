ed67ed6e0c414f787fcf38090645b41e0b4c3441
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 22:16:20 +0000

    5aea52a docs: format examples with replacing <docs-code> typescript examples with fenced ts code blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-3686987
+ * @license Angular v20.3.12+sha-5aea52a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-3686987');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-5aea52a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
