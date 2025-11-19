49f17d9c0cba97ed17cac9a3e52b17dd198420f8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:57:14 +0000

    38f4ea1 docs: add signal forms validation guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e9ba63a
+ * @license Angular v21.1.0-next.0+sha-38f4ea1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e9ba63a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38f4ea1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
