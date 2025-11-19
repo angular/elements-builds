66764379223b47320c70e1837dc842113f586c15
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 21:11:37 +0000

    3d9c82c build: provide a specific version of zone.js for the vscode pre standalone test

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-28810e0
+ * @license Angular v21.0.0+sha-3d9c82c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-28810e0');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-3d9c82c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
