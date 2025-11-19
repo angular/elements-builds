9e7f9e6c22ac6b5b63c247f9cbbecc899b899e33
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 17:46:24 +0000

    eb2f665 docs: release notes for the v20.3.13 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a51ba9c
+ * @license Angular v21.1.0-next.0+sha-eb2f665
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a51ba9c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-eb2f665');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
