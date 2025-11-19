e532cd34804005e4617ff8b2905bae84db8327cb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:05:51 +0000

    28c1a2d docs: add signal forms comparison guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-012326e
+ * @license Angular v21.0.0-rc.3+sha-28c1a2d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-012326e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-28c1a2d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
