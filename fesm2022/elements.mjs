7df3c39759eaf84c23909920250db694f8ac4386
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 16:18:58 +0000

    56b40d8 build: update dependency @inquirer/prompts to v8

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7a910e6
+ * @license Angular v21.1.0-next.0+sha-56b40d8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-7a910e6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-56b40d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
