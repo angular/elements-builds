aa3e0c8a4cbd30befe639ffad3c5f2de21eabc10
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 16:18:20 +0000

    17b7d56 build: force angular cli to run in non-TTY mode for adev and devapp

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-28c1a2d
+ * @license Angular v21.0.0-rc.3+sha-17b7d56
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-28c1a2d');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-17b7d56');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
