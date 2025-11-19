6430e7083f30e2d7d5acd02028b0781f3d42d9f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:03:53 +0000

    330e046 docs: fix docs heading alignment

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-5aea52a
+ * @license Angular v20.3.12+sha-330e046
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-5aea52a');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-330e046');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
