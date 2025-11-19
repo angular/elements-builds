bee64db4a14f546bdb08f807f5ac1089996db058
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:13:18 +0000

    96a8d58 fix(docs-infra): add install in pnpm

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-330e046
+ * @license Angular v20.3.12+sha-96a8d58
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-330e046');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-96a8d58');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
