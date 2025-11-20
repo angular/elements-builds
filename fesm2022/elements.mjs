ac17a7a571ac0fd9d8f70ea104410162eac5a509
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 21:46:50 +0000

    11da7a6 docs(core): remove outdated regex template restriction in best-practices.md

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-396c25e
+ * @license Angular v21.1.0-next.0+sha-11da7a6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-396c25e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-11da7a6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
