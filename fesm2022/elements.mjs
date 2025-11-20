bdf62b54dd368e0eabad1055029431db1aa9bdb4
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 16:50:28 +0000

    183d194 docs: add 20 to 21 update guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29185a8
+ * @license Angular v21.1.0-next.0+sha-183d194
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-29185a8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-183d194');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
