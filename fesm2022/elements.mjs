440b0a1e1522d9e6e5c21eed10d37f40d0676748
kirjs <kirts@google.com>
2025-11-20 16:50:59 +0000

    eadc8de docs: add 20 to 21 update guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-f349a91
+ * @license Angular v21.0.0+sha-eadc8de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f349a91');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-eadc8de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
