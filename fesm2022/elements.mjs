b59f51f9c066e29d457c6023023306873a81c81a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-20 00:45:11 +0000

    f31fbdd docs: adds a link to the signal forms example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2bf0c52
+ * @license Angular v21.0.0+sha-f31fbdd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2bf0c52');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f31fbdd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
