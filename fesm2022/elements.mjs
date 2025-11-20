7ca4d2f7b2c9f96119473972ab36018e72ddb52c
kirjs <kirts@google.com>
2025-11-20 16:25:48 +0000

    f349a91 docs: fix standlone menu overlay

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-630a3b2
+ * @license Angular v21.0.0+sha-f349a91
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-630a3b2');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f349a91');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
