6ebefdd41d295a52dd4268855e45eab38ca3c648
kirjs <kirts@google.com>
2025-11-21 15:52:05 +0000

    053bf98 docs: iterate on signal forms essentials guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2f44ede
+ * @license Angular v21.0.0+sha-053bf98
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2f44ede');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-053bf98');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
