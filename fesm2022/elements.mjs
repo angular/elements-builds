1165497c9ec285417d0ab8f454ff9d2d4943698a
kirjs <kirts@google.com>
2025-11-21 15:22:56 +0000

    2f44ede docs: add missing ARIA patterns and API reference Adds the missing ARIA usage patterns and links to the relevant API reference sections to improve accessibility guidance and ensure examples follow recommended best practices.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-1925721
+ * @license Angular v21.0.0+sha-2f44ede
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1925721');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2f44ede');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
