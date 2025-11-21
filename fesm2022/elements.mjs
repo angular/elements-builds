cd814808e219bf05d95a311df0e257cdfca21d37
kirjs <kirts@google.com>
2025-11-21 17:29:55 +0000

    1dfce39 docs: move aria tabs into content organization category

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-bc34083
+ * @license Angular v21.0.0+sha-1dfce39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-bc34083');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1dfce39');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
