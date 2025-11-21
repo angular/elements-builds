690cb06ed7d0b42460119a3d305adbba95a7b9ab
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:38:25 +0000

    38a354f docs: Adds signal type checking documentation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f884c18
+ * @license Angular v21.1.0-next.0+sha-38a354f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f884c18');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38a354f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
