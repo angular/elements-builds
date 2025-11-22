6306ee523967c3acc8823df757ff61219e96fdf6
kirjs <kirts@google.com>
2025-11-22 14:14:30 +0000

    696aaa1 docs: fix router event subscription in error handler example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-a70a191
+ * @license Angular v21.0.0+sha-696aaa1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a70a191');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-696aaa1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
