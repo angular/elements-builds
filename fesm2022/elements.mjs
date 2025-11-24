0ce4dd3c6137f6df4f9f4d38cadcb43e55924b2b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:12:04 +0000

    48a05dd docs: fix v21 event header image

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-72b1f7f
+ * @license Angular v21.1.0-next.0+sha-48a05dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-72b1f7f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-48a05dd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
