780cbb336773ce8a36a3dd5dc92e501e8973a6c0
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 16:15:45 +0000

    b5ae62c docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0203dad
+ * @license Angular v21.1.0-next.0+sha-b5ae62c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0203dad');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b5ae62c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
