bbb332c7a3c9faa6717b4539b07027bf825b6851
kirjs <kirts@google.com>
2025-11-24 18:33:09 +0000

    534cc3d docs: standardize aria code examples to not use component suffix

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-a62162d
+ * @license Angular v21.0.0+sha-534cc3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a62162d');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-534cc3d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
