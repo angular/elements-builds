931c92c3acb07fd84554e94a664492fc2ee6d48f
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:44:33 +0000

    a4f50bd fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b26fe73
+ * @license Angular v21.1.0-next.0+sha-a4f50bd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b26fe73');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a4f50bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
