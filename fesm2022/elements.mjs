220df97084bea4ee800d5c5da95918e7f8374444
kirjs <kirts@google.com>
2025-11-24 15:29:06 +0000

    1f20b54 docs(docs-infra): simplify tab selection

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2d658b3
+ * @license Angular v21.0.0+sha-1f20b54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2d658b3');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
