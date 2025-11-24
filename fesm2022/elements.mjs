4c1fc58aaee382260da65ba08d982e6a39e2d902
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:23:04 +0000

    9dd7cc1 ci: adding myself as optional to the compiler scope

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0f4b11c
+ * @license Angular v21.1.0-next.0+sha-9dd7cc1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0f4b11c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-9dd7cc1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
