10508cda8fb653da9c011e49b0c80530e1f422f4
GitHub <noreply@github.com>
2025-11-24 15:06:19 +0000

    f7f9707 build: run e2e in sandbox

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5961e96
+ * @license Angular v21.1.0-next.0+sha-f7f9707
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5961e96');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7f9707');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
