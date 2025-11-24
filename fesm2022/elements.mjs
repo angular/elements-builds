82d2b00ad3d5d65378f91e36f6aec1e377c9757a
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 22:03:31 +0000

    51ebe9a refactor(core): Remove toggles used for zoneless by default migration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ca6ab6c
+ * @license Angular v21.1.0-next.0+sha-51ebe9a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ca6ab6c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-51ebe9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
