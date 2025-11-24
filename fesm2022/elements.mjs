8780e255b71b8e6b2df2cefeb0b3e66c75af1e41
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 15:28:54 +0000

    119c6dd docs(docs-infra): simplify tab selection

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f7f9707
+ * @license Angular v21.1.0-next.0+sha-119c6dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7f9707');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-119c6dd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
