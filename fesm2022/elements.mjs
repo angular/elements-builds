724aaa6805afdaba78fe53a3e7e1063caaa0ef06
kirjs <kirts@google.com>
2025-11-24 16:35:45 +0000

    9852033 refactor(compiler-cli): add a resource debugName transform (#64172)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-102cb87
+ * @license Angular v21.0.0+sha-9852033
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-102cb87');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9852033');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
