ce4dd82e1cfaf262843b77175c8abaa4f709d85e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 16:38:35 +0000

    5bfa027 fix(compiler-cli): escape angular control flow in jsdoc

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-346caef
+ * @license Angular v21.1.0-next.0+sha-5bfa027
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-346caef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5bfa027');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
