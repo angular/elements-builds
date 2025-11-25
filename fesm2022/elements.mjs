ddf9172fa3767ddcc25e4b6cd538af61055ed8eb
kirjs <kirts@google.com>
2025-11-25 15:55:23 +0000

    e3f5f34 refactor(forms): Make reset take value

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-9aa3e92
+ * @license Angular v21.0.0+sha-e3f5f34
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9aa3e92');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-e3f5f34');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
