6fa09859b13f59af745494c70471747228675cdc
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 12:47:44 +0000

    a294e3b docs: fix example imports and missing event URL reference

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-51ebe9a
+ * @license Angular v21.1.0-next.0+sha-a294e3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-51ebe9a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a294e3b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
