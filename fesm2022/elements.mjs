c9da0ced6c36c2433887a2c4630efc78f61468a2
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 16:43:54 +0000

    3b1fa82 fix(core): use injected `DOCUMENT` for `CSP_NONCE`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5bfa027
+ * @license Angular v21.1.0-next.0+sha-3b1fa82
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5bfa027');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3b1fa82');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
