f7ae6c1cff641934d3e6e73c65e8cfe577f25b70
kirjs <kirts@google.com>
2025-11-25 18:51:51 +0000

    3240d85 fix(http): prevent XSRF token leakage to protocol-relative URLs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-908b5a4
+ * @license Angular v21.0.0+sha-3240d85
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-908b5a4');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-3240d85');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
