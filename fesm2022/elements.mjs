817a66f56c448c419b265f0d86d73f99fcc9dfe3
GitHub <noreply@github.com>
2025-11-25 15:39:05 +0000

    c70e246 feat(forms): add DI option for classes on `Field` directive

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-67d41e9
+ * @license Angular v21.1.0-next.0+sha-c70e246
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-67d41e9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c70e246');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
