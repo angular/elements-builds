a7befd128206c0d05b00218063c587c8b9809400
kirjs <kirts@google.com>
2025-11-26 18:47:16 +0000

    4207d89 docs(docs-infra): fix visible lines/regions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-1260d1a
+ * @license Angular v21.0.1+sha-4207d89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-1260d1a');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-4207d89');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
