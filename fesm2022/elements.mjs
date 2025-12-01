7be32a5dfe912f534a164bd33ded4b6578ea82e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:24:31 +0000

    8ab7594 docs: format typescript examples and replace <docs-code> with fenced code blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-ed32e8c
+ * @license Angular v21.0.2+sha-8ab7594
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-ed32e8c');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-8ab7594');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
