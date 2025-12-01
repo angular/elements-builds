9fde630d3a0cef5828b1dddea4e287dc443ee1f4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:56:34 +0000

    242d332 docs: release notes for the v21.0.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-76d097e
+ * @license Angular v21.1.0-next.0+sha-242d332
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-76d097e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-242d332');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
