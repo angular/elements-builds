e0ba1b05058254951a7d746edaf5957654ccbcef
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 09:33:12 +0000

    d1ca8ae fix(compiler): prevent XSS via SVG animation `attributeName` and MathML/SVG URLs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.14+sha-136e923
+ * @license Angular v20.3.14+sha-d1ca8ae
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.14+sha-136e923');
+const VERSION = /* @__PURE__ */ new Version('20.3.14+sha-d1ca8ae');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
