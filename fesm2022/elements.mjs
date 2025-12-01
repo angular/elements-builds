68e87e99cf27943c4ac8c27f37851ef077eaa924
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:02:42 +0000

    721080a docs: fix npm link URL in creating-libraries guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-242d332
+ * @license Angular v21.1.0-next.0+sha-721080a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-242d332');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-721080a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
