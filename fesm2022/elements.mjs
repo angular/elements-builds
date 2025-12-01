4bb6932223ca2cbb23200b507d51e15726058e74
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:37:39 +0000

    aea2465 docs: update notes to use consistent formatting in various guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-25bf417
+ * @license Angular v21.1.0-next.0+sha-aea2465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-25bf417');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-aea2465');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
