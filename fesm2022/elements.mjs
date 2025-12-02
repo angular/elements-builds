34650a3e668cb0e1656abb704207b4755063be99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:34:33 +0000

    e95d2e3 docs: Update adev/src/content/guide/forms/signals/models.md

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-c2a8934
+ * @license Angular v21.0.2+sha-e95d2e3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-c2a8934');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-e95d2e3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
