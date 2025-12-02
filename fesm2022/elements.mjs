0e7d7cf4ec872eee3867b500846bb68aacaaf7df
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:42:17 +0000

    893fbdf docs: update syntax highlighting in component documentation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-706d762
+ * @license Angular v21.0.2+sha-893fbdf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-706d762');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-893fbdf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
