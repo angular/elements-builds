7fdac2386cede43050d4cb0685b08f282d129769
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 12:08:55 +0000

    f35b2ef refactor(compiler): Generate the `controlCreate` instruction after the native element has been created

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a784995
+ * @license Angular v21.1.0-next.0+sha-f35b2ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a784995');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f35b2ef');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
