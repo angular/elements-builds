243f9e9013526754f51532cad5478d75b475ea8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:29:42 +0000

    34caae5 ci: use defined ng-dev config for deploying docs site

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-2e5d187
+ * @license Angular v21.0.3+sha-34caae5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-2e5d187');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-34caae5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
