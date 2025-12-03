946a6490d19e0f9f9e9ed8e428092a1f94b7f2eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 20:56:06 +0000

    bf1c12c fix(forms): memoize reads of child fields in signal forms (#65802)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-565c022
+ * @license Angular v21.0.3+sha-bf1c12c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-565c022');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-bf1c12c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
