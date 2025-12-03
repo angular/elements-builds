4402970cb47414fc36c39b4ff830aba6c6d565f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:10:20 +0000

    13353bf Revert "build: update cross-repo angular dependencies"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e26aa86
+ * @license Angular v21.1.0-next.0+sha-13353bf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e26aa86');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-13353bf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
