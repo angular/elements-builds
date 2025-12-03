8a169f8dd6c8215b2cfb1867ac07638225358d23
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:58:17 +0000

    819b640 docs: prevent showing typescript diagnostic errors until the node runtime reaches a ready state

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-13353bf
+ * @license Angular v21.1.0-next.0+sha-819b640
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-13353bf');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-819b640');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
