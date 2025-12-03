b5a3c604bc3258c47fa2187441ad1551d0fe4136
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:34:41 +0000

    2e5d187 fix(docs-infra): prevent readonly TypeScript keyword from being linked

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c355ffd
+ * @license Angular v21.0.3+sha-2e5d187
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c355ffd');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-2e5d187');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
