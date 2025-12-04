4bcdc92baffae0658d573e96a8d8407a11aa3af3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-04 19:50:19 +0000

    28e7546 feat(docs-infra): forward preview error locations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c3bd22d
+ * @license Angular v21.0.3+sha-28e7546
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c3bd22d');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-28e7546');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
