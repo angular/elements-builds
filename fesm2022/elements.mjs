a9ceac27211368e58b9899e6749ca9fa3168ed77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 16:47:59 +0000

    8bedd99 build: format manifest files after updating its version

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-28e7546
+ * @license Angular v21.0.3+sha-8bedd99
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-28e7546');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-8bedd99');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
