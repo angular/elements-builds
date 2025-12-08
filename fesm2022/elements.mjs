7402fd842aa4672c7b9e196fd57b69f5186bfcc1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 18:09:40 +0000

    253dc95 feat(docs-infra): add complexity level badge

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-dd47c57
+ * @license Angular v21.1.0-next.1+sha-253dc95
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-dd47c57');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-253dc95');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
