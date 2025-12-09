57d4cd8fe9246796f11b69af35a03fe6f47d2e8a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-09 21:48:45 +0000

    9ace7d7 build: add the ignore all rc files flag when running bazel info

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-253dc95
+ * @license Angular v21.1.0-next.1+sha-9ace7d7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-253dc95');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-9ace7d7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
