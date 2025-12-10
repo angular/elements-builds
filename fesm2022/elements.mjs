8a08b947eb00e646a6140bc313a188f96928b913
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:37:36 +0000

    af6c01f build: update pnpm to v10.25.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-954c546
+ * @license Angular v21.1.0-next.1+sha-af6c01f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-954c546');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-af6c01f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
