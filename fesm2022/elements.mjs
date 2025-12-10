2cea4caa903481c63f8c74efb13349be433b988d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 16:28:20 +0000

    d355205 refactor(core): match spec updates for FakeNavigation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-9ace7d7
+ * @license Angular v21.1.0-next.1+sha-d355205
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-9ace7d7');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d355205');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
