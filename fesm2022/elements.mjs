21d5aaf310be42e1b5452d5a1153b8ef8d479a9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 17:40:06 +0000

    834593b docs: use fenced code blocks and format examples

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-01ab257
+ * @license Angular v21.0.3+sha-834593b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-01ab257');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-834593b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
