6eae542231ccecfaeed5dd629ed567b01a74a357
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 19:26:13 +0000

    4f6014a fix(core): avoid false-positive deprecation when using `InjectionToken` with factory only

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.2+sha-1636451
+ * @license Angular v21.1.0-next.2+sha-4f6014a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-1636451');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-4f6014a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
