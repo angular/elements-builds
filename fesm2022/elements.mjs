e83ea7fdec7e3735613463c8fa7bfe47430c0d9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:53:13 +0000

    fa4e6e6 release: cut the v21.0.4 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c937da4
+ * @license Angular v21.0.4+sha-fa4e6e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c937da4');
+const VERSION = /* @__PURE__ */new Version('21.0.4+sha-fa4e6e6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
