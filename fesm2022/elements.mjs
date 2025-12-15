891470806f086e59411276840d596f94c6c55617
GitHub <noreply@github.com>
2025-12-15 22:19:16 +0000

    d4111ee refactor(compiler): remove unnecessary sanitization for safe attributes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-193aa33
+ * @license Angular v21.1.0-next.3+sha-d4111ee
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-193aa33');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-d4111ee');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
