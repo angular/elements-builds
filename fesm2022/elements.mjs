9cfc49876bd7dbdc7efe6c79c9502ba5c6498d50
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 19:40:04 +0000

    a2aa8fe docs: format CLI builder JSON examples using fenced blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-23fbe5c
+ * @license Angular v21.1.0-next.3+sha-a2aa8fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-23fbe5c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-a2aa8fe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
