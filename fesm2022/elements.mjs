f988e5f3bf97923858cc38e9dbab17949dacd480
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 17:49:17 +0000

    b5205c7 docs(docs-infra): throw when rendering absolute links to adev in our guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-8243bb3
+ * @license Angular v21.1.0-next.3+sha-b5205c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-8243bb3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b5205c7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
