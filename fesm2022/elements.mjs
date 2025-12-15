2a4e451e8215490920b2e16165aac19a45e16859
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 19:49:25 +0000

    44d4439 refactor(forms): add signal forms to the type tests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-a2aa8fe
+ * @license Angular v21.1.0-next.3+sha-44d4439
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-a2aa8fe');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-44d4439');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
