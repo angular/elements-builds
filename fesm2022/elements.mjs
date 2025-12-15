5e230ee62caa11d06a8bcf1440a09418b9f2e7d3
Andrew Kushnir <akushnir@google.com>
2025-12-15 22:20:27 +0000

    31b9090 refactor(compiler): remove unnecessary sanitization for safe attributes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-5afb1eb
+ * @license Angular v21.0.5+sha-31b9090
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5afb1eb');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-31b9090');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
