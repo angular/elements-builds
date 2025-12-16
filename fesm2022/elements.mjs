0dbf4a22431640253602c8799748e7147a4562bd
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 17:28:12 +0000

    af77b89 ci: reformat files

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-3ff20a5
+ * @license Angular v21.1.0-next.3+sha-af77b89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-3ff20a5');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-af77b89');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
