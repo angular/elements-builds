734f682bbf4f0cb1870f8c4e054af9d916d8fb5d
Andrew Kushnir <akushnir@google.com>
2025-12-16 18:30:57 +0000

    729b964 refactor(forms): rename field to fieldTree in FieldContext and ValidationError

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-0a5fd6b
+ * @license Angular v21.0.5+sha-729b964
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0a5fd6b');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-729b964');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
