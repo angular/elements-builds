b00044994eb9725c4be66a90259cff9c1dab8272
Andrew Kushnir <akushnir@google.com>
2025-12-16 17:46:06 +0000

    0a5fd6b refactor(core): conditionally include debug names based on `ngDevMode`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-61a4648
+ * @license Angular v21.0.5+sha-0a5fd6b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-61a4648');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0a5fd6b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
