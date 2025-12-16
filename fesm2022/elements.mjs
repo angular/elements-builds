57ce6418d5db5e5d1b8094124052483624d41dd9
Andrew Kushnir <akushnir@google.com>
2025-12-16 23:02:54 +0000

    feed89e ci: reformat files

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-729b964
+ * @license Angular v21.0.5+sha-feed89e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-729b964');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-feed89e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
