b1bb20f8efe97e237a5a6d0d390d15efbc36c9cb
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:57:01 +0000

    3b3b982 docs: add documentation for schema validation with Signal Forms

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-14b529e
+ * @license Angular v21.0.5+sha-3b3b982
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-14b529e');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3b3b982');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
