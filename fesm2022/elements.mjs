50549cf7561db62f80c6cedd2d51bfa43d811918
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 22:30:53 +0000

    ad88c72 docs: fix incorrect line highlighting in tutorials

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-664fa39
+ * @license Angular v21.1.0-next.4+sha-ad88c72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-664fa39');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-ad88c72');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
