0c3ed90a5ef5f7c9649e75db76d83262f30c0169
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 17:39:12 +0000

    f516370 fix(core): use mutable ResponseInit type for RESPONSE_INIT token

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-9fa77af
+ * @license Angular v21.1.0-next.3+sha-f516370
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-9fa77af');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-f516370');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
