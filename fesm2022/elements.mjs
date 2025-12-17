9da2a389aedc22dc272530728fa204302efb1c6c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:18:56 +0000

    8d7bcbc build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-a27bdd3
+ * @license Angular v21.0.5+sha-8d7bcbc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-a27bdd3');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-8d7bcbc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
