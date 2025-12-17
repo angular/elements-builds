3ae13fe95a203603a29a237d3fec0da263dc6831
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 00:45:48 +0000

    c501b25 fix(core): explicitly cast signal node value to String

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-6270bba
+ * @license Angular v21.1.0-next.3+sha-c501b25
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-6270bba');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-c501b25');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
