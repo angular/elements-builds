3f45be73dcab62ebb149ba61d59c7c119288c1f6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-18 01:00:07 +0000

    e5a7eb3 build: update cross-repo angular dependencies to v21.1.0-next.3

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-99ad18a
+ * @license Angular v21.1.0-next.4+sha-e5a7eb3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-99ad18a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e5a7eb3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
