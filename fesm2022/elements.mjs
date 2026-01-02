d249f9f954d2fc1d200c69e626072dd0c54ac92f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 10:41:56 +0000

    1765ebe Revert "refactor(core): Add `ngDevMode` guards and new sanitization error codes"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-d868a5b
+ * @license Angular v21.1.0-next.4+sha-1765ebe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-d868a5b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-1765ebe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
