10c6486e3615ef1b190af1f064364a903c031515
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 22:06:35 +0000

    df2aaaa docs(vscode-extension): Update developer docs to match current setup

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-1532be9
+ * @license Angular v21.1.0-next.4+sha-df2aaaa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-1532be9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-df2aaaa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
