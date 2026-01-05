2f4c07375dd88e982ff68b61e3e024c9202bfa36
GitHub <noreply@github.com>
2026-01-05 19:47:51 +0000

    5edceff feat(router): add controls for route cleanup

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-e6631f0
+ * @license Angular v21.1.0-next.4+sha-5edceff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e6631f0');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-5edceff');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
