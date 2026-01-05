b75a92940c60d1ba99d19c942ba312463030f03d
kirjs <kirts@google.com>
2026-01-05 17:41:52 +0000

    9494b56 docs(docs-infra): sync heading colors between docs and API pages

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-7e9dc56
+ * @license Angular v21.0.6+sha-9494b56
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7e9dc56');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-9494b56');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
