ff299493d484d6dd4f5a592b401a2e31b96c0c06
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 20:26:10 +0000

    1530814 docs(docs-infra): Add package filter to the API list

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-5edceff
+ * @license Angular v21.1.0-next.4+sha-1530814
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-5edceff');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-1530814');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
