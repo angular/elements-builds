59d46d27369400f8dc2fb27fbcbbbd1a399c63df
kirjs <kirts@google.com>
2026-01-05 17:00:22 +0000

    6c911ad docs: fix reference to parent from children resolvers

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-71a4373
+ * @license Angular v21.0.6+sha-6c911ad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-71a4373');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-6c911ad');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
