c744cc9dc4ecb63035a539c3b9bf8e8824fa07a1
kirjs <kirts@google.com>
2026-01-06 17:27:12 +0000

    8e80874 fix(compiler): better types for a few expression AST nodes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-45bfd82
+ * @license Angular v21.0.6+sha-8e80874
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-45bfd82');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-8e80874');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
