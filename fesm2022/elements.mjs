42f439279b16d7ec88b5c64dbf547031593204e1
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 15:16:55 +0000

    db470f0 refactor(router): Remove internal rxjs recognize implementation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-71cde39
+ * @license Angular v21.1.0-next.4+sha-db470f0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-71cde39');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-db470f0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
