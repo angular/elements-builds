89dacd220767fd14a9dd3cd7b53694dbc0884dd6
kirjs <kirts@google.com>
2026-01-06 15:39:04 +0000

    45bfd82 docs(forms): add documentation links to Signal Forms API

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-9d33cf3
+ * @license Angular v21.0.6+sha-45bfd82
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-9d33cf3');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-45bfd82');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
