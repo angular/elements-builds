2cd631fd6eaa10a5bb277126f9a3ea4a1764da53
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 18:23:49 +0000

    82a2de2 refactor(forms): bind native properties on interop controls

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-10da2f9
+ * @license Angular v21.1.0-next.4+sha-82a2de2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-10da2f9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-82a2de2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
