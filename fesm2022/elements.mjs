8d11e21460e1d9bf1b5431a79e535abe05c13ebd
kirjs <kirts@google.com>
2026-01-07 19:16:59 +0000

    35fe58f docs(vscode-extension): update angular.io links to angular.dev in messages

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-c3f3ebe
+ * @license Angular v21.0.6+sha-35fe58f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c3f3ebe');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-35fe58f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
