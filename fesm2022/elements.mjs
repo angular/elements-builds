37a3ce28b4e884abb05e7ed9656b44a313239561
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 19:16:25 +0000

    8c8c5b2 docs(vscode-extension): update angular.io links to angular.dev in messages

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-0d6ac92
+ * @license Angular v21.1.0-next.4+sha-8c8c5b2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-0d6ac92');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-8c8c5b2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
