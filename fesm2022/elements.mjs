3b76e74763e4948b5d6bd7282812ac32ec3f75e7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-07 23:51:54 +0000

    0ad3adc fix(compiler): Support empty cases

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-bb0dc8c
+ * @license Angular v21.1.0-next.4+sha-0ad3adc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-bb0dc8c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-0ad3adc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
