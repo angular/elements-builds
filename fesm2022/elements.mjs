785d3bf78cd13967716874190281769467cc74a3
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 20:58:10 +0000

    18810d8 build: update lock file maintenance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-46dbd18
+ * @license Angular v21.1.0-next.4+sha-18810d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-46dbd18');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-18810d8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
