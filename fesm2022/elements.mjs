2b6a96270b8e669a6c1eed6fc7de6f4d2afa5ab2
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:40:23 +0000

    b17f93b docs(docs-infra): Use category for signal forms guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-640693d
+ * @license Angular v21.1.0-next.4+sha-b17f93b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-640693d');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-b17f93b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
