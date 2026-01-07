9ce20040f9106f27f8c7343d5963820a931349fa
kirjs <kirts@google.com>
2026-01-07 19:03:24 +0000

    c3f3ebe docs: remove Component suffix from examples in DI & Drag and Drop

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-61370dc
+ * @license Angular v21.0.6+sha-c3f3ebe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-61370dc');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c3f3ebe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
