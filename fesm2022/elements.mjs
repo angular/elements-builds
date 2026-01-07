7c4590c86e86a4df8bb2b521ab3e299af49f2805
Jan Olaf Martin <jan.krems@gmail.com>
2026-01-07 21:32:40 +0000

    2c93286 release: bump Angular DevTools version to 1.7.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-18810d8
+ * @license Angular v21.1.0-next.4+sha-2c93286
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-18810d8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-2c93286');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
