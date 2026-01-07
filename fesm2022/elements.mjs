f3629c0dbffd97ed47c176074edc4d6168f3ab1b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 16:50:31 +0000

    97fd1de Revert "refactor(router): Add support for precommitHandler in Navigation integration"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-b17f93b
+ * @license Angular v21.1.0-next.4+sha-97fd1de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-b17f93b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-97fd1de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
