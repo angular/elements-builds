156c95a96eae0088ffd791110220b1f30e5aeb31
kirjs <kirts@google.com>
2026-01-07 14:14:57 +0000

    0ff2d5a docs: fixed incorrect link in migrating-to-vitest.md

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-4161161
+ * @license Angular v21.0.6+sha-0ff2d5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-4161161');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-0ff2d5a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
