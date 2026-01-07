08b8b3933f7e45a1e913ee5f0daec207b3fccd3a
kirjs <kirts@google.com>
2026-01-07 20:14:25 +0000

    ff53175 refactor(forms): remove `customError()`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-c3adc5a
+ * @license Angular v21.0.6+sha-ff53175
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c3adc5a');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-ff53175');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
