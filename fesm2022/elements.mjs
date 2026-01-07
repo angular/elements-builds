6fde15575841ec6a7d4392cf4a7d70959f1864c5
kirjs <kirts@google.com>
2026-01-07 17:43:37 +0000

    7c74674 fix(core): avoid leaking view data in animations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-7738eb9
+ * @license Angular v21.0.6+sha-7c74674
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7738eb9');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7c74674');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
