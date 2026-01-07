e5d71698671fca20dee4693d75ab46f9f539d47b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 21:49:14 +0000

    8fd585c release: cut the v21.0.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-ff53175
+ * @license Angular v21.0.7+sha-8fd585c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-ff53175');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-8fd585c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
