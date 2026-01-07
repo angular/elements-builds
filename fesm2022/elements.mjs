dfd7416fb192c6af6b59c147b13e4c538112ba1e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:14:14 +0000

    f811c6c docs: fixed incorrect link in migrating-to-vitest.md

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-174d2da
+ * @license Angular v21.1.0-next.4+sha-f811c6c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-174d2da');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f811c6c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
