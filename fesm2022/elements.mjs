9e2d35bf67c80427ed598315226488ada392b545
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 18:45:46 +0000

    241bdeb feat(devtools): create an info snackbar when a user logs a property

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-a6d7ee0
+ * @license Angular v21.1.0-next.4+sha-241bdeb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-a6d7ee0');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-241bdeb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
