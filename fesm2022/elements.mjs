95f5391af0d78a497f1318eaf419926d47e19614
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-07 22:39:21 +0000

    f5bad8e refactor(core): Improve tree-shakable `RuntimeError` error for `INVALID_FIELD_DIRECTIVE_HOST`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.7+sha-8fd585c
+ * @license Angular v21.0.7+sha-f5bad8e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.7+sha-8fd585c');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-f5bad8e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
