3b2ba98a3bf62b32ec8e97a5d3432bbdd8689041
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 16:39:57 +0000

    f727f8e docs: fix broken anchor link for Angular versioning section

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-12fccc5
+ * @license Angular v21.1.0-next.4+sha-f727f8e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-12fccc5');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f727f8e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
