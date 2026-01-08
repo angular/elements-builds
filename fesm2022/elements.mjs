065d784373b305d92adc114ec54d3df59d11fc0a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 21:32:59 +0000

    696adbc refactor(core): Use the provided Document value rather than global in FakeNavigation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-a0904c8
+ * @license Angular v21.0.8+sha-696adbc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-a0904c8');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-696adbc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
