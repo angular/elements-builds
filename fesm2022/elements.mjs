a3d79bfaffb638c4692ef7803b8162bd1ead4f68
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:42:52 +0000

    a6efb77 docs(docs-infra): avoid persistent AbortSignal listener in wait debounce in search

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-a63f6d0
+ * @license Angular v21.0.8+sha-a6efb77
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-a63f6d0');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-a6efb77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
