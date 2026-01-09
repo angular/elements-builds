748d14ae840f85d528758677de4da0f648d2ef82
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 16:02:30 +0000

    491a156 docs(docs-infra): improve label fallback logic in search results

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-baee12d
+ * @license Angular v21.0.8+sha-491a156
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-baee12d');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-491a156');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
