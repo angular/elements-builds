7a3347efbd113010c1ef5d796052e611eeaf2a17
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 22:44:00 +0000

    615a77b refactor(compiler-cli): update error message links to versioned docs (#66374)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-5bc072e
+ * @license Angular v21.0.8+sha-615a77b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5bc072e');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-615a77b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
