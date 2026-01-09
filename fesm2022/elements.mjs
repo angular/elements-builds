2210dc033f916135eb89065e1dfb4cf63c39ffe1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 18:46:08 +0000

    5bc072e ci: add pnpm and typescript exclusions to a Renovate package rule.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-018edd5
+ * @license Angular v21.0.8+sha-5bc072e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-018edd5');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5bc072e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
