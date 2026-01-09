58adf1faeef8706e1841a49b37582a32c82d8cd6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:35:28 +0000

    da364d2 refactor(router): Add support for precommitHandler in Navigation integration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-4fb24ca
+ * @license Angular v21.2.0-next.0+sha-da364d2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4fb24ca');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-da364d2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
