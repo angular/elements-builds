095070f501f3500e538cf3ead93dc7f0cf552236
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 16:59:24 +0000

    7d985ce docs(forms): Clarify returning errors from submit functions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e66aeac
+ * @license Angular v21.2.0-next.0+sha-7d985ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e66aeac');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7d985ce');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
