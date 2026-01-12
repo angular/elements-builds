e05592577a9c67b03e016d976e521c6e58703a0c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 18:11:19 +0000

    511d9ca docs: remove Component suffix in animations, HTTP, and SSR guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-b228355
+ * @license Angular v21.2.0-next.0+sha-511d9ca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b228355');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-511d9ca');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
