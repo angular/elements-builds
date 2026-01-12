96e36bb4fa08d305ad834517b73b75c4ef8d39ac
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:20:26 +0000

    1f47e88 build(docs-infra): Update dependency @xterm/xterm to v6

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-d1f6bcd
+ * @license Angular v21.1.0-rc.0+sha-1f47e88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-d1f6bcd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1f47e88');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
