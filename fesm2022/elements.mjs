3d9bb5d6e259b6d19127ddeb584f97c124f83d7e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:54:29 +0000

    49e18e5 fix(docs-infra): improve symbolic green contrast for WCAG compliance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-dfd8c52
+ * @license Angular v21.0.8+sha-49e18e5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-dfd8c52');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-49e18e5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
