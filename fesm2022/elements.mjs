fe0fb52aa22a58725e583422f08491eba9864245
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:46:29 +0000

    a056413 docs(docs-infra): Show API decorators docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ad699ce
+ * @license Angular v21.2.0-next.0+sha-a056413
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ad699ce');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a056413');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
