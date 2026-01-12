264045b03704aa068aaecd7ec29dd5432e69c8c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:22:56 +0000

    4871131 docs: improve separation between enter and leave example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-c7d6053
+ * @license Angular v21.1.0-rc.0+sha-4871131
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-c7d6053');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-4871131');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
