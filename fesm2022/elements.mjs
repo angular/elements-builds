59e83bf92c8f3f9e34d0dde4532ea74037b4fc7b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:36:28 +0000

    dfd8c52 docs: update testing guide to include Vitest and improve authGuard test setup

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-5b0e705
+ * @license Angular v21.0.8+sha-dfd8c52
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5b0e705');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-dfd8c52');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
