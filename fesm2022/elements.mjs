39b6477a5bd98f6a6d6c9dcdb60ffeba59bc127f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:34:23 +0000

    77226dd docs: change language section to "community translations"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-49e18e5
+ * @license Angular v21.0.8+sha-77226dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-49e18e5');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-77226dd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
