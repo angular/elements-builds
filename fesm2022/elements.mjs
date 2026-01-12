91104f8cda652da69348e3f64ee69335731ed5c7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:36:34 +0000

    204aaca docs: update testing guide to include Vitest and improve authGuard test setup

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-4871131
+ * @license Angular v21.1.0-rc.0+sha-204aaca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-4871131');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-204aaca');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
