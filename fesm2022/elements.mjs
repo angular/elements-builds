dd7bb846f49e748f96ecce2041da50561fdc837a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 17:10:12 +0000

    871fa1b docs: update OnChanges API reference w/ signal input

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-6e6a022
+ * @license Angular v21.2.0-next.0+sha-871fa1b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6e6a022');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-871fa1b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
