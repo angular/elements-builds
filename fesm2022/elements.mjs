343202067614e7776c6be1c34a4cbdffac8f7c40
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:24:37 +0000

    99b10e4 build: update cherrypick changelog to stash

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-01e5bab
+ * @license Angular v21.0.8+sha-99b10e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-01e5bab');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-99b10e4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
