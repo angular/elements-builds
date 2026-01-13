65da6654922ad4b706b10eae1edcbe2d25ed766d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 00:10:39 +0000

    6e8f142 build: improve changelog generation and tag fetching in release script

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-d5caac6
+ * @license Angular v21.0.8+sha-6e8f142
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d5caac6');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-6e8f142');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
