af5dbd5cf19b673ca2063f7d9f1a90dd5334675c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:14:49 +0000

    a649fc8 fix(vscode-extension): add syntax highlighting for arrow functions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-114d03f
+ * @license Angular v21.1.0-rc.0+sha-a649fc8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-114d03f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-a649fc8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
