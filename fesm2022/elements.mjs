63cb144d47f55f35b04ad315624aa7198a7404f1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 19:49:23 +0000

    cb5879f refactor(core): remove ng when invoking `enableProdMode`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-029685b
+ * @license Angular v21.2.0-next.0+sha-cb5879f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-029685b');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-cb5879f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
