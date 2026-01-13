df87e74ca52550114e80c27b12d5e54d3ba89772
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:36:29 +0000

    559797c docs: update hierarchical-dependency-injection.md

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-76a6984
+ * @license Angular v21.1.0-rc.0+sha-559797c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-76a6984');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-559797c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
