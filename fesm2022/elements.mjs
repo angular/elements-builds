a5f5cafdcb73b4448169533f328952add35de44e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 21:12:40 +0000

    227acdd build: Update vscode release script

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-cb5879f
+ * @license Angular v21.2.0-next.0+sha-227acdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-cb5879f');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-227acdd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
