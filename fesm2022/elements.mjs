0ca42c0734f31cc1484666d73f74734b5a0121d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:05:21 +0000

    79e6dfd docs(docs-infra): use Signals Forms focusBoundControl

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-de9277b
+ * @license Angular v21.0.8+sha-79e6dfd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-de9277b');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-79e6dfd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
