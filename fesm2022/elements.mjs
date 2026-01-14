f0c0286d92eeb0ac28652678fc6b550326c834cb
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:29:28 +0000

    d710ebe docs(docs-infra): Exempts animation-related symbols from linking

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-b0b341f
+ * @license Angular v21.0.8+sha-d710ebe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-b0b341f');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d710ebe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
