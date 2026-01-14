9e8bab060c9aa4e6b7cb850dce2ec4c69feadbe2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:16:20 +0000

    b0b341f docs: add IMPORTANT prefix to pipe usage warning and

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-79e6dfd
+ * @license Angular v21.0.8+sha-b0b341f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-79e6dfd');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-b0b341f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
