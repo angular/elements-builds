f3f1058fa41cfb132ea14018be9cc01e2a50e64c
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 17:03:52 +0000

    c0446da docs(docs-infra): prevent heading from linking symbols

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-ae4e2e0
+ * @license Angular v21.1.0+sha-c0446da
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-ae4e2e0');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-c0446da');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
