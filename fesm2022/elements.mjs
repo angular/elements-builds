e16150cf7838870d938078beeb20007d5c136a41
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 21:47:49 +0000

    d9c980a build: initial test of TypeScript 6

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-417c385
+ * @license Angular v21.2.0-next.0+sha-d9c980a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-417c385');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d9c980a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
