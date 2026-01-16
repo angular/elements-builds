5fd3eecd06916aa39d07ab4a91324b207d1ffe07
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 19:11:05 +0000

    e55260f build: update lock file

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-52bc020
+ * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-52bc020');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e55260f-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
