283eaf37a13200b2aab51380600b282ae0a2bfbe
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 21:33:00 +0000

    5d46554 docs: Add section on `animate.leave` for clarity

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
+ * @license Angular v21.2.0-next.0+sha-5d46554-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e55260f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-5d46554-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
