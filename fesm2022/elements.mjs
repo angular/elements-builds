194d10469867c41317ea5c39573875b4dfbe7679
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 17:24:18 +0000

    54fc393 build: update dependency @csstools/css-calc to v3

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d9c980a
+ * @license Angular v21.2.0-next.0+sha-54fc393
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d9c980a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-54fc393');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
