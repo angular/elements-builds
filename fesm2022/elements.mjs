867c80a05ac0e9bdcdafba0fc165dea4442ce7c5
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:41:10 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-6b34e5f-with-local-changes
+ * @license Angular v21.1.0+sha-82593de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-6b34e5f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-82593de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
