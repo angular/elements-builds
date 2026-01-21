a1c7d0e1f4f38eb3d2e8240e5bd44f40df5155da
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 00:25:53 +0000

    3c8d697 fix(docs-infra): don't use private symbol

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-e664249
+ * @license Angular v21.1.0+sha-3c8d697
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-e664249');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-3c8d697');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
