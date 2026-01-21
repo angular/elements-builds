801216a60f9bfca61d77393c242f49665d454e2b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 22:05:40 +0000

    f577f8c release: cut the v21.1.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-643f0f4
+ * @license Angular v21.1.1+sha-f577f8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-643f0f4');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f577f8c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
