d74b70f43a91af2bd6747b6dc4aef40337048ce1
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:35:49 +0000

    6284126 refactor(common): cleanup unused code in doc regions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94a8f79
+ * @license Angular v21.2.0-next.0+sha-6284126
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94a8f79');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6284126');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
