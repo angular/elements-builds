ee264cd78f1bdf50f48aec0d6007e2858c914dc1
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 19:44:24 +0000

    1884226 docs(docs-infra): Handle additional description format

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-223ff74
+ * @license Angular v21.1.0+sha-1884226
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-1884226');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
