17e2f66413b60635f205277a0d5ae706edbd41c9
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:10:35 +0000

    c9043fb refactor(compiler-cli): remove deprecated signature usage

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-61dbdee
+ * @license Angular v21.1.1+sha-c9043fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-61dbdee');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c9043fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
