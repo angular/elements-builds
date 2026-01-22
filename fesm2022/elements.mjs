182906d8453f971ff257a1e97808e03ac327b64b
GitHub <noreply@github.com>
2026-01-22 18:37:02 +0000

    27eeff9 release: bump VSCode extension version to 21.1.1 (#66714)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f577f8c
+ * @license Angular v21.1.1+sha-27eeff9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f577f8c');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-27eeff9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
