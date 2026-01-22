b2a25ba0a0a39047417338cb62df12d14175e1f4
Andrew Scott <atscott01@gmail.com>
2026-01-22 23:36:09 +0000

    d1135f5 build: update dependency aspect_rules_js to v2.9.2

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-30b50e7
+ * @license Angular v21.2.0-next.0+sha-d1135f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-30b50e7');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d1135f5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
