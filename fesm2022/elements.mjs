d3cfd57360cc327e623894f2dc6a960d1f39f813
GitHub <noreply@github.com>
2026-01-22 21:18:18 +0000

    145e77d build: Update prettier to 3.8

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-dbc1452
+ * @license Angular v21.2.0-next.0+sha-145e77d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-dbc1452');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-145e77d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
