1c2f04fbac722318703dacdd13eb4ef74b2de74f
Andrew Scott <atscott@google.com>
2026-01-23 19:53:56 +0000

    e6ea328 docs: update `bootstrapApplication` docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-b203ab0
+ * @license Angular v21.1.1+sha-e6ea328
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b203ab0');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-e6ea328');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
