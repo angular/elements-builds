d79473e31d929a1a545258fc9ae282a494d9f3a0
Andrew Scott <atscott@google.com>
2026-01-23 01:06:53 +0000

    f7eb996 docs: update signal form tutorials to use `FormField`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-d9e7ab3
+ * @license Angular v21.1.1+sha-f7eb996
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-d9e7ab3');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f7eb996');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
