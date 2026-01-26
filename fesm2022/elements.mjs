2bed37902a82155421463f6ee1770e83c4bb5aeb
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:40:39 +0000

    b51bab5 feat(router): Add partial ActivatedRouteSnapshot information to `canMatch` params

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-899df39
+ * @license Angular v21.2.0-next.0+sha-b51bab5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-899df39');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b51bab5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
