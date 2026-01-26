496e419aab59bbe9ed6b9cd2ebf65854cf6df06e
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:50:55 +0000

    3151bc3 docs: Updates form documentation (#66732)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-b4227fb
+ * @license Angular v21.1.1+sha-3151bc3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b4227fb');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-3151bc3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
