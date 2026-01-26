ed2a9ab1fe956b5b2c80da5e1c2db52fa446685e
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:04:49 +0000

    c133a2c docs: normalize spacing in animation example boxes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-3151bc3
+ * @license Angular v21.1.1+sha-c133a2c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-3151bc3');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c133a2c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
