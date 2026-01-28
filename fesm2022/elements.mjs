829a8e07d8f46d959d8eefb3e6b45823d3531b17
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:30:00 +0000

    7bb241f refactor(language-service): introduce withFallback helper in ts_plugin

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e682e53
+ * @license Angular v21.2.0-next.0+sha-7bb241f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e682e53');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7bb241f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
