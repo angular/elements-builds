a27467379f762515d3775162a6003d382864e984
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:20:24 +0000

    31e6f0d test(forms): submit behavior while validation is pending

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-6601f06
+ * @license Angular v21.1.1+sha-31e6f0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-6601f06');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-31e6f0d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
