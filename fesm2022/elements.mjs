fbc8c5b553853747089e5a0abb7919a11ac5dc37
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:45:41 +0000

    3de87e1 build: update pnpm to v10.28.2

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-affd236
+ * @license Angular v21.1.1+sha-3de87e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-affd236');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-3de87e1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
