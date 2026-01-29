977ba2a1b73731705041ba5d98bf01534df64048
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:12:31 +0000

    0a4c647 refactor(core): Remove unsued properties in differ factories

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-cfe7340
+ * @license Angular v21.1.2+sha-0a4c647
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-cfe7340');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-0a4c647');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
