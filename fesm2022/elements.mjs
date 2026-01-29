5e05c87a30c300b9e7a23044422d8e3dcf4e7614
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:26:39 +0000

    a67e007 refactor(forms): move control logic into `FormField` directive

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-6990f88
+ * @license Angular v21.2.0-next.1+sha-a67e007
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-6990f88');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-a67e007');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
