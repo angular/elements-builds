569dad34b5ac5d7c232bb4b4f2c4987af2e40c63
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 17:27:20 +0000

    b1bf535 fix(forms): Resolves debounce promise on abort in debounceForDuration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-26d1215
+ * @license Angular v21.2.0-next.1+sha-b1bf535
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-26d1215');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-b1bf535');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
