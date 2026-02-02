bef529170998b75e85827ac1e094d753d2d44b1a
GitHub <noreply@github.com>
2026-02-02 18:09:43 +0000

    3d09d8e docs: update zoneless guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8d5210c
+ * @license Angular v21.2.0-next.1+sha-3d09d8e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8d5210c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-3d09d8e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
