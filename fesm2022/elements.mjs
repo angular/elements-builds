331114a291e0f71131ee1a869c6af9d0b6870ec7
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 19:17:36 +0000

    e5110b4 fix(core): export DirectiveWithBindings

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-575f8a3
+ * @license Angular v21.1.2+sha-e5110b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-575f8a3');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-e5110b4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
