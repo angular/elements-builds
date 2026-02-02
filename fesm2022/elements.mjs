4d6783314409fbab767d513b5a27440a2a4898dc
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:22:53 +0000

    575f8a3 docs(animations): fix small typo in animations migrations docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-eb28032
+ * @license Angular v21.1.2+sha-575f8a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-eb28032');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-575f8a3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
