21b95a747a849724d7efc2a1f78cc3183da268ba
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:50:19 +0000

    2cf4da0 fix(core): hold constructors weakly in DepsTracker cache

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-f15d33d
+ * @license Angular v21.1.2+sha-2cf4da0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-f15d33d');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-2cf4da0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
