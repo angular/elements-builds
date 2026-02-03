86f30b898cbe88a72abb2ca9fd79e058edfcfc84
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:39:28 +0000

    f15d33d docs(docs-infra): extract call signature return type.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-6505a26
+ * @license Angular v21.1.2+sha-f15d33d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-6505a26');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-f15d33d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
