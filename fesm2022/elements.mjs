c5f773be7580eb5cf495e549de3ca9ba194ba3c8
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 22:29:47 +0000

    680d99b refactor(forms): work around internal issue

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-cab5ddd
+ * @license Angular v21.2.0-next.1+sha-680d99b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-cab5ddd');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-680d99b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
