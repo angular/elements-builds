5ace8174475dec798570f8ac44227c227cc81974
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 19:18:22 +0000

    c633697 Revert "fix(compiler-cli): update diagnostic to flag no-op arrow functions in listeners"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-be4ffb6
+ * @license Angular v21.1.3+sha-c633697
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-be4ffb6');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c633697');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
