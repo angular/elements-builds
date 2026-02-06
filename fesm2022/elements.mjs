30d31bff0dc469efd94a93f78cb2f03ffc14b3bb
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 15:47:31 +0000

    24c0c5a feat(forms): support signal-based schemas in validateStandardSchema

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-6fa2270
+ * @license Angular v21.2.0-next.2+sha-24c0c5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-6fa2270');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-24c0c5a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
