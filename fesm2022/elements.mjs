d18134644928b7608587cb9e616bb0e2d39735ae
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 18:25:01 +0000

    460dbbb docs: remove non-existant method

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-15c71fb
+ * @license Angular v21.2.0-next.2+sha-460dbbb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-15c71fb');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-460dbbb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
