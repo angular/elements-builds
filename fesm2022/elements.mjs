eb12b0525e1697f7b028cb88e449e9af4cf20ab2
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:02:26 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-70668be
+ * @license Angular v21.1.3+sha-0729181
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-70668be');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-0729181');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
