be2b4691bd0bb341cdc728b51aa958c32ac006d4
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:55:58 +0000

    51cc914 feat(common): support height in ImageLoaderConfig and built-in loaders

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-e53c8ab
+ * @license Angular v21.2.0-next.2+sha-51cc914
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-e53c8ab');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-51cc914');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
