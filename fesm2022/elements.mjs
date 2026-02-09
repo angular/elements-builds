09722cdf5b25b9b772e40af37ce6f3e66904d8ef
Andrew Kushnir <akushnir@google.com>
2026-02-09 16:03:30 +0000

    c298694 refactor(benchpress): remove zone-based testing utilities

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-054972c
+ * @license Angular v21.1.3+sha-c298694
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-054972c');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c298694');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
