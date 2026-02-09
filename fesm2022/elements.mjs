adf6942e99a1ed37da3ad44446f6efc7cea3661c
Andrew Kushnir <akushnir@google.com>
2026-02-09 20:40:32 +0000

    3905015 fix(http): correctly parse ArrayBuffer and Blob in transfer cache

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-c298694
+ * @license Angular v21.1.3+sha-3905015
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c298694');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-3905015');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
