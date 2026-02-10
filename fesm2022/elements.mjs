32710b536518d8f7457f257dac381ab7661a9cc7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 00:02:29 +0000

    c6bbf40 docs(forms): replace 'legacy' with 'existing' in migration guide and navigation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-19542a3
+ * @license Angular v21.1.3+sha-c6bbf40
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-19542a3');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c6bbf40');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
