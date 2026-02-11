f45458f4740bf83408ba1ff52fb8fcd60269c8fe
Matthew Beck <me@mattrb.com>
2026-02-11 21:45:53 +0000

    8868130 build: add skills in antigravity

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-289ddc9
+ * @license Angular v21.1.4+sha-8868130
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-289ddc9');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-8868130');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
