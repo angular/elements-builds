da1ff6f1b1712b0612477f5b281fc03f0138ea59
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:49:04 +0000

    7150df2 build: update cross-repo angular dependencies to v21.1.4

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-6060aad
+ * @license Angular v21.1.4+sha-7150df2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-6060aad');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7150df2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
