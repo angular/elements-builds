67659b66da76bae39aaf788e2e1690bf14033a57
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-11 18:02:47 +0000

    39cff9c release: cut the v21.2.0-next.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-ad81510
+ * @license Angular v21.2.0-next.3+sha-39cff9c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-ad81510');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-39cff9c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
