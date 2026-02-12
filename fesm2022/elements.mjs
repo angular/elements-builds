a7db72975e3affb6dcb9a87756b88cef0d111ca4
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 18:23:34 +0000

    5b59dbd build: lock file maintenance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-06d94ac
+ * @license Angular v21.2.0-next.3+sha-5b59dbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-06d94ac');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-5b59dbd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
