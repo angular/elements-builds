460bc9d237c5228933ccee214d61b2440eefd996
Matthew Beck <me@mattrb.com>
2026-02-13 18:12:10 +0000

    5a86528 docs: Adds new agent skills to Angular

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-23c0858
+ * @license Angular v21.1.4+sha-5a86528
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-23c0858');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-5a86528');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
