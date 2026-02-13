726e4045959e7c51101c664ac90f190eaf869085
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 23:46:48 +0000

    a37b6c5 docs: fix formatting in pipes.md example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a6a0347
+ * @license Angular v21.2.0-next.3+sha-a37b6c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a6a0347');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
