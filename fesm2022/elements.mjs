f656316778e8a11a3d599390be132fd5e118b432
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 00:59:07 +0000

    ddfc833 refactor(common): improve image directive typings

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-5b59dbd
+ * @license Angular v21.2.0-next.3+sha-ddfc833
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-5b59dbd');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-ddfc833');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
