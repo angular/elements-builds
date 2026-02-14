e4ae9f13cff4764408ddfffee1a48dd37cd8e138
Matt Beck <mattrb@google.com>
2026-02-14 00:55:58 +0000

    a3588a0 refactor(compiler-cli): Add skeleton tests around source->source compiler transform mode

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-670dee9
+ * @license Angular v21.1.4+sha-a3588a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-670dee9');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a3588a0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
