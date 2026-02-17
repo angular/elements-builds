f741026a2c228e8c78a76838d48ae0cb13639597
Matt Beck <mattrb@google.com>
2026-02-17 19:38:07 +0000

    9c1ec6c fix(zone.js): support passthrough of Promise.try API

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-fc873cd
+ * @license Angular v21.1.4+sha-9c1ec6c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-fc873cd');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-9c1ec6c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
