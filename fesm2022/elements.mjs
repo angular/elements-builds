b895acb933c47d3da530deea665d9d269a6db78d
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 21:08:32 +0000

    b481294 refactor(forms): remove unused generic type parameter

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-dea0819
+ * @license Angular v21.2.0-next.3+sha-b481294
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-dea0819');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b481294');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
