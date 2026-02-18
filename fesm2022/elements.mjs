4eba3ae1b86f9f7b124fa70f9f7485782b082fe9
GitHub <noreply@github.com>
2026-02-18 19:44:33 +0000

    9a9f4e7 docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-b8811b4
+ * @license Angular v21.2.0-next.3+sha-9a9f4e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9a9f4e7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
