911bea839914b68d2257d94571a3e2faaf08b113
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:28:58 +0000

    f9d6d98 release: cut the v21.1.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-b90a0fc
+ * @license Angular v21.1.5+sha-f9d6d98
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-b90a0fc');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-f9d6d98');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
