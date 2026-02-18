1fe2a1215d79944dfd3a9f304064efaadbef95e6
GitHub <noreply@github.com>
2026-02-18 19:10:24 +0000

    b8811b4 docs: add signal forms async operations guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-47486f8
+ * @license Angular v21.2.0-next.3+sha-b8811b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-47486f8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
