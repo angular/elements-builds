f448fe29440e329fa16298d7acaa6a963bb09e99
Douglas Parker <dgp1130@users.noreply.github.com>
2026-02-18 18:56:51 +0000

    47486f8 release: bump Angular DevTools version to 1.11.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-c36385c
+ * @license Angular v21.2.0-next.3+sha-47486f8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-c36385c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-47486f8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
