848d863141a6bf5cedd0f8edb7d6c23bc8d0f3ad
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 15:21:43 +0000

    680e1c0 docs: fix custom control metadata example in signal forms guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-8de0d25
+ * @license Angular v21.2.0-next.3+sha-680e1c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-8de0d25');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-680e1c0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
