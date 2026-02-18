20f9d51b36f6aeaefec8cf2cb8c1479e382e8fe5
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:15:40 +0000

    567f292 fix(forms): support custom controls as host directives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-9a9f4e7
+ * @license Angular v21.2.0-next.3+sha-567f292
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9a9f4e7');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-567f292');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
