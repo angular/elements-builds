51aaa6b21996a6da1ea39444d45bde55bfaa1ba7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 23:54:49 +0000

    cccaed0 docs(docs-infra): preserve content before docs-card-container in adev

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-9943cb7
+ * @license Angular v21.1.5+sha-cccaed0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-9943cb7');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-cccaed0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
