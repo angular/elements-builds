ed5884c2f6f46f3d1e3793a0d4c75a0cb735d8b9
GitHub <noreply@github.com>
2026-02-19 17:06:01 +0000

    a3731a1 refactor(devtools): rename directive-explorer component names and folder structure

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-1771d54
+ * @license Angular v21.3.0-next.0+sha-a3731a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-1771d54');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a3731a1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
