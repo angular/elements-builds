d9fb29457d0be603aa87bc4c134602835a06d34f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 19:28:58 +0000

    5cea357 docs: update changelog to note no changes in 21.1.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-79f1cd4
+ * @license Angular v21.2.0-rc.0+sha-5cea357
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-79f1cd4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-5cea357');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
