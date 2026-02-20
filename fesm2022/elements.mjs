cca0be4d576b09c611d530c1bf6affebfce5fd03
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:22:07 +0000

    3c4deaa refactor(common): log a warning when a JsonPipe receives a signal

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-c0cb604
+ * @license Angular v21.2.0-rc.0+sha-3c4deaa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-c0cb604');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-3c4deaa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
