6eb3d140f4124e6b0d93feac7bb70b0ed0c8a976
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 17:23:49 +0000

    246a984 feat(core): add TestBed.getFixture

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-f90e556
+ * @license Angular v22.0.0-next.0+sha-246a984
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-f90e556');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-246a984');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
