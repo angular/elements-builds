54b928b7b3ba0c4f11785abac99ac9dfb0834d35
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 19:06:46 +0000

    0f4ba4a ci: update ng-dev config and pullapprove

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-866da08
+ * @license Angular v21.1.5+sha-0f4ba4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-866da08');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0f4ba4a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
