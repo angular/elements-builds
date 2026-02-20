d7ecedbda4e0cef321e2ec57675f3bd965bbd209
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:05:06 +0000

    f68078b docs: add section on reactive context and async operations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-bc473cf
+ * @license Angular v21.3.0-next.0+sha-f68078b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-bc473cf');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-f68078b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
