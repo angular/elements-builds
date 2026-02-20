601bdca11204135aa66ab0be26999acf5e49514a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:33:50 +0000

    631f60d fix(forms): preserve parse errors when parse returns value

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-390efd5
+ * @license Angular v21.2.0-rc.0+sha-631f60d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-390efd5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-631f60d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
