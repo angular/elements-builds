f2c8c88071c6a1eef79b5aa1071f67cd706e4420
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:50:09 +0000

    c143de2 refactor(compiler-cli): improve diagnostic with help link

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-3b64e7d
+ * @license Angular v21.1.5+sha-c143de2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-3b64e7d');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-c143de2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
