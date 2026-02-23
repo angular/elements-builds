54995040e856ad48f5dcc350abea92d3bbf7d54a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:16:13 +0000

    a73d543 docs: Add a compiler entry to the roadmap

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-b9d3fe3
+ * @license Angular v21.2.0-rc.0+sha-a73d543
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-b9d3fe3');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-a73d543');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
