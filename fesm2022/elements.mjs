4365d084562ecc0c2f2c7dadd0c7a65529ff7c73
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:21:30 +0000

    30e486a fix(docs-infra): use shared heading ID generation logic

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-a73d543
+ * @license Angular v21.2.0-rc.0+sha-30e486a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-a73d543');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-30e486a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
