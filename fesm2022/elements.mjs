38511e3647cf85ff595090537fde6bd7f995f572
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:29:12 +0000

    24b39ab docs: update README to clarify app setup confirmation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0101922
+ * @license Angular v21.1.5+sha-24b39ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0101922');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b39ab');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
