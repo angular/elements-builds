88c9916fffc93e131354dd33353f83de6b14d802
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:06:04 +0000

    0101922 docs: fix roadmap introduction text

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-5c4d9b2
+ * @license Angular v21.1.5+sha-0101922
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-5c4d9b2');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0101922');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
