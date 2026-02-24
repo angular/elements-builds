eb7cf8a60707f0535bacee0898cf60d16ca7f1d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:07:07 +0000

    1a151f2 docs: add docs for `@switch` Exhaustive type checking

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-ab1c84e
+ * @license Angular v21.2.0-rc.0+sha-1a151f2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-ab1c84e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-1a151f2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
