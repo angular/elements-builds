c64d44824dd74fc34b6443ebb2521053112af532
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:08:58 +0000

    957466d docs(docs-infra): scroll home features section into view on tab change

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-01a1795
+ * @license Angular v22.0.0-next.0+sha-957466d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-01a1795');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-957466d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
