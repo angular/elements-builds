af4db29d128a0d421f94ffc482aca842a9b36677
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-25 16:18:16 +0000

    89e8661 refactor(common): log a warning when a KeyValuePipe receives a signal

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-c235b57
+ * @license Angular v21.1.5+sha-89e8661
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-c235b57');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-89e8661');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
