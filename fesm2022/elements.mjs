bd3ef6434bb44a447174041ea43a6a0816dee724
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 19:28:55 +0000

    0b6ac7f docs: release notes for the v21.1.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-0af53fb
+ * @license Angular v22.0.0-next.0+sha-0b6ac7f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-0af53fb');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-0b6ac7f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
