932f5cefbd0226fd63de1f084295765b5103f483
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 19:26:47 +0000

    523c29f release: cut the v21.1.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-89e8661
+ * @license Angular v21.1.6+sha-523c29f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-89e8661');
+const VERSION = /* @__PURE__ */new Version('21.1.6+sha-523c29f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
