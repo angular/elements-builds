b5d859278e20f8f66938f5695fa3198d99654abd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 20:28:04 +0000

    ac06941 build: update to latest sha of dev-infra repo

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-225a566
+ * @license Angular v21.2.0+sha-ac06941
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-225a566');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-ac06941');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
