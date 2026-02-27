408ee07693e83a584a4d31a4fcdb74a8d36d56ad
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-27 17:09:14 +0000

    9bb4f02 build: use different default port for dev-app

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-cda3e89
+ * @license Angular v22.0.0-next.0+sha-9bb4f02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-cda3e89');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-9bb4f02');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
