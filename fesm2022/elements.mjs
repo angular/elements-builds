3ace6cdc372fb255f34241b2289ee945caaa4cb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:53:14 +0000

    23cb9a4 refactor(forms): address feedback

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-fc5f9d0
+ * @license Angular v21.2.0+sha-23cb9a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-fc5f9d0');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-23cb9a4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
