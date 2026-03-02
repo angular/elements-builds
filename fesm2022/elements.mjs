2221ac5ee7de8a0f353f354c846bf2b764a6f319
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 19:14:59 +0000

    a79e8af docs: update doc for `provideZoneChangeDetection`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-23cb9a4
+ * @license Angular v21.2.0+sha-a79e8af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-23cb9a4');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-a79e8af');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
