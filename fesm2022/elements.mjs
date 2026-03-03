d6470dbfb869cfdfc03f11842cf616f3da6d023b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-03 17:18:17 +0000

    5e54506 docs: fix side navigation bar foreground colors in both dark and light modes #67379

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-1f51c61
+ * @license Angular v21.2.0+sha-5e54506
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-1f51c61');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-5e54506');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
