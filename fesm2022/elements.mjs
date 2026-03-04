b119b03a5aee3c2a90b84ddee0f614a7b03c7c21
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 18:16:26 +0000

    c25c84d docs: add customization options for browser URL in routing guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-f4160c2
+ * @license Angular v21.2.0+sha-c25c84d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f4160c2');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-c25c84d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
