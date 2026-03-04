d7818fd489ab4b0fc7450c4996a280abda3104cf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 16:09:36 +0000

    84e79f5 build: update pnpm to v10.30.3

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-657d532
+ * @license Angular v21.2.0+sha-84e79f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-657d532');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84e79f5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
