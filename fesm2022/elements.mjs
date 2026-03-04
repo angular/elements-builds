76b0d6c7ec1d49bd52da8f94a6a50fdddc7606cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 16:27:29 +0000

    b4ec3cc fix(core): prevent child animation elements from being orphaned

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-84e79f5
+ * @license Angular v21.2.0+sha-b4ec3cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84e79f5');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-b4ec3cc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
