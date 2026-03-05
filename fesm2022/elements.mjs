48e140390d241cb741249e5a98b9c764ecfeb89c
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:18:35 +0000

    c822bf8 fix(compiler-cli): always parenthesize object literals in TCB

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-fd3b441
+ * @license Angular v21.2.1+sha-c822bf8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-fd3b441');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-c822bf8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
