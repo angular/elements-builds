8e583189c0a3ed202ae9a6f69d54caaff37849e3
Andrew Scott <atscott01@gmail.com>
2026-03-06 19:05:24 +0000

    759ee22 build: update all github actions to v7

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-cc64def
+ * @license Angular v22.0.0-next.1+sha-759ee22
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-cc64def');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-759ee22');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
