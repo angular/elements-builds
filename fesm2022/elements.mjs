64ea56dab15a7caf3fa3f97eed341d063f3eb974
Andrew Scott <atscott01@gmail.com>
2026-03-06 17:54:44 +0000

    e7d1d8d refactor(core): add tracing for component creation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-c822bf8
+ * @license Angular v21.2.1+sha-e7d1d8d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-c822bf8');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-e7d1d8d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
