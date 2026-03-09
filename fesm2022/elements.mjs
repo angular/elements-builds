f9436e918004c2d67bdb6dce95ac188403619b35
Andrew Scott <atscott@google.com>
2026-03-09 17:36:23 +0000

    628f3f9 docs: add Turkish community translation of Angular documentation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-79ac570
+ * @license Angular v21.2.1+sha-628f3f9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-79ac570');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-628f3f9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
