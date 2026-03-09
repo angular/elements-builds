8395be20ef801a55bec50e7806719f6346738078
Andrew Scott <atscott01@gmail.com>
2026-03-09 18:29:19 +0000

    0588133 refactor(core): remove private export of `getDebugNode`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-bd72571
+ * @license Angular v21.2.2+sha-0588133
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-bd72571');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-0588133');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
