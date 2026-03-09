361cc80d29af42db027ea7dc1d8fd9b5ad0868ca
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:47:01 +0000

    4a9b715 test(forms): cover transformedValue without FormField context

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-c499c57
+ * @license Angular v21.2.2+sha-4a9b715
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-c499c57');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-4a9b715');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
