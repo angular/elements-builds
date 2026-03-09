9f2d820d0219d37388f9ac5f78a8694dde38d330
GitHub <noreply@github.com>
2026-03-09 20:31:35 +0000

    b918bed feat(core): allow debouncing signals

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-82b758e
+ * @license Angular v22.0.0-next.1+sha-b918bed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-82b758e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-b918bed');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
