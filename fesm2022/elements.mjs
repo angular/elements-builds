be1a2f57855731c05999de6c10308b24a50d1486
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:30:09 +0000

    1969338 docs(core): clarify provideZoneChangeDetection usage in v21+

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-9794e75
+ * @license Angular v22.0.0-next.1+sha-1969338
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-9794e75');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-1969338');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
