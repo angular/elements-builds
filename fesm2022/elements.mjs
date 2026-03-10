9b258b1b4e35c8b13ab7750567959259289fe9af
Andrew Scott <atscott01@gmail.com>
2026-03-10 16:29:10 +0000

    106cf4e docs: Update migration docs for vitest

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-024e307
+ * @license Angular v21.2.2+sha-106cf4e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-024e307');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-106cf4e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
