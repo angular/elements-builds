dfb5e92099abec53de816349aeb39dd65c47f202
Andrew Scott <atscott01@gmail.com>
2026-03-10 20:15:35 +0000

    02a617e build: update dependency @rollup/plugin-babel to v7

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-a675950
+ * @license Angular v22.0.0-next.1+sha-02a617e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-a675950');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-02a617e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
