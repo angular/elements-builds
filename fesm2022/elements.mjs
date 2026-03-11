b21be2d7b0a5e31556534eb9751229ac21bbeacc
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:00:11 +0000

    6c73aac refactor(common): Removes unused generic type parameters from `KeyValueDiffers`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-d45fced
+ * @license Angular v21.2.2+sha-6c73aac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-d45fced');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-6c73aac');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
