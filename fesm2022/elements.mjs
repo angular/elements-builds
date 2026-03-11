865f7f7e5aa4ee825ffc20d70a06d02d0dbb3e1d
Andrew Scott <atscott01@gmail.com>
2026-03-11 16:38:51 +0000

    7513558 docs: combine multiple documentation improvements into one PR

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-575f302
+ * @license Angular v21.2.2+sha-7513558
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-575f302');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-7513558');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
