aeea31e7d59bb4ab65d74b74f6e8889de08690e6
Andrew Scott <atscott01@gmail.com>
2026-03-11 16:48:44 +0000

    ed48c41 refactor(compiler-cli): simplifies `IncrementalCompilation.fresh`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-7513558
+ * @license Angular v21.2.2+sha-ed48c41
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-7513558');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-ed48c41');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
