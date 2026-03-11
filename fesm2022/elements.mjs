2fbe709775ed25c0d348419d628d5d6235f044e3
Andrew Scott <atscott01@gmail.com>
2026-03-11 19:10:22 +0000

    e759365 release: cut the v21.2.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-21b1c3b
+ * @license Angular v21.2.3+sha-e759365
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-21b1c3b');
+const VERSION = /* @__PURE__ */new Version('21.2.3+sha-e759365');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
