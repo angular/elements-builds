7f5140c8e9e43ab96f8b81d3051730ffec4c61a7
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 23:42:20 +0000

    17d8a88 refactor(core): Update determineLongestAnimation to account for playback rate

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-89c9a4d
+ * @license Angular v22.0.0-next.3+sha-17d8a88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-89c9a4d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-17d8a88');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
