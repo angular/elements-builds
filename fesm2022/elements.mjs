6c36235da7183830e9edeeb9beaffd5eb99ba3da
Matthew Beck <me@mattrb.com>
2026-03-12 21:05:34 +0000

    5d755be refactor: ensure modules with 'declare global' are correctly handled by Rollup

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-8017617
+ * @license Angular v21.2.4+sha-5d755be
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-8017617');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-5d755be');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
