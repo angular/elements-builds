d181640cf462fc18f93b9c6ec478d40d5fa3db3e
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:41:06 +0000

    89c9a4d feat(router): Add `options` optional parameter for `withComponentInputBinding`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-8edcf41
+ * @license Angular v22.0.0-next.3+sha-89c9a4d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-8edcf41');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-89c9a4d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
