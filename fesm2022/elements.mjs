d747c78c3818422ef6e791dbca44ce1296c78c70
Matthew Beck <me@mattrb.com>
2026-03-13 19:22:00 +0000

    21cab20 docs: combine multiple documentation typo fixes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-334ae10
+ * @license Angular v21.2.4+sha-21cab20
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-334ae10');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-21cab20');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
