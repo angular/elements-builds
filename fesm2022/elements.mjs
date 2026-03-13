4c75e774790243b504a3f4dda624efc92f93e274
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 18:58:54 +0000

    334ae10 fix(compiler): ensure generated code compiles

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-6f41efc
+ * @license Angular v21.2.4+sha-334ae10
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-6f41efc');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-334ae10');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
