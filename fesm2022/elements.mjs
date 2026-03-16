df717b2da7b1d8b458587f5c49619d22f911f427
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 16:19:23 +0000

    dc04465 fix(core): clean up dehydrated views during HMR component replacement

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-ada150c
+ * @license Angular v22.0.0-next.3+sha-dc04465
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-ada150c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-dc04465');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
