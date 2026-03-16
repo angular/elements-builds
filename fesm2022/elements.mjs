b47795e349312df982cd1c1d5e8956c03dbc7c08
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 20:54:31 +0000

    ca328ea docs: update cross-repo adev docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-faeeef6
+ * @license Angular v21.2.4+sha-ca328ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-faeeef6');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-ca328ea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
