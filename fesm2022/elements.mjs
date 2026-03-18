0b6fb3786bd8c9d2220b319db11addf9d563bac4
Matthew Beck <me@mattrb.com>
2026-03-18 00:45:39 +0000

    737777e docs: add Angular Developer Skill updates

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-076d41c
+ * @license Angular v21.2.4+sha-737777e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-076d41c');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-737777e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
