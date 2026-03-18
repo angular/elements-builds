3639803284deefa9a6a43be4583b6ccaff6dc051
Matthew Beck <me@mattrb.com>
2026-03-18 17:37:15 +0000

    851ef77 Revert "refactor(core): Ensure determineLongestAnimation is run synchronously after style applies"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-0e496c1
+ * @license Angular v21.2.4+sha-851ef77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-0e496c1');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-851ef77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
