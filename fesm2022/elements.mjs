1db7cfc918adc75eeb57010c1a99fbd5797a6041
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 22:34:51 +0000

    41b1410 feat(forms): support binding `number|null` to `<input type="text">`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-c9e6263
+ * @license Angular v22.0.0-next.4+sha-41b1410
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-c9e6263');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-41b1410');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
