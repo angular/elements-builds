9f469db212f5826bc6e12d267d202fb7bbcf5a2f
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:19:59 +0000

    831746e docs: Add callout about the Component interface.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-9182b62
+ * @license Angular v21.2.5+sha-831746e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-9182b62');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-831746e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
