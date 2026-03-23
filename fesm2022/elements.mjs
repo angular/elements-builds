bdce092d3a8eac1590f2676860cbdaccd1d87369
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:31:43 +0000

    b658acc docs: fix js doc of signal forms ignoreValidators option.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-8818095
+ * @license Angular v21.2.5+sha-b658acc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-8818095');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-b658acc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
