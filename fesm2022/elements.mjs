0a7e95414bfbfb63b5d7537064c38d52db04547e
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:42:41 +0000

    f46e8bd refactor(compiler): remove `fullTemplateTypeCheck` compiler option.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-2615f35
+ * @license Angular v22.0.0-next.4+sha-f46e8bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-2615f35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-f46e8bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
