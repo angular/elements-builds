b7b4329d924f5be50bffaaaf741c0ce55d6f0474
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 16:55:32 +0000

    293ac66 ci: support publishing dev-skills to snapshot repository

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-d2054c7
+ * @license Angular v22.0.0-next.4+sha-293ac66
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-d2054c7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-293ac66');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
