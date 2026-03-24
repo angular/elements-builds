dfe4847a83407c1c9d6a59ce10e5620aba5ea3f0
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 21:55:17 +0000

    e731bbd docs: clarify size budget configuration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-c7a1892
+ * @license Angular v21.2.5+sha-e731bbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-c7a1892');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-e731bbd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
