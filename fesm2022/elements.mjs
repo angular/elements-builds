f9f104941c6bf7c8c5761a05d0cc4df6db00673a
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 23:36:05 +0000

    eae8f7e feat(core): Set default Component changeDetection strategy to OnPush

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-050b14b
+ * @license Angular v22.0.0-next.4+sha-eae8f7e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-050b14b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-eae8f7e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
