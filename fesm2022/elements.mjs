1b43f668fb44e0714df24c0e0617540b537b1d80
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 21:26:34 +0000

    24e52d4 feat(forms): add debounce option to validateAsync and validateHttp

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-b5af15a
+ * @license Angular v22.0.0-next.4+sha-24e52d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-b5af15a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-24e52d4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
