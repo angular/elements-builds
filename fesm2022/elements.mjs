4b0cb2ca42f0526791203d15fbb792936910301c
GitHub <noreply@github.com>
2026-03-25 20:44:22 +0000

    b5af15a test(forms): support directive composition with `FormField`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-eecfa4c
+ * @license Angular v22.0.0-next.4+sha-b5af15a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-eecfa4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-b5af15a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
