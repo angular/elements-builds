93bd9363a5abbcd4a67fb4c8509105eb4bf119d5
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 20:14:09 +0000

    a6c49e0 refactor(language-service): Export template target from API

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-2c7d3b5
+ * @license Angular v21.2.5+sha-a6c49e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-2c7d3b5');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a6c49e0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
