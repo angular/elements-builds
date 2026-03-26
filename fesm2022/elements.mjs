24b0268432f252739dc1e2f827ca59a69f4f5dc0
Andrew Scott <atscott01@gmail.com>
2026-03-26 21:41:12 +0000

    b453c0c refactor(language-service): Update getTcbNodesOfTemplateAtPosition to be usable without compiler (#67898)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-807cabd
+ * @license Angular v21.2.6+sha-b453c0c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-807cabd');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-b453c0c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
