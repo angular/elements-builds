80afcc820f349e4f197c9f9a7c0f5f84405c9e09
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:44:52 +0000

    d268df6 build: remove unused `@nginfra/angular-linking` dependency

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-d4c085d
+ * @license Angular v22.0.0-next.5+sha-d268df6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-d4c085d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-d268df6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
