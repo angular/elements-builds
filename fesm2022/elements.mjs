003af8bd65ff90e68c1fcb9c77d09f3d020f6978
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 15:16:00 +0000

    bba5ed8 fix(compiler-cli): prevent recursive scope checks for invalid NgModule imports

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-56c872d
+ * @license Angular v21.2.6+sha-bba5ed8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-56c872d');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-bba5ed8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
