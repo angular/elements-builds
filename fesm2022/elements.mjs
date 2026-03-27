0edc1ece461b97cf4990717f52d2463c33711f9d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 15:06:20 +0000

    12b3a1a refactor(migrations):  keep the `http-xhr-backend` migration idempotent

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-43ef717
+ * @license Angular v22.0.0-next.5+sha-12b3a1a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-43ef717');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-12b3a1a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
