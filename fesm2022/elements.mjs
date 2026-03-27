8887dd6505bd990b8cbdc9127f086055d14b0b7a
Andrew Scott <atscott01@gmail.com>
2026-03-27 18:47:22 +0000

    86bbc94 docs: add agent skills documentation and new readme

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-9d79ec6
+ * @license Angular v22.0.0-next.5+sha-86bbc94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-9d79ec6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-86bbc94');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
