2bcae8c86a347b4e7155a5d65e152bf844b861b0
GitHub <noreply@github.com>
2026-03-27 16:24:14 +0000

    9d79ec6 docs(docs-infra): introduce a custom UrlSerializer

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-fcd0bb0
+ * @license Angular v22.0.0-next.5+sha-9d79ec6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-fcd0bb0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-9d79ec6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
