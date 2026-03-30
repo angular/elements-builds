22ce7c759645a90e764e95ee5b3b536ce74ec531
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:17:48 +0000

    efbba94 docs: align list formatting and improve emphasis

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-94b1b53
+ * @license Angular v21.2.6+sha-efbba94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-94b1b53');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-efbba94');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
