51e20a718cf899468d458a7406ff64d1f48eece4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:21:33 +0000

    9ea8cb6 docs(docs-infra): sanitize markdown tooltip in Code editor

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-efbba94
+ * @license Angular v21.2.6+sha-9ea8cb6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-efbba94');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-9ea8cb6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
