d8c602ca2757ba460df92b8f31d2338f6ef82e72
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 10:00:49 +0000

    f22d9a7 docs: modernize CLI reference for build and test

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-cf7846b
+ * @license Angular v21.2.6+sha-f22d9a7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-cf7846b');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-f22d9a7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
