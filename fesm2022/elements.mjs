a3a4990372a4fcf179c1dd9d51c5a47044075854
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 19:02:21 +0000

    0b08e29 test(core): refactors test to use `timeout` utility

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-f363ab4
+ * @license Angular v21.2.7+sha-0b08e29
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-f363ab4');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-0b08e29');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
