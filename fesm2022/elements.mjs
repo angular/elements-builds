659553423efd6bed381590aa8f0c80114ee16a40
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 09:53:54 +0000

    0960592 fix(router): pass outlet context to split to fix empty path named outlets

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-f916531
+ * @license Angular v21.2.6+sha-0960592
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-f916531');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-0960592');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
