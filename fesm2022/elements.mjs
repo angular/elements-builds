386ab6c899b5cb2cdedac206a1fe70f76e6cc8d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 06:44:44 +0000

    7ba8929 test: fix flakiness in `image-directive` e2e tests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-ef7679b
+ * @license Angular v22.0.0-next.5+sha-7ba8929
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-ef7679b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-7ba8929');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
