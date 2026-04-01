1f4de64a0384248933941a38a3535929d6c3cfbc
Andrew Scott <atscott01@gmail.com>
2026-04-01 19:35:21 +0000

    75ac120 fix(language-service): get quick info at local var location to align with TS semantics and support type narrowing

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-c15e3a0
+ * @license Angular v22.0.0-next.6+sha-75ac120
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-c15e3a0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-75ac120');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
