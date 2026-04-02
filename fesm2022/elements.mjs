af3a44d549ad57d1da22d75a928086d0d0ffd43c
Andrew Scott <atscott01@gmail.com>
2026-04-02 22:08:54 +0000

    2ae0912 refactor(core): address review comments on NG0750 error message

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-05bcc60
+ * @license Angular v21.2.7+sha-2ae0912
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-05bcc60');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-2ae0912');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
