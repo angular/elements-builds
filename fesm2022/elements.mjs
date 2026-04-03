218a73b04ba65d7616e327ed98adb7a92a4b7a3f
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:29:02 +0000

    b0dc2fb docs(forms): clarify disabled FormArray value behavior

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-ef204bd
+ * @license Angular v21.2.7+sha-b0dc2fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-ef204bd');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-b0dc2fb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
