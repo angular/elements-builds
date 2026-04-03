ac80b296713eba0f507d8c8e601e1aa792f680ba
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:05:42 +0000

    ef204bd build: update bazel dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-4441a95
+ * @license Angular v21.2.7+sha-ef204bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-4441a95');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-ef204bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
