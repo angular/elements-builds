a5cd82704572e836514efb94bfa458f5bf398fab
Andrew Scott <atscott01@gmail.com>
2026-04-03 16:50:52 +0000

    9c55fcb feat(core): de-duplicate host directives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-9d76ac8
+ * @license Angular v22.0.0-next.6+sha-9c55fcb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-9d76ac8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-9c55fcb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
