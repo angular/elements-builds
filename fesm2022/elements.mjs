6fba67c821b42d256b751d3382a35b3caddebdf1
Andrew Scott <atscott01@gmail.com>
2026-04-06 19:37:58 +0000

    d715c36 refactor(compiler-cli): fix failing tests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-c91bd07-with-local-changes
+ * @license Angular v21.2.7+sha-d715c36-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-c91bd07-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-d715c36-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
