e7ec11acd787ca9814fdaffe485eb17a93f4b6af
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:19:32 +0000

    2cb67c0 docs: update cross-repo adev docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-1ef503e
+ * @license Angular v22.0.0-next.6+sha-2cb67c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-1ef503e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-2cb67c0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
