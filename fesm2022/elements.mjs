6178c456c925ff86e3c4fb34edfe24862002344a
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:11:04 +0000

    1ef503e test(http): disable XSRF and mock location in HttpClient tests to avoid Domino failures and state leakage

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-d95e856
+ * @license Angular v22.0.0-next.6+sha-1ef503e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-d95e856');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-1ef503e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
