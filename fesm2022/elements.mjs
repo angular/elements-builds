fbfec70478fe20e23411736d32d1e4a07c5649da
Andrew Scott <atscott01@gmail.com>
2026-04-07 16:57:03 +0000

    1c9c453 docs(docs-infra): fix v21 event video not responsive on mobile

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-236d6d4
+ * @license Angular v22.0.0-next.6+sha-1c9c453
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-236d6d4');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-1c9c453');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
