c6e4d39e703a828d6e4a4d7ffa4fb033280f7d0b
Andrew Scott <atscott01@gmail.com>
2026-04-07 21:59:27 +0000

    e95d846 fix(vscode-extension): restore correct highlighting

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-de56d74
+ * @license Angular v22.0.0-next.6+sha-e95d846
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-de56d74');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-e95d846');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
