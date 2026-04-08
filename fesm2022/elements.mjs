8a68f4fd1cb9a7c86fc2f4b29e8b729911babb07
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:42:36 +0000

    6fee651 refactor(compiler-cli): decouple SymbolBuilder from BoundTarget and minimize adapter surface

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-c7518a4
+ * @license Angular v22.0.0-next.6+sha-6fee651
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-c7518a4');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-6fee651');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
