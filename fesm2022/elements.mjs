985ac9cb5b678afd1af9438139efaed3fb48894b
Andrew Scott <atscott01@gmail.com>
2026-04-08 16:49:27 +0000

    c70b4fe docs: fix typos and formatting in spam policy and saved replies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-30c950f
+ * @license Angular v22.0.0-next.6+sha-c70b4fe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-30c950f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-c70b4fe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
