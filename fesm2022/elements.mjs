00d3ddc25f9bb215b66868805d86adee39ead6b3
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:38:38 +0000

    057cc6d fix(core): remove obsolete iOS cursor pointer hack in event delegation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-dea76c7
+ * @license Angular v21.2.7+sha-057cc6d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-dea76c7');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-057cc6d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
