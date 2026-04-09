64ff1d6f8267797226d659b505681d7a9d5ec620
GitHub <noreply@github.com>
2026-04-09 18:56:03 +0000

    836094c fix(service-worker): resolve TS 6.0 compatibility for messageerror listener

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-68628dd
+ * @license Angular v22.0.0-next.7+sha-836094c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-68628dd');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-836094c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
