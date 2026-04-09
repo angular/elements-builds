c2c85c17e03b11b4d4cf436c3be49f2248f42251
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:18:32 +0000

    b9265cc docs: update typescript version requirement for angular 22

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1a16a40
+ * @license Angular v22.0.0-next.7+sha-b9265cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1a16a40');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-b9265cc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
