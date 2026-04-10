33f65b35112f548438610536c639d8e1cd86207d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 19:06:44 +0000

    a89b565 refactor(language-service): Avoid deep import of comments helper

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-e453848
+ * @license Angular v22.0.0-next.7+sha-a89b565
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-e453848');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a89b565');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
