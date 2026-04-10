0eb90db4da490a83b4ce4da5947c6e02801b3962
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 18:54:23 +0000

    e453848 test(platform-browser): verify that Angular supports bootstrapping under shadow roots

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1415d86
+ * @license Angular v22.0.0-next.7+sha-e453848
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1415d86');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-e453848');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
