4c18fc3521362e0c1733dc55a6281c41342330ad
kirjs <kirts@google.com>
2026-04-10 14:47:08 +0000

    b4a747a docs(docs-infra): fix homepage nav overlay and banner visibility between 701–900px

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-b52a626
+ * @license Angular v21.2.8+sha-b4a747a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b52a626');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b4a747a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
