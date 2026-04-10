7b23c2163bd206825872535fddd519604a858b7b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 17:30:16 +0000

    1415d86 fix(migrations): Fix typo for strict-template migration Fix typo for strict-template migration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-a268547
+ * @license Angular v22.0.0-next.7+sha-1415d86
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a268547');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1415d86');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
