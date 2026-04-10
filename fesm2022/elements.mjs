0a8da16c5e053058e15388cfe6d310c96af12782
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 17:20:21 +0000

    a268547 build: update rules_angular setup in MODULE.bazel

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-843f425
+ * @license Angular v22.0.0-next.7+sha-a268547
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-843f425');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a268547');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
