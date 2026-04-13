cf9793d7c9f611d65dd998c79b77f00255581444
kirjs <kirts@google.com>
2026-04-13 08:06:24 +0000

    e2e7211 docs(docs-infra): consolidate tab menu margins for phone breakpoint

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-b351d49
+ * @license Angular v21.2.8+sha-e2e7211
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b351d49');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-e2e7211');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
