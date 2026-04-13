bff5c450a5aa01005b3077015120cba5cd83933c
kirjs <kirts@google.com>
2026-04-13 08:14:54 +0000

    245bcdd docs(docs-infra): fix card container overflow on mobile viewports

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-e2e7211
+ * @license Angular v21.2.8+sha-245bcdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-e2e7211');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-245bcdd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
