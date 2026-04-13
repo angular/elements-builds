a358735abebf915c286afcb3e649737a9996fcd1
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 19:36:08 +0000

    f9b74e9 refactor(core): simplifying the ComponentFactory usage

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1fa8bb7
+ * @license Angular v22.0.0-next.7+sha-f9b74e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1fa8bb7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f9b74e9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
