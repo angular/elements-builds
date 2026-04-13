47c85238ff3681396c5f6dec8339926cb0b2e544
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:14:32 +0000

    c3d4be4 docs(docs-infra): fix card container overflow on mobile viewports

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-fda8d20
+ * @license Angular v22.0.0-next.7+sha-c3d4be4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-fda8d20');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-c3d4be4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
