5d7291a2e01df68968bcf48fb69d4ce1650ad837
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:29:00 +0000

    973ede6 refactor: Mobile layout api reference Fix mobile layout shift in API reference

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-59513b7
+ * @license Angular v22.0.0-next.7+sha-973ede6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-59513b7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-973ede6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
