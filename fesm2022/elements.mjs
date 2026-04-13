4ecde3383b401faac374cac93cc6db471e5465d5
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 18:26:01 +0000

    cb19c69 docs(devtools): create router tree documentation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-eb2b06f
+ * @license Angular v22.0.0-next.7+sha-cb19c69
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-eb2b06f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-cb19c69');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
