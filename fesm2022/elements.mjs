a8beefc128b8f9b3f56646eac57dc26000d02101
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 15:59:27 +0000

    0454d4c refactor(core): deprecate `withIncrementalHydration`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-39e382a
+ * @license Angular v22.0.0-next.7+sha-0454d4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-39e382a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-0454d4c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
