356f62ee2a07201602d5b86ee2934bc97092a4a7
kirjs <kirts@google.com>
2026-04-13 13:09:05 +0000

    540536c fix(http): add CSP nonce support to JsonpClientBackend

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-f603d47
+ * @license Angular v21.2.8+sha-540536c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-f603d47');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-540536c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
