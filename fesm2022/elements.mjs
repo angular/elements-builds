d5f0adc2f45201945b113f952ac72de5936f2f32
kirjs <kirts@google.com>
2026-04-14 09:44:31 +0000

    684e9fd fix(router): normalize multiple leading slashes in URL parser

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-bb8cdd9
+ * @license Angular v21.2.8+sha-684e9fd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-bb8cdd9');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-684e9fd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
