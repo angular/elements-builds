1359701db26db4c2f74639c279d5e3a343e5451e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:17:53 +0000

    9bead58 docs: release notes for the v20.3.19 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-c326548
+ * @license Angular v22.0.0-next.8+sha-9bead58
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-c326548');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-9bead58');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
