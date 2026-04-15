eda2038d9aed606b3a4b22a964b22af8a8254e19
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 14:31:57 +0000

    b2cff79 docs(docs-infra): add background to playground template dropdown

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-75f2cb8
+ * @license Angular v22.0.0-next.7+sha-b2cff79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-75f2cb8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-b2cff79');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
