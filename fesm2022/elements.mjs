eb0f450f31f9394149783005c8250109380d551f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:16:49 +0000

    b9ec542 release: cut the v20.3.19 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.18+sha-303d4cd
+ * @license Angular v20.3.19+sha-b9ec542
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.18+sha-303d4cd');
+const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-b9ec542');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
