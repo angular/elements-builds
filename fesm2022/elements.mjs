89db4c17da30291546375c15b23ea8bda125c0ab
kirjs <kirts@google.com>
2026-04-15 19:47:42 +0000

    528a93a docs(router): document .. traversal and relativeTo pitfalls in router.navigate()

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-32a8302
+ * @license Angular v21.2.8+sha-528a93a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-32a8302');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-528a93a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
