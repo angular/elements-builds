a07f9ecacd96456f5f9fe25af9282fcbbcf49510
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 18:05:09 +0000

    c738d45 build: update all github actions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-e14d5ea
+ * @license Angular v21.2.9+sha-c738d45
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-e14d5ea');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c738d45');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
