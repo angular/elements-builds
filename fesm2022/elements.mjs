88caff93a22e95eaaf30ff9db4f73eaefec81158
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 21:30:57 +0000

    6d10b8b docs: add inject to structural directive imports

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c738d45
+ * @license Angular v21.2.9+sha-6d10b8b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c738d45');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-6d10b8b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
