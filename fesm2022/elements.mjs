499eed5836d749babd37baa7111006ef9d376520
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:46:22 +0000

    ce883d9 build: update peer dependencies and bump version

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-be74909
+ * @license Angular v21.2.9+sha-ce883d9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-be74909');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-ce883d9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
