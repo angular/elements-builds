39b1c4369253e6f98b703538ef28a9d1d1bf9336
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:59:18 +0000

    e5b93ea docs: fix wording in reactive forms guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-ce883d9
+ * @license Angular v21.2.9+sha-e5b93ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-ce883d9');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-e5b93ea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
