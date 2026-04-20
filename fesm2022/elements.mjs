ab5db64e49d728ddb4e74925f08a3328b200124f
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:38:25 +0000

    a9ddc5d docs: warn against storing secrets in environment files

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-0d08d9c
+ * @license Angular v21.2.9+sha-a9ddc5d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-0d08d9c');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-a9ddc5d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
