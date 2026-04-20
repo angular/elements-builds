40386c2baf4237d9de08b30ab02eea30c27a6940
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 23:55:02 +0000

    6eff439 fix(router): restore internal URL on popstate when `browserUrl` is used

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-d27e2c2
+ * @license Angular v22.0.0-next.8+sha-6eff439
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-d27e2c2');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-6eff439');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
