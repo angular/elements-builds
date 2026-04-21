29e6644d5fccac96733e1bc15d7cf94f30d07a1f
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 17:29:47 +0000

    28760bd build: update cross-repo angular dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-fa6a3d2
+ * @license Angular v22.0.0-next.8+sha-28760bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-fa6a3d2');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-28760bd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
