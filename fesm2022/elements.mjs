67465a837160a45cd314f988881a1c80335fda79
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 18:59:53 +0000

    09b9a62 build: lock file maintenance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-28760bd
+ * @license Angular v22.0.0-next.8+sha-09b9a62
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-28760bd');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-09b9a62');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
