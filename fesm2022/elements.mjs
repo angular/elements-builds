71bb0dc50aeb8bc91f314f216bb1df900ec19aed
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 17:16:33 +0000

    5533ab4 fix(migrations): fix NgClass leaving trailing comma after removal

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-4dc7bf5
+ * @license Angular v21.2.9+sha-5533ab4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4dc7bf5');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-5533ab4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
