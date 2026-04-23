1644c6255dc6ce3004e58b52c088a18826e5e81a
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 22:21:00 +0000

    65c205d build: update cross-repo angular dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-218e2d0
+ * @license Angular v22.0.0-next.9+sha-65c205d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-218e2d0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-65c205d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
