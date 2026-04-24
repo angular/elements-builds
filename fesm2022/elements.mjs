de4a7064af055ab43caac1e86b27d81ff88af3d8
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 23:04:00 +0000

    08930e6 docs: update cross-repo adev docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-ac9babe
+ * @license Angular v22.0.0-next.9+sha-08930e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-ac9babe');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-08930e6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
