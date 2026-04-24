a57e8acfe7ff794ac09bc6a23d709156554ffb14
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:16:52 +0000

    8ce8b93 build: update pnpm to v10.33.2

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-f3308dc
+ * @license Angular v21.2.10+sha-8ce8b93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-f3308dc');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-8ce8b93');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
