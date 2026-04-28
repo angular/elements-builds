1c33c9fc3e2dbcde6236669fb82f6549348d65f9
Alon Mishne <amishne@users.noreply.github.com>
2026-04-28 17:38:51 +0000

    fb7e67c docs: use inject() in @Self example in hierarchical DI guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-9bcbf37
+ * @license Angular v21.2.10+sha-fb7e67c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-9bcbf37');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-fb7e67c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
