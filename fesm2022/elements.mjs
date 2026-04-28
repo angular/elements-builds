1b5b34a648925f32ddaa46ce2ec86a529e9e3e8a
GitHub <noreply@github.com>
2026-04-28 22:48:08 +0000

    2896c93 feat(compiler): Angular expressions with optional chaining returns `undefined`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-ef38017
+ * @license Angular v22.0.0-next.9+sha-2896c93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-ef38017');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-2896c93');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
