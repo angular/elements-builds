3d61366ec340ea615e2fed29f1e023f66cbd2ca0
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 16:50:02 +0000

    9bcbf37 refactor(core): fix bundling symbol test

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-a3d14fc
+ * @license Angular v21.2.10+sha-9bcbf37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-a3d14fc');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-9bcbf37');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
