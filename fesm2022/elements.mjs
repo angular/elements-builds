369d256949562ed4ead1762fefb7c7d7f2dbfb85
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 20:50:41 +0000

    af24a4a fix(vscode-extension): Look for tsdk override in the new js/ts.tsdk.path setting

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-37ec63e
+ * @license Angular v22.0.0-next.9+sha-af24a4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-37ec63e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-af24a4a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
