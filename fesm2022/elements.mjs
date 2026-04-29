dd0a5122110cf5ccf33fb1e8ca52eb8e6b11c85f
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 21:06:50 +0000

    13c0422 docs(docs-infra): open update guide external links in new tabs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-af24a4a
+ * @license Angular v22.0.0-next.9+sha-13c0422
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-af24a4a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-13c0422');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
