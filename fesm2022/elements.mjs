2856461e542a28c5222a35ec2248779de491224d
GitHub <noreply@github.com>
2026-05-05 23:04:04 +0000

    f81fa6e docs: add documentation for lazy loading services and update navigation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.10+sha-8a7f955
+ * @license Angular v22.0.0-next.10+sha-f81fa6e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-8a7f955');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-f81fa6e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
