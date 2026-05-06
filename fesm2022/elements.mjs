72d2abbec66eb83d974671394fad7b99b1f0e5f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 21:49:55 +0000

    25e4e07 fix(platform-server): ensure origin has a trailing slash when parsing url (#68468)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.19+sha-b9ec542
+ * @license Angular v20.3.19+sha-25e4e07
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-b9ec542');
+const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-25e4e07');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
