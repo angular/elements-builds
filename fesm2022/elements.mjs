77fea3b8d4d6511fc451be41921938cd6929b6f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 21:21:39 +0000

    3b0ae5f feat(core): add `provideWebMcpTools`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.10+sha-7745365
+ * @license Angular v22.0.0-next.10+sha-3b0ae5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-7745365');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-3b0ae5f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
