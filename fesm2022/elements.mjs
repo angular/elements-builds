e95b5b40313a200cca5c9b9384aec1a6fd3142d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:39:43 +0000

    a02ac76 docs: release notes for the v20.3.20 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.10+sha-3b0ae5f
+ * @license Angular v22.0.0-next.10+sha-a02ac76
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-3b0ae5f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-a02ac76');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
