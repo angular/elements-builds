406fe586eea2ee82fbea198f729743c49bfea593
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 00:14:29 +0000

    9e38ed7 fix(core): sanitizer typings

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.11+sha-1aeebbe
+ * @license Angular v21.2.11+sha-9e38ed7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.11+sha-1aeebbe');
+const VERSION = /* @__PURE__ */new Version('21.2.11+sha-9e38ed7');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
