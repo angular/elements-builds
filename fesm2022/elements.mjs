9e82ce4c0574b22c6289750c6325d6d4b6182804
Matthew Beck <me@mattrb.com>
2026-05-07 22:17:11 +0000

    2a1e6ec docs: normalize product name casing across docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-bca94ee
+ * @license Angular v21.2.12+sha-2a1e6ec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-bca94ee');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-2a1e6ec');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
