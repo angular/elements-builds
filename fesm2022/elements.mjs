a256aba810284b4d93bf6224fc48c1d05f7fcd7a
Matthew Beck <me@mattrb.com>
2026-05-07 22:03:54 +0000

    bca94ee docs: Update error display for password field in signal forms playground

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-5316753
+ * @license Angular v21.2.12+sha-bca94ee
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-5316753');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-bca94ee');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
