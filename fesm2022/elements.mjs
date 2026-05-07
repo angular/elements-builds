4531830f7c02383a6ac5cf5923bf235d7511fe1a
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 23:02:47 +0000

    7b4791f docs: add `FormField/FormRoot` imports + move comment w/backticks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-2fcfffb
+ * @license Angular v22.1.0-next.0+sha-7b4791f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-2fcfffb');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7b4791f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
