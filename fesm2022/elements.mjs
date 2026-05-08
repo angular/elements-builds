677e3ebb4511ec53f494d471bfb00cccd0b6a608
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 16:06:02 +0000

    0d02c6a build: update dependency bazel to v8.7.0

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-2a19754
+ * @license Angular v22.0.0-next.12+sha-0d02c6a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-2a19754');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-0d02c6a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
