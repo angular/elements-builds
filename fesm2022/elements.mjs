c67e1a54cd9d6f4491452856c1809b58c63af946
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 16:13:50 +0000

    1d621ea build: update pnpm to v10.33.4

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-0d02c6a
+ * @license Angular v22.0.0-next.12+sha-1d621ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-0d02c6a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1d621ea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
