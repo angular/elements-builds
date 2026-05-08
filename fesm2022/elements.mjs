e35639bcff0b4392b512a8c9af7841d7af8d3959
Matthew Beck <me@mattrb.com>
2026-05-08 21:16:56 +0000

    119a19e fix(platform-server): forward BEFORE_APP_SERIALIZED errors to ErrorHandler

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-1d621ea
+ * @license Angular v22.0.0-next.12+sha-119a19e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1d621ea');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-119a19e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
