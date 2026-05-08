fe02efdfbb3ed87f171c34cec38399133ecaee7c
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 15:27:24 +0000

    2a19754 release: cut the v22.0.0-next.12 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.11+sha-8ebae1d
+ * @license Angular v22.0.0-next.12+sha-2a19754
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.11+sha-8ebae1d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-2a19754');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
