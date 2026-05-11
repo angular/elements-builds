164e6407660c5817df53743e1cb963c0e3f92a4a
Matthew Beck <me@mattrb.com>
2026-05-11 18:54:50 +0000

    5a6e946 docs: remove experimental warnings from resource/signal forms documentation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-119a19e
+ * @license Angular v22.0.0-next.12+sha-5a6e946
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-119a19e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-5a6e946');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
