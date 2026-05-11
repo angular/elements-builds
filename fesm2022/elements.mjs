8349f01cbf6d925d2a687e453c89a642dc140438
Matthew Beck (Berry) <me@mattrb.com>
2026-05-11 19:16:45 +0000

    ef134ac docs: add new signal forms field metadata guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-538d0a8
+ * @license Angular v22.1.0-next.0+sha-ef134ac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-538d0a8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ef134ac');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
