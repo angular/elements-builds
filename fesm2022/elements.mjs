c3fc6c03adb72de188b10e4f8235899aa846da9d
Matthew Beck <me@mattrb.com>
2026-05-11 19:51:32 +0000

    49a133a refactor(compiler-cli): drop @ts-ignore around jsDocParsingMode

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-c93d158
+ * @license Angular v21.2.12+sha-49a133a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-c93d158');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
