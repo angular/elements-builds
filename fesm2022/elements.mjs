45afbd2bba64bc0e2303e210441d295ef5b26d0f
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 21:56:39 +0000

    1bb2c68 docs: release notes for the v19.2.22 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6a07fdd
+ * @license Angular v22.1.0-next.0+sha-1bb2c68
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6a07fdd');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1bb2c68');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
