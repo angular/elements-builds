6d1bb83145b26bd79a815261ac469edcb33f339d
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:58:44 +0000

    a617967 refactor(http): update HTTP resource options APIs to stable

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-62710ad
+ * @license Angular v22.1.0-next.0+sha-a617967
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-62710ad');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a617967');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
