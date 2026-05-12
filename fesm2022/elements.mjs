e3bb6bdf435e87f750c4e1409b97f8ef86e82fb3
Matthew Beck <me@mattrb.com>
2026-05-12 01:40:21 +0000

    e9d1c7e refactor(router): Move target RouterState creation before 'blocking' stage

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-8391cf0
+ * @license Angular v22.0.0-next.12+sha-e9d1c7e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-8391cf0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-e9d1c7e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
