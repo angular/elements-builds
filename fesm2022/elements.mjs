2b21dbf80f2bfa894feefa07ae862805582ffca4
Matthew Beck <me@mattrb.com>
2026-05-12 17:55:30 +0000

    7ab78d5 ci: mark devversion as unavailable

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-49a133a
+ * @license Angular v21.2.12+sha-7ab78d5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
