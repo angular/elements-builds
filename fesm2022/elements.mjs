73060c28201b6b4958f0e6415f3c1401f46d3008
Matthew Beck <me@mattrb.com>
2026-05-13 18:38:20 +0000

    1e079a8 docs: add clarification around plain object models

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-7ab78d5
+ * @license Angular v21.2.12+sha-1e079a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-1e079a8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
