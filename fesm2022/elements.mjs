1830cbf48b8aa4847d90bd57dd9d759f63cdfe85
Matthew Beck <me@mattrb.com>
2026-05-13 19:29:44 +0000

    1f0c1ab refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-ccb7d42
+ * @license Angular v22.0.0-next.12+sha-1f0c1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-ccb7d42');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
