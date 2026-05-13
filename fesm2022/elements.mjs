388aca2d2140db8c288c875651ddd403f03e4717
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 22:43:58 +0000

    dbf3853 docs: add version 22 to the browser support reference table

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ec63947
+ * @license Angular v22.1.0-next.0+sha-dbf3853
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ec63947');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-dbf3853');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
