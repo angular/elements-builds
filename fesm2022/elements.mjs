5151bfeb3d062532cd70fe408520137df2a080c2
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 18:54:26 +0000

    defb172 build: update dependency node to v22.22.3

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-4dd9839
+ * @license Angular v22.0.0-rc.0+sha-defb172
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-4dd9839');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-defb172');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
