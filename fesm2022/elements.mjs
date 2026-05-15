5600d7907adfd852e016189714a0f90cfa60bcf1
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 17:49:31 +0000

    358d2e6 docs: move Angular 19 to unsupported versions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-71f8d48
+ * @license Angular v22.1.0-next.0+sha-358d2e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-71f8d48');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-358d2e6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
