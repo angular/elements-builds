b2b13ee42f5229170f5ea7b8aec991db01ff8db0
leonsenft <leonsenft@users.noreply.github.com>
2026-05-18 20:40:45 +0000

    c0f5227 fix(core): do not insert todo when migrating void @Output

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-fc434c1
+ * @license Angular v21.2.13+sha-c0f5227
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-fc434c1');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-c0f5227');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
