758e0e213b1128e1904820e0358f9f9e8f85c2f8
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 20:28:30 +0000

    7f444e1 docs: Fix accepted Provider type in doc snippet

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-c0f5227
+ * @license Angular v21.2.13+sha-7f444e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-c0f5227');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-7f444e1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
