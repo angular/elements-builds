7f7c1e6aad2883bfaa623b2d361af4a88063c87c
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 20:49:00 +0000

    027c7c0 docs: update HTTP testing setup guidance

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-7f444e1
+ * @license Angular v21.2.13+sha-027c7c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-7f444e1');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-027c7c0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
