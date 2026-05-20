d8753ec87111f87c2724e3d281dc6a510b007739
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:28:22 +0000

    7f1cfcb docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-cbddb5c
+ * @license Angular v22.0.0-rc.0+sha-7f1cfcb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-cbddb5c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-7f1cfcb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
