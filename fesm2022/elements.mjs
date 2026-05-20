78f82368ebbf19157ce4751b6e5bf37a60a68938
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 20:41:22 +0000

    232b21d docs: document content projection limitations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-1d6e71d
+ * @license Angular v21.2.13+sha-232b21d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1d6e71d');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-232b21d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
