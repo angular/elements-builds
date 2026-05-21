0ff24ba2649ba2bcad4a9b7010599da9dd3705d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-21 18:09:44 +0000

    1563aae perf(forms): avoid redundant invalidations in parser errors signal

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-fd6ef34
+ * @license Angular v22.1.0-next.0+sha-1563aae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fd6ef34');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1563aae');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
