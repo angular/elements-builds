01ac7a2775abc5606985c5e70b6eafce0d79b53a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 21:14:41 +0000

    2200b4a refactor(compiler): add support for compiling NgModules under isolatedDeclarations

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-b25964b
+ * @license Angular v22.0.0-rc.1+sha-2200b4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-b25964b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-2200b4a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
