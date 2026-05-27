de612d800b6dfd1af34f993c8acc5d9c67db772d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 23:49:12 +0000

    d88b796 build: update cross-repo angular dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-618c850
+ * @license Angular v22.0.0-rc.1+sha-d88b796
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-618c850');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-d88b796');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
