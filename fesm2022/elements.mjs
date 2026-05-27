eb7994f6ed2a650e8163e1687927fc03da8f5b11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 17:38:28 +0000

    37e8aad fix(platform-server): prevent SSRF bypasses via backslash URLs in HttpClient

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-525e160
+ * @license Angular v21.2.14+sha-37e8aad
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-525e160');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-37e8aad');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
