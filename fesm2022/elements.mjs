1c2bc4a53fa261254424c6d927a18a4943342e17
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 22:07:57 +0000

    af04936 fix(core): sanitize meta selectors

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-e617fa0
+ * @license Angular v20.3.21+sha-af04936
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-e617fa0');
+const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-af04936');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
