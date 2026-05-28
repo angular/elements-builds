60ebba0565e82870bcb206012163158e2db6b8b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-28 16:59:25 +0000

    99c1eeb docs: release notes for the v21.2.15 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e2ccea9
+ * @license Angular v22.1.0-next.0+sha-99c1eeb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e2ccea9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-99c1eeb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
