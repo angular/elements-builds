cca034c8d9d1501d842c2e446f16fb6033c005fc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:18:17 +0000

    c1cefe9 build: update bazel dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-94d520f
+ * @license Angular v22.0.0-rc.1+sha-c1cefe9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-94d520f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-c1cefe9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
