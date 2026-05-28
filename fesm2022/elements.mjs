9479c165fd13eb752085ef6cdd3cb2116b195569
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 13:55:20 +0000

    d40acc6 fix(compiler): prevent namespaced SVG <style> elements from being stripped

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.22+sha-db20819
+ * @license Angular v20.3.22+sha-d40acc6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.22+sha-db20819');
+const VERSION = /* @__PURE__ */ new Version('20.3.22+sha-d40acc6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
