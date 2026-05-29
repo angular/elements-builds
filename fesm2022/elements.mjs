32160a9579909ab856520512065c79fff8d67e37
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:05:53 +0000

    7e38336 fix(core): use Object.create(null) for LOCALE_DATA as a hardening measure

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-f6d8e64
+ * @license Angular v21.2.15+sha-7e38336
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-f6d8e64');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-7e38336');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
