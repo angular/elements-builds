39c435a279cd285594c2c28ea3bcf785e687c7ca
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 12:25:03 +0000

    8680b51 fix(platform-server): update domino to latest version

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.23+sha-a1eeeb8
+ * @license Angular v20.3.23+sha-8680b51
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.23+sha-a1eeeb8');
+const VERSION = /* @__PURE__ */ new Version('20.3.23+sha-8680b51');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
