1978afd8a85b67faee155daa83207b4e9cadb6e6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 11:26:17 +0000

    4795b35 fix(common): only strip a literal /index.html suffix from URLs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-5f0c9f8
+ * @license Angular v22.0.0-rc.2+sha-4795b35
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-5f0c9f8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-4795b35');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
