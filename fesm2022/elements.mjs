c760c4cd101804246057307ce6f49516fb33c45f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 10:01:34 +0000

    76dfb94 docs: changelog typo fixes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-36ee538
+ * @license Angular v22.1.0-next.0+sha-76dfb94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-36ee538');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-76dfb94');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
