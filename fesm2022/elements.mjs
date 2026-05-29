3f78ac689fd77ad6d905b7fe8b08f6a3634aa1d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 09:53:30 +0000

    c203b70 build: update all github actions

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-ebd698b
+ * @license Angular v22.0.0-rc.2+sha-c203b70
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-ebd698b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-c203b70');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
