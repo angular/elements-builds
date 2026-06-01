650e37a9a937a5f6a201f702dbcb4190ac38b6e1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:26:19 +0000

    e834c37 docs: add v22 entries to the update guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-61b3483
+ * @license Angular v22.0.0-rc.2+sha-e834c37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-61b3483');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-e834c37');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
