4dc61f8ba16dd82abb879692cdf04743f512230e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:02:17 +0000

    1cd4f54 fix(docs-infra): readable contrast for DEV/EXP api badges in light mode

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-69c0d48
+ * @license Angular v21.2.15+sha-1cd4f54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-69c0d48');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-1cd4f54');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
