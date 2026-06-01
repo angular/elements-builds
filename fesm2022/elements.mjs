04d8165dc4172d86cd860de792a655bb1e793a3e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 20:12:38 +0000

    7584e35 release: cut the v22.0.0-rc.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-7b91309
+ * @license Angular v22.0.0-rc.3+sha-7584e35
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-7b91309');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-7584e35');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
