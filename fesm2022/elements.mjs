25fa8d259d9c5a0a244ca350c58859b7159b9c9e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:31:58 +0000

    5215f73 build: update dependency @mcp-b/webmcp-polyfill to v3

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-f0b28f6
+ * @license Angular v22.1.0-next.0+sha-5215f73
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f0b28f6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-5215f73');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
