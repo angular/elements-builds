2477595fea70545ff97f8d3a7a3fbbbae57b00d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 11:37:34 +0000

    f3c9249 fix(core): harden inherit definition feature against polluted prototypes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-a301ba2
+ * @license Angular v22.0.0-rc.3+sha-f3c9249
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-a301ba2');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-f3c9249');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
