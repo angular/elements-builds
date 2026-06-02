0106de798b5599ed5e441948b8f91a80d03e76ad
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 12:22:31 +0000

    736c4ab refactor(core): fix broken unit test.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-3fd6897
+ * @license Angular v21.2.15+sha-736c4ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-3fd6897');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-736c4ab');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
