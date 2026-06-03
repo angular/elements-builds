0bf83af7f2a9efb327aa89fb58705f4a9aa61b36
Andrew Scott <atscott01@gmail.com>
2026-06-03 22:52:50 +0000

    592b121 refactor(devtools): Prevent cross-tab privilege escalation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-8e4fd59
+ * @license Angular v22.0.0+sha-592b121
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8e4fd59');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-592b121');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
