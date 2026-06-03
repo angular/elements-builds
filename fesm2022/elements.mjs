82fce44080d029c8e14be29c990ca590a2aefde9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 12:37:44 +0000

    03188dd release: cut the v21.2.16 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-736c4ab
+ * @license Angular v21.2.16+sha-03188dd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-736c4ab');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-03188dd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
