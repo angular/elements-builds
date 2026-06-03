3d92af7122a182d4e5a41fa86e4138d6acce1e9c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 09:11:19 +0000

    e10647b docs: update signal forms conditional rules

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-f3c9249
+ * @license Angular v22.0.0-rc.3+sha-e10647b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-f3c9249');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-e10647b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
