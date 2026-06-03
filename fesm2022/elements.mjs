a799add529814ab68f82c804581df568dd6cefee
Andrew Scott <atscott01@gmail.com>
2026-06-03 23:10:32 +0000

    6bde84f fix(core): harden TransferState restoration against DOM clobbering

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7896e74
+ * @license Angular v22.1.0-next.0+sha-6bde84f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7896e74');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6bde84f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
