f75a8dd3f2f44fbf1abdfdaffc4f72e5cf068ffe
Andrew Scott <atscott01@gmail.com>
2026-06-03 18:10:45 +0000

    566ad05 fix(common): skip transfer cache for uncacheable HTTP traffic

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-768a349
+ * @license Angular v20.3.24+sha-566ad05
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-768a349');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-566ad05');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
