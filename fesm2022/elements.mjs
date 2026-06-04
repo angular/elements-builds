306435215e2fba75fbd4ff3932f16eae79aa0cf1
Andrew Scott <atscott01@gmail.com>
2026-06-04 19:38:39 +0000

    c00387b docs: mention `parse` of `validateHttp`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-669146b
+ * @license Angular v22.0.0+sha-c00387b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-669146b');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-c00387b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
