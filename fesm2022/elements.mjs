f1b6721b9b8e075a09c1ca9c71691983cc7c7dca
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:57:43 +0000

    3551074 fix(platform-server): harden platform location origin validation during SSR

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-bc55749
+ * @license Angular v21.2.16+sha-3551074
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-bc55749');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-3551074');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
