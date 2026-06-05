76792f5e1f4baa7dd82dd83d16699efc8d12f022
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:29:24 +0000

    652b76b docs: bump schematics-for-libraries example peer deps to v22

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-8f06057
+ * @license Angular v22.0.0+sha-652b76b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8f06057');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-652b76b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
