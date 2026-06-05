c748b4b2b91c985d99716dd056341b68e1f1f275
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:48:51 +0000

    1a62130 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-49368c1
+ * @license Angular v20.3.24+sha-1a62130
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-49368c1');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-1a62130');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
