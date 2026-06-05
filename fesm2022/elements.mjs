7d9a67e130e06e5f6af873a2560444ffc7394a1d
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:53:12 +0000

    ed2420c fix(vscode-extension): inspect tsdk configurations correctly

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-af04e26
+ * @license Angular v22.1.0-next.0+sha-ed2420c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-af04e26');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ed2420c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
