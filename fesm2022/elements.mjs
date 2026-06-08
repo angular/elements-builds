0672fc45bf122f8b3beff6f1532b01077c911734
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:26:48 +0000

    3c70270 fix(compiler): sanitize two-way properties

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-422e689
+ * @license Angular v22.1.0-next.0+sha-3c70270
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-422e689');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3c70270');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
