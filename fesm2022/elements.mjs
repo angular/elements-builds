1d978dc3308922830c806d59591e598238899f83
Andrew Scott <atscott01@gmail.com>
2026-06-09 18:20:54 +0000

    43edc84 fix(router): use native URL object for navigation boundary and comparison

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-c4b5fa3
+ * @license Angular v22.0.0+sha-43edc84
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-c4b5fa3');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-43edc84');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
