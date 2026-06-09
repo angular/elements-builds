70a76e5692e856261bf9d0f4360fe1111630f1a4
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:04:33 +0000

    8ddda4d fix(docs-infra): improve version picker dropdown positioning

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-96b6419
+ * @license Angular v22.0.0+sha-8ddda4d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-96b6419');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8ddda4d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
