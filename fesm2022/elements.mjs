5409cc2ca1bae154f3ee1484a0372016be2006e1
Andrew Scott <atscott01@gmail.com>
2026-06-09 23:28:50 +0000

    01ea640 refactor(core): Fix DirectiveDefinition interface to allow abstract classes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-a704b08
+ * @license Angular v22.0.0+sha-01ea640
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-a704b08');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-01ea640');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
