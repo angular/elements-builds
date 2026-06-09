255eaf5ee314a515f3314480bdeaa0ee6cd9c6ba
GitHub <noreply@github.com>
2026-06-09 17:12:52 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-86a56dc
+ * @license Angular v21.2.16+sha-13fb0af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-86a56dc');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-13fb0af');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
