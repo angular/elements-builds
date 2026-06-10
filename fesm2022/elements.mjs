3b4c8ac491f0cf4358fe4375d5f99c9fd257ff29
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:21:14 +0000

    8b32559 release: cut the v22.0.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-2dd65d2
+ * @license Angular v22.0.1+sha-8b32559
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-2dd65d2');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-8b32559');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
