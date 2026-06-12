2a27f41145677ab0e6c1865a7da1b756d535a9d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:23:22 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-15314c1
+ * @license Angular v22.0.1+sha-1d97355
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-15314c1');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-1d97355');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
