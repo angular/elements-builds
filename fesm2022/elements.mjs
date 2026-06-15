6a904448ba891bb05596b1518787a9ce71442ed9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 19:04:16 +0000

    fef8bcf refactor(compiler-cli): Export indexer API for use in hybrid analysis

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-be45e8e
+ * @license Angular v22.0.1+sha-fef8bcf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-be45e8e');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fef8bcf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
