0094668d8d511754282b663715f00e001bfcf676
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 16:36:01 +0000

    6f11719 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fe7f9ed
+ * @license Angular v22.0.1+sha-6f11719
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fe7f9ed');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-6f11719');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
