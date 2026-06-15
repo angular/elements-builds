cdcd811b80ca2b1625087c95909e2b8f16c85466
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 16:07:43 +0000

    fe7f9ed fix(docs-infra): center social and theme menus under their triggers on tablet

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-1d97355
+ * @license Angular v22.0.1+sha-fe7f9ed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-1d97355');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fe7f9ed');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
