27d1d0a8166bb27e9f4b0a2ad632f366d0121444
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:38:24 +0000

    93d0a5f fix(core): prevent unsubscribe during emit from throwing off other listeners

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fef8bcf
+ * @license Angular v22.0.1+sha-93d0a5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fef8bcf');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-93d0a5f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
