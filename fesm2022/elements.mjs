d6730499365fb5e0f4dcb105fef237d65a1702d2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 21:22:23 +0000

    302dd0f release: cut the v22.1.0-next.1 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-f540400
+ * @license Angular v22.1.0-next.1+sha-302dd0f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f540400');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-302dd0f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
