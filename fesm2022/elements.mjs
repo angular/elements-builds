50c8360e592f571fd73e610fe8fac5fb783375a5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 17:38:51 +0000

    e3d392f ci: add id-token: write permission to release workflow

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-74f2b91');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e3d392f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
