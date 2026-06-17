fb11e87a57de97b7c154f1c884d7f99e9b151a6f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 20:17:03 +0000

    45192ba ci: update dev-infra reference to new commit SHA

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-1e79dd3
+ * @license Angular v22.1.0-next.0+sha-45192ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1e79dd3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-45192ba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
