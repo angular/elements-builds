282abd276145903a0c6fce41aec7076072904801
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 16:31:16 +0000

    1566442 build: update cross-repo angular dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-ffcc8ee
+ * @license Angular v22.1.0-next.1+sha-1566442
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-ffcc8ee');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-1566442');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
