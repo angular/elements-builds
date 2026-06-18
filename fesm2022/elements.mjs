e360a5aac43d90e59c338dfd17575a93bc65f38a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-18 13:34:03 +0000

    c23ddd6 docs(docs-infra): remove deprecated ts flags

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-302dd0f
+ * @license Angular v22.1.0-next.1+sha-c23ddd6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-302dd0f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-c23ddd6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
