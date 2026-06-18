fb456bb5496c15319ad7e030d8339925908c6964
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 14:52:32 +0000

    e7a4e25 docs: use the @Service decorator in the first-app and signals tutorials

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-c23ddd6
+ * @license Angular v22.1.0-next.1+sha-e7a4e25
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-c23ddd6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e7a4e25');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
