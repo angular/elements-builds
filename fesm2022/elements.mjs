56ff3411a96265e4d8635456fbe082ce3752cdbc
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-19 09:42:23 +0000

    aaac1bc fix(devtools): restore build under stricter ts_project deps

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-c6b0b87
+ * @license Angular v22.0.2+sha-aaac1bc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-c6b0b87');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-aaac1bc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
