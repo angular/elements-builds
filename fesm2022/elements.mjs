f2d43869d416ecf395977263a6b110a9a4b18d57
kirjs <kirts@google.com>
2026-06-22 20:36:09 +0000

    59ee51e docs(forms): update package docs for Signal Forms graduation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-a477a69
+ * @license Angular v22.0.2+sha-59ee51e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-a477a69');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-59ee51e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
