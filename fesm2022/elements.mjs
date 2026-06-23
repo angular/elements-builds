1f62c83d643f94b5981e69c1bff22b7a02878520
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 19:10:03 +0000

    b7ab51d build: update all github actions to v6.0.9

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-50f252e
+ * @license Angular v22.0.2+sha-b7ab51d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-50f252e');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-b7ab51d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
