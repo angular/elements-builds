8b2a4386166c52051bb51f300afaa0c2cc332932
kirjs <kirts@google.com>
2026-06-23 17:06:11 +0000

    2799304 fix(core): avoid uncaught promise errors in injectAsync prefetching

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-f598dcf
+ * @license Angular v22.0.2+sha-2799304
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-f598dcf');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-2799304');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
