962956301fe1a885acaa9207981e3fa5fb7b041c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 18:21:26 +0000

    50f252e build: update bazel dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-2799304
+ * @license Angular v22.0.2+sha-50f252e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-2799304');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-50f252e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
