4017d5a05e686201c6d02a9451344faa7d94a5cd
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 14:41:18 +0000

    d7d6a08 build: update dependency node to v22.23.1

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-681148c
+ * @license Angular v22.1.0-next.1+sha-d7d6a08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-681148c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-d7d6a08');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
