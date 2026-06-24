824d768ae275583610bf298fbb5c516f0117b23a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 15:33:38 +0000

    edea40b docs: add doc to focusBoundControl feature

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-ea17725
+ * @license Angular v22.1.0-next.1+sha-edea40b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-ea17725');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-edea40b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
