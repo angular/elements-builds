e0f7c4bd47b2b40314a8204cf42af22ea13cad05
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 16:25:51 +0000

    2d33fd5 fix(zone.js): harden zoneSymbolEventNames and patches against __proto__ key

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-12fcec8
+ * @license Angular v22.1.0-next.1+sha-2d33fd5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-12fcec8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-2d33fd5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
