d1332f18a34d090f67d0e84dfdd36f176caccbc7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 17:43:56 +0000

    32c1f30 docs: update Vitest configuration option from `configFile` to `runnerConfig`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-0b1cbbd
+ * @license Angular v22.0.4+sha-32c1f30
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0b1cbbd');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-32c1f30');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
