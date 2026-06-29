621690a11eeaf7c37a22cd661f72fe4ae7c97f7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 23:12:04 +0000

    b5ea340 refactor(compiler): remove `visitAttributeComment`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-ca76b2c
+ * @license Angular v22.0.4+sha-b5ea340
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ca76b2c');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-b5ea340');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
