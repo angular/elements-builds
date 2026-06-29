d0af9e3250044978dfa5f8530445c854a00d4dd2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:29:01 +0000

    0c18ccb docs(docs-infra): fix typo and malformed markdown bullet in angular-developer skill

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-32c1f30
+ * @license Angular v22.0.4+sha-0c18ccb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-32c1f30');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0c18ccb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
