0bfaeb6486ff057d7da1815d91d9e22ab2c6dd64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:40:06 +0000

    baf09a9 fix(compiler-cli): include toSignal in debugName transform

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-0c18ccb
+ * @license Angular v22.0.4+sha-baf09a9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0c18ccb');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-baf09a9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
