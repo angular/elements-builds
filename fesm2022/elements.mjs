e88c8236133bca2e86a6ff61a491a4ccf8db6fd8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 20:30:35 +0000

    a5f1b20 Revert "fix(compiler-cli): include toSignal in debugName transform"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.3+sha-a720094
+ * @license Angular v22.1.0-next.3+sha-a5f1b20
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-a720094');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-a5f1b20');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
