84112313c457d2f43058020d058c7f0d3a0726a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 20:54:26 +0000

    2b3277f release: cut the v22.0.5 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-0554bf5
+ * @license Angular v22.0.5+sha-2b3277f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0554bf5');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2b3277f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
