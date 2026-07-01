19d8557f0edcd14a54e78a1273f744c496ba1acf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 00:35:57 +0000

    ced0180 fix(core): reject dynamic script host elements

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-af7edd9
+ * @license Angular v22.0.4+sha-ced0180
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-af7edd9');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ced0180');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
