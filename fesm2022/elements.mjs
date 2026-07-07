271bf5fa872974f4a8474dd3c0445c7db97b837f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:20:33 +0000

    91df739 fix(http): prevent caching of responses with Set-Cookie headers

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-8d22cc9
+ * @license Angular v21.2.17+sha-91df739
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-8d22cc9');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-91df739');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
