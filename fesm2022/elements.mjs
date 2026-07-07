73cf31f937ed13eae5a45879795a3f6a7b20c7dc
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 18:12:00 +0000

    eb2a8ff fix(compiler-cli): apply debugName transform to required signal queries

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-2f8134b
+ * @license Angular v22.1.0-next.4+sha-eb2a8ff
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-2f8134b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-eb2a8ff');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
