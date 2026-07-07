554496d5ec48603c38386964c7bb75be70918839
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 16:27:16 +0000

    1f6b1ce docs: enhance readability of utility methods section in testing guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-731d665
+ * @license Angular v22.1.0-next.4+sha-1f6b1ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-731d665');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-1f6b1ce');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
