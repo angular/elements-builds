f0a1eaace59f90414795f2c2aae4635c4f24da4d
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 17:25:08 +0000

    c91a951 refactor(core): move devtools ai tool definition types to primitives

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-ab721b4
+ * @license Angular v22.0.5+sha-c91a951
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab721b4');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-c91a951');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
