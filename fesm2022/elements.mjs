7dadd973a99cfb0d1beacfab406fcd5664a47bca
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 20:08:26 +0000

    25b7afa Revert "fix(forms): allow multiple async validators"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-3ac2c7b
+ * @license Angular v22.1.0-next.4+sha-25b7afa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-3ac2c7b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-25b7afa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
