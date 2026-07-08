f771e90feeed48790d9a5918507196cbea8228c7
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 21:43:23 +0000

    89e2c77 release: cut the v22.0.6 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-d1aaf1a
+ * @license Angular v22.0.6+sha-89e2c77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-d1aaf1a');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-89e2c77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
