950d875bd56bc39506eb89bf267fa30d3d5a5434
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 18:33:25 +0000

    ab18f5f refactor(core): cleanup Meta service

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-2d52d53
+ * @license Angular v22.0.5+sha-ab18f5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2d52d53');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab18f5f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
