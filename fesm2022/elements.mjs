834a8b5e1e4890057c7ebcf46277e1d3e5dc549d
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:21:39 +0000

    baa093b fix(service-worker): preserve referrer policy in asset requests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.25+sha-b963f61
+ * @license Angular v20.3.25+sha-baa093b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.25+sha-b963f61');
+const VERSION = /* @__PURE__ */ new Version('20.3.25+sha-baa093b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
