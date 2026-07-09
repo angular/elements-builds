3ec9f77e4f54980d40b855088f271aed40eec44f
Matthew Beck <me@mattrb.com>
2026-07-09 16:43:10 +0000

    5bd00ad fix(compiler): support foreign components inside control flow blocks (#69674)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-e3ac727
+ * @license Angular v22.1.0-next.5+sha-5bd00ad
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-e3ac727');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-5bd00ad');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
