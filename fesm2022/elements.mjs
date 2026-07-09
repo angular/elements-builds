bf3ffd5b03108c93da9d2ad10d6e855ebc9d7bc0
Matthew Beck <me@mattrb.com>
2026-07-09 17:05:32 +0000

    53ca8c2 test(language-service): empty template literal interpolation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-5bd00ad
+ * @license Angular v22.1.0-next.5+sha-53ca8c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-5bd00ad');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-53ca8c2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
