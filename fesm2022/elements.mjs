f2ce42ea673737f7072e77a256bf8df9e2bdf468
Matthew Beck <me@mattrb.com>
2026-07-10 18:11:35 +0000

    e6378eb fix(docs-infra): avoid search dialog empty-state flicker while re-querying

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-1fb4678
+ * @license Angular v22.1.0-next.5+sha-e6378eb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-1fb4678');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-e6378eb');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
