153753778b97ed6090a7c1753f0bd178bbba1347
GitHub <noreply@github.com>
2026-07-10 21:33:02 +0000

    f48d276 refactor(devtools): add static nodes for html-only block content in the component tree

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-4fad2c0
+ * @license Angular v22.1.0-next.5+sha-f48d276
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-4fad2c0');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-f48d276');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
