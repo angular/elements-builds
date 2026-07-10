1697ca176ee578fd4052e7dd188c08accbe9d870
Matthew Beck <me@mattrb.com>
2026-07-10 01:04:17 +0000

    1fb4678 Revert "fix(core): allow static attributes for explicit input transforms"

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-b178e83
+ * @license Angular v22.1.0-next.5+sha-1fb4678
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-b178e83');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-1fb4678');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
