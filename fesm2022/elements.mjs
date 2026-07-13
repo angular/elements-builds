b0caceb322746b1d8b4af2dafa9cdded7f656cc5
Matthew Beck <me@mattrb.com>
2026-07-13 18:57:23 +0000

    f6a00ff docs(docs-infra): remove orphaned IDX config left behind by #68946

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-13d9cc3
+ * @license Angular v22.1.0-next.5+sha-f6a00ff
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-13d9cc3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-f6a00ff');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
