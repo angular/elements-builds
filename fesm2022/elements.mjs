2572e233df35a2dd2ebf2855214f543dc759456d
Matthew Beck <me@mattrb.com>
2026-07-14 18:09:37 +0000

    fb6b354 fix(platform-browser): prevent ReDoS in SOURCEMAP_URL_REGEXP

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-6d043f8
+ * @license Angular v22.1.0-next.5+sha-fb6b354
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-6d043f8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-fb6b354');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
