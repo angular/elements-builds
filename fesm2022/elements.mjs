c5b3c361017d790f34ea1a44047753000aa00ebf
Matthew Beck <me@mattrb.com>
2026-07-14 15:05:45 +0000

    ca37790 docs: add native html validation clarification in signal forms docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d5e0b13
+ * @license Angular v22.1.0-next.5+sha-ca37790
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d5e0b13');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-ca37790');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
