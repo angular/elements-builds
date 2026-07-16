e3b5c3fc2d4cd54300007c2f9f29add4410f83b1
Matthew Beck <me@mattrb.com>
2026-07-16 04:29:52 +0000

    265de87 docs: release notes for the v22.0.7 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d43acaf
+ * @license Angular v22.1.0-next.5+sha-265de87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d43acaf');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-265de87');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
