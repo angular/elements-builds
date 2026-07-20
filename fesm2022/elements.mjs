83399c8168ed26ccf2bd157a2646aa8ea1090fb3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 16:20:04 +0000

    223e402 fix(migrations): preserve NgClass import on partial migration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-79fe1c3
+ * @license Angular v22.1.0-next.6+sha-223e402
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-79fe1c3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-223e402');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
