a145eebf55506eeee379642056cd09325e9ec7dd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:55:53 +0000

    d6cad90 docs: fix broken link to Angular AI Skills page

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-f032f5a
+ * @license Angular v22.1.0-next.6+sha-d6cad90
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-f032f5a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-d6cad90');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
