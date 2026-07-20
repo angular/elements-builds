6c06c1ea4967eddb9d3263d25f4f167910b6aa6e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:26:17 +0000

    f032f5a docs: document the inlineStyleLanguage workspace config option

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-a7ebf1c
+ * @license Angular v22.1.0-next.6+sha-f032f5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-a7ebf1c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-f032f5a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
