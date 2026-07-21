d8541a25c3f052e647d285a4984599e5107d69b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 17:35:37 +0000

    388daea fix(migrations): correctly migrate ngClass with mixed space-separated keys

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-fb68711
+ * @license Angular v22.0.7+sha-388daea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-fb68711');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-388daea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
