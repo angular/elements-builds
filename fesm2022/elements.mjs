34c86bf8d2eb4dc4adb28fdbcd55e311f518c9ac
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 11:36:04 +0000

    a79eb0d build: update devinfra digest to 24f9214

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-8309412
+ * @license Angular v22.0.7+sha-a79eb0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-8309412');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-a79eb0d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
