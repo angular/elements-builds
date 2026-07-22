85d967d6b90e5c431a45b81b66db6031aad2b8e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 12:31:18 +0000

    ab52df4 fix(docs-infra): fail the guide build when a markdown file starts with a BOM

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-9a1e620
+ * @license Angular v22.1.0-next.6+sha-ab52df4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-9a1e620');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-ab52df4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
