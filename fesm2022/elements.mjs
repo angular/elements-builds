00290f6a04ec9c92ce4ac795daf7d4d719d58779
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:06:14 +0000

    e62905e docs: update stale Twitter reference to X in the URL matcher guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-66f27a4
+ * @license Angular v22.0.7+sha-e62905e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-66f27a4');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-e62905e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
