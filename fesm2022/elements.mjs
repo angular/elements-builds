6372e4d806bf424f45b57c52bb262da0227a7c10
GitHub <noreply@github.com>
2026-07-24 20:29:09 +0000

    9949dcc fix(http): enable xsrf for root-provided HttpClient

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.18+sha-5d00aa2
+ * @license Angular v21.2.18+sha-9949dcc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.18+sha-5d00aa2');
+const VERSION = /* @__PURE__ */new Version('21.2.18+sha-9949dcc');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
