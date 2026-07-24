45cdd408249058ff51ded3fafb2851c89b8a54a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 20:51:58 +0000

    a99fb91 fix(language-service): account for strictTemplates being enabled by default

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-b2978db
+ * @license Angular v22.1.0-rc.0+sha-a99fb91
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-b2978db');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-a99fb91');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
