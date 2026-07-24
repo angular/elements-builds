bf7166bce896b46d74c5b0f555170fe8078d28ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:41:34 +0000

    1784530 docs: modernize directives guides (#69822)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-28d59e8
+ * @license Angular v22.2.0-next.0+sha-1784530
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-28d59e8');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1784530');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
