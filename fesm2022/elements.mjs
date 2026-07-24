03e54eefa3b987b83772b2b1dd5163131e990325
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:33:12 +0000

    433276b docs: use https for external links in adev

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-548ef47
+ * @license Angular v22.0.8+sha-433276b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-548ef47');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-433276b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
