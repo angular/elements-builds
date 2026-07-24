d8082bc4dfbdfc8bef04fdc0c338e4ce554de182
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 21:01:11 +0000

    d346905 fix(migrations): correctly detect `then`/`else` keywords in control flow migration

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-232cfea
+ * @license Angular v22.0.8+sha-d346905
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-232cfea');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-d346905');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
