4c7a12eee9bf5d8d034bf281e9540b6ce65e3375
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:54:38 +0000

    07cb840 release: cut the v21.2.19 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.18+sha-7b884f5
+ * @license Angular v21.2.19+sha-07cb840
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.18+sha-7b884f5');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-07cb840');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
