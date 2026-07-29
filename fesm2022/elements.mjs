e9896268e665895c83a93e14c1077214fa465bba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:54:22 +0000

    098d28f docs: fix self-referential link in defer block description

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-a7d67f9
+ * @license Angular v22.1.0-rc.0+sha-098d28f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-a7d67f9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-098d28f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
