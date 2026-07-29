ba5167529d7d9a2c0f70a7dc775662bc82aa9426
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 17:07:47 +0000

    7825e60 docs: invoke signal inputs in adev examples and tutorials

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-098d28f
+ * @license Angular v22.1.0-rc.0+sha-7825e60
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-098d28f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-7825e60');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
