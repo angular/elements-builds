62a44957412b59d592592233882ca448a6ff53c0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 15:59:04 +0000

    db0d4a1 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.26+sha-4d62760
+ * @license Angular v20.3.26+sha-db0d4a1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.26+sha-4d62760');
+const VERSION = /* @__PURE__ */ new Version('20.3.26+sha-db0d4a1');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
