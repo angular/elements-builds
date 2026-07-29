e4ed151c6215ffa82a89c5d1f22461ff2f675bfb
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:47:48 +0000

    d366e4b release: cut the v22.1.0 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-7825e60
+ * @license Angular v22.1.0+sha-d366e4b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-7825e60');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d366e4b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
