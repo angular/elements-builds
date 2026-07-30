6fa3f9ef1ea4c4cb461ee93ca462525ae20e01ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 15:52:29 +0000

    644455b refactor(core): update acceptance test

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-c4b1432
+ * @license Angular v22.1.0+sha-644455b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-c4b1432');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-644455b');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
