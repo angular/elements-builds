5f0caa7fd587318f68d734a76e0df5a757f288e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 17:04:45 +0000

    01eeee4 docs: replace Firebase Studio references with Antigravity

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-949de9c
+ * @license Angular v22.1.0+sha-01eeee4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-949de9c');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-01eeee4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
