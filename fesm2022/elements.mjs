5f686ea9dc218b86e647a721c996274400f149c7
GitHub <noreply@github.com>
2026-07-30 15:35:31 +0000

    6afe6fa fix(core): sanitize host bindings on concrete hosts

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-07cb840
+ * @license Angular v21.2.19+sha-6afe6fa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-07cb840');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
