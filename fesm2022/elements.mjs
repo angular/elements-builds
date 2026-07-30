56b005a17d6f4e080c0053e495a4189a7b1ae833
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 15:27:37 +0000

    c4b1432 docs: document overlay stacking change in v21 update guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-d366e4b
+ * @license Angular v22.1.0+sha-c4b1432
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d366e4b');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-c4b1432');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
