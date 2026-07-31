08d186a3f67e17d8c402de5a4944d66eadfa301d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 18:24:48 +0000

    3a29329 fix(devtools): Extension name for non googlers.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-690eecd
+ * @license Angular v22.2.0-next.0+sha-3a29329
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-690eecd');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-3a29329');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
