fe36bbd6e171294e5f7304c7209b81addabc4c74
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 19:37:01 +0000

    aa6d318 fix(core): initialize hydration triggers after late runtime activation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-d6ec7c7
+ * @license Angular v22.1.0+sha-aa6d318
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d6ec7c7');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-aa6d318');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
