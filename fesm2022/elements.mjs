2b05a779bed8d29b0e10eb8262a68ca297641f7e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 15:45:05 +0000

    690eecd docs: document deprecated allowSignalWrites option in v19 update guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-245e6f3
+ * @license Angular v22.2.0-next.0+sha-690eecd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-245e6f3');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-690eecd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
