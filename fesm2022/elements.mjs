e43e5c12ae5be6623a285b99656c27cbe1f64f3a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 17:38:55 +0000

    6f86d66 docs: fix supported Node.js version in installation prerequisites

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-ac3728e
+ * @license Angular v22.1.0+sha-6f86d66
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-ac3728e');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-6f86d66');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
