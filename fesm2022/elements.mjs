16e51b00fe2b0481fc4c350323bd78f1c2cfc0ae
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 23:28:59 +0000

    4631a6e refactor(forms): modernize Signal Forms tests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-adb770b
+ * @license Angular v22.2.0-next.1+sha-4631a6e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-adb770b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-4631a6e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
