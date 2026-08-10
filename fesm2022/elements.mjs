fd86584c50900a3dba25d878c76db3384fbd3a11
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:15:53 +0000

    768c8c2 fix(docs-infra): contain scroll within version picker dropdown

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-b95f9e2
+ * @license Angular v22.2.0-next.1+sha-768c8c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-b95f9e2');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-768c8c2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
