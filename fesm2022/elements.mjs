fd20d3ff90677c151920b7ab8b0c5666e8fd9ab1
Alon Mishne <amishne@users.noreply.github.com>
2026-08-10 22:43:17 +0000

    0df9d5e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-596356e
+ * @license Angular v22.1.1+sha-0df9d5e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-596356e');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0df9d5e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
