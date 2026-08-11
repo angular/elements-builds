64497faea3a3f7c8441a4445842c70bc73f2b979
Alon Mishne <amishne@users.noreply.github.com>
2026-08-11 19:14:29 +0000

    2f82601 fix(router): limit protocol-relative URL handling to serialization

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-65e2aed
+ * @license Angular v22.1.1+sha-2f82601
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-65e2aed');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-2f82601');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
