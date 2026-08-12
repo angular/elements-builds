515c524b9b34937a3fd6709a5bb4dc07e798744d
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-12 18:20:14 +0000

    3d32db6 docs: clarify animate.leave child animation behavior

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-435f8b2
+ * @license Angular v22.2.0-next.1+sha-3d32db6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-435f8b2');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-3d32db6');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
