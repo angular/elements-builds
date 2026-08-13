681adc9082b4c46f46ceb1b24cded4c66e30ffd4
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 00:12:19 +0000

    dca0a77 release: cut the v21.2.20 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-caf6166
+ * @license Angular v21.2.20+sha-dca0a77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-caf6166');
+const VERSION = /* @__PURE__ */new Version('21.2.20+sha-dca0a77');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
