0e3af1d7d221fafbff7956d4ea27f7ed2a6b8a9c
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 19:06:05 +0000

    148fee3 release: cut the v22.1.2 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-0a708e9
+ * @license Angular v22.1.2+sha-148fee3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0a708e9');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-148fee3');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
