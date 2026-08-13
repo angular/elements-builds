04cd6741a32add56975269da4ab968048c2304b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-13 16:04:04 +0000

    0a708e9 docs: note SafeResourceUrl audio src change

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-910f391
+ * @license Angular v22.1.1+sha-0a708e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-910f391');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0a708e9');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
