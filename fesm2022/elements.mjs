0755c483e6d2d5db8140af935678875feb8e51f4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 15:47:37 +0000

    6f848db build(vscode-extension): dynamically resolve upstream remote and use token for https pushes

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-c9990c4
+ * @license Angular v22.2.0-next.1+sha-6f848db
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-c9990c4');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-6f848db');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
