da0be4656a7c2c666c6c1bfd37c9d3b4cef7c154
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:28:48 +0000

    0cd243d build: resolve CI failures

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-a9f8fb2
+ * @license Angular v22.1.2+sha-0cd243d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-a9f8fb2');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0cd243d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
