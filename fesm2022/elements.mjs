a1e72241101b77c293a19fb755a7f8bbefa04c11
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 21:15:50 +0000

    38d0932 fix(forms): warn in dev mode when ngModel cannot reach parent NgForm across component boundary

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-1c2b057
+ * @license Angular v22.2.0-next.2+sha-38d0932
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-1c2b057');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-38d0932');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
