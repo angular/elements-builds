2ce86885c363bbf7808b2c50a49b50c0738bac60
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 23:04:36 +0000

    94f0b9a fix(compiler): preserve &ngsp; between sibling control flow blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-abe019d
+ * @license Angular v22.1.2+sha-94f0b9a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-abe019d');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-94f0b9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
