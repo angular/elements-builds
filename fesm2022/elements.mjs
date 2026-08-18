ec1fe01f778c08fea6a049890dea7a9116257dd2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 16:52:00 +0000

    49f4e15 ci: exclude markdown files from requiring dev-infra review

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-4413711
+ * @license Angular v22.1.2+sha-49f4e15
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-4413711');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-49f4e15');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
