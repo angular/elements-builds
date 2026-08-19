fcb573a4ae4dcb8822c45e2c8e17004e27239644
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 19:53:40 +0000

    e041f94 refactor(core): pass AbortSignal to WebMCP tools to handle cancellation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-7e39b4a
+ * @license Angular v22.2.0-next.2+sha-e041f94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-7e39b4a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-e041f94');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
