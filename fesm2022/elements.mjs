f1d8d9cb81452340061b7ac132e4ba61c0168201
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 18:17:10 +0000

    7e39b4a docs: update signal forms skill to match the current implementation

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-80a04eb
+ * @license Angular v22.2.0-next.2+sha-7e39b4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-80a04eb');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-7e39b4a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
