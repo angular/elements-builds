8cbd67bb12a445ee8d4e525473051a6329f6993e
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 18:22:01 +0000

    2e2c426 fix(compiler-cli): deduplicate deferred imports across multiple blocks

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-d609cf6
+ * @license Angular v22.2.0-next.3+sha-2e2c426
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-d609cf6');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e2c426');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
