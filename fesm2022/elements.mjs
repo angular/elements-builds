607c61786ac7efe892ae75326058e7fce1a682b4
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 19:07:46 +0000

    238d8bf refactor(docs-infra): hold editor diagnostics in a signal

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-2e2c426
+ * @license Angular v22.2.0-next.3+sha-238d8bf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e2c426');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-238d8bf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
