090dcc5578d10defd269e5443e994b6a3b2a6af6
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:48:41 +0000

    d953d2e docs: add missing tap import and AppConfig interface to environment configuration reference

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-719e1ad
+ * @license Angular v22.2.0-next.3+sha-d953d2e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-719e1ad');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-d953d2e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
