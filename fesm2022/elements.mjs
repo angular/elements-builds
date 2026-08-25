3cc701493c9c102ef5ee909dcec24ab1d467b546
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 23:27:40 +0000

    a46292a fix(compiler-cli): default template diagnostic related message source file to template

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-ddfd8a8
+ * @license Angular v22.2.0-next.3+sha-a46292a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-ddfd8a8');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-a46292a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
