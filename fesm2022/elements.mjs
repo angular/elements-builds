57b843dcdf1488a51efca45edcede4203a729496
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 17:43:11 +0000

    07605ba feat(devtools): implement CD analyzer (#70254)

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-90fc947
+ * @license Angular v22.2.0-next.3+sha-07605ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-90fc947');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-07605ba');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
