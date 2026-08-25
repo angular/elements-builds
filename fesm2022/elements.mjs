e1a2060b951adc569f29a596529e0022c97b8973
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 17:13:23 +0000

    5aa6d97 fix(platform-server): avoid stripping unicode whitespace during url resolution

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.21+sha-73d8bbd
+ * @license Angular v21.2.21+sha-5aa6d97
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.21+sha-73d8bbd');
+const VERSION = /* @__PURE__ */new Version('21.2.21+sha-5aa6d97');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
