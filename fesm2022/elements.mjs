550735d85128280ee149b157e8b4b84b264436ba
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 22:28:14 +0000

    ddfd8a8 refactor(docs-infra): remove example configs for tooling that no longer exists

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-3848b7f
+ * @license Angular v22.2.0-next.3+sha-ddfd8a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-3848b7f');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-ddfd8a8');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
