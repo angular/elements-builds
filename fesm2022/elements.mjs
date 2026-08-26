ce5707bbb2f397aaf92bf5ae4d5e1ecca87b02fb
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 23:41:50 +0000

    4ee0421 release: cut the v20.3.30 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.29+sha-9339a7a
+ * @license Angular v20.3.30+sha-4ee0421
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -410,7 +410,7 @@ function createCustomElement(component, config) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.29+sha-9339a7a');
+const VERSION = /* @__PURE__ */ new Version('20.3.30+sha-4ee0421');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
