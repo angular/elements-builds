a5f52e42e3c677a61d12fddf274b76ce59b750b9
leonsenft <leonsenft@users.noreply.github.com>
2026-08-26 20:56:16 +0000

    151a8f2 refactor(core): Decouple ɵɵFactoryDeclaration and ɵɵInjectableDeclaration from type parameter T.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-b199bdf
+ * @license Angular v22.1.3+sha-151a8f2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-b199bdf');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-151a8f2');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
