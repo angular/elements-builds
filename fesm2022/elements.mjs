3583044547239bcbb98d72c0ec1959a27b0ea465
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 23:46:05 +0000

    575bb6e docs: release notes for the v20.3.30 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-2e6a057
+ * @license Angular v22.2.0-next.3+sha-575bb6e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e6a057');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-575bb6e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
