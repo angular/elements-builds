24697a50ceec8594634742e55e48cfbfffd30cff
Matthew Beck <me@mattrb.com>
2026-08-27 20:53:24 +0000

    ad22e89 refactor(devtools): point the .gitignore comment at live GitHub docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-6f1dc26
+ * @license Angular v22.2.0-next.4+sha-ad22e89
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-6f1dc26');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-ad22e89');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
