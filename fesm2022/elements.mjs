5beadbe605a6b79c06ab83f6d4880bd04146e271
Matthew Beck <me@mattrb.com>
2026-08-28 04:15:50 +0000

    60f8a78 refactor(core): defer foreign component rendering to post-update pass

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-bee058e
+ * @license Angular v22.1.4+sha-60f8a78
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-bee058e');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-60f8a78');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
