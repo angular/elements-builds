f970126de613fad80f6e3d55cd46cc0f67e931b3
Matthew Beck <me@mattrb.com>
2026-08-31 22:29:38 +0000

    4c4a705 fix(zone.js): preserve accessor and non-enumerable event listener options

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-0904f90
+ * @license Angular v22.2.0-next.4+sha-4c4a705
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-0904f90');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-4c4a705');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
