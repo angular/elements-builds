11a64bba3ece9c00470cc63013fb23ea468aa10d
Matthew Beck <me@mattrb.com>
2026-08-31 20:31:00 +0000

    b3bb36a fix(platform-server): resolve HTTP(S) URLs without authority as relative during SSR

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-d9afca0
+ * @license Angular v22.2.0-next.4+sha-b3bb36a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-d9afca0');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-b3bb36a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
