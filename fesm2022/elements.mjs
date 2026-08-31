b2ea23806f83fd30e59b4a5b970edd2382ba3375
Matthew Beck <me@mattrb.com>
2026-08-31 18:37:56 +0000

    341bc0d fix(docs-infra): reject unclosed paired docs elements

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-ffbb7e6
+ * @license Angular v22.1.4+sha-341bc0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-ffbb7e6');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-341bc0d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
