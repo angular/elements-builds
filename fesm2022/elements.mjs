f5b87e8805f84cc4354013f02179c477635cbb89
Matthew Beck <me@mattrb.com>
2026-09-01 21:51:29 +0000

    b2903de ci: move `packages/private` under `fw-general`

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-d90698d
+ * @license Angular v22.1.4+sha-b2903de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-d90698d');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
