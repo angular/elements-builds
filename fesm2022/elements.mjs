a47b5115b54b2dfc032f589e8327f60fb66e5376
Matthew Beck <me@mattrb.com>
2026-09-02 23:16:47 +0000

    731b959 fix(vscode-extension): prevent JSDoc link provider injection

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-94b1d3d
+ * @license Angular v22.1.4+sha-731b959
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-94b1d3d');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-731b959');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
