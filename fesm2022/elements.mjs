cb3324947edbf845b80ac708cfa07ca7295f5613
Matthew Beck <me@mattrb.com>
2026-09-02 22:59:02 +0000

    a66fd64 build: update cross-repo angular dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-de91d10
+ * @license Angular v22.2.0-next.4+sha-a66fd64
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-de91d10');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-a66fd64');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
