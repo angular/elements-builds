f8d274c0fd245ed124645ff3434b27fe5600739e
Matthew Beck <me@mattrb.com>
2026-09-02 21:41:47 +0000

    94b1d3d docs: correct openFiles entries that point at missing files

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-0a1bbbe
+ * @license Angular v22.1.4+sha-94b1d3d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-0a1bbbe');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-94b1d3d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
