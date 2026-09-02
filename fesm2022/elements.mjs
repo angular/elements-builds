eed23946c679f29ecaa5de932f63d72a4a3fe5b7
Matthew Beck <me@mattrb.com>
2026-09-02 21:27:42 +0000

    0a1bbbe release: cut the zone.js-0.16.3 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-b2903de
+ * @license Angular v22.1.4+sha-0a1bbbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-0a1bbbe');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
