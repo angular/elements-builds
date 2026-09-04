50794714061808a0cd3f7f3f3fc3a0f88e0c50d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:55:22 +0000

    ef48630 test(core): replace act with actAsync

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.5+sha-af219ee
+ * @license Angular v22.1.5+sha-ef48630-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.5+sha-af219ee');
+const VERSION = /* @__PURE__ */new Version('22.1.5+sha-ef48630-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
