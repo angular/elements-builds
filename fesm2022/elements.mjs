e99ea10810949e43453d51f4aa8bc681dd5ac32a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:06:15 +0000

    5afdd98 fix(docs-infra): remove space after decorator symbol Handle the separate whitespace token emitted by Shiki when a decorator name is linked to its API reference.

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-9a58353
+ * @license Angular v22.2.0-next.5+sha-5afdd98
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-9a58353');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-5afdd98');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
