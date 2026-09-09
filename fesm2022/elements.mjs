de27644f9c4f2f119ec370e87fce0df0dda98736
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:38:32 +0000

    8576f16 fix(docs-infra): highlight home page code for the resolved theme

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-679c50d
+ * @license Angular v22.2.0-next.7+sha-8576f16
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-679c50d');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8576f16');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
