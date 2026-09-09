04a6f415829aff21091622a9a0ead40fc8d14250
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:01:26 +0000

    81385ea release: cut the v21.2.23 release

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.22+sha-fc2e8fb
+ * @license Angular v21.2.23+sha-81385ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -285,7 +285,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.22+sha-fc2e8fb');
+const VERSION = /* @__PURE__ */new Version('21.2.23+sha-81385ea');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
