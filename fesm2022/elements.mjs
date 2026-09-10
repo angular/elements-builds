deeaed0fcca5ddc6ae7832aab7ba2e2e5e749616
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:14:40 +0000

    29f9dd5 refactor(devtools): update angular optimization plugin for JavaScriptTransformer API

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-b9fb456
+ * @license Angular v22.2.0-next.7+sha-29f9dd5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-b9fb456');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-29f9dd5');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
