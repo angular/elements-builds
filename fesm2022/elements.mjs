eb23fa0f46165d7c30498f9d4e644764f22a0306
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:02:42 +0000

    14d6999 docs(forms): close the unterminated code fences in the compatForm docs

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-66de56b
+ * @license Angular v22.1.6+sha-14d6999
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-66de56b');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-14d6999');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
