1bbf8a98eaf69efd0834349cebbca62e51ff268f
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:23:15 +0000

    376b71e docs(forms): document what required() considers empty

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-4b5571c
+ * @license Angular v22.1.6+sha-376b71e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-4b5571c');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-376b71e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
