d6ff041f9035566805cd2545b195600d0bd9f986
Andrew Scott <atscott01@gmail.com>
2026-09-11 20:20:36 +0000

    4b5571c docs: import bootstrapApplication from platform-browser in the CSP example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-c4bd3e2
+ * @license Angular v22.1.6+sha-4b5571c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-c4bd3e2');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-4b5571c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
