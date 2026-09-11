52d52ad087a2358756f36d3f43d6a869a903525b
Andrew Scott <atscott01@gmail.com>
2026-09-11 18:08:39 +0000

    d0ed76c docs: fix the broken housing location template in the first-app tutorial

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-01d091b
+ * @license Angular v22.2.0-next.7+sha-d0ed76c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-01d091b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-d0ed76c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
