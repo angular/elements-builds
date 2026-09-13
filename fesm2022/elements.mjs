d0e9b619260419b9f6db646750338cc6205ccdaf
Andrew Scott <atscott01@gmail.com>
2026-09-13 21:25:55 +0000

    c60c41e test(core): reduce fakeAsync usage in animation tests

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-fc2f6fd
+ * @license Angular v22.2.0-next.7+sha-c60c41e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-fc2f6fd');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-c60c41e');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
