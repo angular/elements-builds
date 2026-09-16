11fea7c5477e541f3bfa69e59d83f571400344a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 13:19:15 +0000

    47afc90 docs: close the heading tag in the ng-template output example

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-75cbcde
+ * @license Angular v22.1.6+sha-47afc90
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-75cbcde');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-47afc90');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
