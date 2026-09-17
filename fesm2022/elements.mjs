6e2542feee48cac8eb71ecb14426e6b1167cde7d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 18:07:43 +0000

    7e678e0 fix(core): cancel leave animations in nested views during list reordering

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6e3ce79
+ * @license Angular v22.3.0-next.0+sha-7e678e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6e3ce79');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-7e678e0');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
