dc13eea99e87a037badc9caa915a2bdf7c4fa2f2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 17:53:29 +0000

    6e3ce79 docs: use the remote-cache config in the bazel caching guide

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-9ba5faa
+ * @license Angular v22.3.0-next.0+sha-6e3ce79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-9ba5faa');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6e3ce79');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
