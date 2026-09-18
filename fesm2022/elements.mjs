19860a98f157d37128607da8e77d939d213eafc2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-18 16:53:08 +0000

    7569a02 docs(core): add error guide for NG0600

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-1151eb9
+ * @license Angular v22.3.0-next.0+sha-7569a02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-1151eb9');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-7569a02');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
