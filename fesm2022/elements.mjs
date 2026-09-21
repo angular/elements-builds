f352c7b732af4c2acb09c430f98bfce4405b07f5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 18:38:28 +0000

    b0d4010 test(router): Fix broken test

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6688a3d-with-local-changes
+ * @license Angular v22.3.0-next.0+sha-b0d4010-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6688a3d-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-b0d4010-with-local-changes');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
