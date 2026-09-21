a59cdc904f97f2148511caa1638c158f8afa6432
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 16:55:47 +0000

    34c84cd docs: fix stray markup in the image optimization and zoneless guides

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-7569a02
+ * @license Angular v22.3.0-next.0+sha-34c84cd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-7569a02');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-34c84cd');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
