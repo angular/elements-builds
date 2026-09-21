e220ce33088a36cea54a08cea0486745811efb01
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 19:15:06 +0000

    805aa91 docs: update `disabledReasons` and `transformedValue` examples

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-78c7ab3
+ * @license Angular v22.1.7+sha-805aa91
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-78c7ab3');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-805aa91');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
