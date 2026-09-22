1ef70910de31aea8e4a97298dfbc0b7613fb0cda
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:16:53 +0000

    bc402cf fix(core): don't use plain objects as maps for jsaction data

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-378ecfc
+ * @license Angular v22.2.0-rc.0+sha-bc402cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-378ecfc');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-bc402cf');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
