db992593d7f8b1b18fb235540a50130355c26bee
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:24:34 +0000

    249f55f docs: correct the adev writing guide skill

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6cae42c
+ * @license Angular v22.3.0-next.0+sha-249f55f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-249f55f');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
