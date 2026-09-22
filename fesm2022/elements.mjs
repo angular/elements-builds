655879d606389b97c46408e146e0f45991742db7
Alan Agius <alan.agius4@gmail.com>
2026-09-22 18:04:36 +0000

    6cae42c build: update all non-major dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-aa1cc47
+ * @license Angular v22.3.0-next.0+sha-6cae42c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-aa1cc47');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
