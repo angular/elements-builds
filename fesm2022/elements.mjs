3ef4ba6e9214631199e600c38f7be0fc7b1b80e5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:54:29 +0000

    afcda88 docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-249f55f
+ * @license Angular v22.3.0-next.0+sha-afcda88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-249f55f');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-afcda88');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
