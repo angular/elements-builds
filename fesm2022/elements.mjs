bfc0aff725a0250943f8508e5a00b028e4d52746
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:09:39 +0000

    5b52d19 build: update all non-major dependencies

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-742fd44
+ * @license Angular v22.3.0-next.0+sha-5b52d19
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-742fd44');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-5b52d19');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
