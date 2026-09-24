8b9d3d4e0d5883893b09d34b4f81fdbb94329c52
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:03:13 +0000

    7f9c14d docs: Include closing CSS bracket in tutorial

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-4545d66
+ * @license Angular v22.2.0+sha-7f9c14d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-4545d66');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-7f9c14d');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
