1ceef9e7f24acd6aff7d555592883d09088fcf34
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:49:22 +0000

    756e8c4 docs: update releases.md for v22.2

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-870f200
+ * @license Angular v22.2.0+sha-756e8c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-870f200');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-756e8c4');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
