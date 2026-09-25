b4c770a55687d28313aa18cef0058c8e991a0aa2
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:18:22 +0000

    d3eaf9a fix(docs-infra): render one card per API member

--- fesm2022/elements.mjs
+++ fesm2022/elements.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-e4fc745
+ * @license Angular v22.3.0-next.0+sha-d3eaf9a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -290,7 +290,7 @@ function createCustomElement(component, config) {
   return NgElementImpl;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-e4fc745');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-d3eaf9a');
 
 export { NgElement, VERSION, createCustomElement };
 //# sourceMappingURL=elements.mjs.map
